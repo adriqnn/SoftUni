2d185b0d19af53d9120dac09dcfe8e50758220cc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-09-26 03:44:48 +0300

    _06_NestedLoopsExercise

---
+++ 01_Programming_Basics/_06_NestedLoopsExercise/_01_NumberPyramid.java
@@ -0,0 +1,28 @@
+package _01_Programming_Basics._06_NestedLoopsExercise;
+
+import java.util.Scanner;
+
+public class _01_NumberPyramid {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = Integer.parseInt(scan.nextLine());
+
+        int current = 1;
+        boolean isBigger = false;
+        for (int rows = 1; rows <= num; rows++) {
+            for (int cols = 1; cols <= rows; cols++) {
+                if (current > num){
+                    isBigger = true;
+                    break;
+                }
+                System.out.print(current + " ");
+                current++;
+            }
+            if (isBigger){
+                break;
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 01_Programming_Basics/_06_NestedLoopsExercise/_03_SumPrimeNonPrime.java
@@ -0,0 +1,37 @@
+package _01_Programming_Basics._06_NestedLoopsExercise;
+
+import java.util.Scanner;
+
+public class _03_SumPrimeNonPrime {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String console = scan.nextLine();
+
+        int prime = 0;
+        int nonprime =0;
+
+        while(!console.equals("stop")){
+            boolean isPrime = true;
+            int num = Integer.parseInt(console);
+            if (num<0){
+                System.out.println("Number is negative.");
+                console = scan.nextLine();
+                continue;
+            }
+            for (int i = 2; i < num; i++) {
+                if (num%i == 0){
+                    nonprime += num;
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime){
+                prime +=num;
+            }
+            console = scan.nextLine();
+        }
+        System.out.printf("Sum of all prime numbers is: %d%n",prime);
+        System.out.printf("Sum of all non prime numbers is: %d", nonprime);
+    }
+}
