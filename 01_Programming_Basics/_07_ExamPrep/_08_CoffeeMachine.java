d174c4ceba70eb9ada4d2c8e0a168c4b81553123
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-09-26 03:51:26 +0300

    _07_ExamPrep

---
+++ 01_Programming_Basics/_07_ExamPrep/_08_CoffeeMachine.java
@@ -0,0 +1,58 @@
+package _01_Programming_Basics._07_ExamPrep;
+
+import java.util.Scanner;
+
+public class _08_CoffeeMachine {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String type = scan.nextLine();
+        String sugar = scan.nextLine();
+        int amount = Integer.parseInt(scan.nextLine());
+
+        double price = 0;
+        switch(type){
+            case "Espresso":
+                if(sugar.equals("Without")){
+                    price = amount*0.9*0.65;
+                    if (amount>=5){
+                        price *= 0.75;
+                    }
+                }else if(sugar.equals("Normal")){
+                    price = amount*1.00;
+                    if (amount>=5){
+                        price *= 0.75;
+                    }
+                }else if(sugar.equals("Extra")){
+                    price =  amount*1.20;
+                    if (amount>=5){
+                        price *= 0.75;
+                    }
+                }
+                break;
+            case "Cappuccino":
+                if(sugar.equals("Without")){
+                    price = amount*1.00*0.65;
+                }else if(sugar.equals("Normal")){
+                    price = amount*1.20;
+                }else if(sugar.equals("Extra")){
+                    price =  amount*1.60;
+                }
+                break;
+            case "Tea":
+                if(sugar.equals("Without")){
+                    price = amount*0.5*0.65;
+                }else if(sugar.equals("Normal")){
+                    price = amount*0.60;
+                }else if(sugar.equals("Extra")){
+                    price =  amount*0.70;
+                }
+                break;
+        }
+
+        if (price>15){
+            System.out.printf("You bought %d cups of %s for %.2f lv.",amount,type,price*0.8);
+        }else
+            System.out.printf("You bought %d cups of %s for %.2f lv.",amount,type,price);
+    }
+}
