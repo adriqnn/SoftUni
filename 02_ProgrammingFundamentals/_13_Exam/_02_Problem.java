26480a1c3d2c1513b061d433005ff38dda0d2769
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-09-29 20:38:10 +0300

    _13_Exam

---
+++ 02_ProgrammingFundamentals/_13_Exam/_02_Problem.java
@@ -0,0 +1,30 @@
+package _02_ProgrammingFundamentals._13_Exam;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class _02_Problem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = Integer.parseInt(scan.nextLine());
+        String regex = "(?<grp1>.+)>(?<grp2>[0-9]{3})\\|(?<grp3>[a-z]{3})\\|(?<grp4>[A-Z]{3})\\|(?<grp5>[^><]{3})<(\\1)";
+        Pattern pattern = Pattern.compile(regex);
+
+        for (int i = 0; i < n; i++) {
+            String console = scan.nextLine();
+            Matcher matcher = pattern.matcher(console);
+            if(matcher.find()){
+                String one = matcher.group("grp2");
+                String two = matcher.group("grp3");
+                String three = matcher.group("grp4");
+                String four = matcher.group("grp5");
+                StringBuilder password = new StringBuilder();
+                System.out.println("Password: " + one + two + three + four);
+            }else {
+                System.out.println("Try another password!");
+            }
+        }
+    }
+}
---
+++ 02_ProgrammingFundamentals/_13_Exam/_03_Problem.java
@@ -0,0 +1,71 @@
+package _02_ProgrammingFundamentals._13_Exam;
+
+import java.util.*;
+
+public class _03_Problem {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        Map<String, List<String>> guestLikes = new TreeMap<>();
+        Map<String, List<String>> guestUnlike = new HashMap<>();
+
+        String console = scan.nextLine();
+
+        int unlikedMeals = 0;
+        while(!console.equals("Stop")){
+            String[] commandLine = console.split("-");
+            String command = commandLine[0];
+            String guestName = commandLine[1];
+            String mealName = commandLine[2];
+            switch(command){
+                case "Like":
+                    if(!guestLikes.containsKey(guestName)){
+                        guestLikes.put(guestName,new ArrayList<>());
+                    }
+                    if(!guestLikes.get(guestName).contains(mealName)){
+                        guestLikes.get(guestName).add(mealName);
+                    }
+                    break;
+                case "Unlike":
+                    if (!guestLikes.containsKey(guestName)){
+                        System.out.printf("%s is not at the party.%n",guestName);
+                        console = scan.nextLine();
+                        continue;
+                    }
+                    if(guestLikes.get(guestName).contains(mealName)){
+                        guestLikes.get(guestName).remove(mealName);
+                        System.out.printf("%s doesn't like the %s.%n",guestName,mealName);
+                        unlikedMeals++;
+                    }else{
+                        System.out.printf("%s doesn't have the %s in his/her collection.%n",guestName,mealName);
+                    }
+                    break;
+            }
+            console = scan.nextLine();
+        }
+        guestLikes.entrySet().stream().sorted((m1,m2) -> Integer.compare(m2.getValue().size(), m1.getValue().size()))
+                .forEach(guest -> {
+                    System.out.print(guest.getKey() + ": ");
+                    System.out.print(guest.getValue().toString().replace("[", "").replace("]",""));
+                    System.out.println();
+                });
+        System.out.printf("Unliked meals: %d", unlikedMeals);
+    }
+}
+/*
+carsMileage.entrySet().stream().sorted((car1,car2) -> Integer.compare(car2.getValue(),car1.getValue()))
+                .forEach(carEntry -> {
+                    String carName = carEntry.getKey();
+                    System.out.printf("%s -> Mileage: %d kms, Fuel in the tank: %d lt.%n",carEntry.getKey(),carEntry.getValue(),carsFuel.get(carName));
+                });
+ */
+/*
+courses.entrySet().stream()
+                .sorted((e1,e2) -> Integer.compare(e2.getValue().size(), e1.getValue().size()))
+                .forEach(entry -> {
+                    System.out.println(entry.getKey() + ": " + entry.getValue().size());
+                    //entry.getValue().stream().sorted().forEach(studentName -> System.out.println("-- " + studentName));});
+                    List<String> students = entry.getValue();
+                    students.stream().sorted().forEach(studentName -> System.out.println("-- " + studentName));
+                });
+ */
