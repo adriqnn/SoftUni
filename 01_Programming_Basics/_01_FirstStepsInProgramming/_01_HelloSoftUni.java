4e1c3795491ac8b73911891e9abee75e64948caf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-09-29 20:53:04 +0300

    Delete 01_Programming_Basics directory

--- 01_Programming_Basics/_01_FirstStepsInProgramming/_01_HelloSoftUni.java
+++
@@ -1,7 +0,0 @@
-package _01_Programming_Basics._01_FirstStepsInProgramming;
-
-public class _01_HelloSoftUni {
-    public static void main(String[] args) {
-        System.out.println("Hello SoftUni");
-    }
-}
