0b0a8ebee5e774c8b19e241cfd24ce03e9b337c1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-03 00:10:58 +0300

    _09_UnitTestingExercise

---
+++ 04_JavaOOP/_09_UnitTestingExercise/main/java/_02_/Person.java
@@ -0,0 +1,20 @@
+package _04_JavaOOP._09_UnitTestingExercise.main.java._02_;
+
+public class Person {
+
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getUsername() {
+        return this.name;
+    }
+}
---
+++ 04_JavaOOP/_09_UnitTestingExercise/test/java/four/BubbleTest.java
@@ -0,0 +1,7 @@
+package unittesting.exercise.four;
+
+import static org.junit.Assert.*;
+
+public class BubbleTest {
+
+}
