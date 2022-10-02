35746e12c14d188321211e2e8153ba73c4e6baae
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-03 00:39:49 +0300

    _03_Aquarium

---
+++ 04_JavaOOP/_11_ExamPrep/_03_Aquarium/src/main/java/aquarium/entities/decorations/Ornament.java
@@ -0,0 +1,10 @@
+package aquarium.entities.decorations;
+
+public class Ornament extends BaseDecoration{
+    public static final int ORNAMENT_COMFORT = 1;
+    public static final double ORNAMENT_PRICE = 5.00;
+
+    public Ornament() {
+        super(ORNAMENT_COMFORT,ORNAMENT_PRICE);
+    }
+}
