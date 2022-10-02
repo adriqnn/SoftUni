1aeddbfb53378cb883eb0ff4800f0283e50b9a43
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-02 23:57:11 +0300

    _09_UnitTesting

---
+++ 04_JavaOOP/_09_UnitTesting/test/java/AxeTest.java
@@ -0,0 +1,31 @@
+package unittesting.rpg_lab;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AxeTest {
+
+    @BeforeClass
+
+    @Test
+    public void testWeaponLosesDurabilityAfterAttack(){
+        //Arrange
+        Axe axe = new Axe(10,10);
+        Dummy dummy = new Dummy(100,100);
+        //Act
+        axe.attack(dummy);
+        //Assert
+        Assert.assertEquals(9,axe.getDurabilityPoints());
+
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testAxeAttackThrowsIfAxeIsBroken(){
+        Axe brokenAxe = new Axe(10,0);
+        brokenAxe.attack(null);
+    }
+
+}
