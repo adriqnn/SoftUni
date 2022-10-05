a8705f793d9c08f963fff5461dd256e54df94001
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-05 23:53:39 +0300

    _05_SpringDataIntro

---
+++ 06_SpringData/_05_SpringDataIntro/src/main/java/com/example/spring_data_lab/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.spring_data_lab.repositories;
+
+import com.example.spring_data_lab.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
