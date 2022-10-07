957fb3e7513879b48bcac795b1cd90032934ca63
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-07 20:06:45 +0300

    Delete 09_JavaScriptAdvanced/_04_DOMIntroduction directory

--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_01_EditElements/_01_EditElement.js
+++
@@ -1,6 +0,0 @@
-function editElement(element, match, replacer){
-    const text = element.textContent;
-    const pattern = new RegExp(match, 'g');
-    const result = text.replace(pattern,replacer);
-    element.textContent = result;
-}
--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_02_CollectList/_02_CollectList.js
+++
@@ -1,5 +0,0 @@
-function extractText(){
-    const items = Array.from(document.querySelectorAll('li'));
-    const result = items.map(e => e.textContent).join('\n');
-    document.getElementById('result').value = result;
-}
