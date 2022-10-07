042d590510776375576a7faac6dd7efc41fb3207
adriqnn <109970483+adriqnn@users.noreply.github.com>
2022-10-07 20:06:57 +0300

    Delete 09_JavaScriptAdvanced/_04_DOMIntroductionExercise directory

--- 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_01_Subtraction/subtract.js
+++
@@ -1,6 +0,0 @@
-function subtract(){
-    let firstElementValue = Number(document.getElementById(`firstNumber`).value);
-    let secondElementValue = Number(document.getElementById(`secondNumber`).value);
-    let resultElement = document.getElementById('result');
-    resultElement.textContent = firstElementValue - secondElementValue;
-}
