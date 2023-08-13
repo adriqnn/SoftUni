8ce5ac892b653d2d0f36b0edaa997e34bd8515e5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-13 22:47:56 +0300

    hello softuni task change and added comment

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_01_HelloSoftUni.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_01_HelloSoftUni.java
@@ -2,6 +2,8 @@ package _01_ProgrammingBasics._01_FirstStepsInProgramming;
 
 public class _01_HelloSoftUni {
     public static void main(String[] args) {
+
+        // Print a greeting message
         System.out.println("Hello SoftUni");
     }
 }
