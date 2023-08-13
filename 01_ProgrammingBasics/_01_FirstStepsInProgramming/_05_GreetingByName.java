b14f18fab2a85779c0e93cd153da6766b8c171ac
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 00:25:06 +0300

    05

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_05_GreetingByName.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_05_GreetingByName.java
@@ -2,7 +2,7 @@ package _01_ProgrammingBasics._01_FirstStepsInProgramming;
 
 import java.util.Scanner;
 
-public class _04_InchesToCentimeters {
+public class _05_GreetingByName {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
