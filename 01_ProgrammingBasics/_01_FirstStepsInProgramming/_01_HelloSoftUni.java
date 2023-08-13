903b16be49821ab9a707f2765ddbb73c9a88021b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-13 23:19:33 +0300

    01

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_01_HelloSoftUni.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_01_HelloSoftUni.java
@@ -3,7 +3,7 @@ package _01_ProgrammingBasics._01_FirstStepsInProgramming;
 public class _01_HelloSoftUni {
     public static void main(String[] args) {
 
-        // Print a greeting message 
+        // Print a greeting message
         System.out.println("Hello SoftUni");
     }
 }
