2291b33465048d62378533ddebe52c012a78da26
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-13 23:20:05 +0300

    02

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_02_NumsFrom01To10.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_02_NumsFrom01To10.java
@@ -3,7 +3,7 @@ package _01_ProgrammingBasics._01_FirstStepsInProgramming;
 public class _02_NumsFrom01To10 {
     public static void main(String[] args) {
 
-        //Print numbers from 1 to 10 without a loop
+        // Print numbers from 1 to 10 without a loop
         System.out.println("1");
         System.out.println("2");
         System.out.println("3");
