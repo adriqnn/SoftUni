83c0f5a0d27b85d20a747caa714dd9ff2c83ef6b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:52:46 +0300

    05

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_05_SuppliesForSchool.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_05_SuppliesForSchool.java
@@ -6,16 +6,23 @@ public class _05_SuppliesForSchool {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         int numberOfPens = Integer.parseInt(scan.nextLine());
         int numberOfSharpies = Integer.parseInt(scan.nextLine());
         int litersOfCleaningSupplies = Integer.parseInt(scan.nextLine());
         int discountPercentage = Integer.parseInt(scan.nextLine());
 
+        // Calculate prices for each type of supply
         double pensPrice = numberOfPens * 5.80;
         double sharpiesPrice = numberOfSharpies * 7.20;
         double cleaningSuppliesPrice = litersOfCleaningSupplies * 1.20;
-        double totalPrice = (pensPrice + sharpiesPrice + cleaningSuppliesPrice) * ((100-discountPercentage) * 1.0/100);
 
-        System.out.println(totalPrice);
+        // Calculate total price with discount
+        double totalPrice = (pensPrice + sharpiesPrice + cleaningSuppliesPrice) * (1 - discountPercentage * 0.01);
+
+        // Print the total price
+        System.out.printf("%.2f%n", totalPrice);
+        
+        scan.close();
     }
 }
