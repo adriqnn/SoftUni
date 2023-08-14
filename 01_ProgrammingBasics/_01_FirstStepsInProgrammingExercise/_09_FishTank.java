3875816c48bbdc0e37fbc8ecf475b1846a6d1c15
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:59:29 +0300

    09

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_09_FishTank.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_09_FishTank.java
@@ -6,15 +6,24 @@ public class _09_FishTank {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         int length = Integer.parseInt(scan.nextLine());
         int width = Integer.parseInt(scan.nextLine());
         int height = Integer.parseInt(scan.nextLine());
         double percentage = Double.parseDouble(scan.nextLine());
 
+        // Calculate the volume of the tank in liters
         double volumeInLiters = length * width * height / 1000.0;
+
+        // Calculate the multiplier for water displacement
         double multiplier = (100 - percentage) / 100;
+
+        // Calculate the actual liters of water in the tank
         double litersOfWater = volumeInLiters * multiplier;
 
+        // Print the liters of water
         System.out.println(litersOfWater);
+        
+        scan.close();
     }
 }
