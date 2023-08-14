ef73c0e8114d35868373c2d905219cdcfbd6b236
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 15:37:07 +0300

    01

---
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercies/_01_TrapezoidArea.java
@@ -0,0 +1,22 @@
+package _01_ProgrammingBasics._01_FirstStepsInProgrammingMoreExercies;
+
+import java.util.Scanner;
+
+public class _01_TrapezoidArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input data
+        double basisOne = Double.parseDouble(scan.nextLine());
+        double basisTwo = Double.parseDouble(scan.nextLine());
+        double height = Double.parseDouble(scan.nextLine());
+
+        // Calculate the area of the trapezoid
+        double trapezoidArea = (basisOne + basisTwo) * height / 2;
+
+        // Print the calculated area with two decimal places
+        System.out.printf("%.2f%n", trapezoidArea);
+
+        scan.close();
+    }
+}
