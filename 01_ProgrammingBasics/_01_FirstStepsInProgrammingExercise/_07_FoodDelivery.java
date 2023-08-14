5971eec9aab4ce78e0437ffc106e5e0600da278f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:56:24 +0300

    07

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_07_FoodDelivery.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_07_FoodDelivery.java
@@ -6,17 +6,28 @@ public class _07_FoodDelivery {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         int chickenMenus = Integer.parseInt(scan.nextLine());
         int fishMenus = Integer.parseInt(scan.nextLine());
         int vegetarianMenus = Integer.parseInt(scan.nextLine());
 
+        // Calculate prices for each type of menu
         double priceForChickenMenus = chickenMenus * 10.35;
         double priceForFishMenus = fishMenus * 12.40;
         double priceForVegetarianMenus = vegetarianMenus * 8.15;
+
+        // Calculate total price for menus
         double totalPriceForMenus = priceForChickenMenus + priceForFishMenus + priceForVegetarianMenus;
+
+        // Calculate price for desserts (20%)
         double priceForDesserts = totalPriceForMenus * 0.20;
+
+        // Calculate the final total price including delivery
         double totalPrice = totalPriceForMenus + priceForDesserts + 2.50;
 
-        System.out.println(totalPrice);
+        // Print the final total price
+        System.out.printf("%.2f%n", totalPrice);
+        
+        scan.close();
     }
 }
