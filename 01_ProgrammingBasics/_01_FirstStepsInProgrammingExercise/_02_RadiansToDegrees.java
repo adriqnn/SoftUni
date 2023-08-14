8f5a4c2ef2cffc8bb279b1a07c248928e44d76df
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:40:31 +0300

    02

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_02_RadiansToDegrees.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_02_RadiansToDegrees.java
@@ -6,9 +6,15 @@ public class _02_RadiansToDegrees {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input radians
         double radians = Double.parseDouble(scan.nextLine());
-        double degrees = radians*180/Math.PI;
 
+        // Convert radians to degrees
+        double degrees = radians * 180 / Math.PI;
+
+        // Print the converted degrees
         System.out.println(degrees);
+        
+        scan.close();
     }
 }
