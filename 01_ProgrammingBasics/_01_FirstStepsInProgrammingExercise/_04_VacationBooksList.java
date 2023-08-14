8f27bca2a02d82e7ef64365fdbe096a61a5489ee
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:49:48 +0300

    04

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_04_VacationBooksList.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_04_VacationBooksList.java
@@ -6,13 +6,20 @@ public class _04_VacationBooksList {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         int numberOfPages = Integer.parseInt(scan.nextLine());
-        int pagesReadForAnHour = Integer.parseInt(scan.nextLine());
+        int pagesReadPerHour = Integer.parseInt(scan.nextLine());
         int daysOfReading = Integer.parseInt(scan.nextLine());
 
-        int hoursNeededForReading = numberOfPages/pagesReadForAnHour;
-        int hoursPerDay = hoursNeededForReading/daysOfReading;
+        // Calculate hours needed for reading
+        int hoursNeededForReading = numberOfPages / pagesReadPerHour;
 
+        // Calculate hours of reading per day
+        int hoursPerDay = hoursNeededForReading / daysOfReading;
+
+        // Print the hours of reading per day
         System.out.println(hoursPerDay);
+        
+        scan.close();
     }
 }
