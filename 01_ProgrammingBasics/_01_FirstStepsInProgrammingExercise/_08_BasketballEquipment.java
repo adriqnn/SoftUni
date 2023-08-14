79316f47ac17e61465af3a5e52c884f36afe8f30
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:57:40 +0300

    08

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_08_BasketballEquipment.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_08_BasketballEquipment.java
@@ -6,15 +6,21 @@ public class _08_BasketballEquipment {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         int trainingPrice = Integer.parseInt(scan.nextLine());
 
+        // Calculate prices for each equipment
         double shoes = trainingPrice * 0.6;
         double outfit = shoes * 0.8;
         double basketball = outfit * 0.25;
         double accessories = basketball * 0.2;
 
+        // Calculate the total cost
         double totalSum =  trainingPrice + shoes + outfit + basketball + accessories;
 
-        System.out.println(totalSum);
+        // Print the total cost
+        System.out.printf("%.2f%n", totalSum);
+        
+        scan.close();
     }
 }
