dbec890beba405113bbffef67fa567257ccc95f1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:54:32 +0300

    06

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_06_Repainting.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_06_Repainting.java
@@ -6,17 +6,29 @@ public class _06_Repainting {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        double coveringPlastic = Integer.parseInt(scan.nextLine());
-        double paintInLiters = Integer.parseInt(scan.nextLine());
-        double paintRemoverInLiters = Integer.parseInt(scan.nextLine());
-        double hoursOfWork = Integer.parseInt(scan.nextLine());
+        // Read input data
+        double coveringPlastic = Double.parseDouble(scan.nextLine());
+        double paintInLiters = Double.parseDouble(scan.nextLine());
+        double paintRemoverInLiters = Double.parseDouble(scan.nextLine());
+        double hoursOfWork = Double.parseDouble(scan.nextLine());
 
+        // Calculate prices for each material
         double coveringPlasticPrice = ((coveringPlastic + 2) * 1.50) + 0.40;
         double paintPrice = (paintInLiters * 1.1) * 14.50;
         double paintRemoverPrice = paintRemoverInLiters * 5.00;
+
+        // Calculate total cost for materials
         double totalForMaterials = coveringPlasticPrice + paintPrice + paintRemoverPrice;
+
+        // Calculate cost for labor
         double workPrice = totalForMaterials * 0.3 * hoursOfWork;
 
-        System.out.println(totalForMaterials + workPrice);
+        // Calculate the final total cost
+        double totalCost = totalForMaterials + workPrice;
+
+        // Print the final total cost
+        System.out.printf("%.2f%n", totalCost);
+        
+        scan.close();
     }
 }
