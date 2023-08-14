eccc4a4eb2d2fd853035592dcc5fd0e46c9116cc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 03:03:00 +0300

    06

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_06_ConcatenateData.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_06_ConcatenateData.java
@@ -6,11 +6,15 @@ public class _06_ConcatenateData {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input data
         String firstName = scan.nextLine();
         String lastName = scan.nextLine();
         int age = Integer.parseInt(scan.nextLine());
         String town = scan.nextLine();
 
+        // Print formatted message
         System.out.printf("You are %s %s, a %d-years old person from %s.", firstName, lastName, age, town);
+        
+        scan.close();
     }
 }
