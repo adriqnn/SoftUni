c548ec6c5c99f204370c61182b42afb0727db367
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 03:39:52 +0300

    09

--- 01_ProgrammingBasics/_01_FirstStepsInProgramming/_09_YardGreening.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgramming/_09_YardGreening.java
@@ -6,13 +6,20 @@ public class _09_YardGreening {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        double sqrMetersPlot = Double.parseDouble(scan.nextLine());
+        // Read input data
+        double squareMetersPlot = Double.parseDouble(scan.nextLine());
 
-        double costOfGreening = sqrMetersPlot * 7.61;
+        // Calculate initial cost of greening and discount
+        double costOfGreening = squareMetersPlot * 7.61;
         double discount = costOfGreening * 0.18;
+
+        // Calculate final cost with discount
         double costWithDiscount = costOfGreening - discount;
 
-        System.out.println("The final price is:" + " " + costWithDiscount + " lv.");
-        System.out.println("The discount is:" +" " + discount + " lv.");
+        // Print results
+        System.out.printf("The final price is: %.2f lv.%n", costWithDiscount);
+        System.out.printf("The discount is: %.2f lv.%n", discount);
+
+        scan.close();
     }
 }
