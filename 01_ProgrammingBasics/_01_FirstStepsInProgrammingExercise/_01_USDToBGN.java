ba1dab145ed887741d98a2878dd8dbf0d684ceb8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-14 04:37:19 +0300

    01

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_01_USDToBGN.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingExercise/_01_USDToBGN.java
@@ -6,9 +6,15 @@ public class _01_USDToBGN {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input USD amount
         double usd = Double.parseDouble(scan.nextLine());
+
+        // Convert USD to BGN
         double bgn = usd * 1.79549;
 
+        // Print the converted amount
         System.out.println(bgn);
+        
+        scan.close();
     }
 }
