3ad273c3573adb808425a0bc4b803cdda713ed09
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 19:59:48 +0300

    08

--- 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_08_LunchBreak.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_08_LunchBreak.java
@@ -6,18 +6,23 @@ public class _08_LunchBreak {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the input values
         String nameOfTheSeries = scan.nextLine();
         int episodeLength = Integer.parseInt(scan.nextLine());
         int breakLength = Integer.parseInt(scan.nextLine());
 
-        double lunchTime = breakLength * 1.0/8;
-        double breakTime = breakLength * 1.0/4;
+        // Calculate time allocated for lunch and break
+        double lunchTime = breakLength * 1.0 / 8;
+        double breakTime = breakLength * 1.0 / 4;
         double timeLeft = breakLength - lunchTime - breakTime;
 
-        if(timeLeft >= episodeLength){
+        // Compare time left with episode length and print the appropriate message
+        if (timeLeft >= episodeLength) {
             System.out.printf("You have enough time to watch %s and left with %.0f minutes free time.", nameOfTheSeries, Math.ceil(timeLeft - episodeLength));
-        }else{
-            System.out.printf("You don't have enough time to watch %s, you need %.0f more minutes.",nameOfTheSeries, Math.ceil(episodeLength - timeLeft));
+        } else {
+            System.out.printf("You don't have enough time to watch %s, you need %.0f more minutes.", nameOfTheSeries, Math.ceil(episodeLength - timeLeft));
         }
+
+        scan.close();
     }
 }
