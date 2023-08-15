5b927f7eb554d3fcbc44471afed92ee3c339c50f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 19:50:23 +0300

    05

--- 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_05_GodzillaVsKong.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_05_GodzillaVsKong.java
@@ -6,24 +6,34 @@ public class _05_GodzillaVsKong {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the input values
         double budget = Double.parseDouble(scan.nextLine());
         int extras = Integer.parseInt(scan.nextLine());
-        double clothes = Double.parseDouble(scan.nextLine());
+        double clothesPricePerExtra = Double.parseDouble(scan.nextLine());
 
-        double setting = (budget*0.1);
-        double clothes2 = 0;
+        // Calculate the setting cost
+        double settingCost = budget * 0.1;
 
+        // Calculate the total cost for clothes
+        double clothesCost = clothesPricePerExtra * extras;
 
-        if (extras>150){ clothes2 += ((clothes*extras)*0.9); }
-        else { clothes2 += (clothes*extras); }
+        // Apply discount for clothes if extras are more than 150
+        if (extras > 150) {
+            clothesCost *= 0.9;
+        }
 
-        double sum = budget - (clothes2+setting);
+        // Calculate the remaining budget after expenses
+        double remainingBudget = budget - (settingCost + clothesCost);
 
-        if (sum >= 0) {
+        // Print the result based on the remaining budget
+        if (remainingBudget >= 0) {
             System.out.println("Action!");
-            System.out.printf("Wingard starts filming with %.2f leva left.", sum); }
-        else if ( sum < 0) {
+            System.out.printf("Wingard starts filming with %.2f leva left.", remainingBudget);
+        } else {
             System.out.println("Not enough money!");
-            System.out.printf("Wingard needs %.2f leva more.", Math.abs(sum)); }
+            System.out.printf("Wingard needs %.2f leva more.", Math.abs(remainingBudget));
+        }
+
+        scan.close();
     }
 }
