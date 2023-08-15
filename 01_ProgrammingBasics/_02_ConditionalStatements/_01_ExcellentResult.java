e0cc436d7434a7e479b92f9d068b6932e8bf0750
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 17:32:20 +0300

    01

--- 01_ProgrammingBasics/_02_ConditionalStatements/_01_ExcellentResult.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_01_ExcellentResult.java
@@ -6,11 +6,17 @@ public class _01_ExcellentResult {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the grade input
         int grade = Integer.parseInt(scan.nextLine());
+
+        // Determine if the grade is excellent
         boolean isExcellent = grade >= 5;
 
-        if (grade >=5){
+        // Print "Excellent!" if the grade is excellent
+        if (isExcellent) {
             System.out.println("Excellent!");
         }
+
+        scan.close();
     }
 }
