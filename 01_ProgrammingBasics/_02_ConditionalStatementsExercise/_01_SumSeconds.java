9a9da938f4473f27c073adb54e94cf14f933b9db
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 19:27:12 +0300

    01

--- 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_01_SumSeconds.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_01_SumSeconds.java
@@ -6,18 +6,22 @@ public class _01_SumSeconds {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int time1 = Integer.parseInt(scan.nextLine());
-        int time2 = Integer.parseInt(scan.nextLine());
-        int time3 = Integer.parseInt(scan.nextLine());
+        // Read the input times
+        int timeFirst = Integer.parseInt(scan.nextLine());
+        int timeSecond = Integer.parseInt(scan.nextLine());
+        int timeThird = Integer.parseInt(scan.nextLine());
 
-        int totalTime = time1+time2+time3;
+        // Calculate the total time in seconds
+        int totalTime = timeFirst + timeSecond + timeThird;
+
+        // Convert the total time to minutes and seconds
         int minutes = totalTime / 60;
         int seconds = totalTime % 60;
 
-        if (seconds<10) {
-            System.out.printf("%d:0%d", minutes, seconds);
-        } else {
-            System.out.printf("%d:%d", minutes, seconds);
-        }
+        // Print the result in the desired format
+        String timeResult = seconds < 10 ? String.format("%d:0%d", minutes, seconds) : String.format("%d:%d", minutes, seconds);
+        System.out.println(timeResult);
+        
+        scan.close();
     }
 }
