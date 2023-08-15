91a8738a7e122781069086f46c0e02c271312d93
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 17:45:33 +0300

    04

--- 01_ProgrammingBasics/_02_ConditionalStatements/_04_PasswordGuess.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_04_PasswordGuess.java
@@ -6,12 +6,19 @@ public class _04_PasswordGuess {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // The correct password to compare against
         String password = "s3cr3t!P@ssw0rd";
+
+        // Read the password input
         String passwordInput = scan.nextLine();
 
-        if(passwordInput.equals(password)) {
+        // Check if the input matches the correct password
+        if (passwordInput.equals(password)) {
             System.out.println("Welcome");
-        }else{
-            System.out.println("Wrong password!");}
+        } else {
+            System.out.println("Wrong password!");
+        }
+
+        scan.close();
     }
 }
