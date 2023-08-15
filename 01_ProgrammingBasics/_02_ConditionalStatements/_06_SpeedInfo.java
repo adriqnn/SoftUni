2d73330139c39873bff62b6564de31181f02590f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 17:54:59 +0300

    06

--- 01_ProgrammingBasics/_02_ConditionalStatements/_06_SpeedInfo.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_06_SpeedInfo.java
@@ -6,18 +6,22 @@ public class _06_SpeedInfo {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the speed input
         double speed = Double.parseDouble(scan.nextLine());
 
-        if(speed <= 10){
+        // Categorize the speed based on its value
+        if (speed <= 10) {
             System.out.println("slow");
-        }else if(speed <= 50){
+        } else if (speed <= 50) {
             System.out.println("average");
-        }else if(speed <= 150){
+        } else if (speed <= 150) {
             System.out.println("fast");
-        }else if(speed <= 1000){
+        } else if (speed <= 1000) {
             System.out.println("ultra fast");
-        }else{
+        } else {
             System.out.println("extremely fast");
         }
+
+        scan.close();
     }
 }
