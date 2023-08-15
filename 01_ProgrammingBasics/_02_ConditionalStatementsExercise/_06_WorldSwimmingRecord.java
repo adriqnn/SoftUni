46159ea1be0452f985a59354d0b2c48f11245ba1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 19:52:50 +0300

    06

--- 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_06_WorldSwimmingRecord.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_06_WorldSwimmingRecord.java
@@ -6,19 +6,30 @@ public class _06_WorldSwimmingRecord {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the input values
         double record = Double.parseDouble(scan.nextLine());
         double distance = Double.parseDouble(scan.nextLine());
         double seconds = Double.parseDouble(scan.nextLine());
 
-        double swimDistanceSeconds = distance*seconds;
-        double resistanceSeconds = ((Math.floor(distance/15))*12.5);
-        double record2 = swimDistanceSeconds+resistanceSeconds;
-        double sum = Math.abs(record2-record);
+        // Calculate the time it takes to swim the distance
+        double swimDistanceSeconds = distance * seconds;
 
-        if (record2<record){
-            System.out.printf("Yes, he succeeded! The new world record is %.2f seconds.", record2);
-        }else {
-            System.out.printf("No, he failed! He was %.2f seconds slower.", sum);
+        // Calculate the resistance time due to each 15 meters
+        double resistanceSeconds = Math.floor(distance / 15) * 12.5;
+
+        // Calculate the total time including resistance
+        double totalTime = swimDistanceSeconds + resistanceSeconds;
+
+        // Calculate the difference between the new record and the given record
+        double timeDifference = Math.abs(totalTime - record);
+
+        // Compare the total time with the given record
+        if (totalTime < record) {
+            System.out.printf("Yes, he succeeded! The new world record is %.2f seconds.", totalTime);
+        } else {
+            System.out.printf("No, he failed! He was %.2f seconds slower.", timeDifference);
         }
+        
+        scan.close();
     }
 }
