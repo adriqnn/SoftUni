8520de9ec92efacf63e83e7a38e71ed1bae42bd9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 17:59:40 +0300

    07

--- 01_ProgrammingBasics/_02_ConditionalStatements/_07_AreaOfFigures.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_07_AreaOfFigures.java
@@ -6,34 +6,33 @@ public class _07_AreaOfFigures {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the shape input
         String shape = scan.nextLine();
 
+        // Read additional information based on the shape, calculate and print the area of the shape
         if (shape.equals("square")) {
-            double square = Double.parseDouble(scan.nextLine());
-            double square2 = square * square;
-
-            System.out.printf("%.3f", square2);
+            double squareSide = Double.parseDouble(scan.nextLine());
+            double squareArea = squareSide * squareSide;
+            System.out.printf("%.3f", squareArea);
 
         } else if (shape.equals("rectangle")) {
-            double rectangle = Double.parseDouble(scan.nextLine());
-            double rectangle2 = Double.parseDouble(scan.nextLine());
-            double rectangle3 = rectangle * rectangle2;
-
-            System.out.printf("%.3f", rectangle3);
+            double rectangleWidth = Double.parseDouble(scan.nextLine());
+            double rectangleHeight = Double.parseDouble(scan.nextLine());
+            double rectangleArea = rectangleWidth * rectangleHeight;
+            System.out.printf("%.3f", rectangleArea);
 
         } else if (shape.equals("circle")) {
-            double circle = Double.parseDouble(scan.nextLine());
-            double circle2 = Math.PI * circle * circle;
-
-            System.out.printf("%.3f", circle2);
+            double circleRadius = Double.parseDouble(scan.nextLine());
+            double circleArea = Math.PI * circleRadius * circleRadius;
+            System.out.printf("%.3f", circleArea);
 
         } else if (shape.equals("triangle")) {
-            double triangle = Double.parseDouble(scan.nextLine());
-            double triangle2 = Double.parseDouble(scan.nextLine());
-            double triangle3 = triangle * triangle2;
-            double triangle4 = triangle3 / 2;
-
-            System.out.printf("%.3f", triangle4);
+            double triangleBase = Double.parseDouble(scan.nextLine());
+            double triangleHeight = Double.parseDouble(scan.nextLine());
+            double triangleArea = (triangleBase * triangleHeight) / 2;
+            System.out.printf("%.3f", triangleArea);
         }
+        
+        scan.close();
     }
 }
