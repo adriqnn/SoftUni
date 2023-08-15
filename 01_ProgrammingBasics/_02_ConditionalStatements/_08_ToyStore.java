4d71542c72c4410f37c2ddb6fdf55969811b3f60
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 18:25:29 +0300

    08

--- 01_ProgrammingBasics/_02_ConditionalStatements/_08_ToyStore.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_08_ToyStore.java
@@ -5,33 +5,42 @@ import java.util.Scanner;
 public class _08_ToyStore {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        //Puzzle-2.60/TalkingDoll-3.00/Bear-4.10/Minion-8.20/Truck-2.00/
-        //50 or more - 25% discount;10% from profit for rent
-        double travel = Double.parseDouble(scan.nextLine());
+
+        // Read the input
+        double travelCost = Double.parseDouble(scan.nextLine());
         int puzzles = Integer.parseInt(scan.nextLine());
         int dolls = Integer.parseInt(scan.nextLine());
         int bears = Integer.parseInt(scan.nextLine());
         int minions = Integer.parseInt(scan.nextLine());
         int trucks = Integer.parseInt(scan.nextLine());
 
-        double puzzles2 = puzzles * 2.60;
-        double dolls2 = dolls * 3.00;
-        double bears2 = bears * 4.10;
-        double minions2 = minions * 8.20;
-        double trucks2 = trucks * 2.00;
+        // Calculate the revenue from selling each type of toy
+        double puzzleRevenue = puzzles * 2.60;
+        double dollRevenue = dolls * 3.00;
+        double bearRevenue = bears * 4.10;
+        double minionRevenue = minions * 8.20;
+        double truckRevenue = trucks * 2.00;
 
-        double allToys = puzzles + dolls + bears + minions + trucks;
-        double sum = puzzles2 + dolls2 + bears2 + minions2 + trucks2;
+        // Calculate the total revenue and number of sold toys
+        double totalRevenue = puzzleRevenue + dollRevenue + bearRevenue + minionRevenue + truckRevenue;
+        int totalToysSold = puzzles + dolls + bears + minions + trucks;
 
-        if (allToys >= 50){
-            sum = sum - (sum*0.25);
+        // Apply discounts if the number of sold toys is 50 or more
+        if (totalToysSold >= 50) {
+            totalRevenue *= 0.75; // 25% discount
         }
-        double sumMinusRent = sum - (sum*0.10);
 
-        if (sumMinusRent >= travel){
-            System.out.printf("Yes! %.2f lv left.",sumMinusRent-travel);
+        // Subtract rent and check if there's enough money for travel
+        double profitAfterRent = totalRevenue * 0.9; // 10% for rent
+        double moneyLeft = profitAfterRent - travelCost;
+
+        // Print the result
+        if (moneyLeft >= 0) {
+            System.out.printf("Yes! %.2f lv left.", moneyLeft);
         } else {
-            System.out.printf("Not enough money! %2.2f lv needed.", travel - sumMinusRent);
+            System.out.printf("Not enough money! %.2f lv needed.", Math.abs(moneyLeft));
         }
+        
+        scan.close();
     }
 }
