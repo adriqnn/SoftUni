06514a6c511a725c539c4c103395d63ae940f074
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 19:57:26 +0300

    07

--- 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_07_Shopping.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsExercise/_07_Shopping.java
@@ -6,24 +6,32 @@ public class _07_Shopping {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read the input values
         double budget = Double.parseDouble(scan.nextLine());
         double gpus = Double.parseDouble(scan.nextLine());
         double processors = Double.parseDouble(scan.nextLine());
         int memorySticks = Integer.parseInt(scan.nextLine());
 
+        // Calculate the prices of each component
         double gpusPrice = gpus * 250;
         double processorsPrice = processors * (gpusPrice * 0.35);
         double memoryPrice = memorySticks * (gpusPrice * 0.1);
+
+        // Calculate the total price
         double totalPrice = gpusPrice + processorsPrice + memoryPrice;
 
-        if(gpus > processors){
+        // Apply discount if GPUs are more expensive than processors
+        if (gpus > processors) {
             totalPrice *= 0.85;
         }
 
-        if(totalPrice <= budget){
+        // Compare the total price with the budget and print the result
+        if (totalPrice <= budget) {
             System.out.printf("You have %.2f leva left!", budget - totalPrice);
-        }else{
+        } else {
             System.out.printf("Not enough money! You need %.2f leva more!", totalPrice - budget);
         }
+        
+        scan.close();
     }
 }
