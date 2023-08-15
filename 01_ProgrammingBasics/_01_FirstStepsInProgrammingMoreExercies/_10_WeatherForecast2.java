700747ce4e11a02a55e013823ffef66eaf94d0ed
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 04:55:12 +0300

    10

---
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercies/_10_WeatherForecast2.java
@@ -0,0 +1,33 @@
+package _01_ProgrammingBasics._01_FirstStepsInProgrammingMoreExercies;
+
+import java.util.Scanner;
+
+public class _10_WeatherForecast2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read the temperature input
+        double degrees = Double.parseDouble(scan.nextLine());
+        String weatherType = "";
+
+        // Categorize weather based on temperature ranges
+        if (degrees >= 5.00 && degrees <= 11.9){
+            weatherType = "Cold";
+        } else if (degrees >= 12.00 && degrees <= 14.9){
+            weatherType = "Cool";
+        } else if (degrees >= 15.00 && degrees <= 20.00){
+            weatherType = "Mild";
+        } else if (degrees >= 20.1 && degrees <= 25.9){
+            weatherType = "Warm";
+        } else if (degrees >= 26.00 && degrees <= 35.00){
+            weatherType = "Hot";
+        }else {
+            weatherType = "unknown";
+        }
+
+        // Print the weather type
+        System.out.println(weatherType);
+
+        scan.close();
+    }
+}
