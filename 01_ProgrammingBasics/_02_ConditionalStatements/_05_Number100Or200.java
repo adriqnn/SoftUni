c41583159779d97dd2bb32dfcad5a4e58f3b675b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-15 17:47:49 +0300

    05

--- 01_ProgrammingBasics/_02_ConditionalStatements/_05_Number100Or200.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_05_Number100Or200.java
@@ -6,14 +6,18 @@ public class _05_Number100Or200 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num = Integer.parseInt(scan.nextLine());
+        // Read the number input
+        int number = Integer.parseInt(scan.nextLine());
 
-        if (num < 100){
+        // Categorize the number based on its value
+        if (number < 100) {
             System.out.println("Less than 100");
-        } else if(num <= 200){
+        } else if (number <= 200) {
             System.out.println("Between 100 and 200");
         } else {
             System.out.println("Greater than 200");
         }
+
+        scan.close();
     }
 }
