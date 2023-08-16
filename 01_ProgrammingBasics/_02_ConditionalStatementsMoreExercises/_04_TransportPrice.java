67229d5002ec8a9dd7548f566458d39b1ed39c7f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-16 19:38:42 +0300

    04

---
+++ 01_ProgrammingBasics/_02_ConditionalStatementsMoreExercises/_04_TransportPrice.java
@@ -0,0 +1,33 @@
+package _01_ProgrammingBasics._02_ConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _04_TransportPrice {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read the input
+        int kilometers = Integer.parseInt(scan.nextLine());
+        String timePeriod = scan.nextLine();
+        double tripPrice = 0;
+
+        // Calculate the trip price based on different cases
+        if (kilometers < 20) {
+            if (timePeriod.equals("day")) {
+                tripPrice = 0.70 + (kilometers * 0.79);
+            } else {
+                tripPrice = 0.70 + (kilometers * 0.90);
+            }
+        } else if (kilometers < 100) {
+            tripPrice = kilometers * 0.09;
+        } else {
+            tripPrice = kilometers * 0.06;
+        }
+
+        // Output the calculated trip price
+        System.out.printf("%.2f", tripPrice);
+
+        // Close the scanner
+        scan.close();
+    }
+}
