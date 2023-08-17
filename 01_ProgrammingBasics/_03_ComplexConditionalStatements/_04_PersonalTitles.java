b2b50ce92faef8cad15fad01f108f9ae3922e6a1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 21:31:56 +0300

    04

--- 01_ProgrammingBasics/_03_ComplexConditionalStatements/_04_PersonalTitles.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatements/_04_PersonalTitles.java
@@ -6,17 +6,21 @@ public class _04_PersonalTitles {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values for age and gender
         double age = Double.parseDouble(scan.nextLine());
         String gender = scan.nextLine();
 
-        if (age<16 && gender.equals("f")) {
+        // Print the title based on the given age and gender
+        if (age < 16 && gender.equals("f")) {
             System.out.println("Miss");
-        } else if (age>=16 && gender.equals("f")) {
+        } else if (age >= 16 && gender.equals("f")) {
             System.out.println("Ms.");
-        } else if (age<16 && gender.equals("m")) {
+        } else if (age < 16 && gender.equals("m")) {
             System.out.println("Master");
-        } else if (age>=16 && gender.equals("m")) {
+        } else if (age >= 16 && gender.equals("m")) {
             System.out.println("Mr.");
         }
+
+        scan.close();
     }
 }
