a86d51db6dba701f77fb4f09a4aa11b7f375ab94
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 22:55:47 +0300

    12

--- 01_ProgrammingBasics/_03_ComplexConditionalStatements/_12_TradeCommissions.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatements/_12_TradeCommissions.java
@@ -6,57 +6,63 @@ public class _12_TradeCommissions {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values for city and money
         String city = scan.nextLine();
         double money = Double.parseDouble(scan.nextLine());
 
-        boolean isValid = (city.equals("Sofia") || city.equals("Varna") || city.equals("Plovdiv"));
-        boolean isValid2 = (money >=0 );
+        // Check if the input values are valid
+        boolean isValidCity = city.equals("Sofia") || city.equals("Varna") || city.equals("Plovdiv");
+        boolean isValidMoney = money >= 0;
 
-        if (!isValid || !isValid2){
+        // End the program if invalid data is entered
+        if (!isValidCity || !isValidMoney) {
             System.out.println("error");
+            return;
         }
 
-        double result = 0;
+        // Commission for the trade
+        double commission = 0.0;
 
-        if (city.equals("Sofia")){
-            if ( money > 10000 ){
-                result = money*0.12;
-            }else if (10000 >= money && money >1000){
-                result = money*0.08;
-            }else if ( 1000 >= money && money > 500){
-                result = money*0.07;
-            }else if (500 >= money && money >0){
-                result = money*0.05;
-            }else if (money == 0){
-                result = money*0.05;
-            }
-        }else if (city.equals("Varna")){
-            if ( money > 10000 ){
-                result = money*0.13;
-            }else if (10000 >= money && money >1000){
-                result = money*0.10;
-            }else if ( 1000 >= money && money > 500){
-                result = money*0.075;
-            }else if (500 >= money && money >0){
-                result = money*0.045;
-            }else if (money == 0){
-                result = money*0.045;
-            }
-        }else if (city.equals("Plovdiv")){
-            if ( money > 10000 ){
-                result = money*0.145;
-            }else if (10000 >= money && money >1000){
-                result = money*0.12;
-            }else if ( 1000 >= money && money > 500){
-                result = money*0.08;
-            }else if (500 >= money && money >0){
-                result = money*0.055;
-            }else if (money == 0){
-                result = money*0.055;
-            }
+        // Calculate the commission
+        switch (city) {
+            case "Sofia":
+                if (money <= 500) {
+                    commission = money * 0.05;
+                } else if (money <= 1000) {
+                    commission = money * 0.07;
+                } else if (money <= 10000) {
+                    commission = money * 0.08;
+                } else {
+                    commission = money * 0.12;
+                }
+                break;
+            case "Varna":
+                if (money <= 500) {
+                    commission = money * 0.045;
+                } else if (money <= 1000) {
+                    commission = money * 0.075;
+                } else if (money <= 10000) {
+                    commission = money * 0.10;
+                } else {
+                    commission = money * 0.13;
+                }
+                break;
+            case "Plovdiv":
+                if (money <= 500) {
+                    commission = money * 0.055;
+                } else if (money <= 1000) {
+                    commission = money * 0.08;
+                } else if (money <= 10000) {
+                    commission = money * 0.12;
+                } else {
+                    commission = money * 0.145;
+                }
+                break;
         }
-        if (result>0){
-            System.out.printf("%.2f", result);
-        }
+
+        // Print the commission
+        System.out.printf("%.2f", commission);
+        
+        scan.close();
     }
 }
