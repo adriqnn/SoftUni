74aea9dbc27e776c1cfccfd62be667ecaad750ec
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 21:57:04 +0300

    06

--- 01_ProgrammingBasics/_03_ComplexConditionalStatements/_06_NumberInRange.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatements/_06_NumberInRange.java
@@ -6,12 +6,20 @@ public class _06_NumberInRange {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int number = Integer.parseInt(scan.nextLine());
 
-        if(number >= -100 && number <= 100 && number != 0) {
+        // Set a range for the check
+        final int MIN_RANGE = -100;
+        final int MAX_RANGE = 100;
+
+        // Check if the number is in the set range and print the result
+        if (number >= MIN_RANGE && number <= MAX_RANGE && number != 0) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
+
+        scan.close();
     }
 }
