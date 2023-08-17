23a8fc055395e46bbedec53af39190a208d45a07
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 03:07:17 +0300

    05

---
+++ 01_ProgrammingBasics/_02_ConditionalStatementsMoreExercises/_05_Pets.java
@@ -0,0 +1,36 @@
+package _01_ProgrammingBasics._02_ConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _05_Pets {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        int days = Integer.parseInt(scan.nextLine());
+        int kilogramsOfFood = Integer.parseInt(scan.nextLine());
+        double dogFoodInKilograms = Double.parseDouble(scan.nextLine());
+        double catFoodInKilograms = Double.parseDouble(scan.nextLine());
+        double turtleFoodInGrams = Double.parseDouble(scan.nextLine());
+
+        // Calculate food needed for each pet
+        double dogFoodNeeded = dogFoodInKilograms * days;
+        double catFoodNeeded = catFoodInKilograms * days;
+        double turtleFoodNeeded = (turtleFoodInGrams * days) / 1000;
+
+        // Calculate total food needed
+        double foodNeeded = dogFoodNeeded + catFoodNeeded + turtleFoodNeeded;
+
+        // Calculate remaining food
+        double remainingFood = kilogramsOfFood - foodNeeded;
+
+        // Output the result
+        if (remainingFood >= 0) {
+            System.out.printf("%.0f kilos of food left.", Math.floor(remainingFood));
+        } else {
+            System.out.printf("%.0f more kilos of food are needed.", Math.ceil(Math.abs(remainingFood)));
+        }
+
+        scan.close();
+    }
+}
