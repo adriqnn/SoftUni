9971b4021f74902b7f41108faac37227d1f244e4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 22:34:51 +0300

    10

--- 01_ProgrammingBasics/_03_ComplexConditionalStatements/_10_InvalidNumber.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatements/_10_InvalidNumber.java
@@ -6,10 +6,13 @@ public class _10_InvalidNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int number = Integer.parseInt(scan.nextLine());
 
+        // Check if the number is valid or invalid
         boolean isValid = (number >= 100 && number <= 200 || number == 0);
 
+        // Print only if the number is invalid
         if (!isValid){
             System.out.println("invalid");
         }
