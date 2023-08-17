c17484638afc47a2bcae40bf472e1146b27b9d50
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 00:54:20 +0300

    08

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_08_OnTimeForTheExam.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_08_OnTimeForTheExam.java
@@ -6,63 +6,69 @@ public class _08_OnTimeForTheExam {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int hourOfExam = Integer.parseInt(scan.nextLine());
         int minuteOfExam = Integer.parseInt(scan.nextLine());
         int hourOfArrival = Integer.parseInt(scan.nextLine());
         int minuteOfArrival = Integer.parseInt(scan.nextLine());
 
-        int exam = (hourOfExam*60)+minuteOfExam;
-        int arrival = (hourOfArrival*60)+minuteOfArrival;
+        // Transforming the data into minutes
+        int examInMinutes = (hourOfExam * 60) + minuteOfExam;
+        int arrivalInMinutes = (hourOfArrival * 60) + minuteOfArrival;
 
-        int late = arrival-exam;
-        int early = exam-arrival;
+        // Calculating the time late/early
+        int lateMinutes = arrivalInMinutes - examInMinutes;
+        int earlyMinutes = examInMinutes - arrivalInMinutes;
 
-        String time = "";
-        int value = 0;
+        String timeUnit = "";
+        int value1 = 0;
         int value2 = 0;
 
-        if (exam == arrival){
+        // Determine if arrival is late or early and printing information based on that
+        if (examInMinutes == arrivalInMinutes) {
             System.out.println("On Time");
-        }else if (exam<arrival){
-            if (late>=60){
-                time = "hours";
-                value = late/60;
-                value2 = late%60;
-                if (value2<10) {
+        } else if (examInMinutes < arrivalInMinutes) {
+            if (lateMinutes >= 60) {
+                timeUnit = "hours";
+                value1 = lateMinutes / 60;
+                value2 = lateMinutes % 60;
+                if (value2 < 10) {
                     System.out.println("Late");
-                    System.out.printf("%d:0%d %s after the start", value,value2,time);
-                }else{
+                    System.out.printf("%d:0%d %s after the start%n", value1, value2, timeUnit);
+                } else {
                     System.out.println("Late");
-                    System.out.printf("%d:%d %s after the start", value,value2,time);
+                    System.out.printf("%d:%d %s after the start%n", value1, value2, timeUnit);
                 }
-            }else{
-                time = "minutes";
+            } else {
+                timeUnit = "minutes";
                 System.out.println("Late");
-                System.out.printf("%d %s after the start", late, time);
+                System.out.printf("%d %s after the start%n", lateMinutes, timeUnit);
             }
-        }else{
-            if (early<=30){
-                time = "minutes";
+        } else {
+            if (earlyMinutes <= 30) {
+                timeUnit = "minutes";
                 System.out.println("On time");
-                System.out.printf("%d %s before the start", early, time);
-            }else{
-                if(early>=60){
-                    time = "hours";
-                    value = early/60;
-                    value2 = early%60;
-                    if (value2<10) {
+                System.out.printf("%d %s before the start%n", earlyMinutes, timeUnit);
+            } else {
+                if (earlyMinutes >= 60) {
+                    timeUnit = "hours";
+                    value1 = earlyMinutes / 60;
+                    value2 = earlyMinutes % 60;
+                    if (value2 < 10) {
                         System.out.println("Early");
-                        System.out.printf("%d:0%d %s before the start", value,value2,time);
-                    }else{
+                        System.out.printf("%d:0%d %s before the start%n", value1, value2, timeUnit);
+                    } else {
                         System.out.println("Early");
-                        System.out.printf("%d:%d %s before the start", value,value2,time);
+                        System.out.printf("%d:%d %s before the start%n", value1, value2, timeUnit);
                     }
-                }else{
-                    time = "minutes";
+                } else {
+                    timeUnit = "minutes";
                     System.out.println("Early");
-                    System.out.printf("%d %s before the start", early, time);
+                    System.out.printf("%d %s before the start%n", earlyMinutes, timeUnit);
                 }
             }
         }
+
+        scan.close();
     }
 }
