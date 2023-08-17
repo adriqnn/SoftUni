717d2607f063c38af2061c97c40579e3b0e3cdea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-17 23:45:14 +0300

    01

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_01_Cinema.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_01_Cinema.java
@@ -6,22 +6,32 @@ public class _01_Cinema {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String type = scan.nextLine();
         int row = Integer.parseInt(scan.nextLine());
         int number = Integer.parseInt(scan.nextLine());
 
-        double woot = 1.00*row*number;
+        double ticketPrice = 0.00;
 
+        // Calculate the total earnings based on ticket type
         switch (type) {
             case "Premiere":
-                System.out.printf("%.2f leva", (woot * 12.00));
+                ticketPrice = 12.00;
                 break;
             case "Normal":
-                System.out.printf("%.2f leva", (woot * 7.50));
+                ticketPrice = 7.50;
                 break;
             case "Discount":
-                System.out.printf("%.2f leva", (woot * 5.00));
+                ticketPrice = 5.00;
                 break;
         }
+
+        // Calculate earnings
+        double totalEarnings = ticketPrice * row * number;
+
+        // Display the total earnings formatted with 2 decimal places
+        System.out.printf("Total earnings: %.2f leva%n", totalEarnings);
+        
+        scan.close();
     }
 }
