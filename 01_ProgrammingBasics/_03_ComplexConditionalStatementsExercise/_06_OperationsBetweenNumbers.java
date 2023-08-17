bcb883bfcd336534585dc57829bc441e720a7a0c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 00:12:05 +0300

    06

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_06_OperationsBetweenNumbers.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_06_OperationsBetweenNumbers.java
@@ -6,48 +6,55 @@ public class _06_OperationsBetweenNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num1 = Integer.parseInt(scan.nextLine());
-        int num2 = Integer.parseInt(scan.nextLine());
+        // Read input values
+        int numberOne = Integer.parseInt(scan.nextLine());
+        int numberTwo = Integer.parseInt(scan.nextLine());
         String operator = scan.nextLine();
 
         double result = 0;
-        String type = "";
+        String resultType = "";
 
-        if (operator.equals("+")){
-            result += (num1*(1.00)) + num2;
-        } else if (operator.equals("-")){
-            result += (num1*1.00) - num2;
-        } else if (operator.equals("*")){
-            result += (num1*1.00) * num2;
-        } else if (operator.equals("/")){
-            result += (num1*(1.00)) / num2;
-        }else if (operator.equals("%")){
-            result += (num1*(1.00)) % num2;
-        }
-
-        double result2 = result*1.00;
-        boolean isValid = num2 != 0;
-
-        if ((operator.equals("+")) || (operator.equals("-")) || (operator.equals("*"))){
-            if (result%2 ==0){
-                type = "even";
-                System.out.printf("%d %s %d = %.0f - %s", num1, operator, num2, result,type);
-            }else{
-                type = "odd";
-                System.out.printf("%d %s %d = %.0f - %s", num1, operator, num2, result,type);
+        // Perform the selected operation based on the operator
+        if (operator.equals("+")) {
+            result = numberOne + numberTwo;
+        } else if (operator.equals("-")) {
+            result = numberOne - numberTwo;
+        } else if (operator.equals("*")) {
+            result = numberOne * numberTwo;
+        } else if (operator.equals("/")) {
+            if (numberTwo != 0) {
+                result = numberOne * 1.0 / numberTwo;
+            } else {
+                System.out.printf("Cannot divide %d by zero%n", numberOne);
+                return; // Exit the program if division by zero
             }
-        }else if (operator.equals("/")){
-            if (!isValid)
-                System.out.printf("Cannot divide %d by zero", num1);
-            else{
-                System.out.printf("%d %s %d = %.2f", num1, operator, num2, result);
-            }
-        }else if (operator.equals("%")) {
-            if (!isValid)
-                System.out.printf("Cannot divide %d by zero", num1);
-            else {
-                System.out.printf("%d %s %d = %.0f", num1, operator, num2, result);
+        } else if (operator.equals("%")) {
+            if (numberTwo != 0) {
+                result = numberOne % numberTwo;
+            } else {
+                System.out.printf("Cannot divide %d by zero%n", numberOne);
+                return; // Exit the program if division by zero
             }
         }
+
+        // Determine whether the result is even or odd for +, -, and *
+        if (operator.equals("+") || operator.equals("-") || operator.equals("*")) {
+            if (result % 2 == 0) {
+                resultType = "even";
+            } else {
+                resultType = "odd";
+            }
+        }
+
+        // Display the result based on the operation and input values
+        if (operator.equals("+") || operator.equals("-") || operator.equals("*")) {
+            System.out.printf("%d %s %d = %.0f - %s%n", numberOne, operator, numberTwo, result, resultType);
+        } else if (operator.equals("/")) {
+            System.out.printf("%d %s %d = %.2f%n", numberOne, operator, numberTwo, result);
+        } else if (operator.equals("%")) {
+            System.out.printf("%d %s %d = %.0f%n", numberOne, operator, numberTwo, result);
+        }
+        
+        scan.close();
     }
 }
