8bf69b9f4d362e8ff044b7d66643c6e89fe5fd52
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 00:03:40 +0300

    04

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_04_FishingBoat.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsExercise/_04_FishingBoat.java
@@ -6,61 +6,70 @@ public class _04_FishingBoat {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int budget = Integer.parseInt(scan.nextLine());
         String season = scan.nextLine();
-        int ppl = Integer.parseInt(scan.nextLine());
+        int numberOfPeople = Integer.parseInt(scan.nextLine());
 
-        double price = 0;
-        boolean isValid = season.equals("Autumn");
+        double basePrice = 0;
+        boolean isAutumn = season.equals("Autumn");
 
+        // Calculate the base price based on the season
+        // Apply discounts based on the number of people
         switch (season) {
             case "Spring":
-                price= 3000;
-                if (ppl<=6){
-                    price *=0.90;
-                }else if (ppl<=11){
-                    price *=0.85;
+                basePrice = 3000;
+                if (numberOfPeople <= 6){
+                    basePrice *= 0.90;
+                }else if (numberOfPeople <= 11){
+                    basePrice *= 0.85;
                 }else{
-                    price *=0.75;
+                    basePrice *= 0.75;
                 }
                 break;
             case "Summer":
             case "Autumn":
-                price = 4200;
-                if (ppl<=6){
-                    price *=0.90;
-                }else if (ppl<=11){
-                    price *=0.85;
+                basePrice = 4200;
+                if (numberOfPeople <= 6){
+                    basePrice *= 0.90;
+                }else if (numberOfPeople <= 11){
+                    basePrice *= 0.85;
                 }else{
-                    price *=0.75;
+                    basePrice *= 0.75;
                 }
                 break;
             case "Winter":
-                price = 2600;
-                if (ppl<=6){
-                    price *=0.90;
-                }else if (ppl<=11){
-                    price *=0.85;
+                basePrice = 2600;
+                if (numberOfPeople <= 6){
+                    basePrice *= 0.90;
+                }else if (numberOfPeople <= 11){
+                    basePrice *= 0.85;
                 }else{
-                    price *=0.75;
+                    basePrice *= 0.75;
                 }
                 break;
         }
-        if (isValid) {
-            price*=1.00;
+
+        // Apply additional adjustments based on season and odd/even number of people
+        if (isAutumn) {
+            basePrice *= 1.00;
         }else {
-            if (ppl % 2 == 0) {
-                price *= 0.95;
+            if (numberOfPeople % 2 == 0) {
+                basePrice *= 0.95;
             } else {
-                price *= 1.00;
+                basePrice *= 1.00;
             }
         }
-        double sum = budget - price;
 
-        if(budget>=price){
-            System.out.printf("Yes! You have %.2f leva left.", Math.abs(sum));
+        double difference = budget - basePrice;
+
+        // Determine the appropriate output based on budget
+        if(budget >= basePrice){
+            System.out.printf("Yes! You have %.2f leva left.", Math.abs(difference));
         }else{
-            System.out.printf("Not enough money! You need %.2f leva.", Math.abs(sum));
+            System.out.printf("Not enough money! You need %.2f leva.", Math.abs(difference));
         }
+
+        scan.close();
     }
 }
