672f9f0deb5153ca0addead282748218d1242e7a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 04:48:52 +0300

    05

---
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_05_Vacation.java
@@ -0,0 +1,35 @@
+package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _05_Vacation {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        double budget = Double.parseDouble(scan.nextLine());
+        String season = scan.nextLine().toLowerCase();
+
+        // Add variables
+        String accommodation = "";
+        String location = season.equals("summer") ? "Alaska" : "Morocco";
+        double price = 0;
+
+        // Determine accommodation and price based on budget
+        if(budget <= 1000){
+            accommodation = "Camp";
+            price = season.equals("summer") ? budget * 0.65 : budget * 0.45;
+        }else if(budget <= 3000){
+            accommodation = "Hut";
+            price = season.equals("summer") ? budget * 0.80 : budget * 0.60;
+        }else{
+            accommodation = "Hotel";
+            price = budget * 0.90;
+        }
+
+        // Print the result
+        System.out.printf("%s - %s - %.2f", location, accommodation, price);
+
+        scan.close();
+    }
+}
