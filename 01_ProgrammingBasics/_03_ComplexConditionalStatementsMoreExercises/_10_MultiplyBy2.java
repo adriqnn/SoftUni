078626893328ec4fea1e9a96eaddf5c6a523ace7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 06:05:57 +0300

    10

---
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_10_MultiplyBy2.java
@@ -0,0 +1,28 @@
+package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _10_MultiplyBy2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        double number = Double.parseDouble(scan.nextLine());
+
+        // Multiply the number by 2 and print
+        while(number >= 0){
+            double result = number * 2;
+
+            System.out.printf("Result: %.2f%n", result);
+
+            number = Double.parseDouble(scan.nextLine());
+        }
+
+        // End the program if the number is below 0
+        if(number < 0){
+            System.out.println("Negative number!");
+        }
+
+        scan.close();
+    }
+}
