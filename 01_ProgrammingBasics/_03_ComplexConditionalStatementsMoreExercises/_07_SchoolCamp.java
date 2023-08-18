625e4ef9f67163c8bbb239ef78b663d0e2a995e6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 05:35:25 +0300

    07

---
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_07_SchoolCamp.java
@@ -0,0 +1,63 @@
+package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _07_SchoolCamp {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        String season = scan.nextLine().toLowerCase();
+        String groupType = scan.nextLine().toLowerCase();
+        int studentsCount = Integer.parseInt(scan.nextLine());
+        int nightsCount = Integer.parseInt(scan.nextLine());
+
+        // Add variables
+        double price = 0;
+        String sport = "";
+
+        // Calculate initial price for all the students
+        if (season.equals("winter")) {
+            price = groupType.equals("mixed") ? studentsCount * 10 * nightsCount : studentsCount * 9.60 * nightsCount;
+            if (groupType.equals("girls")) {
+                sport = "Gymnastics";
+            } else if (groupType.equals("boys")) {
+                sport = "Judo";
+            } else {
+                sport = "Ski";
+            }
+        } else if (season.equals("spring")) {
+            price = groupType.equals("mixed") ? studentsCount * 9.50 * nightsCount : studentsCount * 7.20 * nightsCount;
+            if (groupType.equals("girls")) {
+                sport = "Athletics";
+            } else if (groupType.equals("boys")) {
+                sport = "Tennis";
+            } else {
+                sport = "Cycling";
+            }
+        } else {
+            price = groupType.equals("mixed") ? studentsCount * 20 * nightsCount : studentsCount * 15 * nightsCount;
+            if (groupType.equals("girls")) {
+                sport = "Volleyball";
+            } else if (groupType.equals("boys")) {
+                sport = "Football";
+            } else {
+                sport = "Swimming";
+            }
+        }
+
+        // Add in the discount
+        if(studentsCount >= 10 && studentsCount < 20){
+            price *= 0.95;
+        }else if(studentsCount >= 20 && studentsCount < 50){
+            price *= 0.85;
+        }else if(studentsCount >= 50){
+            price *= 0.50;
+        }
+
+        // Print the result
+        System.out.printf("%s %.2f lv.", sport, price);
+
+        scan.close();
+    }
+}
