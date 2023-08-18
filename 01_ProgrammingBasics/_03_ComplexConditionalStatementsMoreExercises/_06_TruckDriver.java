5d6aff1e599259b546d9207b76a7d7dcc3e0cbd0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-18 05:11:34 +0300

    06

---
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_06_TruckDriver.java
@@ -0,0 +1,41 @@
+package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercise;
+
+import java.util.Scanner;
+
+public class _06_TruckDriver {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        String season = scan.nextLine().toLowerCase();
+        double kilometersPerMonth = Double.parseDouble(scan.nextLine());
+
+        // Add variables
+        double courseCost = 0;
+
+        // Calculate the cost of the course based on the kilometers and season, and removing the tax
+        if (kilometersPerMonth <= 5000) {
+            if (season.equals("spring") || season.equals("autumn")) {
+                courseCost = kilometersPerMonth * 0.75 * 4 * 0.90;
+            } else if (season.equals("summer")) {
+                courseCost = kilometersPerMonth * 0.90 * 4 * 0.90;
+            } else {
+                courseCost = kilometersPerMonth * 1.05 * 4 * 0.90;
+            }
+        } else if (kilometersPerMonth <= 10000) {
+            if (season.equals("spring") || season.equals("autumn")) {
+                courseCost = kilometersPerMonth * 0.95 * 4 * 0.90;
+            } else if (season.equals("summer")) {
+                courseCost = kilometersPerMonth * 1.10 * 4 * 0.90;
+            } else {
+                courseCost = kilometersPerMonth * 1.25 * 4 * 0.90;
+            }
+        } else {
+            courseCost = kilometersPerMonth * 1.45 * 4 * 0.90;
+        }
+
+        System.out.printf("%.2f", courseCost);
+
+        scan.close();
+    }
+}
