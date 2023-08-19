546d9808e89f150cd54934ec75554ec47379be8b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:25:38 +0300

    05

--- 01_ProgrammingBasics/_04_LoopsPartOne/_05_CharacterSequence.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_05_CharacterSequence.java
@@ -6,12 +6,16 @@ public class _05_CharacterSequence {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value text
         String text = scan.nextLine();
 
+        // Iterate through each character in the input text
         for (int i =0; i < text.length(); i++){
             char symbol = text.charAt(i);
 
             System.out.println(symbol);
         }
+
+        scan.close();
     }
 }
