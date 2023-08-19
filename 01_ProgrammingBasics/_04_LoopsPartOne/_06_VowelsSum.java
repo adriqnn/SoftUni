22055a776df2aaa3d3606b7625840eec422a994f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:32:02 +0300

    06

--- 01_ProgrammingBasics/_04_LoopsPartOne/_06_VowelsSum.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_06_VowelsSum.java
@@ -6,39 +6,37 @@ public class _06_VowelsSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String text = scan.nextLine();
 
         int sum = 0;
 
+        // Iterate through the input value text and sum all the vowels
         for (int i = 0; i < text.length(); i++){
             char symbol = text.charAt(i);
 
             switch (symbol){
-                case 'a':{
-                    sum+=1;
-                }
-                break;
-                case 'e':{
-                    sum+=2;
-                }
-                break;
-                case 'i':{
-                    sum+=3;
-                }
-                break;
-                case 'o':{
-                    sum+=4;
-                }
-                break;
-                case'u':{
-                    sum+=5;
-                }
-                break;
+                case 'a':
+                    sum += 1;
+                    break;
+                case 'e':
+                    sum += 2;
+                    break;
+                case 'i':
+                    sum += 3;
+                    break;
+                case 'o':
+                    sum += 4;
+                    break;
+                case 'u':
+                    sum += 5;
+                    break;
             }
-
-            if (symbol == 'a');
-
         }
+
+        // Print the sum of vowel values
         System.out.println(sum);
+
+        scan.close();
     }
 }
