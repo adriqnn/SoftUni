fc7faea89940721d2f99faaf55eb39b059f8feb1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 01:35:41 +0300

    06

--- 01_ProgrammingBasics/_04_LoopsPartOneExercise/_06_Oscars.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneExercise/_06_Oscars.java
@@ -6,6 +6,7 @@ public class _06_Oscars {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String actorName = scan.nextLine();
         double academyPoints = Double.parseDouble(scan.nextLine());
         int numberOfJury = Integer.parseInt(scan.nextLine());
@@ -15,16 +16,23 @@ public class _06_Oscars {
         for (int i = 0; i < numberOfJury; i++) {
             String nameOfJuror = scan.nextLine();
             double points = Double.parseDouble(scan.nextLine());
+
+            // Calculate additional points based on the juror's name length
             totalPoints += points * nameOfJuror.length() / 2;
-            if(totalPoints >= 1250.5){
+
+            // Check if the actor has enough points to get a nominee
+            if (totalPoints >= 1250.5) {
                 break;
             }
         }
 
-        if(totalPoints >= 1250.5){
-            System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!", actorName, totalPoints);
-        }else{
-            System.out.printf("Sorry, %s you need %.1f more!", actorName, 1250.5 - totalPoints);
+        // Print the result based on the total points
+        if (totalPoints >= 1250.5) {
+            System.out.printf("Congratulations, %s got a nominee for leading role with %.1f!%n", actorName, totalPoints);
+        } else {
+            System.out.printf("Sorry, %s you need %.1f more!%n", actorName, 1250.5 - totalPoints);
         }
+
+        scan.close();
     }
 }
