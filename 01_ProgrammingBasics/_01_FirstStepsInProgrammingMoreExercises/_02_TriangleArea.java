4a61c7bdd3df15cfa04e0c4fefb75d44b59381f1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:00:23 +0300

    02

---
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercises/_02_TriangleArea.java
@@ -0,0 +1,21 @@
+package _01_ProgrammingBasics._01_FirstStepsInProgrammingMoreExercises;
+
+import java.util.Scanner;
+
+public class _02_TriangleArea {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input data
+        double triangleSide = Double.parseDouble(scan.nextLine());
+        double triangleHeight = Double.parseDouble(scan.nextLine());
+
+        // Calculate the area of the triangle
+        double triangleArea = triangleSide * triangleHeight / 2;
+
+        // Print the calculated area with two decimal places
+        System.out.printf("%.2f%n", triangleArea);
+
+        scan.close();
+    }
+}
