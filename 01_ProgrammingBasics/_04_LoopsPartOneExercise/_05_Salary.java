1c302e9e24ae5634e0e6f86dbed85aacfd2b4cdb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 01:32:25 +0300

    05

--- 01_ProgrammingBasics/_04_LoopsPartOneExercise/_05_Salary.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneExercise/_05_Salary.java
@@ -6,27 +6,40 @@ public class _05_Salary {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int n = Integer.parseInt(scan.nextLine());
         int salary = Integer.parseInt(scan.nextLine());
 
+        // Read new input value
         for (int i = 0; i < n; i++) {
             String site = scan.nextLine();
 
-            if (site.equals("Facebook")){
-                salary -=150;
-            }else if (site.equals("Instagram")){
-                salary -=100;
-            }else if (site.equals("Reddit")){
-                salary -=50;
+            // Deduct salary based on the social media site visited
+            switch (site) {
+                case "Facebook":
+                    salary -= 150;
+                    break;
+                case "Instagram":
+                    salary -= 100;
+                    break;
+                case "Reddit":
+                    salary -= 50;
+                    break;
             }
-            if (salary <=0){
+
+            // Check if the salary has gone negative
+            if (salary <= 0) {
                 break;
             }
         }
-        if (salary<=0){
+
+        // Print the result based on the salary
+        if (salary <= 0) {
             System.out.println("You have lost your salary.");
-        }else{
+        } else {
             System.out.println(salary);
         }
+
+        scan.close();
     }
 }
