1bca56e3b7004cbf5eb547ebcb820c6466d05b7f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 01:27:10 +0300

    04

--- 01_ProgrammingBasics/_04_LoopsPartOneExercise/_04_CleverLily.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneExercise/_04_CleverLily.java
@@ -6,35 +6,39 @@ public class _04_CleverLily {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int age = Integer.parseInt(scan.nextLine());
         double washMachinePrice = Double.parseDouble(scan.nextLine());
         double toyPrice = Double.parseDouble(scan.nextLine());
-
 
         int toysCounter = 0;
         double sumMoney = 0;
         double money = 0;
         int count = 0;
 
-
+        // Calculate the number of toys received and money saved for each birthday
         for (int i = 1; i <= age; i++) {
-            if (i%2 !=0){
+            if (i % 2 != 0) {
                 toysCounter++;
-            }else{
-                money = money+10;
-                sumMoney = sumMoney+money;
+            } else {
+                money += 10;
+                sumMoney += money;
                 count++;
             }
         }
 
+        // Calculate the total money from selling the toys and saved money
         double allToysMoney = toyPrice * toysCounter;
-        double sum = allToysMoney + sumMoney - count;
-        double sum2 = sum-washMachinePrice;
+        double totalMoney = allToysMoney + sumMoney - count;
+        double diff = Math.abs(totalMoney - washMachinePrice);
 
-        if (sum>=washMachinePrice){
-            System.out.printf("Yes! %.2f", Math.abs(sum2));
-        }else{
-            System.out.printf("No! %.2f", Math.abs(sum2));
+        // Determine if Lily can buy the washing machine and print the result
+        if (totalMoney >= washMachinePrice) {
+            System.out.printf("Yes! %.2f%n", diff);
+        } else {
+            System.out.printf("No! %.2f%n", diff);
         }
+
+        scan.close();
     }
 }
