e419c7c99213e4f4fb0a854ae8b84a29fc7b3551
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:38:51 +0300

    08

--- 01_ProgrammingBasics/_04_LoopsPartOne/_08_NumberSequence.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_08_NumberSequence.java
@@ -6,12 +6,15 @@ public class _08_NumberSequence {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Stage the default
         int maxNum = Integer.MIN_VALUE;
         int minNum = Integer.MAX_VALUE;
 
-        for ( int i = 1; i<=n; i++){
+        // Find the max and min numbers in the n sequence
+        for ( int i = 1; i <= n; i++){
             int currentNum = Integer.parseInt(scan.nextLine());
             if (currentNum > maxNum){
                 maxNum = currentNum;
@@ -21,7 +24,10 @@ public class _08_NumberSequence {
             }
         }
 
+        // Print the max and the min numbers
         System.out.printf("Max number: %d%n",maxNum);
         System.out.printf("Min number: %d", minNum);
+
+        scan.close();
     }
 }
