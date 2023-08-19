3d8efd4261fe1456335ee2fcf53ecf1d57fdb957
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:00:41 +0300

    04

---
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercises/_04_VegetableMarket.java
@@ -0,0 +1,23 @@
+package _01_ProgrammingBasics._01_FirstStepsInProgrammingMoreExercises;
+
+import java.util.Scanner;
+
+public class _04_VegetableMarket {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input prices and kilograms
+        double priceForKilogramVegetables = Double.parseDouble(scan.nextLine());
+        double priceForKilogramFruits = Double.parseDouble(scan.nextLine());
+        int kilogramsVegetables = Integer.parseInt(scan.nextLine());
+        int kilogramsFruits = Integer.parseInt(scan.nextLine());
+
+        // Calculate total revenue
+        double revenueInEuro = ((priceForKilogramVegetables * kilogramsVegetables) + (priceForKilogramFruits * kilogramsFruits)) / 1.94;
+
+        // Print the total revenue
+        System.out.printf("%.2f%n", revenueInEuro);
+
+        scan.close();
+    }
+}
