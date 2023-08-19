42814ef0e8b48aa4b74baa836f2aabe2707754f0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:13:23 +0300

    03

--- 01_ProgrammingBasics/_04_LoopsPartOne/_03_Numbers1ToNWithStep3.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_03_Numbers1ToNWithStep3.java
@@ -6,10 +6,14 @@ public class _03_Numbers1ToNWithStep3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Loop from 1 to n with a step of 3 and print each number
         for (int i = 1; i <= n; i = i + 3){
             System.out.println(i);
         }
+
+        scan.close();
     }
 }
