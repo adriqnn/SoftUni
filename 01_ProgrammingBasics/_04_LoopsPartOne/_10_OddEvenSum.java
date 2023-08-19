a9594927db5717581069f1b83dc13a5b7ea69f07
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:44:36 +0300

    10

--- 01_ProgrammingBasics/_04_LoopsPartOne/_10_OddEvenSum.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_10_OddEvenSum.java
@@ -6,25 +6,32 @@ public class _10_OddEvenSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         int sumEven = 0;
         int sumOdd =0;
 
+        // Loop through the n sequence of numbers and sum even and odd numbers
         for (int i = 1; i <= n; i++) {
             int num = Integer.parseInt(scan.nextLine());
 
-            if (i%2 == 0){
+            if (i % 2 == 0){
+                // Add even-indexed numbers to sumEven
                 sumEven += num;
             }else {
+                // Add odd-indexed numbers to sumOdd
                 sumOdd += num;
             }
         }
 
-        if (sumEven==sumOdd){
+        // Compare the sums and print the result
+        if (sumEven == sumOdd){
             System.out.printf("Yes%n Sum = %d",sumEven);
         }else{
             System.out.printf("No %n Diff = %d",Math.abs(sumEven-sumOdd));
         }
+
+        scan.close();
     }
 }
