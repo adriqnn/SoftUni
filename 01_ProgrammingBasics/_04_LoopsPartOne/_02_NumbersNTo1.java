01c962f8b12aac228f82357275b0bcf913fd1fed
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:11:42 +0300

    02

--- 01_ProgrammingBasics/_04_LoopsPartOne/_02_NumbersNTo1.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_02_NumbersNTo1.java
@@ -6,10 +6,14 @@ public class _02_NumbersNTo1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Loop from n down to 1 and print each number
         for (int i = n; i >= 1; i--){
             System.out.println(i);
         }
+
+        scan.close();
     }
 }
