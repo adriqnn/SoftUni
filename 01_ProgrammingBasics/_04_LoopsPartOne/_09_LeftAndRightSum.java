f562027ed8531bf9a8a1d420038b67868f0d66d1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:41:55 +0300

    09

--- 01_ProgrammingBasics/_04_LoopsPartOne/_09_LeftAndRightSum.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_09_LeftAndRightSum.java
@@ -6,24 +6,31 @@ public class _09_LeftAndRightSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         int leftSum = 0;
         int rightSum = 0;
 
+        // Read and calculate the sum of the left sequence
         for(int i =1; i <= n; i++) {
             int currentNum = Integer.parseInt(scan.nextLine());
             leftSum += currentNum;
         }
+
+        // Read and calculate the sum of the right sequence
         for (int i = 1; i <= n; i++) {
             int currentNum = Integer.parseInt(scan.nextLine());
             rightSum += currentNum;
         }
 
+        // Compare the sums and print the result
         if (leftSum == rightSum){
             System.out.printf("Yes, sum = %d",rightSum);
         }else {
             System.out.printf("No, diff = %d",Math.abs(leftSum-rightSum));
         }
+
+        scan.close();
     }
 }
