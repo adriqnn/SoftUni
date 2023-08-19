a3921391cb8f9e4287bb782d63ffedd3d380614e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:34:48 +0300

    07

--- 01_ProgrammingBasics/_04_LoopsPartOne/_07_SumNumbers.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_07_SumNumbers.java
@@ -6,14 +6,20 @@ public class _07_SumNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         int sum = 0;
 
+        // Sum all the numbers for the n loop
         for (int i = 1; i <= n; i++){
             int currentNum = Integer.parseInt(scan.nextLine());
             sum += currentNum;
         }
+
+        // Print the sum of numbers
         System.out.println(sum);
+
+        scan.close();
     }
 }
