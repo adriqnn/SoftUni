3aeee67358c4455ea352d67a8a78e526410e9770
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:08:55 +0300

    01

--- 01_ProgrammingBasics/_04_LoopsPartOne/_01_NumbersFrom1To100.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_01_NumbersFrom1To100.java
@@ -3,6 +3,7 @@ package _01_ProgrammingBasics._04_LoopsPartOne;
 public class _01_NumbersFrom1To100 {
     public static void main(String[] args) {
 
+        // Print the numbers from 1 to 100 
         for (int i = 1; i <= 100; i = i + 1) {
             System.out.println(i);
         }
