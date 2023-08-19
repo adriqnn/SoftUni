ca9a2cecd0d31269f29c3086f726beaff2e77af4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 01:20:45 +0300

    02

--- 01_ProgrammingBasics/_04_LoopsPartOneExercise/_02_HalfSumElement.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneExercise/_02_HalfSumElement.java
@@ -6,27 +6,32 @@ public class _02_HalfSumElement {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         int nSum = 0;
         int maxNum = Integer.MIN_VALUE;
 
-        for (int i = 1; i <=n ; i++) {
-
+        // Read integers and find the maximum
+        for (int i = 1; i <= n; i++) {
+            // Read new input value
             int num = Integer.parseInt(scan.nextLine());
 
-            if (num>maxNum){
+            if (num > maxNum) {
                 maxNum = num;
             }
             nSum += num;
         }
 
-        int element = nSum-maxNum;
+        int element = nSum - maxNum;
 
-        if (element == maxNum){
-            System.out.printf("Yes %n Sum = %d",maxNum);
-        }else{
-            System.out.printf("No %n Diff = %d", Math.abs(element-maxNum));
+        // Compare the sums and print the result
+        if (element == maxNum) {
+            System.out.printf("Yes%nSum = %d%n", maxNum);
+        } else {
+            System.out.printf("No%nDiff = %d%n", Math.abs(element - maxNum));
         }
+
+        scan.close();
     }
 }
