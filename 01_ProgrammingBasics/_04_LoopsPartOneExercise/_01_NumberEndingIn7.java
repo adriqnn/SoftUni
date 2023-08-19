5102ff3e5cf484fc5d2de77c377791f12aa61951
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 01:18:09 +0300

    01

--- 01_ProgrammingBasics/_04_LoopsPartOneExercise/_01_NumberEndingIn7.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneExercise/_01_NumberEndingIn7.java
@@ -3,9 +3,11 @@ package _01_ProgrammingBasics._04_LoopsPartOneExercise;
 public class _01_NumberEndingIn7 {
     public static void main(String[] args) {
 
+        // Iterate through numbers from 7 to 1000
         for (int i = 7; i <= 1000; i++) {
 
-            if (i%10 == 7){
+            // Check if the last digit is 7
+            if (i % 10 == 7) {
                 System.out.println(i);
             }
         }
