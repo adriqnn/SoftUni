613b619ff08a13a2b22fdcdc33dad386d090b452
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:24:00 +0300

    04

--- 01_ProgrammingBasics/_04_LoopsPartOne/_04_EvenPowersOf2.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_04_EvenPowersOf2.java
@@ -6,17 +6,21 @@ public class _04_EvenPowersOf2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         int power = 1;
 
+        // Loop through the powers of 2 from 2^0 to 2^n
         for (int i = 0; i <= n; i++){
             if(i == 0){
                 System.out.println(1);
-            }else if (i%2 == 0){
+            }else if (i % 2 == 0){
                 System.out.println(power);
             }
-            power=power*2;
+            power = power * 2;
         }
+
+        scan.close();
     }
 }
