81005ad8be3c501cc750804d5fe03e7d32e0cb83
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-19 04:01:22 +0300

    09

---
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercises/_09_WeatherForecast.java
@@ -0,0 +1,17 @@
+package _01_ProgrammingBasics._01_FirstStepsInProgrammingMoreExercises;
+
+import java.util.Scanner;
+
+public class _09_WeatherForecast {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read the weather type input
+        String weatherType = scan.nextLine();
+
+        // Determine and print the appropriate message
+        System.out.printf("%s%n", weatherType.equals("sunny") ? "It's warm outside!" : "It's cold outside!");
+
+        scan.close();
+    }
+}
