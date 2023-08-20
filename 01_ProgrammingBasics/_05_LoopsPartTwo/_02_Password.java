f0d30654ac81576d7a7de28efaa45376ee7ba2f7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:35:47 +0300

    02

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_02_Password.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_02_Password.java
@@ -6,14 +6,20 @@ public class _02_Password {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String username = scan.nextLine();
         String password = scan.nextLine();
 
         String input = scan.nextLine();
 
+        // Use a while loop to repeatedly ask for the password until it matches
         while(!input.equals(password)){
             input = scan.nextLine();
         }
+
+        // Welcome the user after successful login
         System.out.printf("Welcome %s!",username);
+
+        scan.close();
     }
 }
