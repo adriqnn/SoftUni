9d888da2ee5b67fc6df593e7fdb87ab1e9dfd136
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:51:09 +0300

    06

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_06_MaxNumber.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_06_MaxNumber.java
@@ -6,17 +6,25 @@ public class _06_MaxNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String command = scan.nextLine();
 
+        // Staging
         int num = Integer.MIN_VALUE;
 
         while(!command.equals("Stop")){
             int number = Integer.parseInt(command);
-            if (number>num){
+
+            // Check if the current number is greater than the current maximum
+            if (number > num){
                 num = number;
             }
             command = scan.nextLine();
         }
+        
+        // Display the maximum number
         System.out.println(num);
+
+        scan.close();
     }
 }
