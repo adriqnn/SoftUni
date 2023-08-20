58230336766e39f630eb7695481befef9229efd6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:48:12 +0300

    05

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_05_AccountBalance.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_05_AccountBalance.java
@@ -6,21 +6,31 @@ public class _05_AccountBalance {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String command = scan.nextLine();
 
-        double account = 0;
+        // Initialize the account balance
+        double accountBalance = 0;
 
         while(!command.equals("NoMoreMoney")){
             double deposit = Double.parseDouble(command);
 
-            if (deposit <0){
+            // Exit the loop if the amount is invalid
+            if (deposit < 0){
                 System.out.println("Invalid operation!");
                 break;
             }
-            account += deposit;
-            System.out.printf("Increase: %.2f%n",deposit);
+
+            // Update the account balance and display the increase
+            accountBalance += deposit;
+            System.out.printf("Increase: %.2f%n", deposit);
+
             command = scan.nextLine();
         }
-        System.out.printf("Total: %.2f",account);
+
+        // Display the final account balance
+        System.out.printf("Total: %.2f", accountBalance);
+
+        scan.close();
     }
 }
