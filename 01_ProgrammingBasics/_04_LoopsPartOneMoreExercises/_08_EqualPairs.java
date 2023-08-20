b0988986b4c547f3300687ad35251b41e0291618
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 16:08:11 +0300

    08

---
+++ 01_ProgrammingBasics/_04_LoopsPartOneMoreExercises/_08_EqualPairs.java
@@ -0,0 +1,10 @@
+package _01_ProgrammingBasics._04_LoopsPartOneMoreExercises;
+
+import java.util.Scanner;
+
+public class _08_EqualPairs {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    }
+}
