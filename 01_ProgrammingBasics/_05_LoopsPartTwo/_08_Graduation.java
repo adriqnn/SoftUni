cf5d13d1d2a098f83fa6ceee62bbcda0e7b2e92c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 01:00:41 +0300

    08

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_08_Graduation.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_08_Graduation.java
@@ -6,30 +6,40 @@ public class _08_Graduation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String name = scan.nextLine();
 
+        // Add variables
         int excluded = 0;
         int count = 1;
         double sum = 0;
 
+        // Use a while loop to input and process the student's grades
         while (count <= 12){
             double grade = Double.parseDouble(scan.nextLine());
 
-            if (grade<4.0){
+            if (grade < 4.0) {
                 excluded++;
-            }else{
+            } else {
                 count++;
             }
-            if (excluded>1){
+
+            // Check if the student has been excluded
+            if (excluded > 1) {
                 break;
             }
 
             sum += grade;
         }
-        if (excluded>1){
-            System.out.printf("%s has been excluded at %d grade",name,count);
-        }else{
-            System.out.printf("%s graduated. Average grade: %.2f",name,sum/12);
+
+        // Display the result based on the student's status
+        if (excluded > 1) {
+            System.out.printf("%s has been excluded at %d grade%n", name, count);
+        } else {
+            double averageGrade = sum / 12;
+            System.out.printf("%s graduated. Average grade: %.2f%n", name, averageGrade);
         }
+
+        scan.close();
     }
 }
