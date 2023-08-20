17731c9ab00620d198ec5f0c25501f785d77613b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:39:33 +0300

    04

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_04_Sequence2K1.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_04_Sequence2K1.java
@@ -6,14 +6,19 @@ public class _04_Sequence2K1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Staging
         int k = 1;
 
+        // Use a while loop to generate and print the sequence
         while(k <= n){
             System.out.println(k);
 
             k = 2 * k + 1;
         }
+
+        scan.close();
     }
 }
