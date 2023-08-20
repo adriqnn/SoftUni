0b8f684bc496917f326650dce551c1c7c4d4a0c1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:55:58 +0300

    07

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_07_MinNumber.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_07_MinNumber.java
@@ -6,16 +6,25 @@ public class _07_MinNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String command = scan.nextLine();
+
+        // Staging
         int num = Integer.MAX_VALUE;
 
         while(!command.equals("Stop")){
             int number = Integer.parseInt(command);
-            if (number<num){
-                num=number;
+
+            // Check if the current number is lower than the current minimum
+            if (number < num){
+                num = number;
             }
             command = scan.nextLine();
         }
+
+        // Display the minimum number
         System.out.println(num);
+
+        scan.close();
     }
 }
