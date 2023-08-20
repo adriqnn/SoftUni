ab506a597e0ce2aba7b5db76e210cd4891462dac
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-20 03:42:24 +0300

    02

---
+++ 01_ProgrammingBasics/_04_LoopsPartOneMoreExercises/_02_Hospital.java
@@ -0,0 +1,42 @@
+package _01_ProgrammingBasics._04_LoopsPartOneMoreExercises;
+
+import java.util.Scanner;
+
+public class _02_Hospital {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        int hospitalPeriod = Integer.parseInt(scan.nextLine());
+
+        // Add variables
+        int availableDoctors = 7;
+        int treatedPatients = 0;
+        int untreatedPatients = 0;
+
+        //
+        for (int i = 1; i <= hospitalPeriod; i++) {
+            // Check if it's a day to evaluate doctor availability
+            if( i % 3 == 0 && untreatedPatients > treatedPatients){
+                availableDoctors++;
+            }
+
+            // Read the number of patients for the current day
+            int patients = Integer.parseInt(scan.nextLine());
+
+            // Treat patients based on doctor availability
+            if(availableDoctors >= patients){
+                treatedPatients += patients;
+            }else{
+                treatedPatients += availableDoctors;
+                untreatedPatients += Math.abs(patients - availableDoctors);
+            }
+        }
+
+        // Print the number of treated and untreated patients
+        System.out.printf("Treated patients: %d.%n", treatedPatients);
+        System.out.printf("Untreated patients: %d.", untreatedPatients);
+
+        scan.close();
+    }
+}
