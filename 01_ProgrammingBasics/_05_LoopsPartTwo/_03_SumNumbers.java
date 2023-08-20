2b248c843cd5a8447725fdfd8a5074ccf5391d3c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 00:37:40 +0300

    03

--- 01_ProgrammingBasics/_05_LoopsPartTwo/_03_SumNumbers.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwo/_03_SumNumbers.java
@@ -6,14 +6,20 @@ public class _03_SumNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int initialNum = Integer.parseInt(scan.nextLine());
 
         int sum = 0;
 
+        // Use a while loop to continuously read numbers and add them to the sum
         while(sum<initialNum){
             int currentNum = Integer.parseInt(scan.nextLine());
             sum += currentNum;
         }
+
+        // Display the final sum
         System.out.println(sum);
+
+        scan.close();
     }
 }
