386b14e8ec235e2b3cf6e0eadf072d55819bf22c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 02:48:17 +0300

    02

--- 01_ProgrammingBasics/_06_NestedLoopsExercise/_02_EqualSumsEvenOddPosition.java
+++ 01_ProgrammingBasics/_06_NestedLoopsExercise/_02_EqualSumsEvenOddPosition.java
@@ -6,26 +6,35 @@ public class _02_EqualSumsEvenOddPosition {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int num1 = Integer.parseInt(scan.nextLine());
         int num2 = Integer.parseInt(scan.nextLine());
 
-        for (int i = num1; i <= num2 ; i++) {
+        // Print number in between the numbers above based on the requirement
+        for (int i = num1; i <= num2; i++) {
             String current = "" + i;
-            int even =0;
-            int odd =0;
+
+            int even = 0;
+            int odd = 0;
+
             for (int j = 0; j < current.length(); j++) {
                 int currentDigit = Integer.parseInt("" + current.charAt(j));
-                if(j%2==0){
-                    even +=currentDigit;
-                }
-                if(j%2!=0){
-                    odd +=currentDigit;
-                }
 
+                // Check if the current index is even or odd
+                if(j % 2 == 0){
+                    even += currentDigit;
+                }
+                if(j % 2 != 0){
+                    odd += currentDigit;
+                }
             }
+
+            // If the sums of even and odd-positioned digits are equal, print the number
             if (even == odd){
                 System.out.print(i + " ");
             }
         }
+
+        scan.close();
     }
 }
