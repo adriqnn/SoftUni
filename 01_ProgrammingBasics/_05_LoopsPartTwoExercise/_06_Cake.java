cd48d9274130d5b1ff1672aaf3d07e7df1c60f91
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 06:11:03 +0300

    06

--- 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_06_Cake.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_06_Cake.java
@@ -6,25 +6,37 @@ public class _06_Cake {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int width = Integer.parseInt(scan.nextLine());
         int length = Integer.parseInt(scan.nextLine());
         String console = scan.nextLine();
-        int wholeCake = width*length;
-        int cake = wholeCake;
-        int allSlices = 0;
 
+        // Add variables
+        int wholeCake = width * length;
+        int remainingCake = wholeCake;
+        int totalSlices = 0;
+
+        // Using while manage the slices of the cake
         while(!console.equals("STOP")){
             int slices = Integer.parseInt(console);
-            cake -= slices;
-            allSlices += slices;
-            if (cake<=0){
-                System.out.printf("No more cake left! You need %d pieces more.", Math.abs(wholeCake-allSlices));
+
+            // Update the remaining cake and total slices taken
+            remainingCake -= slices;
+            totalSlices += slices;
+
+            if (remainingCake <= 0){
+                System.out.printf("No more cake left! You need %d pieces more.", Math.abs(wholeCake - totalSlices));
                 break;
             }
+
             console = scan.nextLine();
         }
+
+        // Display the remaining pieces if the user stopped
         if (console.equals("STOP")){
-            System.out.printf("%d pieces are left.",cake);
+            System.out.printf("%d pieces are left.",remainingCake);
         }
+
+        scan.close();
     }
 }
