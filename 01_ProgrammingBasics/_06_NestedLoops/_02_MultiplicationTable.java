c7bebf245f291dd514eeb98a602b0af9822d0323
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 20:57:56 +0300

    02

--- 01_ProgrammingBasics/_06_NestedLoops/_02_MultiplicationTable.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_02_MultiplicationTable.java
@@ -6,7 +6,7 @@ public class _02_MultiplicationTable {
         // Print multiplication table from 1 to 10
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
-                System.out.printf("%d * %d = %d%n",i,j,i*j);
+                System.out.printf("%d * %d = %d%n", i, j, i * j);
             }
         }
     }
