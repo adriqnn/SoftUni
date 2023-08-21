3b2627677528c2d6866b30b2ec1a85844ee98e75
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 05:23:47 +0300

    01

--- 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_01_OldBooks.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_01_OldBooks.java
@@ -6,23 +6,29 @@ public class _01_OldBooks {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String book = scan.nextLine();
-        String fav = scan.nextLine();
+        String favoriteBook = scan.nextLine();
 
+        // Staging
         int count = 0;
 
-        while(!fav.equals(book)){
-            if (fav.equals("No More Books")){
+        // Use a while loop to search for the favorite book
+        while(!favoriteBook.equals(book)){
+            if (favoriteBook.equals("No More Books")){
                 System.out.println("The book you search is not here!");
                 System.out.printf("You checked %d books.", count);
                 break;
             }
             count++;
-            fav = scan.nextLine();
+            favoriteBook = scan.nextLine();
         }
 
-        if (fav.equals(book)){
+        // Display the result based on whether the favorite book was found
+        if (favoriteBook.equals(book)){
             System.out.printf("You checked %d books and found it.", count);
         }
+
+        scan.close();
     }
 }
