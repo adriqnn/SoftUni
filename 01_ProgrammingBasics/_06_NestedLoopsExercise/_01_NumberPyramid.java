8470be04bc620f32077437b3ca15e68f5ee7b0c0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 02:42:56 +0300

    01

--- 01_ProgrammingBasics/_06_NestedLoopsExercise/_01_NumberPyramid.java
+++ 01_ProgrammingBasics/_06_NestedLoopsExercise/_01_NumberPyramid.java
@@ -6,23 +6,32 @@ public class _01_NumberPyramid {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
 
+        // Add variables
         int current = 1;
         boolean isBigger = false;
+        
+        // Print the number pyramid
         for (int rows = 1; rows <= num; rows++) {
             for (int cols = 1; cols <= rows; cols++) {
                 if (current > num){
                     isBigger = true;
                     break;
                 }
+                
                 System.out.print(current + " ");
                 current++;
             }
+            
             if (isBigger){
                 break;
             }
+            
             System.out.println();
         }
+
+        scan.close();
     }
 }
