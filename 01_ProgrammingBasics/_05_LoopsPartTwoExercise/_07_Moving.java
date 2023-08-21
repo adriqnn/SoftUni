890cc18830f182eff8f76391edd62dbb3df9ffc5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 06:15:47 +0300

    07

--- 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_07_Moving.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_07_Moving.java
@@ -6,29 +6,39 @@ public class _07_Moving {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int width = Integer.parseInt(scan.nextLine());
         int length = Integer.parseInt(scan.nextLine());
         int height = Integer.parseInt(scan.nextLine());
 
-        int volume = width*length*height;
+        // Add variables
+        int volume = width * length * height;
+        int boxesSum = 0;
 
         String input = scan.nextLine();
 
-        int boxesSum = 0;
-
+        // Determine how many boxes are needed for the moving
         while(!input.equals("Done")){
             int boxes = Integer.parseInt(input);
-            boxesSum +=boxes;
-            if(boxesSum>=volume){
+            boxesSum += boxes;
+
+            if(boxesSum >= volume){
                 break;
             }
+
             input = scan.nextLine();
         }
-        int diff = Math.abs(boxesSum-volume);
-        if (boxesSum>=volume){
+
+        // Display the remaining or excess space in the room
+        int diff = Math.abs(boxesSum - volume);
+
+        // Print result 
+        if (boxesSum >= volume){
             System.out.printf("No more free space! You need %d Cubic meters more.", diff);
         }else{
-            System.out.printf("%d Cubic meters left.",diff);
+            System.out.printf("%d Cubic meters left.", diff);
         }
+
+        scan.close();
     }
 }
