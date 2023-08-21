33dacbd58cdd2d2144aff3d45dd4a93aea5b021b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 20:59:35 +0300

    03

--- 01_ProgrammingBasics/_06_NestedLoops/_03_Combinations.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_03_Combinations.java
@@ -6,18 +6,24 @@ public class _03_Combinations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
+        
+        // Add variable
         int count = 0;
 
         for (int i = 0; i <= num ; i++) {
             for (int j = 0; j <= num ; j++) {
                 for (int k = 0; k <=num ; k++) {
-                    if(i+j+k == num){
+                    if(i + j + k == num){
                         count++;
                     }
                 }
             }
         }
+        
         System.out.println(count);
+        
+        scan.close();
     }
 }
