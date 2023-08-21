1ee0a03f390f3b37803e3e2378871c33137dedb7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 02:21:11 +0300

    04

--- 01_ProgrammingBasics/_06_NestedLoops/_04_SumOfTwoNumbers.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_04_SumOfTwoNumbers.java
@@ -6,28 +6,38 @@ public class _04_SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int start = Integer.parseInt(scan.nextLine());
         int end = Integer.parseInt(scan.nextLine());
         int magicNum = Integer.parseInt(scan.nextLine());
 
+        // Add variables
         int count = 0;
         boolean isDone = false;
 
+        // Cycle from start to end and search for the magic number
         for (int i = start; i <= end; i++) {
             for (int j = start; j <= end; j++) {
                 count++;
-                if (i+j == magicNum){
-                    System.out.printf("Combination N:%d (%d + %d = %d)%n",count,i,j,i+j );
+
+                // Print result if magic number is found
+                if (i + j == magicNum){
+                    System.out.printf("Combination N:%d (%d + %d = %d)%n", count, i, j, i + j);
                     isDone = true;
                     break;
                 }
             }
+
             if (isDone){
                 break;
             }
         }
+
+        // Print result
         if(!isDone){
             System.out.printf("%d combinations - neither equals %d",count,magicNum);
         }
+
+        scan.close();
     }
 }
