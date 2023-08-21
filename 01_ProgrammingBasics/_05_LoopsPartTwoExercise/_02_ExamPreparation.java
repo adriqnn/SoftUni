b287b6a52b6e5489ec0f4eb3615c13632d2e8e93
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 05:30:36 +0300

    02

--- 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_02_ExamPreparation.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_02_ExamPreparation.java
@@ -6,33 +6,47 @@ public class _02_ExamPreparation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int grade = Integer.parseInt(scan.nextLine());
-        String name = scan.nextLine();
+        // Read input values
+        int poorGradeThreshold = Integer.parseInt(scan.nextLine());
+        String problemName = scan.nextLine();
 
-        boolean isTrue = false;
-        int countBad = 0;
-        double average = 0;
-        int count = 0;
-        String last = "";
+        // Add variables
+        boolean needBreak = false;
+        int poorGradeCount = 0;
+        double totalScore = 0;
+        int problemCount = 0;
+        String lastProblem = "";
 
-        while(!name.equals("Enough")){
-            int gradeNew = Integer.parseInt(scan.nextLine());
-            if (gradeNew<=4){
-                countBad++;
-                if (countBad==grade){
-                    System.out.printf("You need a break, %d poor grades.",countBad);
+        // Use a while loop to input and process the grades and problem names
+        while(!problemName.equals("Enough")){
+            int grade = Integer.parseInt(scan.nextLine());
+
+            if (grade <= 4){
+                poorGradeCount++;
+
+                if (poorGradeCount == poorGradeThreshold){
+                    needBreak = true;
                     break;
                 }
             }
-            last = name;
-            average += gradeNew;
-            count++;
-            name = scan.nextLine();
+
+            lastProblem = problemName;
+            totalScore += grade;
+            problemCount++;
+
+            problemName = scan.nextLine();
         }
-        if (name.equals("Enough")){
-            System.out.printf("Average score: %.2f\n", average/count);
-            System.out.printf("Number of problems: %d\n", count);
-            System.out.printf("Last problem: %s", last);
+
+        if (needBreak) {
+            System.out.printf("You need a break, %d poor grades.%n", poorGradeCount);
+        } else {
+            // Calculate and display the average score, problem count, and last problem
+            double averageScore = totalScore / problemCount;
+            System.out.printf("Average score: %.2f%n", averageScore);
+            System.out.printf("Number of problems: %d%n", problemCount);
+            System.out.printf("Last problem: %s%n", lastProblem);
         }
+
+        scan.close();
     }
 }
