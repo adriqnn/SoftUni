adc57b7a34b91f05a3a458e53e814a00e93913ad
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 05:51:45 +0300

    04

--- 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_04_Walking.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoExercise/_04_Walking.java
@@ -6,28 +6,37 @@ public class _04_Walking {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String console = scan.nextLine();
+
         int stepsCounter = 0;
 
+        // Use a while loop to track the steps walked
         while (!console.equals("Going home")){
             int steps = Integer.parseInt(console);
             stepsCounter += steps;
-            if (stepsCounter>=10000){
-                System.out.printf("Goal reached! Good job!%n" +
-                        "%d steps over the goal!", Math.abs(10000-stepsCounter));
+
+            if (stepsCounter >= 10000){
+                System.out.printf("Goal reached! Good job!%n%d steps over the goal!", Math.abs(10000 - stepsCounter));
                 break;
             }
+
             console = scan.nextLine();
         }
+
+        // Print result at the end
         if (console.equals("Going home")){
             int stepsAfter = Integer.parseInt(scan.nextLine());
+            
             stepsCounter += stepsAfter;
-            if (stepsCounter>=10000){
-                System.out.printf("Goal reached! Good job!%n" +
-                        "%d steps over the goal!", Math.abs(10000-stepsCounter));
+            
+            if (stepsCounter >= 10000){
+                System.out.printf("Goal reached! Good job!%n%d steps over the goal!", Math.abs(10000 - stepsCounter));
             }else{
-                System.out.printf("%d more steps to reach goal.",Math.abs(10000-stepsCounter));
+                System.out.printf("%d more steps to reach goal.",Math.abs(10000 - stepsCounter));
             }
         }
+
+        scan.close();
     }
 }
