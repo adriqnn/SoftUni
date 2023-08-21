731e88aa34c1b7df61cddf4c5af884b85d229f9d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 20:46:15 +0300

    01

--- 01_ProgrammingBasics/_06_NestedLoops/_01_Clock.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_01_Clock.java
@@ -2,6 +2,8 @@ package _01_ProgrammingBasics._06_NestedLoops;
 
 public class _01_Clock {
     public static void main(String[] args) {
+        
+        // Print the time
         for (int h = 0; h <= 23; h++) {
             for (int m = 0; m <= 59; m++) {
                 System.out.printf("%d:%d%n", h, m);
