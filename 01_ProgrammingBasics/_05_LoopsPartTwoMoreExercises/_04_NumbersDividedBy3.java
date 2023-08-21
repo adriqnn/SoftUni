7098f5514715c6ecb67eb1400f96c0cdacb071b6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-21 19:24:19 +0300

    04

---
+++ 01_ProgrammingBasics/_05_LoopsPartTwoMoreExercises/_04_NumbersDividedBy3.java
@@ -0,0 +1,12 @@
+package _01_ProgrammingBasics._05_LoopsPartTwoMoreExercises;
+
+public class _04_NumbersDividedBy3 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 100; i++) {
+            if(i % 3 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
