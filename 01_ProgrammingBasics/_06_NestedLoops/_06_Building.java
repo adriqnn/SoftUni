97acdbfaa72e4f59f0089fae9a25d7147237c15d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 02:35:24 +0300

    06

--- 01_ProgrammingBasics/_06_NestedLoops/_06_Building.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_06_Building.java
@@ -6,26 +6,38 @@ public class _06_Building {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int floors = Integer.parseInt(scan.nextLine());
         int rooms = Integer.parseInt(scan.nextLine());
 
-        for (int i = floors; i > floors-1; i=floors-1) {
+        // Print last floor large apartments
+        for (int i = floors; i > floors - 1; i = floors - 1) {
             for (int j = 0; j < rooms; j++) {
-                System.out.printf("L%d%d ",i,j);
+                System.out.printf("L%d%d ", i, j);
             }
         }
-        for (int i = floors-1; i >= 1;i--){
+
+        // Cycle through the rest of the building
+        for (int i = floors - 1; i >= 1; i--){
+
+            // New line
             System.out.printf("%n");
-            if (i%2 != 0) {
+
+            // Print apartments
+            if (i % 2 != 0) {
                 for (int j = 0; j < rooms; j++) {
                     System.out.printf("A%d%d ", i, j);
                 }
             }
-            if (i%2 ==0) {
+
+            // Print offices
+            if (i % 2 == 0) {
                 for (int j = 0; j < rooms; j++) {
                     System.out.printf("O%d%d ", i, j);
                 }
             }
         }
+
+        scan.close();
     }
 }
