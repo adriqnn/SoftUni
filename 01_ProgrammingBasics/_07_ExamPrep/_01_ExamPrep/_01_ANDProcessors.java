bad61568ce0b81056309c00a907d88d505779a55
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-23 02:33:38 +0300

    01

---
+++ 01_ProgrammingBasics/_07_ExamPrep/_01_ExamPrep/_01_ANDProcessors.java
@@ -0,0 +1,28 @@
+package _01_ProgrammingBasics._08_Exam;
+
+import java.util.Scanner;
+
+public class _01_ANDProcessors {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        int processors = Integer.parseInt(scan.nextLine());
+        int staff = Integer.parseInt(scan.nextLine());
+        int days = Integer.parseInt(scan.nextLine());
+
+        // Add variables
+        int work = staff * days * 8;
+        double processorsCost = Math.floor(1.00 * work / 3);
+        double total = Math.abs(processors - processorsCost);
+
+        // Print result
+        if (processors <= processorsCost){
+            System.out.printf("Profit: -> %.2f BGN", total * 110.10);
+        }else{
+            System.out.printf("Losses: -> %.2f BGN", total * 110.10);
+        }
+
+        scan.close();
+    }
+}
---
+++ 01_ProgrammingBasics/_07_ExamPrep/_01_ExamPrep/_02_CatFood.java
@@ -0,0 +1,42 @@
+package _01_ProgrammingBasics._08_Exam;
+
+import java.util.Scanner;
+
+public class _02_CatFood {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        int n = Integer.parseInt(scan.nextLine());
+
+        // Add variables
+        int catBig = 0;
+        int catMedium = 0;
+        int catSmall = 0;
+        int total = 0;
+
+        // Calculate statistics
+        for (int i = 0; i < n; i++) {
+            int grams = Integer.parseInt(scan.nextLine());
+
+            if(grams < 200){
+                catSmall++;
+            }else if(grams < 300){
+                catMedium++;
+            }else if(grams < 400){
+                catBig++;
+            }
+            total += grams;
+        }
+
+        double price = ((1.00 * total) / 1000) * 12.45;
+
+        // Print result
+        System.out.printf("Group 1: %d cats.%n", catSmall);
+        System.out.printf("Group 2: %d cats.%n", catMedium);
+        System.out.printf("Group 3: %d cats.%n", catBig);
+        System.out.printf("Price for food per day: %.2f lv.", price);
+
+        scan.close();
+    }
+}
