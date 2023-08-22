1e821cab6923318f0d0b87ce88ba73bb92a2e6c7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 22:26:22 +0300

    09

--- 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_09_SumOfTwoNumbers.java
+++ 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_09_SumOfTwoNumbers.java
@@ -6,13 +6,16 @@ public class _09_SumOfTwoNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int startOfInterval = Integer.parseInt(scan.nextLine());
         int endOfInterval = Integer.parseInt(scan.nextLine());
         int magicNumber = Integer.parseInt(scan.nextLine());
 
+        // Add variables
         int combinations = 0;
         boolean magicNumberFound = false;
 
+        // Find the combinations if there are any
         forLoopOuter:
         for (int i = startOfInterval; i <= endOfInterval; i++) {
             for (int j = startOfInterval; j <= endOfInterval; j++) {
@@ -27,8 +30,11 @@ public class _09_SumOfTwoNumbers {
             }
         }
 
+        // Print a message if there are no combinations
         if(!magicNumberFound){
             System.out.printf("%d combinations - neither equals %d", combinations, magicNumber);
         }
+        
+        scan.close();
     }
 }
