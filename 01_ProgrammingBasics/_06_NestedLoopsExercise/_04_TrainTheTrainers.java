319bc8bbd402e0b553c1cfceae88314c7386d0b6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-22 03:14:45 +0300

    04

--- 01_ProgrammingBasics/_06_NestedLoopsExercise/_04_TrainTheTrainers.java
+++ 01_ProgrammingBasics/_06_NestedLoopsExercise/_04_TrainTheTrainers.java
@@ -6,24 +6,35 @@ public class _04_TrainTheTrainers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int n = Integer.parseInt(scan.nextLine());
-        String console = scan.nextLine();
+        String presentationName = scan.nextLine();
 
-        double averageWhole = 0;
-        int count =0;
-        while(!console.equals("Finish")){
-            count++;
-            double average =0;
-            String console2 = "";
+        // Add variables
+        double totalAverage = 0;
+        int presentationCount = 0;
+
+        // Evaluate all presentations
+        while(!presentationName.equals("Finish")){
+            presentationCount++;
+            double presentationAverage = 0;
+
             for (int i = 1; i <= n; i++) {
                 double grade = Double.parseDouble(scan.nextLine());
-                average+=grade;
-                console2 = console;
+                presentationAverage += grade;
             }
-            averageWhole += average/n;
-            System.out.printf("%s - %.2f.%n",console2,average/n);
-            console = scan.nextLine();
+            
+            totalAverage += presentationAverage/n;
+            
+            System.out.printf("%s - %.2f.%n", presentationName, presentationAverage/n);
+            presentationName = scan.nextLine();
         }
-        System.out.printf("Student's final assessment is %.02f.%n",averageWhole/count);
+        
+        double finalAverage = totalAverage / presentationCount;
+        
+        // Print the result
+        System.out.printf("Student's final assessment is %.02f.%n", finalAverage);
+        
+        scan.close();
     }
 }
