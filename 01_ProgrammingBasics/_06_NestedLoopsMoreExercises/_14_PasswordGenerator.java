5fa948b748a7fa2fd332c0a1eb303048dd8454f7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-23 01:51:40 +0300

    14

---
+++ 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_14_PasswordGenerator.java
@@ -0,0 +1,28 @@
+package _01_ProgrammingBasics._06_NestedLoopsMoreExercises;
+
+import java.util.Scanner;
+
+public class _14_PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        int n = Integer.parseInt(scan.nextLine());
+        int l = Integer.parseInt(scan.nextLine());
+
+        // Determine the correct output based on the requirements and print
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                for (char k = 'a'; k < 'a' + l; k++) {
+                    for (char m = 'a'; m < 'a' +l; m++) {
+                        for (int o = 1; o <= n; o++) {
+                            if(o > i && o > j){
+                                System.out.printf("%d%d%s%s%d ", i, j, k, m, o);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
