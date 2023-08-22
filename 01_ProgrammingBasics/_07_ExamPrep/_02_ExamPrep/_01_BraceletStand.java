006c1e2cd54c0ed6260409f2e79516aee65d4c27
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-23 02:34:51 +0300

    02

---
+++ 01_ProgrammingBasics/_07_ExamPrep/_02_ExamPrep/_01_BraceletStand.java
@@ -0,0 +1,30 @@
+package _01_ProgrammingBasics._08_Exam;
+
+import java.util.Scanner;
+
+public class _01_BraceletStand {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        double money = Double.parseDouble(scan.nextLine());
+        double profit = Double.parseDouble(scan.nextLine());
+        double expenses = Double.parseDouble(scan.nextLine());
+        double gift = Double.parseDouble(scan.nextLine());
+
+        // Add variables
+        double money2 = 5 * money;
+        double profit2 = 5 * profit;
+        double saved = money2 + profit2;
+        double expenses2 = saved - expenses;
+
+        // Print result
+        if(gift <= expenses2){
+            System.out.printf("Profit: %.2f BGN, the gift has been purchased.", expenses2);
+        }else{
+            System.out.printf("Insufficient money: %.2f BGN.", Math.abs(expenses2 - gift));
+        }
+
+        scan.close();
+    }
+}
---
+++ 01_ProgrammingBasics/_07_ExamPrep/_02_ExamPrep/_02_FinalCompetition.java
@@ -0,0 +1,44 @@
+package _01_ProgrammingBasics._08_Exam;
+
+import java.util.Scanner;
+
+public class _02_FinalCompetition {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input values
+        int dancers = Integer.parseInt(scan.nextLine());
+        double points = Double.parseDouble(scan.nextLine());
+        String season = scan.nextLine();
+        String place = scan.nextLine();
+
+        // Add variable
+        double prize = 0;
+
+        // Determine the prize
+        if(place.equals("Bulgaria")){
+            prize = points * dancers;
+            if (season.equals("summer")){
+                prize *= 0.95;
+            }else if(season.equals("winter")){
+                prize *= 0.92;
+            }
+        }else{
+            prize = (points * dancers) + ((points * dancers) * 0.50);
+            if (season.equals("summer")){
+                prize *= 0.90;
+            }else if(season.equals("winter")){
+                prize *= 0.85;
+            }
+        }
+
+        double charity = prize * 0.75;
+        prize -= charity;
+
+        // Print result
+        System.out.printf("Charity - %.2f%n", charity);
+        System.out.printf("Money per dancer - %.2f%n", prize / dancers);
+
+        scan.close();
+    }
+}
