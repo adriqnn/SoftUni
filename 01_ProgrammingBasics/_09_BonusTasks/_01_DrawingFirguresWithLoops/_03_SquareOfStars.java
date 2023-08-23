502c2bda1b9615220b2929b558d5aad3027fa4fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-24 00:37:21 +0300

    03

--- 01_ProgrammingBasics/_09_BonusTasks/_01_DrawingFirguresWithLoops/_03_SquareOfStars.java
+++ 01_ProgrammingBasics/_09_BonusTasks/_01_DrawingFirguresWithLoops/_03_SquareOfStars.java
@@ -14,6 +14,7 @@ public class _03_SquareOfStars {
             for (int j = 1; j <= n; j++) {
                 System.out.print("* ");
             }
+            
             System.out.println();
         }
 
