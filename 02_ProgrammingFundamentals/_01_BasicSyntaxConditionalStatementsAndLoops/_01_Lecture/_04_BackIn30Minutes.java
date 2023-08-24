7f8dbcf9e88c5723da1e2cb5fe47760517a2bf9d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-24 23:09:24 +0300

    04

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_04_BackIn30Minutes.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_04_BackIn30Minutes.java
@@ -6,17 +6,20 @@ public class _04_BackIn30Minutes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int hours = Integer.parseInt(scan.nextLine());
         int minutes = Integer.parseInt(scan.nextLine());
 
-        int timePlus30 = hours*60 + minutes + 30;
-        int newHour = timePlus30/60;
-        int newMinutes = timePlus30%60;
+        // Add variables
+        int timePlus30 = hours * 60 + minutes + 30;
+        int newHour = timePlus30 / 60;
+        int newMinutes = timePlus30 % 60;
 
         if (newHour > 23){
             newHour = 0;
         }
 
+        // Print result
         System.out.printf("%d:%02d",newHour,newMinutes);
     }
 }
