7653a6bce9e3af8af9f9edbf2fb3a0a90bb1b96f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-24 23:07:46 +0300

    03

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_03_PassedOrFailed.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_03_PassedOrFailed.java
@@ -6,8 +6,10 @@ public class _03_PassedOrFailed {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         double grade = Double.parseDouble(scan.nextLine());
 
+        // Print result
         if (grade >= 3.00){
             System.out.println("Passed!");
         }else{
