e20e563ec779b8f51f89c6017aee40d0c2efbef2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-24 23:10:19 +0300

    05

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_05_MonthPrinter.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_05_MonthPrinter.java
@@ -6,12 +6,15 @@ public class _05_MonthPrinter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 
+        // Print result
         try{
-            System.out.println(months[num-1]);
+            System.out.println(months[num - 1]);
         }catch (Exception e){
             System.out.println("Error!");
         }
