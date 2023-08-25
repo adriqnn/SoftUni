6864d8523d9e25fc712097960327764da3a0d14d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-26 02:29:35 +0300

    02

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_02_EnglishNameOfTheLastDigit.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_02_EnglishNameOfTheLastDigit.java
@@ -6,11 +6,16 @@ public class _02_EnglishNameOfTheLastDigit {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
-        int n = num%10;
 
-        String[] word ={"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        // Add variables
+        int n = num % 10;
+        String[] word = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
+        // Print result
         System.out.println(word[n]);
+
+        scan.close();
     }
 }
