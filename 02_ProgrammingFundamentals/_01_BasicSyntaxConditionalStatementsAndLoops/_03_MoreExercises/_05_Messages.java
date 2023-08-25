707d380dfda6692ae4d9503b1b5683415b61c160
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-26 02:41:00 +0300

    05

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_05_Messages.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_05_Messages.java
@@ -6,17 +6,24 @@ public class _05_Messages {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         StringBuilder build = new StringBuilder();
+
+        // Decipher the message
         for (int i = 1; i <= n; i++) {
             int console = Integer.parseInt(scan.nextLine());
             int numDigits = String.valueOf(console).length();
-            int mainDigit = console%10;
-            int offset = (mainDigit-2)*3;
+
+            int mainDigit = console % 10;
+            int offset = (mainDigit - 2) * 3;
+
             if (mainDigit == 8 || mainDigit == 9){
                 offset += 1;
             }
+
             int index = offset + numDigits - 1;
             char symbol = (char)(index + 97);
 
@@ -25,6 +32,10 @@ public class _05_Messages {
             }else
                 build.append(symbol);
         }
+
+        // Print result
         System.out.println(build);
+
+        scan.close();
     }
 }
