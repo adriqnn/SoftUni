9c7d776b4244f5556e95e8623b0b5438d1a74d89
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 22:28:21 +0300

    06

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_06_StrongNumber.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_06_StrongNumber.java
@@ -6,24 +6,34 @@ public class _06_StrongNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String console = scan.nextLine();
         int n = Integer.parseInt(console);
+
+        // Add variable
         int sum = 0;
 
+        // Sum values
         for (int i = 0; i < console.length(); i++) {
             char symbol = console.charAt(i);
             int num = Character.getNumericValue(symbol);
+            
             int factorial = 1;
             for (int j = 1; j <= num; j++) {
-                factorial = factorial*j;
+                factorial = factorial * j;
 
             }
+            
             sum += factorial;
         }
+
+        // Print result
         if (sum == n){
             System.out.println("yes");
         }else{
             System.out.println("no");
         }
+
+        scan.close();
     }
 }
