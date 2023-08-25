9eaf616b3b30786024cb68640758adb3aa4c0928
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-26 02:37:30 +0300

    04

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseString.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseString.java
@@ -6,13 +6,22 @@ public class _04_ReverseString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
+
+        // Add variable
         StringBuilder rebuild = new StringBuilder();
 
+        // Reverse string
         for (int i = console.length()-1; i >= 0; i--) {
             char symbol = console.charAt(i);
+
             rebuild.append(symbol);
         }
+
+        // Print result
         System.out.println(rebuild);
+
+        scan.close();
     }
 }
