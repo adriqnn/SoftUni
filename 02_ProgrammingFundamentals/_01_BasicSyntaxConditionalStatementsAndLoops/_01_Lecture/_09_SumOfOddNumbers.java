4f6c2e3606ff3c080c11a076984a97fc0295ac3f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 21:38:59 +0300

    09

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_09_SumOfOddNumbers.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_09_SumOfOddNumbers.java
@@ -6,13 +6,19 @@ public class _09_SumOfOddNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int sum = 0;
+        
+        // Sum odd numbers
         for (int i = 1; i < n*2; i+=2) {
             sum += i;
             System.out.println(i);
         }
+        
+        // Print result
         System.out.println("Sum: " + sum);
     }
 }
