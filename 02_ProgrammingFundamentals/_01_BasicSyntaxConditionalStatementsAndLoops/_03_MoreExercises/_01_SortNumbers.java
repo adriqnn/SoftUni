77426965995665ca2839158195405d42b9435cf4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-26 02:28:01 +0300

    01

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_01_SortNumbers.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_01_SortNumbers.java
@@ -1,25 +1,32 @@
 package _02_ProgrammingFundamentals._01_BasicSyntaxConditionalStatementsAndLoops._03_MoreExercises;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.Collections;
 
 public class _01_SortNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Add variable
         List<Integer> numbers = new ArrayList<>();
 
+        // Read input values
         for (int i = 0; i < 3; i++) {
             int n = Integer.parseInt(scan.nextLine());
             numbers.add(n);
         }
+
+        // Additional methods of solving
         /*numbers.stream().sorted((e1,e2) -> Integer.compare(e2,e1)).collect(Collectors.toList()).forEach(System.out::println);*/
         /*numbers.stream().sorted(Comparator.reverseOrder()).forEach(e -> System.out.println(e));*/
-        Collections.reverse(numbers);
-        for (int e: numbers) {
-            System.out.println(e);
-        }
+        //Collections.reverse(numbers);
+
+        // Print result
+        numbers.stream().sorted((e1,e2) -> Integer.compare(e2,e1)).collect(Collectors.toList()).forEach(System.out::println);
+
+        scan.close();
     }
 }
