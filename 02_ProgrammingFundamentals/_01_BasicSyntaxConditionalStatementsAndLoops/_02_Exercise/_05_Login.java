3ab9e324c703071f956f482d81ec5106d6860efc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 22:25:12 +0300

    05

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_05_Login.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_05_Login.java
@@ -6,24 +6,36 @@ public class _05_Login {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String username = scan.nextLine();
+        String console = scan.nextLine();
 
+        // Add variables
         StringBuilder reverseUsername = new StringBuilder();
+        int count = 0;
+
+        // Reverse the username
         for (int i = username.length()-1; i >= 0; i--) {
             reverseUsername.append(username.charAt(i));
         }
-        String console = scan.nextLine();
 
-        int count = 0;
+        // Check if the user can log in or block after few unsuccessful attempts
         while (!console.equals(reverseUsername.toString())){
             count++;
+            
             if(count == 4){
                 System.out.printf("User %s blocked!%n",username);
                 return;
             }
+            
             System.out.println("Incorrect password. Try again.");
+            
             console = scan.nextLine();
         }
+
+        // Print result
         System.out.printf("User %s logged in.%n",username);
+
+        scan.close();
     }
 }
