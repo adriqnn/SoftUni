e25e6b7edd266f6adac83dc07e5c5f8e7727c369
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 21:44:20 +0300

    12

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_12_EvenNumber.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_12_EvenNumber.java
@@ -6,16 +6,20 @@ public class _12_EvenNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
 
+        // Determine if the input number is even and accept another number
         while (!console.equals("STOP")){
             int n = Math.abs(Integer.parseInt(console));
-            if (n%2 == 0){
-                System.out.printf("The number is: %d%n",Math.abs(n));
+
+            if (n % 2 == 0){
+                System.out.printf("The number is: %d%n", Math.abs(n));
                 break;
             }else{
                 System.out.printf("Please write an even number.%n");
             }
+            
             console = scan.nextLine();
         }
     }
