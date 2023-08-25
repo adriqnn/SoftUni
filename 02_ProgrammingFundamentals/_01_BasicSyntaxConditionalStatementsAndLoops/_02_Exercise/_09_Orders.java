a7f92754c2c65aafdbdf7af68bdb1f94c9c50dd0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-26 00:00:47 +0300

    09

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_09_Orders.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_09_Orders.java
@@ -6,11 +6,16 @@ public class _09_Orders {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int numberOfOrders = Integer.parseInt(scan.nextLine());
+        
+        // Add variable
         double totalPrice = 0;
-
-
+        
+        // Calculate the price for all the orders
         for (int i = 0; i < numberOfOrders; i++) {
+            
+            // Orders
             double pricePerCapsule = Double.parseDouble(scan.nextLine());
             int daysInMonth = Integer.parseInt(scan.nextLine());
             int capsulesCount = Integer.parseInt(scan.nextLine());
@@ -19,6 +24,10 @@ public class _09_Orders {
             System.out.printf("The price for the coffee is: $%.2f%n", priceFormula);
             totalPrice += priceFormula;
         }
+        
+        // Print result
         System.out.printf("Total: $%.2f", totalPrice);
+
+        scan.close();
     }
 }
