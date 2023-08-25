f48af2c16e7e312760589d09e5677a9a9e7d21d2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 22:18:02 +0300

    04

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_04_PrintAndSum.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_04_PrintAndSum.java
@@ -6,15 +6,23 @@ public class _04_PrintAndSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int nStart = Integer.parseInt(scan.nextLine());
         int nEnd = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int sum = 0;
+        
+        // Print the numbers
         for (int i = nStart; i <= nEnd; i++) {
             System.out.print(i + " ");
             sum += i;
         }
+        
+        // Print result
         System.out.println();
         System.out.printf("Sum: %d", sum);
+        
+        scan.close();
     }
 }
