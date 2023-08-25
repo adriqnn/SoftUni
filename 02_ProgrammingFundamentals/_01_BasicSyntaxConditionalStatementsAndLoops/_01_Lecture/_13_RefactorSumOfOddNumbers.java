afe1333a3b459f53321d881076161527168b3d0e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 21:51:50 +0300

    13

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_13_RefactorSumOfOddNumbers.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_13_RefactorSumOfOddNumbers.java
@@ -6,13 +6,22 @@ public class _13_RefactorSumOfOddNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int sum = 0;
-        for (int i = 1; i < n*2; i+=2) {
+
+        // Sum all the odd numbers
+        for (int i = 1; i < n * 2; i += 2) {
             sum += i;
+
             System.out.println(i);
         }
+
+        // Print sum of odd numbers
         System.out.println("Sum: " + sum);
+
+        scan.close();
     }
 }
