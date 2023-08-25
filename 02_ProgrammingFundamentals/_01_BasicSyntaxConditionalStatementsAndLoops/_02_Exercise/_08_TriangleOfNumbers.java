41b088be123991c67ab9870d1097aa2569b26d8b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-25 23:58:30 +0300

    08

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_08_TriangleOfNumbers.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_08_TriangleOfNumbers.java
@@ -6,13 +6,17 @@ public class _08_TriangleOfNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Print triangle of numbers
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
                 System.out.print(i + " ");
             }
             System.out.println();
         }
+
+        scan.close();
     }
 }
