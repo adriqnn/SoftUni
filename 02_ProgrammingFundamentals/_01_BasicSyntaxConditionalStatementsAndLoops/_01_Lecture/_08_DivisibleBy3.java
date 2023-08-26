1a672cf11b8ade1e3fec5855fc7e77cf79982080
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 01:59:48 +0300

    08

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_08_DivisibleBy3.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_08_DivisibleBy3.java
@@ -4,7 +4,7 @@ public class _08_DivisibleBy3 {
     public static void main(String[] args) {
 
         // Print numbers that are divisible by 3
-        for (int i = 3; i <= 99; i+=3) {
+        for (int i = 3; i <= 99; i += 3) {
             System.out.println(i);
         }
     }
