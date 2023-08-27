7b5dce7118744b6cac52fa00cc70a3df09674544
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:27:58 +0300

    05

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_05_PrintPartOfASCIITable.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_05_PrintPartOfASCIITable.java
@@ -6,12 +6,16 @@ public class _05_PrintPartOfASCIITable {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n1 = Integer.parseInt(scan.nextLine());
         int n2 = Integer.parseInt(scan.nextLine());
 
+        // Print ASCII range
         for (int i = n1; i <= n2; i++) {
             char symbol = (char) i;
             System.out.print(symbol + " ");
         }
+
+        scan.close();
     }
 }
