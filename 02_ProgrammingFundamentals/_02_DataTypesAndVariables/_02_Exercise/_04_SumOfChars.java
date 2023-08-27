574caf6c063f01e68b1e8bcfd5eba266648f2ce4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 23:59:22 +0300

    04

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_04_SumOfChars.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_04_SumOfChars.java
@@ -6,13 +6,21 @@ public class _04_SumOfChars {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int sum = 0;
+
+        // Sum the chars
         for (int i = 0; i < n; i++) {
             char symbol = scan.nextLine().charAt(0);
             sum += symbol;
         }
+
+        // Print result
         System.out.println("The sum equals: " + sum);
+
+        scan.close();
     }
 }
