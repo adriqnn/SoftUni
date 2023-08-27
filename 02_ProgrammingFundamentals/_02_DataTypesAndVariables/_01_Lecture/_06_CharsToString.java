6b699f2af30318ff934f6004d430cfca924cf69d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 22:14:18 +0300

    06

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString.java
@@ -6,7 +6,10 @@ public class _06_CharsToString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String one = scan.nextLine(),two = scan.nextLine(),three = scan.nextLine();
-        System.out.println(one+two+three);
+        // Read input values
+        String one = scan.nextLine(), two = scan.nextLine(), three = scan.nextLine();
+
+        // Print result
+        System.out.println(one + two + three);
     }
 }
