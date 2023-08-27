b7b41c9ba2334dd187160bf0ca7648ee0d9460c2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:31:36 +0300

    06

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_TriplesOfLatinLetters.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_TriplesOfLatinLetters.java
@@ -6,17 +6,21 @@ public class _06_TriplesOfLatinLetters {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Print latin letters
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 for (int k = 0; k < n; k++) {
                     char first = (char) ('a' + i);
                     char second = (char) ('a' + j);
                     char third = (char) ('a' + k);
-                    System.out.printf("%c%c%c%n",first,second,third);
+                    System.out.printf("%c%c%c%n", first, second, third);
                 }
             }
         }
+
+        scan.close();
     }
 }
