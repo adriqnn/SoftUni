8c8092e0e21de787791d32a89c7dc561f747ada8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:46:34 +0300

    10

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_PokeMon.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_PokeMon.java
@@ -6,21 +6,28 @@ public class _10_PokeMon {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int pokemonPowerN = Integer.parseInt(scan.nextLine());
         int distanceM = Integer.parseInt(scan.nextLine());
         int exhaustionFactorY = Integer.parseInt(scan.nextLine());
 
-        int originalValue = pokemonPowerN/2;
+        // Add variables
+        int originalValue = pokemonPowerN / 2;
         int successfulPokes = 0;
 
-        while(pokemonPowerN>=distanceM){
+        // Check if pokemon can make the move
+        while(pokemonPowerN >= distanceM){
             successfulPokes++;
             pokemonPowerN -= distanceM;
             if (pokemonPowerN == originalValue && exhaustionFactorY != 0){
                 pokemonPowerN /= exhaustionFactorY;
             }
         }
+        
+        // Print result
         System.out.println(pokemonPowerN);
         System.out.println(successfulPokes);
+        
+        scan.close();
     }
 }
