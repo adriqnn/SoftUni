467c45dac2faf0f973a22532f67c770e46537976
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 22:32:42 +0300

    11

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_11_RefactorVolumeOfPyramid.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_11_RefactorVolumeOfPyramid.java
@@ -6,10 +6,15 @@ public class _11_RefactorVolumeOfPyramid {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         double length = Double.parseDouble(scan.nextLine()), width = Double.parseDouble(scan.nextLine()),height = Double.parseDouble(scan.nextLine());
+        
+        // Print result
         System.out.print("Length: ");
         System.out.print("Width: ");
         System.out.print("Height: ");
-        System.out.printf("Pyramid Volume: %.2f", (length * width * height)/3);
+        System.out.printf("Pyramid Volume: %.2f", (length * width * height) / 3);
+        
+        scan.close();
     }
 }
