cf26a1f1c24c8941cd746e9475cc1af4c4b3d0a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:39:16 +0300

    08

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_BeerKegs.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_BeerKegs.java
@@ -6,20 +6,30 @@ public class _08_BeerKegs {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
-        double min = 0;
-        String big = "";
+        // Add variable
+        double volumeOfTheBiggestKeg = 0;
+        String modelOfTheBiggestKeg = "";
+        
+        // Determine the volume
         for (int i = 0; i < n; i++) {
             String model = scan.nextLine();
             double radius = Double.parseDouble(scan.nextLine());
             int height = Integer.parseInt(scan.nextLine());
-            double volume = Math.PI*radius*radius*height;
-            if(volume > min){
-                min = volume;
-                big = model;
+            
+            double volume = Math.PI * radius * radius * height;
+            
+            if(volume > volumeOfTheBiggestKeg){
+                volumeOfTheBiggestKeg = volume;
+                modelOfTheBiggestKeg = model;
             }
         }
-        System.out.println(big);
+
+        // Print result
+        System.out.println(modelOfTheBiggestKeg);
+        
+        scan.close();
     }
 }
