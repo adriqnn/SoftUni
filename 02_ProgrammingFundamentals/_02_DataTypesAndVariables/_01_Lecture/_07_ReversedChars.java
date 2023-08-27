97d46a66e23ce1b13ad3f7c00bc10bf73df7b87f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 22:17:31 +0300

    07

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_07_ReversedChars.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_07_ReversedChars.java
@@ -6,7 +6,10 @@ public class _07_ReversedChars {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        String one = scan.nextLine(),two = scan.nextLine(),three = scan.nextLine();
-        System.out.println(three+" "+two+" "+one);
+        // Read input values
+        String one = scan.nextLine(), two = scan.nextLine(), three = scan.nextLine();
+
+        // Print result
+        System.out.println(three + " " +  two + " " + one);
     }
 }
