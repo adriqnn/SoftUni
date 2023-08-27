d2d123155dc6d1b768bed0bd917dd7e937cf995e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 22:31:33 +0300

    10

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_10_SpecialNumbers.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_10_SpecialNumbers.java
@@ -6,20 +6,27 @@ public class _10_SpecialNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Find special number
         for (int i = 1; i <= n; i++) {
             int num = i;
             int sum = 0;
+
             while(num != 0){
-                sum += num%10;
+                sum += num % 10;
                 num /= 10;
             }
+
+            // Print result
             if(sum  == 5 || sum  == 7 || sum == 11){
                 System.out.printf("%d -> True%n",i);
             }else{
                 System.out.printf("%d -> False%n",i);
             }
         }
+
+        scan.close();
     }
 }
