caad5d6ff6154016a3faf9ad9ace331969a168fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:34:09 +0300

    07

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_07_WaterOverflow.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_07_WaterOverflow.java
@@ -6,17 +6,27 @@ public class _07_WaterOverflow {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int capacity = 0;
+
+        // Determine the capacity
         for (int i = 0; i < n; i++) {
             int liters = Integer.parseInt(scan.nextLine());
+            
             if(capacity + liters > 255){
                 System.out.println("Insufficient capacity!");
                 continue;
             }
+            
             capacity += liters;
         }
+
+        // Print result
         System.out.println(capacity);
+
+        scan.close();
     }
 }
