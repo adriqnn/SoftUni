0b2657235da4261bd12806746c239fe0b36b4abd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 00:48:44 +0300

    11

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_11_Snowballs.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_11_Snowballs.java
@@ -6,20 +6,25 @@ public class _11_Snowballs {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int numberOfSnowballs = Integer.parseInt(scan.nextLine());
 
+        // Add variables
         double max = Double.NEGATIVE_INFINITY;
         int maxSnowballSnow = 0;
         int maxSnowballTime = 0;
         int maxSnowballQuality = 0;
         double maxSnowballValue = 0;
 
+        // Determine which one is the best snowball
         for (int i = 0; i < numberOfSnowballs; i++) {
             int snowballSnow = Integer.parseInt(scan.nextLine());
             int snowballTime = Integer.parseInt(scan.nextLine());
             int snowballQuality = Integer.parseInt(scan.nextLine());
+            
             double snowballValue = Math.pow((1.00*snowballSnow/snowballTime),1.00*snowballQuality);
-            if (snowballValue>max){
+            
+            if (snowballValue > max){
                 maxSnowballSnow = snowballSnow;
                 maxSnowballTime = snowballTime;
                 maxSnowballQuality = snowballQuality;
@@ -27,6 +32,10 @@ public class _11_Snowballs {
                 max = snowballValue;
             }
         }
-        System.out.printf("%d : %d = %.0f (%d)",maxSnowballSnow,maxSnowballTime,maxSnowballValue,maxSnowballQuality);
+        
+        // Print result
+        System.out.printf("%d : %d = %.0f (%d)", maxSnowballSnow, maxSnowballTime, maxSnowballValue, maxSnowballQuality);
+        
+        scan.close();
     }
 }
