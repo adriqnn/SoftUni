c2996d9a8a32b5e12140a3390c5fc242f622cf23
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-27 23:16:48 +0300

    02

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_02_SumDigits.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_02_SumDigits.java
@@ -6,14 +6,21 @@ public class _02_SumDigits {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
 
+        // Add variable
         int sum = 0;
+        
+        // Sum the numbers
         for (int i = 0; i < console.length(); i++) {
             char symbol = console.charAt(i);
+            
             int n = Character.getNumericValue(symbol);
             sum += n;
         }
+        
+        // Print result
         System.out.println(sum);
     }
 }
