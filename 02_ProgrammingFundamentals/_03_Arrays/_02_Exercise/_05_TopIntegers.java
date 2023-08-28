9f772f0a4a4ac9b2d113dd08a1d17e5b362c586a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:54:23 +0300

    05

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_05_TopIntegers.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_05_TopIntegers.java
@@ -9,22 +9,33 @@ public class _05_TopIntegers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int[] numbersArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        // Add variable
         List<Integer> topIntegers = new ArrayList<>();
 
+
+        // Determine the top integers
         for (int i = 0; i < numbersArray.length; i++) {
 
             boolean isBigger = true;
+
             for (int j = i+1; j < numbersArray.length; j++) {
                 if(numbersArray[i] <= numbersArray[j]){
                     isBigger = false;
                     break;
                 }
             }
+
             if (isBigger) {
                 topIntegers.add(numbersArray[i]);
             }
         }
+
+        // Print result
         topIntegers.forEach(e -> System.out.print(e + " "));
+
+        scan.close();
     }
 }
