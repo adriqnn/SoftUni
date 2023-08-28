7c095f529c9b2663185a2d319a964baba2658086
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:44:59 +0300

    02

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_02_CommonElements.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_02_CommonElements.java
@@ -6,15 +6,21 @@ public class _02_CommonElements {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] arrOne = scan.nextLine().split("\\s+");
         String[] arrTwo = scan.nextLine().split("\\s+");
 
+        // Print common elements in two arrays
         for (int i = 0; i < arrOne.length; i++) {
+
             for (int j = 0; j < arrTwo.length; j++) {
+
                 if(arrOne[i].equals(arrTwo[j])){
                     System.out.print(arrTwo[j] + " ");
                 }
             }
         }
+
+        scan.close();
     }
 }
