9e47fb4e8ac9870c27b2b8f9621dbf86519f4ac6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 03:52:08 +0300

    02

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_02_FromLeftToTheRight.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_02_FromLeftToTheRight.java
@@ -7,25 +7,37 @@ public class _02_FromLeftToTheRight {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Examine left and right numbers
         for (int i = 0; i < n; i++) {
             long[] numbersArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToLong(Long::parseLong).toArray();
-            int sum = 0;
-            if (numbersArray[0] >= numbersArray[1]){
-                numbersArray[0] = Math.abs(numbersArray[0]);
-                while (numbersArray[0] > 0){
-                    sum += numbersArray[0]%10;
-                    numbersArray[0] = numbersArray[0]/10;
+
+            // Add variables
+            long firstNumber = numbersArray[0];
+            long secondNumber = numbersArray[1];
+            long sum = 0;
+
+            // Compare numbers
+            if (firstNumber >= secondNumber){
+                firstNumber = Math.abs(firstNumber);
+                while (firstNumber > 0){
+                    sum += firstNumber % 10;
+                    firstNumber = firstNumber / 10;
                 }
             }else {
-                numbersArray[1] = Math.abs(numbersArray[1]);
-                while (numbersArray[1] > 0){
-                    sum += numbersArray[1]%10;
-                    numbersArray[1] = numbersArray[1]/10;
+                secondNumber = Math.abs(secondNumber);
+                while (secondNumber > 0){
+                    sum += secondNumber % 10;
+                    secondNumber = secondNumber / 10;
                 }
             }
+
+            // Print result
             System.out.println(sum);
         }
+
+        scan.close();
     }
 }
