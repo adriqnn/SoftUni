23da63ed2df7db2a12e61b7707bee9b08664f9c0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:43:13 +0300

    01

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_01_Train.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_01_Train.java
@@ -7,16 +7,25 @@ public class _01_Train {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int n = Integer.parseInt(scan.nextLine());
+        // Read input value
+        int countOfTrainWagons = Integer.parseInt(scan.nextLine());
 
-        int[] train = new int[n];
+        // Add variables
+        int[] train = new int[countOfTrainWagons];
         int sum = 0;
-        for (int i = 0; i < n; i++) {
+
+        // Add passengers to the train and sum all of them
+        for (int i = 0; i < countOfTrainWagons; i++) {
             train[i] = (scan.nextInt());
+
             sum += train[i];
         }
+
+        // Print result
         Arrays.stream(train).forEach(e -> System.out.print(e + " "));
         System.out.println();
         System.out.println(sum);
+
+        scan.close();
     }
 }
