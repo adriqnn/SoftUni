efe6f21f66a704c27e9ad677e7992bc4db8c014c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 04:04:45 +0300

    05

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_05_DecryptingMessage.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_05_DecryptingMessage.java
@@ -6,15 +6,24 @@ public class _05_DecryptingMessage {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int keyToDecrypt = Integer.parseInt(scan.nextLine());
         int numberOfLines = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         StringBuilder message = new StringBuilder();
+
+        // Decrypt the message
         for (int i = 0; i < numberOfLines; i++) {
             char symbol = scan.nextLine().charAt(0);
             symbol = (char) (symbol+keyToDecrypt);
+
             message.append(symbol);
         }
+
+        // Print result
         System.out.println(message);
+
+        scan.close();
     }
 }
