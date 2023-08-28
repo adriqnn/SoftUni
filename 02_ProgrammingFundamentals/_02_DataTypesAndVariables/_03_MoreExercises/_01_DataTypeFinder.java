31b61ace22e230d356175e44ad4aeeb857533eea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 03:44:06 +0300

    01

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_01_DataTypeFinder.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_01_DataTypeFinder.java
@@ -8,16 +8,20 @@ public class _01_DataTypeFinder {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
 
+        // Add variables for regex
         String regexInteger = "^-?[0-9]+$";
         Pattern patternInteger = Pattern.compile(regexInteger);
         String regexFloat = "^-?[0-9]+\\.[0-9]+$";
         Pattern patternFloat = Pattern.compile(regexFloat);
 
+        // Determine what is the input
         while(!console.equals("END")){
             Matcher matcherInteger = patternInteger.matcher(console);
             Matcher matcherFloat = patternFloat.matcher(console);
+            
             if(console.equalsIgnoreCase("true") || console.equalsIgnoreCase("false")){
                 System.out.printf("%s is boolean type%n", console);
             }else if (console.length() == 1 && (console.charAt(0) <= 47 || console.charAt(0) >= 58)){
@@ -29,7 +33,10 @@ public class _01_DataTypeFinder {
             }else{
                 System.out.printf("%s is string type%n",console);
             }
+            
             console = scan.nextLine();
         }
+        
+        scan.nextLine();
     }
 }
