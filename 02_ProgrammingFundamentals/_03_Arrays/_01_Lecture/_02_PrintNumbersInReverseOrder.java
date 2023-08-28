c4c0c8fe64794f2c0ac907534a392615300cd8ce
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:20:02 +0300

    02

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_02_PrintNumbersInReverseOrder.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_02_PrintNumbersInReverseOrder.java
@@ -6,16 +6,23 @@ public class _02_PrintNumbersInReverseOrder {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         int[] numbers = new int[n];
 
+        // Reverse the array
         for (int i = n-1; i >= 0; i--) {
             int number = Integer.parseInt(scan.nextLine());
             numbers[i] = number;
         }
+        
+        // Print result
         for (int num: numbers) {
             System.out.print(num + " ");
         }
+        
+        scan.close();
     }
 }
