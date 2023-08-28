daf87495c5dd31c226aad943d7129cadbc4d8a10
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:21:24 +0300

    03

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_03_SumEvenNumbers.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_03_SumEvenNumbers.java
@@ -7,14 +7,22 @@ public class _03_SumEvenNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
+        // Add variable
         int sum = 0;
+        
+        // Calculate sum
         for (int i = 0; i < numbers.length; i++) {
             if(numbers[i] % 2 == 0){
                 sum += numbers[i];
             }
         }
+        
+        // Print result
         System.out.println(sum);
+        
+        scan.close();
     }
 }
