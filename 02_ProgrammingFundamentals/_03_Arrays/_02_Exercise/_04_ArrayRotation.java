0b19baf36f364b455a919fd20bf4e1ea54730e13
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:49:38 +0300

    04

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_04_ArrayRotation.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_04_ArrayRotation.java
@@ -7,18 +7,26 @@ public class _04_ArrayRotation {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         int n = Integer.parseInt(scan.nextLine());
 
         for (int i = 0; i < n; i++) {
             int numberInTheFront = numbers[0];
+            
             int[] changingArray = new int[numbers.length];
+            
             for (int j = 0; j < numbers.length-1; j++) {
                 changingArray[j] = numbers[j+1];
             }
+            
             changingArray[changingArray.length-1] = numberInTheFront;
             numbers = changingArray;
         }
+
+        // Print result
         Arrays.stream(numbers).forEach(e -> System.out.print(e + " "));
+
+        scan.close();
     }
 }
