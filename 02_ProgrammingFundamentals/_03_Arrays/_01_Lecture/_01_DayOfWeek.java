2684594c3d031f772dd2e2b0c0110a44a15546b3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:18:13 +0300

    01

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_01_DayOfWeek.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_01_DayOfWeek.java
@@ -6,14 +6,19 @@ public class _01_DayOfWeek {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
-        String[] days = {"Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"};
+        // Add variable
+        String[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" ,"Saturday", "Sunday"};
 
+        // Print result
         try{
             System.out.println(days[n-1]);
         }catch (Exception e){
             System.out.println("Invalid day!");
         }
+
+        scan.close();
     }
 }
