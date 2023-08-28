2a3b307dbf3895d9a2f4fcdde3612e8a062eb7af
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 00:11:29 +0300

    08

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_08_MagicSum.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_08_MagicSum.java
@@ -7,15 +7,20 @@ public class _08_MagicSum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int[] numbersArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         int n = Integer.parseInt(scan.nextLine());
 
+        // Print each pair that the sum of equals the number n
         for (int i = 0; i < numbersArray.length; i++) {
+
             for (int j = i+1; j < numbersArray.length; j++) {
                 if(numbersArray[i] + numbersArray[j] == n){
                     System.out.println(numbersArray[i] + " " + numbersArray[j]);
                 }
             }
         }
+
+        scan.close();
     }
 }
