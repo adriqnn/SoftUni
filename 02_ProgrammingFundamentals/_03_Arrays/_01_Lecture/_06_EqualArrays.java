ac03a4509cd31464326209302ab84d3df95ff2d7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:33:12 +0300

    06

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_06_EqualArrays.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_06_EqualArrays.java
@@ -7,24 +7,31 @@ public class _06_EqualArrays {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int[] number1 = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-        int[] number2 = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        // Read input values
+        int[] numberOneArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int[] numberTwoArray = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
+        // Add variables
         int sum = 0;
         boolean isEqual = true;
-        for (int i = 0; i < number1.length; i++) {
-            if(number1[i] == number2[i]){
-                sum += number1[i];
+        
+        for (int i = 0; i < numberOneArray.length; i++) {
+            if(numberOneArray[i] == numberTwoArray[i]){
+                sum += numberOneArray[i];
             }else{
                 isEqual = false;
                 sum = i;
                 break;
             }
         }
+        
+        // Print result
         if(isEqual){
             System.out.println("Arrays are identical. Sum: " + sum);
         }else{
             System.out.println("Arrays are not identical. Found difference at "+sum+" index.");
         }
+        
+        scan.close();
     }
 }
