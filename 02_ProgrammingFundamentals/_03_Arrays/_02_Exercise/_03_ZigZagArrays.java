bf5f30a86a1e9ce70b18f3772825f9f932b82091
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:46:51 +0300

    03

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_03_ZigZagArrays.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_03_ZigZagArrays.java
@@ -7,10 +7,12 @@ public class _03_ZigZagArrays {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int n = Integer.parseInt(scan.nextLine());
         int[] numbersArrayOne = new int[n];
         int[] numbersArrayTwo = new int[n];
 
+        // Gather arrays in the selected pattern from the input value
         for (int i = 0; i < n; i++) {
             int[] line = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
@@ -22,8 +24,12 @@ public class _03_ZigZagArrays {
                 numbersArrayTwo[i] = line[0];
             }
         }
+        
+        // Print result
         Arrays.stream(numbersArrayOne).forEach(e -> System.out.print(e + " "));
         System.out.println();
         Arrays.stream(numbersArrayTwo).forEach(e -> System.out.print(e + " "));
+        
+        scan.close();
     }
 }
