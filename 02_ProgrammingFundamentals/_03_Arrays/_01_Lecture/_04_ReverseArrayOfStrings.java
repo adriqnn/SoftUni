2e23033322bf4202797fdb5eb226cf5ffd01bfab
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:25:02 +0300

    04

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_04_ReverseArrayOfStrings.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_04_ReverseArrayOfStrings.java
@@ -6,16 +6,22 @@ public class _04_ReverseArrayOfStrings {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] arr = scan.nextLine().split("\\s+");
 
+        // Reverse the array
         for (int i = 0; i < arr.length/2; i++) {
             String first = arr[i];
             String last = arr[arr.length-i-1];
             arr[i] = last;
             arr[arr.length-1-i] = first;
         }
+        
+        // Print result
         for (String e: arr) {
             System.out.print(e + " ");
         }
+        
+        scan.close();
     }
 }
