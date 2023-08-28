991508aba0ff6e7ef0264ca0a9be7aec0eea3409
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-28 23:35:13 +0300

    07

--- 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_07_CondenseArrayToNumber.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_01_Lecture/_07_CondenseArrayToNumber.java
@@ -7,16 +7,24 @@ public class _07_CondenseArrayToNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Raed input values
         int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
+        // Condense the array
         while (numbers.length > 1){
             int[] condensed = new int[numbers.length-1];
+            
             for (int i = 0; i < numbers.length-1; i++) {
                 condensed[i] = numbers[i] + numbers[i+1];
 
             }
+            
             numbers = condensed;
         }
+        
+        // Print result
         System.out.println(numbers[0]);
+        
+        scan.close();
     }
 }
