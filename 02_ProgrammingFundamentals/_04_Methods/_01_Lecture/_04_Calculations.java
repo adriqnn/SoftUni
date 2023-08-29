7c323f7675546a6549bd32735bd640fdb7aa2dbc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 22:16:07 +0300

    04

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_04_Calculations.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_04_Calculations.java
@@ -6,22 +6,26 @@ public class _04_Calculations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String operation = scan.nextLine();
         int num1 = Integer.parseInt(scan.nextLine());
         int num2 = Integer.parseInt(scan.nextLine());
 
-        whatIsTheResult(operation,num1,num2);
+        // Create method to do the calculations
+        whatIsTheResult(operation, num1, num2);
+
+        scan.close();
     }
 
     private static void whatIsTheResult(String operation, int num1, int num2) {
         if (operation.equals("add")){
-            System.out.println(num1+num2);
+            System.out.println(num1 + num2);
         }else if (operation.equals("multiply")){
-            System.out.println(num1*num2);
+            System.out.println(num1 * num2);
         }else if (operation.equals("subtract")){
-            System.out.println(num1-num2);
+            System.out.println(num1 - num2);
         }else if (operation.equals("divide")){
-            System.out.println(num1/num2);
+            System.out.println(num1 / num2);
         }
     }
 }
