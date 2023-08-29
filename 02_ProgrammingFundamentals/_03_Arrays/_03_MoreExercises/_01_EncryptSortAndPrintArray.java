87898547eb3f84a4088f17534f644ec01bb21191
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 04:50:22 +0300

    01

--- 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_01_EncryptSortAndPrintArray.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_01_EncryptSortAndPrintArray.java
@@ -7,25 +7,37 @@ public class _01_EncryptSortAndPrintArray {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
+
+        // Add variable
         int[] listOfNamesValues = new int[num];
 
+        // Transform the strings into numeric values
         for (int i = 0; i < num; i++) {
             String console = scan.nextLine();
+
             int sum = 0;
+
             for (int j = 0; j < console.length(); j++) {
                 char symbol = console.charAt(j);
+
                 if (symbol == 'a' || symbol == 'e' || symbol == 'u' || symbol == 'i' || symbol == 'o') {
                     sum += symbol * console.length();
                 } else if (symbol == 'A' || symbol == 'E' || symbol == 'U' || symbol == 'I' || symbol == 'O') {
                     sum += symbol * console.length();
                 }else{
-                    sum +=  symbol/console.length();
+                    sum +=  symbol / console.length();
                 }
             }
+
             listOfNamesValues[i] = sum;
         }
+
+        // Print result
         Arrays.sort(listOfNamesValues);
         Arrays.stream(listOfNamesValues).forEach(System.out::println);
+
+        scan.close();
     }
 }
