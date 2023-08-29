1548ebf957fb8690a2b4ebafe17f5a098391e42a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 22:35:10 +0300

    07

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_07_RepeatString.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_07_RepeatString.java
@@ -6,16 +6,23 @@ public class _07_RepeatString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String console = scan.nextLine();
         int num = Integer.parseInt(scan.nextLine());
-        repeatString(console,num);
+        
+        // Create method that repeats a string
+        repeatString(console, num);
+        
+        scan.close();
     }
 
     private static void repeatString(String console, int num) {
         StringBuilder newString = new StringBuilder();
+        
         for (int i = 0; i < num; i++) {
             newString.append(console);
         }
+        
         System.out.println(newString);
     }
 }
