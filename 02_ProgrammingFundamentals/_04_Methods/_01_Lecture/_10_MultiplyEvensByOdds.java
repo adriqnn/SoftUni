94dea7cf39866c3bf81dff812caf83aed26ab15a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 22:49:44 +0300

    10

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_10_MultiplyEvensByOdds.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_10_MultiplyEvensByOdds.java
@@ -6,26 +6,33 @@ public class _10_MultiplyEvensByOdds {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String num = scan.nextLine();
 
+        // Create method for multiplication
         whatIf(num);
+
+        scan.close();
     }
 
     private static void whatIf(String number) {
-
         int thisNumber = Math.abs(Integer.parseInt(number));
         String num = String.valueOf(thisNumber);
+
         int sumEven = 0;
         int sumOdd = 0;
+
         for (int i = 0; i < num.length(); i++) {
             char symbol = num.charAt(i);
             int n = Character.getNumericValue(symbol);
-            if ( n%2 == 0){
+
+            if ( n % 2 == 0){
                 sumEven += n;
             }else {
                 sumOdd += n;
             }
         }
-        System.out.println(sumEven*sumOdd);
+
+        System.out.println(sumEven * sumOdd);
     }
 }
