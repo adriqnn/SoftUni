66d09f04f1908be58341901e926a3dda8098cd4f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 04:54:57 +0300

    02

--- 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle.java
@@ -6,16 +6,20 @@ public class _02_PascalTriangle {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
-
+        // Print pascal triangle
         for (int i = 0; i < n; i++) {
             int number = 1;
             for (int j = 0; j <= i; j++) {
                 System.out.print(number + " ");
-                number = number*(i-j)/(j+1);
+                number = number * (i - j) / (j + 1);
             }
+
             System.out.println();
         }
+
+        scan.close();
     }
 }
