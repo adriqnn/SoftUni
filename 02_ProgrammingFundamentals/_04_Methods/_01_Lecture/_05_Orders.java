255d2baf5a26a341644c4afa5e673f4a71eb33c1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 22:27:18 +0300

    05

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_05_Orders.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_05_Orders.java
@@ -6,20 +6,25 @@ public class _05_Orders {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String item = scan.nextLine();
         double quantity = Double.parseDouble(scan.nextLine());
+
+        // Create method for incoming orders
         whatIsThePrice(item,quantity);
+
+        scan.close();
     }
 
     private static void whatIsThePrice(String item, double quantity) {
         if (item.equals("coffee")){
-            System.out.printf("%.2f",1.50*quantity);
+            System.out.printf("%.2f", 1.50 * quantity);
         }else if (item.equals("water")){
-            System.out.printf("%.2f",1.00*quantity);
+            System.out.printf("%.2f", 1.00 * quantity);
         }else if (item.equals("coke")){
-            System.out.printf("%.2f",1.40*quantity);
+            System.out.printf("%.2f", 1.40 * quantity);
         }else if (item.equals("snacks")){
-            System.out.printf("%.2f",2.00*quantity);
+            System.out.printf("%.2f", 2.00 * quantity);
         }
     }
 }
