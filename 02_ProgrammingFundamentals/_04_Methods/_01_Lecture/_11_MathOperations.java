e13b2981a259080d2847b99a6bf14feeeffc2ced
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-29 22:52:08 +0300

    11

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_11_MathOperations.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_11_MathOperations.java
@@ -7,23 +7,30 @@ public class _11_MathOperations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int n1 = Integer.parseInt(scan.nextLine());
         String operation = scan.nextLine();
         int n2 = Integer.parseInt(scan.nextLine());
-        mathOperations(n1,operation,n2);
+
+        // Create method for math operations
+        mathOperations(n1, operation, n2);
+        
+        scan.close();
     }
 
     private static void mathOperations(int n1, String operation, int n2) {
         double result = 0;
+        
         if (operation.equals("/")){
-            result = n1/n2;
+            result = n1 / n2;
         }else if (operation.equals("*")){
-            result = n1*n2;
+            result = n1 * n2;
         }else if (operation.equals("+")){
-            result = n1+n2;
+            result = n1 + n2;
         }else if (operation.equals("-")){
-            result = n1-n2;
+            result = n1 - n2;
         }
+        
         System.out.println(new DecimalFormat("0.##").format(result));
     }
 }
