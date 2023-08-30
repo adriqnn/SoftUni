60c7ab5b44bf9e9c83530bf944b17cd42845b746
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 04:45:43 +0300

    07

--- 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_07_NxNMatrix.java
+++ 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_07_NxNMatrix.java
@@ -6,15 +6,22 @@ public class _07_NxNMatrix {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
+
+        // Create method that prints a number matrix
         printNxN(n);
+
+        scan.close();
     }
 
     private static void printNxN(int n) {
         for (int i = 0; i < n; i++) {
+            
             for (int j = 0; j < n; j++) {
                 System.out.print(n + " ");
             }
+            
             System.out.println();
         }
     }
