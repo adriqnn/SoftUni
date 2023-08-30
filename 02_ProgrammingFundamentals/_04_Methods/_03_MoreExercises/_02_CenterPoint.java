a12aff09086feae4e9881d61fe8c9cce16262d92
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 05:01:10 +0300

    02

--- 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_02_CenterPoint.java
+++ 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_02_CenterPoint.java
@@ -6,22 +6,24 @@ public class _02_CenterPoint {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         double x1 = Integer.parseInt(scan.nextLine());
         double y1 = Integer.parseInt(scan.nextLine());
         double x2 = Integer.parseInt(scan.nextLine());
         double y2 = Integer.parseInt(scan.nextLine());
 
-        rectangleDistance(x1,y1,x2,y2);
+        // Create method that prints the closest point
+        rectangleDistance(x1, y1, x2, y2);
     }
 
     private static void rectangleDistance(double x1, double y1, double x2, double y2) {
-        double first = Math.sqrt((x1*x1)+(y1*y1));
-        double second = Math.sqrt((x2*x2)+(y2*y2));
+        double first = Math.sqrt((x1 * x1) + (y1 * y1));
+        double second = Math.sqrt((x2 * x2) + (y2 * y2));
 
         if (first <= second){
-            System.out.printf("(%.0f, %.0f)",x1,y1);
+            System.out.printf("(%.0f, %.0f)", x1, y1);
         }else{
-            System.out.printf("(%.0f, %.0f)",x2,y2);
+            System.out.printf("(%.0f, %.0f)", x2, y2);
         }
     }
 }
