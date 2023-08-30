3adae7f31f7251ed71478c80203829e16ac3d352
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 04:44:00 +0300

    06

--- 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_06_MiddleCharacters.java
+++ 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_06_MiddleCharacters.java
@@ -6,16 +6,26 @@ public class _06_MiddleCharacters {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
 
+        // Create method that prints the middle of a string
         middleString(console);
+
+        scan.close();
     }
 
     private static void  middleString(String console) {
+        
         if (console.length() % 2 == 0){
-            System.out.printf("%c%c",console.charAt(console.length()/2-1),console.charAt(console.length()/2));
+            char middleEvenOne = console.charAt((console.length() / 2) - 1);
+            char middleEvenTwo = console.charAt(console.length() / 2);
+            
+            System.out.printf("%c%c", middleEvenOne, middleEvenTwo);
         }else{
-            System.out.println(console.charAt(console.length()/2));
+            char middleOdd = console.charAt(console.length() / 2);
+            
+            System.out.println(middleOdd);
         }
     }
 }
