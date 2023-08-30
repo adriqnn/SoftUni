1757883624eeead875fc70a94230f9fa491e62a4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 04:17:16 +0300

    01

--- 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_01_SmallestOfThreeNumbers.java
+++ 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_01_SmallestOfThreeNumbers.java
@@ -6,15 +6,16 @@ public class _01_SmallestOfThreeNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         int n1 = Integer.parseInt(scan.nextLine());
         int n2 = Integer.parseInt(scan.nextLine());
         int n3 = Integer.parseInt(scan.nextLine());
 
-        printBiggestNumber(n1,n2,n3);
+        // Create method to determine the smallest number out of three numbers
+        printBiggestNumber(n1, n2, n3);
     }
 
     private static void printBiggestNumber(int n1, int n2, int n3) {
-
-        System.out.println(Math.min(n1,(Math.min(n2,n3))));
+        System.out.println(Math.min(n1, (Math.min(n2, n3))));
     }
 }
