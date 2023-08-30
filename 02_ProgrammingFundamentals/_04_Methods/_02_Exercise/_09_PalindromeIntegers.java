9f91fdc694faf28459b2463bb304bc0a1072c928
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 04:50:43 +0300

    09

--- 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_09_PalindromeIntegers.java
+++ 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_09_PalindromeIntegers.java
@@ -6,24 +6,30 @@ public class _09_PalindromeIntegers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String  number = scan.nextLine();
+
         while(!number.equals("END")){
+
+            // Create method that determines if the input is palindrome
             System.out.println(isPalindromeInteger(number));
+
             number = scan.nextLine();
         }
+        
+        scan.close();
     }
+
     private static boolean isPalindromeInteger(String number) {
         StringBuilder second = new StringBuilder();
+
         for (int i = number.length()-1; i >= 0; i--) {
             second.append(number.charAt(i));
         }
+        
         int n1 = Integer.parseInt(number);
         int n2 = Integer.parseInt(second.toString());
 
-        if (n1 == n2 ){
-            return true;
-        }else{
-            return false;
-        }
+        return n1 == n2;
     }
 }
