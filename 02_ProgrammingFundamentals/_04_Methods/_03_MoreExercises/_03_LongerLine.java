4b3a9f5dff3055ff1b2c9789b24bf7553d6212ea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 05:31:17 +0300

    03

--- 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_03_LongerLine.java
+++ 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_03_LongerLine.java
@@ -6,6 +6,7 @@ public class _03_LongerLine {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         double x1 = Integer.parseInt(scan.nextLine());
         double y1 = Integer.parseInt(scan.nextLine());
         double x2 = Integer.parseInt(scan.nextLine());
@@ -16,29 +17,32 @@ public class _03_LongerLine {
         double a2 = Integer.parseInt(scan.nextLine());
         double b2 = Integer.parseInt(scan.nextLine());
 
-        printLongerLine(x1,y1,x2,y2,a1,b1,a2,b2);
+        // Create method that determines the longer line
+        printLongerLine(x1, y1, x2, y2, a1, b1, a2, b2);
+
+        scan.close();
     }
     private static void printLongerLine(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2) {
-
-        double distance1 = Math.sqrt(((x2-x1)*(x2-x1))+((y2-y1)*(y2-y1)));
-        double distance2 = Math.sqrt(((a2-a1)*(a2-a1))+((y2-y1)*(y2-y1)));
+        double distance1 = Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+        double distance2 = Math.sqrt(((a2 - a1) * (a2 - a1)) + ((b2 - b1) * (b2 - b1)));
 
         if (distance1 >= distance2){
-            rectangleDistance(x1,y1,x2,y2);
+            rectangleDistance(x1, y1, x2, y2);
         }else{
-            rectangleDistance(a1,b1,a2,b2);
+            rectangleDistance(a1, b1, a2, b2);
         }
     }
+
     private static void rectangleDistance(double x1, double y1, double x2, double y2) {
-        double first = Math.sqrt((x1*x1)+(y1*y1));
-        double second = Math.sqrt((x2*x2)+(y2*y2));
+        double first = Math.sqrt((x1 * x1) + (y1 * y1));
+        double second = Math.sqrt((x2 * x2) + (y2 * y2));
 
         if (first <= second){
-            System.out.printf("(%.0f, %.0f)",x1,y1);
-            System.out.printf("(%.0f, %.0f)",x2,y2);
+            System.out.printf("(%.0f, %.0f)", x1, y1);
+            System.out.printf("(%.0f, %.0f)", x2, y2);
         }else{
-            System.out.printf("(%.0f, %.0f)",x2,y2);
-            System.out.printf("(%.0f, %.0f)",x1,y1);
+            System.out.printf("(%.0f, %.0f)", x2, y2);
+            System.out.printf("(%.0f, %.0f)", x1, y1);
         }
     }
 }
