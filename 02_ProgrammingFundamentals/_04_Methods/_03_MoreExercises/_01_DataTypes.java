339ae7dccacfba995e6a5ed128d3aa7553ca8af6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-30 04:58:32 +0300

    01

--- 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_01_DataTypes.java
+++ 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_01_DataTypes.java
@@ -6,27 +6,38 @@ public class _01_DataTypes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String type = scan.nextLine();
+        
+        // Determine the data type of the input
         if(type.equals("int")){
-            int num1 = Integer.parseInt(scan.nextLine());
-            printDataTypes(num1);
+            int num = Integer.parseInt(scan.nextLine());
+            
+            printDataTypes(num);
         }else if(type.equals("real")){
-            double num2 = Double.parseDouble(scan.nextLine());
-            printDataTypes(num2);
+            double num = Double.parseDouble(scan.nextLine());
+            
+            printDataTypes(num);
         }else if(type.equals("string")){
             String line = scan.nextLine();
+            
             printDataTypes(line);
         }
+        
+        scan.close();
     }
 
-    private static void printDataTypes(int num1) {
-        System.out.println(num1*2);
+    private static void printDataTypes(int num) {
+        System.out.println(num * 2);
     }
-    private static void printDataTypes(double num2) {
-        System.out.printf("%.2f",num2*1.5);
+    
+    private static void printDataTypes(double num) {
+        System.out.printf("%.2f", num * 1.5);
     }
+    
     private static void printDataTypes(String line) {
         String newString = "$" + line + "$";
+        
         System.out.println(newString);
     }
 }
