f021f49ee96ca05cdbd2bc0c5c8fa10635025409
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-01 02:11:49 +0300

    03

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_01_Lecture/_03_BigFactorial.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_01_Lecture/_03_BigFactorial.java
@@ -7,13 +7,21 @@ public class _03_BigFactorial {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int factorial = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         BigDecimal result = new BigDecimal(1);
+
+        // Determine the factorial of the input
         for (int i = 1; i <= factorial; i++) {
 
             result = result.multiply(new BigDecimal(String.valueOf(i)));
         }
+
+        // Print result
         System.out.println(result);
+
+        scan.close();
     }
 }
