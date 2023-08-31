bb2c20d3ff8cbd54e62f89667ce4d3d16219cf79
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-01 02:09:20 +0300

    02

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_01_Lecture/_02_SumBigNumbers.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_01_Lecture/_02_SumBigNumbers.java
@@ -7,10 +7,14 @@ public class _02_SumBigNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         BigInteger one = new BigInteger(scan.nextLine());
         BigInteger two = new BigInteger(scan.nextLine());
 
+        // Sum the input values
         BigInteger sum = one.add(two);
+
+        // Print result
         System.out.println(sum);
     }
 }
