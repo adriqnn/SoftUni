8a811865d1b59c243ecdcaa7b0c2ae6dabcdd594
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 05:36:44 +0300

    05

--- 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_05_DrumSet.java
+++ 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_05_DrumSet.java
@@ -10,16 +10,23 @@ public class _05_DrumSet {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input vlaues
         double moneyToSpend = Double.parseDouble(scan.nextLine());
         List<Integer> drumsBaseQuality = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        // Add variable
         List<Integer> drumsActualQualityNow = new ArrayList<>(drumsBaseQuality);
+
         String commands = scan.nextLine();
 
+        // Calculate the quality of drums based on the hit power
         while(!commands.equals("Hit it again, Gabsy!")){
             int hitPower = Integer.parseInt(commands);
+            
             for (int i = 0; i < drumsActualQualityNow.size(); i++) {
                 drumsActualQualityNow.set(i,(drumsActualQualityNow.get(i) - hitPower));
             }
+            
             for (int i = 0; i < drumsActualQualityNow.size(); i++) {
                 if(drumsActualQualityNow.get(i) <= 0){
                     if(moneyToSpend - (drumsBaseQuality.get(i)*3) >= 0){
@@ -31,10 +38,15 @@ public class _05_DrumSet {
                     }
                 }
             }
+            
             commands = scan.nextLine();
         }
+        
+        // Print result
         drumsActualQualityNow.stream().forEach(e -> System.out.print(e + " "));
         System.out.println();
         System.out.printf("Gabsy has %.2flv.",moneyToSpend);
+        
+        scan.close();
     }
 }
