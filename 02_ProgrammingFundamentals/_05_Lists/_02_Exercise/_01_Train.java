ebf889c08e6f5236efb96e63f6be68df437774ea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 04:19:08 +0300

    01

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_01_Train.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_01_Train.java
@@ -9,16 +9,20 @@ public class _01_Train {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         List<Integer> trainWagonsPassengers = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
         int capacityOfWagon = Integer.parseInt(scan.nextLine());
         String passengersListActions = scan.nextLine();
 
+        // Arrange the train based on commands
         while(!passengersListActions.equals("end")){
             if(passengersListActions.split("\\s+")[0].equals("Add")){
                 int addWagonAndPassengers = Integer.parseInt(passengersListActions.split("\\s+")[1]);
+                
                 trainWagonsPassengers.add(addWagonAndPassengers);
             }else{
                 int addPassengers = Integer.parseInt(passengersListActions.split("\\s+")[0]);
+                
                 for (int i = 0; i < trainWagonsPassengers.size(); i++) {
                     if(trainWagonsPassengers.get(i) + addPassengers <= capacityOfWagon){
                         trainWagonsPassengers.set(i,trainWagonsPassengers.get(i) + addPassengers);
@@ -26,8 +30,13 @@ public class _01_Train {
                     }
                 }
             }
+            
             passengersListActions = scan.nextLine();
         }
+        
+        // Print result
         trainWagonsPassengers.stream().forEach(e -> System.out.print(e + " "));
+        
+        scan.close();
     }
 }
