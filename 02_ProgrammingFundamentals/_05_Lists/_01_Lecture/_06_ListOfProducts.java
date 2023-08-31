c0295c822118f0575dbc5a300c1601c72d7fab60
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 03:28:34 +0300

    06

--- 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_06_ListOfProducts.java
+++ 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_06_ListOfProducts.java
@@ -9,15 +9,25 @@ public class _06_ListOfProducts {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
-        List<String> productsList = new ArrayList<>();
 
+        // Add variable
+        List<String> productsList = new ArrayList<>();
+        
+        // Create list
         for (int i = 0; i < n; i++) {
             productsList.add(scan.nextLine());
         }
+        
+        // Arrange list
         Collections.sort(productsList);
+        
+        // Print result
         for (int i = 0; i < productsList.size(); i++) {
             System.out.printf("%d.%s%n",i+1,productsList.get(i));
         }
+        
+        scan.close();
     }
 }
