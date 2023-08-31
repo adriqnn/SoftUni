9ed51d9a064f768bb33f37b40d6192ca3fdbedb8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 03:31:38 +0300

    07

--- 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_07_RemoveNegativesAndReverse.java
+++ 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_07_RemoveNegativesAndReverse.java
@@ -10,19 +10,27 @@ public class _07_RemoveNegativesAndReverse {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         List<Integer> numbersList = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
 
+        // Remove negative numbers
         for (int i = 0; i < numbersList.size(); i++) {
             if(numbersList.get(i) < 0 ){
                 numbersList.remove(i);
                 i--;
             }
         }
+        
+        // Reverse list
         Collections.reverse(numbersList);
+        
+        // Print result
         if (numbersList.isEmpty()){
             System.out.println("empty");
         }else{
             numbersList.stream().forEach(e-> System.out.print(e + " "));
         }
+        
+        scan.close();
     }
 }
