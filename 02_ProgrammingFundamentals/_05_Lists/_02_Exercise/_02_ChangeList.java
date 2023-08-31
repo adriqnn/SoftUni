954524f2a45bb5ff4dbc34ed129d4c082c7e5639
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 04:28:15 +0300

    02

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_02_ChangeList.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_02_ChangeList.java
@@ -9,7 +9,7 @@ public class _02_ChangeList {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input value
+        // Read input values
         List<Integer> numbersList = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
         String console = scan.nextLine();
 
