e3e6b6b4269072d26f7904b4fddd171c842b5058
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 04:37:27 +0300

    06

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_06_CardsGame.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_06_CardsGame.java
@@ -9,9 +9,11 @@ public class _06_CardsGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         List<Integer> cardsList1 = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
         List<Integer> cardsList2 = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
 
+        // Play card game and determine who wins
         while(cardsList1.size() > 0 && cardsList2.size() > 0){
             if(cardsList1.get(0).equals(cardsList2.get(0))){
                 cardsList1.remove(0);
@@ -19,26 +21,35 @@ public class _06_CardsGame {
             }else if(cardsList1.get(0) > cardsList2.get(0)){
                 cardsList1.add(cardsList1.get(0));
                 cardsList1.add(cardsList2.get(0));
+                
                 cardsList1.remove(0);
                 cardsList2.remove(0);
             }else if(cardsList1.get(0) < cardsList2.get(0)){
                 cardsList2.add(cardsList2.get(0));
                 cardsList2.add(cardsList1.get(0));
+                
                 cardsList1.remove(0);
                 cardsList2.remove(0);
             }
         }
+
         int sum = 0;
+
+        // Print result
         if(cardsList1.isEmpty()){
             for (Integer integer : cardsList2) {
                 sum += integer;
             }
+
             System.out.printf("Second player wins! Sum: %d%n",sum);
         }else {
             for (Integer integer : cardsList1) {
                 sum += integer;
             }
+
             System.out.printf("First player wins! Sum: %d%n",sum);
         }
+
+        scan.close();
     }
 }
