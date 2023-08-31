2ffa105bbd206419970641a033029f4b6796a1cc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-08-31 05:24:27 +0300

    03

--- 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_03_TakeSkipRope.java
+++ 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_03_TakeSkipRope.java
@@ -8,20 +8,27 @@ public class _03_TakeSkipRope {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         StringBuilder console = new StringBuilder(scan.nextLine());
+
+        // Add variables
         List<Integer> decryptIntegers = new ArrayList<>();
         List<String> decryptStrings = new ArrayList<>();
         List<Integer> takeList = new ArrayList<>();
         List<Integer> skipList = new ArrayList<>();
 
+        // Determine if char is digit or not
         for (int i = 0; i < console.length(); i++) {
             char symbol = console.charAt(i);
+
             if(Character.isDigit(symbol)){
                 decryptIntegers.add(Character.getNumericValue(symbol));
             }else{
                 decryptStrings.add(Character.toString(symbol));
             }
         }
+
+        // Determine the needed integers
         for (int i = 0; i < decryptIntegers.size(); i++) {
             if(i % 2 == 0){
                 takeList.add(decryptIntegers.get(i));
@@ -29,17 +36,24 @@ public class _03_TakeSkipRope {
                 skipList.add(decryptIntegers.get(i));
             }
         }
+        
+        
         StringBuilder fromDecryptStrings = new StringBuilder();
         decryptStrings.stream().forEach(fromDecryptStrings::append);
+        
         StringBuilder result = new StringBuilder();
+        
+        // Decrypt String
         for (int i = 0; i < takeList.size(); i++) {
             int take = takeList.get(i);
             int skip = skipList.get(i);
+            
             try{
                 try{
                     for (int j = 0; j < take; j++) {
                         result.append(fromDecryptStrings.charAt(j));
                     }
+                    
                     fromDecryptStrings.delete(0,take);
                     fromDecryptStrings.delete(0,skip);
                 }catch (Exception e){
@@ -47,6 +61,10 @@ public class _03_TakeSkipRope {
             }catch(Exception e){
             }
         }
+
+        // Print result
         System.out.println(result);
+
+        scan.close();
     }
 }
