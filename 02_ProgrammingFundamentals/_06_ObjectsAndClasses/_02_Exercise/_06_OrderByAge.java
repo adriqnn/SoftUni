3e42e7c90d03885731810d1a1a32a8e50b6e1088
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-01 09:33:01 +0300

    06

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge.java
@@ -9,36 +9,55 @@ public class _06_OrderByAge {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input vale
         String console = scan.nextLine();
 
+        // Add variable
         List<Album> names = new ArrayList<>();
+
+
+        // Create collection of people
         while(!console.equals("End")){
             String[] consoleLine = console.split("\\s+");
+
             String name = consoleLine[0];
             String personalNumber = consoleLine[1];
             int age = Integer.parseInt(consoleLine[2]);
-            console = scan.nextLine();
-            Album person = new Album(name,personalNumber,age);
+
+            Album person = new Album(name, personalNumber, age);
             names.add(person);
+
+            console = scan.nextLine();
         }
+
+        // Sort
         names.sort(Comparator.comparing(Album::getAge));
-        names.forEach(e -> System.out.println(e));
+
+        // Print result
+        names.forEach(System.out::println);
+
+        scan.close();
     }
 }
+
+// Create practice Album class
 class Album{
     private String name ;
     private String personalNUmber;
     private int age;
+
     public Album(String name, String personalNUmber, int age) {
         this.name = name;
         this.personalNUmber = personalNUmber;
         this.age = age;
     }
+
     public int getAge() {
         return age;
     }
+
     @Override
     public String toString(){
-        return String.format("%s with ID: %s is %d years old.",this.name,this.personalNUmber,this.age);
+        return String.format("%s with ID: %s is %d years old.", this.name, this.personalNUmber, this.age);
     }
 }
