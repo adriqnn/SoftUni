8b9337f72048441029269ff61d11adbda0ee89e8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-01 09:18:27 +0300

    04

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_04_Articles2.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_04_Articles2.java
@@ -9,20 +9,26 @@ public class _04_Articles2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
+        
+        // add variable
         List<ArticlesDatabase> articles = new ArrayList<>();
 
+        // Collect data for all the articles
         for (int i = 0; i < n; i++) {
             String[] articleLines = scan.nextLine().split(", ");
             String title = articleLines[0];
             String content = articleLines[1];
             String author = articleLines[2];
+            
             ArticlesDatabase article = new ArticlesDatabase(title,content,author);
             articles.add(article);
         }
 
         String command = scan.nextLine();
 
+        // Print result
         if(command.equals("title")){
             articles.sort(Comparator.comparing(ArticlesDatabase::getTitle));
             articles.forEach(e -> System.out.printf("%s - %s: %s%n",e.getTitle(),e.getContent(),e.getAuthor()));
@@ -33,23 +39,31 @@ public class _04_Articles2 {
             articles.sort(Comparator.comparing(ArticlesDatabase::getAuthor));
             articles.forEach(e -> System.out.printf("%s - %s: %s%n",e.getTitle(),e.getContent(),e.getAuthor()));
         }
+        
+        scan.close();
     }
 }
+
+// Create practice AricleDatabase class
 class ArticlesDatabase{
     private String title;
     private String content;
     private String author;
+    
     public ArticlesDatabase(String title, String content, String author) {
         this.title = title;
         this.content = content;
         this.author = author;
     }
+    
     public String getTitle() {
         return title;
     }
+    
     public String getContent() {
         return content;
     }
+    
     public String getAuthor() {
         return author;
     }
