5bf18410654fc54e0dba11edc0bcc97341af71bb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-02 23:11:05 +0300

    01

--- 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_01_ComputerStore1.java
+++ 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_01_ComputerStore1.java
@@ -6,32 +6,42 @@ public class _01_ComputerStore1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String console = scan.nextLine();
 
+        // Add variables
         boolean isRegular = false;
         double price = 0;
+
+        // Make statistics for the orders
         while (!console.equals("special")){
             if (console.equals("regular")){
                 isRegular = true;
                 break;
             }
+
             double plus = Double.parseDouble(console);
-            if(plus<0){
+
+            if(plus < 0){
                 System.out.println("Invalid price!");
+
                 console = scan.nextLine();
                 continue;
             }
+
             price += plus;
             console = scan.nextLine();
         }
+
         if(price == 0){
             System.out.println("Invalid order!");
             return;
         }
 
-        double taxes = price*0.2;
+        double taxes = price * 0.2;
         price = price + taxes;
 
+        // Print result
         if (isRegular){
             System.out.printf("Congratulations you've just bought a new computer!%n" +
                     "Price without taxes: %.2f$%n" +
@@ -45,5 +55,7 @@ public class _01_ComputerStore1 {
                     "-----------%n" +
                     "Total price: %.2f$",price-taxes,taxes,price*0.9);
         }
+
+        scan.close();
     }
 }
