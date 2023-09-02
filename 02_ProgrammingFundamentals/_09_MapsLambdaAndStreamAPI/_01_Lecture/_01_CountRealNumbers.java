179a3804f65dc8fbd8dca054fd4a790c12a3de53
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-03 01:38:44 +0300

    01

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_01_CountRealNumbers.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_01_CountRealNumbers.java
@@ -9,24 +9,29 @@ public class _01_CountRealNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] input = scan.nextLine().split(" ");
         double[] nums = Arrays.stream(input).mapToDouble(Double::parseDouble).toArray();
 
+        // Add variable
         Map<Double, Integer> numbers = new TreeMap<>();
 
+        // Count numbers
         for (double number : nums) {
             if(!numbers.containsKey(number)){
-                numbers.put(number,1);
+                numbers.put(number, 1);
             } else {
                 int occurrences = numbers.get(number);
                 occurrences++;
-                numbers.put(number,occurrences);
+                numbers.put(number, occurrences);
             }
         }
 
+        // Print result
         for (Map.Entry<Double, Integer> entry : numbers.entrySet()) {
-
-            System.out.printf("%.0f -> %d%n", entry.getKey(),entry.getValue());
+            System.out.printf("%.0f -> %d%n", entry.getKey(), entry.getValue());
         }
+
+        scan.close();
     }
 }
