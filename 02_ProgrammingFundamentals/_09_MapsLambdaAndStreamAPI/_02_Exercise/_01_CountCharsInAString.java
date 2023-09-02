78d28ab1c359f5bee6c400f21d5b6950d03e54f9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-03 02:08:11 +0300

    01

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_01_CountCharsInAString.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_01_CountCharsInAString.java
@@ -8,15 +8,20 @@ public class _01_CountCharsInAString {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String text = scan.nextLine();
 
+        // Add variable
         Map<Character, Integer> lettersCount = new LinkedHashMap<>();
 
+        // Count chars in a string
         for (int i = 0; i < text.length(); i++) {
             char currentSymbol = text.charAt(i);
+
             if (currentSymbol==' '){
                 continue;
             }
+
             if(!lettersCount.containsKey(currentSymbol)){
                 lettersCount.put(currentSymbol, 1);
             }else{
@@ -25,11 +30,15 @@ public class _01_CountCharsInAString {
                 lettersCount.put(currentSymbol,currentCount);
             }
         }
-        //can use var instead!
+
+        // Print result
+        // Can use var instead!
         for (Map.Entry<Character, Integer> entry : lettersCount.entrySet()){
             System.out.println(entry.getKey() + " -> " + entry.getValue());
         }
 
         //lettersCount.entrySet().stream().forEach(entry -> System.out.println(entry.getKey() + " -> " + entry.getValue()));
+
+        scan.close();
     }
 }
