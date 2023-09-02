7f4525147d4e96eb6923baf479bf1cba9a75c6c6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-02 23:15:28 +0300

    01

--- 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_01_ComputerStore2.java
+++ 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_01_ComputerStore2.java
@@ -6,13 +6,17 @@ public class _01_ComputerStore2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String userInput = scan.nextLine();
+
+        // Add variable
         double totalWithoutTaxes = 0;
 
+        // Make statistics for the orders
         while(!userInput.equals("regular") && !userInput.equals("special")){
             double currentPrice = Double.parseDouble(userInput);
 
-            if(currentPrice<0){
+            if(currentPrice < 0){
                 System.out.println("Invalid price!");
             }else{
                 totalWithoutTaxes += currentPrice;
@@ -21,6 +25,7 @@ public class _01_ComputerStore2 {
             totalWithoutTaxes += currentPrice;
             userInput = scan.nextLine();
         }
+
         double taxes = totalWithoutTaxes * 0.2;
         double finalPrice = totalWithoutTaxes + taxes;
 
@@ -30,10 +35,13 @@ public class _01_ComputerStore2 {
         }
 
         if (userInput.equals("special")){
-            finalPrice = finalPrice*0.9;
+            finalPrice = finalPrice * 0.9;
         }
 
+        // Print result
         printReceipt(taxes, finalPrice, totalWithoutTaxes);
+
+        scan.close();
     }
 
     private static void printReceipt(double taxes, double finalPrice, double totalWithoutTaxes) {
@@ -41,6 +49,6 @@ public class _01_ComputerStore2 {
         System.out.printf("Price without taxes: %.2f$%n", totalWithoutTaxes);
         System.out.printf("Taxes: %.2f$%n", taxes);
         System.out.println("-----------");
-        System.out.printf("Total price: %.2f$",finalPrice);
+        System.out.printf("Total price: %.2f$", finalPrice);
     }
 }
