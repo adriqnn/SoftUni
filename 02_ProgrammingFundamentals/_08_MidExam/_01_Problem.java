36b537130ed402a27be37277d172a3df353d3f23
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-02 22:50:49 +0300

    01

--- 02_ProgrammingFundamentals/_08_MidExam/_01_Problem.java
+++ 02_ProgrammingFundamentals/_08_MidExam/_01_Problem.java
@@ -6,17 +6,26 @@ public class _01_Problem {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variable
         double everything = 0;
+        
+        // Calculate price
         for (int i = 0; i < n; i++) {
             double pricePerCapsule = Double.parseDouble(scan.nextLine());
             int days = Integer.parseInt(scan.nextLine());
             int capsuleCount = Integer.parseInt(scan.nextLine());
-            double total = ((days*capsuleCount)*pricePerCapsule);
+            
+            double total = ((days * capsuleCount) * pricePerCapsule);
             everything += total;
-            System.out.printf("The price for the coffee is: $%.2f%n",total);
+            System.out.printf("The price for the coffee is: $%.2f%n", total);
         }
+        
+        // Print result
         System.out.printf("Total: $%.2f%n",everything);
+        
+        scan.close();
     }
 }
