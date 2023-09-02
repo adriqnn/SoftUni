3d61a4d0764acfafc49f3577be725f94fb1e9369
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-02 22:47:01 +0300

    01

---
+++ 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_02_MemoryGame.java
@@ -0,0 +1,73 @@
+package _02_ProgrammingFundamentals._07_MidExamPrep;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class _02_MemoryGame {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<String> game = Arrays.stream(scan.nextLine().split(" ")).collect(Collectors.toList());
+        String console = scan.nextLine();
+
+        int round = 0;
+        while (!console.equals("end")){
+            String[] hmm = console.split(" ");
+            int[] input = Arrays.stream((hmm)).mapToInt(e -> Integer.parseInt(e)).toArray();
+            if (input[0] == input[1]){
+                round++;
+                String add = "-" + round + "a";
+                game.add(game.size()/2,add);
+                game.add(game.size()/2,add);
+                System.out.println("Invalid input! Adding additional elements to the board");
+                console = scan.nextLine();
+                continue;
+            }
+            if (input[0]<0 || input[0]>game.size()-1){
+                round++;
+                String add = "-" + round + "a";
+                game.add(game.size()/2,add);
+                game.add(game.size()/2,add);
+                System.out.println("Invalid input! Adding additional elements to the board");
+                console = scan.nextLine();
+                continue;
+            }
+            if (input[1]<0 || input[1]>game.size()-1){
+                round++;
+                String add = "-" + round + "a";
+                //String add = String.format("-%da", round);
+                game.add(game.size()/2,add);
+                game.add(game.size()/2,add);
+                System.out.println("Invalid input! Adding additional elements to the board");
+                console = scan.nextLine();
+                continue;
+            }
+            if(game.get(input[0]).equals(game.get(input[1]))){
+                String element = game.get(input[0]);
+                game.remove(Math.max(input[0],input[1]));
+                game.remove(Math.min(input[0],input[1]));
+                round++;
+                System.out.printf("Congrats! You have found matching elements - %s!%n", element);
+                console = scan.nextLine();
+                if(game.isEmpty()){
+                    System.out.printf("You have won in %d turns!%n", round);
+                    break;
+                }
+                continue;
+            }
+            if(!game.get(input[0]).equals(game.get(input[1]))){
+                round++;
+                System.out.println("Try again!");
+                console = scan.nextLine();
+                continue;
+            }
+            console = scan.nextLine();
+        }
+        if (console.equals("end") && !game.isEmpty()){
+            System.out.println("Sorry you lose :(");
+            System.out.println(game.toString().replaceAll("[\\[\\],]", ""));
+        }
+    }
+}
---
+++ 02_ProgrammingFundamentals/_07_MidExamPrep/_01_MidExamPrep/_03_TheLift.java
@@ -0,0 +1,49 @@
+package _02_ProgrammingFundamentals._07_MidExamPrep;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _03_TheLift {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int people = Integer.parseInt(scan.nextLine());
+        int[] state = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < state.length; i++) {
+            if(state[i]<4){
+                int n1 = 4 - state[i]; // free spots
+                if (people >= n1){
+                    state[i] = state[i] + n1;
+                    people -= n1;
+                }else{
+                    state[i] = state[i] + people;
+                    people -= people;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < state.length; i++) {
+            if (state[i] < 4){
+                int n = 4-state[i];
+                count += n;
+            }
+
+        }
+        if (count > 0 && people == 0){
+            System.out.println("The lift has empty spots!");
+            for (int i : state) {
+                System.out.print(i + " ");
+            }
+        }else if (count == 0 && people > 0){
+            System.out.printf("There isn't enough space! %d people in a queue!%n",people);
+            for (int i : state) {
+                System.out.print(i + " ");
+            }
+        }else if (count == 0 && people == 0){
+            for (int i : state) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
