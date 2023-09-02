5e2e8a504f6a212177c7876d4a2d3e22d89c8608
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-03 01:51:11 +0300

    04

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_04_WordFilter.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_04_WordFilter.java
@@ -6,8 +6,13 @@ import java.util.Scanner;
 public class _04_WordFilter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String[] input = scan.nextLine().split(" ");
 
+        // Print result
         Arrays.stream(input).filter(word -> word.length() % 2 == 0).forEach(word -> System.out.println(word));
+
+        scan.close();
     }
 }
