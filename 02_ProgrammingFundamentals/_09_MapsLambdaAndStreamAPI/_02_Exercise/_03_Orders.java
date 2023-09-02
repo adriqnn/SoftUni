ff953461a999770d3a9cb00270eaef1833d58441
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-03 02:14:23 +0300

    03

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_03_Orders.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_03_Orders.java
@@ -6,18 +6,23 @@ public class _03_Orders {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        Map<String, List<Double>> orders = new LinkedHashMap<>();
+        // Read input value
         String input = scan.nextLine();
 
-        while(!input.equals("buy")){
+        // Add variable
+        Map<String, List<Double>> orders = new LinkedHashMap<>();
 
+        // Combine all the orders and sort them
+        while(!input.equals("buy")){
             String[] order = input.split("\\s+");
+            
             String item = order[0];
             double price = Double.parseDouble(order[1]);
             double quantity = Double.parseDouble(order[2]);
 
             if(!orders.containsKey(item)){
                 List<Double> information = new ArrayList<>();
+                
                 information.add(price);
                 information.add(quantity);
                 orders.put(item, information);
@@ -25,11 +30,17 @@ public class _03_Orders {
                 if(orders.get(item).get(0) != price){
                     orders.get(item).set(0, price);
                 }
+                
                 double currentQuantity = orders.get(item).get(1);
                 orders.get(item).set(1, currentQuantity + quantity);
             }
+            
             input = scan.nextLine();
         }
+
+        // Print result
         orders.forEach((item, info) -> System.out.printf("%s -> %.2f%n", item, info.get(0) * info.get(1)));
+
+        scan.close();
     }
 }
