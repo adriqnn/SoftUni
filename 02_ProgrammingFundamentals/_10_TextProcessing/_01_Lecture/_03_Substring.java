afdcb52434542d2f3e02ee2bdf42fbd060ff7dff
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 16:40:13 +0300

    03

--- 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_03_Substring.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_03_Substring.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class _03_Substring {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String stringToRemove = scan.nextLine();
         String consoleString = scan.nextLine();
+        
+        // Remove the string from console
         while(consoleString.contains(stringToRemove)){
-            consoleString = consoleString.replaceFirst(stringToRemove,"");
+            consoleString = consoleString.replaceFirst(stringToRemove, "");
         }
+        
+        // Print result
         System.out.println(consoleString);
+        
+        scan.close();
     }
 }
