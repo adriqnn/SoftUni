02b219f3d9cec03afe334df1fb378f0cd452d586
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 16:38:03 +0300

    02

--- 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_02_RepeatStrings.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_02_RepeatStrings.java
@@ -6,20 +6,28 @@ public class _02_RepeatStrings {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] console = scan.nextLine().split(" ");
 
+        // Repeat string
         for (String word : console) {
-            String repeatedWord = repeatWord(word,word.length());
+            String repeatedWord = repeatWord(word, word.length());
+            
+            // Print result
             System.out.print(repeatedWord);
         }
 
+        scan.close();
     }
 
+    // Method that concatenates a string multiple times
     private static String repeatWord(String word, int length) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
+        
         for (int i = 0; i < length; i++) {
-            result += word;
+            result.append(word);
         }
-        return result;
+        
+        return result.toString();
     }
 }
