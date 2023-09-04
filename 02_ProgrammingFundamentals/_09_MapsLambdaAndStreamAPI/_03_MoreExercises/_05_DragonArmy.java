97c5fd92d688daa6f120633472e5667382712271
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 06:54:17 +0300

    05

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_03_MoreExercises/_05_DragonArmy.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_03_MoreExercises/_05_DragonArmy.java
@@ -7,9 +7,13 @@ public class _05_DragonArmy {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         int inputLines = Integer.parseInt(scan.nextLine());
+
+        // Add variable
         Map<String, Map<String, List<Double>>> database = new LinkedHashMap<>();
 
+        // Create database for dragons
         for (int i = 0; i < inputLines; i++) {
             String[] information = scan.nextLine().split(" ");
 
@@ -24,16 +28,20 @@ public class _05_DragonArmy {
             Collections.addAll(stats, damage,health,armor);
             database.get(type).put(name, stats);
         }
+
+        // Print results
         database.entrySet().forEach(type -> {
             String typeOf = type.getKey();
             double dmgAverage = type.getValue().values().stream().map(e -> e.get(0)).mapToDouble(Double::valueOf).sum()/type.getValue().size();
             double healthAverage = type.getValue().values().stream().map(e -> e.get(1)).mapToDouble(Double::valueOf).sum()/type.getValue().size();
             double armorAverage = type.getValue().values().stream().map(e -> e.get(2)).mapToDouble(Double::valueOf).sum()/type.getValue().size();
 
-            String firstLine = String.format("%s::(%.2f/%.2f/%.2f)",typeOf,dmgAverage,healthAverage,armorAverage);
-            String secondLine = type.getValue().entrySet().stream().map(e -> String.format("-%s -> damage: %.0f, health: %.0f, armor: %.0f", e.getKey(),e.getValue().get(0),e.getValue().get(1), e.getValue().get(2))).collect(Collectors.joining("\n"));
+            String firstLine = String.format("%s::(%.2f/%.2f/%.2f)", typeOf, dmgAverage, healthAverage, armorAverage);
+            String secondLine = type.getValue().entrySet().stream().map(e -> String.format("-%s -> damage: %.0f, health: %.0f, armor: %.0f", e.getKey(), e.getValue().get(0), e.getValue().get(1), e.getValue().get(2))).collect(Collectors.joining("\n"));
 
             System.out.printf("%s%n%s%n", firstLine, secondLine);
         });
+        
+        scan.close();
     }
 }
