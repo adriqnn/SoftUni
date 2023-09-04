55d23652b35840e660599b1b9a33560155f3ac35
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 06:14:36 +0300

    08

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_08_CompanyUsers.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_08_CompanyUsers.java
@@ -7,27 +7,45 @@ public class _08_CompanyUsers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        Map<String, Set<String>> employees = new LinkedHashMap<>();
+        // Read input value
         String input = scan.nextLine();
 
+        // Add variable
+        Map<String, Set<String>> employees = new LinkedHashMap<>();
+
+        // Make employee database
         while(!input.equals("End")){
             String[] line = input.split(" -> ");
+
             String company = line[0];
             String id = line[1];
 
             if(!employees.containsKey(company)){
-                employees.put(company, new HashSet<>());
+                employees.put(company, new LinkedHashSet<>());
             }
+
             employees.get(company).add(id);
+
             input = scan.nextLine();
         }
 
+        // Print result
         employees.entrySet().forEach(element -> {
             String company = element.getKey();
-            String ids = element.getValue().stream().sorted().map(string -> String.format("-- %s",string)).collect(Collectors.joining("\n"));
+            String ids = element.getValue().stream().map(string -> String.format("-- %s",string)).collect(Collectors.joining("\n"));
 
             System.out.printf("%s%n%s%n",company,ids);
         });
+
+        scan.close();
+
+//        employees.entrySet().forEach(element -> {
+//            String company = element.getKey();
+//            String ids = element.getValue().stream().sorted().map(string -> String.format("-- %s",string)).collect(Collectors.joining("\n"));
+//
+//            System.out.printf("%s%n%s%n",company,ids);
+//        });
+
 
 //        employees.entrySet().forEach(element -> {
 //            String company = element.getKey();
