e242e0a4b99c3c380fa77558f8f10ac0ab13b94d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 00:26:16 +0300

    04

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_04_CaesarCipher.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_04_CaesarCipher.java
@@ -5,12 +5,19 @@ import java.util.Scanner;
 public class _04_CaesarCipher {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String console = scan.nextLine();
+
+        // Encrypt the message
         StringBuilder encrypt = new StringBuilder();
         for (int i = 0; i < console.length(); i++) {
-            char symbol = (char) (console.charAt(i) +3);
+            char symbol = (char) (console.charAt(i) + 3);
+
             encrypt.append(Character.valueOf(symbol));
         }
+
+        // Print result
         System.out.println(encrypt);
     }
 }
