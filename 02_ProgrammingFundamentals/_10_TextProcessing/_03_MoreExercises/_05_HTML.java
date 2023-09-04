72b9bec47c79a77b2f4f156d82ba6d9e16ac9623
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 01:41:37 +0300

    05

--- 02_ProgrammingFundamentals/_10_TextProcessing/_03_MoreExercises/_05_HTML.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_03_MoreExercises/_05_HTML.java
@@ -23,10 +23,10 @@ public class _05_HTML {
         }
 
         // Print result
-        System.out.printf("<h1>%n    %s%n</h1>%n",title);
-        System.out.printf("<article>%n    %s%n</article>%n",content);
+        System.out.printf("<h1>%n    %s%n</h1>%n", title);
+        System.out.printf("<article>%n    %s%n</article>%n", content);
         for (int i = 0; i < commentsList.size(); i++) {
-            System.out.printf("<div>%n    %s%n</div>%n",commentsList.get(i));
+            System.out.printf("<div>%n    %s%n</div>%n", commentsList.get(i));
         }
 
         scan.close();
