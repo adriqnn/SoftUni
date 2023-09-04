af9aaee393e6efe7d99cfd7563ebd8d6e87c5d52
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 01:46:34 +0300

    02

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_01_Lecture/_02_MatchPhoneNumber.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_01_Lecture/_02_MatchPhoneNumber.java
@@ -9,14 +9,24 @@ import java.util.regex.Pattern;
 public class _02_MatchPhoneNumber {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String console = scan.nextLine();
+
+        // Add variables
         String regexPhone = "\\+359(?<separator>[ -])2\\1\\d{3}\\1\\d{4}\\b";
         Pattern patternPhone = Pattern.compile(regexPhone);
         Matcher matcherPhone = patternPhone.matcher(console);
         List<String> matchedPhones = new ArrayList<>();
+
+        // Match phone numbers and save them
         while(matcherPhone.find()){
             matchedPhones.add(matcherPhone.group());
         }
+
+        // Print result
         System.out.println(matchedPhones.toString().replaceAll("[\\[\\]]",""));
+
+        scan.close();
     }
 }
