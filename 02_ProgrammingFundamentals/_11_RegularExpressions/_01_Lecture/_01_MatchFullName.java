4fd591615bd0f82033a1c07b47f39e3309cc2e86
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 01:44:43 +0300

    01

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_01_Lecture/_01_MatchFullName.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_01_Lecture/_01_MatchFullName.java
@@ -7,13 +7,22 @@ import java.util.regex.Pattern;
 public class _01_MatchFullName {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input value
+        String console = scan.nextLine();
+        
+        // Add variables
         String regexName = "(?<name>\\b[A-Z][a-z]+ [A-Z][a-z]+\\b)";
         Pattern patternName = Pattern.compile(regexName);
-        String console = scan.nextLine();
         Matcher matcherName = patternName.matcher(console);
+        
+        // Match the name and print result
         while(matcherName.find()){
             String find = matcherName.group("name");
+            
             System.out.print(find + " ");
         }
+        
+        scan.close();
     }
 }
