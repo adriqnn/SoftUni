c2d0ad195ddca214aa4d673d42825ceef1ad3b32
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 00:40:25 +0300

    06

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_06_ReplaceRepeatingChars.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_06_ReplaceRepeatingChars.java
@@ -5,13 +5,21 @@ import java.util.Scanner;
 public class _06_ReplaceRepeatingChars {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input value
         StringBuilder console = new StringBuilder(scan.nextLine());
+        
+        // Replace repeating character
         for (int i = 1; i < console.length(); i++) {
             if(console.charAt(i) == console.charAt(i-1)){
                 console.deleteCharAt(i);
                 i--;
             }
         }
+        
+        // Print result
         System.out.println(console);
+        
+        scan.close();
     }
 }
