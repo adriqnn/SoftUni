7662f9f3d865496cf2d979716cb705717efa8953
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 23:59:31 +0300

    03

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_03_ExtractFile.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_03_ExtractFile.java
@@ -6,8 +6,15 @@ public class _03_ExtractFile {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String[] console = scan.nextLine().split("\\\\");
+        
+        // Add variable
         String[] file = console[console.length-1].split("\\.");
-        System.out.printf("File name: %s%nFile extension: %s%n", file[0],file[1]);
+        
+        // Print result
+        System.out.printf("File name: %s%nFile extension: %s%n", file[0], file[1]);
+        
+        scan.close();
     }
 }
