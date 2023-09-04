426daa63f599b44d97ce85307620d05caadc63e6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 01:20:21 +0300

    05

--- 02_ProgrammingFundamentals/_10_TextProcessing/_03_MoreExercises/_05_HTML.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_03_MoreExercises/_05_HTML.java
@@ -8,18 +8,27 @@ public class _05_HTML {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String title = scan.nextLine();
         String content = scan.nextLine();
         String comments = scan.nextLine();
+
+        // Add variable
         List<String> commentsList = new ArrayList<>();
+
+        // Create the HTML list of comments
         while(!comments.equals("end of comments")){
             commentsList.add(comments);
             comments = scan.nextLine();
         }
+
+        // Print result
         System.out.printf("<h1>%n    %s%n</h1>%n",title);
         System.out.printf("<article>%n    %s%n</article>%n",content);
         for (int i = 0; i < commentsList.size(); i++) {
             System.out.printf("<div>%n    %s%n</div>%n",commentsList.get(i));
         }
+
+        scan.close();
     }
 }
