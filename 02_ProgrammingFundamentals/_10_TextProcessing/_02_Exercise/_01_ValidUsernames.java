722ec874b73bdcca681cddf9b427a0089586145c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 00:17:49 +0300

    01

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_01_ValidUsernames.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_01_ValidUsernames.java
@@ -11,25 +11,28 @@ public class _01_ValidUsernames {
 
         // Validate usernames
         for (int i = 0; i < console.length; i++) {
+
+            // Validate the length
             if(console[i].length() >= 3 && console[i].length() <= 16){
                 boolean isValid = true;
 
+                // Validate characters
                 for (int j = 0; j < console[i].length(); j++) {
                     char symbol = console[i].charAt(j);
-                    if(Character.isLetterOrDigit(symbol) || symbol == '-' || symbol == '_' ){
 
-                    }else{
-                        isValid = false;
+                    isValid = Character.isLetterOrDigit(symbol) || symbol == '-' || symbol == '_';
+                    if(!isValid){
+                        break;
                     }
                 }
-                
+
+                // Print result
                 if(isValid){
                     System.out.println(console[i]);
                 }
             }
         }
-        
+
         scan.close();
     }
 }
-
