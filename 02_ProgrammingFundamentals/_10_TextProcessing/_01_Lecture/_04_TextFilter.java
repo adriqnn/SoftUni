6a75736dbe0a4714c1db97c42f71ebac23b3ed72
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-04 16:41:55 +0300

    04

--- 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_04_TextFilter.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_01_Lecture/_04_TextFilter.java
@@ -6,16 +6,25 @@ public class _04_TextFilter {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] words = scan.nextLine().split(", ");
         String text = scan.nextLine();
 
+        // Filter the words
         for (int i = 0; i < words.length; i++) {
             StringBuilder replacement = new StringBuilder();
+            
             for (int j = 0; j < words[i].length(); j++) {
                 replacement.append("*");
             }
+            
             text = text.replaceAll(words[i], replacement.toString());
         }
+        
+        // Print result
         System.out.println(text);
+        
+        scan.close();
     }
 }
+
