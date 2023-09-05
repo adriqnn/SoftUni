eb01c2a1fc796a55fb30bc01193c1b4ad29aff11
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 04:25:28 +0300

    06

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_06_ExtractEmails.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_06_ExtractEmails.java
@@ -8,14 +8,20 @@ public class _06_ExtractEmails {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String input = scan.nextLine();
+
+        // Add variables
         String regexEmail = "((?<=\\s)[a-zA-Z0-9]+([-.]\\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\\.[a-z]{2,}))";
         Pattern patternEmail = Pattern.compile(regexEmail);
         Matcher matcherEmail = patternEmail.matcher(input);
 
+        // Extract emails and print result
         while (matcherEmail.find()){
             String email = matcherEmail.group(1);
             System.out.println(email);
         }
+
+        scan.close();
     }
 }
