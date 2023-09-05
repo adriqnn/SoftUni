1e614ecf7989064390c5e9efeb8b864c4ccb5897
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-05 04:24:02 +0300

    05

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_05_NetherRealmsV3.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_05_NetherRealmsV3.java
@@ -12,8 +12,10 @@ public class _05_NetherRealmsV3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         List<String> console = Arrays.stream(scan.nextLine().split(",\\s*")).map(e -> e.replaceAll(" ", "")).collect(Collectors.toList());
 
+        // Add variables
         String regexHealth = "[^\\d\\+\\-\\*\\/.]{1}";
         Pattern patternHealth = Pattern.compile(regexHealth);
         String regexAttack = "[+\\-]?[0-9]+\\.?[0-9]*";
@@ -21,22 +23,29 @@ public class _05_NetherRealmsV3 {
         String regexAction = "[*\\/]";
         Pattern patternAction = Pattern.compile(regexAction);
 
+        // Determine the result of the nether realm battle
         for (int i = 0; i < console.size(); i++) {
             Matcher matcherHealth = patternHealth.matcher(console.get(i));
             int health = 0;
+            
             while(matcherHealth.find()){
                 health += (int) matcherHealth.group().charAt(0);
             }
+            
             double attackDemons = 0;
             Matcher matcherAttack = patternAttack.matcher(console.get(i));
+            
             while(matcherAttack.find()){
                 attackDemons += Double.parseDouble(matcherAttack.group());
             }
+            
             Matcher matcherAction = patternAction.matcher(console.get(i));
             List<Character> actions = new ArrayList<>();
+            
             while(matcherAction.find()){
                 actions.add(matcherAction.group().charAt(0));
             }
+            
             for (int j = 0; j < actions.size(); j++) {
                 if(actions.get(j) == '*'){
                     attackDemons *= 2;
@@ -44,7 +53,11 @@ public class _05_NetherRealmsV3 {
                     attackDemons /=2;
                 }
             }
+            
+            // Print result
             System.out.printf("%s - %d health, %.2f damage%n", console.get(i), health, attackDemons);
         }
+        
+        scan.close();
     }
 }
