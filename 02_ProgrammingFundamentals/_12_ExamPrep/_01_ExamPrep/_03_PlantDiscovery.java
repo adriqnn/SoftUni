d364b1e085721a54f3aa547f0b7086f7830bcc66
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-06 21:44:45 +0300

    03

--- 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_03_PlantDiscovery.java
+++ 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_03_PlantDiscovery.java
@@ -5,28 +5,40 @@ import java.util.*;
 public class _03_PlantDiscovery {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         int plantNumber = Integer.parseInt(scan.nextLine());
+
+        // Add variable
         Map<String,PlantData> currentPlants = new LinkedHashMap<>();
 
-        //evaluate in intellij
+        // Add plants
         for (int i = 0; i < plantNumber; i++) {
             String plantText = scan.nextLine();
+
             String[] plantParts = plantText.split("<->");
+
             String plantName = plantParts[0];
             int plantRarity = Integer.parseInt(plantParts[1]);
+            
             PlantData newPlant = new PlantData(new ArrayList<>(), plantRarity);
             currentPlants.put(plantName, newPlant);
         }
         String command = scan.nextLine();
+
+        // Create plant data
         while(!command.equals("Exhibition")){
             String[] commandParts = command.split(": | - ");
+
             String commandName = commandParts[0];
             String plantName = commandParts[1];
+
             if(!currentPlants.containsKey(plantName)){
                 command = scan.nextLine();
                 System.out.println("error");
                 continue;
             }
+
             switch(commandName){
                 case "Rate":
                     int rating = Integer.parseInt(commandParts[2]);
@@ -45,6 +57,8 @@ public class _03_PlantDiscovery {
             }
             command = scan.nextLine();
         }
+
+        // Print result
         System.out.println("Plants for the exhibition:");
         currentPlants.entrySet().stream().sorted((p1,p2) -> {
             int result = Integer.compare(p2.getValue().getRarity(), p1.getValue().getRarity());
@@ -54,9 +68,12 @@ public class _03_PlantDiscovery {
             return result;
 
         }).forEach(p -> System.out.printf("- %s; Rarity: %d; Rating: %.2f%n", p.getKey(),p.getValue().getRarity(),p.getValue().getAverageRating()));
+
+        scan.close();
     }
 }
 
+// Create practice PlantData class
 class PlantData{
     private List<Integer> ratings;
     private int rarity;
@@ -79,9 +96,11 @@ class PlantData{
 
     public double getAverageRating(){
         double sumOfAllRatings = 0;
+
         for (Integer rating : this.ratings){
             sumOfAllRatings += rating;
         }
+
         if(sumOfAllRatings == 0){
             return 0;
         }else{
