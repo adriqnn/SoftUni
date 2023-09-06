94043d4da1a2fd3ca2399537b0fa8ebb62865cc1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-06 21:29:16 +0300

    01

--- 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_01_WorldTour.java
+++ 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_01_WorldTour.java
@@ -6,42 +6,60 @@ public class _01_WorldTour {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         StringBuilder initialDestination = new StringBuilder(scan.nextLine());
         String command = scan.nextLine();
 
+        // Determine world tour route
         while(!command.equals("Travel")){
             String[] input = command.split(":");
+
             String switchCommand = input[0];
+
             switch (switchCommand){
                 case "Add Stop":
                     int index1 = Integer.parseInt(input[1]);
                     String addString = input[2];
-                    if (indexIsValid(index1,initialDestination.toString())) {
+
+                    if (indexIsValid(index1, initialDestination.toString())) {
                         initialDestination.insert(index1, addString);
+
                         System.out.println(initialDestination);
                     }
+
                     break;
                 case "Remove Stop":
                     int index2 = Integer.parseInt(input[1]);
                     int index3 = Integer.parseInt(input[2]);
-                    if (indexIsValid(index2,initialDestination.toString()) && indexIsValid(index3,initialDestination.toString())) {
+
+                    if (indexIsValid(index2,initialDestination.toString()) && indexIsValid(index3, initialDestination.toString())) {
                         initialDestination.delete(index2, index3+1);
                     }
+
                     System.out.println(initialDestination);
+
                     break;
                 case "Switch":
                     String oldString = input[1];
                     String newString = input[2];
+
                     String destinationAsString = initialDestination.toString();
-                    destinationAsString = destinationAsString.replaceAll(oldString,newString);
+                    destinationAsString = destinationAsString.replaceAll(oldString, newString);
                     initialDestination = new StringBuilder(destinationAsString);
+
                     System.out.println(initialDestination);
                     break;
             }
+            
             command = scan.nextLine();
         }
+
+        // Print result
         System.out.printf("Ready for world tour! Planned stops: %s",  initialDestination);
+
+        scan.close();
     }
+
     public static boolean indexIsValid(int index, String text) {
         return index >= 0 && index < text.length();
     }
