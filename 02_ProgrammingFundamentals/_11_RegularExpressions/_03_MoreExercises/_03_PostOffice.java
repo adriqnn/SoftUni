92176399b933952266766b8d42df282b0c3dc47b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-06 19:47:27 +0300

    03

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_03_MoreExercises/_03_PostOffice.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_03_MoreExercises/_03_PostOffice.java
@@ -9,10 +9,12 @@ public class _03_PostOffice {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] input = scan.nextLine().split("\\|");
 
-
+        // Add varaibles
         StringBuilder characters = new StringBuilder();
+
         String regexCapitalLetters = "([#$%*&]+)(?<string>[A-Z]+)\\1";
         Pattern patternCapitalLetters = Pattern.compile(regexCapitalLetters);
         Matcher matcherCapitalLetters = patternCapitalLetters.matcher(input[0]);
@@ -21,6 +23,7 @@ public class _03_PostOffice {
             characters.append(matcherCapitalLetters.group("string"));
         }
 
+        // Decipher post office message
         for (int i = 0; i < characters.length(); i++) {
             char letter = characters.charAt(i);
 
@@ -32,9 +35,13 @@ public class _03_PostOffice {
             while(matcherWordLength.find()){
                 wordLength = Integer.parseInt(matcherWordLength.group("length")) + 1;
             }
+
             int finalWordLength = wordLength;
+
             Arrays.stream(input[2].split("\\s+")).filter(e -> (e.length() == finalWordLength && e.charAt(0) == letter)).forEach(System.out::println);
         }
+
+        scan.close();
     }
 }
 //    String capitalLetters = "([#$%*&]+)(?<string>[A-Z]+)\\1";
