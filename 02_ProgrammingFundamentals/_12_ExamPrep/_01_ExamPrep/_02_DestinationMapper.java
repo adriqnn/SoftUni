9f038253f3738f947f95a4764fc7afc09fdf393c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-06 21:32:06 +0300

    02

--- 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_02_DestinationMapper.java
+++ 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_02_DestinationMapper.java
@@ -10,26 +10,34 @@ public class _02_DestinationMapper {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
         String console = scan.nextLine();
 
+        // Add variables
         String regex = "(?<equals>[=\\/])(?<destination>[A-Z][A-Za-z]{2,})(\\1)";
         Pattern regexPattern = Pattern.compile(regex);
         Matcher regexMatcher = regexPattern.matcher(console);
-        List<String> locations = new ArrayList<>();
 
+        List<String> locations = new ArrayList<>();
         int sum = 0;
         //int travelPoints = locations.stream().mapToInt(location -> location.length()).sum();
         //int travelPoints = locations.stream().mapToInt(String::length).sum();
         //String resultText = String.join(", ", locations);
+
+        // Determine destination
         while (regexMatcher.find()){
             locations.add(regexMatcher.group("destination"));
             for (int i = 0; i < regexMatcher.group("destination").length(); i++) {
                 sum++;
             }
         }
+
+        // Print result
         System.out.print("Destinations: ");
         //locations.stream().forEach(e -> System.out.print(e));
         System.out.println(locations.toString().replace("[", "").replace("]",  ""));
         System.out.println("Travel Points: " + sum);
+
+        scan.close();
     }
 }
