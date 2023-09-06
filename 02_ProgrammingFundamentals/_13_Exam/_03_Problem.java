22de7959dd26184ed57e387e3f284c127f7fe061
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-06 22:44:42 +0300

    01

--- 02_ProgrammingFundamentals/_13_Exam/_03_Problem.java
+++ 02_ProgrammingFundamentals/_13_Exam/_03_Problem.java
@@ -6,25 +6,33 @@ public class _03_Problem {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input value
+        String console = scan.nextLine();
+        
+        // Add variables
         Map<String, List<String>> guestLikes = new TreeMap<>();
         Map<String, List<String>> guestUnlike = new HashMap<>();
-
-        String console = scan.nextLine();
-
+        
         int unlikedMeals = 0;
+        
+        // Make statistics for likes and dislikes
         while(!console.equals("Stop")){
             String[] commandLine = console.split("-");
+            
             String command = commandLine[0];
             String guestName = commandLine[1];
             String mealName = commandLine[2];
+            
             switch(command){
                 case "Like":
                     if(!guestLikes.containsKey(guestName)){
                         guestLikes.put(guestName,new ArrayList<>());
                     }
+                    
                     if(!guestLikes.get(guestName).contains(mealName)){
                         guestLikes.get(guestName).add(mealName);
                     }
+                    
                     break;
                 case "Unlike":
                     if (!guestLikes.containsKey(guestName)){
@@ -32,6 +40,7 @@ public class _03_Problem {
                         console = scan.nextLine();
                         continue;
                     }
+                    
                     if(guestLikes.get(guestName).contains(mealName)){
                         guestLikes.get(guestName).remove(mealName);
                         System.out.printf("%s doesn't like the %s.%n",guestName,mealName);
@@ -39,10 +48,14 @@ public class _03_Problem {
                     }else{
                         System.out.printf("%s doesn't have the %s in his/her collection.%n",guestName,mealName);
                     }
+                    
                     break;
             }
+            
             console = scan.nextLine();
         }
+        
+        // Print result
         guestLikes.entrySet().stream().sorted((m1,m2) -> Integer.compare(m2.getValue().size(), m1.getValue().size()))
                 .forEach(guest -> {
                     System.out.print(guest.getKey() + ": ");
@@ -50,6 +63,8 @@ public class _03_Problem {
                     System.out.println();
                 });
         System.out.printf("Unliked meals: %d", unlikedMeals);
+        
+        scan.close();
     }
 }
 /*
