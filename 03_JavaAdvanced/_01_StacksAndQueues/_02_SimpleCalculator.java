e8e1b7672e8c28b4af9f792cc6a1a3b84ddb8084
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-07 23:39:06 +0300

    02

--- 03_JavaAdvanced/_01_StacksAndQueues/_02_SimpleCalculator.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_02_SimpleCalculator.java
@@ -7,26 +7,41 @@ import java.util.Scanner;
 public class _02_SimpleCalculator {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input values
         String[] tokens = scan.nextLine().split("\\s+");
+        
+        // Add variables
         ArrayDeque<String> stack = new ArrayDeque<>();
         Collections.addAll(stack,tokens);
         /*String[] input = scan.nextLine().split("\\s+");
         for (String s : input) {
             stack.push(s);
         }*/
+
+        // Read array and push values to a deque
         //Arrays.stream(scan.nextLine().split("\\s+")).forEach(stack::push);
+
+        //Read array and push values to a newly created deque
         //Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toCollection(ArrayDeque::new)).forEach(stack::push);
+        
+        // Execute calculations
         while (stack.size() > 1){
             int first = Integer.valueOf(stack.pop());
             String operation = stack.pop();
             int second = Integer.valueOf(stack.pop());
+            
             switch(operation){
-                case "+": stack.push(String.valueOf(first+second));
+                case "+": stack.push(String.valueOf(first + second));
                     break;
-                case "-": stack.push(String.valueOf(first-second));
+                case "-": stack.push(String.valueOf(first - second));
                     break;
             }
         }
+        
+        // Print result
         System.out.println(stack.pop());
+        
+        scan.close();
     }
 }
