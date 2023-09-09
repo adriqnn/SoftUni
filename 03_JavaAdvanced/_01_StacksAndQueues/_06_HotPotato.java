98ddf13287bec5ef4d34324ee7e53a190e6d2f58
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-09 22:56:12 +0300

    06

--- 03_JavaAdvanced/_01_StacksAndQueues/_06_HotPotato.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_06_HotPotato.java
@@ -6,18 +6,31 @@ import java.util.Scanner;
 public class _06_HotPotato {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String[] players = scan.nextLine().split("\\s+");
         int n = Integer.parseInt(scan.nextLine());
+
+        // Add variables
         ArrayDeque<String> queue = new ArrayDeque<>();
+
+        // Create queue
         for (String e : players) {
             queue.offer(e);
         }
+
+        // Play the game
         while(queue.size() > 1){
             for (int i = 1; i < n; i++) {
                 queue.offer(queue.poll());
             }
+
             System.out.println("Removed " + queue.poll());
         }
+
+        // Print result
         System.out.println("Last is " + queue.poll());
+        
+        scan.close();
     }
 }
