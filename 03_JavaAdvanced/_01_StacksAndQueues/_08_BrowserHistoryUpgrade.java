3bfdb44a16b418c86aaaf88a797262e8f2a9b80d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-09 23:07:08 +0300

    08

--- 03_JavaAdvanced/_01_StacksAndQueues/_08_BrowserHistoryUpgrade.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_08_BrowserHistoryUpgrade.java
@@ -6,22 +6,29 @@ import java.util.Scanner;
 public class _08_BrowserHistoryUpgrade {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Add variables
         ArrayDeque<String> history = new ArrayDeque<>();
         ArrayDeque<String> forward = new ArrayDeque<>();
         String input = "";
+        
+        // Read input value and create browser history
         while(!"Home".equals(input = scan.nextLine())){
             if(input.equals("back")){
                 if(history.size() > 1){
                     forward.push(history.pop());
+                    
                     System.out.println(history.peek());
                 }else{
                     System.out.println("no previous URLs");
                 }
                 continue;
             }
+            
             if(input.equals("forward")){
                 if(forward.size() > 0){
                     history.push(forward.pop());
+                    
                     System.out.println(history.peek());
                     continue;
                 }else{
@@ -29,9 +36,16 @@ public class _08_BrowserHistoryUpgrade {
                 }
                 continue;
             }
+            
+            // Print result
             System.out.println(input);
+            
             history.push(input);
+            
             forward.clear();
         }
+        
+        scan.close();
     }
 }
+
