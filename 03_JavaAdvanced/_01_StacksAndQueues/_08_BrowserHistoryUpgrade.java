39d4a3089a8ae3de927adf17a906e9a990cab4e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-10 02:27:35 +0300

    08

--- 03_JavaAdvanced/_01_StacksAndQueues/_08_BrowserHistoryUpgrade.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_08_BrowserHistoryUpgrade.java
@@ -6,46 +6,47 @@ import java.util.Scanner;
 public class _08_BrowserHistoryUpgrade {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         // Add variables
         ArrayDeque<String> history = new ArrayDeque<>();
         ArrayDeque<String> forward = new ArrayDeque<>();
-        String input = "";
-        
+        String currentURL = "";
+
         // Read input value and create browser history
-        while(!"Home".equals(input = scan.nextLine())){
-            if(input.equals("back")){
+        while(!"Home".equals(currentURL = scan.nextLine())){
+            if(currentURL.equals("back")){
                 if(history.size() > 1){
                     forward.push(history.pop());
-                    
+
                     System.out.println(history.peek());
                 }else{
                     System.out.println("no previous URLs");
                 }
+                
                 continue;
             }
-            
-            if(input.equals("forward")){
+
+            if(currentURL.equals("forward")){
                 if(forward.size() > 0){
                     history.push(forward.pop());
-                    
+
                     System.out.println(history.peek());
                     continue;
                 }else{
                     System.out.println("no next URLs");
                 }
+                
                 continue;
             }
-            
+
             // Print result
-            System.out.println(input);
-            
-            history.push(input);
-            
+            System.out.println(currentURL);
+
+            history.push(currentURL);
+
             forward.clear();
         }
-        
+
         scan.close();
     }
 }
-
