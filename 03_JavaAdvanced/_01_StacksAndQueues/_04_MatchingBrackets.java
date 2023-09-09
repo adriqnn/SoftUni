f272839941a32f6bb268e68d4abd6f45984dabd7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-09 21:52:49 +0300

    04

--- 03_JavaAdvanced/_01_StacksAndQueues/_04_MatchingBrackets.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_04_MatchingBrackets.java
@@ -6,16 +6,27 @@ import java.util.Scanner;
 public class _04_MatchingBrackets {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        // Read input value
         String expression = scan.nextLine();
+
+        // Add variable
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+
+        // Determine if the brackets are matching
         for (int i = 0; i < expression.length(); i++) {
             char current = expression.charAt(i);
+            
             if(current == '('){
                 stack.push(i);
             }else if (current == ')'){
                 int begin = stack.pop();
-                System.out.println(expression.substring(begin,i+1));
+                
+                System.out.println(expression.substring(begin, i+1));
             }
         }
+        
+        scan.close();
     }
 }
