28a5af35f40ba988c96dfd2a3aae63556eea734c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-10 00:15:26 +0300

    07

--- 03_JavaAdvanced/_01_StacksAndQueues/_07_MathPotato.java
+++ 03_JavaAdvanced/_01_StacksAndQueues/_07_MathPotato.java
@@ -7,27 +7,42 @@ public class _07_MathPotato {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        // Read input values
         String[] input = scan.nextLine().split("\\s+");
         int n = Integer.parseInt(scan.nextLine());
 
+        // Add variables
         ArrayDeque<String> players = new ArrayDeque<>();
+
+        // Create queue
         for (String child : input) {
             players.offer(child);
         }
+
         int cycle = 1;
+
+        // Play the game
         while(players.size() > 1){
             for (int i = 1; i < n; i++) {
                 players.offer(players.poll());
             }
+
             if(isPrime(cycle)){
                 System.out.println("Prime " + players.peek());
             }else {
                 System.out.println("Removed " + players.poll());
             }
+
             cycle ++;
         }
+
+        // Print result
         System.out.println("Last is " + players.poll());
+
+        scan.close();
     }
+
+    // Method that determines if a number is a prime or not
     private static boolean isPrime(int cycle) {
         if (cycle <= 1){
             return false;
