10a308598576f4e5c322a43ac99ebf2cbf518829
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-10 03:17:57 +0300

    02

--- 03_JavaAdvanced/_01_StacksAndQueuesExercise/_02_BasicStackOperations.java
+++ 03_JavaAdvanced/_01_StacksAndQueuesExercise/_02_BasicStackOperations.java
@@ -7,32 +7,47 @@ import java.util.Scanner;
 public class _02_BasicStackOperations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         int[] lineOfNumbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
-        int elementsToPush = lineOfNumbers[0];
+        int[] stackLine = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        // Add variables
         int elementsToPop = lineOfNumbers[1];
         int elementPresent = lineOfNumbers[2];
-        int[] stackLine = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
         ArrayDeque<Integer> stack = new ArrayDeque<>();
         Arrays.stream(stackLine).forEach(stack::push);
         int min = Integer.MAX_VALUE;
+
+        // Pop set number of elements
         for (int i = 0; i < elementsToPop; i++) {
             stack.pop();
         }
+
         int size = stack.size();
+
+        // Check if the input number is present
         for (int i = 0; i < size; i++) {
             int n = stack.pop();
+
             if(n == elementPresent){
                 System.out.println("true");
                 return;
             }
+
             if(n < min){
                 min = n;
             }
         }
+
+        // Print result
         if (stack.size() == 0 && min == Integer.MAX_VALUE){
             System.out.println("0");
         }else {
             System.out.println(min);
         }
+
+        scan.close();
     }
 }
