902fe864b21d27ad02d3ca5668bdf9582741e808
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-11 19:42:43 +0300

    06

--- 03_JavaAdvanced/_01_StacksAndQueuesExercise/_06_RecursiveFibonacci.java
+++ 03_JavaAdvanced/_01_StacksAndQueuesExercise/_06_RecursiveFibonacci.java
@@ -5,21 +5,32 @@ import java.util.Scanner;
 public class _06_RecursiveFibonacci {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input value
         int num = Integer.parseInt(scan.nextLine());
+        
+        // Print fibonacci
         printRecursiveFibonacci(num);
+        
+        scan.close();
     }
+    
+    // Method that prints fibonacci
     private static void printRecursiveFibonacci(int num) {
         if (num == 0){
             System.out.println("0");
             return;
         }
+        
         long number1InSequence = 1;
         long number2InSequence = 1;
+        
         for (int i = 1; i < num; i++) {
             long temporary = number2InSequence;
             number2InSequence = number1InSequence + number2InSequence;
             number1InSequence = temporary;
         }
+        
         System.out.println(number2InSequence);
     }
 }
