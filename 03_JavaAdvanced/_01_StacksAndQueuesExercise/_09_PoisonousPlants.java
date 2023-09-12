c66aacb8671cd7d9aaf6a7a2b32e1065ff332607
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-12 21:16:01 +0300

    09

--- 03_JavaAdvanced/_01_StacksAndQueuesExercise/_09_PoisonousPlants.java
+++ 03_JavaAdvanced/_01_StacksAndQueuesExercise/_09_PoisonousPlants.java
@@ -6,30 +6,44 @@ import java.util.stream.Collectors;
 public class _09_PoisonousPlants {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         int flowers = Integer.parseInt(scan.nextLine());
         List<Long> numbersList = Arrays.stream(scan.nextLine().split("\\s+")).map(Long::parseLong).collect(Collectors.toList());
+
+        // Add variables
         Deque<Integer> indexStack = new ArrayDeque<>();
         boolean noMoreChanges = false;
         int days = 0;
+
+        // Determine the health of the plants
         while (!noMoreChanges){
             if(numbersList.size() > 1){
+
                 for (int i = 1; i < numbersList.size(); i++) {
                     if(numbersList.get(i) > numbersList.get(i-1)){
                         indexStack.push(i);
                     }
                 }
+
             }
+
             if(indexStack.isEmpty() || numbersList.size() <= 1){
                 noMoreChanges = true;
             }else{
                 int n = indexStack.size();
+
                 for (int i = 0; i < n; i++) {
                     int index = indexStack.pop();
                     numbersList.remove(index);
                 }
+
                 days++;
             }
         }
+
         System.out.println(days);
+
+        scan.close();
     }
 }
