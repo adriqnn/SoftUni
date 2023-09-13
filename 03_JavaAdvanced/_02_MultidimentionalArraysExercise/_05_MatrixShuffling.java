508ca661080c1bfa3a8fc531398c6702d58790ef
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-13 19:13:05 +0300

    05

--- 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_05_MatrixShuffling.java
+++ 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_05_MatrixShuffling.java
@@ -51,6 +51,7 @@ public class _05_MatrixShuffling {
             for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
+            
             System.out.println();
         }
     }
