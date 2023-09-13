3bafc5ea1df03ab494372fb82bf0e8e97df29ca7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-13 16:23:00 +0300

    02

--- 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_02_MatrixOfPalindromes.java
+++ 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_02_MatrixOfPalindromes.java
@@ -5,22 +5,35 @@ import java.util.Scanner;
 public class _02_MatrixOfPalindromes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String[] console = scan.nextLine().split("\\s+");
-        int n1 = Integer.parseInt(console[0]);
-        int n2 = Integer.parseInt(console[1]);
-        String[][] matrix = new String[n1][n2];
-        char one = 97;
-        char two = 97;
+
+        // Add variables
+        int matrixRows = Integer.parseInt(console[0]);
+        int matrixCols = Integer.parseInt(console[1]);
+        String[][] matrix = new String[matrixRows][matrixCols];
+        char palindromeCharacter = 97;
+        char middleCharacter = 97;
+
+        // Print a matrix of character palindromes
         for (int i = 0; i < matrix.length; i++) {
+
             for (int j = 0; j < matrix[i].length; j++) {
-                String thisLine = "" + one + two + one;
-                matrix[i][j] = thisLine;
-                two++;
+                String characterPalindrome = "" + palindromeCharacter + middleCharacter + palindromeCharacter;
+
+                matrix[i][j] = characterPalindrome;
+                middleCharacter++;
+
                 System.out.print(matrix[i][j] + " ");
             }
-            one++;
-            two = (char) (97 + i + 1);
+            
+            palindromeCharacter++;
+            middleCharacter = (char) (97 + i + 1);
+            
             System.out.println();
         }
+
+        scan.close();
     }
 }
