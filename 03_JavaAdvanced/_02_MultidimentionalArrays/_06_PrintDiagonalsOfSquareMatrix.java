e15539a547a5d61f1ca28db9b4bb65954bc7df7e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-13 15:29:23 +0300

    06

--- 03_JavaAdvanced/_02_MultidimentionalArrays/_06_PrintDiagonalsOfSquareMatrix.java
+++ 03_JavaAdvanced/_02_MultidimentionalArrays/_06_PrintDiagonalsOfSquareMatrix.java
@@ -6,24 +6,38 @@ import java.util.Scanner;
 public class _06_PrintDiagonalsOfSquareMatrix {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         int rows = Integer.parseInt(scan.nextLine());
+
+        // Read matrix
         int[][] matrix = readMatrix(rows,scan,"\\s+");
+
+        // Add variable
         int row = 0, col = 0;
+
+
         while(row < rows && col < rows){
             int current = matrix[row][col];
             System.out.print(current + " ");
             row++;
             col++;
         }
+
         System.out.println();
+
         row = rows - 1;
         col = 0;
+
+        // Print result
         while(row >= 0 && col < rows){
             int current = matrix[row][col];
             System.out.print(current + " ");
             row--;
             col++;
         }
+
+        scan.close();
     }
     public static int[][] readMatrix(int rows, Scanner scan, String splitPattern){
         int[][] matrix = new int[rows][];
