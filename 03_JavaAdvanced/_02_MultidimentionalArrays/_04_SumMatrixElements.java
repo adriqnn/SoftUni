0d9725357e4d84065c2291bb194a1a5afc65bdf0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-13 15:18:20 +0300

    04

--- 03_JavaAdvanced/_02_MultidimentionalArrays/_04_SumMatrixElements.java
+++ 03_JavaAdvanced/_02_MultidimentionalArrays/_04_SumMatrixElements.java
@@ -6,25 +6,41 @@ import java.util.Scanner;
 public class _04_SumMatrixElements {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String[] line = scan.nextLine().split(", ");
         int rows = Integer.parseInt(line[0]);
         int cols = Integer.parseInt(line[1]);
+
+        // Read the matrix
         int[][] matrix = readMatrix(rows, scan, ", ");
+
+        // Add variable
         int sum = 0;
+
+        // Sum elements of the matrix
         for (int r = 0; r < matrix.length; r++) {
             for (int c = 0; c < matrix[r].length; c++) {
                 sum += matrix[r][c];
             }
         }
+
+        // Print result
         System.out.println(rows);
         System.out.println(cols);
         System.out.println(sum);
+
+        scan.close();
     }
+
+    // Method that reads the matrix
     public static int[][] readMatrix(int rows, Scanner scan, String splitPattern){
         int[][] matrix = new int[rows][];
+        
         for (int i = 0; i < rows; i++) {
             matrix[i] = Arrays.stream(scan.nextLine().split(splitPattern)).mapToInt(Integer::parseInt).toArray();
         }
+        
         return matrix;
     }
 }
