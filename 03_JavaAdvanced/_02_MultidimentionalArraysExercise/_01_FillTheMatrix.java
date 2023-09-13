47507061a202a745af3927f37223646b87d6a10e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-13 16:03:24 +0300

    01

--- 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_01_FillTheMatrix.java
+++ 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_01_FillTheMatrix.java
@@ -5,20 +5,32 @@ import java.util.Scanner;
 public class _01_FillTheMatrix {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
+        
+        // Add variables
         int n = Integer.parseInt(input.split(", ")[0]);
         String pattern = input.split(", ")[1];
         int[][] matrix = new int[n][n];
+
+        // Fill the matrix based on the given pattern
         if(pattern.equals("A")){
             fillMatrixPatternA(matrix);
         }else if(pattern.equals("B")){
             fillMatrixPatternB(matrix);
         }
+
+        // Print result
         printMatrix(matrix);
+
+        scan.close();
     }
 
+    // Method that fills the matrix in pattern B
     private static void fillMatrixPatternB(int[][] matrix) {
         int n = 1;
+
         for (int col = 0; col < matrix.length; col++) {
             if(col%2 == 0){
                 for (int i = 0; i < matrix[col].length; i++) {
@@ -31,14 +43,19 @@ public class _01_FillTheMatrix {
             }
         }
     }
+
+    // Method that fills the matrix in pattern A
     private static void fillMatrixPatternA(int[][] matrix) {
         int n = 1;
+
         for (int col = 0; col < matrix.length; col++) {
             for (int row = 0; row < matrix.length; row++) {
                 matrix[row][col] = n++;
             }
         }
     }
+
+    // Method that prints the matrix
     public static void printMatrix(int[][] matrix){
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
