c82db80cfa9292260990b630832fa36e2ebcd822
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-14 23:29:34 +0300

    02

--- 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_02_SoftUniParty.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_02_SoftUniParty.java
@@ -1,6 +1,5 @@
 package _03_JavaAdvanced._03_SetsAndMapsAdvanced;
 
-
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
@@ -8,28 +7,31 @@ import java.util.TreeSet;
 public class _02_SoftUniParty {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         // Read input value
         String input = scan.nextLine();
-        
+
         // Add variable
         Set<String> guests = new TreeSet<>();
-        
+
+        // Guests arriving
         while(!input.equals("PARTY")){
             guests.add(input);
             input = scan.nextLine();
         }
-        
+
         input = scan.nextLine();
+        
+        // Guests departing
         while(!input.equals("END")){
             guests.remove(input);
             input = scan.nextLine();
         }
-        
+
         // Print result
         System.out.println(guests.size());
-        System.out.println(String.join(System.lineSeparator(),guests));
-        
+        System.out.println(String.join(System.lineSeparator(), guests));
+
         scan.close();
     }
 }
