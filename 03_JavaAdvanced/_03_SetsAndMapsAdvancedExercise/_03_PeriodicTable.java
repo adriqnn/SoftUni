37d07747e5f6dda0a726642b91c9b6ac0f6130c7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 01:15:55 +0300

    03

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_03_PeriodicTable.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_03_PeriodicTable.java
@@ -8,18 +8,30 @@ import java.util.TreeSet;
 public class _03_PeriodicTable {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Set<String> elements = new TreeSet<>();
+
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
+
+        // Add variable
+        Set<String> elements = new TreeSet<>();
+
+        // Unique periodic table elements
         for (int i = 0; i < n; i++) {
             String[] input = scan.nextLine().split("\\s+");
             elements.addAll(Arrays.asList(input));
+
             /*for (int j = 0; j < input.length; j++) {
                 elements.add(input[j]);
             }*/
             /*Collections.addAll(elements,input);*/
         }
+
+        // Print result
         for (String e : elements) {
             System.out.print(e + " ");
         }
+
+        scan.close();
     }
 }
+
