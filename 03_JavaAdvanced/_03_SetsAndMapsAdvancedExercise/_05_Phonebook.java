4a3a56596476a01415db1b430e5163cd7346cb25
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 01:21:48 +0300

    05

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_05_Phonebook.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_05_Phonebook.java
@@ -7,22 +7,36 @@ import java.util.Scanner;
 public class _05_Phonebook {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String phone = scan.nextLine();
-        Map<String,String> phonebook = new HashMap<>();
+
+        // Add variable
+        Map<String, String> phonebook = new HashMap<>();
+
+        // Make phonebook record
         while(!phone.equals("search")){
             String name = phone.split("-")[0];
             String number = phone.split("-")[1];
-            phonebook.put(name,number);
+            
+            phonebook.put(name, number);
+            
             phone = scan.nextLine();
         }
+
         String search = scan.nextLine();
+        
+        // Determine if the entry is in the phonebook
         while(!search.equals("stop")){
             if(phonebook.containsKey(search)){
                 System.out.println(search + " -> " + phonebook.get(search));
             }else{
                 System.out.println("Contact " + search + " does not exist.");
             }
+            
             search = scan.nextLine();
         }
+        
+        scan.close();
     }
 }
