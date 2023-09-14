a07d29df87ad64448316257ef9308ac516d2fec7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 01:39:57 +0300

    06

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_06_FixEmails.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_06_FixEmails.java
@@ -8,20 +8,33 @@ import java.util.Scanner;
 public class _06_FixEmails {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Map<String,String> emailList = new LinkedHashMap<>();
-        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        // Read input value
         String console = scan.nextLine();
+
+        // Add variables
+        Map<String, String> emailList = new LinkedHashMap<>();
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        // Record names/emails
         while(!console.equals("stop")){
             queue.offer(console);
             console = scan.nextLine();
         }
+
+        // Organize the dataset
         while(!queue.isEmpty()){
             String name = queue.poll();
             String email = queue.poll();
+            
             if (!email.toLowerCase().endsWith("us") && !email.toLowerCase().endsWith("uk") && !email.toLowerCase().endsWith("com")){
                 emailList.put(name,email);
             }
         }
+
+        // Print result
         emailList.entrySet().forEach(e -> System.out.println(e.getKey() + " -> " + e.getValue()));
+
+        scan.close();
     }
 }
