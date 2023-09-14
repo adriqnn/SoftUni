c6acc54860eefd0e8d4b6a262496fd967b095303
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-14 19:26:20 +0300

    08

--- 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_08_TheHeiganDance.java
+++ 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_08_TheHeiganDance.java
@@ -6,121 +6,172 @@ import java.util.Scanner;
 public class _08_TheHeiganDance {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        double dmg = Double.parseDouble(scan.nextLine());
+
+        // Add variables
         String position = "7 7";
         double raidHP = 18500;
         double bossHP = 3000000;
-        double dmg = Double.parseDouble(scan.nextLine());
         int dot = 0;
         String finalSpell = "";
+
+        // Boss tactic
         while(raidHP > 0 & bossHP > 0){
             bossHP -= dmg;
+
             if (dot > 0){
                 raidHP -= 3500;
+
                 if(raidHP <= 0){
                     finalSpell = "Plague Cloud";
                 }
+
                 dot = 0;
+
             }
+
             if(raidHP <= 0 || bossHP <= 0){
                 break;
             }
+
             String[] actions = scan.nextLine().split("\\s+");
+
             String spell = actions[0];
             int r = Integer.parseInt(actions[1]);
             int c = Integer.parseInt(actions[2]);
             int[][] room = new int[15][15];
+
             Arrays.stream(room).forEach(e -> Arrays.fill(e,9));
             damageIndexes(r, c, room);
+
             int one = Integer.parseInt(position.split("\\s+")[0]);
             int two = Integer.parseInt(position.split("\\s+")[1]);
+
             if(spell.equals("Cloud")){
                 String newPosition = move(position,room);
+
                 if(newPosition.equals(position) && room[one][two] == -1){
                     raidHP -= 3500;
+
                     if(raidHP <= 0){
                         finalSpell = "Plague Cloud";
                     }
+
                     dot = 1;
+
                 }else{
                     position = newPosition;
                 }
             }else if (spell.equals("Eruption")){
                 String newPosition = move(position,room);
+
                 if(newPosition.equals(position) && room[one][two] == -1){
                     raidHP -= 6000;
+
                     if(raidHP <= 0){
                         finalSpell = "Eruption";
                     }
+
                 }else{
                     position = newPosition;
                 }
             }
         }
+
+        // Print result
         if(bossHP > 0) {
             System.out.printf("Heigan: %.2f%n", bossHP);
         }else{
             System.out.println("Heigan: Defeated!");
         }
+
         if(raidHP > 0) {
-            System.out.printf("Player: %.0f%n",raidHP);
+            System.out.printf("Player: %.0f%n", raidHP);
         }else{
-            System.out.printf("Player: Killed by %s%n",finalSpell);
+            System.out.printf("Player: Killed by %s%n", finalSpell);
         }
-        System.out.printf("Final position: %s, %s%n",position.split("\\s+")[0],position.split("\\s+")[1]);
+
+        System.out.printf("Final position: %s, %s%n", position.split("\\s+")[0], position.split("\\s+")[1]);
+
+        scan.close();
     }
+
+    // Method that moves the raid
     private static String move(String position, int[][] room) {
         String[] positionArray = position.split("\\s+");
         int row = Integer.parseInt(positionArray[0]);
         int col = Integer.parseInt(positionArray[1]);
+
         if (isValidIndex(row-1,col,room) && room[row-1][col] != -1 && room[row][col] == -1){
             int thisUp = row-1;
             return thisUp + " " + col;
         }
+
         if (isValidIndex(row,col+1,room) && room[row][col+1] != -1 && room[row][col] == -1){
             int thisRight = col+1;
             return row + " " + thisRight;
         }
+
         if (isValidIndex(row+1,col,room) && room[row+1][col] != -1 && room[row][col] == -1){
             int thisDown = row+1;
             return thisDown + " " + col;
         }
+
         if (isValidIndex(row,col-1,room) && room[row][col-1] != -1 && room[row][col] == -1){
             int thisLeft = col-1;
             return row + " " + thisLeft;
         }
+
         return row + " " + col;
     }
+
+    // Method that determines if the checked index is valid
     private static boolean isValidIndex(int row, int col, int[][] room) {
         return row >= 0 && row < room.length && col >= 0 && col < room[row].length;
     }
+
+    // Method that determines if the index is damaging
     private static int[][] damageIndexes(int r, int c, int[][] room) {
-        if (isValidIndex(r,c,room)){
+        if (isValidIndex(r, c, room)){
             room[r][c] = -1;
         }
-        if (isValidIndex(r,c-1,room)){
+
+        if (isValidIndex(r, c-1, room)){
             room[r][c-1] = -1;
         }
-        if (isValidIndex(r,c+1,room)){
+
+        if (isValidIndex(r, c+1, room)){
             room[r][c+1] = -1;
         }
-        if (isValidIndex(r-1,c-1,room)){
+
+        if (isValidIndex(r-1, c-1, room)){
             room[r-1][c-1] = -1;
         }
-        if (isValidIndex(r-1,c,room)){
+
+        if (isValidIndex(r-1, c, room)){
             room[r-1][c] = -1;
         }
-        if (isValidIndex(r-1,c+1,room)){
+
+        if (isValidIndex(r-1, c+1, room)){
             room[r-1][c+1] = -1;
         }
-        if (isValidIndex(r+1,c-1,room)){
+
+        if (isValidIndex(r+1, c-1, room)){
             room[r+1][c-1] = -1;
         }
-        if (isValidIndex(r+1,c,room)){
+
+        if (isValidIndex(r+1, c, room)){
             room[r+1][c] = -1;
         }
-        if (isValidIndex(r+1,c+1,room)){
+
+        if (isValidIndex(r+1, c+1, room)){
             room[r+1][c+1] = -1;
         }
+
         return room;
     }
 }
+
+
