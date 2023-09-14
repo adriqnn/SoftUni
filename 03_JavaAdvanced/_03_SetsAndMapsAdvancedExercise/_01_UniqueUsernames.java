964caba1c0b339b7ae260f684a3b59558f2c6494
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 01:08:59 +0300

    01

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_01_UniqueUsernames.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_01_UniqueUsernames.java
@@ -7,12 +7,23 @@ import java.util.Set;
 public class _01_UniqueUsernames {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Set<String> usernames = new LinkedHashSet<>();
+        
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
+        
+        // Add variable
+        Set<String> usernames = new LinkedHashSet<>();
+        
+        // Make database for unique usernames
         for (int i = 0; i < n; i++) {
             String console = scan.nextLine();
+            
             usernames.add(console);
         }
+        
+        // Print result
         usernames.forEach(System.out::println);
+        
+        scan.close();
     }
 }
