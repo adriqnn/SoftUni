3f08641e26c59252f2ecdcea5696d77033d73922
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 02:39:21 +0300

    09

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_09_PopulationCounter.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_09_PopulationCounter.java
@@ -5,12 +5,17 @@ import java.util.Scanner;
 
 public class _09_PopulationCounter {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        // Add variables
         String input = "";
         LinkedHashMap<String , LinkedHashMap<String, Long>> countriesAndCities = new LinkedHashMap<>();
         LinkedHashMap<String, Long> countriesOnly = new LinkedHashMap<>();
-        while(!(input = sc.nextLine()).equals("report")){
+
+        // Population counter
+        while(!(input = scan.nextLine()).equals("report")){
             String [] data = input.split("\\|");
+
             String city = data[0];
             String country = data[1];
             Long population = Long.parseLong(data[2]);
@@ -20,10 +25,13 @@ public class _09_PopulationCounter {
                 countriesOnly.put(country, 0L);
             }
             countriesOnly.put(country,countriesOnly.get(country)+population);
+
             if(!countriesAndCities.get(country).containsKey(city)){
                 countriesAndCities.get(country).put(city,population);
             }
         }
+
+        // Print result
         countriesAndCities.entrySet().stream()
                 .sorted((c1,c2)-> countriesOnly.get(c2.getKey()).compareTo(countriesOnly.get(c1.getKey())))
                 .forEach(country ->{
@@ -33,5 +41,7 @@ public class _09_PopulationCounter {
                                 System.out.format("=>%s: %d\n", city.getKey(),city.getValue());
                             });
                 });
+
+        scan.close();
     }
 }
