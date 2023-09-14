175306e7c0ff78aa9f1e08259d05eb232a74efb4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 00:31:37 +0300

    06

--- 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_06_ProductSop.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_06_ProductSop.java
@@ -1,5 +1,4 @@
 package _03_JavaAdvanced._03_SetsAndMapsAdvanced;
-
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -8,23 +7,37 @@ import java.util.TreeMap;
 public class _06_ProductSop {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input value
         String input = scan.nextLine();
+        
+        // Add variable
         Map<String, Map<String,Double>> shopsWithPrices = new TreeMap<>();
+        
+        //
         while(!input.equals("Revision")){
             String[] params = input.split(", ");
+            
             String shop = params[0];
             String product = params[1];
             double price = Double.parseDouble(params[2]);
+            
             shopsWithPrices.putIfAbsent(shop,new LinkedHashMap<>());
             Map<String,Double> productsAndPrices = shopsWithPrices.get(shop);
+            
             productsAndPrices.put(product,price);
             input = scan.nextLine();
         }
+        
+        // Print result
         for (String shop : shopsWithPrices.keySet()) {
             System.out.println(shop + "->");
+            
             for (var entry : shopsWithPrices.get(shop).entrySet()) {
                 System.out.printf("Product: %s, Price: %.1f%n", entry.getKey(), entry.getValue());
             }
         }
+        
+        scan.close();
     }
 }
