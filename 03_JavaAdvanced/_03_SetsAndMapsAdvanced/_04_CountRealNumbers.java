7e2bc9e6bff8029418c19d6d4eac66fe25eaba63
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 00:40:09 +0300

    04

--- 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_04_CountRealNumbers.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_04_CountRealNumbers.java
@@ -7,7 +7,7 @@ public class _04_CountRealNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input value
+        // Read input values
         List<Double> numbers = Arrays.stream(scan.nextLine().split("\\s+")).map(Double::parseDouble).collect(Collectors.toList());
 
         // Add variable
