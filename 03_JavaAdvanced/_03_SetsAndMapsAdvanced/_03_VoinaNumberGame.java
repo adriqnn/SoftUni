ea17f86cc1a8c244fe87957ddb6c6a45ef1f06ce
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-14 23:43:45 +0300

    03

--- 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_03_VoinaNumberGame.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_03_VoinaNumberGame.java
@@ -6,16 +6,25 @@ import java.util.stream.Collectors;
 public class _03_VoinaNumberGame {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input values
         Set<Integer> firstDeck = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toCollection(LinkedHashSet::new));
         Set<Integer> secondDeck = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toCollection(LinkedHashSet::new));
+        
+        // Add variables
         int rounds = 50;
+        
+        // Play war card game
         while(rounds-- > 0 && !firstDeck.isEmpty() && !secondDeck.isEmpty()){
             Iterator<Integer> firstIterator = firstDeck.iterator();
             Iterator<Integer> secondIterator = secondDeck.iterator();
+            
             int firstCard = firstIterator.next();
             firstIterator.remove();
+            
             int secondCard = secondIterator.next();
             secondIterator.remove();
+            
             if(firstCard > secondCard){
                 firstDeck.add(firstCard);
                 firstDeck.add(secondCard);
@@ -24,6 +33,8 @@ public class _03_VoinaNumberGame {
                 secondDeck.add(secondCard);
             }
         }
+        
+        // Print result
         if(firstDeck.size() > secondDeck.size()){
             System.out.println("First player win!");
         }else if(secondDeck.size() > firstDeck.size()){
@@ -31,5 +42,7 @@ public class _03_VoinaNumberGame {
         }else{
             System.out.println("Draw!");
         }
+        
+        scan.close();
     }
 }
