8482da0068fc25ef36906d52c17a26e9af85ca66
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-14 23:26:42 +0300

    01

--- 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_01_ParkingLot.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_01_ParkingLot.java
@@ -8,25 +8,38 @@ import java.util.stream.Collectors;
 public class _01_ParkingLot {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
+
         Set<String> parkingLot = new LinkedHashSet<>();
+
+        // Make a car record
         while(!input.equals("END")){
             String[] tokens = input.split(", ");
+
             String action = tokens[0];
             String registration = tokens[1];
+
             if(action.equals("IN")){
                 parkingLot.add(registration);
             }else{
                 parkingLot.remove(registration);
             }
+
             input = scan.nextLine();
         }
+
         /*if(parkingLot.isEmpty()){
             System.out.println("Parking Lot is Empty");
         }else{
             parkingLot.stream().collect(Collectors.joining(System.lineSeparator()));
         }*/
+
+        // Print result
         String output = parkingLot.isEmpty() ? "Parking Lot is Empty" : parkingLot.stream().collect(Collectors.joining(System.lineSeparator()));
         System.out.println(output);
+
+        scan.close();
     }
 }
