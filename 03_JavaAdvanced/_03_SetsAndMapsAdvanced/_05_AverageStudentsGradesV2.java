480504c927d8fcb8c5e9e9c650023badd0261f92
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 00:19:46 +0300

    05

---
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvanced/_05_AverageStudentsGradesV2.java
@@ -0,0 +1,36 @@
+package _03_JavaAdvanced._03_SetsAndMapsAdvanced;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class _05_AverageStudentsGradesV2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        int n = Integer.parseInt(scan.nextLine());
+
+        // Add variables
+        Map<String, List<Double>> studentGrades = new TreeMap<>();
+
+        // Determine the average grades of students
+        for (int i = 0; i < n; i++) {
+            String[] input = scan.nextLine().split("\\s+");
+
+            String name = input[0];
+            double grade = Double.parseDouble(input[1]);
+
+            studentGrades.computeIfAbsent(name, k -> new ArrayList<>()).add(grade);
+        }
+
+        studentGrades.forEach((name, grades) -> {
+            double average = grades.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+
+            System.out.printf("%s -> %s (avg: %.2f)%n", name, grades.stream()
+                    .map(Object::toString)
+                    .collect(Collectors.joining(" ")), average);
+        });
+
+        scan.close();
+    }
+}
