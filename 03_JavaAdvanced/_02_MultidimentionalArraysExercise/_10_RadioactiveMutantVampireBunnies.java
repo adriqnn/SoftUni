f862e5fffdaea02467bbbec334e688c9f2aad752
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-14 20:04:22 +0300

    10

--- 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_10_RadioactiveMutantVampireBunnies.java
+++ 03_JavaAdvanced/_02_MultidimentionalArraysExercise/_10_RadioactiveMutantVampireBunnies.java
@@ -6,22 +6,33 @@ import java.util.Scanner;
 public class _10_RadioactiveMutantVampireBunnies {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         String[] dimensions = scan.nextLine().split("\\s+");
+
+        // Add variables
         int row = Integer.parseInt(dimensions[0]);
         int col = Integer.parseInt(dimensions[1]);
         char[][] rabbitDen = new char[row][];
+
+        // Create the matrix
         for (int i = 0; i < rabbitDen.length; i++) {
             rabbitDen[i] = scan.nextLine().replaceAll(" ", "").toCharArray();
         }
+
+        // Create the move-set
         String movement = scan.nextLine();
         ArrayDeque<Character> moves = new ArrayDeque<>();
         for (int i = 0; i < movement.length(); i++) {
             moves.offer(movement.charAt(i));
         }
+
         int player = 1;
         int playerLastR = -10;
         int playerLastL = -10;
         String playerCoordinates = "";
+
+        // Determine player location
         for (int i = 0; i < rabbitDen.length; i++) {
             for (int j = 0; j < rabbitDen[i].length; j++) {
                 if(rabbitDen[i][j] == 'P'){
@@ -29,13 +40,18 @@ public class _10_RadioactiveMutantVampireBunnies {
                 }
             }
         }
+
+        // PLay the game
         while(!moves.isEmpty() && player == 1){
             char move = moves.poll();
+
             int r = Integer.parseInt(playerCoordinates.split("\\s+")[0]);
             int c = Integer.parseInt(playerCoordinates.split("\\s+")[1]);
+
             if(move == 'U'){
                 try {
                     rabbitDen[r][c] = '.';
+
                     if(rabbitDen[r-1][c] != 'B'){
                         rabbitDen[r-1][c] = 'P';
                         int moveUP = r-1;
@@ -45,6 +61,7 @@ public class _10_RadioactiveMutantVampireBunnies {
                         playerLastR = r-1;
                         playerLastL = c;
                     }
+
                 }catch (Exception e){
                     player += 1;
                     playerLastR = r;
@@ -53,6 +70,7 @@ public class _10_RadioactiveMutantVampireBunnies {
             }else if(move == 'D'){
                 try {
                     rabbitDen[r][c] = '.';
+
                     if(rabbitDen[r+1][c] != 'B'){
                         rabbitDen[r+1][c] = 'P';
                         int moveDOWN = r+1;
@@ -62,6 +80,7 @@ public class _10_RadioactiveMutantVampireBunnies {
                         playerLastR = r+1;
                         playerLastL = c;
                     }
+
                 }catch (Exception e){
                     player += 1;
                     playerLastR = r;
@@ -70,6 +89,7 @@ public class _10_RadioactiveMutantVampireBunnies {
             }else if(move == 'L'){
                 try {
                     rabbitDen[r][c] = '.';
+
                     if(rabbitDen[r][c-1] != 'B'){
                         rabbitDen[r][c-1] = 'P';
                         int moveLeft = c-1;
@@ -87,6 +107,7 @@ public class _10_RadioactiveMutantVampireBunnies {
             }else if(move == 'R'){
                 try {
                     rabbitDen[r][c] = '.';
+
                     if(rabbitDen[r][c+1] != 'B'){
                         rabbitDen[r][c+1] = 'P';
                         int moveRight = c+1;
@@ -102,6 +123,8 @@ public class _10_RadioactiveMutantVampireBunnies {
                     playerLastL = c;
                 }
             }
+
+            // Determine the indexes
             ArrayDeque<String> indexes = new ArrayDeque<>();
             for (int i = 0; i < rabbitDen.length; i++) {
                 for (int j = 0; j < rabbitDen[i].length; j++) {
@@ -110,32 +133,41 @@ public class _10_RadioactiveMutantVampireBunnies {
                     }
                 }
             }
+
+            // Determine the outcome based on the provided indexes
             while(!indexes.isEmpty()){
                 String[] indexesForMultiply = indexes.poll().split("\\s+");
+
                 int rr = Integer.parseInt(indexesForMultiply[0]);
                 int cc = Integer.parseInt(indexesForMultiply[1]);
+
                 if(isValid(rr-1,cc,rabbitDen)){
                     if(rabbitDen[rr-1][cc] == 'P'){
                         player -= 1;
                         playerLastR = rr-1;
                         playerLastL = cc;
                     }
+
                     rabbitDen[rr-1][cc] = 'B';
                 }
+
                 if(isValid(rr+1,cc,rabbitDen)){
                     if(rabbitDen[rr+1][cc] == 'P'){
                         player -= 1;
                         playerLastR = rr+1;
                         playerLastL = cc;
                     }
+
                     rabbitDen[rr+1][cc] = 'B';
                 }
+
                 if(isValid(rr,cc-1,rabbitDen)){
                     if(rabbitDen[rr][cc-1] == 'P'){
                         player -= 1;
                         playerLastR = rr;
                         playerLastL = cc-1;
                     }
+
                     rabbitDen[rr][cc-1] = 'B';
                 }
                 if(isValid(rr,cc+1,rabbitDen)){
@@ -144,22 +176,31 @@ public class _10_RadioactiveMutantVampireBunnies {
                         playerLastR = rr;
                         playerLastL = cc+1;
                     }
+
                     rabbitDen[rr][cc+1] = 'B';
                 }
             }
         }
+
+        // Print result
         for (int i = 0; i < rabbitDen.length; i++) {
             for (int j = 0; j < rabbitDen[i].length; j++) {
                 System.out.print(rabbitDen[i][j]);
             }
+
             System.out.println();
         }
+
         if (player == 2){
-            System.out.printf("won: %d %d%n", playerLastR,playerLastL);
+            System.out.printf("won: %d %d%n", playerLastR, playerLastL);
         }else if(player == 0){
-            System.out.printf("dead: %d %d%n",playerLastR,playerLastL);
+            System.out.printf("dead: %d %d%n", playerLastR, playerLastL);
         }
+
+        scan.close();
     }
+
+    // Method that checks if the given index is valid
     private static boolean isValid(int rr, int cc, char[][] rabbitDen) {
         return (rr >= 0 && rr < rabbitDen.length && cc >= 0 && cc < rabbitDen[rr].length);
     }
