d32ffcbcbcce381731958c2b56db8ee7ccc40105
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-15 03:32:58 +0300

    11

--- 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_11_LegendaryFarming.java
+++ 03_JavaAdvanced/_03_SetsAndMapsAdvancedExercise/_11_LegendaryFarming.java
@@ -7,48 +7,62 @@ import static java.util.Collections.reverseOrder;
 public class _11_LegendaryFarming {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input value
+        String input = scan.nextLine();
 
+        // Add variables
         Map<String, Integer> legendaries = new LinkedHashMap<>();
+        Map<String, Integer> garbage = new TreeMap<>();
         legendaries.put("Shadowmourne", 0);
         legendaries.put("Valanyr", 0);
         legendaries.put("Dragonwrath", 0);
-        Map<String, Integer> garbage = new TreeMap<>();
-
-        String input = scan.nextLine();
         boolean collected = false;
 
+        // Make legendary statistics
         while(!collected){
             String[] items = input.split("\\s+");
+            
             for (int i = 0; i < items.length; i += 2) {
                 String item = items[i+1].toLowerCase();
                 int quantity = Integer.parseInt(items[i]);
+                
                 if(item.equals("shards")){
                     int shadowmourne = legendaries.get("Shadowmourne");
+                    
                     if(shadowmourne + quantity >= 250){
                         System.out.println("Shadowmourne obtained!");
                         collected = true;
                         legendaries.put("Shadowmourne", shadowmourne + quantity - 250);
                         break;
                     }
+                    
                     legendaries.put("Shadowmourne", shadowmourne + quantity);
+                    
                 }else if(item.equals("fragments")){
                     int valanyr = legendaries.get("Valanyr");
+                    
                     if(valanyr + quantity >= 250){
                         System.out.println("Valanyr obtained!");
                         collected = true;
                         legendaries.put("Valanyr", valanyr + quantity - 250);
                         break;
                     }
+                    
                     legendaries.put("Valanyr", valanyr + quantity);
+                    
                 }else if(item.equals("motes")){
                     int dragonwrath = legendaries.get("Dragonwrath");
+                    
                     if(dragonwrath + quantity >= 250){
                         System.out.println("Dragonwrath obtained!");
                         collected = true;
                         legendaries.put("Dragonwrath", dragonwrath + quantity - 250);
                         break;
                     }
+                    
                     legendaries.put("Dragonwrath", dragonwrath + quantity);
+                    
                 }else{
                     if(!garbage.containsKey(item)){
                         garbage.put(item, 0);
@@ -57,9 +71,11 @@ public class _11_LegendaryFarming {
                     garbage.put(item, quantity + inBags);
                 }
             }
+            
             if(collected){
                 break;
             }
+            
             input = scan.nextLine();
         }
 
@@ -74,7 +90,10 @@ public class _11_LegendaryFarming {
             }
         });
 
+        // Print result
         map.entrySet().stream().sorted(reverseOrder(Map.Entry.comparingByValue())).forEach(e -> System.out.printf("%s: %d%n",e.getKey(), e.getValue()));
         garbage.entrySet().forEach(e -> System.out.printf("%s: %d%n", e.getKey(), e.getValue()));
+        
+        scan.close();
     }
 }
