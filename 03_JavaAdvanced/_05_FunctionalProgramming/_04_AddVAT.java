56c78669c892136dc3aabe4d7e841987cff750c9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:57:40 +0300

    04

--- 03_JavaAdvanced/_05_FunctionalProgramming/_04_AddVAT.java
+++ 03_JavaAdvanced/_05_FunctionalProgramming/_04_AddVAT.java
@@ -7,14 +7,14 @@ import java.util.function.UnaryOperator;
 public class _04_AddVAT {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         // Unary operator that calculates VAT
         UnaryOperator<Double> vatCalculator = val -> val * 1.20;
-        
+
         // Print result
         System.out.println("Prices with VAT:");
-        Arrays.stream(scan.nextLine().split(", ")).map(Double::parseDouble).map(vatCalculator).forEach(e -> System.out.printf("%.2f%n",e));
-        
+        Arrays.stream(scan.nextLine().split(", ")).map(Double::parseDouble).map(vatCalculator).forEach(e -> System.out.printf("%.2f%n", e));
+
         scan.close();
     }
 }
