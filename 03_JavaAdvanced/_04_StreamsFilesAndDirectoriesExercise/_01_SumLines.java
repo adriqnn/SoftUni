19d4eeb4e23ae0de39dd9069c9fda8dde8c4f4ff
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:17:55 +0300

    01

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_01_SumLines.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_01_SumLines.java
@@ -7,14 +7,22 @@ import java.util.List;
 
 public class _01_SumLines {
     public static void main(String[] args) throws IOException {
+
+        // Path
         String pathStr = "...";
         Path path = Path.of(pathStr);
+
+        // Read file path
         List<String> allLines = Files.readAllLines(path);
+
+        // Sum and print the ASCII values of each line
         allLines.stream().map(String::toCharArray).forEach(charArray -> {
             int sum = 0;
+
             for (char symbol : charArray){
                 sum += symbol;
             }
+
             System.out.println(sum);
         });
     }
