f2ab74653ffc9c778290bbf5a23c8a2be468e9a7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:32:22 +0300

    06

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_06_WordCount.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_06_WordCount.java
@@ -7,21 +7,31 @@ import java.util.*;
 
 public class _06_WordCount {
     public static void main(String[] args) throws IOException {
+
+        // Paths
         Path pathToWords = Path.of("...");
-        LinkedHashMap<String,Integer> wordsCount = new LinkedHashMap<>();
-        List<String> allLinesWords = Files.readAllLines(pathToWords);
-        allLinesWords.forEach(e -> Arrays.stream(e.split("\\s+")).forEach(word -> wordsCount.put(word,0)));
         Path pathText = Path.of("...");
+
+        // Read file paths
+        List<String> allLinesWords = Files.readAllLines(pathToWords);
         List<String> textAllLines = Files.readAllLines(pathText);
+
+        LinkedHashMap<String, Integer> wordsCount = new LinkedHashMap<>();
+        allLinesWords.forEach(e -> Arrays.stream(e.split("\\s+")).forEach(word -> wordsCount.put(word, 0)));
+        
+        // Count how many times the word is contained in the text
         for (String e : textAllLines) {
             String[] wordInLine = e.split("\\s+");
+            
             Arrays.stream(wordInLine).forEach(word -> {
                 if(wordsCount.containsKey(word)){
                     int currentCount = wordsCount.get(word);
-                    wordsCount.put(word,currentCount + 1);
+                    wordsCount.put(word, currentCount + 1);
                 }
             });
         }
+        
+        // Print result
         wordsCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
     }
 }
