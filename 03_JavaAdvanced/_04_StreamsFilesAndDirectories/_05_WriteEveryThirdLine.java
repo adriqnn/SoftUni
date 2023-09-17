f3158b34a5ccc3fe2f3a64205a752b9138d13ac6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:03:56 +0300

    05

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_05_WriteEveryThirdLine.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_05_WriteEveryThirdLine.java
@@ -9,20 +9,30 @@ import java.util.Scanner;
 public class _05_WriteEveryThirdLine {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+        
+        // Path
         String path = "...";
+        
+        // Reader
         FileInputStream inputStream = new FileInputStream(path);
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader reader = new BufferedReader(inputStreamReader);
         //BufferedWriter writer = new BufferedWriter(new PrintWriter());
+        
+        //Read
         String line = reader.readLine();
         int count = 1;
+        
+        // Print every third line of the input file text
         while(line != null){
             if(count % 3 == 0){
                 System.out.println(line);
             }
+            
             line = reader.readLine();
             count++;
         }
+        
         inputStream.close();
         scan.close();
         inputStreamReader.close();
