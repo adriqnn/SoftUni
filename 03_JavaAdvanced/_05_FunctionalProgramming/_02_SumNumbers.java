c9a923d1cfedc71c1982490ebd206d35c05969a2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:54:46 +0300

    02

--- 03_JavaAdvanced/_05_FunctionalProgramming/_02_SumNumbers.java
+++ 03_JavaAdvanced/_05_FunctionalProgramming/_02_SumNumbers.java
@@ -7,11 +7,22 @@ import java.util.function.Function;
 public class _02_SumNumbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
-        //Function<String,Integer> countIntegersInString = str ->{ return str.split(", ").length; };
-        Function<String,Integer> countIntegersInString = str -> str.split(", ").length;
-        Function<String,Integer> sumIntegersInString = str -> Arrays.stream(str.split(", ")).mapToInt(Integer::parseInt).sum();
+        
+        //Function<String, Integer> countIntegersInString = str ->{ return str.split(", ").length; };
+        
+        // Function for counting 
+        Function<String, Integer> countIntegersInString = str -> str.split(", ").length;
+        
+        // Function for sum
+        Function<String, Integer> sumIntegersInString = str -> Arrays.stream(str.split(", ")).mapToInt(Integer::parseInt).sum();
+        
+        // Print result
         System.out.println("Count = " + countIntegersInString.apply(input));
         System.out.println("Sum = " + sumIntegersInString.apply(input));
+        
+        scan.close();
     }
 }
