72a095ab7019984ac2f98f4c8918e1e26243ca91
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:56:11 +0300

    03

--- 03_JavaAdvanced/_05_FunctionalProgramming/_03_CountUppercaseWords.java
+++ 03_JavaAdvanced/_05_FunctionalProgramming/_03_CountUppercaseWords.java
@@ -1,5 +1,6 @@
 package _03_JavaAdvanced._05_FunctionalProgramming;
 
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -9,10 +10,19 @@ import java.util.stream.Collectors;
 public class _03_CountUppercaseWords {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input values
         String[] input = scan.nextLine().split("\\s+");
+        
+        // Predicate(boolean) for checking if a character is upper case
         Predicate<String> predicate = str -> Character.isUpperCase(str.charAt(0));
+        
         List<String> upperCaseStrings = Arrays.stream(input).filter(predicate).collect(Collectors.toList());
+        
+        //Print result
         System.out.println(upperCaseStrings.size());
         System.out.println(String.join(System.lineSeparator(),upperCaseStrings));
+        
+        scan.close();
     }
 }
