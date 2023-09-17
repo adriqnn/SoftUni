f7447d0d3bfbb268d1279453e8a42f821d505945
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:19:38 +0300

    02

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_02_SumBytes.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_02_SumBytes.java
@@ -8,15 +8,25 @@ import java.util.Scanner;
 public class _02_SumBytes {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+
+        // Path
         String pathStr = "...";
+
+        // Read file path
         byte[] allBytes = Files.readAllBytes(Path.of(pathStr));
+
         long sum = 0;
+
+        // Sum all ASCII character values
         for (byte v : allBytes) {
             if(v != 10 && v != 13) {
                 sum += v;
             }
         }
+
+        // Print result
         System.out.println(sum);
+
         scan.close();
     }
 }
