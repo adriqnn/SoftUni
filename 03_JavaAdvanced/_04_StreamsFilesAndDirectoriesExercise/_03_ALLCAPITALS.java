1b152d193a427b2a4afc377e4eaeba8eefee23da
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:21:47 +0300

    03

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_03_ALLCAPITALS.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_03_ALLCAPITALS.java
@@ -8,12 +8,19 @@ import java.util.Scanner;
 public class _03_ALLCAPITALS {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+        
+        // Path
         String pathStr = "...";
+        
+        // Read file path
         Files.readAllLines(Path.of(pathStr)).forEach(e -> System.out.println(e.toUpperCase()));
         //BufferedWriter writer = new BufferedWriter(new FileWriter(name));
+        
+        // Print result
         //writer.write(name) with try/catch and e.printStackTrace;
         //writer.newLine();
         //writer.close();
+        
         scan.close();
     }
 }
