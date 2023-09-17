dfaa767229e602be381f23a695704387d2b7c7e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:24:57 +0300

    05

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_05_LineNumbers.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_05_LineNumbers.java
@@ -9,13 +9,23 @@ import java.util.Scanner;
 public class _05_LineNumbers {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+
+        // Path
         String pathStr = "...";
+
+        // Read file path
         List<String> allLines = Files.readAllLines(Path.of(pathStr));
+
+        //Print result
         //PrintWriter writer = new PrintWriter(name);
+
         int count = 1;
+
+        // Print numbered lines from the file
         for (String e : allLines) {
             System.out.println(count++ + ". " + e);
         }
+
         //writer.close();
         scan.close();
     }
