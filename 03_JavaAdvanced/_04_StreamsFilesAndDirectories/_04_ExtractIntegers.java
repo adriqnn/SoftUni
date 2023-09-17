df020b3e9e056049f779ddc90ec20ca77eacdda8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:02:04 +0300

    04

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_04_ExtractIntegers.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_04_ExtractIntegers.java
@@ -6,17 +6,26 @@ import java.util.Scanner;
 
 public class _04_ExtractIntegers {
     public static void main(String[] args) throws IOException {
+
+        // Path
         String path = "...";
+
+        // Reader
         FileInputStream inputStream = new FileInputStream(path);
         Scanner scan = new Scanner(inputStream);
         //PrintOutputStream out = new PrintStream(...);
+
+        // Extract only the integers from the input file
         while(scan.hasNext()){
             if(scan.hasNextInt()){
                 int number = scan.nextInt();
+
                 System.out.println(number);
             }
+
             scan.next();
         }
+
         inputStream.close();
         scan.close();
     }
