22452272f205e4018f0cedce72d89fdd87a822ce
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:11:51 +0300

    08

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_08_NestedFolders.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_08_NestedFolders.java
@@ -8,23 +8,36 @@ import java.util.Scanner;
 public class _08_NestedFolders {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+
+        // Path
         String path = "...";
+
+        // File
         File root = new File(path);
+
+        // Add variable
         ArrayDeque<File> dirs = new ArrayDeque<>();
         dirs.offer(root);
         int count = 0;
+
+        // Print all files
         while(!dirs.isEmpty()){
             File current = dirs.poll();
             File[] nestedFiles = current.listFiles();
+
             for(File e : nestedFiles){
                 if (e.isDirectory()){
                     dirs.offer(e);
                 }
             }
             count++;
+
             System.out.println(current.getName());
         }
+
+        // Print result
         System.out.println(count + " folders");
+
         scan.close();
     }
 }
