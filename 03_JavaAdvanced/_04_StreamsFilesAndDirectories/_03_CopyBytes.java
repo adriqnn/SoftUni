989fc2b5522fe1b675a04b2ad6580cb14eee2190
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:00:30 +0300

    03

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_03_CopyBytes.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_03_CopyBytes.java
@@ -7,19 +7,25 @@ import java.util.Scanner;
 public class _03_CopyBytes {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
-        String path = "...";
-        FileInputStream inputStream = new FileInputStream(path);
 
+        // Path
+        String path = "...";
+
+        // Reader
+        FileInputStream inputStream = new FileInputStream(path);
         int value = inputStream.read();
 
+        // Print ASCII values of the input text from file
         while(value != -1){
             if(value != 10 && value != 32){
                 System.out.print(value);
             }else{
                 System.out.print((char) value);
             }
+
             value = inputStream.read();
         }
+
         inputStream.close();
         scan.close();
     }
