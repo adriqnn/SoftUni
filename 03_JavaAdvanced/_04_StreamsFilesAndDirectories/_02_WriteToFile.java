7ee680152a51b0280d0ac75a6d1be108dcca203f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 03:58:35 +0300

    02

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_02_WriteToFile.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_02_WriteToFile.java
@@ -7,21 +7,31 @@ import java.util.Set;
 
 public class _02_WriteToFile {
     public static void main(String[] args) throws IOException {
-        String path = "...";
-        FileInputStream inputStream = new FileInputStream(path);
         Scanner scan = new Scanner(System.in);
+
+        // Path
+        String path = "...";
+
+        // Reader
+        FileInputStream inputStream = new FileInputStream(path);
         /*while(scan.hasNextLine()) {
             String line = scan.nextLine();
         }*/
         int value = inputStream.read();
-        Set<Character> punctuation = Set.of(',','.','!','?');
+
+        Set<Character> punctuation = Set.of(',', '.', '!', '?');
+
+        // Print the input text without punctuation signs
         while(value != -1){
             char current = (char) value;
+            
             if(!punctuation.contains(current)){
                 System.out.print(current);
             }
+            
             value = inputStream.read();
         }
+
         inputStream.close();
         scan.close();
     }
