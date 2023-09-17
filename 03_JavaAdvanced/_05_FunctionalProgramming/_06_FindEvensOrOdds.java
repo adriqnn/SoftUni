4e51fbd766c86fc69763965af3efcf2b105b6d00
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 05:14:36 +0300

    06

--- 03_JavaAdvanced/_05_FunctionalProgramming/_06_FindEvensOrOdds.java
+++ 03_JavaAdvanced/_05_FunctionalProgramming/_06_FindEvensOrOdds.java
@@ -9,9 +9,15 @@ import java.util.stream.IntStream;
 public class _06_FindEvensOrOdds {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input values
         int[] range = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
         String condition = scan.nextLine();
+        
+        // Print result
         printNumbersIn(range[0], range[1], condition.equals("even") ? n -> n % 2 == 0: n -> n % 2 != 0);
+        
+        scan.close();
     }
     private static void printNumbersIn(int beginInclusive, int endInclusive, Predicate<Integer> predicate){
         System.out.println(IntStream.rangeClosed(beginInclusive, endInclusive).boxed().filter(predicate).map(String::valueOf).collect(Collectors.joining(" ")));
