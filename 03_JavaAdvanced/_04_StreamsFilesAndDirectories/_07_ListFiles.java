bcb8d64ef9e784b432ab6de0b035d82a6b074814
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:09:21 +0300

    07

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_07_ListFiles.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_07_ListFiles.java
@@ -6,34 +6,46 @@ import java.util.Scanner;
 public class _07_ListFiles {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // File path
         File file = new File("...");
+        
+        // List all files in the directory
         if(file.exists()){
             if(file.isDirectory()){
                 File[] files = file.listFiles();
+                
                 for (File f : files) {
                     if(!f.isDirectory()){
-                        System.out.printf("%s: [%s]%n",f.getName(),f.length());
+                        System.out.printf("%s: [%s]%n", f.getName(), f.length());
                     }
                 }
             }
         }
+        
         scan.close();
     }
 }
+
+
 /*public class ListFiles_07 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         File file = new File("");
+        
         if(file.exists()){
             if(file.isDirectory()){
                 File[] files = file.listFiles();
+                
                 for (File f : files) {
                     if(!f.isDirectory()){
-                        System.out.printf("%s: [%s]%n",f.getName(),f.length());
+                        System.out.printf("%s: [%s]%n", f.getName(), f.length());
                     }
                 }
             }
         }
+        
         scan.close();
     }
 }
