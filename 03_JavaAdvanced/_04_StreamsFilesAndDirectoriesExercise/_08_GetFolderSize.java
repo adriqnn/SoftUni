5c97eeb77a3608475e2968434956a677bf23686e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 04:38:38 +0300

    08

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_08_GetFolderSize.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectoriesExercise/_08_GetFolderSize.java
@@ -8,13 +8,22 @@ import java.util.List;
 
 public class _08_GetFolderSize {
     public static void main(String[] args) {
+        
+        // Path
         String path = "...";
+        
+        // Folders
         File folder = new File(path);
         File[] filesInFolder = folder.listFiles();
+        
         int folderSize = 0;
+        
+        // Get folder size
         for (File e : filesInFolder) {
             folderSize += e.length();
         }
+        
+        // Print result
         System.out.println("Folder size: " + folderSize);
     }
 }
