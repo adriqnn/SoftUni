686ae1f37c5d9191b0eab25fa9d61b5f06426119
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-17 03:54:57 +0300

    01

--- 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_01_ReadFile.java
+++ 03_JavaAdvanced/_04_StreamsFilesAndDirectories/_01_ReadFile.java
@@ -7,13 +7,21 @@ import java.util.Scanner;
 public class _01_ReadFile {
     public static void main(String[] args) throws IOException {
         Scanner scan = new Scanner(System.in);
+        
+        // Path
         String path = "...";
+        
+        // Reader
         FileInputStream inputStream = new FileInputStream(path);
         int value = inputStream.read();
+        
+        // Read file from the system and print binary
         while(value != -1){
             System.out.print(Integer.toBinaryString(value) + " ");
+            
             value = inputStream.read();
         }
+        
         inputStream.close();
         scan.close();
     }
