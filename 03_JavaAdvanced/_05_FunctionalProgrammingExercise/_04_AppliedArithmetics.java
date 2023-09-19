856440d7994072c4995747a179f3604a37f2eb25
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:40:39 +0300

    04

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_04_AppliedArithmetics.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_04_AppliedArithmetics.java
@@ -8,31 +8,46 @@ import java.util.function.Function;
 public class _04_AppliedArithmetics {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
         int[] numbers = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
+        // Create math functions and a consumer
         Function<int[],int[]> add = a -> Arrays.stream(a).map(number -> number += 1).toArray();
         Function<int[],int[]> multiply = a -> Arrays.stream(a).map(number -> number *= 2).toArray();
         Function<int[],int[]> subtract = a -> Arrays.stream(a).map(number -> number -= 1).toArray();
         Consumer<int[]> print = a -> Arrays.stream(a).forEach(number -> System.out.print(number + " "));
+
+        // Read commands
         String command = scan.nextLine();
+
+        // Execute math commands
         while(!command.equals("end")){
             switch(command){
                 case "add":
                     numbers = add.apply(numbers);
+
                     break;
                 case "multiply":
                     numbers = multiply.apply(numbers);
+
                     break;
                 case "subtract":
                     numbers = subtract.apply(numbers);
+
                     break;
                 case "print":
                     print.accept(numbers);
+
                     System.out.println();
+
                     break;
             }
+
             command = scan.nextLine();
         }
+
+        scan.close();
     }
 }
