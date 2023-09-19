5a5bb53f3115254102a5ff3049b48807c409a1fc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:35:52 +0300

    03

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_03_CustomMinFunction.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_03_CustomMinFunction.java
@@ -11,11 +11,20 @@ import java.util.stream.Collectors;
 public class _03_CustomMinFunction {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
+
+        // Input values in Integer list
         List<Integer> numbers = Arrays.stream(input.split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        // Function for extracting the min number
         Function<List<Integer>, Integer> getMinNumber = Collections::min;
+
+        // Print result
         System.out.println(getMinNumber.apply(numbers));
 
+        // Other Solution
 //        Consumer<List<Integer>> print = list -> System.out.println(Collections.min(list));
 //        print.accept(numbers);
 
