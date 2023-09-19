1a39540b88dbf15f9a887626ccd105741808fcf8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:54:39 +0300

    07

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_07_FindTheSmallestElement.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_07_FindTheSmallestElement.java
@@ -11,12 +11,22 @@ import java.util.stream.Collectors;
 public class _07_FindTheSmallestElement {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         List<Integer> numbers = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
+
+        // Consumer that prints the smallest right-most integer
         Consumer<List<Integer>> printMinIndexOfMinElement = list -> {
             int min = Collections.min(list);
+
             System.out.println(list.lastIndexOf(min));
         };
+
+        // Print result
         printMinIndexOfMinElement.accept(numbers);
+
+        scan.close();
+
 //        Function<List<Integer>,Integer> getIndexOfMinElement = list -> list.lastIndexOf(Collections.min(list));
 //        System.out.println(getIndexOfMinElement.apply(numbers));
     }
