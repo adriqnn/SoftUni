ca161543643ac692aad3235e238e2532cea0ba47
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:46:41 +0300

    06

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_06_PredicateForNames.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_06_PredicateForNames.java
@@ -7,9 +7,17 @@ import java.util.function.Predicate;
 public class _06_PredicateForNames {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         int n = Integer.parseInt(scan.nextLine());
-        Predicate<String> validLength = name -> name.length() <= n;
         String[] names = scan.nextLine().split("\\s+");
+
+        // Predicate to check for a name length
+        Predicate<String> validLength = name -> name.length() <= n;
+
+        // Print result
         Arrays.stream(names).filter(validLength).forEach(System.out::println);
+
+        scan.close();
     }
 }
