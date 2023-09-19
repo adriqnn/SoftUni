21325c509a18823c55617bb01fe6e560479db221
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 22:04:50 +0300

    10

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_10_PredicateParty.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_10_PredicateParty.java
@@ -10,15 +10,20 @@ import java.util.stream.Collectors;
 public class _10_PredicateParty {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        List<String> invited = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toCollection(ArrayList::new));
 
+        // Read input values
+        List<String> invited = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toCollection(ArrayList::new));
         String console = scan.nextLine();
+
+        // Make a party list
         while(!console.equals("Party!")){
             Predicate<String> predicate = null;
             String[] commandLine = console.split("\\s+");
+
             String first = commandLine[0];
             String second = commandLine[1];
             String third = commandLine[2];
+
             if(console.contains("Length")){
                 predicate = x -> x.length() == Integer.parseInt(third);
             }else if(console.contains("StartsWith")){
@@ -26,29 +31,40 @@ public class _10_PredicateParty {
             }else{
                 predicate = x -> x.endsWith(third);
             }
+
             if(console.contains("Remove")){
                 ArrayList<String> toRemove = new ArrayList<>();
+
                 for (String guest : invited) {
                     if(predicate.test(guest)){
                         toRemove.add(guest);
                     }
                 }
+
                 invited.removeAll(toRemove);
+
             }else{
                 ArrayList<String> toAdd = new ArrayList<>();
+
                 for (String guest : invited) {
                     if(predicate.test(guest)){
                         toAdd.add(guest);
                     }
                 }
+
                 invited.addAll(toAdd);
             }
+
             console = scan.nextLine();
         }
+
+        // Print result
         if(invited.isEmpty()){
             System.out.println("Nobody is going to the party!");
         }else{
             System.out.println(invited.stream().sorted().collect(Collectors.toCollection(ArrayList::new)).toString().replaceAll("([\\[\\]])","") + " are going to the party!");
         }
+
+        scan.close();
     }
 }
