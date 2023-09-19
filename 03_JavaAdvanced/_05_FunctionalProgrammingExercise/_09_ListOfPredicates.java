5e4d62cc8f0c0207be68874778ee5ce51e4fe865
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 22:00:17 +0300

    09

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_09_ListOfPredicates.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_09_ListOfPredicates.java
@@ -9,19 +9,31 @@ import java.util.stream.Collectors;
 public class _09_ListOfPredicates {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input values
         int n = Integer.parseInt(scan.nextLine());
+
+        // Boxed
         List<Integer> numbersForDivision = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
+
+        // BiFunction that finds all numbers divisible by given output
         BiFunction<List<Integer>, Integer, Boolean> isDivisible = ((list, number) -> {
             for (int integer : list) {
                 if (number % integer != 0) {
                     return false;
                 }
-            }return true;
+            }
+
+            return true;
         });
+
+        // Print result
         for (int i = 1; i <= n; i++) {
             if(isDivisible.apply(numbersForDivision,i)){
                 System.out.print(i + " ");
             }
         }
+
+        scan.close();
     }
 }
