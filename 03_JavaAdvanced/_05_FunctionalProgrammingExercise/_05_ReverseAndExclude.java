64478e686460995600f986f612ef0df477d2a242
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:44:02 +0300

    05

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_05_ReverseAndExclude.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_05_ReverseAndExclude.java
@@ -10,13 +10,24 @@ import java.util.stream.Collectors;
 public class _05_ReverseAndExclude {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
+
+        // Add variables
         List<Integer> numbers = Arrays.stream(input.split("\\s+")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         int n = Integer.parseInt(scan.nextLine());
+
         Collections.reverse(numbers);
+
+        // Predicate to match the given condition for division
         Predicate<Integer> checkDivision = number -> number % n == 0;
         numbers.removeIf(number -> number % n == 0);
         numbers.removeIf(checkDivision);
+
+        // Print result
         numbers.forEach(number -> System.out.print(number + " "));
+
+        scan.close();
     }
 }
