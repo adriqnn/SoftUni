a21c090a80dbc69514c3c495a881bf3aa117cbcd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-19 21:30:31 +0300

    02

--- 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_02_KnightsOfHonor.java
+++ 03_JavaAdvanced/_05_FunctionalProgrammingExercise/_02_KnightsOfHonor.java
@@ -7,9 +7,19 @@ import java.util.function.Consumer;
 public class _02_KnightsOfHonor {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        // Read input value
         String input = scan.nextLine();
+
+        // Input values
         String[] names = input.split("\\s+");
+        
+        // Printer consumer
         Consumer<String> printName = name -> System.out.println("Sir " + name);
+
+        // Print result
         Arrays.stream(names).forEach(printName);
+
+        scan.close();
     }
 }
