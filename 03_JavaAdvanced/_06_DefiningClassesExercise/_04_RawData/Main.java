9d8494eb8b458cf530b414bf433bbe8ee3a2b350
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:55:03 +0300

    04

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_04_RawData/Main.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_04_RawData/Main.java
@@ -11,8 +11,10 @@ public class Main {
         int n = Integer.parseInt(scan.nextLine());
 
         List<Car> carList = new ArrayList<>();
+        
         for (int i = 0; i < n; i++) {
             String[] console = scan.nextLine().split("\\s+");
+            
             String model = console[0];
             int engineHP = Integer.parseInt(console[2]);
             String cargoType = console[4];
@@ -20,9 +22,11 @@ public class Main {
             double tire2Pressure = Double.parseDouble(console[7]);
             double tire3Pressure = Double.parseDouble(console[9]);
             double tire4Pressure = Double.parseDouble(console[11]);
+            
             Car newCar = new Car(model,engineHP,cargoType,tire1Pressure,tire2Pressure,tire3Pressure,tire4Pressure);
             carList.add(newCar);
         }
+        
         String cargo = scan.nextLine();
 
         if(cargo.equals("fragile")){
@@ -38,5 +42,7 @@ public class Main {
                 }
             }
         }
+        
+        scan.close();
     }
 }
