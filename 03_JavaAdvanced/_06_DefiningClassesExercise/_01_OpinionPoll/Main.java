2ae74550388050f61e5ace187b94873eb9259379
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:39:29 +0300

    01

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_01_OpinionPoll/Main.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_01_OpinionPoll/Main.java
@@ -8,21 +8,30 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int n = Integer.parseInt(scan.nextLine());
+
         List<Persons> listOfPeople = new ArrayList<>();
+
         for (int i = 0; i < n; i++) {
             String[] consoleLine = scan.nextLine().split("\\s+");
+
             String person = consoleLine[0];
             int age = Integer.parseInt(consoleLine[1]);
-            Persons newOne = new Persons(consoleLine[0],Integer.parseInt(consoleLine[1]));
+
+            Persons newOne = new Persons(consoleLine[0], Integer.parseInt(consoleLine[1]));
             listOfPeople.add(newOne);
         }
+
         listOfPeople.sort(Comparator.comparing(Persons::getPerson));
         listOfPeople.forEach(e -> {
             if(e.getAge() > 30) {
                 System.out.println(e.getPerson() + " - " + e.getAge());
             }
         });
-        //listOfPeople.stream().filter(p -> p.getAge() > 30).sorted((person1,person2) -> person1.getPerson().compareTo(person2.getPerson())).forEach(System.out::println));
+
+        //listOfPeople.stream().filter(p -> p.getAge() > 30).sorted((person1, person2) -> person1.getPerson().compareTo(person2.getPerson())).forEach(System.out::println));
+
+        scan.close();
     }
 }
