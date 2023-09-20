b016e29e5f253144104ef26cdbb5c88fb46b7101
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:26:25 +0300

    01

--- 03_JavaAdvanced/_06_DefiningClasses/_01_CarInfo/Main.java
+++ 03_JavaAdvanced/_06_DefiningClasses/_01_CarInfo/Main.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         int n = Integer.parseInt(scan.nextLine());
-        
+
         ArrayList<CarInfo> cars = new ArrayList<>();
-        
+
         for (int i = 0; i < n; i++) {
             String[] carLine = scan.nextLine().split("\\s+");
-            
-            CarInfo car = new CarInfo(carLine[0], carLine[1], Integer.parseInt(carLine[2]));
+
+            CarInfo car = new CarInfo(carLine[0],carLine[1],Integer.parseInt(carLine[2]));
             cars.add(car);
         }
-        
+
         cars.forEach(System.out::println);
+        
+        scan.close();
     }
 }
+
