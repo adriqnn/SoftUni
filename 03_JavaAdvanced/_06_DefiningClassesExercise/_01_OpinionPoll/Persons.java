b356562874a6edb67f6226940983c7c35a01ee26
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:38:24 +0300

    01

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_01_OpinionPoll/Persons.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_01_OpinionPoll/Persons.java
@@ -3,16 +3,20 @@ package _03_JavaAdvanced._06_DefiningClassesExercise._01_OpinionPoll;
 public class Persons {
     private String person;
     private int age;
+    
     public Persons(String person, int age) {
         this.person = person;
         this.age = age;
     }
+    
     public int getAge() {
         return age;
     }
+    
     public String getPerson() {
         return person;
     }
+    
     @Override
     public String toString(){
         return String.format("%s - %d",this.person,this.age);
