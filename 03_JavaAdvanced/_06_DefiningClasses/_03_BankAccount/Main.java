8e32adf3d01114323093564efd84ee7b9c41a207
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:31:38 +0300

    03

--- 03_JavaAdvanced/_06_DefiningClasses/_03_BankAccount/Main.java
+++ 03_JavaAdvanced/_06_DefiningClasses/_03_BankAccount/Main.java
@@ -7,29 +7,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        Map<Integer,BankAccount> bankAccounts = new HashMap<>();
+
         String command = scan.nextLine();
+
+        Map<Integer, BankAccount> bankAccounts = new HashMap<>();
+
         while(!command.equals("End")){
             String output = null;
             String[] tokens = command.split("\\s+");
+
             if(tokens[0].equals("Create")){
                 BankAccount account = new BankAccount();
                 bankAccounts.put(account.getId(),account);
+
                 output = "Account ID" + account.getId() + " created";
+
             }else if(tokens[0].equals("Deposit")){
                 int id = Integer.parseInt(tokens[1]);
                 int amount = Integer.parseInt(tokens[2]);
+
                 if(!bankAccounts.containsKey(id)){
                     output = "Account does not exist";
                 }else{
                     bankAccounts.get(id).deposit(amount);
                     output = "Deposited " + amount + " to ID" + id;
                 }
+
             }else if(tokens[0].equals("SetInterest")){
                 BankAccount.setInterestRate(Double.parseDouble(tokens[1]));
             }else{
                 int id = Integer.parseInt(tokens[1]);
                 int years = Integer.parseInt(tokens[2]);
+
                 if(!bankAccounts.containsKey(id)){
                     output = "Account does not exist";
                 }else{
@@ -37,10 +46,14 @@ public class Main {
                     output = String.format("%.2f", interest);
                 }
             }
+
             if(output != null){
                 System.out.println(output);
             }
+
             command = scan.nextLine();
         }
+
+        scan.close();
     }
 }
