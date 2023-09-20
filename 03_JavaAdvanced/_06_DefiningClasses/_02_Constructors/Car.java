8fe3f6b278bb928e8758df0d012fda947839f4cb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:25:51 +0300

    02

--- 03_JavaAdvanced/_06_DefiningClasses/_02_Constructors/Car.java
+++ 03_JavaAdvanced/_06_DefiningClasses/_02_Constructors/Car.java
@@ -12,11 +12,11 @@ public class Car {
     }
     
     public Car(String brand) {
-        this(brand,"unknown",-1);
+        this(brand, "unknown", -1);
     }
     
     @Override
     public String toString(){
-        return String.format("The car is: %s %s - %d HP.",this.brand,this.model,this.hp);
+        return String.format("The car is: %s %s - %d HP.", this.brand, this.model, this.hp);
     }
 }
