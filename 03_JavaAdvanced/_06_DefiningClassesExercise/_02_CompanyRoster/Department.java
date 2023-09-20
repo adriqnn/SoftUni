f765e747eee480a3f146694e2169c05f6149c8ad
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-20 19:48:31 +0300

    02

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_02_CompanyRoster/Department.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_02_CompanyRoster/Department.java
@@ -6,16 +6,20 @@ import java.util.List;
 public class Department {
     private List<Employee> employees;
     private String name;
+    
     public Department(String name){
         this.name = name;
         employees = new ArrayList<>();
     }
+    
     public List<Employee> getEmployees() {
         return employees;
     }
+    
     public double calculateAverageSalary(){
         return  this.employees.stream().mapToDouble(Employee::getSalary).average().orElse(0.0);
     }
+    
     public String getName() {
         return name;
     }
