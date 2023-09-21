b5c51eb4f454109ad01f71ef4838e9fd911a7cb3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-21 20:44:28 +0300

    05

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Car.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Car.java
@@ -12,36 +12,39 @@ public class Car {
         this.weight = 0;
         this.color = "n/a";
     }
-    
+
     public Car(String model, Engine engine, int weight, String color) {
         this(model, engine);
         this.weight = weight;
         this.color = color;
     }
-    
+
     public Car(String model, Engine engine, int weight) {
         this(model, engine);
         this.weight = weight;
     }
-    
+
     public Car(String model, Engine engine, String color) {
         this(model, engine);
         this.color = color;
     }
-    
+
     @Override
     public String toString(){
         String printWeight;
+        
         if(weight == 0){
             printWeight = "n/a";
         }else{
             printWeight = weight + "";
         }
+        
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append(model).append(":").append("\n")
                 .append(engine.toString()).append("\n")
                 .append("Weight: ").append(printWeight).append("\n")
                 .append("Color: ").append(color);
+        
         return stringBuilder.toString();
     }
 }
