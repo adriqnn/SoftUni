21cac2c98dec3624dfe4ab076cde682e1573ebcc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-21 20:45:09 +0300

    05

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Engine.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Engine.java
@@ -3,7 +3,6 @@ package _03_JavaAdvanced._06_DefiningClassesExercise._05_CarSalesman;
 public class Engine {
     private String model;
     private int power;
-
     private int displacement;
     private String efficiency;
 
@@ -13,19 +12,23 @@ public class Engine {
         this.displacement = 0;
         this.efficiency = "n/a";
     }
+    
     public Engine(String model, int power, int displacement, String efficiency) {
         this(model,power);
         this.displacement = displacement;
         this.efficiency = efficiency;
     }
+    
     public Engine(String model, int power, int displacement) {
         this(model,power);
         this.displacement = displacement;
     }
+    
     public Engine(String model, int power, String efficiency) {
         this(model,power);
         this.efficiency = efficiency;
     }
+    
     @Override
     public String toString(){
         String displacementPrint;
