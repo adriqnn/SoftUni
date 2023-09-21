261482c9bef9768be8f2cf749531941a4dd27ebb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-21 21:43:55 +0300

    07

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Person.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Person.java
@@ -5,12 +5,12 @@ import java.util.List;
 import java.util.Map;
 
 public class Person {
-    private String name;//unique
-    private Company company;//unique
-    private Map<String, List<Pokemon>> pokemon;//multiple
-    private Map<String,List<Parents>> parents;//multiple
-    private Map<String,List<Children>> children;//multiple
-    private Car car;//unique
+    private String name; //unique
+    private Company company; //unique
+    private Map<String, List<Pokemon>> pokemon; //multiple
+    private Map<String, List<Parents>> parents; //multiple
+    private Map<String, List<Children>> children; //multiple
+    private Car car; //unique
 
     public Person(String name) {
         this.name = name;
@@ -24,26 +24,33 @@ public class Person {
     public String getName() {
         return name;
     }
+
     public Company getCompany() {
         return company;
     }
-    public Map<String,List<Pokemon>> getPokemon() {
+
+    public Map<String, List<Pokemon>> getPokemon() {
         return pokemon;
     }
-    public Map<String,List<Parents>>getParents() {
+
+    public Map<String, List<Parents>>getParents() {
         return parents;
     }
-    public Map<String,List<Children>> getChildren() {
+
+    public Map<String, List<Children>> getChildren() {
         return children;
     }
+
     public Car getCar() {
         return car;
     }
+
     @Override
     public String toString(){
-        String name1 = this.name + "\n";
-        String company2 = this.company.toString();
-        String car6 = this.car.toString();
-        return name1 + company2 + car6;
+        String nameString = this.name + "\n";
+        String companyString = this.company.toString();
+        String carString = this.car.toString();
+        
+        return nameString + companyString + carString;
     }
 }
