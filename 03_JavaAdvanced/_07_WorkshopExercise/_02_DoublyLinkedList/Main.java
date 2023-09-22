74cd81a9c180d3158ed2f0931851ced3343a0c3e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-22 23:20:33 +0300

    02

--- 03_JavaAdvanced/_07_WorkshopExercise/_02_DoublyLinkedList/Main.java
+++ 03_JavaAdvanced/_07_WorkshopExercise/_02_DoublyLinkedList/Main.java
@@ -2,6 +2,6 @@ package _03_JavaAdvanced._07_WorkshopExercise._02_DoublyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test DoublyLinkedList and Node
     }
 }
