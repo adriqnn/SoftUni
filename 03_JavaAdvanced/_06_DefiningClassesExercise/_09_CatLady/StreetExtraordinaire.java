b1f0bc4223cbd661591162288b02ce18f048b62c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-22 23:03:32 +0300

    09

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_09_CatLady/StreetExtraordinaire.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_09_CatLady/StreetExtraordinaire.java
@@ -7,6 +7,7 @@ public class StreetExtraordinaire extends Cat {
         super(name);
         this.decibelsOfMeows = decibelsOfMeows;
     }
+    
     @Override
     public String toString() {
         return String.format("%s %s %.2f", this.getClass().getSimpleName(), super.getName(), this.decibelsOfMeows);
