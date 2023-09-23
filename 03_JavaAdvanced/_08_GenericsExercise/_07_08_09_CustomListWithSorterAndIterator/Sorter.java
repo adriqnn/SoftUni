58ea592a72b874390b168ffa2389df6fd75f52d0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-24 01:41:53 +0300

    08

--- 03_JavaAdvanced/_08_GenericsExercise/_07_08_09_CustomListWithSorterAndIterator/Sorter.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_07_08_09_CustomListWithSorterAndIterator/Sorter.java
@@ -4,10 +4,12 @@ public class Sorter {
     public static <T extends Comparable<T>> void sort(CustomList<T> customList){
         for (int i = 0; i < customList.size(); i++) {
             T element = customList.get(i);
+            
             for (int j = i+1; j < customList.size(); j++) {
                 T nextElement = customList.get(j);
+                
                 if(element.compareTo(nextElement) > 0){
-                    customList.swap(i,j);
+                    customList.swap(i, j);
                 }
             }
         }
