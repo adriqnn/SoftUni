dd1711fe7fb5d462a0b4f444c156f6bf1d6074b3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 21:41:15 +0300

    02

--- 03_JavaAdvanced/_08_Generics/_02_GenericArrayCreator/ArrayCreator.java
+++ 03_JavaAdvanced/_08_Generics/_02_GenericArrayCreator/ArrayCreator.java
@@ -7,13 +7,19 @@ public class ArrayCreator {
     public static<T> T[] create(int length,T def){
         /*T[] arr = ((T[])new Object[length]);
         return arr;*/
+        
         T[] arr = (T[]) Array.newInstance(def.getClass(), length);
-        Arrays.fill(arr,def);
+        
+        Arrays.fill(arr, def);
+        
         return arr;
     }
+    
     public static<T> T[] create(Class<T> clazz,int length,T def){
         T[] arr = (T[])Array.newInstance(clazz, length);
-        Arrays.fill(arr,def);
+        
+        Arrays.fill(arr, def);
+        
         return arr;
     }
 }
