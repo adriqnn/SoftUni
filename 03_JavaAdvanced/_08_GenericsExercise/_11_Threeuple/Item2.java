0914ef956ce50049f0a42b9d411233cfb64fc328
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-24 02:48:56 +0300

    11

---
+++ 03_JavaAdvanced/_08_GenericsExercise/_11_Threeuple/Item2.java
@@ -0,0 +1,24 @@
+package _03_JavaAdvanced._08_GenericsExercise._11_Threeuple;
+
+public class Item2<G> {
+    G element;
+
+    public Item2(G element) {
+        this.element = element;
+    }
+
+    public G getElement() {
+        return element;
+    }
+
+    public Item2<G> setElement(G element) {
+        this.element = element;
+
+        return this;
+    }
+
+    @Override
+    public String toString(){
+        return this.element.toString();
+    }
+}
