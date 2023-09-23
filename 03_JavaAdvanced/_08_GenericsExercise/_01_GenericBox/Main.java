76da24f8b6fcb1bd6c740e0d41aac137eedc1865
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 22:23:24 +0300

    01

--- 03_JavaAdvanced/_08_GenericsExercise/_01_GenericBox/Main.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_01_GenericBox/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // Test Box
         Scanner scan = new Scanner(System.in);
+        
         int n = Integer.parseInt(scan.nextLine());
-
         Box<String> box = new Box();
+        
         for (int i = 0; i < n; i++) {
             String value = scan.nextLine();
             box.add(value);
