2ea78a13a7b977f92a5d550e618471635a3c2ca2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-24 01:04:12 +0300

    06

--- 03_JavaAdvanced/_08_GenericsExercise/_06_GenericCountMethodDouble/Main.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_06_GenericCountMethodDouble/Main.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Test Box
         Scanner scan = new Scanner(System.in);
-        int n = Integer.parseInt(scan.nextLine());
 
+        int n = Integer.parseInt(scan.nextLine());
         Box<Double> box = new Box();
+
         for (int i = 0; i < n; i++) {
             Double value = Double.parseDouble(scan.nextLine());
             box.add(value);
         }
+
         Double compareToThis = Double.parseDouble(scan.nextLine());
+
         System.out.println(box.countOfGreaterItems(compareToThis));
+
+        scan.close();
     }
 }
