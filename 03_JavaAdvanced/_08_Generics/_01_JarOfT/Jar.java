def5dc4d6085487967b6fce460fcb36c0cd11a9b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 21:36:53 +0300

    01

--- 03_JavaAdvanced/_08_Generics/_01_JarOfT/Jar.java
+++ 03_JavaAdvanced/_08_Generics/_01_JarOfT/Jar.java
@@ -9,9 +9,11 @@ public class Jar<T> {
     public Jar() {
         this.elements = new ArrayDeque<>();
     }
+    
     public void add(T element){
         this.elements.push(element);
     }
+    
     public T remove(){
         return this.elements.pop();
     }
