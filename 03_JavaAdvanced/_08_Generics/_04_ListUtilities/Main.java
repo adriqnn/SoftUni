243e71951a63d93e26472c0d75be3a60b54ac79f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 21:49:20 +0300

    04

--- 03_JavaAdvanced/_08_Generics/_04_ListUtilities/Main.java
+++ 03_JavaAdvanced/_08_Generics/_04_ListUtilities/Main.java
@@ -2,6 +2,8 @@ package _03_JavaAdvanced._08_Generics._04_ListUtilities;
 
 public class Main {
     public static void main(String[] args) {
+        // Test ListUtils
+        
         /*List<Integer> integers = List.of(13,42,73,69);
         List<String> strings = List.of("13","42","73","69");
         System.out.println(ListUtilities.getMin(integers));
