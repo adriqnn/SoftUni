91ce53f35721ff8fc76642082edb859efc8656d1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-24 00:57:53 +0300

    05

--- 03_JavaAdvanced/_08_GenericsExercise/_05_GenericCountMethodString/Main.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_05_GenericCountMethodString/Main.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Test Box
         Scanner scan = new Scanner(System.in);
+        
         int n = Integer.parseInt(scan.nextLine());
-
         Box<String> box = new Box();
+        
         for (int i = 0; i < n; i++) {
             String value = scan.nextLine();
             box.add(value);
         }
+        
         String compareToThis = scan.nextLine();
+        
         System.out.println(box.countOfGreaterItems(compareToThis));
+        
+        scan.close();
     }
 }
