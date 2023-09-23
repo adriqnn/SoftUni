1aa9adfac1c22669fb8ce3fc7f6ad4b0bdb82c3a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 21:39:51 +0300

    01

--- 03_JavaAdvanced/_08_Generics/_01_JarOfT/Main.java
+++ 03_JavaAdvanced/_08_Generics/_01_JarOfT/Main.java
@@ -2,7 +2,7 @@ package _03_JavaAdvanced._08_Generics._01_JarOfT;
 
 public class Main {
     public static void main(String[] args) {
-        
+        // Test Jar
         Jar<Integer> integerJar = new Jar<>();
         Jar<String> stringJar = new Jar<>();
         
