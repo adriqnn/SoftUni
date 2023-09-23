ac4b6df1a9d43adc61dfe0fc88c88382b2481b74
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 22:25:22 +0300

    02

--- 03_JavaAdvanced/_08_GenericsExercise/_02_GenericBoxOfInteger/Main.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_02_GenericBoxOfInteger/Main.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Test Box
         Scanner scan = new Scanner(System.in);
+        
         int n = Integer.parseInt(scan.nextLine());
-
         Box<Integer> box = new Box();
+        
         for (int i = 0; i < n; i++) {
             int value = Integer.parseInt(scan.nextLine());;
             box.add(value);
         }
+        
         System.out.println(box);
+        
+        scan.close();
     }
 }
