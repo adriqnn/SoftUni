4561e4affb2e6a03c1a6d4a09d3cee983ba3dc63
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-23 22:28:51 +0300

    03

--- 03_JavaAdvanced/_08_GenericsExercise/_03_GenericSwapMethodString/Main.java
+++ 03_JavaAdvanced/_08_GenericsExercise/_03_GenericSwapMethodString/Main.java
@@ -4,18 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Test Box
         Scanner scan = new Scanner(System.in);
-        int n = Integer.parseInt(scan.nextLine());
 
+        int n = Integer.parseInt(scan.nextLine());
         Box<String> box = new Box();
+
         for (int i = 0; i < n; i++) {
             String value = scan.nextLine();
             box.add(value);
         }
+
         String[] indexes = scan.nextLine().split("\\s+");
+
         int firstIndex = Integer.parseInt(indexes[0]);
         int secondIndex = Integer.parseInt(indexes[1]);
-        box.swap(firstIndex,secondIndex);
+
+        box.swap(firstIndex, secondIndex);
+
         System.out.println(box);
+        
+        scan.close();
     }
 }
