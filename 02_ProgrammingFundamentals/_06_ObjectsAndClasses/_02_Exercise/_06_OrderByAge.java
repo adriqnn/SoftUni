a7c9a3542e130ef4784ea8afb1fb9956063d6747
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-24 08:21:25 +0300

    06

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge.java
@@ -40,24 +40,24 @@ public class _06_OrderByAge {
     }
 }
 
-// Create practice Album class
-class Album{
-    private String name ;
-    private String personalNUmber;
-    private int age;
+// Create practice Student class
+class Student {
+    private String firstName;
+    private String lastName;
+    private double grade;
 
-    public Album(String name, String personalNUmber, int age) {
-        this.name = name;
-        this.personalNUmber = personalNUmber;
-        this.age = age;
+    public Student(String firstName, String lastName, double grade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.grade = grade;
     }
 
-    public int getAge() {
-        return age;
+    public double getGrade() {
+        return this.grade;
     }
 
     @Override
     public String toString(){
-        return String.format("%s with ID: %s is %d years old.", this.name, this.personalNUmber, this.age);
+        return String.format("%s %s: %.2f%n", this.firstName, this.lastName, this.grade);
     }
 }
