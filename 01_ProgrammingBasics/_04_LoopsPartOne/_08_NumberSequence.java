fc0b7edbdd9d6516d2addbafab754564a5482530
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-25 20:43:57 +0300

    08

--- 01_ProgrammingBasics/_04_LoopsPartOne/_08_NumberSequence.java
+++ 01_ProgrammingBasics/_04_LoopsPartOne/_08_NumberSequence.java
@@ -16,16 +16,18 @@ public class _08_NumberSequence {
         // Find the max and min numbers in the n sequence
         for ( int i = 1; i <= n; i++){
             int currentNum = Integer.parseInt(scan.nextLine());
+            
             if (currentNum > maxNum){
                 maxNum = currentNum;
             }
+            
             if (currentNum < minNum){
                 minNum = currentNum;
             }
         }
 
         // Print the max and the min numbers
-        System.out.printf("Max number: %d%n",maxNum);
+        System.out.printf("Max number: %d%n", maxNum);
         System.out.printf("Min number: %d", minNum);
 
         scan.close();
