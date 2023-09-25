d6103a17dacc4f6b37070db14baa9a2462cffd55
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-26 02:50:09 +0300

    09

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_09_PokemonDontGo.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_09_PokemonDontGo.java
@@ -18,37 +18,41 @@ public class _09_PokemonDontGo {
         // Read and execute commands based on the input
         while(!pokemonDistance.isEmpty()){
             int element = Integer.parseInt(scan.nextLine());
+            
             if(element >= 0 && element < pokemonDistance.size()){
                 int removedElement = pokemonDistance.get(element);
                 pokemonDistance.remove(element);
                 sum += removedElement;
+                
                 for (int i = 0; i < pokemonDistance.size(); i++) {
                     if(pokemonDistance.get(i) <= removedElement){
-                        pokemonDistance.set(i,pokemonDistance.get(i) + removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) + removedElement);
                     }else{
-                        pokemonDistance.set(i,pokemonDistance.get(i) - removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) - removedElement);
                     }
                 }
             }else if(element < 0){
                 int removedElement = pokemonDistance.get(0);
-                pokemonDistance.set(0,pokemonDistance.get(pokemonDistance.size()-1));
+                pokemonDistance.set(0, pokemonDistance.get(pokemonDistance.size()-1));
                 sum += removedElement;
+                
                 for (int i = 0; i < pokemonDistance.size(); i++) {
                     if(pokemonDistance.get(i) <= removedElement){
-                        pokemonDistance.set(i,pokemonDistance.get(i) + removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) + removedElement);
                     }else{
-                        pokemonDistance.set(i,pokemonDistance.get(i) - removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) - removedElement);
                     }
                 }
             }else if(element > pokemonDistance.size()-1){
                 int removedElement = pokemonDistance.get(pokemonDistance.size()-1);
                 pokemonDistance.set(pokemonDistance.size()-1, pokemonDistance.get(0));
                 sum += removedElement;
+                
                 for (int i = 0; i < pokemonDistance.size(); i++) {
                     if(pokemonDistance.get(i) <= removedElement){
-                        pokemonDistance.set(i,pokemonDistance.get(i) + removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) + removedElement);
                     }else{
-                        pokemonDistance.set(i,pokemonDistance.get(i) - removedElement);
+                        pokemonDistance.set(i, pokemonDistance.get(i) - removedElement);
                     }
                 }
             }
