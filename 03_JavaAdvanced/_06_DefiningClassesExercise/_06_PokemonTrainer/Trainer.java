6f3775253b9ab6ab2e5ab613d4bb37662b44397d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-30 23:11:34 +0300

    06

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_06_PokemonTrainer/Trainer.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_06_PokemonTrainer/Trainer.java
@@ -10,40 +10,40 @@ public class Trainer {
 
     public Trainer(String name){
         this.name = name;
-        numberOfBadges = 0;
-        animals = new ArrayList<>();
+        this.numberOfBadges = 0;
+        this.animals = new ArrayList<>();
     }
-    
+
     public int getNumberOfBadges() {
-        return numberOfBadges;
+        return this.numberOfBadges;
     }
-    
+
     public List<Pokemon> getAnimals() {
-        return animals;
+        return this.animals;
     }
-    
+
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setNumberOfBadges(int numberOfBadges) {
         this.numberOfBadges = numberOfBadges;
     }
-    
+
     public void setAnimals(List<Pokemon> animals) {
         this.animals = animals;
     }
-    
+
     @Override
     public String toString(){
         int number = 0;
-        
-        for (int i = 0; i < animals.size(); i++) {
-            if(animals.get(i).getHealth() > 0){
+
+        for (int i = 0; i < this.animals.size(); i++) {
+            if(this.animals.get(i).getHealth() > 0){
                 number++;
             }
         }
-        
+
         return this.name + " " + this.numberOfBadges + " " + number;
     }
 }
