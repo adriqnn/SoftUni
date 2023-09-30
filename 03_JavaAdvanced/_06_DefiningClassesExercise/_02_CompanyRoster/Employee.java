5436d54d6e1756d37e5ffdcbb012eb81ebcb83e8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-30 22:59:24 +0300

    02

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_02_CompanyRoster/Employee.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_02_CompanyRoster/Employee.java
@@ -16,47 +16,47 @@ public class Employee {
         this.email = "n/a";
         this.age = -1;
     }
-    
+
     public Employee(String name, double salary, String position, String department, String email, int age){
         this(name, salary, position, department);
         this.email = email;
         this.age = age;
     }
-    
+
     public Employee(String name, double salary, String position, String department, String email){
         this(name, salary, position, department);
         this.email = email;
     }
-    
+
     public Employee(String name, double salary, String position, String department, int age){
         this(name, salary, position, department);
         this.age = age;
     }
-    
+
     public String getName() {
-        return name;
+        return this.name;
     }
-    
+
     public double getSalary() {
-        return salary;
+        return this.salary;
     }
-    
+
     public String getPosition() {
-        return position;
+        return this.position;
     }
-    
+
     public String getDepartment() {
-        return department;
+        return this.department;
     }
-    
+
     public String getEmail() {
-        return email;
+        return this.email;
     }
-    
+
     public int getAge() {
-        return age;
+        return this.age;
     }
-    
+
     @Override
     public String toString(){
         return  String.format("%s %.2f %s %d", this.name, this.salary, this.email, this.age);
