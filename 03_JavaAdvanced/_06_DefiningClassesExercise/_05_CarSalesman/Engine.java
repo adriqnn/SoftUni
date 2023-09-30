6ab682008fcd4dcd4e7631880da8bbbfc671c593
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-30 23:09:56 +0300

    05

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Engine.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_05_CarSalesman/Engine.java
@@ -12,39 +12,39 @@ public class Engine {
         this.displacement = 0;
         this.efficiency = "n/a";
     }
-    
+
     public Engine(String model, int power, int displacement, String efficiency) {
         this(model,power);
         this.displacement = displacement;
         this.efficiency = efficiency;
     }
-    
+
     public Engine(String model, int power, int displacement) {
         this(model,power);
         this.displacement = displacement;
     }
-    
+
     public Engine(String model, int power, String efficiency) {
         this(model,power);
         this.efficiency = efficiency;
     }
-    
+
     @Override
     public String toString(){
         String displacementPrint;
-        
-        if(displacement == 0){
+
+        if(this.displacement == 0){
             displacementPrint = "n/a";
         }else{
-            displacementPrint = displacement + "";
+            displacementPrint = this.displacement + "";
         }
-        
+
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(model).append(":").append("\n")
-                .append("Power: ").append(power).append("\n")
+        stringBuilder.append(this.model).append(":").append("\n")
+                .append("Power: ").append(this.power).append("\n")
                 .append("Displacement: ").append(displacementPrint).append("\n")
-                .append("Efficiency: ").append(efficiency);
-        
+                .append("Efficiency: ").append(this.efficiency);
+
         return stringBuilder.toString();
     }
 }
