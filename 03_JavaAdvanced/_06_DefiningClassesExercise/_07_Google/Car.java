fb8a3999d3594376759a44ef01aa7f46e4c3e186
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-30 23:28:40 +0300

    07

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Car.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Car.java
@@ -10,11 +10,11 @@ public class Car {
     }
 
     public String getModel() {
-        return model;
+        return this.model;
     }
 
     public String getSpeed() {
-        return speed;
+        return this.speed;
     }
 
     public void setModel(String model) {
@@ -24,13 +24,13 @@ public class Car {
     public void setSpeed(String speed) {
         this.speed = speed;
     }
-    
+
     @Override
     public String toString(){
         if(this.model.equals("")){
             return "Car:" + "\n";
         }
-        
+
         return "Car:" + "\n" + this.model + " " + this.speed + "\n";
     }
 }
