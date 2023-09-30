1511ebd330a8e7ef33444a6da994597fc9b40a0a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-09-30 23:34:12 +0300

    07

--- 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Parents.java
+++ 03_JavaAdvanced/_06_DefiningClassesExercise/_07_Google/Parents.java
@@ -10,11 +10,11 @@ public class Parents {
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public String getDate() {
-        return date;
+        return this.date;
     }
 
     public void setName(String name) {
@@ -24,7 +24,7 @@ public class Parents {
     public void setDate(String date) {
         this.date = date;
     }
-    
+
     @Override
     public String toString(){
         return this.name + " " + this.date;
