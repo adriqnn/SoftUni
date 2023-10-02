8a7d5d397439a6b32e12f3a556e64f5cae0dbbfa
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-02 03:27:34 +0300

    03

--- 03_JavaAdvanced/_07_WorkshopExercise/_03_LinkedListTail/Node.java
+++ 03_JavaAdvanced/_07_WorkshopExercise/_03_LinkedListTail/Node.java
@@ -7,9 +7,9 @@ public class Node {
     public Node(int value){
         this.value = value;
     }
-    
+
     @Override
     public String toString(){
-        return value + "";
+        return this.value + "";
     }
 }
