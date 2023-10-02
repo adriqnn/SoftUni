ba47655a00dc79ee3edf67cbef95d119ebbe460e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-02 03:29:42 +0300

    03

--- 03_JavaAdvanced/_07_WorkshopExercise/_03_LinkedListTail/LinkedListTail.java
+++ 03_JavaAdvanced/_07_WorkshopExercise/_03_LinkedListTail/LinkedListTail.java
@@ -11,73 +11,73 @@ public class LinkedListTail {
 
     public void addFirst(int element){
         Node newNode = new Node(element);
-        
+
         if(!isEmpty()){
-            newNode.next = head;
+            newNode.next = this.head;
         }else{
             this.tail = newNode;
         }
-        
+
         this.head = newNode;
-        size++;
+        this.size++;
     }
-    
+
     public void addLast(int element){
         if(isEmpty()){
             addFirst(element);
             return;
         }
-        
+
         Node newNode = new Node(element);
-        
+
         this.tail.next = newNode;
-        tail = newNode;
+        this.tail = newNode;
         this.size++;
     }
-    
+
     public int removeFirst(){
         if(isEmpty()){
             throw new IllegalStateException("Can't remove from empty list");
         }
-        
+
         int result = this.head.value;
         this.head = this.head.next;
         this.size--;
-        
+
         if(isEmpty()){
             this.head = this.tail = null;
         }
-        
+
         return result;
     }
-    
+
     public int removeLast(){
         if(this.size < 2){
             return removeFirst();
         }
-        
+
         int result = 0;
         Node currentNode = this.head;
-        
+
         while (currentNode.next.next != null){
             currentNode = currentNode.next;
         }
-        
+
         result = currentNode.next.value;
         currentNode.next = null;
         this.size--;
         this.tail = currentNode;
-        
+
         return result;
     }
-    
+
     public int get(int index){
         checkIndex(index);
-        
+
         int result = 0;
         int currentIndex = 0;
         Node currentNode = this.head;
-        
+
         while(currentNode != null){
             if(currentIndex == index){
                 result = currentNode.value;
@@ -86,48 +86,50 @@ public class LinkedListTail {
             currentNode = currentNode.next;
             currentIndex++;
         }
-        
+
         return result;
     }
-    
+
     private void checkIndex(int index){
         if(index < 0 || index >= this.size){
             throw new IndexOutOfBoundsException("The index " + index + " is out of bounds.");
         }
     }
-    
+
     public int size(){
         return this.size;
     }
-    
+
     public boolean isEmpty(){
         return this.size == 0;
     }
-    
+
     public void forEach(Consumer<Integer> consumer){
         Node currentNode = this.head;
-        
+
         while(currentNode != null){
             consumer.accept(currentNode.value);
             currentNode = currentNode.next;
         }
     }
-    
+
     /*public int[] toArray(){
         int[] array = new int[this.size];
         int counter = 0;
         Node currentNode = this.head;
+        
         while(currentNode != null){
             array[counter] = currentNode.value;
             counter++;
         }
+        
         return array;
     }*/
-    
+
     public int[] toArray(){
         List<Integer> result = new ArrayList<>();
         forEach((result::add));
-        
+
         return  result.stream().mapToInt(e -> e).toArray();
     }
 }
