f31362b535f2aee6b53ee27f36c3fd140d9e8a65
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-03 23:48:59 +0300

    06

--- 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_06_StrategyPattern/AgeComparator.java
+++ 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_06_StrategyPattern/AgeComparator.java
@@ -3,8 +3,9 @@ package _03_JavaAdvanced._09_IteratorsAndComparatorsExercise._06_StrategyPattern
 import java.util.Comparator;
 
 public class AgeComparator implements Comparator<Person> {
+    
     @Override
     public int compare(Person o1, Person o2) {
-        return Integer.compare(o1.getAge(),o2.getAge());
+        return Integer.compare(o1.getAge(), o2.getAge());
     }
 }
