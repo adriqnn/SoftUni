63894f082c01e3c79db2900f32938d0bbc002a51
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-03 23:09:10 +0300

    01

---
+++ 03_JavaAdvanced/_09_IteratorsAndComparators/_01_02_03_04_Book/Main.java
@@ -0,0 +1,8 @@
+package _03_JavaAdvanced._09_IteratorsAndComparators._01_02_03_04_Book;
+
+public class Main {
+    public static void main(String[] args) {
+        // Test Book, BookComparator and Library
+
+    }
+}
