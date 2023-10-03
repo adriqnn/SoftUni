d862eab77938b793280e4b847f739bfa9e33540e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-03 23:38:05 +0300

    05

--- 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_05_ComparingObjects/Main.java
+++ 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_05_ComparingObjects/Main.java
@@ -7,28 +7,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+
         String input = scan.nextLine();
         List<Person> people = new ArrayList<>();
-        
+
         while(!input.equals("END")){
             String[] peopleParameters = input.split("\\s+");
-            
+
             String name = peopleParameters[0];
             int age = Integer.parseInt(peopleParameters[1]);
             String town = peopleParameters[2];
-            
-            Person person = new Person(name,age,town);
+
+            Person person = new Person(name, age, town);
             people.add(person);
-            
+
             input = scan.nextLine();
         }
-        
+
         int comparePerson = Integer.parseInt(scan.nextLine());
         int size = people.size();
-        Person personToCompare = people.remove(comparePerson-1);
+        
+        Person personToCompare = people.remove(comparePerson - 1);
+        
         int samePeople = 0;
         int differentPeople = 0;
+        
         for (Person person : people) {
             if(person.compareTo(personToCompare) == 0){
                 samePeople++;
@@ -36,6 +39,7 @@ public class Main {
                 differentPeople++;
             }
         }
+        
         if(samePeople == 0){
             System.out.println("No matches");
         }else{
