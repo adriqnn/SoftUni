662862169327d77430f17d1abcc8cfad7ff8453f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-03 23:33:19 +0300

    04

--- 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_04_Froggy/Main.java
+++ 03_JavaAdvanced/_09_IteratorsAndComparatorsExercise/_04_Froggy/Main.java
@@ -9,16 +9,22 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         String[] stonesArray = scan.nextLine().split(", ");
         Lake lake = new Lake(Arrays.stream(stonesArray).map(Integer::parseInt).collect(Collectors.toList()));
         List<String> output = new ArrayList<>();
+
         for (Integer stone : lake) {
             output.add(stone + "");
         }
+
         /*Iterator<Integer> frog = lake.iterator();
         while(frog.hasNext()){
             System.out.println(frog.next());
         }*/
+
         System.out.println(String.join(", ",output));
+        
+        scan.close();
     }
 }
