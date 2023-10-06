84245d8a87f79fed0e1ddb2b4646f7a40bd067b1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-06 22:13:33 +0300

    01

--- 03_JavaAdvanced/_10_ExamPrep/_01_/SantasPresentFactory.java
+++ 03_JavaAdvanced/_10_ExamPrep/_01_/SantasPresentFactory.java
@@ -3,7 +3,7 @@ package _03_JavaAdvanced._10_ExamPrep._01_;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class SantaSPresentFactory {
+public class SantasPresentFactory {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         List<Integer> boxesInput = Arrays.stream(scan.nextLine().split("\\s+")).map(Integer::parseInt).collect(Collectors.toList());
