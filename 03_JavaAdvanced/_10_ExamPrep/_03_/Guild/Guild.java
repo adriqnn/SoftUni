3a55b64466fd5db55df747a5cd0548276e162e13
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-07 05:38:37 +0300

    03

--- 03_JavaAdvanced/_10_ExamPrep/_03_/Guild/Guild.java
+++ 03_JavaAdvanced/_10_ExamPrep/_03_/Guild/Guild.java
@@ -20,54 +20,54 @@ public class Guild {
     }
 
     public void addPlayer(Player player){
-        if(this.size < capacity) {
+        if(this.size < this.capacity) {
             this.roster.putIfAbsent(player.getName(), player);
-            size++;
+            this.size++;
         }
     }
 
     public boolean removePlayer(String name){
-        boolean containsThis = roster.containsKey(name);
-        
+        boolean containsThis = this.roster.containsKey(name);
+
         if(containsThis) {
-            roster.remove(name);
-            size--;
+            this.roster.remove(name);
+            this.size--;
         }
-        
+
         return containsThis;
     }
 
     public void promotePlayer(String name){
-        if(roster.containsKey(name)) {
-            if (!roster.get(name).getRank().equals("Member")) {
-                roster.get(name).setRank("Member");
+        if(this.roster.containsKey(name)) {
+            if (!this.roster.get(name).getRank().equals("Member")) {
+                this.roster.get(name).setRank("Member");
             }
         }
 
     }
 
     public void demotePlayer(String name){
-        if(roster.containsKey(name)) {
-            if (!roster.get(name).getRank().equals("Trial")) {
-                roster.get(name).setRank("Trial");
+        if(this.roster.containsKey(name)) {
+            if (!this.roster.get(name).getRank().equals("Trial")) {
+                this.roster.get(name).setRank("Trial");
             }
         }
     }
 
     public Player[] kickPlayersByClass(String clazz){
         List<Player> kicked = new ArrayList<>();
-        
-        for (Map.Entry<String, Player> k : roster.entrySet()) {
+
+        for (Map.Entry<String, Player> k : this.roster.entrySet()) {
             if(k.getValue().getClazz().equals(clazz)){
                 kicked.add(k.getValue());
             }
         }
-        
+
         for (Player s : kicked) {
-            roster.remove(s.getName());
+            this.roster.remove(s.getName());
             this.size--;
         }
-        
+
         return kicked.toArray(new Player[0]);
     }
 
@@ -78,16 +78,16 @@ public class Guild {
     public String report(){
         /*
         StringBuilder sb = new StringBuilder();
-        
+
         sb.append("Players in the a.java.advanced.exampreps.bakery.guild: " + this.name + System.lineSeparator());
-        for (Player p : roster.values()) {
+        for (Player p : this.roster.values()) {
             sb.append(p).append(System.lineSeparator());
         }
-        
+
         return sb.toString();
         */
-        
+
         return String.format(
-                "Players in the guild: %s:%n%s", name, roster.values().stream().map(Player::toString).collect(Collectors.joining(System.lineSeparator())));
+                "Players in the guild: %s:%n%s", this.name, this.roster.values().stream().map(Player::toString).collect(Collectors.joining(System.lineSeparator())));
     }
 }
