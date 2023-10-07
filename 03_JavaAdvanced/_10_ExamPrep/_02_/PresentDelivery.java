69da5b3307cb55bfbf60fec040508f3563e6da93
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-07 04:02:32 +0300

    02

--- 03_JavaAdvanced/_10_ExamPrep/_02_/PresentDelivery.java
+++ 03_JavaAdvanced/_10_ExamPrep/_02_/PresentDelivery.java
@@ -5,42 +5,53 @@ import java.util.Scanner;
 public class PresentDelivery {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int presents = Integer.parseInt(scan.nextLine());
         int neighbourhood = Integer.parseInt(scan.nextLine());
         char[][] neighbourhoodMatrix = new char[neighbourhood][];
+
         for (int i = 0; i < neighbourhoodMatrix.length; i++) {
             neighbourhoodMatrix[i] = scan.nextLine().replaceAll(" ","").toCharArray();
         }
+
         int startRow = 0;
         int startCol = 0;
         int good = 0;
         int gotPresent = 0;
         int bad = 0;
         int cookies = 0;
+
         for (int i = 0; i < neighbourhoodMatrix.length; i++) {
             for (int j = 0; j < neighbourhoodMatrix[i].length; j++) {
                 if(neighbourhoodMatrix[i][j] == 'S'){
                     startRow = i;
                     startCol = j;
                 }
+
                 if(neighbourhoodMatrix[i][j] == 'X'){
                     bad++;
                 }
+
                 if(neighbourhoodMatrix[i][j] == 'V'){
                     good++;
                 }
+
                 if(neighbourhoodMatrix[i][j] == 'C'){
                     cookies++;
                 }
             }
         }
+
         boolean ranOut = false;
         String command = scan.nextLine();
+
         while(!command.equals("Christmas morning") && presents > 0){
             neighbourhoodMatrix[startRow][startCol] = '-';
+
             if(command.equals("up")){
-                if(isValid(startRow-1,startCol,neighbourhoodMatrix)){
+                if(isValid(startRow-1, startCol, neighbourhoodMatrix)){
                     startRow--;
+
                     if(neighbourhoodMatrix[startRow][startCol] == 'V'){
                         presents--;
                         gotPresent++;
@@ -48,27 +59,34 @@ public class PresentDelivery {
                         if(neighbourhoodMatrix[startRow][startCol-1] == 'V' || neighbourhoodMatrix[startRow][startCol-1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol-1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol-1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow][startCol+1] == 'V' || neighbourhoodMatrix[startRow][startCol+1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol+1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol+1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow-1][startCol] == 'V' || neighbourhoodMatrix[startRow-1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow-1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow-1][startCol] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow+1][startCol] == 'V' || neighbourhoodMatrix[startRow+1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow+1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow+1][startCol] =='V'){
                                 gotPresent++;
                             }
@@ -79,8 +97,9 @@ public class PresentDelivery {
                     break;
                 }
             }else if(command.equals("down")){
-                if(isValid(startRow+1,startCol,neighbourhoodMatrix)){
+                if(isValid(startRow+1, startCol, neighbourhoodMatrix)){
                     startRow++;
+
                     if(neighbourhoodMatrix[startRow][startCol] == 'V'){
                         presents--;
                         gotPresent++;
@@ -88,27 +107,34 @@ public class PresentDelivery {
                         if(neighbourhoodMatrix[startRow][startCol-1] == 'V' || neighbourhoodMatrix[startRow][startCol-1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol-1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol-1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow][startCol+1] == 'V' || neighbourhoodMatrix[startRow][startCol+1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol+1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol+1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow-1][startCol] == 'V' || neighbourhoodMatrix[startRow-1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow-1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow-1][startCol] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow+1][startCol] == 'V' || neighbourhoodMatrix[startRow+1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow+1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow+1][startCol] =='V'){
                                 gotPresent++;
                             }
@@ -119,8 +145,9 @@ public class PresentDelivery {
                     break;
                 }
             }else if(command.equals("left")){
-                if(isValid(startRow,startCol-1,neighbourhoodMatrix)){
+                if(isValid(startRow, startCol-1, neighbourhoodMatrix)){
                     startCol--;
+
                     if(neighbourhoodMatrix[startRow][startCol] == 'V'){
                         presents--;
                         gotPresent++;
@@ -128,27 +155,34 @@ public class PresentDelivery {
                         if(neighbourhoodMatrix[startRow][startCol-1] == 'V' || neighbourhoodMatrix[startRow][startCol-1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol-1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol-1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow][startCol+1] == 'V' || neighbourhoodMatrix[startRow][startCol+1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol+1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol+1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow-1][startCol] == 'V' || neighbourhoodMatrix[startRow-1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow-1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow-1][startCol] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow+1][startCol] == 'V' || neighbourhoodMatrix[startRow+1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow+1][startCol] = '-';
+
                             if(neighbourhoodMatrix[startRow+1][startCol] =='V'){
                                 gotPresent++;
                             }
@@ -159,8 +193,9 @@ public class PresentDelivery {
                     break;
                 }
             }else if(command.equals("right")){
-                if(isValid(startRow,startCol+1,neighbourhoodMatrix)){
+                if(isValid(startRow, startCol+1, neighbourhoodMatrix)){
                     startCol++;
+
                     if(neighbourhoodMatrix[startRow][startCol] == 'V'){
                         presents--;
                         gotPresent++;
@@ -168,27 +203,34 @@ public class PresentDelivery {
                         if(neighbourhoodMatrix[startRow][startCol-1] == 'V' || neighbourhoodMatrix[startRow][startCol-1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol-1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol-1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow][startCol+1] == 'V' || neighbourhoodMatrix[startRow][startCol+1] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow][startCol+1] = '-';
+
                             if(neighbourhoodMatrix[startRow][startCol+1] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow-1][startCol] == 'V' || neighbourhoodMatrix[startRow-1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow-1][startCol] = '-';
+                            
                             if(neighbourhoodMatrix[startRow-1][startCol] =='V'){
                                 gotPresent++;
                             }
                         }
+
                         if(neighbourhoodMatrix[startRow+1][startCol] == 'V' || neighbourhoodMatrix[startRow+1][startCol] == 'X' && presents > 0){
                             presents--;
                             neighbourhoodMatrix[startRow+1][startCol] = '-';
+                            
                             if(neighbourhoodMatrix[startRow+1][startCol] =='V'){
                                 gotPresent++;
                             }
@@ -199,29 +241,37 @@ public class PresentDelivery {
                     break;
                 }
             }
+            
             neighbourhoodMatrix[startRow][startCol] = 'S';
             if(presents == 0){
                 break;
             }
+            
             command = scan.nextLine();
         }
+        
         if(ranOut || presents == 0){
             System.out.println("Santa ran out of presents!");
         }
+        
         for (int i = 0; i < neighbourhoodMatrix.length; i++) {
             for (int j = 0; j < neighbourhoodMatrix[i].length; j++) {
                 System.out.print(neighbourhoodMatrix[i][j] + " ");
             }
+            
             System.out.println();
         }
+        
         if(gotPresent == good){
             System.out.printf("Good job, Santa! %d happy nice kid/s.%n",good);
         }else{
             System.out.printf("No presents for %d nice kid/s.%n",good-gotPresent);
         }
 
+        scan.close();
     }
-    public static boolean isValid(int row,int col,char[][] matrix){
+    
+    public static boolean isValid(int row, int col, char[][] matrix){
         return row >= 0 && row < matrix.length && col >= 0 && col < matrix[row].length;
     }
 }
