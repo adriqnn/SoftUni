21bf65d52e04f4952b79c8bdc440165c140b6a26
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-07 03:48:53 +0300

    02

--- 03_JavaAdvanced/_10_ExamPrep/_02_/Selling.java
+++ 03_JavaAdvanced/_10_ExamPrep/_02_/Selling.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Selling {
     public static int otherOR;
     public static int otherOC;
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         
