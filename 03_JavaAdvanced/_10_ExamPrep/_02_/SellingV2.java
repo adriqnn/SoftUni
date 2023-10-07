582808fd54f0170a74e23679ae8de77658443b68
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-07 03:49:02 +0300

    02

--- 03_JavaAdvanced/_10_ExamPrep/_02_/SellingV2.java
+++ 03_JavaAdvanced/_10_ExamPrep/_02_/SellingV2.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class SellingV2 {
     public static int rowP;
     public static int colP;
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int size = Integer.parseInt(scan.nextLine());
