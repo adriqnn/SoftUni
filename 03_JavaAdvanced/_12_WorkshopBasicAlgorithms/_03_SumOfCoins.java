ab3223a77178fb729b3289a7846d5d1136b9ce05
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-09 00:33:19 +0300

    03

--- 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_03_SumOfCoins.java
+++ 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_03_SumOfCoins.java
@@ -12,23 +12,16 @@ public class _03_SumOfCoins {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
         String coinsLine = reader.readLine();
-
+        
         LinkedHashMap<Integer, Integer> coinsUsage = new LinkedHashMap<>();
-
         int[] availableCoins = new int[6];
         int targetSum = 923;
         Map<Integer, Integer> selectedCoins = ChooseCoins(availableCoins, targetSum);
 
-        Arrays.stream(coinsLine
-                        .replaceAll("Coins: ", "")
-                        .split(", "))
-                .map(Integer::parseInt)
-                .sorted((f, s) -> Integer.compare(s, f))
-                .forEach((num) -> coinsUsage.put(num, 0));
+        Arrays.stream(coinsLine.replaceAll("Coins: ", "").split(", ")).map(Integer::parseInt).sorted((f, s) -> Integer.compare(s, f)).forEach((num) -> coinsUsage.put(num, 0));
 
         String[] line = reader.readLine().split(": ");
         int sum = Integer.parseInt(line[1]);
-
         int totalCount = 0;
 
         for (Integer coinValue : coinsUsage.keySet()) {
@@ -38,18 +31,16 @@ public class _03_SumOfCoins {
             totalCount += count;
             sum -= count * coinValue;
         }
+        
         if (sum > 0) {
             System.out.println("Error");
             return;
         }
 
         System.out.println("Number of coins to take: " + totalCount);
-        coinsUsage
-                .entrySet()
-                .stream()
-                .filter((entry) -> entry.getValue() != 0)
-                .forEach((entry) -> System.out.println(
-                        entry.getValue() + " coin(s) with value " + entry.getKey()));
+        coinsUsage.entrySet().stream().filter((entry) -> entry.getValue() != 0).forEach((entry) -> System.out.println(entry.getValue() + " coin(s) with value " + entry.getKey()));
+
+        reader.close();
     }
 
     private static Map<Integer, Integer> ChooseCoins(int[] availableCoins, int targetSum) {
