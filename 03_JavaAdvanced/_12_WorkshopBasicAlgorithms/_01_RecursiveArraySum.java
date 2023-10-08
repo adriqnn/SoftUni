9519b5d39c13631f4312d8b01843a91e28f25c34
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-09 00:05:05 +0300

    01

--- 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_01_RecursiveArraySum.java
+++ 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_01_RecursiveArraySum.java
@@ -6,16 +6,20 @@ import java.util.Scanner;
 public class _01_RecursiveArraySum {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int[] arr = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
 
-        int sum = calcSum(arr,0);
+        int[] arr = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+        int sum = calcSum(arr, 0);
+
         System.out.println(sum);
+
+        scan.close();
     }
 
     private static int calcSum(int[] arr, int i) {
         if(i == arr.length - 1){
             return arr[i];
         }
-        return arr[i] + calcSum(arr,i+1);
+
+        return arr[i] + calcSum(arr, i + 1);
     }
 }
