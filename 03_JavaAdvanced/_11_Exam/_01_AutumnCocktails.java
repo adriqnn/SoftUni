df20df32f4ddd9ca83e1ccbb5cb780f9fb1f0a85
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-08 05:00:01 +0300

    01

--- 03_JavaAdvanced/_11_Exam/_01_AutumnCocktails.java
+++ 03_JavaAdvanced/_11_Exam/_01_AutumnCocktails.java
@@ -8,24 +8,31 @@ import java.util.Scanner;
 public class _01_AutumnCocktails {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         Deque<Integer> bucketsOfIngredientsQueue = new ArrayDeque<>();
         Deque<Integer> freshnessLevelStack = new ArrayDeque<>();
+
         Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).forEach(bucketsOfIngredientsQueue::offer);
         Arrays.stream(scan.nextLine().split(" ")).map(Integer::parseInt).forEach(freshnessLevelStack::push);
+
         int ps = 0;
         int th = 0;
         int ah = 0;
         int hf = 0;
+
         while(!bucketsOfIngredientsQueue.isEmpty() && !freshnessLevelStack.isEmpty()){
             int buckets = bucketsOfIngredientsQueue.peek();
             int freshness = freshnessLevelStack.peek();
+
             if(buckets == 0 ) {
                 bucketsOfIngredientsQueue.poll();
                 continue;
             }
+
             int multiply = buckets*freshness;
             bucketsOfIngredientsQueue.poll();
             freshnessLevelStack.pop();
+
             if(multiply == 150){
                 ps++;
             }else if(multiply == 250){
@@ -38,33 +45,44 @@ public class _01_AutumnCocktails {
                 buckets += 5;
                 bucketsOfIngredientsQueue.offer(buckets);
             }
+
             /*if(ps > 0 && th > 0 && ah > 0 && hf > 0){
                 break;
             }*/
         }
+        
         if(ps > 0 && th > 0 && ah > 0 && hf > 0){
             System.out.println("It's party time! The cocktails are ready!");
         }else{
             System.out.println("What a pity! You didn't manage to prepare all cocktails.");
         }
+        
         if(bucketsOfIngredientsQueue.size() > 0){
             int sum = 0;
+            
             for (Integer e : bucketsOfIngredientsQueue) {
                 sum += e;
             }
-            System.out.printf("Ingredients left: %d%n",sum);
+            
+            System.out.printf("Ingredients left: %d%n", sum);
         }
+        
         if(ah > 0){
-            System.out.printf(" # Apple Hinny --> %d%n",ah);
+            System.out.printf(" # Apple Hinny --> %d%n", ah);
         }
+        
         if(hf > 0){
-            System.out.printf(" # High Fashion --> %d%n",hf);
+            System.out.printf(" # High Fashion --> %d%n", hf);
         }
+        
         if(ps > 0){
-            System.out.printf(" # Pear Sour --> %d%n",ps);
+            System.out.printf(" # Pear Sour --> %d%n", ps);
         }
+        
         if(th > 0){
-            System.out.printf(" # The Harvest --> %d%n",th);
+            System.out.printf(" # The Harvest --> %d%n", th);
         }
+        
+        scan.close();
     }
 }
