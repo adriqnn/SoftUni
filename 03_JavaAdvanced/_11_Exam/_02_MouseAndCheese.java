f6805c31e32d6f266c771a8a420222dd3a1caf34
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-08 05:02:55 +0300

    02

--- 03_JavaAdvanced/_11_Exam/_02_MouseAndCheese.java
+++ 03_JavaAdvanced/_11_Exam/_02_MouseAndCheese.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 public class _02_MouseAndCheese {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         int size = Integer.parseInt(scan.nextLine());
         char[][] territoryMatrix = new char[size][];
+        
         for (int i = 0; i < territoryMatrix.length; i++) {
             territoryMatrix[i] = scan.nextLine().toCharArray();
         }
+        
         int r = 0;
         int c = 0;
+        
         for (int i = 0; i < territoryMatrix.length; i++) {
             for (int j = 0; j < territoryMatrix[i].length; j++) {
                 if(territoryMatrix[i][j] == 'M'){
@@ -20,19 +24,24 @@ public class _02_MouseAndCheese {
                 }
             }
         }
+        
         String command = scan.nextLine();
         boolean isOut = false;
         int cheeseEaten = 0;
+        
         while(!command.equals("end")){
             territoryMatrix[r][c] = '-';
+            
             if(command.equals("up")){
-                if(isValid(r-1,c,territoryMatrix)){
+                if(isValid(r-1, c, territoryMatrix)){
                     r--;
+                    
                     if(territoryMatrix[r][c] == 'c'){
                         cheeseEaten++;
                     }else if(territoryMatrix[r][c] == 'B'){
                         territoryMatrix[r][c] = '-';
                         r--;
+                        
                         if(territoryMatrix[r][c] == 'c') {
                             cheeseEaten++;
                         }
@@ -41,13 +50,15 @@ public class _02_MouseAndCheese {
                     isOut = true;
                 }
             }else if(command.equals("down")){
-                if(isValid(r+1,c,territoryMatrix)){
+                if(isValid(r+1, c, territoryMatrix)){
                     r++;
+                    
                     if(territoryMatrix[r][c] == 'c'){
                         cheeseEaten++;
                     }else if(territoryMatrix[r][c] == 'B'){
                         territoryMatrix[r][c] = '-';
                         r++;
+                        
                         if(territoryMatrix[r][c] == 'c') {
                             cheeseEaten++;
                         }
@@ -56,13 +67,15 @@ public class _02_MouseAndCheese {
                     isOut = true;
                 }
             }else if(command.equals("left")){
-                if(isValid(r,c-1,territoryMatrix)){
+                if(isValid(r, c-1, territoryMatrix)){
                     c--;
+                    
                     if(territoryMatrix[r][c] == 'c'){
                         cheeseEaten++;
                     }else if(territoryMatrix[r][c] == 'B'){
                         territoryMatrix[r][c] = '-';
                         c--;
+                        
                         if(territoryMatrix[r][c] == 'c') {
                             cheeseEaten++;
                         }
@@ -71,13 +84,15 @@ public class _02_MouseAndCheese {
                     isOut = true;
                 }
             }else if(command.equals("right")){
-                if(isValid(r,c+1,territoryMatrix)){
+                if(isValid(r, c+1, territoryMatrix)){
                     c++;
+                    
                     if(territoryMatrix[r][c] == 'c'){
                         cheeseEaten++;
                     }else if(territoryMatrix[r][c] == 'B'){
                         territoryMatrix[r][c] = '-';
                         c++;
+                        
                         if(territoryMatrix[r][c] == 'c') {
                             cheeseEaten++;
                         }
@@ -86,28 +101,36 @@ public class _02_MouseAndCheese {
                     isOut = true;
                 }
             }
+            
             if(isOut){
                 break;
             }
+            
             territoryMatrix[r][c] = 'M';
             command = scan.nextLine();
         }
+        
         if(isOut){
             System.out.println("Where is the mouse?");
         }
+        
         if(cheeseEaten < 5){
-            System.out.printf("The mouse couldn't eat the cheeses, she needed %d cheeses more.%n",5-cheeseEaten);
+            System.out.printf("The mouse couldn't eat the cheeses, she needed %d cheeses more.%n", 5 - cheeseEaten);
         }else{
-            System.out.printf("Great job, the mouse is fed %d cheeses!%n",cheeseEaten);
+            System.out.printf("Great job, the mouse is fed %d cheeses!%n", cheeseEaten);
         }
+        
         for (int i = 0; i < territoryMatrix.length; i++) {
             for (int j = 0; j < territoryMatrix[i].length; j++) {
                 System.out.print(territoryMatrix[i][j]);
             }
+            
             System.out.println();
         }
-
+        
+        scan.close();
     }
+    
     public static boolean isValid(int r, int c, char[][] matrix){
         return r >= 0 && r < matrix.length && c >= 0 && c < matrix[r].length;
     }
