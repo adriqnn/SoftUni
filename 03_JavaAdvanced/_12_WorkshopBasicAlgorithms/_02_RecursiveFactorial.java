5aa0dda8d922cf92bbd53af5b552c527a1c935cb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-09 00:06:00 +0300

    02

--- 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_02_RecursiveFactorial.java
+++ 03_JavaAdvanced/_12_WorkshopBasicAlgorithms/_02_RecursiveFactorial.java
@@ -1,20 +1,25 @@
 package _03_JavaAdvanced._12_WorkshopBasicAlgorithms;
 
+
 import java.util.Scanner;
 
 public class _02_RecursiveFactorial {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         int n = Integer.parseInt(scan.nextLine());
-        int sum = factorial(n);
+        long sum = factorial(n);
+
         System.out.println(sum);
 
-
+        scan.close();
     }
-    static int factorial(int n){
+
+    static long factorial(int n){
         if(n <= 1){
             return 1;
         }
-        return n*factorial(n-1);
+
+        return n * factorial(n - 1);
     }
 }
