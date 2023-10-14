fe0f2d7a75e9258450ac54eddf0a3249a2f7ca9c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-14 22:33:55 +0300

    02

--- 04_JavaOOP/_01_WorkingWithAbstraction/_02_PointInRectangle/Main.java
+++ 04_JavaOOP/_01_WorkingWithAbstraction/_02_PointInRectangle/Main.java
@@ -6,18 +6,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         int[] coordinates = readArray(scan);
-        Point pointA = new Point(coordinates[0],coordinates[1]);
-        Point pointB = new Point(coordinates[2],coordinates[3]);
-        Rectangle rectangle = new Rectangle(pointA,pointB);
+        
+        Point pointA = new Point(coordinates[0], coordinates[1]);
+        Point pointB = new Point(coordinates[2], coordinates[3]);
+        
+        Rectangle rectangle = new Rectangle(pointA, pointB);
+        
         int n = Integer.parseInt(scan.nextLine());
+        
         while(n-- > 0){
             int[] tokens = readArray(scan);
-            Point p = new Point(tokens[0],tokens[1]);
+            Point p = new Point(tokens[0], tokens[1]);
             boolean isInside = rectangle.contains(p);
+            
             System.out.println(isInside);
         }
+        
+        scan.close();
     }
+    
     public static int[] readArray(Scanner scan){
         return Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
     }
