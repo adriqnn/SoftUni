71afff96603bbba7562d1ded65b5541c1026e173
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-14 22:49:30 +0300

    03

--- 04_JavaOOP/_01_WorkingWithAbstraction/_03_StudentSystem/StudentSystem.java
+++ 04_JavaOOP/_01_WorkingWithAbstraction/_03_StudentSystem/StudentSystem.java
@@ -17,11 +17,14 @@ public class StudentSystem {
     public String parseCommand(String[] args){
         String command = args[0];
         String name = args[1];
+        
         switch(command){
             case "Create":
                 int age = Integer.parseInt(args[2]);
                 double grade = Double.parseDouble(args[3]);
-                createAndRegisterStudent(name,age,grade);
+                
+                createAndRegisterStudent(name, age, grade);
+                
                 return null;
             case "Show":
                 return getStudentInfo(name);
@@ -33,12 +36,14 @@ public class StudentSystem {
 
     private String getStudentInfo(String name) {
         Student student = this.students.get(name);
+        
         //throw new IllegalArgumentException("No student with name " + name);
         assert student != null;
+        
         return StudentInfoFormatter.getFormatter(student);
     }
 
     private void createAndRegisterStudent(String name, int age, double grade) {
-        this.students.putIfAbsent(name,new Student(name,age,grade));
+        this.students.putIfAbsent(name, new Student(name, age, grade));
     }
 }
