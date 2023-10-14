6f0964805edd62100437465474eb69cd0b2790fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-14 22:54:27 +0300

    04

--- 04_JavaOOP/_01_WorkingWithAbstraction/_04_HotelReservation/Main.java
+++ 04_JavaOOP/_01_WorkingWithAbstraction/_04_HotelReservation/Main.java
@@ -5,13 +5,18 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         String[] console = scan.nextLine().split("\\s+");
+        
         double pricePerDay = Double.parseDouble(console[0]);
         int numberOfDays = Integer.parseInt(console[1]);
         Season season = Season.valueOf(console[2].toUpperCase());
         Discount discountType = Discount.valueOf(console[3]);
 
-        PriceCalculator priceCalculator = new PriceCalculator(pricePerDay,numberOfDays,season,discountType);
+        PriceCalculator priceCalculator = new PriceCalculator(pricePerDay, numberOfDays, season, discountType);
+        
         System.out.println(priceCalculator.returnPrice());
+        
+        scan.close();
     }
 }
