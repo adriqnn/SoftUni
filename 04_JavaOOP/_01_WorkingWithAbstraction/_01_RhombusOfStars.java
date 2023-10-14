47b18dc27844bc6d9676cb53c3897907b22ad778
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-14 22:27:03 +0300

    01

--- 04_JavaOOP/_01_WorkingWithAbstraction/_01_RhombusOfStars.java
+++ 04_JavaOOP/_01_WorkingWithAbstraction/_01_RhombusOfStars.java
@@ -23,8 +23,6 @@ public class _01_RhombusOfStars {
     
     private String printMiddle(){
         return repeatAndPrintString(n, "* ") + System.lineSeparator();
-
-        
     }
     
     private String printBottom(){
