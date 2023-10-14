dff6d87d623de776fe50d74cf36f0485c310f5d1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-14 22:29:50 +0300

    01

--- 04_JavaOOP/_01_WorkingWithAbstraction/_01_RhombusOfStarsV3.java
+++ 04_JavaOOP/_01_WorkingWithAbstraction/_01_RhombusOfStarsV3.java
@@ -2,19 +2,25 @@ package _04_JavaOOP._01_WorkingWithAbstraction;
 
 import java.util.Scanner;
 
-public class _02_RhombusOfStarsV3 {
+public class _01_RhombusOfStarsV3 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         int n = Integer.parseInt(scan.nextLine());
+        
         //printRhombus(n);
         _01_RhombusOfStars rhombus = new _01_RhombusOfStars(n);
         System.out.println(rhombus.getRhombus());
+        
+        scan.close();
     }
+    
     public static void printRhombus(int n){
         printTop(n);
         printMiddle(n);
         printBottom(n);
     }
+    
     public static void printTop(int n){
         for (int i = 1; i < n; i++) {
             repeatAndPrintString(n-i," ");
@@ -22,10 +28,12 @@ public class _02_RhombusOfStarsV3 {
             System.out.println();
         }
     }
+    
     public static void printMiddle(int n){
         repeatAndPrintString(n,"* ");
         System.out.println();
     }
+    
     public static void printBottom(int n){
         for (int i = 1; i < n; i++) {
             repeatAndPrintString(i," ");
@@ -33,9 +41,11 @@ public class _02_RhombusOfStarsV3 {
             System.out.println();
         }
     }
+    
     public static void repeatAndPrintString(int count, String str){
         for (int i = 0; i < count; i++) {
             System.out.print(str);
         }
     }
+    
 }
