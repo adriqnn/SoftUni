0052d2ab43a243ec9545ff01a0060772122dd239
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-16 20:35:38 +0300

    06

--- 04_JavaOOP/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Gem.java
+++ 04_JavaOOP/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Gem.java
@@ -10,11 +10,11 @@ public class Gem {
     }
 
     public long getValue() {
-        return value;
+        return this.value;
     }
 
     public String getName() {
-        return name;
+        return this.name;
     }
 
     public void setValue(long value) {
