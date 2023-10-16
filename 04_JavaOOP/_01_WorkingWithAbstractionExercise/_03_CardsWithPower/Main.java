6f290c8ae4c5e0283970d4c7cf8f4ccd6734148b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-16 19:52:12 +0300

    03

--- 04_JavaOOP/_01_WorkingWithAbstractionExercise/_03_CardsWithPower/Main.java
+++ 04_JavaOOP/_01_WorkingWithAbstractionExercise/_03_CardsWithPower/Main.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
         CardRanks cardRank = CardRanks.valueOf(scan.nextLine());
         CardSuits cardSuit = CardSuits.valueOf(scan.nextLine());
-        //System.out.println("Card name: %s of %s; Card power: %d",);
+        
+        Card card = new Card(cardSuit, cardRank);
 
-        Card card = new Card(cardSuit,cardRank);
         System.out.println(card);
+
+        scan.close();
     }
 }
