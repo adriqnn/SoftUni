a83f78ba6697bbb1213d8c621d7a17c0809e93d6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-16 19:57:43 +0300

    04

--- 04_JavaOOP/_01_WorkingWithAbstractionExercise/_04_TrafficLights/Main.java
+++ 04_JavaOOP/_01_WorkingWithAbstractionExercise/_04_TrafficLights/Main.java
@@ -8,19 +8,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         Color[] signals = Arrays.stream(scan.nextLine().split(" ")).map(Color::valueOf).toArray(Color[]::new);
         List<TrafficLight> trafficLightList = new ArrayList<>();
+        
         for(Color color : signals){
             TrafficLight trafficLight = new TrafficLight(color);
             trafficLightList.add(trafficLight);
         }
+        
         int n = Integer.parseInt(scan.nextLine());
+        
         for (int i = 0; i < n; i++) {
             for (TrafficLight trafficLight : trafficLightList){
                 trafficLight.changeColor();
+                
                 System.out.print(trafficLight + " ");
             }
+            
             System.out.println();
         }
+        
+        scan.close();
     }
 }
