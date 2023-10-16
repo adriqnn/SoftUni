d4caf9d48ee7b33bce0fabf2de39d512bc41a20a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-16 20:43:04 +0300

    06

--- 04_JavaOOP/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Main.java
+++ 04_JavaOOP/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Main.java
@@ -14,19 +14,23 @@ public class Main {
         for (int i = 0; i < itemsString.length; i += 2) {
             String nameOf = itemsString[i];
             long value = Long.parseLong(itemsString[i+1]);
+
             if(itemsString[i].equalsIgnoreCase("GOLD")){
                 Gold gold = new Gold(value);
                 valuables.addGold(gold);
             }else if(itemsString[i].toUpperCase().endsWith("GEM") && itemsString[i].length() > 3){
-                Gem gem = new Gem(nameOf,value);
+                Gem gem = new Gem(nameOf, value);
                 valuables.addGems(gem);
             }else if(itemsString[i].length() == 3){
-                Cash cash = new Cash(nameOf,value);
+                Cash cash = new Cash(nameOf, value);
                 valuables.addCash(cash);
             }else{
                 continue;
             }
         }
+
         System.out.println(valuables.printValuablesBag());
+
+        scan.close();
     }
 }
