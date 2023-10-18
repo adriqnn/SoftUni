64ef9c7b760c9ad1daf7b210eea9c11b952893f6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-18 19:40:19 +0300

    02

--- 04_JavaOOP/_02_Encapsulation/_02_SalaryIncrease/Person.java
+++ 04_JavaOOP/_02_Encapsulation/_02_SalaryIncrease/Person.java
@@ -6,11 +6,6 @@ public class Person {
     private int age;
     private double salary;
 
-
-    public String getLastName() {
-        return lastName;
-    }
-
     public Person(String firstName, String lastName, int age, double salary) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -18,36 +13,41 @@ public class Person {
         this.salary = salary;
     }
 
-
     public void increaseSalary(double percentage){
         double sal = this.salary;
+        
         if(this.getAge() < 30){
             percentage /= 2;
-            sal += (percentage/100)*sal;
+            sal += (percentage / 100) * sal;
         }else {
-            sal += (percentage/100)*sal;
+            sal += (percentage / 100) * sal;
         }
+        
         this.setSalary(sal);
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
     }
 
     @Override
     public String toString(){
         return this.firstName + " " + this.lastName + " gets " + this.salary + " leva";
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
-        this.salary = salary;
     }
 }
