273f3a48f916f4917c60f496084345e351d8bbed
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-18 19:33:19 +0300

    01

--- 04_JavaOOP/_02_Encapsulation/_01_SortByNameAndAge/Main.java
+++ 04_JavaOOP/_02_Encapsulation/_01_SortByNameAndAge/Main.java
@@ -10,26 +10,29 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
         int n = Integer.parseInt(reader.readLine());
-
         List<Person> people = new ArrayList<>();
 
         for (int i = 0; i < n; i++) {
             String[] input = reader.readLine().split(" ");
-            people.add(new Person(input[0],input[1],Integer.parseInt(input[2])));
+            people.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
         }
 
         Collections.sort(people, (firstPerson, secondPerson) -> {
             int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+            
             if(sComp != 0){
                 return sComp;
             }else{
-                return Integer.compare(firstPerson.getAge(),secondPerson.getAge());
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
             }
         });
 
         for (Person person : people) {
             System.out.println(person.toString());
         }
+        
+        reader.close();
     }
 }
