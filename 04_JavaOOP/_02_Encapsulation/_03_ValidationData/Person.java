1dc9d21e20ac34b6bcf9b0c0a81161b692385a36
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-18 19:43:08 +0300

    03

--- 04_JavaOOP/_02_Encapsulation/_03_ValidationData/Person.java
+++ 04_JavaOOP/_02_Encapsulation/_03_ValidationData/Person.java
@@ -6,10 +6,6 @@ public class Person {
     private int age;
     private double salary;
 
-    public String getLastName() {
-        return lastName;
-    }
-
     public Person(String firstName, String lastName, int age, double salary) {
         this.setFirstName(firstName);
         this.setLastName(lastName);
@@ -17,10 +13,15 @@ public class Person {
         this.setSalary(salary);
     }
 
+    public String getLastName() {
+        return this.lastName;
+    }
+
     private void setSalary(double salary) {
         if (salary < 460) {
             throw new IllegalArgumentException("Salary cannot be less than 460 leva");
         }
+        
         this.salary = salary;
     }
 
@@ -28,6 +29,7 @@ public class Person {
         if (age <= 0) {
             throw new IllegalArgumentException("Age cannot be zero or negative integer");
         }
+        
         this.age = age;
     }
 
@@ -41,35 +43,34 @@ public class Person {
         this.firstName = firstName;
     }
 
-    private void ensureNameLength(String nameParam,String fieldName){
+    private void ensureNameLength(String nameParam, String fieldName){
         if(nameParam.length() < 3){
             throw new IllegalArgumentException(fieldName + " name cannot be less than 3 symbols");
         }
     }
 
     public void increaseSalary(double increment) {
-        if (age < 30) {
+        if (this.age < 30) {
             increment = increment / 2;
         }
 
         this.salary = this.salary * (1.00 + increment / 100);
     }
 
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
 
     @Override
     public String toString(){
         return this.firstName + " " + this.lastName + " gets " + this.salary + " leva";
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public double getSalary() {
-        return salary;
     }
 }
