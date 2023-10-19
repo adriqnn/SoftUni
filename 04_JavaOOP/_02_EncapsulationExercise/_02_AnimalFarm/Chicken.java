0db93895993ff0a82b3c34fa18ce0f7b4b1292d6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-19 22:03:55 +0300

    02

--- 04_JavaOOP/_02_EncapsulationExercise/_02_AnimalFarm/Chicken.java
+++ 04_JavaOOP/_02_EncapsulationExercise/_02_AnimalFarm/Chicken.java
@@ -13,14 +13,17 @@ public class Chicken {
         if(age <= 0 || age > 15){
             throw new IllegalArgumentException("Age should be between 0 and 15.");
         }
+        
         this.age = age;
     }
 
     private void setName(String name) {
         name = name.trim();
+        
         if(name.isEmpty()){
             throw new IllegalArgumentException("Name cannot be empty.");
         }
+        
         this.name = name;
     }
 
@@ -34,9 +37,9 @@ public class Chicken {
     }
 
     private double calculateProductPerDay(){
-        if(age < 6){
+        if(this.age < 6){
             return 2;
-        }else if(age < 11){
+        }else if(this.age < 11){
             return 1;
         }else{
             return 0.75;
