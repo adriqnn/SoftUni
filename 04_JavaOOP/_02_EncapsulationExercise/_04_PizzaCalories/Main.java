9f1dbed1138960b7b411ae9d1195755c884f79dc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-19 22:17:31 +0300

    04

--- 04_JavaOOP/_02_EncapsulationExercise/_04_PizzaCalories/Main.java
+++ 04_JavaOOP/_02_EncapsulationExercise/_04_PizzaCalories/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._02_EncapsulationExercise._04_PizzaCalories;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Pizza, Topping and Dough
     }
 }
