78fca20cdfdf2e776044245e8a2d6c2fa570c306
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-20 21:36:11 +0300

    02

--- 04_JavaOOP/_03_InheritanceExercise/_02_Zoo/Main.java
+++ 04_JavaOOP/_03_InheritanceExercise/_02_Zoo/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._03_InheritanceExercise._02_Zoo;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Animal, Mammal, Reptile, Bear, Gorilla, Lizard and Snake
     }
 }
