57c4abdcfd6afb6e9537115dd64d118d600e19db
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-20 22:07:13 +0300

    03

--- 04_JavaOOP/_03_InheritanceExercise/_03_PlayersAndMonsters/Main.java
+++ 04_JavaOOP/_03_InheritanceExercise/_03_PlayersAndMonsters/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._03_InheritanceExercise._03_PlayersAndMonsters;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Hero, Elf, Knight, Wizard, MuseElf, DarkKnight, DarkWizard, BladeKnight and SoulMaster
     }
 }
