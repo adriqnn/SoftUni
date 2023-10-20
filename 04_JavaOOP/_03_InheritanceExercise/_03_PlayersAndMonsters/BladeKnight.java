44fd32be1a169f5164a3b15f42769740ea35b735
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-20 21:59:39 +0300

    03

--- 04_JavaOOP/_03_InheritanceExercise/_03_PlayersAndMonsters/BladeKnight.java
+++ 04_JavaOOP/_03_InheritanceExercise/_03_PlayersAndMonsters/BladeKnight.java
@@ -1,6 +1,7 @@
 package _04_JavaOOP._03_InheritanceExercise._03_PlayersAndMonsters;
 
 public class BladeKnight extends DarkKnight{
+    
     public BladeKnight(String username, int level) {
         super(username, level);
     }
