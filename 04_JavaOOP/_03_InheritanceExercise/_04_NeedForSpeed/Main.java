1f1c63dbdd4aa412dd172c0f51f584a501087718
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 21:19:23 +0300

    04

--- 04_JavaOOP/_03_InheritanceExercise/_04_NeedForSpeed/Main.java
+++ 04_JavaOOP/_03_InheritanceExercise/_04_NeedForSpeed/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._03_InheritanceExercise._04_NeedForSpeed;
 
 public class Main {
     public static void main(String[] args) {
-
-    }
+        // Test Vehicle, Car, Motorcycle, FamilyCar, SportCar, CrossMotorcycle and RaceMotorcycle
+    }   
 }
