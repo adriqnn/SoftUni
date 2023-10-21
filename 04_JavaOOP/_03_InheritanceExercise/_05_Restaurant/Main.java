4cadb721ae4c1dd86b117b7320c99338cfcea22d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:24:24 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Main.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Product, Food, Beverage, Desert, MainDish, Starter, ColdBeverage, HotBeverage, Salmon, Soup, Cake, Coffee and Tea
     }
 }
