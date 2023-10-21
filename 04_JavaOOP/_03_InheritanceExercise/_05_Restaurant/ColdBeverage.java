abaf946e2c08eecae1a473570c2c5a3a1b8e9fdf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:16:08 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/ColdBeverage.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/ColdBeverage.java
@@ -3,6 +3,7 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 import java.math.BigDecimal;
 
 public class ColdBeverage extends Beverage{
+    
     public ColdBeverage(String name, BigDecimal price, double milliliters) {
         super(name, price, milliliters);
     }
