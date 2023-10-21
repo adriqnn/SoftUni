9b8960f9d5f979448944fee4d7df898ebe22a274
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:18:52 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Salmon.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Salmon.java
@@ -2,7 +2,6 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 
 import java.math.BigDecimal;
 
-
 public class Salmon extends MainDish{
     private final static double SALMON_GRAMS = 22;
 
