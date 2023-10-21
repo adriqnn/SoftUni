c8327ee278e9033ff3d3d70d9a50a642a20d52a0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:17:15 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/MainDish.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/MainDish.java
@@ -3,6 +3,7 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 import java.math.BigDecimal;
 
 public class MainDish extends Food{
+    
     public MainDish(String name, BigDecimal price, double grams) {
         super(name, price, grams);
     }
