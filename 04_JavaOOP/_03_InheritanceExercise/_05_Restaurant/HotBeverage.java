2f2ebb31cbb7c4f5e0b47089ade5129f26feb7d5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:16:59 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/HotBeverage.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/HotBeverage.java
@@ -3,6 +3,7 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 import java.math.BigDecimal;
 
 public class HotBeverage extends Beverage {
+    
     public HotBeverage(String name, BigDecimal price, double milliliters) {
         super(name, price, milliliters);
     }
