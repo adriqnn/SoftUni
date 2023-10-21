3742b1765615d71f0b7183af56a5e0d783bfaa59
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:31:36 +0300

    06

--- 04_JavaOOP/_03_InheritanceExercise/_06_Animals/Dog.java
+++ 04_JavaOOP/_03_InheritanceExercise/_06_Animals/Dog.java
@@ -1,6 +1,7 @@
 package _04_JavaOOP._03_InheritanceExercise._06_Animals;
 
 public class Dog extends Animal{
+    
     public Dog(String name, int age, String gender) {
         super(name, age, gender);
     }
