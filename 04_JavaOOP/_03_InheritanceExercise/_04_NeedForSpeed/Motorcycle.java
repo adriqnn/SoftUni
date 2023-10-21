ed150a3ede2e7f743a370434c2c26a757e9cf644
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 21:19:33 +0300

    04

--- 04_JavaOOP/_03_InheritanceExercise/_04_NeedForSpeed/Motorcycle.java
+++ 04_JavaOOP/_03_InheritanceExercise/_04_NeedForSpeed/Motorcycle.java
@@ -1,6 +1,7 @@
 package _04_JavaOOP._03_InheritanceExercise._04_NeedForSpeed;
 
 public class Motorcycle extends Vehicle{
+    
     public Motorcycle(double fuel, int horsePower) {
         super(fuel, horsePower);
     }
