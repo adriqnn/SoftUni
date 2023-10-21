51965266f72962a936f8bf93ca4e283f1b53abe3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-21 22:19:00 +0300

    05

--- 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Soup.java
+++ 04_JavaOOP/_03_InheritanceExercise/_05_Restaurant/Soup.java
@@ -3,6 +3,7 @@ package _04_JavaOOP._03_InheritanceExercise._05_Restaurant;
 import java.math.BigDecimal;
 
 public class Soup extends Starter{
+    
     public Soup(String name, BigDecimal price, double grams) {
         super(name, price, grams);
     }
