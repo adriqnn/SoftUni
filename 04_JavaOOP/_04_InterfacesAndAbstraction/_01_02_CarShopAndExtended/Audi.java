1d4bb7c5ea2ce21685660b3acff33ac6f0fea0f7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-22 21:00:46 +0300

    01

--- 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Audi.java
+++ 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Audi.java
@@ -1,12 +1,11 @@
 package _04_JavaOOP._04_InterfacesAndAbstraction._01_02_CarShopAndExtended;
 
 public class Audi extends CarImpl implements Rentable {
-
     private final Integer minRentDay;
     private final Double pricePerDay;
 
     public Audi(String model, String color, Integer horsePower, String countryProduced, Integer minRentDay, Double pricePerDay) {
-        super(model,color,horsePower,countryProduced);
+        super(model, color, horsePower, countryProduced);
         this.minRentDay = minRentDay;
         this.pricePerDay = pricePerDay;
     }
