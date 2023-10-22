e3206e8a49b16824cfb457174d0426e554c8c1a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-22 22:09:00 +0300

    05

--- 04_JavaOOP/_04_InterfacesAndAbstraction/_05_BorderControl/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstraction/_05_BorderControl/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._04_InterfacesAndAbstraction._05_BorderControl;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Identifiable, Citizen and Robot
     }
 }
