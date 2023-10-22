ba579d81cfc0b1c2d6bd3ebab8f53f2c29da1058
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-22 21:03:43 +0300

    01

--- 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Seat.java
+++ 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Seat.java
@@ -4,7 +4,7 @@ public class Seat extends CarImpl implements Sellable{
     private final Double price;
 
     public Seat(String model, String color, Integer horsePower, String countryProduced, Double price){
-        super(model,color,horsePower,countryProduced);
+        super(model, color, horsePower, countryProduced);
         this.price = price;
     }
 
