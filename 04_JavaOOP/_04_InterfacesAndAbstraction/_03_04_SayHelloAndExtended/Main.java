c87135a98b060a97f1137ebd9b77a8edd5357117
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-22 21:22:21 +0300

    03

--- 04_JavaOOP/_04_InterfacesAndAbstraction/_03_04_SayHelloAndExtended/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstraction/_03_04_SayHelloAndExtended/Main.java
@@ -2,6 +2,7 @@ package _04_JavaOOP._04_InterfacesAndAbstraction._03_04_SayHelloAndExtended;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Person, BasePerson, European, Bulgarian and Chinese
     }
 }
+
