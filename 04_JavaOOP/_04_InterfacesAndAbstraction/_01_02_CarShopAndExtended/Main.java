232e8da0238c24836241d5edb3ddf86004e1b45a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-22 21:02:37 +0300

    01

--- 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstraction/_01_02_CarShopAndExtended/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._04_InterfacesAndAbstraction._01_02_CarShopAndExtended;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Car, Sellable, Rentable, CarImpl, Audi and Seat
     }
 }
