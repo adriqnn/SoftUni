c3a61f8f7b0a1bdc4e4a0485ccc173cd1c8d7ba1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-24 01:53:37 +0300

    07

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._04_InterfacesAndAbstractionExercise._07_CollectionHierarchy
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Collection, Addable, AddRemovable, MyList, AddCollection, AddRemoveCollection and MyListImpl
     }
 }
