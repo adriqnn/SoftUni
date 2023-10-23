c4febcf8434d34c59a9e08a09e636d86480875a1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-23 23:05:13 +0300

    05

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_05_Telephony/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_05_Telephony/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._04_InterfacesAndAbstractionExercise._05_Telephony;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Callable, Browsable and Smartphone
     }
 }
