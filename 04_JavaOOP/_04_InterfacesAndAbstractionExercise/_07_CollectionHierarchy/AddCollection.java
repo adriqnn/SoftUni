882fecb81d1e98d6218229d4e6416d48867540af
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-24 01:50:49 +0300

    07

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/AddCollection.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/AddCollection.java
@@ -1,6 +1,5 @@
 package _04_JavaOOP._04_InterfacesAndAbstractionExercise._07_CollectionHierarchy;
 
-
 public class AddCollection extends Collection implements Addable{
     @Override
     public int add(String item) {
