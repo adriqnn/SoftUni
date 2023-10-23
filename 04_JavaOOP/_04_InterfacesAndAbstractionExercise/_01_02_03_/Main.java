628df79fb8e5c2331c2d1b50465ac66899de22f5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-23 22:45:51 +0300

    01

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_01_02_03_/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_01_02_03_/Main.java
@@ -2,6 +2,7 @@ package _04_JavaOOP._04_InterfacesAndAbstractionExercise._01_02_03_;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Birthable, Person, Identifiable, Citizen, Pet and Robot
     }
 }
+
