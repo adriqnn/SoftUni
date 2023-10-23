5cab6fc9dd89f3520848ffed6b56b20e5eab9172
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-24 01:48:40 +0300

    07

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/MyListImpl.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_07_CollectionHierarchy/MyListImpl.java
@@ -8,7 +8,7 @@ public class MyListImpl extends Collection implements MyList{
 
     @Override
     public int add(String item) {
-        super.getItems().add(0,item);
+        super.getItems().add(0, item);
         return 0;
     }
 
