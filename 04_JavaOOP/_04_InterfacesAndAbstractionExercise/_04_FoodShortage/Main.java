12c641d228fc51ea5279f97c405109a34438096c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-23 22:56:46 +0300

    04

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_04_FoodShortage/Main.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_04_FoodShortage/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._04_InterfacesAndAbstractionExercise._04_FoodShortage;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Person, Identifiable, Buyer, Citizen and Rebel
     }
 }
