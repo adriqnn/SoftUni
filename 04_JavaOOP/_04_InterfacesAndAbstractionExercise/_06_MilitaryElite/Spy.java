d7ec37a719bd823aebeb9e58a542fcffa3991844
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-24 01:41:18 +0300

    06

--- 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_06_MilitaryElite/Spy.java
+++ 04_JavaOOP/_04_InterfacesAndAbstractionExercise/_06_MilitaryElite/Spy.java
@@ -3,7 +3,6 @@ package _04_JavaOOP._04_InterfacesAndAbstractionExercise._06_MilitaryElite;
 public class Spy extends Soldier{
     private String codeNumber;
 
-
     public Spy(int id, String firstName, String lastName,String number) {
         super(id, firstName, lastName);
         this.codeNumber = number;
@@ -12,6 +11,7 @@ public class Spy extends Soldier{
     @Override
     public String toString() {
         StringBuilder sb =  new StringBuilder();
+        
         sb.append("Name: ").append(this.getFirstName()).append(" ").append(this.getLastName()).append(" Id: ").append(this.getId()).append(System.lineSeparator());
         sb.append("Code Number: ").append(this.codeNumber).append(System.lineSeparator());
         return sb.toString();
