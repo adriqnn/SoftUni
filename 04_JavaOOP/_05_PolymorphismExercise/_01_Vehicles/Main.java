605aad0ab8139fabac47d941e81b63b9f4353aca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-25 06:10:27 +0300

    01

--- 04_JavaOOP/_05_PolymorphismExercise/_01_Vehicles/Main.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_01_Vehicles/Main.java
@@ -11,21 +11,22 @@ public class Main {
         String[] tokens = scan.nextLine().split("\\s+");
         double carFuelAmount = Double.parseDouble(tokens[1]);
         double carConsumption = Double.parseDouble(tokens[2]);
-        Vehicle car = new Car(carFuelAmount,carConsumption);
+        Vehicle car = new Car(carFuelAmount, carConsumption);
 
         tokens = scan.nextLine().split("\\s+");
         double truckFuelAmount = Double.parseDouble(tokens[1]);
         double truckConsumption = Double.parseDouble(tokens[2]);
-        Vehicle truck = new Truck(truckFuelAmount,truckConsumption);
+        Vehicle truck = new Truck(truckFuelAmount, truckConsumption);
 
-        Map<String,Vehicle> vehicles = new LinkedHashMap<>();
-        vehicles.put("Car",car);
-        vehicles.put("Truck",truck);
+        Map<String, Vehicle> vehicles = new LinkedHashMap<>();
+        vehicles.put("Car", car);
+        vehicles.put("Truck", truck);
 
         int n = Integer.parseInt(scan.nextLine());
 
         for (int i = 0; i < n; i++) {
             tokens = scan.nextLine().split("\\s+");
+            
             String commandName = tokens[0];
             String vehicleType = tokens[1];
             double amount = Double.parseDouble(tokens[2]);
@@ -39,6 +40,9 @@ public class Main {
                     break;
             }
         }
+        
         vehicles.values().forEach(System.out::println);
+
+        scan.close();
     }
 }
