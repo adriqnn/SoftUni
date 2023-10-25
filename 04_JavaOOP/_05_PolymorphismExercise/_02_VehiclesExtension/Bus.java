ec2197e2ddf26704475e7e0013287fb0fc15f683
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-25 06:31:29 +0300

    02

--- 04_JavaOOP/_05_PolymorphismExercise/_02_VehiclesExtension/Bus.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_02_VehiclesExtension/Bus.java
@@ -1,12 +1,22 @@
 package _04_JavaOOP._05_PolymorphismExercise._02_VehiclesExtension;
 
 public class Bus extends VehicleImpl{
+    private static final double AC_ADDITIONAL_CONSUMPTION = 1.4;
+    private boolean isEmpty;
+
     public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
         super(fuelQuantity, fuelConsumption, tankCapacity);
+        isEmpty = true;
     }
 
     @Override
-    public String drive(double distance) {
-        return super.drive(distance);
+    public void setFuelConsumption(double fuelConsumption) {
+        if(!this.isEmpty){
+            super.setFuelConsumption(fuelConsumption + AC_ADDITIONAL_CONSUMPTION);
+        }
+    }
+
+    public void setEmpty(boolean empty) {
+        isEmpty = empty;
     }
 }
