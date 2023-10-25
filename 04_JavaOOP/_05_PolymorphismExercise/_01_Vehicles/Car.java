59bf9d77a392e18b0a054805a75f86155744d2b9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-25 06:09:16 +0300

    01

--- 04_JavaOOP/_05_PolymorphismExercise/_01_Vehicles/Car.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_01_Vehicles/Car.java
@@ -3,7 +3,6 @@ package _04_JavaOOP._05_PolymorphismExercise._01_Vehicles;
 public class Car extends VehicleImpl{
     private static final double AC_ADDITIONAL_CONSUMPTION = 0.9;
 
-
     public Car(double fuelQuantity, double fuelConsumption) {
         super(fuelQuantity, fuelConsumption);
     }
