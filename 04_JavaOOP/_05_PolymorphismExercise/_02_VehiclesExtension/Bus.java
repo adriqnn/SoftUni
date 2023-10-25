f5200e01d0f53c087948eec72e2244045a89cdd3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-25 06:15:24 +0300

    02

--- 04_JavaOOP/_05_PolymorphismExercise/_02_VehiclesExtension/Bus.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_02_VehiclesExtension/Bus.java
@@ -1,6 +1,5 @@
 package _04_JavaOOP._05_PolymorphismExercise._02_VehiclesExtension;
 
-
 public class Bus extends VehicleImpl{
     public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
         super(fuelQuantity, fuelConsumption, tankCapacity);
