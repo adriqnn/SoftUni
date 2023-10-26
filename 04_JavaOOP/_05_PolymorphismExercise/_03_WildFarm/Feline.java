8771aab6359adec4b998e6c134e602417cd9c438
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-26 21:39:06 +0300

    03

--- 04_JavaOOP/_05_PolymorphismExercise/_03_WildFarm/Feline.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_03_WildFarm/Feline.java
@@ -4,5 +4,4 @@ public abstract class Feline extends Mammal{
     public Feline(String name, String type, double weight, String livingRegion) {
         super(name, type, weight, livingRegion);
     }
-
 }
