bc4db6eb13dba645899a2839da8b2f2cab1aafdf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-26 21:38:53 +0300

    03

--- 04_JavaOOP/_05_PolymorphismExercise/_03_WildFarm/Cat.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_03_WildFarm/Cat.java
@@ -3,8 +3,8 @@ package _04_JavaOOP._05_PolymorphismExercise._03_WildFarm;
 import java.text.DecimalFormat;
 
 public class Cat extends Feline {
-
     private String breed;
+    
     public Cat(String name, String type, double weight, String livingRegion, String breed) {
         super(name, type, weight, livingRegion);
         this.breed = breed;
