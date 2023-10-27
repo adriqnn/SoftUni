3fc3af39747fd32876d842a1cdfcc1acd6cd2641
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-27 06:11:46 +0300

    05

--- 04_JavaOOP/_05_PolymorphismExercise/_05_Calculator/Extensions.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_05_Calculator/Extensions.java
@@ -1,5 +1,6 @@
 package _04_JavaOOP._05_PolymorphismExercise._05_Calculator;
 
+
 public abstract class Extensions {
     static InputInterpreter buildInterpreter(CalculationEngine engine){
         return new InputInterpreter(engine);
