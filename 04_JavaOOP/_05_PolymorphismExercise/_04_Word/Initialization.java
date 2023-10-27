db9c6eca136a5e783cb47c73486693fc5600c114
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-27 06:03:56 +0300

    03

--- 04_JavaOOP/_05_PolymorphismExercise/_04_Word/Initialization.java
+++ 04_JavaOOP/_05_PolymorphismExercise/_04_Word/Initialization.java
@@ -1,6 +1,6 @@
 package _04_JavaOOP._05_PolymorphismExercise._04_Word;
 
-public abstract class Initialization implements CommandInterface{
+public abstract class Initialization implements CommandInterface {
     public static CommandInterface buildCommandInterface(StringBuilder text){
         return new CommandExecution(text);
     }
