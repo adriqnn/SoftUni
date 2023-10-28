586193df0298b28f0238250a5d2af4b6b1f735fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:37:20 +0300

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/_02_DrawingShape/Main.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/_02_DrawingShape/Main.java
@@ -2,7 +2,6 @@ package _04_JavaOOP._06_SOLID._02_OpenClosed._02_DrawingShape;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Open/Closed example
     }
 }
-
