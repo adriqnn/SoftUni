000f24288f878099596774939f10214107162075
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:27:47 +0300

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/_01_FileStream/Progress.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/_01_FileStream/Progress.java
@@ -3,13 +3,11 @@ package _04_JavaOOP._06_SOLID._02_OpenClosed._01_FileStream;
 public class Progress {
     private final File file;
 
-    public Progress(File file)
-    {
+    public Progress(File file) {
         this.file = file;
     }
 
-    public int getCurrentPercent()
-    {
+    public int getCurrentPercent() {
         return this.file.getSent() * 100 / this.file.getLength();
     }
 }
