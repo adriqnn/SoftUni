98ff450187594d2ee56e0f8f2d1f013d306fa5e7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:49:15 +0300

    04

--- 04_JavaOOP/_06_SOLID/_04_InterfaceSegregation/Main.java
+++ 04_JavaOOP/_06_SOLID/_04_InterfaceSegregation/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._04_InterfaceSegregation;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Interface Segregation example
     }
 }
