c9e21233dc7257913d33596c1451198df9b7494c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:25:35 +0300

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/_01_FileStream/Main.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/_01_FileStream/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._02_OpenClosed._01_FileStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Open/Closed example
     }
 }
