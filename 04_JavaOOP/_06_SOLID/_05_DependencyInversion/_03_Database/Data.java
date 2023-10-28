d40557af7454291a37ff2dfe0744d3da7cf66204
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:55:12 +0300

    05

--- 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_03_Database/Data.java
+++ 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_03_Database/Data.java
@@ -1,26 +1,22 @@
 package _04_JavaOOP._06_SOLID._05_DependencyInversion._03_Database;
 
 public class Data {
-    public Iterable<Integer> courseIds()
-    {
+    public Iterable<Integer> courseIds() {
         // return course ids
         return null;
     }
 
-    public Iterable<String> courseNames()
-    {
+    public Iterable<String> courseNames() {
         // return course names
         return null;
     }
 
-    public Iterable<String> search(String substring)
-    {
+    public Iterable<String> search(String substring) {
         // return found results
         return null;
     }
 
-    public String getCourseById(int id)
-    {
+    public String getCourseById(int id) {
         // return course by id
         return null;
     }
