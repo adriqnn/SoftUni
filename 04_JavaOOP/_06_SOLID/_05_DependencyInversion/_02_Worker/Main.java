710ed04e00e36465a73da74ae9106381bcc72b10
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:52:53 +0300

    05

--- 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_02_Worker/Main.java
+++ 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_02_Worker/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._05_DependencyInversion._02_Worker;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Dependency Inversion example
     }
 }
