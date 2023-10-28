cd3a16c4e8ee1328e755c1d5bcd1f550c273ccd4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:24:27 +0300

    01

--- 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/Main.java
+++ 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._01_SingleResponsibility;
 
 public class Main {
     public static void main(String[] args) {
-        // Single Responsibility short example
+        // Single Responsibility example
     }
 }
