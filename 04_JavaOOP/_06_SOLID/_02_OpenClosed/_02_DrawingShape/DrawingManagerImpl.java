f56f03c206fb39ee2112c63a7301668680fcc70a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:36:11 +0300

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/_02_DrawingShape/DrawingManagerImpl.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/_02_DrawingShape/DrawingManagerImpl.java
@@ -7,22 +7,18 @@ public class DrawingManagerImpl implements DrawingManager {
 
     @Override
     public void draw(Shape shape) {
-        if (shape instanceof Circle)
-        {
-            this.drawCircle((Circle)shape);
-        }
-        else if (shape instanceof Rectangle)
-        {
-            this.drawRectangle(((Rectangle)shape));
+        if (shape instanceof Circle) {
+            this.drawCircle((Circle) shape);
+        } else if (shape instanceof Rectangle) {
+            this.drawRectangle((Rectangle) shape);
         }
     }
-    public void drawCircle(Circle circle)
-    {
+    
+    public void drawCircle(Circle circle) {
         // Draw Circle
     }
 
-    public void drawRectangle(Rectangle rectangle)
-    {
+    public void drawRectangle(Rectangle rectangle) {
         // Draw Rectangle
     }
 }
