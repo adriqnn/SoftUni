80d35cb4d9092a75af523093a4bef3fa735e821d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:54:32 +0300

    05

--- 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_03_Database/Courses.java
+++ 04_JavaOOP/_06_SOLID/_05_DependencyInversion/_03_Database/Courses.java
@@ -1,35 +1,27 @@
 package _04_JavaOOP._06_SOLID._05_DependencyInversion._03_Database;
 
 public class Courses {
-    public void printAll()
-    {
+    public void printAll() {
         Data database = new Data();
         Iterable<String> courses = database.courseNames();
-
         //print courses
     }
 
-    public void printIds()
-    {
+    public void printIds() {
         Data database = new Data();
         Iterable<Integer>coursesIds = database.courseIds();
-
         //print course ids
     }
 
-    public void printById(int id)
-    {
+    public void printById(int id) {
         Data database = new Data();
         String course = database.getCourseById(id);
-
         // print course
     }
 
-    public void search(String substring)
-    {
+    public void search(String substring) {
         Data database = new Data();
         Iterable<String> courses = database.search(substring);
-
         // print courses
     }
 }
