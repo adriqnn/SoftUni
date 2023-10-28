6b6ab85977fbb6a6846a3bf165da68f9ba6a2139
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:39:24 +0300

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/_03_ShoppingCart/Main.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/_03_ShoppingCart/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._02_OpenClosed._03_ShoppingCart;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Open/Closed example
     }
 }
