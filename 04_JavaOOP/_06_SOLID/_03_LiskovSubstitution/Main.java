9e48794bcd11f333231777cfa04b5cc536083de0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-28 07:44:18 +0300

    03

--- 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Main.java
+++ 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._06_SOLID._03_LiskovSubstitution;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        // Liskov Substitution example
     }
 }
