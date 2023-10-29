124d02d7a54ed57ed4e695b87b95ef4f4542c63d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:28:39 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/interfaces/Appender.java
+++ 04_JavaOOP/_06_SOLIDExercise/interfaces/Appender.java
@@ -4,6 +4,7 @@ import _04_JavaOOP._06_SOLIDExercise.enums.ReportLevel;
 
 public interface Appender {
     void append(String time, String message, ReportLevel reportLevel);
+    
     void setReportLevel(ReportLevel reportLevel);
 }
 
