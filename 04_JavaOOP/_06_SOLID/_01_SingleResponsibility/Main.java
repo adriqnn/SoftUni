4123e12aed76d37f662de11c85e2150e3b795c54
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:39:45 +0200

    Delete 04_JavaOOP/_06_SOLID/_01_SingleResponsibility directory

--- 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/Main.java
+++
@@ -1,7 +0,0 @@
-package _04_JavaOOP._06_SOLID._01_SingleResponsibility;
-
-public class Main {
-    public static void main(String[] args) {
-        // Single Responsibility example
-    }
-}
--- 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/interfaces/Shape.java
+++
@@ -1,5 +0,0 @@
-package _04_JavaOOP._06_SOLID._01_SingleResponsibility.interfaces;
-
-public interface Shape {
-    void draw(Renderer renderer, DrawingRepository drawingRepository);
-}
