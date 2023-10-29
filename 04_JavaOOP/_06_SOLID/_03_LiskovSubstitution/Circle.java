2c19c4f8c543688dcf3d1f6a33549471ced26348
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:51:10 +0200

    03

---
+++ 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Circle.java
@@ -0,0 +1,23 @@
+package _04_JavaOOP._06_SOLID._03_LiskovSubstitution;
+
+public class Circle extends Shape{
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return this.radius;
+    }
+
+    public Circle setRadius(double radius) {
+        this.radius = radius;
+        return this;
+    }
+
+    @Override
+    public double getArea() {
+        return 0;
+    }
+}
