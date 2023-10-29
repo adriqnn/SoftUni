a49e565cb4f7c83982eaf8ee9004ba58f03c5b0d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:54:04 +0200

    03

--- 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Rectangle.java
+++ 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Rectangle.java
@@ -4,6 +4,11 @@ public class Rectangle extends Shape {
     private double width;
     private double height;
 
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
     public double getWidth() {
         return this.width;
     }
