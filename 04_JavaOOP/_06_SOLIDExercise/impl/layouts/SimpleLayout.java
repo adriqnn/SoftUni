cdc50c6d10594dd4c8b121d0dc05aa0bef63bbac
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:26:06 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/impl/layouts/SimpleLayout.java
+++ 04_JavaOOP/_06_SOLIDExercise/impl/layouts/SimpleLayout.java
@@ -4,8 +4,9 @@ import _04_JavaOOP._06_SOLIDExercise.enums.ReportLevel;
 import _04_JavaOOP._06_SOLIDExercise.interfaces.Layout;
 
 public class SimpleLayout implements Layout {
+    
     @Override
     public String format(String time, String message, ReportLevel reportLevel) {
-        return String.format("%s - %s - %s",time,reportLevel,message);
+        return String.format("%s - %s - %s", time, reportLevel, message);
     }
 }
