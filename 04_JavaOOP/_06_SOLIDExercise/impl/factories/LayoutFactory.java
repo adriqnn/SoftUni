f209e0024ed13feb46b340240788b4a65968e177
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:23:34 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/impl/factories/LayoutFactory.java
+++ 04_JavaOOP/_06_SOLIDExercise/impl/factories/LayoutFactory.java
@@ -10,11 +10,13 @@ public class LayoutFactory implements Factory<Layout> {
     @Override
     public Layout produce(String input) {
         Layout layout = null;
+        
         if(input.equals("SimpleLayout")){
             layout = new SimpleLayout();
         }else if(input.equals("XmlLayout")){
             layout = new XmlLayout();
         }
+        
         return layout;
     }
 }
