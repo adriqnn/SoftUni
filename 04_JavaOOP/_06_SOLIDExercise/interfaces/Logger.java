404baa71f6074daeb0384720dc051fd393739eb5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:30:20 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/interfaces/Logger.java
+++ 04_JavaOOP/_06_SOLIDExercise/interfaces/Logger.java
@@ -1,10 +1,13 @@
 package _04_JavaOOP._06_SOLIDExercise.interfaces;
 
 public interface Logger {
-
-    void logInfo(String time,String message);
-    void logWarning(String time,String message);
-    void logError(String time,String message);
-    void logCritical(String time,String message);
-    void logFatal(String time,String message);
+    void logInfo(String time, String message);
+    
+    void logWarning(String time, String message);
+    
+    void logError(String time, String message);
+    
+    void logCritical(String time, String message);
+    
+    void logFatal(String time, String message);
 }
