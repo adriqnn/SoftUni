c1299da8d727676a97be0bd29c16362baff45001
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:26:34 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/impl/layouts/XmlLayout.java
+++ 04_JavaOOP/_06_SOLIDExercise/impl/layouts/XmlLayout.java
@@ -7,7 +7,6 @@ public class XmlLayout implements Layout {
 
     @Override
     public String format(String time, String message, ReportLevel reportLevel) {
-        return String.format(
-                "<log>%n   <date>%s</date>%n   <level>%s</level>%n   <message>%s</message>%n</log>%n", time, reportLevel, message);
+        return String.format("<log>%n   <date>%s</date>%n   <level>%s</level>%n   <message>%s</message>%n</log>%n", time, reportLevel, message);
     }
 }
