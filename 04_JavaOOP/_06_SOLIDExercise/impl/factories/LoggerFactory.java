e0533cd1a2a3eb69cdc3724ad019a1f6c31d3fe7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-29 04:25:01 +0200

    01

--- 04_JavaOOP/_06_SOLIDExercise/impl/factories/LoggerFactory.java
+++ 04_JavaOOP/_06_SOLIDExercise/impl/factories/LoggerFactory.java
@@ -6,7 +6,7 @@ import _04_JavaOOP._06_SOLIDExercise.interfaces.Factory;
 import _04_JavaOOP._06_SOLIDExercise.interfaces.Logger;
 
 public class LoggerFactory implements Factory<Logger> {
-    AppenderFactory appenderFactory;
+    private AppenderFactory appenderFactory;
 
     public LoggerFactory() {
         this.appenderFactory = new AppenderFactory();
@@ -15,6 +15,7 @@ public class LoggerFactory implements Factory<Logger> {
     @Override
     public Logger produce(String input) {
         String[] tokens = input.split(System.lineSeparator());
+
         Appender[] appenders = new Appender[tokens.length];
 
         for (int i = 0; i < appenders.length; i++) {
