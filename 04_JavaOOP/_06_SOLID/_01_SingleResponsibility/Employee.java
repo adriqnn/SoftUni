08ee60f72598081c42ad24de5a749c159b025467
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-30 22:35:20 +0200

    01

---
+++ 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/Employee.java
@@ -0,0 +1,29 @@
+package _04_JavaOOP._06_SOLID._01_SingleResponsibility;
+
+// Class representing an individual employee with properties like name and salary
+
+public class Employee {
+    private String name;
+    private double salary;
+
+    public Employee(String name, double salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
