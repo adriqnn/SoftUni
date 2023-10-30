7b1363e8b27196f3944f13c9419c523db05bc1eb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-30 22:35:25 +0200

    01

---
+++ 04_JavaOOP/_06_SOLID/_01_SingleResponsibility/EmployeeData.java
@@ -0,0 +1,30 @@
+package _04_JavaOOP._06_SOLID._01_SingleResponsibility;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// Class responsible for employee data and storage
+
+public class EmployeeData {
+    private List<Employee> employees = new ArrayList<>();
+
+    public void addEmployee(Employee employee) {
+        this.employees.add(employee);
+    }
+
+    public void removeEmployee(Employee employee) {
+        this.employees.remove(employee);
+    }
+
+    public List<Employee> getAllEmployees() {
+        return this.employees;
+    }
+
+    public List<Employee> getEmployees() {
+        return this.employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+}
