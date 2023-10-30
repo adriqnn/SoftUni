a71ef6b21ad2995980c0feb5060af38e49989483
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-30 23:34:08 +0200

    04

--- 04_JavaOOP/_06_SOLID/_04_InterfaceSegregation/Main.java
+++ 04_JavaOOP/_06_SOLID/_04_InterfaceSegregation/Main.java
@@ -3,5 +3,19 @@ package _04_JavaOOP._06_SOLID._04_InterfaceSegregation;
 public class Main {
     public static void main(String[] args) {
         // Interface Segregation example
+
+        RegularWorker regularWorker = new RegularWorker();
+        SupervisorWorker supervisorWorker = new SupervisorWorker();
+        RobotWorker robotWorker = new RobotWorker();
+        ManagerEmployee managerEmployee = new ManagerEmployee();
+
+        regularWorker.work();
+
+        supervisorWorker.work();
+        supervisorWorker.eat();
+
+        robotWorker.work();
+
+        managerEmployee.manage();
     }
 }
