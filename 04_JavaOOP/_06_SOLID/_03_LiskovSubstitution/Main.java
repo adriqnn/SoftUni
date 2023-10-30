1db3ebb583af3e2197f15e6e1e5e989bebd85329
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-30 23:31:42 +0200

    03

--- 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Main.java
+++ 04_JavaOOP/_06_SOLID/_03_LiskovSubstitution/Main.java
@@ -3,5 +3,15 @@ package _04_JavaOOP._06_SOLID._03_LiskovSubstitution;
 public class Main {
     public static void main(String[] args) {
         // Liskov Substitution example
+        
+        // Create instances of different shapes
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0, 6.0);
+        Shape square = new Square(4.0);
+
+        // Calculate and display their areas
+        System.out.println("Circle Area: " + circle.calculateArea());
+        System.out.println("Rectangle Area: " + rectangle.calculateArea());
+        System.out.println("Square Area: " + square.calculateArea());
     }
 }
