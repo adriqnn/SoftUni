f696cb37f5ac981e40bdf7bab1edd6cb15d11a72
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-10-30 23:12:18 +0200

    02

--- 04_JavaOOP/_06_SOLID/_02_OpenClosed/Main.java
+++ 04_JavaOOP/_06_SOLID/_02_OpenClosed/Main.java
@@ -3,5 +3,24 @@ package _04_JavaOOP._06_SOLID._02_OpenClosed;
 public class Main {
     public static void main(String[] args) {
         // Open Closed example
+
+        // Creating items and orders
+        CartItem productItem = new ProductItem("Laptop", 1000.0);
+        CartItem serviceItem = new ServiceItem("Software Installation", 50.0);
+
+        Order inStoreOrder = new InStoreOrder();
+        Order onlineOrder = new OnlineOrder("123 Main St, City");
+
+        inStoreOrder.addItem(productItem);
+        inStoreOrder.addItem(serviceItem);
+
+        onlineOrder.addItem(productItem);
+
+        // Calculate and display total prices
+        System.out.println("Total Price for In-Store Order: $" + inStoreOrder.calculateTotalPrice());
+        inStoreOrder.checkout();
+
+        System.out.println("Total Price for Online Order: $" + onlineOrder.calculateTotalPrice());
+        onlineOrder.checkout();
     }
 }
