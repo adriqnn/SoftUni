b85729e6a2a7af0ed53c9416899993610a170360
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:38:12 +0200

    03

---
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/models/units/Gunner.java
@@ -0,0 +1,10 @@
+package _04_JavaOOP._07_ReflectionAndAnnotationExercise._03_04_05_BarracksWars.models.units;
+
+public class Gunner extends AbstractUnit{
+    private static final int GUNNER_HEALTH = 20;
+    private static final int GUNNER_DAMAGE = 20;
+
+    public Gunner() {
+        super(GUNNER_HEALTH, GUNNER_DAMAGE);
+    }
+}
