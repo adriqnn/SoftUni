8ae922f984e14aac1fd1a54e7a548528ef3d74a4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:33:09 +0200

    03

---
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/interfaces/CommandInterpreter.java
@@ -0,0 +1,5 @@
+package _04_JavaOOP._07_ReflectionAndAnnotationExercise._03_04_05_BarracksWars.interfaces;
+
+public interface CommandInterpreter {
+    Executable interpretCommand(String[] data, String commandName);
+}
