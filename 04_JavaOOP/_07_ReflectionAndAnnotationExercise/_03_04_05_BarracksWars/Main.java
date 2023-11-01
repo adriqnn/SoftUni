bd87939c697f763eff1a19d14d6a418d9566515e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:39:41 +0200

    03

--- 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/Main.java
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/Main.java
@@ -14,6 +14,7 @@ public class Main {
         UnitFactory unitFactory = new UnitFactoryImpl();
 
         Runnable engine = new Engine(repository, unitFactory);
+        
         engine.run();
     }
 }
