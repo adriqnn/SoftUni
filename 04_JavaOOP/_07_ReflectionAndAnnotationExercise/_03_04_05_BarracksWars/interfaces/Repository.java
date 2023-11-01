e268fa671c46573535e30285c86242fbcede23c4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:33:25 +0200

    03

---
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/interfaces/Repository.java
@@ -0,0 +1,8 @@
+package _04_JavaOOP._07_ReflectionAndAnnotationExercise._03_04_05_BarracksWars.interfaces;
+
+public interface Repository {
+    void addUnit(Unit unit);
+    String getStatistics();
+    void removeUnit(String unitType);
+}
+
