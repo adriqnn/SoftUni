13520b44574d69072ee7b9aea3e3d05d103cb92f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:29:40 +0200

    Delete 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/interfaces directory

--- 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/interfaces/Attacker.java
+++
@@ -1,5 +0,0 @@
-package _04_JavaOOP._07_ReflectionAndAnnotationExercise._03_04_05_BarracksWars.interfaces;
-
-public interface Attacker {
-    int getAttackDamage();
-}
--- 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/interfaces/Destroyable.java
+++
@@ -1,6 +0,0 @@
-package _04_JavaOOP._07_ReflectionAndAnnotationExercise._03_04_05_BarracksWars.interfaces;
-
-public interface Destroyable {
-    int getHealth();
-    void setHealth(int health);
-}
