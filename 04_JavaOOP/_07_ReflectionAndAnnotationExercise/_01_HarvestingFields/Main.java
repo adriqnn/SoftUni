93fb0f28e0d22e0355289a25412100afa17628a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:13:00 +0200

    01

--- 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_01_HarvestingFields/Main.java
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_01_HarvestingFields/Main.java
@@ -7,41 +7,54 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String input = scan.nextLine();
+
         Class<RichSoilLand> clazz = RichSoilLand.class;
         Field[] declaredFields = clazz.getDeclaredFields();
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
 
         while(!input.equals("HARVEST")){
             switch(input){
                 case "public":
-					/*Arrays.stream(declaredFields).filter(f -> Modifier.isPublic(f.getModifiers()))
-							.forEach(f -> {System.out.printf("%s %s %s%n",Modifier.toString(f.getModifiers()),f.getType().getSimpleName(),f.getName());});*/
+					/*
+					Arrays.stream(declaredFields).filter(f -> Modifier.isPublic(f.getModifiers())).forEach(f -> {
+					    System.out.printf("%s %s %s%n", Modifier.toString(f.getModifiers()), f.getType().getSimpleName(), f.getName());
+					    });
+					*/
+
                     for(Field field : declaredFields){
                         if(Modifier.isPublic(field.getModifiers())){
-                            System.out.printf("%s %s %s%n",Modifier.toString(field.getModifiers()),field.getType().getSimpleName(),field.getName());
+                            System.out.printf("%s %s %s%n", Modifier.toString(field.getModifiers()), field.getType().getSimpleName(), field.getName());
                         }
                     }
+
                     break;
                 case "private":
                     for(Field field : declaredFields){
                         if(Modifier.isPrivate(field.getModifiers())){
-                            System.out.printf("%s %s %s%n",Modifier.toString(field.getModifiers()),field.getType().getSimpleName(),field.getName());
+                            System.out.printf("%s %s %s%n", Modifier.toString(field.getModifiers()), field.getType().getSimpleName(), field.getName());
                         }
                     }
+                    
                     break;
                 case "protected":
                     for(Field field : declaredFields){
                         if(Modifier.isProtected(field.getModifiers())){
-                            System.out.printf("%s %s %s%n",Modifier.toString(field.getModifiers()),field.getType().getSimpleName(),field.getName());
+                            System.out.printf("%s %s %s%n", Modifier.toString(field.getModifiers()), field.getType().getSimpleName(), field.getName());
                         }
                     }
+                    
                     break;
                 case "all":
-                    Arrays.stream(declaredFields).forEach(f -> {System.out.printf("%s %s %s%n",Modifier.toString(f.getModifiers()),f.getType().getSimpleName(),f.getName());});
+                    Arrays.stream(declaredFields).forEach(f -> {System.out.printf("%s %s %s%n", Modifier.toString(f.getModifiers()), f.getType().getSimpleName(), f.getName());});
+                    
                     break;
             }
+
             input = scan.nextLine();
         }
+
+        scan.close();
     }
 }
