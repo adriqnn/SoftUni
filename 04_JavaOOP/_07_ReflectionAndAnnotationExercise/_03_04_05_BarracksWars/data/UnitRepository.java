669df42fbb21421080494278705956c4967725f4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-01 23:28:11 +0200

    03

--- 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/data/UnitRepository.java
+++ 04_JavaOOP/_07_ReflectionAndAnnotationExercise/_03_04_05_BarracksWars/data/UnitRepository.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class UnitRepository implements Repository {
-
     private Map<String, Integer> amountOfUnits;
 
     public UnitRepository() {
@@ -16,6 +15,7 @@ public class UnitRepository implements Repository {
 
     public void addUnit(Unit unit) {
         String unitType = unit.getClass().getSimpleName();
+        
         if (!this.amountOfUnits.containsKey(unitType)) {
             this.amountOfUnits.put(unitType, 0);
         }
@@ -26,24 +26,24 @@ public class UnitRepository implements Repository {
 
     public String getStatistics() {
         StringBuilder statBuilder = new StringBuilder();
-        for (Map.Entry<String, Integer> entry : amountOfUnits.entrySet()) {
-            String formattedEntry =
-                    String.format("%s -> %d%n", entry.getKey(), entry.getValue());
+        
+        for (Map.Entry<String, Integer> entry : this.amountOfUnits.entrySet()) {
+            String formattedEntry = String.format("%s -> %d%n", entry.getKey(), entry.getValue());
             statBuilder.append(formattedEntry);
         }
-        statBuilder.setLength(
-                statBuilder.length() - System.lineSeparator().length());
-
+        
+        statBuilder.setLength(statBuilder.length() - System.lineSeparator().length());
         return statBuilder.toString();
     }
 
     public void removeUnit(String unitType)  {
         Integer unitCount = this.amountOfUnits.get(unitType);
+        
         if(unitCount == null || unitCount <= 0){
             this.amountOfUnits.remove(unitType);
             throw new IllegalArgumentException("No such units in repository.");
         }else{
-            this.amountOfUnits.put(unitType,--unitCount);
+            this.amountOfUnits.put(unitType, --unitCount);
         }
     }
 }
