9372c0c4404ea4e7f935c7a823e2f89a159d2b75
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-02 23:20:51 +0200

    01

--- 04_JavaOOP/_08_ExceptionsAndErrorHandling/_01_NumberInRange.java
+++ 04_JavaOOP/_08_ExceptionsAndErrorHandling/_01_NumberInRange.java
@@ -7,6 +7,7 @@ public class _01_NumberInRange {
         Scanner scan = new Scanner(System.in);
 
         String[] range = scan.nextLine().split("\\s+");
+        
         int startRange = Integer.parseInt(range[0]);
         int endRange = Integer.parseInt(range[1]);
         System.out.printf("Range: [%d...%d]%n", startRange, endRange);
@@ -14,17 +15,22 @@ public class _01_NumberInRange {
         boolean isValid = false;
         while(!isValid){
             String input = scan.nextLine();
+            
             try{
                 int number = Integer.parseInt(input);
+                
                 if(number >= startRange && number <= endRange){
                     System.out.printf("Valid number: %d%n", number);
                     isValid = true;
                     continue;
                 }
+                
                 System.out.printf("Invalid number: %d%n", number);
             }catch(Exception e){
                 System.out.printf("Invalid number: %s%n", input);
             }
         }
+        
+        scan.close();
     }
 }
