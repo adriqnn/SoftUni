d75adcd88d526bce218303530ed6c06b15e3eff3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-03 05:39:01 +0200

    03

---
+++ 04_JavaOOP/_09_UnitTestingExercise/test/java/_03_/ListIteratorTest.java
@@ -0,0 +1,63 @@
+package unittesting.exercise.three;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.OperationNotSupportedException;
+
+public class ListIteratorTest {
+    private static final String[] testArray = new String[]{"a", "b", "c", "d"}; // without new also is possible
+    ListIterator listIterator;
+
+    @Before
+    public void prepare() throws OperationNotSupportedException {
+        this.listIterator = new ListIterator(testArray);
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testConstructorShouldThrowException() throws OperationNotSupportedException {
+        new ListIterator(null);
+    }
+
+    @Test
+    public void testConstructorShouldCreateListIterator() throws OperationNotSupportedException {
+        ListIterator listIterator = new ListIterator("a", "b", "c", "d");
+    }
+
+    @Test
+    public void testHasNext(){
+        Assert.assertTrue(this.listIterator.hasNext());
+        this.listIterator.move();
+        Assert.assertTrue(this.listIterator.hasNext());
+        this.listIterator.move();
+        Assert.assertTrue(this.listIterator.hasNext());
+        this.listIterator.move();
+        Assert.assertFalse(this.listIterator.hasNext());
+    }
+
+    @Test
+    public void testMove(){
+        Assert.assertTrue(this.listIterator.move());
+        Assert.assertTrue(this.listIterator.move());
+        Assert.assertTrue(this.listIterator.move());
+        Assert.assertFalse(this.listIterator.move());
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void testPrintShouldThrowExceptionForEmptyList() throws OperationNotSupportedException {
+        listIterator = new ListIterator();
+        listIterator.print();
+    }
+
+    @Test
+    public void testPrintSuccess(){
+        int index = 0;
+        
+        while(this.listIterator.hasNext()){
+            Assert.assertEquals(testArray[index], this.listIterator.print());
+            index++;
+            this.listIterator.move();
+        }
+    }
+}
