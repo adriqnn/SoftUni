7c30fe5e97a66183bf309710a82eb11517609305
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-04 06:04:56 +0200

    06

---
+++ 04_JavaOOP/_09_UnitTestingExercise/test/java/_06_/AlarmTest.java
@@ -0,0 +1,41 @@
+package _04_JavaOOP._09_UnitTestingExercise._06_;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class AlarmTest {
+    private static final double LOW_PRESSURE_VALUE = 12.0;
+    private static final double HIGH_PRESSURE_VALUE = 50.0;
+    private static final double NORMAL_PRESSURE_VALUE = 18.0;
+
+    @Test
+    public void testAlarmTurnsOnForLowPressure(){
+        Sensor fakeSensorForLowPressure = Mockito.mock(Sensor.class);
+        when(fakeSensorForLowPressure.popNextPressurePsiValue()).thenReturn(LOW_PRESSURE_VALUE);
+        Alarm alarm = new Alarm(fakeSensorForLowPressure);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+
+    @Test
+    public void testAlarmTurnsOnForHighPressure(){
+        Sensor fakeSensorForLowPressure = Mockito.mock(Sensor.class);
+        when(fakeSensorForLowPressure.popNextPressurePsiValue()).thenReturn(HIGH_PRESSURE_VALUE);
+        Alarm alarm = new Alarm(fakeSensorForLowPressure);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+
+    @Test
+    public void testAlarmIsOffForNormalPressure(){
+        Sensor fakeSensorForLowPressure = Mockito.mock(Sensor.class);
+        when(fakeSensorForLowPressure.popNextPressurePsiValue()).thenReturn(NORMAL_PRESSURE_VALUE);
+        Alarm alarm = new Alarm(fakeSensorForLowPressure);
+        alarm.check();
+        Assert.assertFalse(alarm.getAlarmOn());
+    }
+}
