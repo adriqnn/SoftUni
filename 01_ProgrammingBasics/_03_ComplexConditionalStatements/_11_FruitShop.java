8fa292d055c96e6a074f6e1a21c145b34249b770
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-05 20:42:15 +0200

    11

--- 01_ProgrammingBasics/_03_ComplexConditionalStatements/_11_FruitShop.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatements/_11_FruitShop.java
@@ -12,7 +12,7 @@ public class _11_FruitShop {
         double quantity = Double.parseDouble(scan.nextLine());
 
         // Check if the inputs are valid
-        boolean isValidFruit = (fruit.equals("banana") || fruit.equals("apple") ||fruit.equals("orange") ||fruit.equals("grapefruit") ||fruit.equals("kiwi") ||fruit.equals("pineapple") ||fruit.equals("grapes"));
+        boolean isValidFruit = (fruit.equals("banana") || fruit.equals("apple") || fruit.equals("orange") || fruit.equals("grapefruit") || fruit.equals("kiwi") || fruit.equals("pineapple") || fruit.equals("grapes"));
         boolean isValidWeekDay = (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday") || day.equals("Saturday") || day.equals("Sunday"));
 
         // End the program if either input is invalid
@@ -24,35 +24,35 @@ public class _11_FruitShop {
         // Print the result
         if (day.equals("Monday") || day.equals("Tuesday") || day.equals("Wednesday") || day.equals("Thursday") || day.equals("Friday")) {
             if (fruit.equals("banana")){
-                System.out.printf("%.2f",(quantity * 2.50));
+                System.out.printf("%.2f", (quantity * 2.50));
             }else if (fruit.equals("apple")) {
-                System.out.printf("%.2f",(quantity * 1.20));
+                System.out.printf("%.2f", (quantity * 1.20));
             }else if (fruit.equals("orange")) {
-                System.out.printf("%.2f",(quantity * 0.85));
+                System.out.printf("%.2f", (quantity * 0.85));
             }else if (fruit.equals("grapefruit")) {
-                System.out.printf("%.2f",(quantity * 1.45));
+                System.out.printf("%.2f", (quantity * 1.45));
             }else if (fruit.equals("kiwi")) {
-                System.out.printf("%.2f",(quantity * 2.70));
+                System.out.printf("%.2f", (quantity * 2.70));
             }else if (fruit.equals("pineapple")) {
-                System.out.printf("%.2f",(quantity * 5.50));
+                System.out.printf("%.2f", (quantity * 5.50));
             }else {
-                System.out.printf("%.2f",(quantity * 3.85));
+                System.out.printf("%.2f", (quantity * 3.85));
             }
         }else {
             if (fruit.equals("banana")){
-                System.out.printf("%.2f",(quantity * 2.70));
+                System.out.printf("%.2f", (quantity * 2.70));
             }else if (fruit.equals("apple")) {
-                System.out.printf("%.2f",(quantity * 1.25));
+                System.out.printf("%.2f", (quantity * 1.25));
             }else if (fruit.equals("orange")) {
-                System.out.printf("%.2f",(quantity * 0.90));
+                System.out.printf("%.2f", (quantity * 0.90));
             }else if (fruit.equals("grapefruit")) {
-                System.out.printf("%.2f",(quantity * 1.60));
+                System.out.printf("%.2f", (quantity * 1.60));
             }else if (fruit.equals("kiwi")) {
-                System.out.printf("%.2f",(quantity * 3.00));
+                System.out.printf("%.2f", (quantity * 3.00));
             }else if (fruit.equals("pineapple")) {
-                System.out.printf("%.2f",(quantity * 5.60));
+                System.out.printf("%.2f", (quantity * 5.60));
             }else {
-                System.out.printf("%.2f",(quantity * 4.20));
+                System.out.printf("%.2f", (quantity * 4.20));
             }
         }
 
