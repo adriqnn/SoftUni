e89fcf95a7f1db7c5a7c1824ad852b279a175d89
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-05 22:33:00 +0200

    01

--- 04_JavaOOP/_10_TestDrivenDevelopmentExercise/main/java/Main.java
+++ 04_JavaOOP/_10_TestDrivenDevelopmentExercise/main/java/Main.java
@@ -1,7 +1,7 @@
-package _04_JavaOOP._10_TestDrivenDevelopmentExercise.main.java;
+package _04_JavaOOP._10_TestDrivenDevelopmentExercise;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Chainblock, ChainblockImpl, Transaction, TransactionImpl and TransactionStatus;
     }
 }
