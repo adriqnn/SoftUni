9c32761c80f5512ff61431400bad0d307eca43fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-05 22:12:25 +0200

    01

--- 04_JavaOOP/_10_TestDrivenDevelopment/main/java/Main.java
+++ 04_JavaOOP/_10_TestDrivenDevelopment/main/java/Main.java
@@ -2,6 +2,6 @@ package _04_JavaOOP._10_TestDrivenDevelopment.main.java;
 
 public class Main {
     public static void main(String[] args) {
-
+        // Test Instock, Product and ProductStock
     }
 }
