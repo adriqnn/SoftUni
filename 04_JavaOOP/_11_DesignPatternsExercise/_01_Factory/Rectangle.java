47d1a9517baea7fbc49dfefbb2bfb9e2faae353d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 20:25:07 +0200

    01

---
+++ 04_JavaOOP/_11_DesignPatternsExercise/_01_Factory/Rectangle.java
@@ -0,0 +1,7 @@
+package _04_JavaOOP._11_DesignPatternsExercise._01_Factory;
+
+public class Rectangle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Rectangle");
+    }
+}
