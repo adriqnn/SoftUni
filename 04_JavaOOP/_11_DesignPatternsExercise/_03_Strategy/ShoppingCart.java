a3a12d8e7f322e0ad2c2c98f5188ce2e032a2412
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 22:11:09 +0200

    03

---
+++ 04_JavaOOP/_11_DesignPatternsExercise/_03_Strategy/ShoppingCart.java
@@ -0,0 +1,13 @@
+package _04_JavaOOP._11_DesignPatternsExercise._03_Strategy;
+
+public class ShoppingCart {
+    private PaymentStrategy paymentStrategy;
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void checkout(int amount) {
+        paymentStrategy.pay(amount);
+    }
+}
