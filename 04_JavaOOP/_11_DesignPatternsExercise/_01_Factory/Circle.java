3b13a93142abd296844eb58b2eb8d2bfea7bbabb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 20:25:02 +0200

    01

---
+++ 04_JavaOOP/_11_DesignPatternsExercise/_01_Factory/Circle.java
@@ -0,0 +1,7 @@
+package _04_JavaOOP._11_DesignPatternsExercise._01_Factory;
+
+public class Circle implements Shape {
+    public void draw() {
+        System.out.println("Drawing a Circle");
+    }
+}
