0fc13e85c24f3a106fb37c2283668678051c4334
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 19:28:12 +0200

    03

---
+++ 04_JavaOOP/_11_DesignPatterns/_03_Command/Fan.java
@@ -0,0 +1,11 @@
+package _04_JavaOOP._11_DesignPatterns._03_Command;
+
+public class Fan {
+    public void turnOn() {
+        System.out.println("Fan is on");
+    }
+
+    public void turnOff() {
+        System.out.println("Fan is off");
+    }
+}
