a8b6b80f3400252c41449fb205a7d4748ea5e2a4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 05:55:11 +0200

    05

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_05_Largest3Numbers.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_01_Lecture/_05_Largest3Numbers.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class _05_Largest3Numbers {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
+        // Read input values
         String[] input = scan.nextLine().split(" ");
 
+        // Sort, limit and print result
         Arrays.stream(input).map(Integer::parseInt).sorted((left, right) -> right.compareTo(left)).limit(3).forEach(System.out::println);
+        
+        scan.close();
     }
-}
