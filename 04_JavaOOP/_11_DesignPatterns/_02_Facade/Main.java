d75d0f6524caf78a59e37364d22e92caab41d874
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 19:15:21 +0200

    02

---
+++ 04_JavaOOP/_11_DesignPatterns/_02_Facade/Main.java
@@ -0,0 +1,8 @@
+package _04_JavaOOP._11_DesignPatterns._02_Facade;
+
+public class Main {
+    public static void main(String[] args) {
+        ComputerFacade computer = new ComputerFacade();
+        computer.start();
+    }
+}
