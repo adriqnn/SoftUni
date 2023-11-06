06907c0f5fcfde14d7eeb1893fcf48d029ed58fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-06 19:29:16 +0200

    03

---
+++ 04_JavaOOP/_11_DesignPatterns/_03_Command/Light.java
@@ -0,0 +1,11 @@
+package _04_JavaOOP._11_DesignPatterns._03_Command;
+
+public class Light {
+    public void turnOn() {
+        System.out.println("Light is on");
+    }
+
+    public void turnOff() {
+        System.out.println("Light is off");
+    }
+}
