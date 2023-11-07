721a0f0aed8ea918ac8526f1d954ee857906d556
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-07 18:37:41 +0200

    Delete 04_JavaOOP/_11_ExamPrep/_01_Restaurant/src directory

--- 04_JavaOOP/_11_ExamPrep/_01_Restaurant/src/main/java/restaurant/entities/healthyFoods/VeganBiscuits.java
+++
@@ -1,9 +0,0 @@
-package restaurant.entities.healthyFoods;
-
-public class VeganBiscuits extends Food {
-    private static final int InitialSaladPortion = 205;
-
-    public VeganBiscuits(String name, double price) {
-        super(name, InitialSaladPortion, price);
-    }
-}
