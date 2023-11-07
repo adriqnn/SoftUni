4abf837b9180fba206104cc8204045f2a2c7d1ec
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-07 19:54:37 +0200

    01

---
+++ 04_JavaOOP/_12_ExamPrep/_01_ExamPrep/Restaurant/src/main/java/restaurant/repositories/interfaces/Repository.java
@@ -0,0 +1,9 @@
+package restaurant.repositories.interfaces;
+
+import java.util.Collection;
+
+public interface Repository<T> {
+    Collection<T> getAllEntities();
+
+    void add(T entity);
+}
---
+++ 04_JavaOOP/_12_ExamPrep/_01_ExamPrep/Restaurant/src/test/java/shopAndGoods/ShopTest.java
@@ -0,0 +1,139 @@
+package shopAndGoods;
+
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.naming.OperationNotSupportedException;
+import java.util.Map;
+
+public class ShopTest {
+    private Shop shop;
+
+    @Before
+    public void setUp(){
+        this.shop = new Shop();
+    }
+
+    @Test (expected = UnsupportedOperationException.class)
+    public void testGetShelvesShouldReturnUnmodifiableCollection(){
+        this.shop.getShelves().clear();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddGoodsShouldFailForInvalidShelve() throws OperationNotSupportedException {
+        this.shop.addGoods("invalid_test_shelve", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testAddGoodsShouldFailForExistingGood() throws OperationNotSupportedException {
+        Goods goods = new Goods("test_good", "test_code");
+        this.shop.addGoods("Shelves1", goods);
+        this.shop.addGoods("Shelves1", goods);
+    }
+
+    @Test
+    public void testAddGoodsShouldReturnCorrectMessageOnAddition() throws OperationNotSupportedException {
+        Goods goods = new Goods("test_good", "test_code");
+        String expected = "Goods: test_code is placed successfully!";
+        String actual = this.shop.addGoods("Shelves1", goods);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRemoveGoodsShouldFailForInvalidShelve() throws OperationNotSupportedException {
+        this.shop.removeGoods("invalid_test_shelve", null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testRemoveGoodsShouldFailForDifferentGoodOnTheSameShelve() throws OperationNotSupportedException {
+        Goods goods = new Goods("test_good", "test_code");
+        Goods goodsOther = new Goods("test_good_other", "test_code_other");
+        this.shop.addGoods("Shelves1", goods);
+        this.shop.removeGoods("Shelves1", goodsOther);
+    }
+
+    @Test
+    public void testRemoveGoodsShouldReturnCorrectMessage() throws OperationNotSupportedException {
+        Goods goods = new Goods("test_good", "test_code");
+        this.shop.addGoods("Shelves1", goods);
+        String expected = "Goods: test_code is removed successfully!";
+        String actual = this.shop.removeGoods("Shelves1", goods);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemoveGoodsShouldSetTheShelveValueToNull() throws OperationNotSupportedException {
+        Goods goods = new Goods("test_good", "test_code");
+        this.shop.addGoods("Shelves1", goods);
+        this.shop.removeGoods("Shelves1", goods);
+
+        Goods emptySlot = this.shop.getShelves().get("Shelves1");
+
+        Assert.assertNull(emptySlot);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testAddGoodDoesNotContainKey() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves13", goods);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testGetNull() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves1", goods);
+        shop.addGoods("Shelves1", goods);
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testBoolean() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves1", goods);
+        shop.addGoods("Shelves2", goods);
+    }
+
+    @Test
+    public void testPut() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves1", goods);
+        Assert.assertEquals(goods, shop.getShelves().get("Shelves1"));
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testRemoveGoodDoesNotContainKey() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.removeGoods("Shelves13", goods);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void testRemove() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.removeGoods("Shelves1", goods);
+    }
+
+    @Test
+    public void assertNull() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves1", goods);
+        shop.removeGoods("Shelves1", goods);
+        Assert.assertNull(shop.getShelves().get("Shelves1"));
+    }
+
+    @Test
+    public void assertString() throws OperationNotSupportedException {
+        Shop shop = new Shop();
+        Goods goods = new Goods("a", "a");
+        shop.addGoods("Shelves1", goods);
+        String b = "Goods: a is removed successfully!";
+        Assert.assertEquals(b, shop.removeGoods("Shelves1",goods));
+    }
+}
