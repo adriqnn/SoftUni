f9ec36642a0599be5fe298585f669200c1a7c261
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-07 22:02:09 +0200

    Delete 04_JavaOOP/_11_ExamPrep/_04_SpaceStation/src directory

--- 04_JavaOOP/_11_ExamPrep/_04_SpaceStation/src/main/java/spaceStation/models/astronauts/Meteorologist.java
+++
@@ -1,9 +0,0 @@
-package spaceStation.models.astronauts;
-
-public class Meteorologist extends BaseAstronaut{
-    private static final double METEOROLOGIST_ASTRONAUT_OXYGEN_LEVEL = 90;
-
-    public Meteorologist(String name) {
-        super(name, METEOROLOGIST_ASTRONAUT_OXYGEN_LEVEL);
-    }
-}
