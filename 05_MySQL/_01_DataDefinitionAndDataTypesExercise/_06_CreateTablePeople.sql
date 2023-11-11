96597c76f00056db45819edd90c1ffa115922cbb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-11 23:58:43 +0200

    06

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_06_CreateTablePeople.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_06_CreateTablePeople.sql
@@ -1,17 +1,18 @@
-CREATE TABLE `people`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`name` VARCHAR(200) NOT NULL,
-`picture` BLOB,
-`height` DOUBLE(10,2),
-`weight` DOUBLE(10,2),
-`gender` CHAR(1) NOT NULL,
-`birthdate` DATE NOT NULL,
-`biography` TEXT
+CREATE TABLE `people` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200) NOT NULL,
+    `picture` BLOB,
+    `height` DOUBLE(10,2),
+    `weight` DOUBLE(10,2),
+    `gender` CHAR(1) NOT NULL,
+    `birthdate` DATE NOT NULL,
+    `biography` TEXT
 );
-INSERT INTO `people` (`name`,`gender`,`birthdate`)
+
+INSERT INTO `people` (`name`, `gender`, `birthdate`)
 VALUES
-('Test','M',DATE(NOW())),
-('Test','M',DATE(NOW())),
-('Test','M',DATE(NOW())),
-('Test','M',DATE(NOW())),
-('Test','M',DATE(NOW()));
+('Test', 'M', NOW()),
+('Test', 'M', NOW()),
+('Test', 'M', NOW()),
+('Test', 'M', NOW()),
+('Test', 'M', NOW());
