98ba4b4dbf005d27a82a77f39589433cf2468286
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-11 23:47:43 +0200

    02

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_02_AlterMinionsTable.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_02_AlterMinionsTable.sql
@@ -2,6 +2,6 @@ ALTER TABLE `minions`
 ADD COLUMN `town_id` INT;
 
 ALTER TABLE `minions`
-Add CONSTRAINT `fk_minions_towns`
+ADD CONSTRAINT `fk_minions_towns`
 FOREIGN KEY (`town_id`)
 REFERENCES `towns`(`id`);
