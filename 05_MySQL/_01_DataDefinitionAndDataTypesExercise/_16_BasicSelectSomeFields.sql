07496851cec60b816d053fc3ec39cc19375b2b78
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 00:18:39 +0200

    16

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_16_BasicSelectSomeFields.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_16_BasicSelectSomeFields.sql
@@ -1,6 +1,8 @@
-SELECT `name` from `towns`
+SELECT `name` FROM `towns`
 ORDER BY `name`;
-SELECT `name` from `departments`
+
+SELECT `name` FROM `departments`
 ORDER BY `name`;
-SELECT `first_name`,`last_name`,`job_title`,`salary` FROM `employees`
+
+SELECT `first_name`, `last_name`, `job_title`, `salary` FROM `employees`
 ORDER BY `salary` DESC;
