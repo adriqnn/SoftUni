cc754658007747afea3bd0ad268049a8cf165940
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-11 23:15:25 +0200

    05

--- 05_MySQL/_01_DataDefinitionAndDataTypes/_05_ModifyingColumns.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypes/_05_ModifyingColumns.sql
@@ -1,2 +1,5 @@
-ALTER TABLE employees
+ALTER TABLE `employees`
 CHANGE COLUMN `middle_name` `middle_name` VARCHAR(100) NOT NULL;
+
+ALTER TABLE `employees`
+MODIFY COLUMN `middle_name` VARCHAR(100) NOT NULL;
