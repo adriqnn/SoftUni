3a68e21fa4f9bb25c6c5dbe0bcf7ffec2e4d875c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 00:10:36 +0200

    11

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_11_MoviesDatabase.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_11_MoviesDatabase.sql
@@ -1,51 +1,58 @@
-CREATE TABLE `directors`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`director_name` VARCHAR(50) NOT NULL,
-`notes` TEXT
+CREATE TABLE `directors` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `director_name` VARCHAR(50) NOT NULL,
+    `notes` TEXT
 );
-INSERT INTO `directors`(`director_name`,`notes`)
+
+INSERT INTO `directors` (`director_name`, `notes`)
 VALUES
-('t1','notes'),
-('t2','notes'),
-('t3','notes'),
-('t4','notes'),
-('t5','notes');
-CREATE TABLE `genres`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`genre_name` VARCHAR(50) NOT NULL,
-`notes` TEXT
+('t1', 'notes'),
+('t2', 'notes'),
+('t3', 'notes'),
+('t4', 'notes'),
+('t5', 'notes');
+
+CREATE TABLE `genres` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `genre_name` VARCHAR(50) NOT NULL,
+    `notes` TEXT
 );
-INSERT INTO `genres`(`genre_name`,`notes`)
+
+INSERT INTO `genres` (`genre_name`, `notes`)
 VALUES
-('t1','notes'),
-('t2','notes'),
-('t3','notes'),
-('t4','notes'),
-('t5','notes');
-CREATE TABLE `categories`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`category_name` VARCHAR(50) NOT NULL,
-`notes` TEXT
+('t1', 'notes'),
+('t2', 'notes'),
+('t3', 'notes'),
+('t4', 'notes'),
+('t5', 'notes');
+
+CREATE TABLE `categories` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `category_name` VARCHAR(50) NOT NULL,
+    `notes` TEXT
 );
-INSERT INTO `categories`(`category_name`,`notes`)
+
+INSERT INTO `categories` (`category_name`, `notes`)
 VALUES
-('t1','notes'),
-('t2','notes'),
-('t3','notes'),
-('t4','notes'),
-('t5','notes');
-CREATE TABLE `movies`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`title` VARCHAR(50) NOT NULL,
-`director_id` INT,
-`copyright_year` INT,
-`length` INT,
-`genre_id` INT,
-`category_id` INT,
-`rating` DOUBLE,
-`notes` TEXT
+('t1', 'notes'),
+('t2', 'notes'),
+('t3', 'notes'),
+('t4', 'notes'),
+('t5', 'notes');
+
+CREATE TABLE `movies` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(50) NOT NULL,
+    `director_id` INT,
+    `copyright_year` INT,
+    `length` INT,
+    `genre_id` INT,
+    `category_id` INT,
+    `rating` DOUBLE,
+    `notes` TEXT
 );
-INSERT INTO `movies`(`title`)
+
+INSERT INTO `movies` (`title`)
 VALUES
 ('t1'),
 ('t2'),
