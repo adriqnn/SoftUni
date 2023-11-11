49c0798784e1ae126811140d0a09b54017448c89
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 00:48:40 +0200

    02

--- 05_MySQL/_02_BasicCRUD/_02_SelectEmployeesWithFilter.sql
+++ 05_MySQL/_02_BasicCRUD/_02_SelectEmployeesWithFilter.sql
@@ -1,4 +1,3 @@
-SELECT `id`, CONCAT(`first_name`,' ',`last_name`) AS 'Full name',`job_title` as 'Job Title',`salary` as 'Salary'
-FROM employees
+SELECT `id`, CONCAT(`first_name`, ' ', `last_name`) AS 'Full name', `job_title` AS 'Job Title', `salary` AS 'Salary' FROM employees
 WHERE `salary` > 1000
 ORDER BY `id`;
