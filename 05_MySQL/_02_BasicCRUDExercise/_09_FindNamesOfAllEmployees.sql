039a57f578a4d23a9ee76b236a903987ec6cedab
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 01:07:42 +0200

    09

--- 05_MySQL/_02_BasicCRUDExercise/_09_FindNamesOfAllEmployees.sql
+++ 05_MySQL/_02_BasicCRUDExercise/_09_FindNamesOfAllEmployees.sql
@@ -1,3 +1,3 @@
-SELECT concat_ws(' ',`first_name`,`middle_name`,`last_name`) AS 'Full Name' FROM `employees`
-WHERE `salary` IN (25000,14000,12500,23600)
+SELECT CONCAT_WS(' ', `first_name`, `middle_name`, `last_name`) AS 'Full Name' FROM `employees`
+WHERE `salary` IN (25000, 14000, 12500, 23600)
 ORDER BY `employee_id`;
