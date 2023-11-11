bba5cbb919e64c48c60d04ad0fb7a865f9da67ca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 00:09:15 +0200

    10

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_10_SetUniqueField.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_10_SetUniqueField.sql
@@ -1,5 +1,9 @@
 ALTER TABLE `users`
 DROP PRIMARY KEY,
-ADD CONSTRAINT pk_users
-PRIMARY KEY `users`(`id`),
+ADD CONSTRAINT pk_users PRIMARY KEY `users`(`id`),
 CHANGE COLUMN `username` `username` VARCHAR(50) UNIQUE;
+
+ALTER TABLE `users`
+DROP PRIMARY KEY,
+ADD CONSTRAINT `pk_users` PRIMARY KEY (`id`),
+MODIFY COLUMN `username` VARCHAR(50) UNIQUE;
