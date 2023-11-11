1a5085efc60d3a3c6abb15c2a7504a97f49b61de
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-11 22:56:20 +0200

    02

--- 05_MySQL/_01_DataDefinitionAndDataTypes/_02_InsertDataInTables.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypes/_02_InsertDataInTables.sql
@@ -1,3 +1,3 @@
-insert into  employees (first_name, last_name) values ('ivan','ivanov');
-insert into  employees (first_name, last_name) values ('ivann','ivanovv');
-insert into  employees (first_name, last_name) values ('ivannn','ivanovvv');
+INSERT INTO employees (first_name, last_name) VALUES ('ivan', 'ivanov');
+INSERT INTO employees (first_name, last_name) VALUES ('ivann', 'ivanovv');
+INSERT INTO employees (first_name, last_name) VALUES ('ivannn', 'ivanovvv');
