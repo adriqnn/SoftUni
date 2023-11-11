c09eafd42a28e3b429705b2dd4b1f7f885cc6cf6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 00:00:39 +0200

    07

--- 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_07_CreateTableUsers.sql
+++ 05_MySQL/_01_DataDefinitionAndDataTypesExercise/_07_CreateTableUsers.sql
@@ -1,16 +1,16 @@
-CREATE TABLE `users`(
-`id` INT PRIMARY KEY AUTO_INCREMENT,
-`username` VARCHAR(30) NOT NULL,
-`password` VARCHAR(26) NOT NULL,
-`profile_picture` BLOB,
-`last_login_time` TIME,
-`is_deleted` BOOLEAN
+CREATE TABLE `users` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `username` VARCHAR(30) NOT NULL,
+    `password` VARCHAR(26) NOT NULL,
+    `profile_picture` BLOB,
+    `last_login_time` TIME,
+    `is_deleted` BOOLEAN
 );
 
-INSERT INTO `users` (`username`,`password`)
+INSERT INTO `users` (`username`, `password`)
 VALUES
-('Test1','Pass'),
-('Test2','Pass'),
-('Test3','Pass'),
-('Test4','Pass'),
-('Test5','Pass');
+('Test1', 'Pass'),
+('Test2', 'Pass'),
+('Test3', 'Pass'),
+('Test4', 'Pass'),
+('Test5', 'Pass');
