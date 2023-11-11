e5366c58b5a9beeab378d17ba6c1fb2b22c01f71
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-12 01:29:11 +0200

    23

--- 05_MySQL/_02_BasicCRUDExercise/_23_CountriesAndCurrencyEuroNotEuro.sql
+++ 05_MySQL/_02_BasicCRUDExercise/_23_CountriesAndCurrencyEuroNotEuro.sql
@@ -1,2 +1,2 @@
-SELECT `country_name`,`country_code`, IF(`currency_code` = 'EUR', 'Euro', 'Not Euro') as 'currency' FROM `countries`
-ORDER BY `country_name`ASC;
+SELECT `country_name`, `country_code`, IF(`currency_code` = 'EUR', 'Euro', 'Not Euro') AS 'currency' FROM `countries`
+ORDER BY `country_name` ASC;
