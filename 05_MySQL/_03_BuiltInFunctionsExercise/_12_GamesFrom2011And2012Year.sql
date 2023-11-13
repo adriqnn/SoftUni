0a3070cba0c7ab205c8d708ecec35020c5c083ca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 22:54:15 +0200

    12

--- 05_MySQL/_03_BuiltInFunctionsExercise/_12_GamesFrom2011And2012Year.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_12_GamesFrom2011And2012Year.sql
@@ -1,3 +1,3 @@
-SELECT `name`,date_format(`start`,'%Y-%m-%d') as 'start' FROM `games`
-WHERE YEAR(`start`) = '2011' OR YEAR(`start`) = 2012
-ORDER BY `start`,`name` LIMIT 50;
+SELECT `name`, DATE_FORMAT(`start`, '%Y-%m-%d') AS 'start' FROM `games`
+WHERE YEAR(`start`) = 2011 OR YEAR(`start`) = 2012
+ORDER BY `start`, `name` LIMIT 50;
