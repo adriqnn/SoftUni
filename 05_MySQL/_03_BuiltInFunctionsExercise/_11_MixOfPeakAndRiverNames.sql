fe9410a2f4ea05d724a1e8f7f7755af8e6eaf4d3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 22:50:37 +0200

    11

--- 05_MySQL/_03_BuiltInFunctionsExercise/_11_MixOfPeakAndRiverNames.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_11_MixOfPeakAndRiverNames.sql
@@ -1,4 +1,3 @@
-SELECT p.peak_name, r.river_name, lower(concat(LEFT(p.peak_name, char_length(p.peak_name)-1),r.river_name)) AS mix 
-FROM `peaks` AS p, `rivers` AS r
-WHERE lower(RIGHT(p.peak_name,1)) = lower(LEFT(r.river_name,1))
+SELECT p.peak_name, r.river_name, LOWER(CONCAT(LEFT(p.peak_name, CHAR_LENGTH(p.peak_name) - 1), r.river_name)) AS mix FROM `peaks` AS p, `rivers` AS r
+WHERE LOWER(RIGHT(p.peak_name, 1)) = LOWER(LEFT(r.river_name, 1))
 ORDER BY mix;
