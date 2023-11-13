c7f23d3e5a1a33d8ac29dd9c8def9b25c3dcd3b3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 22:32:46 +0200

    02

--- 05_MySQL/_03_BuiltInFunctionsExercise/_02_FindNamesOfAllEmployeesByLastName.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_02_FindNamesOfAllEmployeesByLastName.sql
@@ -1,3 +1,3 @@
-SELECT `first_name`,`last_name` FROM `employees`
-WHERE lower(`last_name`) LIKE '%ei%'
+SELECT `first_name`, `last_name` FROM `employees`
+WHERE LOWER(`last_name`) LIKE '%ei%'
 ORDER BY `employee_id`;
