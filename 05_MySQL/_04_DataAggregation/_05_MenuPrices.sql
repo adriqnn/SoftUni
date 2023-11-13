d2d1aa56c2ef3b62f25f26ce600b159c25da6f85
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 00:19:35 +0200

    05

--- 05_MySQL/_04_DataAggregation/_05_MenuPrices.sql
+++ 05_MySQL/_04_DataAggregation/_05_MenuPrices.sql
@@ -1,3 +1,2 @@
-SELECT `category_id`, round(AVG(`price`),2) AS 'Average Price', round(min(`price`),2) AS 'Cheapest Product', round(max(`price`),2) AS 'Most Expensive Product'
-FROM `products`
+SELECT `category_id`, ROUND(AVG(`price`), 2) AS 'Average Price', ROUND(MIN(`price`), 2) AS 'Cheapest Product', ROUND(MAX(`price`), 2) AS 'Most Expensive Product' FROM `products`
 GROUP BY `category_id`;
