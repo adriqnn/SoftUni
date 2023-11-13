8ba297240d42e9d08a81c5c240c7f71dd0a85a6d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 23:20:11 +0200

    01

--- 05_MySQL/_04_DataAggregation/_01_DepartmentsInfo.sql
+++ 05_MySQL/_04_DataAggregation/_01_DepartmentsInfo.sql
@@ -1,4 +1,3 @@
-SELECT `department_id`, count(*) AS 'Number of employees'
-FROM `employees`
+SELECT `department_id`, COUNT(*) AS 'Number of employees' FROM `employees`
 GROUP BY `department_id`
 ORDER BY `department_id`, 'Number of employees';
