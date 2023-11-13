3b3c73524b9583520a75120ceacfae33b02eff67
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 22:55:49 +0200

    13

--- 05_MySQL/_03_BuiltInFunctionsExercise/_13_UserEmailProviders.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_13_UserEmailProviders.sql
@@ -1,2 +1,5 @@
 SELECT `user_name`, REGEXP_REPLACE(`email`,'.*@','') AS 'email provider' from `users`
 ORDER BY `email provider` ASC, `user_name` ASC;
+
+SELECT `user_name`, SUBSTRING_INDEX(`email`, '@', -1) AS 'email provider' FROM `users`
+ORDER BY `email provider` ASC, `user_name` ASC;
