1794c37c36355f4436917f4c7fc433956100ddc6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 23:02:37 +0200

    16

--- 05_MySQL/_03_BuiltInFunctionsExercise/_16_OrdersTable.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_16_OrdersTable.sql
@@ -1,4 +1,1 @@
-SELECT `product_name`, `order_date`,  
-adddate(`order_date`,INTERVAL 3 DAY) AS 'pay_due',
-adddate(`order_date`,INTERVAL 1 MONTH) AS 'deliver_due'
-FROM `orders`;
+SELECT`product_name`, `order_date`, ADDDATE(`order_date`, INTERVAL 3 DAY) AS 'pay_due', ADDDATE(`order_date`, INTERVAL 1 MONTH) AS 'deliver_due' FROM `orders`;
