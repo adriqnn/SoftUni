e74d6ddaedc6266e2dc47e81c61fb4a441b54db6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 22:31:03 +0200

    01

--- 05_MySQL/_03_BuiltInFunctionsExercise/_01_FindNamesOfAllEmployeesByFirstName.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_01_FindNamesOfAllEmployeesByFirstName.sql
@@ -1,3 +1,3 @@
-SELECT `first_name`,`last_name` FROM `employees`
-WHERE lower(`first_name`) LIKE 'sa%'
+SELECT `first_name`, `last_name` FROM `employees`
+WHERE LOWER(`first_name`) LIKE 'sa%'
 ORDER BY `employee_id`;
