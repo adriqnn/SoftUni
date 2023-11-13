479fced429126d509b62396f46d1f3793ca7c9b6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 23:01:29 +0200

    15

--- 05_MySQL/_03_BuiltInFunctionsExercise/_15_ShowAllGamesWithDuration.sql
+++ 05_MySQL/_03_BuiltInFunctionsExercise/_15_ShowAllGamesWithDuration.sql
@@ -1,13 +1,13 @@
 SELECT `name` AS 'game',
-CASE 
-WHEN hour(`g`.`start`) BETWEEN 0 and 11 THEN 'Morning' 
-WHEN hour(`g`.`start`) BETWEEN 12 and 17 THEN 'Afternoon' 
-ELSE 'Evening'
-END AS 'Part of the Day',
-CASE
-WHEN `g`.`duration` <= 3 THEN 'Extra Short'
-WHEN `g`.`duration` BETWEEN 4 and 6 THEN 'Short'
-WHEN `g`.`duration` BETWEEN 7 and 10 THEN 'Long'
-ELSE 'Extra Long'
-END AS 'Duration'
- FROM `games` AS `g`;
+    CASE
+        WHEN HOUR(`g`.`start`) BETWEEN 0 AND 11 THEN 'Morning'
+        WHEN HOUR(`g`.`start`) BETWEEN 12 AND 17 THEN 'Afternoon'
+        ELSE 'Evening'
+    END AS 'Part of the Day',
+    CASE
+        WHEN `g`.`duration` <= 3 THEN 'Extra Short'
+        WHEN `g`.`duration` BETWEEN 4 AND 6 THEN 'Short'
+        WHEN `g`.`duration` BETWEEN 7 AND 10 THEN 'Long'
+        ELSE 'Extra Long'
+    END AS 'Duration'
+FROM `games` AS `g`;
