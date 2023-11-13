7bb69cc8e60b4501cbdc195cb9e2ef9e1ef8cc2b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-13 23:21:09 +0200

    02

--- 05_MySQL/_04_DataAggregation/_02_AverageSalary.sql
+++ 05_MySQL/_04_DataAggregation/_02_AverageSalary.sql
@@ -1,4 +1,3 @@
-SELECT `department_id`, round(AVG(`salary`), 2) AS 'Average Salary'
-FROM `employees`
+SELECT `department_id`, ROUND(AVG(`salary`), 2) AS 'Average Salary' FROM `employees`
 GROUP BY `department_id`
 ORDER BY `department_id`, 'Average Salary';
