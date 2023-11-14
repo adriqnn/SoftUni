67cd1f6ed61551e503d109ce56515a51ba05d632
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:04:17 +0200

    08

--- 05_MySQL/_04_DataAggregationExercise/_08_DepositCharge.sql
+++ 05_MySQL/_04_DataAggregationExercise/_08_DepositCharge.sql
@@ -1,3 +1,3 @@
-SELECT `deposit_group`,`magic_wand_creator`,min(`deposit_charge`) AS 'min_deposit_carge' FROM `wizzard_deposits`
-GROUP BY `deposit_group`,`magic_wand_creator`
-ORDER BY `magic_wand_creator` ASC,`deposit_group` ASC;
+SELECT `deposit_group`, `magic_wand_creator`, MIN(`deposit_charge`) AS 'min_deposit_charge' FROM `wizzard_deposits`
+GROUP BY `deposit_group`, `magic_wand_creator`
+ORDER BY `magic_wand_creator` ASC, `deposit_group` ASC;
