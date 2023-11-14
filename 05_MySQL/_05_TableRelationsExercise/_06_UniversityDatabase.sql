44a0d59cb32e68ef2eb8bff0fba7b4b2a28032ad
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 04:41:43 +0200

    06

--- 05_MySQL/_05_TableRelationsExercise/_06_UniversityDatabase.sql
+++ 05_MySQL/_05_TableRelationsExercise/_06_UniversityDatabase.sql
@@ -15,7 +15,7 @@ CREATE TABLE `students` (
 CREATE TABLE `payments` (
     `payment_id` INT PRIMARY KEY AUTO_INCREMENT,
     `payment_date` DATE,
-    `payment_amount` DECIMAL(8 , 2 ),
+    `payment_amount` DECIMAL(8, 2),
     `student_id` INT,
     CONSTRAINT `fk_payments_students` FOREIGN KEY (`student_id`)
         REFERENCES `students` (`student_id`)
@@ -29,7 +29,7 @@ CREATE TABLE `subjects` (
 CREATE TABLE `agenda` (
     `student_id` INT,
     `subject_id` INT,
-    CONSTRAINT `pk_agenda` PRIMARY KEY (`student_id` , `subject_id`),
+    CONSTRAINT `pk_agenda` PRIMARY KEY (`student_id`, `subject_id`),
     CONSTRAINT `fk_agenda_students` FOREIGN KEY (`student_id`)
         REFERENCES `students` (`student_id`),
     CONSTRAINT `fk_agenda_subjects` FOREIGN KEY (`subject_id`)
