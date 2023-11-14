f66737ca8f94dfecaee6b9b604d131b757a16d99
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:07:19 +0200

    10

--- 05_MySQL/_04_DataAggregationExercise/_10_FirstLetter.sql
+++ 05_MySQL/_04_DataAggregationExercise/_10_FirstLetter.sql
@@ -1,4 +1,4 @@
-SELECT substring(`first_name`,1,1) AS 'first_letter' FROM `wizzard_deposits`
+SELECT SUBSTRING(`first_name`, 1, 1) AS 'first_letter' FROM `wizzard_deposits`
 WHERE `deposit_group` LIKE 'Troll Chest'
 GROUP BY `first_letter`
 ORDER BY `first_letter` ASC;
