2cac4a5e37a4cd9a1698f9ac58492bba7134811a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:21:39 +0200

    17

--- 05_MySQL/_04_DataAggregationExercise/_17_SalaryChallenge.sql
+++ 05_MySQL/_04_DataAggregationExercise/_17_SalaryChallenge.sql
@@ -1,7 +1,7 @@
-SELECT `first_name`,`last_name`,e1.`department_id` FROM `employees` AS e1
-JOIN(
-SELECT e2.`department_id`,avg(e2.`salary`) AS 'average_salary' FROM `employees` as e2
-GROUP BY e2.`department_id`
-) AS `dep_average` ON e1.department_id = dep_average.department_id
+SELECT `first_name`, `last_name`, e1.`department_id` FROM `employees` AS e1
+JOIN 
+  ( SELECT e2.`department_id`, AVG(e2.`salary`) AS 'average_salary' FROM `employees` AS e2
+    GROUP BY e2.`department_id`) AS `dep_average` ON e1.department_id = dep_average.department_id
 WHERE `salary` > dep_average.average_salary
-ORDER BY e1.`department_id`,`employee_id` LIMIT 10;
+ORDER BY e1.`department_id`, `employee_id`
+LIMIT 10;
