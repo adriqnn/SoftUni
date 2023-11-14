fa42ea5ac3a4e8f1c128fd93e8a74edf689bdc0c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:18:17 +0200

    16

--- 05_MySQL/_04_DataAggregationExercise/_16_ThirdHighestSalary.sql
+++ 05_MySQL/_04_DataAggregationExercise/_16_ThirdHighestSalary.sql
@@ -1,5 +1,8 @@
-SELECT `department_id`, (SELECT DISTINCT e2.`salary` FROM `employees` as e2 WHERE e1.department_id = e2.department_id ORDER BY e2.`salary` DESC LIMIT 2,1) AS 'third_highest_salary' 
-FROM `employees` as e1
+SELECT `department_id`, 
+  ( SELECT DISTINCT e2.`salary` FROM `employees` AS e2
+    WHERE e1.department_id = e2.department_id
+    ORDER BY e2.`salary` DESC
+    LIMIT 2, 1) AS 'third_highest_salary' FROM `employees` AS e1
 GROUP BY e1.`department_id`
 HAVING `third_highest_salary` IS NOT NULL
 ORDER BY `department_id`;
