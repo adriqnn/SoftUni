51c763d8f1774d33caff769d81364a7cb8e1a847
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 04:21:06 +0200

    04

--- 05_MySQL/_05_TableRelations/_04_DeleteMountains.sql
+++ 05_MySQL/_05_TableRelations/_04_DeleteMountains.sql
@@ -1,13 +1,14 @@
-CREATE TABLE `mountains`(
-`id` INT AUTO_INCREMENT PRIMARY KEY,
-`name` VARCHAR(45)
+CREATE TABLE `mountains` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(45)
 );
-CREATE TABLE `peaks`(
-`id` INT AUTO_INCREMENT PRIMARY KEY,
-`name` VARCHAR(45),
-`mountain_id` INT,
-CONSTRAINT
-FOREIGN KEY (`mountain_id`)
-REFERENCES `mountains`(`id`)
-ON DELETE CASCADE
+
+CREATE TABLE `peaks` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(45),
+    `mountain_id` INT,
+    CONSTRAINT `fk_peaks_mountains`
+    FOREIGN KEY (`mountain_id`)
+    REFERENCES `mountains` (`id`)
+    ON DELETE CASCADE
 );
