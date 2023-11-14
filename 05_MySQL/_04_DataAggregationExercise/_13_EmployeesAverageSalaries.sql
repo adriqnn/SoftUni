def7fc73220859bca5aecaf58276629f5ab2613e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:13:28 +0200

    13

--- 05_MySQL/_04_DataAggregationExercise/_13_EmployeesAverageSalaries.sql
+++ 05_MySQL/_04_DataAggregationExercise/_13_EmployeesAverageSalaries.sql
@@ -1,5 +1,4 @@
-SELECT `department_id`,
-IF(`department_id` = 1,avg(`salary`) + 5000,avg(`salary`)) AS `avg_salary` FROM `employees`
+SELECT `department_id`, IF(`department_id` = 1, AVG(`salary`) + 5000, AVG(`salary`)) AS `avg_salary` FROM `employees`
 WHERE `salary` > 30000 AND `manager_id` != 42
 GROUP BY `department_id`
 ORDER BY `department_id`;
