046cb029d21fa9e3fd7eb11b10a4b0c604663d99
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 04:57:55 +0200

    02

--- 05_MySQL/_06_SubqueriesAndJOINs/_02_TownsAndAddresses.sql
+++ 05_MySQL/_06_SubqueriesAndJOINs/_02_TownsAndAddresses.sql
@@ -1,4 +1,4 @@
-SELECT `t`.`town_id`,`t`.`name`,`address_text` FROM `addresses` AS `a`
+SELECT `t`.`town_id`, `t`.`name`, `address_text` FROM `addresses` AS `a`
 JOIN `towns` AS `t` USING (`town_id`)
-WHERE `t`.`name` IN ('Sofia','San Francisco','Carnation')
-ORDER BY `t`.`town_id`,`a`.`address_id`;
+WHERE `t`.`name` IN ('Sofia', 'San Francisco', 'Carnation')
+ORDER BY `t`.`town_id`, `a`.`address_id`;
