5ce453a1170f8125a5a09dcc279d21aa666043bf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 03:10:17 +0200

    11

--- 05_MySQL/_04_DataAggregationExercise/_11_AverageInterest.sql
+++ 05_MySQL/_04_DataAggregationExercise/_11_AverageInterest.sql
@@ -1,4 +1,4 @@
-SELECT `deposit_group`,`is_deposit_expired`,avg(deposit_interest) FROM `wizzard_deposits`
+SELECT `deposit_group`, `is_deposit_expired`, AVG(`deposit_interest`) AS 'average_deposit_interest' FROM `wizzard_deposits`
 WHERE `deposit_start_date` > '1985-01-01'
-GROUP BY `deposit_group`,`is_deposit_expired`
+GROUP BY `deposit_group`, `is_deposit_expired`
 ORDER BY `deposit_group` DESC, `is_deposit_expired` ASC;
