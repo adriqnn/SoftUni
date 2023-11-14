6c9294ca6ea67e8a79a066d1174d08fc004782eb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 04:14:34 +0200

    01

--- 05_MySQL/_05_TableRelations/_01_MountainsAndPeaks.sql
+++ 05_MySQL/_05_TableRelations/_01_MountainsAndPeaks.sql
@@ -1,15 +1,15 @@
-CREATE TABLE `mountains`(
-`id` INT AUTO_INCREMENT PRIMARY KEY,
-`name` VARCHAR(45)
+CREATE TABLE `mountains` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(45)
 );
 
-CREATE TABLE `peaks`(
-`id` INT AUTO_INCREMENT PRIMARY KEY,
-`name` VARCHAR(45),
-`mountain_id` INT
+CREATE TABLE `peaks` (
+    `id` INT AUTO_INCREMENT PRIMARY KEY,
+    `name` VARCHAR(45),
+    `mountain_id` INT
 );
 
 ALTER TABLE `peaks`
-ADD CONSTRAINT
+ADD CONSTRAINT `fk_peaks_mountains`
 FOREIGN KEY (`mountain_id`)
 REFERENCES `mountains`(`id`);
