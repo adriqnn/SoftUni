35de668ac5c4ae4103a82bafc041422f57c575bd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-14 04:19:57 +0200

    03

--- 05_MySQL/_05_TableRelations/_03_SoftUniHiking.sql
+++ 05_MySQL/_05_TableRelations/_03_SoftUniHiking.sql
@@ -1,2 +1,2 @@
-SELECT `r`.`starting_point`,`r`.`end_point`,`r`.`leader_id`,concat_ws(' ',`first_name`,`last_name`) AS 'leader_name' FROM `routes` AS `r`
+SELECT `r`.`starting_point`, `r`.`end_point`, `r`.`leader_id`, CONCAT_WS(' ', `first_name`, `last_name`) AS 'leader_name' FROM `routes` AS `r`
 JOIN `campers` AS `c` ON `r`.`leader_id` = `c`.`id`;
