0f24ee382f64397d6758ba632b8e99dd4b43e95c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:57:05 +0200

    06

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_06_EmployeesHiredAfter.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_06_EmployeesHiredAfter.sql
@@ -1,4 +1,4 @@
-SELECT `e`.`first_name`,`e`.`last_name`,`e`.`hire_date`,`d`.`name` FROM `employees` AS `e`
+SELECT `e`.`first_name`, `e`.`last_name`, `e`.`hire_date`, `d`.`name` FROM `employees` AS `e`
 JOIN `departments` AS `d` USING (`department_id`)
-WHERE `e`.`hire_date` > '1999-1-1' AND (`d`.`name` = 'Sales' OR `d`.`name` = 'Finance')
+WHERE `e`.`hire_date` > '1999-01-01' AND `d`.`name` IN ('Sales', 'Finance')
 ORDER BY `e`.`hire_date` ASC;
