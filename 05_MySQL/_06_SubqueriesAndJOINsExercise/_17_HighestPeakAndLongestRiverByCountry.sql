8fefe2b1371dcbecae450fd579fb2eb390e70e46
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 21:53:49 +0200

    17

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_17_HighestPeakAndLongestRiverByCountry.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_17_HighestPeakAndLongestRiverByCountry.sql
@@ -1,7 +1,8 @@
-SELECT `c`.`country_name`,max(`p`.`elevation`) AS 'highest_peak',max(`r`.`length`) AS 'longest_river' FROM `countries` AS `c`
+SELECT `c`.`country_name`, MAX(`p`.`elevation`) AS 'highest_peak', MAX(`r`.`length`) AS 'longest_river' FROM `countries` AS `c`
 LEFT JOIN `mountains_countries` AS `mc` USING (`country_code`)
 LEFT JOIN `peaks` AS `p` USING (`mountain_id`)
 LEFT JOIN `countries_rivers` AS `cr` USING (`country_code`)
 LEFT JOIN `rivers` AS `r` ON `r`.`id` = `cr`.`river_id`
 GROUP BY `c`.`country_code`
-ORDER BY `highest_peak`DESC,`longest_river`DESC,`c`.`country_name` LIMIT 5;
+ORDER BY `highest_peak` DESC, `longest_river` DESC, `c`.`country_name`
+LIMIT 5;
