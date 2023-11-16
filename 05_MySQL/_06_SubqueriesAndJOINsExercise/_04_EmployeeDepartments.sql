ece37f3a833390c72eff0e0644a2249ebd22fee2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:54:10 +0200

    04

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_04_EmployeeDepartments.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_04_EmployeeDepartments.sql
@@ -1,4 +1,5 @@
-SELECT `e`.`employee_id`,`e`.`first_name`,`e`.`salary`,`d`.`name` FROM `employees` AS `e`
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`salary`, `d`.`name` FROM `employees` AS `e`
 JOIN `departments` AS `d` USING (`department_id`)
 WHERE `e`.`salary` > 15000
-ORDER BY `d`.`department_id` DESC LIMIT 5;
+ORDER BY `d`.`department_id` DESC
+LIMIT 5;
