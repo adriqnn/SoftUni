a78f0515417b14c67ba254d4a3e47de41234b3c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 20:14:07 +0200

    12

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_12_HighestPeaksInBulgaria.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_12_HighestPeaksInBulgaria.sql
@@ -1,4 +1,4 @@
-SELECT `c`.`country_code`,`m`.`mountain_range`,`p`.`peak_name`,`p`.`elevation` FROM `countries` AS `c`
+SELECT `c`.`country_code`, `m`.`mountain_range`, `p`.`peak_name`, `p`.`elevation` FROM `countries` AS `c`
 JOIN `mountains_countries` AS `mc` ON `c`.`country_code` = `mc`.`country_code`
 JOIN `mountains` AS `m` ON `mc`.`mountain_id` = `m`.`id`
 JOIN `peaks` AS `p` ON `p`.`mountain_id` = `m`.`id`
