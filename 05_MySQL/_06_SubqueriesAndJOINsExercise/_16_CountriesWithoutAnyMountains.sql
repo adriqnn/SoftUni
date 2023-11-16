ae518de19c420bec67ace755324ebb14c3d6f28a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 21:09:48 +0200

    16

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_16_CountriesWithoutAnyMountains.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_16_CountriesWithoutAnyMountains.sql
@@ -1,4 +1,4 @@
-SELECT count(*) FROM `countries` AS `c`
-LEFT JOIN `mountains_countries` AS `mc` USING(`country_code`)
+SELECT COUNT(*) FROM `countries` AS `c`
+LEFT JOIN `mountains_countries` AS `mc` USING (`country_code`)
 LEFT JOIN `mountains` AS `m` ON `m`.`id` = `mc`.`mountain_id`
 WHERE `mountain_id` IS NULL;
