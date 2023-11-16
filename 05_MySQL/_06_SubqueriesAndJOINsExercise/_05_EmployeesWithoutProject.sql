b538f2ee4347f653ff228dae32ea26521329c366
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:56:21 +0200

    05

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_05_EmployeesWithoutProject.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_05_EmployeesWithoutProject.sql
@@ -1,4 +1,5 @@
-SELECT `e`.`employee_id`,`e`.`first_name` FROM `employees` AS `e`
+SELECT `e`.`employee_id`, `e`.`first_name` FROM `employees` AS `e`
 LEFT JOIN `employees_projects` AS `ep` USING (`employee_id`)
-WHERE `ep`.project_id IS NULL
-ORDER BY `e`.`employee_id` DESC LIMIT 3;
+WHERE `ep`.`project_id` IS NULL
+ORDER BY `e`.`employee_id` DESC
+LIMIT 3;
