19ee8585f2fa916ab431ef991ede3ffc7775fe8b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 20:17:02 +0200

    13

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_13_CountMountainRanges.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_13_CountMountainRanges.sql
@@ -1,5 +1,5 @@
-SELECT `c`.`country_code`, count(`mc`.`mountain_id`) AS 'count' FROM `countries` AS `c`
+SELECT `c`.`country_code`, COUNT(`mc`.`mountain_id`) AS 'count' FROM `countries` AS `c`
 JOIN `mountains_countries` AS `mc` USING(`country_code`)
+WHERE `c`.`country_code` IN ('BG', 'US', 'RU')
 GROUP BY `c`.`country_code`
-HAVING `c`.`country_code` IN ('BG','US','RU')
 ORDER BY `count` DESC;
