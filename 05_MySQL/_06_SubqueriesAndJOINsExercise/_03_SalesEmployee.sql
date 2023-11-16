b6575aff796c3ccb4bfd69fdc2a0d331c76532ca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:52:57 +0200

    03

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_03_SalesEmployee.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_03_SalesEmployee.sql
@@ -1,4 +1,4 @@
-SELECT `e`.`employee_id`,`e`.`first_name`,`e`.`last_name`,`d`.`name` FROM `employees` AS `e`
+SELECT `e`.`employee_id`, `e`.`first_name`, `e`.`last_name`, `d`.`name` FROM `employees` AS `e`
 JOIN `departments` AS `d` USING (`department_id`)
 WHERE `d`.`name` = 'Sales'
 ORDER BY `e`.`employee_id` DESC;
