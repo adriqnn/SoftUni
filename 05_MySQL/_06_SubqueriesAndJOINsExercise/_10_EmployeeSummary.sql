39bd3e96f78b13ceb824f69e91e01f4c1d14b4ef
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 20:08:44 +0200

    10

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_10_EmployeeSummary.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_10_EmployeeSummary.sql
@@ -1,7 +1,4 @@
-SELECT `e`.`employee_id`,
-concat(`e`.`first_name`,' ',`e`.`last_name`) AS `employee_name`, 
-concat(`m`.`first_name`,' ',`m`.`last_name`) AS `manager_name`,
-`d`.`name` FROM `employees` AS `e`
+SELECT `e`.`employee_id`, CONCAT(`e`.`first_name`, ' ', `e`.`last_name`) AS `employee_name`, CONCAT(`m`.`first_name`, ' ', `m`.`last_name`) AS `manager_name`, `d`.`name` FROM `employees` AS `e`
 JOIN `employees` AS `m` ON `m`.`employee_id` = `e`.`manager_id`
-JOIN  `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
+JOIN `departments` AS `d` ON `e`.`department_id` = `d`.`department_id`
 ORDER BY `e`.`employee_id` LIMIT 5;
