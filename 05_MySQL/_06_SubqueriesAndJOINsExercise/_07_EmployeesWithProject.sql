7471d48ba8541b6f5cbf8fabcf22b099297eb742
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:58:16 +0200

    07

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_07_EmployeesWithProject.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_07_EmployeesWithProject.sql
@@ -1,5 +1,6 @@
-SELECT `e`.`employee_id`,`e`.`first_name`,`p`.`name` FROM `employees` AS `e`
+SELECT `e`.`employee_id`, `e`.`first_name`, `p`.`name` FROM `employees` AS `e`
 JOIN `employees_projects` AS `ep` USING (`employee_id`)
 JOIN `projects` AS `p` USING (`project_id`)
 WHERE `p`.`start_date` > '2002-08-13 23:59:59' AND `p`.`end_date` IS NULL
-ORDER BY `e`.`first_name`,`p`.`name` LIMIT 5;
+ORDER BY `e`.`first_name`, `p`.`name`
+LIMIT 5;
