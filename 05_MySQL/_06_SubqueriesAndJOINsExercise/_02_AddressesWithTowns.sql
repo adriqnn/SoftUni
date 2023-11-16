2c8ae086e05e983b00e6b4ce0ae0fef44912173c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 19:48:19 +0200

    02

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_02_AddressesWithTowns.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_02_AddressesWithTowns.sql
@@ -1,4 +1,5 @@
-SELECT `e`.`first_name`,`e`.`last_name`,`t`.`name`,`a`.`address_text`  FROM `employees` as `e`
-JOIN `addresses` as `a` USING (`address_id`)
-JOIN `towns` as `t` ON `t`.`town_id` = `a`.`town_id`
-ORDER BY e.`first_name` , e.`last_name` LIMIT 5;
+SELECT `e`.`first_name`, `e`.`last_name`, `t`.`name`, `a`.`address_text` FROM `employees` AS `e`
+JOIN `addresses` AS `a` USING (`address_id`)
+JOIN `towns` AS `t` ON `t`.`town_id` = `a`.`town_id`
+ORDER BY `e`.`first_name`, `e`.`last_name`
+LIMIT 5;
