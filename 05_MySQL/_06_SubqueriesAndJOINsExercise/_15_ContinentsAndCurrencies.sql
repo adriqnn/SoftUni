abdac3263f65e0db2d5712bec95d4ba1e515c253
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-16 21:09:17 +0200

    15

--- 05_MySQL/_06_SubqueriesAndJOINsExercise/_15_ContinentsAndCurrencies.sql
+++ 05_MySQL/_06_SubqueriesAndJOINsExercise/_15_ContinentsAndCurrencies.sql
@@ -1,5 +1,9 @@
-SELECT `c`.`continent_code`,`c`.`currency_code`,count(*) AS 'currency_usage' FROM `countries` AS `c`
-GROUP BY `c`.`continent_code`,`c`.`currency_code`
-HAVING `currency_usage` > 1 AND `currency_usage` = (SELECT count(*) as 'muc' FROM `countries` AS `c2`
-WHERE `c2`.`continent_code` = `c`.`continent_code`GROUP BY `c2`.`currency_code`ORDER BY `muc` DESC LIMIT 1)
-ORDER BY `c`.`continent_code`,`c`.`currency_code`;
+SELECT `c`.`continent_code`, `c`.`currency_code`, COUNT(*) AS 'currency_usage' FROM `countries` AS `c`
+GROUP BY `c`.`continent_code`, `c`.`currency_code`
+HAVING `currency_usage` > 1 AND `currency_usage` = (
+    SELECT COUNT(*) AS 'muc' FROM `countries` AS `c2`
+    WHERE `c2`.`continent_code` = `c`.`continent_code`
+    GROUP BY `c2`.`currency_code`
+    ORDER BY `muc` DESC
+    LIMIT 1 )
+ORDER BY `c`.`continent_code`, `c`.`currency_code`;
