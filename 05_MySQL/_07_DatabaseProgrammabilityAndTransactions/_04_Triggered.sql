ad6bbbfa678eb6f1465ca3ce7586937d3ce4e3cb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:10:36 +0200

    04

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_04_Triggered.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_04_Triggered.sql
@@ -8,20 +8,20 @@ CREATE TABLE `deleted_employees` (
     `salary` DECIMAL(19 , 4 ) NOT NULL
 );
 
-CREATE 
-    TRIGGER `trigger_employee`
- BEFORE DELETE ON `employees` FOR EACH ROW 
+CREATE TRIGGER `trigger_employee`
+BEFORE DELETE ON `employees` 
+FOR EACH ROW 
     INSERT INTO `deleted_employees`(
-    `first_name`, 
-    `last_name`, 
-    `middle_name`, 
-    `job_title`, 
-    `department_id`, 
-    `salary`) 
+        `first_name`, 
+        `last_name`, 
+        `middle_name`, 
+        `job_title`, 
+        `department_id`, 
+        `salary`) 
     VALUES (
-    OLD.`first_name`, 
-    OLD.`last_name`, 
-    OLD.`middle_name`, 
-    OLD.`job_title`, 
-    OLD.`department_id`, 
-    OLD.`salary`);
+        OLD.`first_name`, 
+        OLD.`last_name`, 
+        OLD.`middle_name`, 
+        OLD.`job_title`, 
+        OLD.`department_id`, 
+        OLD.`salary`);
