5ff25ae4fc86f887d67c628cd504ba39b8fda1cc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:28:20 +0200

    05

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_05_SalaryLevelFunction.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_05_SalaryLevelFunction.sql
@@ -2,13 +2,13 @@ CREATE FUNCTION ufn_get_salary_level(salary DOUBLE(10, 4))
 RETURNS VARCHAR(20)
 DETERMINISTIC
 BEGIN
-	DECLARE salary_level VARCHAR(20);
+    DECLARE salary_level VARCHAR(20);
 
-	CASE
-		WHEN salary < 30000 THEN SET salary_level = 'Low';
-		WHEN salary <= 50000 THEN SET salary_level = 'Average';
-		WHEN salary > 50000 THEN SET salary_level = 'High';
-	END CASE;
+    CASE
+        WHEN salary < 30000 THEN SET salary_level = 'Low';
+        WHEN salary <= 50000 THEN SET salary_level = 'Average';
+        WHEN salary > 50000 THEN SET salary_level = 'High';
+    END CASE;
 
     RETURN salary_level;
 END;
