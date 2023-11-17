6fcdf95bbc5fb212ea0bb8968bbc419ac68acd95
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:31:00 +0200

    06

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_06_EmployeesBySalaryLevel.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_06_EmployeesBySalaryLevel.sql
@@ -1,10 +1,11 @@
-CREATE PROCEDURE usp_get_employees_by_salary_level(level_of_salary varchar(20))
+CREATE PROCEDURE usp_get_employees_by_salary_level(IN level_of_salary VARCHAR(20))
 BEGIN
-SELECT `e`.`first_name`,`e`.`last_name` FROM `employees` AS `e`
-WHERE  (CASE
-		WHEN level_of_salary = 'low' THEN `e`.`salary` < 30000
-		WHEN level_of_salary = 'average' THEN `e`.`salary` >= 30000 AND `e`.`salary` <= 50000
-		WHEN level_of_salary = 'high' THEN `e`.`salary` > 50000
+    SELECT `e`.`first_name`, `e`.`last_name`
+    FROM `employees` AS `e`
+    WHERE (CASE
+            WHEN LOWER(level_of_salary) = 'low' THEN `e`.`salary` < 30000
+            WHEN LOWER(level_of_salary) = 'average' THEN `e`.`salary` >= 30000 AND `e`.`salary` <= 50000
+            WHEN LOWER(level_of_salary) = 'high' THEN `e`.`salary` > 50000
         END)
-ORDER BY `e`.`first_name` DESC ,`e`.`last_name` DESC;
-END
+    ORDER BY `e`.`first_name` DESC, `e`.`last_name` DESC;
+END;
