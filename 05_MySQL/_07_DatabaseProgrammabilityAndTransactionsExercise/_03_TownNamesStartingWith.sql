f85690ad05d03532b541f647ac99ecdb1795f27d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:18:20 +0200

    03

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_03_TownNamesStartingWith.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_03_TownNamesStartingWith.sql
@@ -1,6 +1,6 @@
-CREATE PROCEDURE usp_get_towns_starting_with(town_name_start TEXT)
+CREATE PROCEDURE usp_get_towns_starting_with(IN town_name_start TEXT)
 BEGIN
-	SELECT `t`.`name` FROM `towns` AS `t`
-    WHERE `t`.`name` LIKE concat(town_name_start,'%')
+    SELECT `t`.`name` FROM `towns` AS `t`
+    WHERE `t`.`name` LIKE CONCAT(town_name_start, '%')
     ORDER BY `t`.`name`;
-END
+END;
