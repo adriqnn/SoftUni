069d14f2f9c5a301f6e99ff8ed00e63c3be78506
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 22:59:38 +0200

    03

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_03_EmployeesPromotionByID.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_03_EmployeesPromotionByID.sql
@@ -1,10 +1,12 @@
-CREATE PROCEDURE usp_raise_salary_by_id(userid INT)
-BEGIN 
-START TRANSACTION;
-IF( (SELECT count(*) FROM employees WHERE employee_id = userid) > 0)
-THEN
-UPDATE employees SET salary = salary*1.05 WHERE employee_id = userid;
-COMMIT;
-ELSE ROLLBACK;
-END IF;
+CREATE PROCEDURE usp_raise_salary_by_id(IN userid INT)
+BEGIN
+    START TRANSACTION;
+
+    IF EXISTS (SELECT 1 FROM employees WHERE employee_id = userid)
+    THEN
+        UPDATE employees SET salary = salary * 1.05 WHERE employee_id = userid;
+        COMMIT;
+    ELSE
+        ROLLBACK;
+    END IF;
 END
