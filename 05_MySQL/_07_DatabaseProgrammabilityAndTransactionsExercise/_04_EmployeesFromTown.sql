196e5e4e19cd19ba4e8a8070479f2f407510cfde
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:20:06 +0200

    04

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_04_EmployeesFromTown.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_04_EmployeesFromTown.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE usp_get_employees_from_town(town_name TEXT)
+CREATE PROCEDURE usp_get_employees_from_town(IN town_name TEXT)
 BEGIN
-	SELECT `e`.`first_name`,`e`.`last_name` FROM `employees` as `e`
+    SELECT `e`.`first_name`, `e`.`last_name` FROM `employees` AS `e`
     JOIN `addresses` AS `a` ON `a`.`address_id` = `e`.`address_id`
     JOIN `towns` AS `t` ON `t`.`town_id` = `a`.`town_id`
-    WHERE `t`.`name` = `town_name`
-    ORDER BY `e`.`first_name`,`e`.`last_name`,`e`.`employee_id`;
-END
+    WHERE `t`.`name` = town_name
+    ORDER BY `e`.`first_name`, `e`.`last_name`, `e`.`employee_id`;
+END;
