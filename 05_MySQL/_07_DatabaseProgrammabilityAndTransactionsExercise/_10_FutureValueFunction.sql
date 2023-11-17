85003d92c29737a8c86f88ad104113e6f26a1973
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:51:11 +0200

    10

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_10_FutureValueFunction.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_10_FutureValueFunction.sql
@@ -1,5 +1,5 @@
-CREATE FUNCTION ufn_calculate_future_value(IN initial_sum DECIMAL(20, 4), IN interest_rate DECIMAL(20, 4), IN years INT)
+CREATE FUNCTION ufn_calculate_future_value(initial_sum DECIMAL(20, 4), interest_rate DECIMAL(20, 4), years INT)
 RETURNS DECIMAL(20, 4)
 BEGIN
-    RETURN initial_sum * POW((1 + interest_rate), years);
-END;
+    RETURN initial_sum * pow((1 + interest_rate), years);
+END
