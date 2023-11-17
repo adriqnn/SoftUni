0db04397ae05b877b9ede618125b2fb456914c23
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 22:47:37 +0200

    01

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_01_CountEmployeesByTown.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactions/_01_CountEmployeesByTown.sql
@@ -2,10 +2,16 @@ CREATE FUNCTION ufn_Count_employees_by_town(cityname VARCHAR(100))
 RETURNS INT
 DETERMINISTIC
 BEGIN
-DECLARE x INT;
-SET x := (SELECT count(*) FROM towns AS t
-LEFT JOIN addresses AS a ON t.town_id = a.town_id
-LEFT JOIN employees AS e ON e.address_id = a.address_id
-WHERE t.name = cityname);
-RETURN x;
+    DECLARE x INT;
+
+    -- Use LIMIT 1 to handle cases where more than one row is returned
+    SET x := (
+        SELECT COUNT(*) FROM towns AS t
+        LEFT JOIN addresses AS a ON t.town_id = a.town_id
+        LEFT JOIN employees AS e ON e.address_id = a.address_id
+        WHERE t.name = cityname
+        LIMIT 1
+    );
+
+    RETURN x;
 END
