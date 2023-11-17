4f8dc69a57f6c5595eb86a20aee81d3b9fda2598
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:47:15 +0200

    09

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_09_PeopleWithBalanceHigherThan.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_09_PeopleWithBalanceHigherThan.sql
@@ -1,8 +1,8 @@
-CREATE PROCEDURE usp_get_holders_with_balance_higher_than(money double(19,4))
+CREATE PROCEDURE usp_get_holders_with_balance_higher_than(IN money DOUBLE(19, 4))
 BEGIN
-	SELECT ah.first_name,ah.last_name FROM account_holders AS ah
-    RIGHT JOIN accounts AS a ON ah.id = a.account_holder_id
+    SELECT ah.first_name, ah.last_name FROM account_holders AS ah
+    LEFT JOIN accounts AS a ON ah.id = a.account_holder_id
     GROUP BY ah.id
-    HAVING sum(balance) > money
-    ORDER by ah.id;
-END
+    HAVING SUM(balance) > money
+    ORDER BY ah.id;
+END;
