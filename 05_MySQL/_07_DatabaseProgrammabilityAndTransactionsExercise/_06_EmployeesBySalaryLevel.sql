a700d8564169640a2fd7e7fc1a43ae9901fc0ac3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-17 23:31:33 +0200

    06

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_06_EmployeesBySalaryLevel.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_06_EmployeesBySalaryLevel.sql
@@ -1,7 +1,6 @@
 CREATE PROCEDURE usp_get_employees_by_salary_level(IN level_of_salary VARCHAR(20))
 BEGIN
-    SELECT `e`.`first_name`, `e`.`last_name`
-    FROM `employees` AS `e`
+    SELECT `e`.`first_name`, `e`.`last_name` FROM `employees` AS `e`
     WHERE (CASE
             WHEN LOWER(level_of_salary) = 'low' THEN `e`.`salary` < 30000
             WHEN LOWER(level_of_salary) = 'average' THEN `e`.`salary` >= 30000 AND `e`.`salary` <= 50000
