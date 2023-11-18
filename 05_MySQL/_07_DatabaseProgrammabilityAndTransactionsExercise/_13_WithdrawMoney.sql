e977e87802038fc3e5394a6b53fd3d2c9c5b98b3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-18 22:52:11 +0200

    13

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_13_WithdrawMoney.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_13_WithdrawMoney.sql
@@ -1,13 +1,13 @@
-CREATE PROCEDURE usp_withdraw_money(account_id INT, money_amount DECIMAL(19, 4))
+CREATE PROCEDURE usp_withdraw_money(`account_id` INT, `money_amount` DECIMAL(19, 4))
 BEGIN
     START TRANSACTION;
-    IF(money_amount <= 0 OR
-    (SELECT a.`balance` FROM `accounts` AS a WHERE a.`id` = account_id) < money_amount)
+    IF (`money_amount` <= 0 OR
+        (SELECT `a`.`balance` FROM `accounts` AS `a` WHERE `a`.`id` = `account_id`) < `money_amount`)
     THEN 
         ROLLBACK;
     ELSE
-        UPDATE `accounts` AS a
-        SET balance = balance - money_amount
-        WHERE a.`id` = account_id;
+        UPDATE `accounts` AS `a`
+        SET `balance` = `balance` - `money_amount`
+        WHERE `a`.`id` = `account_id`;
     END IF;
-END
+END;
