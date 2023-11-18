8cda7d73759e050e96b17a99fb273e4730baf07b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-18 22:54:02 +0200

    14

--- 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_14_MoneyTransfer.sql
+++ 05_MySQL/_07_DatabaseProgrammabilityAndTransactionsExercise/_14_MoneyTransfer.sql
@@ -1,31 +1,28 @@
-CREATE PROCEDURE usp_transfer_money(from_account_id INT, to_account_id INT, amount DECIMAL(20, 4)) 
+CREATE PROCEDURE usp_transfer_money(`from_account_id` INT, `to_account_id` INT, `amount` DECIMAL(20, 4)) 
 BEGIN
-    IF (amount >= 0)
-    AND
-    (SELECT a.`id` FROM `accounts` AS a WHERE a.`id` = from_account_id) IS NOT NULL
-    AND
-    (SELECT a.`id` FROM `accounts` AS a WHERE a.`id` = to_account_id) IS NOT NULL
+    IF (`amount` >= 0)
+       AND
+       (SELECT `a`.`id` FROM `accounts` AS `a` WHERE `a`.`id` = `from_account_id`) IS NOT NULL
+       AND
+       (SELECT `a`.`id` FROM `accounts` AS `a` WHERE `a`.`id` = `to_account_id`) IS NOT NULL
     THEN
-    START TRANSACTION;
-    
-UPDATE `accounts` AS a 
-SET 
-    balance = balance - amount
-WHERE
-    a.`id` = from_account_id;
-    
-UPDATE `accounts` AS a 
-SET 
-    balance = balance + amount
-WHERE
-    a.`id` = to_account_id;
-    
-    IF(SELECT balance FROM `accounts` AS a WHERE a.`id` = from_account_id) < 0
-    THEN
-        ROLLBACK;
-    ELSE
-        COMMIT;
-    END IF;
-    
+        START TRANSACTION;
+
+        UPDATE `accounts` AS `a` 
+        SET `balance` = `balance` - `amount`
+        WHERE
+            `a`.`id` = `from_account_id`;
+
+        UPDATE `accounts` AS `a` 
+        SET `balance` = `balance` + `amount`
+        WHERE
+            `a`.`id` = `to_account_id`;
+
+        IF (SELECT `balance` FROM `accounts` AS `a` WHERE `a`.`id` = `from_account_id`) < 0
+        THEN
+            ROLLBACK;
+        ELSE
+            COMMIT;
+        END IF;
     END IF;
 END;
