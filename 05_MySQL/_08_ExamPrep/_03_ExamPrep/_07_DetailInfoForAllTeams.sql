9fcc8c376f13d6edce88c104e3c90912ea61348e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-19 21:54:35 +0200

    07

---
+++ 05_MySQL/_08_ExamPrep/_03_ExamPrep/_07_DetailInfoForAllTeams.sql
@@ -0,0 +1,2 @@
+SELECT `t`.`name` AS 'team_name', `t`.`established`, `t`.`fan_base`, (SELECT COUNT(*) FROM `players` WHERE `team_id` = `t`.`id`) AS `player_count` FROM `teams` AS `t`
+ORDER BY `player_count` DESC, `fan_base` DESC;
