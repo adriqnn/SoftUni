97ef342bd6fcf76da208a2aedd3cc896961c66a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-19 23:59:22 +0200

    11

--- 05_MySQL/_08_ExamPrep/_01_ExamPrep/_11_FindGoodPlaymakerByTeams.sql
+++ 05_MySQL/_08_ExamPrep/_01_ExamPrep/_11_FindGoodPlaymakerByTeams.sql
@@ -1,8 +1,9 @@
-CREATE PROCEDURE `udp_find_playmaker` (min_dribble_points INT, team_name VARCHAR(45))
+CREATE PROCEDURE `udp_find_playmaker` (`min_dribble_points` INT, `team_name` VARCHAR(45))
 BEGIN
-	SELECT CONCAT(p.`first_name`, ' ', p.`last_name`) AS `full_name`, p.`age`, p.`salary`, sd.`dribbling`, sd.`speed`, t.`name` FROM `skills_data` AS sd
-	JOIN `players` AS p ON p.`skills_data_id` = sd.`id`
-	RIGHT JOIN `teams` AS t ON t.`id` = p.`team_id`
-	WHERE t.`name` = team_name AND sd.`speed` > (SELECT AVG(`speed`) FROM `skills_data`) AND sd.`dribbling` > min_dribble_points
-	ORDER BY sd.`speed` DESC LIMIT 1;
+    SELECT CONCAT(`p`.`first_name`, ' ', `p`.`last_name`) AS `full_name`, `p`.`age`, `p`.`salary`, `sd`.`dribbling`, `sd`.`speed`, `t`.`name`
+    FROM `skills_data` AS `sd`
+    JOIN `players` AS `p` ON `p`.`skills_data_id` = `sd`.`id`
+    RIGHT JOIN `teams` AS `t` ON `t`.`id` = `p`.`team_id`
+    WHERE `t`.`name` = `team_name` AND `sd`.`speed` > (SELECT AVG(`speed`) FROM `skills_data`) AND `sd`.`dribbling` > `min_dribble_points`
+    ORDER BY `sd`.`speed` DESC LIMIT 1;
 END;
