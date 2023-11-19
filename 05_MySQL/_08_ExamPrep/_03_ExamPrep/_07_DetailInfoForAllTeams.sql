819db37d66a85f8868b2b17179128b806c55a542
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-19 23:13:57 +0200

    Delete 05_MySQL/_08_ExamPrep/_03_ExamPrep directory

--- 05_MySQL/_08_ExamPrep/_03_ExamPrep/_07_DetailInfoForAllTeams.sql
+++
@@ -1,2 +0,0 @@
-SELECT `t`.`name` AS 'team_name', `t`.`established`, `t`.`fan_base`, (SELECT COUNT(*) FROM `players` WHERE `team_id` = `t`.`id`) AS `player_count` FROM `teams` AS `t`
-ORDER BY `player_count` DESC, `fan_base` DESC;
--- 05_MySQL/_08_ExamPrep/_03_ExamPrep/_08_TheFastestPlayerByTowns.sql
+++
@@ -1,8 +0,0 @@
-SELECT MAX(sd.`speed`) AS `max_speed`, t.`name` AS `town_name` FROM `skills_data` AS `sd`
-RIGHT JOIN `players` AS `p` ON `p`.`skills_data_id` = `sd`.`id`
-RIGHT JOIN `teams` AS `tm` ON `p`.`team_id` = `tm`.`id`
-RIGHT JOIN `stadiums` AS `s` ON `tm`.`stadium_id` = `s`.`id`
-RIGHT JOIN `towns` AS `t` ON `s`.`town_id` = `t`.`id`
-WHERE  `tm`.`name` != 'Devify'
-GROUP BY `t`.`id`
-ORDER BY `max_speed` DESC, `t`.`name`;
