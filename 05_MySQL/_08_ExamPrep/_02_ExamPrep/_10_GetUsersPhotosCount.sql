036dde156638da7025366a3ca8c7c7da54264190
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-20 02:23:12 +0200

    10

--- 05_MySQL/_08_ExamPrep/_02_ExamPrep/_10_GetUsersPhotosCount.sql
+++ 05_MySQL/_08_ExamPrep/_02_ExamPrep/_10_GetUsersPhotosCount.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION `udf_users_photos_count` (`username` VARCHAR(30))
 RETURNS INT
 DETERMINISTIC
 BEGIN
-	RETURN (SELECT COUNT(*) AS `photosCount` FROM `users` AS `u`
-	JOIN `users_photos` AS `up` ON `up`.`user_id` = `u`.`id`
+    RETURN (SELECT COUNT(*) AS `photosCount` FROM `users` AS `u`
+    JOIN `users_photos` AS `up` ON `up`.`user_id` = `u`.`id`
     WHERE `u`.`username` = `username`);
 END;
