352131ed4918bcb08c7ee33b4e3f752eaa389615
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-20 02:24:23 +0200

    11

--- 05_MySQL/_08_ExamPrep/_02_ExamPrep/_11_IncreaseUserAge.sql
+++ 05_MySQL/_08_ExamPrep/_02_ExamPrep/_11_IncreaseUserAge.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE `udp_modify_user` (`address` VARCHAR(30), `town` VARCHAR(30))
 BEGIN
-	UPDATE `users` SET `age` = `age` + 10
+    UPDATE `users` SET `age` = `age` + 10
     WHERE
-		EXISTS (SELECT * FROM `addresses` AS `a`
-		WHERE `a`.`address` = `address` AND `town` = `a`.`town`);
+        EXISTS (SELECT * FROM `addresses` AS `a`
+        WHERE `a`.`address` = `address` AND `town` = `a`.`town`);
 END;
