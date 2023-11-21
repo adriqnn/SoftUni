3a39fb08ed7b0604c6745c3cea0d581ebd7298ee
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-22 00:04:04 +0200

    10

--- 05_MySQL/_08_ExamPrep/_03_ExamPrep/_10_FindAllCoursesByClientsPhone.sql
+++ 05_MySQL/_08_ExamPrep/_03_ExamPrep/_10_FindAllCoursesByClientsPhone.sql
@@ -1,10 +1,10 @@
-CREATE FUNCTION udf_courses_by_client(phone_num VARCHAR(20))
+CREATE FUNCTION `udf_courses_by_client` (`phone_num` VARCHAR(20))
 RETURNS INT 
 DETERMINISTIC
 BEGIN
     RETURN (
-        SELECT COUNT(*) FROM courses AS c
-        JOIN clients AS cl ON cl.id = c.client_id
-        WHERE cl.phone_number = phone_num
+        SELECT COUNT(*) FROM `courses` AS `c`
+        JOIN `clients` AS `cl` ON `cl`.`id` = `c`.`client_id`
+        WHERE `cl`.`phone_number` = `phone_num`
     );
 END;
