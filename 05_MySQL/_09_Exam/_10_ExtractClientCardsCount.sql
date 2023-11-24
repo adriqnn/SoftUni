7ca4e3d9a24e05a4b2b3fc515396266cc06fc8a0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-24 23:50:30 +0200

    10

--- 05_MySQL/_09_Exam/_10_ExtractClientCardsCount.sql
+++ 05_MySQL/_09_Exam/_10_ExtractClientCardsCount.sql
@@ -3,8 +3,7 @@ RETURNS INT
 DETERMINISTIC
 BEGIN
     RETURN (
-        SELECT COUNT(`p`.`id`)
-        FROM `customers` AS `c`
+        SELECT COUNT(`p`.`id`) FROM `customers` AS `c`
         JOIN `orders` AS `o` ON `c`.`id` = `o`.`customer_id`
         JOIN `orders_products` AS `op` ON `op`.`order_id` = `o`.`id`
         JOIN `products` AS `p` ON `p`.`id` = `op`.`product_id`
