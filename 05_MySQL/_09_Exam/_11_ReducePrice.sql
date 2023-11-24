a4ba7dc787717d215c73d823fca32a843e24d884
adriqnn <109970483+adriqnn@users.noreply.github.com>
2023-11-24 23:49:56 +0200

    11

---
+++ 05_MySQL/_09_Exam/_11_ReducePrice.sql
@@ -0,0 +1,8 @@
+CREATE PROCEDURE udp_reduce_price(`category_name` VARCHAR(50))
+BEGIN
+	UPDATE `products` AS `p`
+		JOIN `categories` AS `c` ON `p`.`category_id` = `c`.`id`
+		JOIN `reviews` AS `r` ON `p`.`review_id` = `r`.`id` 
+	SET `price` = `price` * 0.7
+	WHERE `category_name` = `c`.`name` AND `rating` < 4;
+END;
