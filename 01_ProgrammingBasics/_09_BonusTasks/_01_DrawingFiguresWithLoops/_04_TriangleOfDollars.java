71027e2af42e514351f58829fbe4cb3e778be5c2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-01-18 14:32:29 +0200

    04

---
+++ 01_ProgrammingBasics/_09_BonusTasks/_01_DrawingFiguresWithLoops/_04_TriangleOfDollars.java
@@ -0,0 +1,22 @@
+package _01_ProgrammingBasics._09_BonusTasks._01_DrawingFiguresWithLoops;
+
+import java.util.Scanner;
+
+public class _04_TriangleOfDollars {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Read input value
+        int n = Integer.parseInt(scan.nextLine());
+
+        // Print a triangle of $ signs
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("$ ");
+            }
+            System.out.println();
+        }
+
+        scan.close();
+    }
+}
