619eac3d5009b5c679c68df5ffc7984bd02445cb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-12 19:49:27 +0200

    03

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_03_PthBit.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_03_PthBit.java
@@ -1,4 +1,17 @@
 package _02_ProgrammingFundamentals._14_BonusTasks._00_05_BitwiseOperations;
 
 public class _03_PthBit {
+  public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int num = Integer.parseInt(scan.nextLine());
+        int positionAt = Integer.parseInt(scan.nextLine());
+
+        int binaryNum = num >> positionAt;
+        int bitAtPosition = binaryNum & 1;
+
+        System.out.println(bitAtPosition);
+
+        scan.close();
+    }
 }
