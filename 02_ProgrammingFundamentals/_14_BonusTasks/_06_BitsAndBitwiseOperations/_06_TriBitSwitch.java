a4d98f8cbd09bfeb8e3b743d7400284928798ca8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-13 04:38:11 +0200

    06

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_06_TriBitSwitch.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_06_TriBitSwitch.java
@@ -1,4 +1,20 @@
 package _02_ProgrammingFundamentals._14_BonusTasks._00_05_BitwiseOperations;
 
 public class _06_TriBitSwitch {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int number = Integer.parseInt(scan.nextLine());
+        int positionAt = Integer.parseInt(scan.nextLine());
+
+        int triBitSwitch = triBitSwitch(number, positionAt);
+        System.out.println(triBitSwitch);
+
+        scan.close();
+    }
+
+    public static int triBitSwitch(int number, int position) {
+        int mask = 7 << position;
+        return (number ^ mask);
+    }
 }
