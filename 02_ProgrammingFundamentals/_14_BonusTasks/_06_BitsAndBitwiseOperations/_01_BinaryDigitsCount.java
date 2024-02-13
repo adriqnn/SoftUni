2476c1ac0589bc5b016636aa56a476531c15813a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-13 02:07:19 +0200

    01

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_01_BinaryDigitsCount.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_01_BinaryDigitsCount.java
@@ -6,20 +6,26 @@ public class _01_BinaryDigitsCount {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num = Integer.parseInt(scan.nextLine());
-        String bit = scan.nextLine();
+        int numberToCheck = Integer.parseInt(scan.nextLine());
+        int binaryDigit = Integer.parseInt(scan.nextLine());
 
-        String binaryNum = Integer.toBinaryString(num);
-        int counter = 0;
-
-        for (int i = 0; i < binaryNum.length(); i++) {
-            if(bit.charAt(0) == binaryNum.charAt(i)){
-                counter++;
-            }
-        }
-
-        System.out.println(counter);
+        int count = countBinaryDigits(numberToCheck, binaryDigit);
+        System.out.println(count);
 
         scan.close();
+    }
+
+    public static int countBinaryDigits(int number, int binaryDigit) {
+        int count = 0;
+
+        while (number > 0) {
+            int digit = number % 2;
+            if (digit == binaryDigit) {
+                count++;
+            }
+            number /= 2;
+        }
+
+        return count;
     }
 }
