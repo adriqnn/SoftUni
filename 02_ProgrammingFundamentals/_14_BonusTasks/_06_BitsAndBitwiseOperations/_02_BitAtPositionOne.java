d9211c15ba4c984260aeb86de13a8c839560c749
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-13 02:11:24 +0200

    02

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_02_BitAtPositionOne.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_02_BitAtPositionOne.java
@@ -6,14 +6,17 @@ public class _02_BitAtPositionOne {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num = Integer.parseInt(scan.nextLine());
+        int number = Integer.parseInt(scan.nextLine());
         int position = 1;
 
-        int binaryNum = num >> position;
-        int bitAtPosition = binaryNum & 1;
-
+        int bitAtPosition = bitAtPosition(number, position);
         System.out.println(bitAtPosition);
 
         scan.close();
+    }
+
+    public static int bitAtPosition(int number, int position) {
+        int binaryNum = number >> position;
+        return binaryNum & 1;
     }
 }
