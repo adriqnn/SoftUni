f3218bcb020302760dd9d5a429300e97bc347ed9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-13 02:22:34 +0200

    04

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_04_BitDestroyer.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_04_BitDestroyer.java
@@ -1,17 +1,20 @@
 package _02_ProgrammingFundamentals._14_BonusTasks._00_05_BitwiseOperations;
 
 public class _04_BitDestroyer {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int num = Integer.parseInt(scan.nextLine());
+        int number = Integer.parseInt(scan.nextLine());
         int positionAt = Integer.parseInt(scan.nextLine());
 
-        int mask = ~(1 << positionAt);
-        int result = num & mask;
-
-        System.out.println(result);
+        int setBitTo0 = setBitTo0(number, positionAt);
+        System.out.println(setBitTo0);
 
         scan.close();
+    }
+
+    public static int setBitTo0(int number, int position) {
+        int mask = ~(1 << position);
+        return number & mask;
     }
 }
