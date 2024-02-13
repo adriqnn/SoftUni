67a2c383c66ceeb274d55ecf5cec0c345e04539d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-13 04:10:00 +0200

    05

--- 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_05_OddTimes.java
+++ 02_ProgrammingFundamentals/_14_BonusTasks/_06_BitsAndBitwiseOperations/_05_OddTimes.java
@@ -1,4 +1,25 @@
 package _02_ProgrammingFundamentals._14_BonusTasks._00_05_BitwiseOperations;
 
 public class _05_OddTimes {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] numbersArray = scan.nextLine().split("\\s+");
+
+        int oddNumber = findTheNumberWithOddOccurrences(numbersArray);
+        System.out.println(oddNumber);
+
+        scan.close();
+    }
+
+    private static int findTheNumberWithOddOccurrences(String[] numbersArray) {
+        int result = 0;
+
+        for(String numStr: numbersArray){
+            int num = Integer.parseInt(numStr);
+            result ^= num;
+        }
+
+        return result;
+    }
 }
