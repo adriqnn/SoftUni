fdadc2dd8c1eccf3a0066557cc10955377c6b54d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-02-17 06:15:13 +0200

    01

---
+++ 17_DataStructuresJavaFundamentals/_02_LinearDataStructures/_01_ArrayList/main/ArrayList.java
@@ -0,0 +1,145 @@
+package implementations;
+
+import interfaces.List;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ArrayList<E> implements List<E> {
+    private static final int INITIAL_SIZE = 4;
+    private Object[] elements;
+    private int size;
+
+    public ArrayList() {
+        this.elements = new Object[INITIAL_SIZE];
+        this.size = 0;
+    }
+
+    @Override
+    public boolean add(E element) {
+        if(this.size == this.elements.length){
+            this.elements = grow();
+        }
+
+        this.elements[this.size++] = element;
+        return true;
+    }
+
+    private Object[] grow() {
+        return Arrays.copyOf(this.elements, this.elements.length * 2);
+    }
+
+    private void checkIndex(int index){
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException(String.format("Index out of bounds: %d for size: %d", index, this.size));
+        }
+    }
+
+    private void insert(int index, E element){
+        if(this.size == this.elements.length){
+            this.elements = grow();
+        }
+
+        E lastElement = this.get(this.size - 1);
+        for (int i = this.size-1; i > index ; i--) {
+            this.elements[i] = this.elements[i-1];
+        }
+
+        this.elements[this.size] = lastElement;
+        this.elements[index] = element;
+        this.size++;
+    }
+
+    private void shift(int index){
+        for (int i = index; i < this.size-1; i++) {
+            this.elements[i] = this.elements[i+1];
+        }
+    }
+
+    private void ensureCapacity() {
+        if(this.size < this.elements.length / 3) {
+            this.elements = shrink();
+        }
+    }
+
+    private Object[] shrink(){
+        return Arrays.copyOf(this.elements, this.elements.length / 2);
+    }
+
+    @Override
+    public boolean add(int index, E element) {
+        checkIndex(index);
+        insert(index, element);
+        return true;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+        return (E) this.elements[index];
+    }
+
+    @Override
+    public E set(int index, E element) {
+        checkIndex(index);
+        E oldElement = this.get(index);
+        this.elements[index] = element;
+        return oldElement;
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+        E oldElement = this.get(index);
+
+        this.shift(index);
+        this.size--;
+        this.elements[size] = null;
+        this.ensureCapacity();
+
+        return (E) oldElement;
+    }
+
+    @Override
+    public int size() {
+        return this.size;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        for (int i = 0; i < this.size; i++) {
+            if(element.equals(this.elements[i])){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return this.indexOf(element) != -1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<E>() {
+            private int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < size();
+            }
+
+            @Override
+            public E next() {
+                return get(index++);
+            }
+        };
+    }
+}
