345dac3ab33b56bf60f3ceaa63b8b5a8864301e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-10 00:13:30 +0200

    01

--- 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/test/HierarchyStructureInitializer.java
+++ 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/test/HierarchyStructureInitializer.java
@@ -1,10 +1,7 @@
-package test;
-
-import main.Hierarchy;
-import main.IHierarchy;
+import Hierarchy;
+import IHierarchy;
 
 public class HierarchyStructureInitializer {
-    
     public static <T> IHierarchy<T> create(T root) {
         return new Hierarchy<>(root);
     }
