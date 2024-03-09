164a00b9a12b337268c85d85b88467c865b3a9eb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-09 23:55:34 +0200

    01

--- 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/test/correctness/Constructor.java
+++ 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/test/correctness/Constructor.java
@@ -4,6 +4,7 @@ import main.Hierarchy;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
+
 import helpers.BaseTest;
 import types.CorrectnessTests;
 
