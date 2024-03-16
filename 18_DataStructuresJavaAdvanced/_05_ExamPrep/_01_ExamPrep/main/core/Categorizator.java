df5c804dd35e5ae3d3251d52a85253861765b1c5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-17 01:16:58 +0200

    01

--- 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/core/Categorizator.java
+++ 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/core/Categorizator.java
@@ -6,18 +6,11 @@ import java.util.List;
 
 public interface Categorizator {
     void addCategory(Category category);
-
     void assignParent(String childCategoryId, String parentCategoryId);
-
     void removeCategory(String categoryId);
-
     boolean contains(Category category);
-
     int size();
-
     Iterable<Category> getChildren(String categoryId);
-
     Iterable<Category> getHierarchy(String categoryId);
-
     Iterable<Category> getTop3CategoriesOrderedByDepthOfChildrenThenByName();
 }
