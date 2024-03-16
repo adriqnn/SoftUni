9c557268274b7f5f198d313035f6b7a9706e24aa
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-17 01:17:25 +0200

    01

--- 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/core/TaskManager.java
+++ 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/core/TaskManager.java
@@ -4,22 +4,13 @@ import models.Task;
 
 public interface TaskManager {
     void addTask(Task task);
-
     boolean contains(Task task);
-
     int size();
-
     Task getTask(String taskId);
-
     void deleteTask(String taskId);
-
     Task executeTask();
-
     void rescheduleTask(String taskId);
-
     Iterable<Task> getDomainTasks(String domain);
-
     Iterable<Task> getTasksInEETRange(int lowerBound, int upperBound);
-
     Iterable<Task> getAllTasksOrderedByEETThenByName();
 }
