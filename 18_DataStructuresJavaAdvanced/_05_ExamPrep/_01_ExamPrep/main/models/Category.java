8e0e0792e9c0373fcfd3d3c33430870a85775727
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-16 05:28:29 +0200

    01

--- 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/models/Category.java
+++ 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/models/Category.java
@@ -2,9 +2,7 @@ package models;
 
 public class Category {
     private String id;
-
     private String name;
-
     private String description;
 
     public Category(String id, String name, String description) {
