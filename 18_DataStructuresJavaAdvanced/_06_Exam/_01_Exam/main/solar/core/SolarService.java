102fbae57616c5bfc46f0a5842fe090acbe4355a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-17 01:14:30 +0200

    01

--- 18_DataStructuresJavaAdvanced/_06_Exam/_01_Exam/main/solar/core/SolarService.java
+++ 18_DataStructuresJavaAdvanced/_06_Exam/_01_Exam/main/solar/core/SolarService.java
@@ -6,22 +6,13 @@ import java.util.Collection;
 
 public interface SolarService {
     void addInverter(Inverter inverter);
-
     void addArray(Inverter inverter, String arrayId);
-
     void addPanel(Inverter inverter, String arrayId, PVModule pvModule);
-
     boolean containsInverter(String id);
-
     boolean isPanelConnected(PVModule pvModule);
-
     Inverter getInverterByPanel(PVModule pvModule);
-
     void replaceModule(PVModule oldModule, PVModule newModule);
-
     Collection<Inverter> getByProductionCapacity();
-
     Collection<Inverter> getByNumberOfPVModulesConnected();
-
     Collection<PVModule> getByWattProduction();
 }
