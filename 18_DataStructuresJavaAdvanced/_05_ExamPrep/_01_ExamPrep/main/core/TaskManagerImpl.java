1542373fea641637d5e31a5483b6189389a41b57
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-16 05:27:12 +0200

    01

---
+++ 18_DataStructuresJavaAdvanced/_05_ExamPrep/_01_ExamPrep/main/core/TaskManagerImpl.java
@@ -0,0 +1,56 @@
+package core;
+
+import models.Task;
+
+public class TaskManagerImpl implements TaskManager {
+
+    @Override
+    public void addTask(Task task) {
+
+    }
+
+    @Override
+    public boolean contains(Task task) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public Task getTask(String taskId) {
+        return null;
+    }
+
+    @Override
+    public void deleteTask(String taskId) {
+
+    }
+
+    @Override
+    public Task executeTask() {
+        return null;
+    }
+
+    @Override
+    public void rescheduleTask(String taskId) {
+
+    }
+
+    @Override
+    public Iterable<Task> getDomainTasks(String domain) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Task> getTasksInEETRange(int lowerBound, int upperBound) {
+        return null;
+    }
+
+    @Override
+    public Iterable<Task> getAllTasksOrderedByEETThenByName() {
+        return null;
+    }
+}
