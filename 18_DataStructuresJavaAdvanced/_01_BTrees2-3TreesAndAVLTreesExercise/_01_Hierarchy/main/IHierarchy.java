6b1ae58f2d548b916b4df9f7e8580569bbbc8c70
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-18 05:28:24 +0200

    01

--- 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/main/IHierarchy.java
+++ 18_DataStructuresJavaAdvanced/_01_BTrees2-3TreesAndAVLTreesExercise/_01_Hierarchy/main/IHierarchy.java
@@ -1,15 +1,9 @@
 public interface IHierarchy<T> extends Iterable<T> {
     int getCount();
-
     void add(T element, T child);
-
     void remove(T element);
-
     Iterable<T> getChildren(T element);
-    
     T getParent(T element);
-
     boolean contains(T element);
-
     Iterable<T> getCommonElements(IHierarchy<T> other);
 }
