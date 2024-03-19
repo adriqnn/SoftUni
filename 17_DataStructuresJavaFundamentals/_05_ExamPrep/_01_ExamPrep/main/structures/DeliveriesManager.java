4da6f9b4064920804fab75b33834239b4e41401b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-19 21:58:41 +0200

    01

--- 17_DataStructuresJavaFundamentals/_05_ExamPrep/_01_ExamPrep/main/structures/DeliveriesManager.java
+++ 17_DataStructuresJavaFundamentals/_05_ExamPrep/_01_ExamPrep/main/structures/DeliveriesManager.java
@@ -5,22 +5,13 @@ import entities.Package;
 
 public interface DeliveriesManager {
     void addDeliverer(Deliverer deliverer);
-
     void addPackage(Package _package);
-
     boolean contains(Deliverer deliverer);
-
     boolean contains(Package _package);
-
     Iterable<Deliverer> getDeliverers();
-
     Iterable<Package> getPackages();
-
     void assignPackage(Deliverer deliverer, Package _package) throws IllegalArgumentException;
-
     Iterable<Package> getUnassignedPackages();
-
     Iterable<Package> getPackagesOrderedByWeightThenByReceiver();
-
     Iterable<Deliverer> getDeliverersOrderedByCountOfPackagesThenByName();
 }
