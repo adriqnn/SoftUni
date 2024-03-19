a1e64b046b29210bde58eed3da9b483943eddf03
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-19 21:47:13 +0200

    04

--- 17_DataStructuresJavaFundamentals/_02_LinearDataStructuresExercise/_04_ReversedList/main/interfaces/AbstractReversedList.java
+++ 17_DataStructuresJavaFundamentals/_02_LinearDataStructuresExercise/_04_ReversedList/main/interfaces/AbstractReversedList.java
@@ -1,14 +1,9 @@
 package interfaces;
 
 public interface AbstractReversedList<E> extends Iterable<E> {
-
     void add(E element);
-
     E get(int index);
-
     E removeAt(int index);
-
     int size();
-
     int capacity();
 }
