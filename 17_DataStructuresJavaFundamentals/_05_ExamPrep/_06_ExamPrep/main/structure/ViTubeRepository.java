229eca19f8cc5f320ddfcecaf15d8e1bc39faaa0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-20 06:22:28 +0200

    06

--- 17_DataStructuresJavaFundamentals/_05_ExamPrep/_06_ExamPrep/main/structure/ViTubeRepository.java
+++ 17_DataStructuresJavaFundamentals/_05_ExamPrep/_06_ExamPrep/main/structure/ViTubeRepository.java
@@ -5,24 +5,14 @@ import entities.Video;
 
 public interface ViTubeRepository {
     void registerUser(User user);
-
     void postVideo(Video video);
-
     boolean contains(User user);
-
     boolean contains(Video video);
-
     Iterable<Video> getVideos();
-
     void watchVideo(User user, Video video) throws IllegalArgumentException;
-
     void likeVideo(User user, Video video) throws IllegalArgumentException;
-
     void dislikeVideo(User user, Video video) throws IllegalArgumentException;
-
     Iterable<User> getPassiveUsers();
-
     Iterable<Video> getVideosOrderedByViewsThenByLikesThenByDislikes();
-
     Iterable<User> getUsersByActivityThenByName();
 }
