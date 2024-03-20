3b6478ac727a0b1b95a0e4c1e58a5ca5eafc4261
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-20 06:22:12 +0200

    06

--- 17_DataStructuresJavaFundamentals/_05_ExamPrep/_06_ExamPrep/main/structure/MovieDatabase.java
+++ 17_DataStructuresJavaFundamentals/_05_ExamPrep/_06_ExamPrep/main/structure/MovieDatabase.java
@@ -5,20 +5,12 @@ import entities.Movie;
 
 public interface MovieDatabase {
     void addActor(Actor actor);
-
     void addMovie(Actor actor, Movie movie) throws IllegalArgumentException;
-
     boolean contains(Actor actor);
-
     boolean contains(Movie movie);
-
     Iterable<Movie> getAllMovies();
-
     Iterable<Actor> getNewbieActors();
-
     Iterable<Movie> getMoviesOrderedByBudgetThenByRating();
-
     Iterable<Actor> getActorsOrderedByMaxMovieBudgetThenByMoviesCount();
-
     Iterable<Movie> getMoviesInRangeOfBudget(double lower, double upper);
 }
