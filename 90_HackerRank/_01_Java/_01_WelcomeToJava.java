72efc1b4906f909944d22e12a47c00df0e36f914
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-21 23:11:25 +0200

    01

---
+++ 90_HackerRank/_01_Java/_01_WelcomeToJava.java
@@ -0,0 +1,9 @@
+package _05_HackerRank._01_Java;
+
+public class _01_WelcomeToJava {
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
