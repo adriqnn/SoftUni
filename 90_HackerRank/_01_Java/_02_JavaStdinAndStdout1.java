0dbcef0ee523256558261e2687311732d841679a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-21 23:11:30 +0200

    02

---
+++ 90_HackerRank/_01_Java/_02_JavaStdinAndStdout1.java
@@ -0,0 +1,17 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Scanner;
+
+public class _02_JavaStdinAndStdout1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n1 = Integer.parseInt(scan.nextLine());
+        int n2 = Integer.parseInt(scan.nextLine());
+        int n3 = Integer.parseInt(scan.nextLine());
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+    }
+}
