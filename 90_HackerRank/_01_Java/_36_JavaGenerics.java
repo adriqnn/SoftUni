b3d61f2ad9c0c484876f94546dfb5e7060295fe1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:17:02 +0200

    36

--- 90_HackerRank/_01_Java/_36_JavaGenerics.java
+++ 90_HackerRank/_01_Java/_36_JavaGenerics.java
@@ -5,10 +5,13 @@ import java.lang.reflect.Method;
 public class _36_JavaGenerics {
     public static void main( String args[] ) {
         Printer myPrinter = new Printer();
+        
         Integer[] intArray = { 1, 2, 3 };
         String[] stringArray = {"Hello", "World"};
+        
         myPrinter.printArray(intArray);
         myPrinter.printArray(stringArray);
+        
         int count = 0;
 
         for (Method method : Printer.class.getDeclaredMethods()) {
