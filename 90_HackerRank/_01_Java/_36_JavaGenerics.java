45689078f17c8a613a6d0232539d524f58db5401
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:15:48 +0200

    36

---
+++ 90_HackerRank/_01_Java/_36_JavaGenerics.java
@@ -0,0 +1,33 @@
+package _05_HackerRank._01_Java;
+
+import java.lang.reflect.Method;
+
+public class _36_JavaGenerics {
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+    }
+}
+
+class Printer{
+    public <T> void printArray(T[] array){
+        //Arrays.stream(array).forEach(System.out::println);
+
+        for (T element : array){
+            System.out.println(element);
+        }
+    }
+}
