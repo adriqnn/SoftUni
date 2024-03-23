601307e02f9bf3856a41f4266c07824a66c52fd2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:16:13 +0200

    39

---
+++ 90_HackerRank/_01_Java/_39_JavaDeque.java
@@ -0,0 +1,34 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class _39_JavaDeque {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] line = scan.nextLine().split("\\s+");
+
+        int n = Integer.parseInt(line[0]);
+        int m = Integer.parseInt(line[1]);
+
+        int[] numbers = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int unique = 0;
+        for (int i = 0; i <= n-m; i++) {
+            HashSet<Integer> uniques = new HashSet<>();
+            for (int j = i; j < i+m; j++) {
+                uniques.add(numbers[j]);
+            }
+
+            if(uniques.size() > unique) {
+                unique = uniques.size();
+            }
+        }
+
+        System.out.println(unique);
+
+        scan.close();
+    }
+}
