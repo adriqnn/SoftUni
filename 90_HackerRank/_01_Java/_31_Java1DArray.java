0b39993e7a56b6dec73ab7b8854bd700bb782ee6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:09:02 +0200

    31

---
+++ 90_HackerRank/_01_Java/_31_Java1DArray.java
@@ -0,0 +1,35 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Scanner;
+
+public class _31_Java1DArray {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int q = scan.nextInt();
+        while (q-- > 0) {
+            int n = scan.nextInt();
+            int leap = scan.nextInt();
+
+            int[] game = new int[n];
+            for (int i = 0; i < n; i++) {
+                game[i] = scan.nextInt();
+            }
+
+            System.out.println( (canWin(leap, game, 0)) ? "YES" : "NO" );
+        }
+
+        scan.close();
+    }
+
+    public static boolean canWin(int leap, int[] game, int i) {
+        if(i > game.length-1){
+            return true;}
+        if(i < 0 || game[i] == 1){
+            return false;
+        }
+
+        game[i] = 1;
+        return canWin(leap, game, i+leap) || canWin(leap, game, i+1) || canWin(leap, game, i-1);
+    }
+}
