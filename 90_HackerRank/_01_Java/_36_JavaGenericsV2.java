fac1bd73dfb1a9b60389ae0d1aa39e5f19a631df
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:17:14 +0200

    36

--- 90_HackerRank/_01_Java/_36_JavaGenericsV2.java
+++ 90_HackerRank/_01_Java/_36_JavaGenericsV2.java
@@ -8,6 +8,7 @@ public class _36_JavaGenericsV2 {
     public static void main( String args[] ) {
         Integer[] intArray = { 1, 2, 3 };
         String[] stringArray = {"Hello", "World"};
+        
         printArray(Arrays.stream(intArray).collect(Collectors.toList()));
         printArray(Arrays.stream(stringArray).collect(Collectors.toList()));
     }
