77959ead2f2ec20eaa00ae11549e56c4e47b6519
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-23 07:20:24 +0200

    39

---
+++ 90_HackerRank/_01_Java/_39_JavaDequeV3.java
@@ -0,0 +1,30 @@
+package _05_HackerRank._01_Java;
+
+import java.util.*;
+
+public class _39_JavaDequeV3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Deque<Integer> deque = new ArrayDeque<>();
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        Set<Integer> set = new HashSet<>();
+
+        int max_count = 0;
+        for (int i = 0; i < n; i++) {
+            int num = scan.nextInt();
+            deque.add(num);
+            set.add(num);
+            if(deque.size() == m){
+                if(max_count < set.size()){
+                    max_count = set.size();
+                }
+                int remove_val = deque.pollFirst();
+                if(!deque.contains(remove_val)){
+                    set.remove((remove_val));
+                }
+            }
+        }
+        System.out.println(max_count);
+    }
+}
