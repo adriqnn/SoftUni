e67d7bde68539a5b51e63fe2c0efee8d0760f983
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:13:33 +0200

    39

--- 90_HackerRank/_01_Java/_39_JavaDequeV4.java
+++ 90_HackerRank/_01_Java/_39_JavaDequeV4.java
@@ -5,16 +5,19 @@ import java.util.*;
 public class _39_JavaDequeV4 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        
         Deque<Integer> deque = new ArrayDeque<>();
+        Set<Integer> set = new HashSet<>();
+        
         int n = scan.nextInt();
         int m = scan.nextInt();
-        Set<Integer> set = new HashSet<>();
 
         if(n == m){
             for (int i = 0; i < n; i++) {
                 int num = scan.nextInt();
                 set.add(num);
             }
+            
             System.out.println(set.size());
             return;
         }
@@ -30,14 +33,19 @@ public class _39_JavaDequeV4 {
                 if(unique < set.size()){
                     unique = set.size();
                 }
+                
                 int remove = deque.pollFirst();
                 if(!deque.contains(remove)){
                     set.remove(remove);
                 }
+                
                 deque.add(num);
                 set.add(num);
             }
         }
+        
         System.out.println(unique);
+
+        scan.close();
     }
 }
