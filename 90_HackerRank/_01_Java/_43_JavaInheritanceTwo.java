fa18a74fd1e30af648093137b18da77b41a160d8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:26:15 +0200

    43

--- 90_HackerRank/_01_Java/_43_JavaInheritanceTwo.java
+++ 90_HackerRank/_01_Java/_43_JavaInheritanceTwo.java
@@ -5,7 +5,6 @@ public class _43_JavaInheritanceTwo {
         Adder a = new Adder();
 
         System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
-
         System.out.print(a.add(10,32) + " " + a.add(10,3) + " " + a.add(10,10) + "\n");
     }
 }
