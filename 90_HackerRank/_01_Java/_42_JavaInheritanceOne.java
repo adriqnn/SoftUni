8031daa575d973bd39c223d063b1a3b6a0427bcf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:22:20 +0200

    42

---
+++ 90_HackerRank/_01_Java/_42_JavaInheritanceOne.java
@@ -0,0 +1,25 @@
+package _05_HackerRank._01_Java;
+
+public class _42_JavaInheritanceOne {
+    public static void main(String[] args) {
+        Bird bird = new Bird();
+        bird.walk();
+        bird.fly();
+        bird.sing();
+    }
+}
+
+class Animal{
+    void walk(){
+        System.out.println("I am walking");
+    }
+}
+
+class Bird extends Animal {
+    void fly() {
+        System.out.println("I am flying");
+    }
+    void sing(){
+        System.out.println("I am singing");
+    }
+}
