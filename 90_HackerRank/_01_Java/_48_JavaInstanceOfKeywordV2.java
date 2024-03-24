92e2b3a154685dbddb319dc620fa1a9d5d09023c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:33:09 +0200

    48

--- 90_HackerRank/_01_Java/_48_JavaInstanceOfKeywordV2.java
+++ 90_HackerRank/_01_Java/_48_JavaInstanceOfKeywordV2.java
@@ -5,33 +5,58 @@ import java.util.*;
 public class _48_JavaInstanceOfKeywordV2 {
     static String count(ArrayList mylist){
         int a = 0,b = 0,c = 0;
+        
         for(int i = 0; i < mylist.size(); i++){
             Object element=mylist.get(i);
-            if(element instanceof ST)
+            
+            if(element instanceof ST){
                 a++;
-            if(element instanceof Rockstar)
+            }
+            
+            if(element instanceof Rockstar){
                 b++;
-            if(element instanceof Hacker)
+            }
+            
+            if(element instanceof Hacker){
                 c++;
+            }
         }
+        
         String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
+        
         return ret;
     }
 
     public static void main(String []args){
-        ArrayList mylist = new ArrayList();
         Scanner sc = new Scanner(System.in);
+        
+        ArrayList mylist = new ArrayList();
         int t = sc.nextInt();
+        
         for(int i=0; i<t; i++){
             String s=sc.next();
-            if(s.equals("Student"))mylist.add(new ST());
-            if(s.equals("Rockstar"))mylist.add(new Rockstar());
-            if(s.equals("Hacker"))mylist.add(new Hacker());
+            
+            if(s.equals("Student")){
+                mylist.add(new ST());
+            }
+            
+            if(s.equals("Rockstar")){
+                mylist.add(new Rockstar());
+            }
+            
+            if(s.equals("Hacker")){
+                mylist.add(new Hacker());
+            }
         }
+        
         System.out.println(count(mylist));
+
+        scan.close();
     }
 }
 
 class ST{}
+
 class Rockstar{}
+
 class Hacker{}
