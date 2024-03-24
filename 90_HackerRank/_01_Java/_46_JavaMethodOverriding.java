af04526d556b4060497ac60f2a9e951da8646431
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:28:24 +0200

    46

--- 90_HackerRank/_01_Java/_46_JavaMethodOverriding.java
+++ 90_HackerRank/_01_Java/_46_JavaMethodOverriding.java
@@ -4,8 +4,10 @@ public class _46_JavaMethodOverriding {
     public static void main(String []args){
         Sports c1 = new Sports();
         Soccer c2 = new Soccer();
+        
         System.out.println(c1.getName());
         c1.getNumberOfTeamMembers();
+        
         System.out.println(c2.getName());
         c2.getNumberOfTeamMembers();
     }
@@ -15,6 +17,7 @@ class Sports{
     String getName(){
         return "Generic Sports";
     }
+    
     void getNumberOfTeamMembers(){
         System.out.println( "Each team has n players in " + getName() );
     }
