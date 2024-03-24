260d84b94d84ad5ff1898d0bc3e2ae863c75057d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-24 09:31:28 +0200

    48

--- 90_HackerRank/_01_Java/_48_JavaInstanceOfKeyword.java
+++ 90_HackerRank/_01_Java/_48_JavaInstanceOfKeyword.java
@@ -6,30 +6,51 @@ import java.util.Scanner;
 public class _48_JavaInstanceOfKeyword {
     static String count(ArrayList list){
         int a = 0, b = 0, c = 0;
+        
         for(int i = 0; i < list.size(); i++){
             Object element = list.get(i);
-            if(element instanceof S)
-            a++;
-            if(element instanceof R)
-            b++;
-            if(element instanceof H)
-            c++;
+            if(element instanceof S){
+                a++;
+            }
+            
+            if(element instanceof R){
+                b++;
+            }
+            
+            if(element instanceof H){
+                c++;
+            }
         }
+        
         String ret = a + " " + b + " " + c;
+        
         return ret;
     }
 
     public static void main(String []args){
-        ArrayList list = new ArrayList();
         Scanner scan = new Scanner(System.in);
+        
+        ArrayList list = new ArrayList();
         int t = scan.nextInt();
         for(int i = 0; i < t; i++){
             String s = scan.next();
-            if(s.equals("Student"))list.add(new S());
-            if(s.equals("Rockstar"))list.add(new R());
-            if(s.equals("Hacker"))list.add(new H());
+            
+            if(s.equals("Student")){
+                list.add(new S());
+            }
+            
+            if(s.equals("Rockstar")){
+                list.add(new R());
+            }
+            
+            if(s.equals("Hacker")){
+                list.add(new H());
+            }
         }
+        
         System.out.println(count(list));
+
+        scan.close();
     }
 }
 
