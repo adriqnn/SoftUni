ffe0c09e1e762ba51654c733a711b1af261a7156
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-25 22:32:43 +0200

    48

--- 90_HackerRank/_01_Java/_48_JavaInstanceOfKeywordV2.java
+++ 90_HackerRank/_01_Java/_48_JavaInstanceOfKeywordV2.java
@@ -4,37 +4,37 @@ import java.util.*;
 
 public class _48_JavaInstanceOfKeywordV2 {
     static String count(ArrayList mylist){
-        int a = 0,b = 0,c = 0;
-        
+        int a = 0, b = 0, c = 0;
+
         for(int i = 0; i < mylist.size(); i++){
             Object element=mylist.get(i);
-            
+
             if(element instanceof ST){
                 a++;
             }
-            
+
             if(element instanceof Rockstar){
                 b++;
             }
-            
+
             if(element instanceof Hacker){
                 c++;
             }
         }
-        
-        String ret = Integer.toString(a)+" "+ Integer.toString(b)+" "+ Integer.toString(c);
-        
+
+        String ret = Integer.toString(a) + " " + Integer.toString(b) + " " + Integer.toString(c);
+
         return ret;
     }
 
     public static void main(String []args){
-        Scanner sc = new Scanner(System.in);
-        
+        Scanner scan = new Scanner(System.in);
+
         ArrayList mylist = new ArrayList();
-        int t = sc.nextInt();
-        
+        int t = scan.nextInt();
+
         for(int i=0; i<t; i++){
-            String s=sc.next();
+            String s = scan.next();
             
             if(s.equals("Student")){
                 mylist.add(new ST());
@@ -48,7 +48,7 @@ public class _48_JavaInstanceOfKeywordV2 {
                 mylist.add(new Hacker());
             }
         }
-        
+
         System.out.println(count(mylist));
 
         scan.close();
