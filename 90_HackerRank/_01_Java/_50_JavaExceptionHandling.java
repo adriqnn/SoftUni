7aa470c5a2f4e5a2fc960a752cf504d2fb2c23d7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-25 07:00:53 +0200

    50

---
+++ 90_HackerRank/_01_Java/_50_JavaExceptionHandling.java
@@ -0,0 +1,19 @@
+package _05_HackerRank._01_Java;
+
+import java.util.Scanner;
+
+public class _50_JavaExceptionHandling {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        try{
+           int n = Integer.parseInt(scan.nextLine());
+           int m = Integer.parseInt(scan.nextLine());
+            System.out.println(n/m);
+        }catch (NumberFormatException e){
+            System.out.println("java.util.InputMismatchException");
+        }catch (Exception f){
+            System.out.println(f);
+        }
+    }
+}
