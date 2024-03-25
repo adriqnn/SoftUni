52a9642e4b6a4ff7111065aa8be316d62ab9cff1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-25 07:01:18 +0200

    54

---
+++ 90_HackerRank/_01_Java/_54_CanYouAccess.java
@@ -0,0 +1,8 @@
+package _05_HackerRank._01_Java;
+
+public class _54_CanYouAccess {
+    public static void main(String[] args) {
+//        o = (Inner.class.newInstance()).new Private();
+//        System.out.println(num + " is " + ((Inner.Private)o).powerof2(num));
+    }
+}
