990bfa9ad27e61d023b19a4060707ea3e00e9e2b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-25 07:01:45 +0200

    59

---
+++ 90_HackerRank/_01_Java/_59_JavaAnnotations.java
@@ -0,0 +1,11 @@
+package _05_HackerRank._01_Java;
+
+public class _59_JavaAnnotations {
+    public static void main(String[] args) {
+//        int budgetLimit();
+//        @FamilyBudget(userRole = "SENIOR" ,budgetLimit = 100)
+//        @FamilyBudget(userRole = "JUNIOR", budgetLimit = 50)
+//        family.budgetLimit();
+//        spend <= budgetLimit
+    }
+}
