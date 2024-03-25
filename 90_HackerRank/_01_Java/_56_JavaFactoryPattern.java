d44a9ae5d0e30a470f7aa9f61f6484a218ea10ac
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-25 07:01:29 +0200

    56

---
+++ 90_HackerRank/_01_Java/_56_JavaFactoryPattern.java
@@ -0,0 +1,12 @@
+package _05_HackerRank._01_Java;
+
+public class _56_JavaFactoryPattern {
+    public static void main(String[] args) {
+//        String orderLower = order.toLowerCase();
+//        if(orderLower.equals("cake")){
+//            return new Cake();
+//        }else {
+//            return new Pizza();
+//        }
+    }
+}
