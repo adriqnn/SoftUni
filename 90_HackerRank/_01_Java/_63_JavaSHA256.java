91fa74682ca37325e0f3da455d868f98aa2bb3c0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-26 11:24:21 +0200

    63

--- 90_HackerRank/_01_Java/_63_JavaSHA256.java
+++ 90_HackerRank/_01_Java/_63_JavaSHA256.java
@@ -8,26 +8,29 @@ import java.util.Scanner;
 public class _63_JavaSHA256 {
     public static void main(String[] args) throws NoSuchAlgorithmException {
         Scanner scan = new Scanner(System.in);
+        
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
-
         String word = scan.nextLine();
 
         byte[] encoded = digest.digest(word.getBytes(StandardCharsets.UTF_8));
         System.out.println(bytesToHex(encoded));
+        
+        scan.close();
     }
 
 
     private static String bytesToHex(byte[] hash){
         StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < hash.length; i++) {
             String hex = Integer.toHexString(0xff & hash[i]);
             if(hex.length() == 1){
                 sb.append('0');
             }
+            
             sb.append(hex);
         }
+        
         return sb.toString();
     }
 }
-
-
