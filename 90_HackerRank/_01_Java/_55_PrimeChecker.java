82743fc86c32a339df6c20af39dfdea6d0e789d0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-26 08:34:22 +0200

    55

--- 90_HackerRank/_01_Java/_55_PrimeChecker.java
+++ 90_HackerRank/_01_Java/_55_PrimeChecker.java
@@ -7,9 +7,10 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class _55_PrimeChecker {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         try{
-            BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
             int n1 = Integer.parseInt(br.readLine());
             int n2 = Integer.parseInt(br.readLine());
             int n3 = Integer.parseInt(br.readLine());
@@ -18,19 +19,20 @@ public class _55_PrimeChecker {
 
             Prime ob = new Prime();
             ob.checkPrime(n1);
-            ob.checkPrime(n1,n2);
-            ob.checkPrime(n1,n2,n3);
-            ob.checkPrime(n1,n2,n3,n4,n5);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
 
-            Method[] methods=Prime.class.getDeclaredMethods();
-            Set<String> set=new HashSet<>();
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
 
             boolean overload=false;
-            for(int i=0;i<methods.length;i++) {
+            for(int i = 0; i < methods.length; i++) {
                 if(set.contains(methods[i].getName())){
                     overload=true;
                     break;
                 }
+                
                 set.add(methods[i].getName());
             }
 
@@ -40,6 +42,8 @@ public class _55_PrimeChecker {
         } catch(Exception e){
             System.out.println(e);
         }
+        
+        br.close();
     }
 }
 
@@ -50,6 +54,7 @@ class Prime{
                 System.out.printf("%d ", of);
             }
         }
+        
         System.out.println();
     }
 
@@ -57,11 +62,13 @@ class Prime{
         if (n <= 1) {
             return false;
         }
+        
         for (int i = 2; i < n; i++) {
             if (n % i == 0){
                 return false;
             }
         }
+        
         return true;
     }
 }
