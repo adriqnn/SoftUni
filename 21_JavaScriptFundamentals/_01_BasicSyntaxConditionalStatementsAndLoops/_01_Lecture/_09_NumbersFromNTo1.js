1cee9168ff844a63fe2d6e314b416edf12ffd41d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:44 +0300

    09

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_09_NumbersFromNTo1.js
@@ -0,0 +1,17 @@
+function numbers_from_n_to_1V1(n) {
+    for (let i = n; i >= 1; i--) {
+        console.log(i);
+    }
+}
+
+const numbers_from_n_to_1V2 = function(n){
+    for (let i = n; i >= 1; i--) {
+        console.log(i);
+    }
+}
+
+const  numbers_from_n_to_1V3 = (n) => {
+    for (let i = n; i >= 1; i--) {
+        console.log(i);
+    }
+}
