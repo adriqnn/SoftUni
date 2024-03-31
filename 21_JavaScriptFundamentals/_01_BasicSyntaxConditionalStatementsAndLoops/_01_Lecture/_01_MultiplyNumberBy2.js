78cf85f8c17dff0ad2ce043a34be59ab72445439
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:04 +0300

    01

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_01_MultiplyNumberBy2.js
@@ -0,0 +1,23 @@
+function multiply_numbers_by_2V1(num){
+    console.log(num * 2);
+}
+
+const multiply_numbers_by_2V2 = function(num) {
+    console.log(num * 2);
+}
+
+const multiply_numbers_by_2V3 = (num) => {
+    console.log(num * 2);
+}
+
+// multiply_numbers_by_2V1(1);
+// multiply_numbers_by_2V1(2);
+// multiply_numbers_by_2V1(3);
+//
+// multiply_numbers_by_2V2(4);
+// multiply_numbers_by_2V2(5);
+// multiply_numbers_by_2V2(6);
+//
+// multiply_numbers_by_2V3(7);
+// multiply_numbers_by_2V3(8);
+// multiply_numbers_by_2V3(9);
