db0416354b79ba00a6136f6f0aaf83ccf3551443
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:39 +0300

    08

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_08_DivisibleBy3.js
@@ -0,0 +1,17 @@
+function divisible_by_3V1 (){
+    for (let i = 3; i <= 99; i += 3) {
+        console.log(i);
+    }
+}
+
+const divisible_by_3V2 = function(){
+    for (let i = 3; i <= 99; i += 3) {
+        console.log(i);
+    }
+}
+
+const divisible_by_3V3 = () => {
+    for (let i = 3; i <= 99; i += 3) {
+        console.log(i);
+    }
+}
