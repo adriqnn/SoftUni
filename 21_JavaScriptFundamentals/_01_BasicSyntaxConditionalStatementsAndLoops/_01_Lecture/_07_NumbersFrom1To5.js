f69720344c6ed709c61ade1661305b232672d8e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:35 +0300

    07

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_07_NumbersFrom1To5.js
@@ -0,0 +1,17 @@
+function numbers_from_1_to_5V1(){
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+const numbers_from_1_to_5V2 = function(){
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
+
+const numbers_from_1_to_5V3 = () => {
+    for (let i = 1; i <= 5; i++) {
+        console.log(i);
+    }
+}
