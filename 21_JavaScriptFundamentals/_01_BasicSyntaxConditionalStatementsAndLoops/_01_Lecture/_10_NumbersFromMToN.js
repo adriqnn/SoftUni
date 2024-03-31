b9c7703dac8bb78b78551b205a900eb46d139fea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:48 +0300

    10

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_10_NumbersFromMToN.js
@@ -0,0 +1,17 @@
+function numbers_from_m_to_nV1(m, n){
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
+
+const numbers_from_m_to_nV2 = function(m, n){
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
+
+const numbers_from_m_to_nV3 = (m, n) => {
+    for (let i = m; i >= n; i--) {
+        console.log(i);
+    }
+}
