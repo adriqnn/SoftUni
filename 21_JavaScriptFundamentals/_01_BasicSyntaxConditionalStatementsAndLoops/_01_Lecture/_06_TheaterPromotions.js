574b4439914f8c91f5366a50f247b35b0bca1c7f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:30 +0300

    06

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_06_TheaterPromotions.js
@@ -0,0 +1,101 @@
+function theater_promotionV1(day, age){
+    let msg = "";
+
+    if(day === "Weekday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "12$";
+        }else if(age >= 19 && age <= 64){
+            msg = "18$";
+        }else{
+            msg = "12$";
+        }
+    }else if(day === "Weekend" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "15$";
+        }else if(age >= 19 && age <= 64){
+            msg = "20$";
+        }else{
+            msg = "15$";
+        }
+    }else if(day === "Holiday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "5$";
+        }else if(age >= 19 && age <= 64){
+            msg = "12$";
+        }else{
+            msg = "10$";
+        }
+    }else{
+        msg = "Error!";
+    }
+
+    console.log(msg);
+}
+
+const theater_promotionV2 = function (day, age){
+    let msg = "";
+
+    if(day === "Weekday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "12$";
+        }else if(age >= 19 && age <= 64){
+            msg = "18$";
+        }else{
+            msg = "12$";
+        }
+    }else if(day === "Weekend" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "15$";
+        }else if(age >= 19 && age <= 64){
+            msg = "20$";
+        }else{
+            msg = "15$";
+        }
+    }else if(day === "Holiday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "5$";
+        }else if(age >= 19 && age <= 64){
+            msg = "12$";
+        }else{
+            msg = "10$";
+        }
+    }else{
+        msg = "Error!";
+    }
+
+    console.log(msg);
+}
+
+const theater_promotionV3 = (day, age) => {
+    let msg = "";
+
+    if(day === "Weekday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "12$";
+        }else if(age >= 19 && age <= 64){
+            msg = "18$";
+        }else{
+            msg = "12$";
+        }
+    }else if(day === "Weekend" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "15$";
+        }else if(age >= 19 && age <= 64){
+            msg = "20$";
+        }else{
+            msg = "15$";
+        }
+    }else if(day === "Holiday" && age >= 0 && age <= 122){
+        if(age <= 18){
+            msg = "5$";
+        }else if(age >= 19 && age <= 64){
+            msg = "12$";
+        }else{
+            msg = "10$";
+        }
+    }else{
+        msg = "Error!";
+    }
+
+    console.log(msg);
+}
