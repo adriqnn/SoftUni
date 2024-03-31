009455451d65a226ab3a4071ac07e868dc18d5f7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-03-31 23:04:16 +0300

    03

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_03_ExcellentGrade.js
@@ -0,0 +1,11 @@
+function excellent_gradeV1(num){
+    num >= 5.50 ? console.log("Excellent") : console.log("Not excellent");
+}
+
+const excellent_gradeV2 = function(num){
+    num >= 5.50 ? console.log("Excellent") : console.log("Not excellent");
+}
+
+const excellent_gradeV3 = (num) => {
+    num >= 5.50 ? console.log("Excellent") : console.log("Not excellent");
+}
