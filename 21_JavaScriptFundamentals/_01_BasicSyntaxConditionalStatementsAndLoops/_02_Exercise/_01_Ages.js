7154f4aedd12a22e3db070a5bf64348ef5aa0c01
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-01 22:15:16 +0300

    01

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_01_Ages.js
@@ -0,0 +1,56 @@
+function agesV1(age){
+    let status;
+    if(age >= 0 && age <= 2){
+        status = "baby";
+    }else if(age >= 3 && age <= 13){
+        status = "child";
+    }else if(age >= 14 && age <= 19){
+        status = "teenager";
+    }else if(age >= 20 && age <= 65){
+        status = "adult";
+    }else if(age >= 66){
+        status = "elder";
+    }else{
+        status = "out of bounds";
+    }
+
+    console.log(status);
+}
+
+const agesV2 = function(age){
+    let status;
+    if(age >= 0 && age <= 2){
+        status = "baby";
+    }else if(age >= 3 && age <= 13){
+        status = "child";
+    }else if(age >= 14 && age <= 19){
+        status = "teenager";
+    }else if(age >= 20 && age <= 65){
+        status = "adult";
+    }else if(age >= 66){
+        status = "elder";
+    }else{
+        status = "out of bounds";
+    }
+
+    console.log(status);
+}
+
+const agesV3 = (age) => {
+    let status;
+    if(age >= 0 && age <= 2){
+        status = "baby";
+    }else if(age >= 3 && age <= 13){
+        status = "child";
+    }else if(age >= 14 && age <= 19){
+        status = "teenager";
+    }else if(age >= 20 && age <= 65){
+        status = "adult";
+    }else if(age >= 66){
+        status = "elder";
+    }else{
+        status = "out of bounds";
+    }
+
+    console.log(status);
+}
