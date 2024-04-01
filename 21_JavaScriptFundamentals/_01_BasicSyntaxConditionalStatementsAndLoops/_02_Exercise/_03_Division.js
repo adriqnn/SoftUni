4dd98a97ba17973b7da0bcd0bb6a0e72dce5b1d4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-01 22:15:24 +0300

    03

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_03_Division.js
@@ -0,0 +1,38 @@
+function divisionV1(num){
+    let division = 0;
+    const divisors = [2, 3, 6, 7, 10];
+
+    divisors.forEach(x => {
+        if(num % x === 0){
+            division = x;
+        }
+    });
+
+    return division !== 0 ? `The number is divisible by ${division}` : "Not divisible";
+}
+
+const divisionV2 = function(num){
+    let division = 0;
+    const divisors = [2, 3, 6, 7, 10];
+
+    divisors.forEach(x => {
+        if(num % x === 0){
+            division = x;
+        }
+    });
+
+    return division !== 0 ? `The number is divisible by ${division}` : "Not divisible";
+}
+
+const divisionV3 = () => {
+    let division = 0;
+    const divisors = [2, 3, 6, 7, 10];
+
+    divisors.forEach(x => {
+        if(num % x === 0){
+            division = x;
+        }
+    });
+
+    return division !== 0 ? `The number is divisible by ${division}` : "Not divisible";
+}
