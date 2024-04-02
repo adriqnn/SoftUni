403f8c18ec3c7c00bb2118eca5ad0646370731d7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-02 23:01:32 +0300

    04

---
+++ 21_JavaScriptFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseString.js
@@ -0,0 +1,11 @@
+function reverse_stringV1(string){
+    return string.split("").reverse().join("");
+}
+
+const reverse_stringV2 = function(string){
+    return string.split("").reverse().join("");
+}
+
+const reverse_stringV3 = (string) => {
+    return string.split("").reverse().join("");
+}
