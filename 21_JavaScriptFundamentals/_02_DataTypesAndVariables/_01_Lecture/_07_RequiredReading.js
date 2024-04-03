a8a846e7ffe3ca29c65e883b62e80f59011a465c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-03 20:39:43 +0300

    07

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_01_Lecture/_07_RequiredReading.js
@@ -0,0 +1,14 @@
+function required_readingV1(book, pages, days){
+    let pagesPerHour = book / pages;
+    console.log(pagesPerHour / days);
+}
+
+const required_readingV2 = function(book, pages, days){
+    let pagesPerHour = book / pages;
+    console.log(pagesPerHour / days);
+}
+
+const required_readingV3 = (book, pages, days) => {
+    let pagesPerHour = book / pages;
+    console.log(pagesPerHour / days);
+}
