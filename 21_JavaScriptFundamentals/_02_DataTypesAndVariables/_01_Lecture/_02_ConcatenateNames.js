046eed176b3a151c0df0816772e3db2156a1c926
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-03 20:38:53 +0300

    02

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_01_Lecture/_02_ConcatenateNames.js
@@ -0,0 +1,11 @@
+function concatenate_namesV1(n1, n2, delimiter){
+    return `${n1}${delimiter}${n2}`;
+}
+
+const concatenate_namesV2 = function(n1, n2, delimiter){
+    return `${n1}${delimiter}${n2}`;
+}
+
+const concatenate_namesV3 = (n1, n2, delimiter) => {
+    return `${n1}${delimiter}${n2}`;
+}
