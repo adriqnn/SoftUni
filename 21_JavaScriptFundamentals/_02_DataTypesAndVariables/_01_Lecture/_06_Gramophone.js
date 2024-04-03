f661f60c7b4aab5280d2b6fcc95212d04f14926d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-03 20:39:37 +0300

    06

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_Gramophone.js
@@ -0,0 +1,20 @@
+function gramophoneV1(band, album, song){
+    let time = (band.length * album.length) * song.length / 2;
+    let rotations = Math.ceil(time / 2.5);
+
+    return `The plate was rotated ${rotations} times.`;
+}
+
+const gramophoneV2 = function (band, album, song){
+    let time = (band.length * album.length) * song.length / 2;
+    let rotations = Math.ceil(time / 2.5);
+
+    return `The plate was rotated ${rotations} times.`;
+}
+
+const gramophoneV3 = (band, album, song) => {
+    let time = (band.length * album.length) * song.length / 2;
+    let rotations = Math.ceil(time / 2.5);
+
+    return `The plate was rotated ${rotations} times.`;
+}
