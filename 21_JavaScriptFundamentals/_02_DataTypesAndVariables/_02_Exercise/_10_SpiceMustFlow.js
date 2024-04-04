45ba49fff9f8670e1f50cca8f01b52e2fa6e4004
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:43:34 +0300

    10

--- 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_SpiceMustFlow.js
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_SpiceMustFlow.js
@@ -1,53 +1,53 @@
 function spice_must_flowV1(number){
-    let spice = 0
-    let days = 0
+    let spice = 0;
+    let days = 0;
 
     function isEnoughSpice() {
-        return spice - 26 > 0
+        return spice - 26 > 0;
     }
 
     while (n >= 100) {
-        spice += n
-        n -= 10
-        days += 1
-        isEnoughSpice() ? (spice -= 26) : (spice = 0)
+        spice += n;
+        n -= 10;
+        days += 1;
+        isEnoughSpice() ? (spice -= 26) : (spice = 0);
     }
 
-    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`
+    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`;
 }
 
 const spice_must_flowV2 = function (){
-    let spice = 0
-    let days = 0
+    let spice = 0;
+    let days = 0;
 
     function isEnoughSpice() {
-        return spice - 26 > 0
+        return spice - 26 > 0;
     }
 
     while (n >= 100) {
-        spice += n
-        n -= 10
-        days += 1
-        isEnoughSpice() ? (spice -= 26) : (spice = 0)
+        spice += n;
+        n -= 10;
+        days += 1;
+        isEnoughSpice() ? (spice -= 26) : (spice = 0);
     }
 
-    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`
+    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`;
 }
 
 const spice_must_flowV3 = () => {
-    let spice = 0
-    let days = 0
+    let spice = 0;
+    let days = 0;
 
     function isEnoughSpice() {
-        return spice - 26 > 0
+        return spice - 26 > 0;
     }
 
     while (n >= 100) {
-        spice += n
-        n -= 10
-        days += 1
-        isEnoughSpice() ? (spice -= 26) : (spice = 0)
+        spice += n;
+        n -= 10;
+        days += 1;
+        isEnoughSpice() ? (spice -= 26) : (spice = 0);
     }
 
-    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`
+    return `${days}\n${isEnoughSpice() ? spice - 26 : 0}`;
 }
