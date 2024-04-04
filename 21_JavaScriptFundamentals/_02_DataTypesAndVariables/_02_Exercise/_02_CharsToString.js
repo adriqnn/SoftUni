6b8c283cbdbb202c5611798e57b3a8420812a36c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:40:47 +0300

    02

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_02_CharsToString.js
@@ -0,0 +1,11 @@
+function chars_to_stringV1(a, b, c){
+    return `${a}${b}${c}`;
+}
+
+const chars_to_stringV2 = function(){
+    return `${a}${b}${c}`;
+}
+
+const chars_to_stringV3 = () => {
+    return `${a}${b}${c}`;
+}
