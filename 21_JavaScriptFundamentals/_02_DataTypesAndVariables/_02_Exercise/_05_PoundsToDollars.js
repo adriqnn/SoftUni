026c804f8988c8ac7ff59a37ece78c0fcc78337f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:41:02 +0300

    05

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_05_PoundsToDollars.js
@@ -0,0 +1,11 @@
+function pounds_to_dollarsV1(pounds){
+    return (pounds * 1.31).toFixed(3);
+}
+
+const pounds_to_dollarsV2 = function(pounds){
+    return (pounds * 1.31).toFixed(3);
+}
+
+const pounds_to_dollarsV3 = (pounds) => {
+    return (pounds * 1.31).toFixed(3);
+}
