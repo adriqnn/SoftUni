43a55853705a8e31c060184b5cbd23df1e84bc0f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:40:51 +0300

    03

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_03_TownInfo.js
@@ -0,0 +1,11 @@
+function town_infoV1(town, population, area){
+    return `Town ${town} has population of ${population} and area ${area} square km.`;
+}
+
+const town_infoV2 = function(town, population, area){
+    return `Town ${town} has population of ${population} and area ${area} square km.`;
+}
+
+const town_infoV3 = (town, population, area) => {
+    return `Town ${town} has population of ${population} and area ${area} square km.`;
+}
