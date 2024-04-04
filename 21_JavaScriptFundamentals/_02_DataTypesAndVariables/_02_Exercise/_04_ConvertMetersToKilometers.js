1ec34018cdc7faea423bcf6f0227fee43236f7ca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:40:57 +0300

    04

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_04_ConvertMetersToKilometers.js
@@ -0,0 +1,11 @@
+function convert_meters_to_kilometersV1(number){
+    return (number / 1000).toFixed(2);
+}
+
+const convert_meters_to_kilometersV2 = function(number){
+    return (number / 1000).toFixed(2);
+}
+
+const convert_meters_to_kilometersV3 = (number) => {
+    return (number / 1000).toFixed(2);
+}
