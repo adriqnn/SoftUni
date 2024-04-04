fdabfa3ba2d356be3f0ff81406dceec4018b04a3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-04 21:41:19 +0300

    08

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_Calculator.js
@@ -0,0 +1,38 @@
+function calculatorV1(...args){
+    const actions = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => x / y,
+        "**": (x, y) => x ** y,
+        "%": (x, y) => x % y,
+    }
+
+    return (actions[args[1]](args[0], args[2])).toFixed(2);
+}
+
+const calculatorV2 = function (...args){
+    const actions = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => x / y,
+        "**": (x, y) => x ** y,
+        "%": (x, y) => x % y,
+    }
+
+    return (actions[args[1]](args[0], args[2])).toFixed(2);
+}
+
+const calculatorV3 = (...args) => {
+    const actions = {
+        "+": (x, y) => x + y,
+        "-": (x, y) => x - y,
+        "*": (x, y) => x * y,
+        "/": (x, y) => x / y,
+        "**": (x, y) => x ** y,
+        "%": (x, y) => x % y,
+    }
+
+    return (actions[args[1]](args[0], args[2])).toFixed(2);
+}
