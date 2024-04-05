11a1a26767a4b46955ac05554c2d8059512925a7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-05 15:31:13 +0300

    01

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_01_DigitsWithWords.js
@@ -0,0 +1,23 @@
+function digits_with_wordsV1(number){
+    let map = {
+        "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+    }
+
+    return map[number];
+}
+
+const digits_with_wordsV2 = function(number){
+    let map = {
+        "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+    }
+
+    return map[number];
+}
+
+const digits_with_wordsV3 = (number) => {
+    let map = {
+        "zero": 0, "one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9
+    }
+
+    return map[number];
+}
