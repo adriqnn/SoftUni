94afd1cbd12eb7737e77c942138f58359923bf74
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-05 03:41:45 +0300

    08

--- 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_Calculator.js
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_Calculator.js
@@ -11,7 +11,7 @@ function calculatorV1(...args){
     return (actions[args[1]](args[0], args[2])).toFixed(2);
 }
 
-const calculatorV2 = function (...args){
+const calculatorV2 = function(...args){
     const actions = {
         "+": (x, y) => x + y,
         "-": (x, y) => x - y,
