6df6c289444cba4cf3eea715824bee9334195312
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-05 15:31:26 +0300

    04

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_04_BiggestOf3Numbers.js
@@ -0,0 +1,11 @@
+function biggest_of_3_numbersV1(a, b, c){
+    return Math.max(a, b, c);
+}
+
+const biggest_of_3_numbersV2 = function(a, b, c){
+    return Math.max(a, b, c);
+}
+
+const biggest_of_3_numbersV3 = (a, b, c) => {
+    return Math.max(a, b, c);
+}
