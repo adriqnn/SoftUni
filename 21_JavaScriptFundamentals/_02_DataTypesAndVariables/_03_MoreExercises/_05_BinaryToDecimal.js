4f7fb3fd307e65267671c2cdc18b6ca0da66d324
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-05 15:31:31 +0300

    05

---
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_05_BinaryToDecimal.js
@@ -0,0 +1,11 @@
+function binary_to_decimalV1(number){
+    return parseInt(number, 2);
+}
+
+const binary_to_decimalV2 = function(number){
+    return parseInt(number, 2);
+}
+
+const binary_to_decimalV3 = (number) => {
+    return parseInt(number, 2);
+}
