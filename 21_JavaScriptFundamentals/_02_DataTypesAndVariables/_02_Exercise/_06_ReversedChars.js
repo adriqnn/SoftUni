75b0e06f38d60e95cd03a20b3026613aa40ce4c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-05 03:39:30 +0300

    06

--- 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_ReversedChars.js
+++ 21_JavaScriptFundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_ReversedChars.js
@@ -2,7 +2,7 @@ function reversed_charsV1(a, b, c){
     return `${c} ${b} ${a}`;
 }
 
-const reversed_charsV2 = function (a, b, c){
+const reversed_charsV2 = function(a, b, c){
     return `${c} ${b} ${a}`;
 }
 
