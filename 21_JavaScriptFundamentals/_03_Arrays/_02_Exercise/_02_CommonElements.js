1a6c9d486fbb0c62ae663beb1304e734e95f8076
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-06 21:38:55 +0300

    02

---
+++ 21_JavaScriptFundamentals/_03_Arrays/_02_Exercise/_02_CommonElements.js
@@ -0,0 +1,40 @@
+function common_elementsV1(arr1, arr2){
+    let result = "";
+
+    for (let i = 0; i < arr1.length; i++) {
+        let current = arr1[i];
+        if(arr2.includes(current)){
+            result += `${current}\n`;
+        }
+    }
+
+    return result;
+}
+
+const common_elementsV2 = function(){
+    let result = "";
+
+    for (let i = 0; i < arr1.length; i++) {
+        let current = arr1[i];
+        if(arr2.includes(current)){
+            result += `${current}\n`;
+        }
+    }
+
+    return result;
+}
+
+const common_elementsV3 = () => {
+    let result = "";
+
+    for (let i = 0; i < arr1.length; i++) {
+        let current = arr1[i];
+        if(arr2.includes(current)){
+            result += `${current}\n`;
+        }
+    }
+
+    return result;
+}
+
+// console.log(common_elementsV1(['Hey', 'hello', 2, 4, 'Peter', 'e'], ['Petar', 10, 'hey', 4, 'hello', '2']));
