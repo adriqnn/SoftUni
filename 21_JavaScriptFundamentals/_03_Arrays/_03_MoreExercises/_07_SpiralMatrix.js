6564830d0822193081170d2fed2a07f721b0b682
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-07 01:35:38 +0300

    07

---
+++ 21_JavaScriptFundamentals/_03_Arrays/_03_MoreExercises/_07_SpiralMatrix.js
@@ -0,0 +1,131 @@
+function spiral_matrixV1(rows, columns){
+    const matrix = new Array(rows).fill(0).map(() => new Array(columns).fill(0));
+
+    let num = 1;
+    let top = 0;
+    let bottom = rows - 1;
+    let left = 0;
+    let right = columns - 1;
+
+    while (num <= rows * columns) {
+        // Traverse from left to right along the top row
+        for (let i = left; i <= right && num <= rows * columns; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+
+        // Traverse from top to bottom along the right column
+        for (let i = top; i <= bottom && num <= rows * columns; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // Traverse from right to left along the bottom row
+        for (let i = right; i >= left && num <= rows * columns; i--) {
+            matrix[bottom][i] = num++;
+        }
+        bottom--;
+
+        // Traverse from bottom to top along the left column
+        for (let i = bottom; i >= top && num <= rows * columns; i--) {
+            matrix[i][left] = num++;
+        }
+        left++;
+    }
+
+    let result = "";
+
+    for (let row of matrix) {
+        result += `${row.join(" ")}\n`
+    }
+
+    return result;
+}
+
+const spiral_matrixV2 = function(rows, columns){
+    const matrix = new Array(rows).fill(0).map(() => new Array(columns).fill(0));
+
+    let num = 1;
+    let top = 0;
+    let bottom = rows - 1;
+    let left = 0;
+    let right = columns - 1;
+
+    while (num <= rows * columns) {
+        // Traverse from left to right along the top row
+        for (let i = left; i <= right && num <= rows * columns; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+
+        // Traverse from top to bottom along the right column
+        for (let i = top; i <= bottom && num <= rows * columns; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // Traverse from right to left along the bottom row
+        for (let i = right; i >= left && num <= rows * columns; i--) {
+            matrix[bottom][i] = num++;
+        }
+        bottom--;
+
+        // Traverse from bottom to top along the left column
+        for (let i = bottom; i >= top && num <= rows * columns; i--) {
+            matrix[i][left] = num++;
+        }
+        left++;
+    }
+
+    let result = "";
+
+    for (let row of matrix) {
+        result += `${row.join(" ")}\n`
+    }
+
+    return result;
+}
+
+const spiral_matrixV3 = (rows, columns) => {
+    const matrix = new Array(rows).fill(0).map(() => new Array(columns).fill(0));
+
+    let num = 1;
+    let top = 0;
+    let bottom = rows - 1;
+    let left = 0;
+    let right = columns - 1;
+
+    while (num <= rows * columns) {
+        // Traverse from left to right along the top row
+        for (let i = left; i <= right && num <= rows * columns; i++) {
+            matrix[top][i] = num++;
+        }
+        top++;
+
+        // Traverse from top to bottom along the right column
+        for (let i = top; i <= bottom && num <= rows * columns; i++) {
+            matrix[i][right] = num++;
+        }
+        right--;
+
+        // Traverse from right to left along the bottom row
+        for (let i = right; i >= left && num <= rows * columns; i--) {
+            matrix[bottom][i] = num++;
+        }
+        bottom--;
+
+        // Traverse from bottom to top along the left column
+        for (let i = bottom; i >= top && num <= rows * columns; i--) {
+            matrix[i][left] = num++;
+        }
+        left++;
+    }
+
+    let result = "";
+
+    for (let row of matrix) {
+        result += `${row.join(" ")}\n`
+    }
+
+    return result;
+}
