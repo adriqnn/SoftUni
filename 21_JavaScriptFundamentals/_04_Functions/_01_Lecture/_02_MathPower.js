7f093497e0f0989ee01ae7ef9ee9ad24bc93f88c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-09 22:44:00 +0300

    02

---
+++ 21_JavaScriptFundamentals/_04_Functions/_01_Lecture/_02_MathPower.js
@@ -0,0 +1,11 @@
+function math_powerV1(number, power){
+    return number ** power;
+}
+
+const math_powerV2 = function(number, power){
+    return number ** power;
+}
+
+const math_powerV3 = (number, power) => {
+    return number ** power;
+}
