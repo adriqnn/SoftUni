c3d3099f18e7841c832c4ed8e229eaff1f57df56
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-09 22:44:06 +0300

    03

---
+++ 21_JavaScriptFundamentals/_04_Functions/_01_Lecture/_03_RepeatString.js
@@ -0,0 +1,11 @@
+function repeat_stringV1(string, times){
+    return string.repeat(times);
+}
+
+const repeat_stringV2 = function(string, times){
+    return string.repeat(times);
+}
+
+const repeat_stringV3 = (string, times) => {
+    return string.repeat(times);
+}
