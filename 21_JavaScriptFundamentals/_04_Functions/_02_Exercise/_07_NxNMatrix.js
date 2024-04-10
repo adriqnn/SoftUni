bf588ee2a151c58e947a49b4d2169404eef05c1b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-10 20:53:32 +0300

    07

---
+++ 21_JavaScriptFundamentals/_04_Functions/_02_Exercise/_07_NxNMatrix.js
@@ -0,0 +1,41 @@
+function nxn_matrixV1(number){
+    let result = "";
+
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            result += `${number} `;
+        }
+
+        result += `\n`;
+    }
+
+    return result;
+}
+
+const nxn_matrixV2 = function(number){
+    let result = "";
+
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            result += `${number} `;
+        }
+
+        result += `\n`;
+    }
+
+    return result;
+}
+
+const nxn_matrixV3 = (number) => {
+    let result = "";
+
+    for (let i = 0; i < number; i++) {
+        for (let j = 0; j < number; j++) {
+            result += `${number} `;
+        }
+
+        result += `\n`;
+    }
+
+    return result;
+}
