8ecdda92c7b121ef2e1062b3942f719cc87107a7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-10 20:52:59 +0300

    01

---
+++ 21_JavaScriptFundamentals/_04_Functions/_02_Exercise/_01_SmallestOfThreeNumbers.js
@@ -0,0 +1,11 @@
+function smallest_of_three_numbersV1(number1, number2, number3){
+    return Math.min(number1, number2, number3);
+}
+
+const smallest_of_three_numbersV2 = function(number1, number2, number3){
+    return Math.min(number1, number2, number3);
+}
+
+const smallest_of_three_numbersV3 = (number1, number2, number3) => {
+    return Math.min(number1, number2, number3);
+}
