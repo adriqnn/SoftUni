7054b6de3739797d239ac8272012d25d4944f5a2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-12 00:24:05 +0300

    10

---
+++ 21_JavaScriptFundamentals/_05_ArraysAdvanced/_02_Exercise/_10_BuildAWall.js
@@ -0,0 +1,71 @@
+function build_a_wallV1(array){
+    let days = [];
+    array.map(e => Number(e));
+
+    while(array.some(e => e !== 30)){
+        let sum = 0;
+        let index = 0;
+
+        array.forEach(e => {
+            if(e < 30){
+                sum += 195;
+                array[index] = array[index] + 1;
+            }
+
+            index++
+        });
+
+        days.push(sum);
+    }
+
+    return `${days.join(", ")}\n${days.reduce((a,v) => a + v, 0) * 1900} pesos`;
+}
+
+const build_a_wallV2 = function(array){
+    let days = [];
+    array.map(e => Number(e));
+
+    while(array.some(e => e !== 30)){
+        let sum = 0;
+        let index = 0;
+
+        array.forEach(e => {
+            if(e < 30){
+                sum += 195;
+                array[index] = array[index] + 1;
+            }
+
+            index++
+        });
+
+        days.push(sum);
+    }
+
+    return `${days.join(", ")}\n${days.reduce((a,v) => a + v, 0) * 1900} pesos`;
+}
+
+const build_a_wallV3 = (array) => {
+    let days = [];
+    array.map(e => Number(e));
+
+    while(array.some(e => e !== 30)){
+        let sum = 0;
+        let index = 0;
+
+        array.forEach(e => {
+            if(e < 30){
+                sum += 195;
+                array[index] = array[index] + 1;
+            }
+
+            index++
+        });
+
+        days.push(sum);
+    }
+
+    return `${days.join(", ")}\n${days.reduce((a,v) => a + v, 0) * 1900} pesos`;
+}
+
+// console.log(build_a_wallV1([21, 25, 28]));
+// console.log(build_a_wallV1([17, 22, 17, 19, 17]));
