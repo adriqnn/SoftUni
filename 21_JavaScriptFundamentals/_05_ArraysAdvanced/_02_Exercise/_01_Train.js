0a49fa4fcd93f89750da5dd9eaadb329592dbc06
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-12 00:23:19 +0300

    01

---
+++ 21_JavaScriptFundamentals/_05_ArraysAdvanced/_02_Exercise/_01_Train.js
@@ -0,0 +1,77 @@
+function trainV1(array){
+    let wagons = array.shift().split(" ").map(e => Number(e));
+    let maxCapacity = Number(array.shift());
+
+    function addPassengers(n) {
+        for (let i = 0; i < wagons.length; i++) {
+            if (wagons[i] + n <= maxCapacity) {
+                wagons[i] += n
+                break
+            }
+        }
+    }
+
+    function add(value) {
+        wagons.push(value)
+    }
+
+    array.forEach(x => {
+        const [command, value] = x.split(" ")
+        value ? add(Number(value)) : addPassengers(Number(command))
+    })
+
+    return wagons.join(" ");
+}
+
+const trainV2 = function(array){
+    let wagons = array.shift().split(" ").map(e => Number(e));
+    let maxCapacity = Number(array.shift());
+
+    function addPassengers(n) {
+        for (let i = 0; i < wagons.length; i++) {
+            if (wagons[i] + n <= maxCapacity) {
+                wagons[i] += n
+                break
+            }
+        }
+    }
+
+    function add(value) {
+        wagons.push(value)
+    }
+
+    array.forEach(x => {
+        const [command, value] = x.split(" ")
+        value ? add(Number(value)) : addPassengers(Number(command))
+    })
+
+    return wagons.join(" ");
+}
+
+const trainV3 = (array) => {
+    let wagons = array.shift().split(" ").map(e => Number(e));
+    let maxCapacity = Number(array.shift());
+
+    function addPassengers(n) {
+        for (let i = 0; i < wagons.length; i++) {
+            if (wagons[i] + n <= maxCapacity) {
+                wagons[i] += n
+                break
+            }
+        }
+    }
+
+    function add(value) {
+        wagons.push(value)
+    }
+
+    array.forEach(x => {
+        const [command, value] = x.split(" ")
+        value ? add(Number(value)) : addPassengers(Number(command))
+    })
+
+    return wagons.join(" ");
+}
+
+
+// console.log(trainV1(['32 54 21 12 4 0 23', '75', 'Add 10', 'Add 0', '30', '10', '75']));
