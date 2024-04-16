c14213c0333ac3b03f11bda133e5320af66b684b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-16 04:01:08 +0300

    02

---
+++ 21_JavaScriptFundamentals/_08_TextProcessing/_01_Lecture/_02_Substring.js
@@ -0,0 +1,11 @@
+function substringV1(word, start, end){
+    return word.slice(start, start + end);
+}
+
+const substringV2 = function(word, start, end){
+    return word.slice(start, start + end);
+}
+
+const substringV3 = (word, start, end) => {
+    return word.slice(start, start + end);
+}
