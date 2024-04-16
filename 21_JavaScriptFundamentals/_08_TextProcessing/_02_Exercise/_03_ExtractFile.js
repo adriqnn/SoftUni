5df261b957a1771d5840bb8b5e0b305c8db60733
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-16 04:41:29 +0300

    03

---
+++ 21_JavaScriptFundamentals/_08_TextProcessing/_02_Exercise/_03_ExtractFile.js
@@ -0,0 +1,28 @@
+function extract_fileV1(path){
+    let file = path.split("\\").pop();
+
+    let name= file.substring(0, file.lastIndexOf("."))
+    let extension= file.substring(file.lastIndexOf(".")+1)
+
+    return `File name: ${name}\nFile extension: ${extension}`;
+}
+
+const extract_fileV2 = function(path){
+    let file = path.split("\\").pop();
+
+    let name= file.substring(0, file.lastIndexOf("."))
+    let extension= file.substring(file.lastIndexOf(".")+1)
+
+    return `File name: ${name}\nFile extension: ${extension}`;
+}
+
+const extract_fileV3 = (path) => {
+    let file = path.split("\\").pop();
+
+    let name= file.substring(0, file.lastIndexOf("."))
+    let extension= file.substring(file.lastIndexOf(".")+1)
+
+    return `File name: ${name}\nFile extension: ${extension}`;
+}
+
+// console.log(extract_fileV1('C:\\Internal\\training-internal\\Template.pptx'));
