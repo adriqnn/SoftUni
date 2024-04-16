1edd0105b2537c1b18ded28dd75365dfb2371859
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-16 04:01:03 +0300

    01

---
+++ 21_JavaScriptFundamentals/_08_TextProcessing/_01_Lecture/_01_PrintCharacters.js
@@ -0,0 +1,11 @@
+function print_charactersV1(word){
+    return word.split("").map(e => `${e}`).join("\n");
+}
+
+const print_charactersV2 = function(word){
+    return word.split("").map(e => `${e}`).join("\n");
+}
+
+const print_charactersV3 = (word) => {
+    return word.split("").map(e => `${e}`).join("\n");
+}
