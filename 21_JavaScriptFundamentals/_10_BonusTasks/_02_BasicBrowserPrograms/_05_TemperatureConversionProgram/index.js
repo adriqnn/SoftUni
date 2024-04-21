8cce631969642289658bee1c4f71799161e90238
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-22 01:42:28 +0300

    js

--- 21_JavaScriptFundamentals/_10_BonusTasks/_02_BasicBrowserPrograms/_05_TemperatureConversionProgram/index.js
+++ 21_JavaScriptFundamentals/_10_BonusTasks/_02_BasicBrowserPrograms/_05_TemperatureConversionProgram/index.js
@@ -2,8 +2,8 @@ const textBox = document.getElementById("textBox");
 const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
-let temp;
 
+let temp;
 
 function convert(){
     if(toFahrenheit.checked){
