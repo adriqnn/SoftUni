805ecae9c4402d5e369e652a53ea8a866c41139d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-23 04:04:19 +0300

    js

--- 21_JavaScriptFundamentals/_10_BonusTasks/_02_BasicBrowserPrograms/_10_RockPaperScissors/index.js
+++ 21_JavaScriptFundamentals/_10_BonusTasks/_02_BasicBrowserPrograms/_10_RockPaperScissors/index.js
@@ -4,6 +4,7 @@ const computerDisplay = document.getElementById("computerDisplay");
 const resultDisplay = document.getElementById("resultDisplay");
 const playerScoreDisplay = document.getElementById("playerScoreDisplay");
 const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
 let playerScore = 0;
 let computerScore = 0;
 
