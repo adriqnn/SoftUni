d5921e8c348947192db1c400b6d3c18c44685e8d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-04-28 23:32:54 +0300

    03

---
+++ 21_JavaScriptFundamentals/_10_BonusTasks/_04_InterestingExamples/_03_ChainCallingFunctions.js
@@ -0,0 +1,21 @@
+function chain_sum_function(n){
+    const inner = function(a){
+        n += a;
+        return inner;
+    };
+
+    inner.toString = function(){
+        return n;
+    };
+
+    return inner;
+}
+
+
+console.log(chain_sum_function(1).toString());
+console.log(chain_sum_function(1)(6)(-3).toString());
+
+
+// chain calling example
+let sum = a => b => b ? sum(a + b) : a;
+console.log(sum(10)(20)(3)(4)());
