d144a32da020e7ff5763d1fd165326a1280dd717
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-08 04:26:10 +0300

    03

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_03_LargestNumber.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_03_LargestNumber.js
@@ -1,11 +1,21 @@
-function largestNumber(num1,num2,num3){
+function largest_numberV1(first, second, third){
+    return `The largest number is ${Math.max(first, second, third)}.`;
+}
+
+function largest_numberV2(...args){
+    return `The largest number is ${Math.max(...args)}.`;
+}
+
+function largest_numberV3(num1, num2, num3){
     let result;
-        if(num1 > num2 && num1 > num3){
-            result = num1;
-        }else if(num2 > num1 && num2 > num3){
-            result = num2;
-        }else if(num3 > num2 && num3 > num1){
-            result = num3;
-        }
-        console.log(`The largest number is ${result}.`);
+
+    if(num1 > num2 && num1 > num3){
+        result = num1;
+    }else if(num2 > num1 && num2 > num3){
+        result = num2;
+    }else if(num3 > num2 && num3 > num1){
+        result = num3;
     }
+
+    return `The largest number is ${result}.`;
+}
