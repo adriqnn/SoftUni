76b421ee7680f596e7ba8860ad1a730527056c5b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-08 04:19:09 +0300

    02

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_02_StringLength.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_02_StringLength.js
@@ -8,3 +8,19 @@ function string_lengthV2(string1, string2, string3){
     const average = Math.floor(characterCount / 3);
     return `${characterCount}\n${average}`;
 }
+
+function string_lengthV3(...args){
+    const [string1, string2, string3] = args;
+
+    const characterCount = string1.length + string2.length + string3.length;
+    const average = Math.floor(characterCount / 3);
+
+    return `${characterCount}\n${average}`;
+}
+
+function string_lengthV4(...args){
+    const characterCount = args.reduce((total, currentString) => total + currentString.length, 0);
+    const average = Math.floor(characterCount / args.length);
+
+    return `${characterCount}\n${average}`;
+}
