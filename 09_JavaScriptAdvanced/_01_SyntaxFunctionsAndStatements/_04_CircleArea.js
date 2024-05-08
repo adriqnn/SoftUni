00471cb509807cd48784a62e4ed04b9a91a4764f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-08 04:55:00 +0300

    04

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_04_CircleArea.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_04_CircleArea.js
@@ -1,8 +1,12 @@
-function circleArea(radius){
+function circle_areaV1(input){
+    return typeof input === 'number' ? Math.round((input ** 2 * Math.PI) * 100 ) / 100 : `We can not calculate the circle area, because we receive a ${typeof input}.`;
+}
+
+function circle_areaV2(radius){
     if(typeof radius == 'number'){
         let area = radius ** 2 * Math.PI;
-        console.log(Math.round(area*100)/100);
+        return Math.round(area * 100) / 100;
     }else{
-        console.log(`We can not calculate the circle area, because we receive a ${typeof radius}.`);
+        return `We can not calculate the circle area, because we receive a ${typeof radius}.`;
     }
 }
