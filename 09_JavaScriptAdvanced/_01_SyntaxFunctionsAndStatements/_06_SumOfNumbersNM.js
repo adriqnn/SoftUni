22f0de173d2f5905213f7ef05b886dc557af47e4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-09 16:00:16 +0300

    06

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_06_SumOfNumbersNM.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_06_SumOfNumbersNM.js
@@ -1,8 +1,20 @@
-function sum(n,m){
+function sum_of_numbers_nmV1(...args){
+    let [n, m] = args;
     let result = 0;
 
-    for(let i=Number(n);i<=Number(m);i++){
+    for(let i= Number(n); i <= Number(m); i++){
         result += i;
     }
-    console.log(result);
+
+    return result;
+}
+
+function sum_of_numbers_nmV2(n, m){
+    let result = 0;
+
+    for(let i= Number(n); i <= Number(m); i++){
+        result += i;
+    }
+
+    return result;
 }
