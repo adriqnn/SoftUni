1f76a74b87e1df117d2c4ebdfb3dfb0ec0fda223
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-09 21:32:40 +0300

    08

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_08_DaysInAMonth.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatements/_08_DaysInAMonth.js
@@ -1,6 +1,6 @@
-function lastDay(month,year){
-    let date = new Date(year,month);
-    date.setDate(1-1);
-    let day = date.getDate();
-    console.log(day);
+function days_in_a_monthV1(month, year) {
+    let date = new Date(year, month);
+    date.setDate(1 - 1);
+
+    return date.getDate();
 }
