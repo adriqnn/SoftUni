4f0e1b5eb81b2bad8f64daf16182135dd735cf5a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 23:28:48 +0300

    08

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_08_ValidityChecker.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_08_ValidityChecker.js
@@ -1,18 +1,45 @@
-function validate(num1,num2,num3,num4){
-    let x1 = num1;
-    let y1 = num2;
-    let x2 = num3;
-    let y2 = num4;
+function validity_checkerV1(...args){
+    let x1 = args.shift();
+    let y1 = args.shift();
+    let x2 = args.shift();
+    let y2 = args.shift();
 
-    // x1 y1 0 0
-    // x2 y2 0 0
-    // x1 y1 x2 y2
+    let result = "";
 
-    let validate1 = (Math.sqrt((0-x1)**2 + (0-y1)**2));
-    let validate2 = (Math.sqrt((0-x2)**2 + (0-y2)**2));
-    let validate3 = (Math.sqrt((x2-x1)**2 + (y2-y1)**2));
+    let distanceX1Y1Z = Math.sqrt((0 - x1) * (0 - x1) + (0 - y1) * (0 - y1));
+    let distanceX2Y2z = Math.sqrt((0 - x2) * (0 - x2) + (0 - y2) * (0 - y2));
+    let distanceX1Y1X2Y2 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
 
-    console.log(`{${x1}, ${y1}} to {0, 0} is ${validate1 % 1 === 0 ? 'valid' : 'invalid'}`);
-    console.log(`{${x2}, ${y2}} to {0, 0} is ${validate2 % 1 === 0 ? 'valid' : 'invalid'}`);
-    console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is ${validate3 % 1 === 0 ? 'valid' : 'invalid'}`);
+    Number.isInteger(distanceX1Y1Z) ? result += `{${x1}, ${y1}} to {${0}, ${0}} is valid\n` : result += `{${x1}, ${y1}} to {${0}, ${0}} is invalid\n`;
+    Number.isInteger(distanceX2Y2z) ? result += `{${x2}, ${y2}} to {${0}, ${0}} is valid\n` : result += `{${x2}, ${y2}} to {${0}, ${0}} is invalid\n`;
+    Number.isInteger(distanceX1Y1X2Y2) ? result += `{${x1}, ${y1}} to {${x2}, ${y2}} is valid\n` : result += `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid\n`;
+
+    return result;
+}
+
+function validity_checkerV2(...points){
+    let [x1, y1, x2, y2] = points;
+    let result = "";
+
+    let distanceX1Y1Z = Math.sqrt((0 - x1) * (0 - x1) + (0 - y1) * (0 - y1));
+    let distanceX2Y2z = Math.sqrt((0 - x2) * (0 - x2) + (0 - y2) * (0 - y2));
+    let distanceX1Y1X2Y2 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+
+    Number.isInteger(distanceX1Y1Z) ? result += `{${x1}, ${y1}} to {${0}, ${0}} is valid\n` : result += `{${x1}, ${y1}} to {${0}, ${0}} is invalid\n`;
+    Number.isInteger(distanceX2Y2z) ? result += `{${x2}, ${y2}} to {${0}, ${0}} is valid\n` : result += `{${x2}, ${y2}} to {${0}, ${0}} is invalid\n`;
+    Number.isInteger(distanceX1Y1X2Y2) ? result += `{${x1}, ${y1}} to {${x2}, ${y2}} is valid\n` : result += `{${x1}, ${y1}} to {${x2}, ${y2}} is invalid\n`;
+
+    return result;
+}
+
+function validity_checkerV3(x1, y1, x2, y2){
+    const isValidDistance = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) % 1 === 0;
+
+    const pairs = [ [x1, y1, 0, 0], [x2, y2, 0, 0], [x1, y1, x2, y2] ];
+    const printMsg = (a, b, c, d, validity) => `{${a}, ${b}} to {${c}, ${d}} is ${validity}`;
+    let result = "";
+
+    pairs.forEach((e, i) => result += `${isValidDistance(...pairs[i]) ? printMsg(...e, "valid") : printMsg(...e, "invalid")}\n`);
+
+    return result;
 }
