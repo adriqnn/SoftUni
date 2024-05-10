c707bc0cb861a49eaf4d72e58fa37b8cb8f54bb1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 03:53:27 +0300

    Update _03_SameNumbers.js

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_03_SameNumbers.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_03_SameNumbers.js
@@ -1,15 +1,37 @@
-function num(number){
+function same_numbersV1(number){
     let input = String(number);
+
+    let char = input.charAt(0);
+    let sum = 0;
+    let result = true;
+
+    for (let i = 0; i < input.length; i++) {
+        if(char !== input.charAt(i)){
+            result = false;
+        }
+
+        sum += Number(input.charAt(i));
+    }
+
+    return result ? `${result}\n${sum}` : `${result}\n${sum}`;
+}
+
+function same_numbersV2(number){
+    // convert to string -> let input = number.toString();
+    let input = String(number);
+
     let isSame = true;
     let firstDigit = input[0];
     let sum = 0;
 
-    for(i = 0;i < input.length;i++){
-        if(input[i] != firstDigit){
+    for(let i = 0;i < input.length;i++){
+        if(input[i] !== firstDigit){
             isSame = false;
         }
+
+        // convert to number -> sum += +input[i];
         sum += Number(input[i]);
     }
-    console.log(isSame);
-    console.log(sum);
+
+    return `${isSame}\n${sum}`
 }
