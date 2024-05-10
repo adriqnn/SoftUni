545ba4a82bdc58775efb1f09eed8ad4fe26a2198
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 04:01:04 +0300

    04

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_04_PreviousDay.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_04_PreviousDay.js
@@ -1,6 +1,6 @@
-function previousDay(year, month, day){
-    let date = new Date(year, month -1, day);
+function previous_dayV1(year, month, day){
+    let date = new Date(year, month - 1, day);
     date.setDate(date.getDate() - 1);
 
-    console.log(`${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`);
+    return `${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()}`;
 }
