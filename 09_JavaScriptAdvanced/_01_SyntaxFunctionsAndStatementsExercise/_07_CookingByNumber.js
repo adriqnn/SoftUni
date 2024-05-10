9a3c142f6b198db2cefea23a6ac8b6c8558dd954
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 21:58:15 +0300

    07

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_07_CookingByNumber.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_07_CookingByNumber.js
@@ -1,15 +1,30 @@
-function cooking(number,command1,command2,command3,command4,command5){
+function cooking_by_numbersV1(...args){
+    let n = args.shift();
+
+    let cooking = {
+        chop: x => n = x / 2,
+        dice: x => n = Math.sqrt(x),
+        spice: x => n = x + 1,
+        bake: x => n = x * 3,
+        fillet: x => n = x * 0.8
+    }
+
+    return args.map(e => cooking[e](n)).join("\n");
+}
+
+function cooking_by_numbersV2(number, command1, command2, command3, command4, command5){
     let n = +number;
+    let result = "";
 
-    let chop = x => x/2;
+    let chop = x => x / 2;
     let dice = x => Math.sqrt(x);
-    let sprice = x => x+1;
-    let bake = x => x*3;
-    let fillet = x => x*0.8;
+    let spice = x => x + 1;
+    let bake = x => x * 3;
+    let fillet = x => x * 0.8;
 
-    let commandList = [command1,command2,command3,command4,command5];
+    let commandList = [command1, command2, command3, command4, command5];
 
-    for(let i=0; i<commandList.length; i++){
+    for(let i= 0; i < commandList.length; i++){
         switch (commandList[i]) {
             case 'chop':
                 number = chop(number);
@@ -18,7 +33,7 @@ function cooking(number,command1,command2,command3,command4,command5){
                 number = dice(number);
                 break;
             case 'spice':
-                number = sprice(number);
+                number = spice(number);
                 break;
             case 'bake':
                 number = bake(number);
@@ -29,6 +44,9 @@ function cooking(number,command1,command2,command3,command4,command5){
             default:
                 break;
         }
-        console.log(number);
+
+        result += `${number}\n`;
     }
+
+    return result;
 }
