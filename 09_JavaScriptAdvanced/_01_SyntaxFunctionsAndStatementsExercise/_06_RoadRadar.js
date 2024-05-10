1b040b1157f6c767866ba2a172260dfaad61cf65
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 16:17:18 +0300

    06

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_06_RoadRadar.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_06_RoadRadar.js
@@ -1,34 +1,61 @@
-function roadRadar(speed, area){
-    let result = ''
+function road_radarV1(speed, area){
+    let result;
     let speedLimit = 0;
     let difference = 0;
     let status = '';
 
-    if(area == 'motorway'){
+    if(area === 'motorway'){
         speedLimit = 130;
         difference = speed - 130;
-    }else if(area == 'interstate'){
+    }else if(area === 'interstate'){
         speedLimit = 90;
         difference = speed - 90;
-    }else if(area == 'city'){
+    }else if(area === 'city'){
         speedLimit = 50;
         difference = speed - 50;
-    }else if(area == 'residential'){
+    }else if(area === 'residential'){
         speedLimit = 20;
         difference = speed - 20;
     }
 
-    if(difference <= 0 && speed >= 0 ){
+    if(difference <= 0){
         result = `Driving ${speed} km/h in a ${speedLimit} zone`;
-    }else if(speed > 0){
-        if(difference >= 41){
+    }else {
+        if(difference > 40){
             status = 'reckless driving';
-        }else if(difference >20){
+        }else if(difference > 20){
             status = 'excessive speeding';
         }else if(difference > 0){
             status = 'speeding';
         }
-        result = `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`
+
+        result = `The speed is ${difference} km/h faster than the allowed speed of ${speedLimit} - ${status}`;
     }
-    console.log(result);
+
+    return result;
+}
+
+function road_radarV2(speed, area){
+    const limits = {
+        motorway: 130,
+        interstate: 90,
+        city: 50,
+        residential: 20
+    }
+
+    const getStatus = n => {
+        const status = {
+            [n > 40]: "reckless driving",
+            [n > 20 && n <= 40]: "excessive speeding",
+            [n <= 20]: "speeding"
+        }
+
+        return status["true"];
+    }
+
+    const speedDiff = speed - limits[area];
+
+    return speedDiff > 0
+        ? `The speed is ${speedDiff} km/h faster than the allowed speed of ${limits[area]} - ${getStatus(speedDiff)}`
+        : `Driving ${speed} km/h in a ${limits[area]} zone`;
 }
