ddb1f7f73fa1b13d28cb2ef9920feda41469477a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-11 00:02:14 +0300

    09

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_09_WordsUppercase.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_09_WordsUppercase.js
@@ -2,6 +2,10 @@ function words_to_uppercaseV1(words){
     let uppercase = String(words).toUpperCase();
     let result = "";
 
+    //let wordsInArray = upper.replace(/([ .,;:!?]+)/g,'*$#').split('*$#');
+    //wordsInArray = wordsInArray.filter(function(e){return e});
+    //let string = wordsInArray.toString().replace(/(,)/g,', ');
+
     return  uppercase.match(/\w+/g).join(", ");
 }
 
