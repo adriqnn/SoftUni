01890fa0dd7baefb63d8263ce4f224f5e9382b33
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-10 22:41:05 +0300

    01

--- 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_01_Fruit.js
+++ 09_JavaScriptAdvanced/_01_SyntaxFunctionsAndStatementsExercise/_01_Fruit.js
@@ -4,3 +4,9 @@ function fruitV1(fruitName, weightInGrams, pricePerKilogram){
 
     return `I need $${price.toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruitName}.`;
 }
+
+function fruitV2(fruitName, weightInGrams, pricePerKilogram){
+    let weight = weightInGrams / 1000;
+
+    return `I need $${(weight * pricePerKilogram).toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruitName}.`;
+}
