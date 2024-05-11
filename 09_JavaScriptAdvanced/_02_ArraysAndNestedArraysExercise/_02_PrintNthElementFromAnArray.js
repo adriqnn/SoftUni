a4552310a167a12696d9aa0b65c90c9c80d72866
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-11 22:53:22 +0300

    02

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_02_PrintNthElementFromAnArray.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_02_PrintNthElementFromAnArray.js
@@ -1,3 +1,12 @@
-function solve(arr,number){
-    return arr.filter((x,i) => i % number == 0)
+function print_every_nth_element_from_an_arrayV1(arr, number){
+    return arr.filter((e, i) => i % number === 0);
+}
+
+function print_every_nth_element_from_an_arrayV2(arr, number){
+    const result = [];
+    for (let i = 0; i < arr.length; i += number) {
+        result.push(arr[i]);
+    }
+
+    return result;
 }
