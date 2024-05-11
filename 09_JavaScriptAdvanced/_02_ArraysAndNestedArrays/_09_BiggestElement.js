340cec5d3d76953475b99a9fe4742f8b8b40a5af
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-11 17:18:02 +0300

    09

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_09_BiggestElement.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_09_BiggestElement.js
@@ -1,3 +1,13 @@
-function solve(arr) {
-    return arr.reduce((a, v) => (a = Math.max(...v) > a ? Math.max(...v) : a), -Infinity)
+function biggest_elementV1(arr){
+    return arr.flat().reduce((a, v) => {
+        if(v > a){
+            a = v;
+        }
+
+        return a;
+    }, -Infinity);
+}
+
+function biggest_elementV2(arr){
+    return arr.reduce((a, v) => (a = Math.max(...v) > a ? Math.max(...v) : a), -Infinity);
 }
