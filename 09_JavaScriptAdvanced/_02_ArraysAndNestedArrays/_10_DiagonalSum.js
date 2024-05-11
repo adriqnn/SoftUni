000ac12416ed097f0eec9d0b81beba760cf7cd52
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-11 19:45:03 +0300

    10

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_10_DiagonalSum.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_10_DiagonalSum.js
@@ -1,5 +1,9 @@
-function solve(arr) {
-    const calcDiagonal = arr => arr.reduce((a, v, i) => a + v[i], 0)
+function diagonal_sumV1(matrix){
+    return `${matrix.reduce((a, v, i) => a + v[i], 0)} ${matrix.reverse().reduce((a, v, i) => a + v[i], 0)}`;
+}
 
-    return `${calcDiagonal(arr)} ${calcDiagonal(arr.reverse())}`
+function diagonal_sumV2(matrix){
+    const calculateDiagonalSum = (arr) => arr.reduce((a, v, i) => a + v[i], 0);
+
+    return `${calculateDiagonalSum(matrix)} ${calculateDiagonalSum(matrix.reverse())}`;
 }
