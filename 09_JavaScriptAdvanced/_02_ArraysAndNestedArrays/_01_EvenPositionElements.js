fa4ab653bf41ba1a32884863833a060b8acd1b7e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-11 03:54:05 +0300

    01

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_01_EvenPositionElements.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArrays/_01_EvenPositionElements.js
@@ -1,10 +1,19 @@
-function solve(input){
-    let result = '';
+function even_position_elementV1(arr){
+    return arr.reduce((a, v, i) => i % 2 === 0 ? a + v + " " : a, "");
+}
 
-    for(let i = 0; i < input.length; i++){
-        if(i % 2 == 0){
-            result += input[i] + ' ';
+function even_position_elementV2(arr){
+    return arr.filter((e, i) => i % 2 === 0).join(" ");
+}
+
+function even_position_elementV3(arr){
+    let result = "";
+
+    for (let i = 0; i < arr.length; i++) {
+        if(i % 2 === 0){
+            result += arr[i] + " ";
         }
     }
-    console.log(result);
+    
+    return result;
 }
