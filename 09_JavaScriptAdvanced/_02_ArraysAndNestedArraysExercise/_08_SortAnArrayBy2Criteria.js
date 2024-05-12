ed8e755b7a356b5dff8b9de666b45580010074f8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 20:47:25 +0300

    08

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_08_SortAnArrayBy2Criteria.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_08_SortAnArrayBy2Criteria.js
@@ -1,11 +1,27 @@
-function solve(arr){
-    arr.sort(twoCriteriaSort);
-    return arr.join('\n');
+function sort_an_array_by_2_criteriaV1(arr){
+    return arr.sort((a, b) => {
+        let result = a.length - b.length;
 
-    function  twoCriteriaSort(current,next){
-        if(current.length === next.length){
-            return current.localeCompare(next)
+        if(result === 0){
+            result = a.localeCompare(b);
         }
-        return current.length - next.length;
+
+        return result;
+    }).join("\n");
+}
+
+function sort_an_array_by_2_criteriaV2(arr){
+    return  arr.sort(twoCriteriaSort).join("\n");
+
+    function twoCriteriaSort(current, next){
+        if(current.length === next.length){
+            return current.localeCompare(next);
+        }
+
+        return  current.length - next.length;
     }
+}
+
+function sort_an_array_by_2_criteriaV3(arr){
+    return arr.sort((a, b) => a.length - b.length === 0 ? a.localeCompare(b) : a.length - b.length).join("\n");
 }
