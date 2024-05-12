e7e67e538408f8be81f789526feb62fa5ca6693d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 06:41:15 +0300

    05

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_05_ExtractIncreasingSubsequenceFromArray.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_05_ExtractIncreasingSubsequenceFromArray.js
@@ -1,13 +1,51 @@
-function solve(arr){
-    let result = [];
-    let biggest = arr[0];
+function extract_increasing_subsequence_from_arrayV1(arr){
+    return arr.reduce((a, v) => {
+        if(a.length === 0){
+            a.push(v);
+        }else if(v >= a[a.length - 1]){
+            a.push(v);
+        }
 
-    for(let i = 0; i< arr.length; i++){
-        let currentNum = arr[i];
-        if(currentNum >= biggest){
-            result.push(currentNum);
-            biggest = currentNum;
+        return a;
+    }, []);
+}
+
+function extract_increasing_subsequence_from_arrayV2(arr){
+    let newArray = [];
+    let helper = -Infinity;
+
+    arr.forEach(e => {
+        if(e >= helper){
+            newArray.push(e);
+            helper = e;
+        }
+    });
+
+    return newArray;
+}
+
+function extract_increasing_subsequence_from_arrayV3(arr){
+    let result = [];
+    let biggestNumber = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+        let currenNumber = arr[i];
+
+        if(currenNumber >= biggestNumber){
+            result.push(currenNumber);
+            biggestNumber = currenNumber;
         }
     }
+
     return result;
+}
+
+function extract_increasing_subsequence_from_arrayV4(arr){
+    return arr.reduce((a, v) => {
+        if(v >= (a[a.length - 1] || arr[0])){
+            a.push(v);
+        }
+
+        return a;
+    }, []);
 }
