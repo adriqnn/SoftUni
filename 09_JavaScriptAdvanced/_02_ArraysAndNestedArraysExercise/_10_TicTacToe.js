3cfb579e7bf00646c11f549d03a234d88d8528fb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 23:42:37 +0300

    10

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_10_TicTacToe.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_10_TicTacToe.js
@@ -1,44 +1,44 @@
-function solve(arr) {
-    const board = [0, 0, 0].map(x => ["false", "false", "false"])
-    let player = "X"
+function tic_tac_toeV1(commands){
+    const board = [0, 0, 0].map(x => ["false", "false", "false"]);
+    let player = "X";
 
-    function isWinningMove(board, row, place) {
-        return (
-            board[row].every(x => x === player) ||
-            board
-                .reduce((a, v) => {
-                    a.push(v[place])
-                    return a
-                }, [])
-                .every(x => x === player) ||
-            [board[0][0], board[1][1], board[2][2]].every(x => x === player) ||
-            [board[0][2], board[1][1], board[2][0]].every(x => x === player)
-        )
+    function isWinningMove(board, row, place){
+        return(
+            board[row].every(x => x === player) || board.reduce((a, v) => {
+                a.push(v[place]);
+                return a;
+            }, [])
+                .every(x => x === player) || [board[0][0], board[1][1], board[2][2]].every(x => x === player) || [board[0][2], board[1][1], board[2][0]].every(x => x === player)
+        );
     }
 
-    const printBoard = board => console.log(board.map(x => x.join("\t")).join("\n"))
+    let result = "";
+    const printBoard = board => board.map(x => result += `${x.join("\t")}\n` );
 
-    for (let i = 0; i < arr.length; i++) {
-        const [row, place] = arr[i].split(" ").map(Number)
+    for (let i = 0; i < commands.length; i++) {
+        const [row, place] = commands[i].split(" ").map(Number);
 
-        if (board[row][place] !== "false") {
-            console.log("This place is already taken. Please choose another!")
-            continue
-        }
-        board[row][place] = player
-
-        if (isWinningMove(board, row, place)) {
-            console.log(`Player ${player} wins!`)
-            printBoard(board)
-            break
+        if(board[row][place] !== "false"){
+            result += `This place is already taken. Please choose another!\n`;
+            continue;
         }
 
-        if (board.every(x => x.every(y => y !== "false"))) {
-            console.log("The game ended! Nobody wins :(")
-            printBoard(board)
-            break
+        board[row][place] = player;
+
+        if(isWinningMove(board, row, place)){
+            result += `Player ${player} wins!\n`;
+            printBoard(board);
+            break;
         }
 
-        player = player === "X" ? "O" : "X"
+        if(board.every(x => x.every(y => y !== "false"))){
+            result += `The game ended! Nobody wins :(\n`;
+            printBoard(board);
+            break;
+        }
+
+        player = player === "X" ? "O" : "X";
     }
+
+    return result;
 }
