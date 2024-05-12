2e97c83bb1998fabea870671e51f5264d2899692
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 06:47:31 +0300

    06

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_06_ListOfNames.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_06_ListOfNames.js
@@ -1,6 +1,19 @@
-function solve(arr){
-    let newArr = arr.sort((a,b) => a.localeCompare(b));
-    for(let i = 0; i< arr.length; i++){
-        console.log(`${i+1}.${newArr[i]}`);
+function list_of_namesV1(arr){
+    return arr.sort((a, b) => a.localeCompare(b)).map((e, i) => `${i + 1}.${e}`).join("\n");
+}
+
+function list_of_namesV2(arr){
+    let sorted = arr.sort((a, b) => a.localeCompare(b));
+    let result = "";
+
+    for (let i = 0; i < sorted.length; i++) {
+        result += `${i+1}.${sorted[i]}\n`
     }
+
+    return result;
+}
+
+function list_of_namesV3(arr){
+    arr = arr.sort((a, b) => a.localeCompare(b));
+    return arr.map((e, i) => `${i + 1}.${e}`).join("\n");
 }
