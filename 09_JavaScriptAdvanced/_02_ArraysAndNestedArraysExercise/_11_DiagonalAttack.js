aeb0363bf67c63bfee76df328c23e78274c3abeb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 23:47:30 +0300

    11

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_11_DiagonalAttack.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_11_DiagonalAttack.js
@@ -1,34 +1,36 @@
-function solve(matrixRows) {
-    let matrix = matrixRows.map(
-        row => row.split(' ').map(Number));
- 
+function diagonal_attackV1(matrixRows){
+    let matrix = matrixRows.map(row => row.split(' ').map(Number));
+    let result = "";
+
     let sumFirstDiagonal = 0;
-    for (var i = 0; i < matrix.length; i++) {
+    for (let i = 0; i < matrix.length; i++) {
         sumFirstDiagonal = sumFirstDiagonal + matrix[i][i];
     }
+
     let sumSecondDiagonal = 0;
-    for (var j = 0; j < matrix.length; j++) {
-        sumSecondDiagonal = sumSecondDiagonal + matrix[j][matrix.length-1-j];
+    for (let j = 0; j < matrix.length; j++) {
+        sumSecondDiagonal = sumSecondDiagonal + matrix[j][matrix.length - 1 - j];
     }
- 
-    if (sumFirstDiagonal == sumSecondDiagonal){
-        for (var q = 0; q < matrix.length; q++) {
-            for (var z = 0; z < matrix.length; z++) {
-                if( q != z && q != matrix.length-1-z)  {
+
+    if (sumFirstDiagonal === sumSecondDiagonal){
+        for (let q = 0; q < matrix.length; q++) {
+            for (let z = 0; z < matrix.length; z++) {
+                if( q !== z && q !== matrix.length - 1 - z)  {
                     matrix[q][z] = sumFirstDiagonal;
                 }
             }
         }
+
+        printMatrix(matrix);
+    } else {
         printMatrix(matrix);
     }
-    else
-    {
-        printMatrix(matrix);
-    }
- 
+
     function printMatrix(matrix) {
-        for (var i = 0; i < matrix.length; i++) {
-            console.log(matrix[i].join(' '))
+        for (let i = 0; i < matrix.length; i++) {
+            result += `${matrix[i].join(' ')}\n`;
         }
     }
+
+    return result;
 }
