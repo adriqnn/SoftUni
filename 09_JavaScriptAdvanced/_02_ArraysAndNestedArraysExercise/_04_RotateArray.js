6a65c3b5e38694da8edd37781e25c678b466c405
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-12 05:59:38 +0300

    04

--- 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_04_RotateArray.js
+++ 09_JavaScriptAdvanced/_02_ArraysAndNestedArraysExercise/_04_RotateArray.js
@@ -1,6 +1,15 @@
-function solve(arr, rotations){
-    for(let i = 0; i < rotations; i++){
+function rotate_arrayV1(arr, rotations){
+    for (let i = 0; i < rotations; i++) {
         arr.unshift(arr.pop());
     }
-    console.log(arr.join(' '));
+
+    return arr.join(" ");
+}
+
+function rotate_arrayV2(arr, rotations){
+    while(rotations-- > 0){
+        arr.unshift(arr.pop());
+    }
+
+    return arr.join(" ");
 }
