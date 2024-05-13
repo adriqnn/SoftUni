d15d31615c4be46221e6c81aea0e3bda99617761
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 06:28:50 +0300

    06

--- 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_06_FromJSONtoHTMLTable.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_06_FromJSONtoHTMLTable.js
@@ -1,17 +1,18 @@
-function foo(str) {
-    const parsed = JSON.parse(str)
-    const escapeChar = w =>
-        `${w}`
-            .replace(/&/g, "&amp;")
-            .replace(/</g, "&lt;")
-            .replace(/>/g, "&gt;")
-            .replace(/"/g, "&quot;")
-            .replace(/'/g, "&#39;")
-    const makeRow = (tag, data) =>
-        `<tr>${data.map(x => `<${tag}>${escapeChar(x)}</${tag}>`).join("")}</tr>`
+function from_json_to_html_tableV1(str){
+    const escapeChar = w => `${w}`.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
 
-    return `<table>
-${makeRow("th", Object.keys(parsed[0]))}
-${parsed.map(x => makeRow("td", Object.values(x))).join("\n")}
-</table>`
+    let result = `<table>\n`;
+    result += `   <tr>${Object.keys(JSON.parse(str)[0]).map(e => `<th>${e}</th>`).join("")}</tr>\n`;
+    result += JSON.parse(str).map(e => `   <tr>${Object.entries(e).map(e => `<td>${escapeChar(e[1])}</td>`).join("")}</tr>`).join("\n");
+    result += `\n</table>`;
+
+    return result;
+}
+
+function from_json_to_html_tableV2(str){
+    const parsed = JSON.parse(str);
+    const escapeChar = w => `${w}`.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
+    const makeRow = (tag, data) => `<tr>${data.map(x => `<${tag}>${escapeChar(x)}</${tag}>`).join("")}</tr>`;
+
+    return `<table>\n${makeRow("th", Object.keys(parsed[0]))}\n${parsed.map(x => makeRow("td", Object.values(x))).join("\n")}\n</table>`;
 }
