17a3a8e113db418fcce673eebc2dea8c26935f76
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 04:33:03 +0300

    03

--- 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_03_CityTaxes.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_03_CityTaxes.js
@@ -1,4 +1,4 @@
-function cityTaxes(name,population,treasury){
+function city_taxesV1(name, population, treasury){
     const result = {
         name,
         population,
@@ -8,11 +8,47 @@ function cityTaxes(name,population,treasury){
             this.treasury += Math.floor(this.population * this.taxRate);
         },
         applyGrowth(percent){
-            this.population += Math.floor(this.population * percent/100)
+            this.population += Math.floor(this.population * percent / 100);
         },
         applyRecession(percent){
-            this.treasury -= Math.floor(this.treasury * percent/100)
+            this.treasury -= Math.floor(this.treasury * percent / 100);
         }
     }
+
     return result;
+}
+
+function city_taxesV2(name, population, treasury){
+    return {
+        name,
+        population,
+        treasury,
+        taxRate: 10,
+        collectTaxes(){
+            this.treasury += Math.floor(this.population * this.taxRate);
+        },
+        applyGrowth(percent){
+            this.population += Math.floor(this.population * percent / 100);
+        },
+        applyRecession(percent){
+            this.treasury -= Math.floor(this.treasury * percent / 100);
+        }
+    }
+}
+
+function city_taxesV3(name, population, treasury){
+    class City{
+        constructor(name, population, treasury) {
+            this.name = name;
+            this.population = population;
+            this.treasury = treasury;
+            this.taxRate = 10;
+        }
+
+        collectTaxes = () => this.treasury += Math.floor(this.population * this.taxRate);
+        applyGrowth= (percent) => this.population += Math.floor(this.population * percent / 100);
+        applyRecession= (percent) => this.treasury -= Math.floor(this.treasury * percent / 100);
+    }
+
+    return new City(name, population, treasury);
 }
