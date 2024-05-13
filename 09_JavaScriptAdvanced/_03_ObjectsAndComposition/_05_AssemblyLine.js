b44be6daec5e0d34d28d37304a550128a22e3013
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 05:44:13 +0300

    05

--- 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_05_AssemblyLine.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_05_AssemblyLine.js
@@ -1,8 +1,9 @@
-function createrAssemblyLine(){
-    return{
+function assembly_lineV1(){
+    return {
         hasClima: (car) => {
             car.temp = 21;
-            car.tempSettings = 21
+            car.tempSettings = 21;
+
             car.adjustTemp = () => {
                 if(car.temp < car.tempSettings){
                     car.temp++;
@@ -11,16 +12,14 @@ function createrAssemblyLine(){
                 }
             };
         },
-
         hasAudio: (car) => {
-            car.currentTrack = {name: '',artist: ''};
+            car.currentTrack = {name: '', artist: ''};
             car.nowPlaying = () => {
                 if(car.currentTrack != null){
                     console.log(`Now playing '${car.currentTrack.name}' by ${car.currentTrack.artist}`);
                 }
             };
         },
-
         hasParktronic: (car) => {
             car.checkDistance = (distance) => {
                 if(distance < 0.1){
@@ -34,6 +33,41 @@ function createrAssemblyLine(){
                 }
             };
         }
-
     };
+}
+
+function assembly_lineV2() {
+    return {
+        hasClima: obj =>
+            Object.assign(obj, {
+                temp: 21,
+                tempSettings: 21,
+                adjustTemp: function () {
+                    if (this.temp !== this.tempSettings) {
+                        this.temp < this.tempSettings ? (this.temp += 1) : (this.temp -= 1);
+                    }
+                },
+            }),
+        hasAudio: obj =>
+            Object.assign(obj, {
+                currentTrack: null,
+                nowPlaying: function () {
+                    if (this.currentTrack !== null) {
+                        console.log(`Now playing '${this.currentTrack.name} by ${this.currentTrack.artist}`);
+                    }
+                },
+            }),
+        hasParktronic: obj =>
+            Object.assign(obj, {
+                checkDistance: function (d) {
+                    const distances = {
+                        [d < 0.1]: "Beep! Beep! Beep!",
+                        [0.1 <= d && d < 0.25]: "Beep! Beep!",
+                        [0.25 <= d && d < 0.5]: "Beep!",
+                    }
+
+                    console.log((Object.entries(distances).find(x => x[0] === 'true') || ['true', ""])[1]);
+                },
+            }),
+    }
 }
