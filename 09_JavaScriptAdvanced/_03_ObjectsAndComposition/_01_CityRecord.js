52c3fee84fd3976526c25992d4ce0b3b2c508685
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 03:28:43 +0300

    01

--- 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_01_CityRecord.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndComposition/_01_CityRecord.js
@@ -1,8 +1,17 @@
-function city(name,population,treasury){
+function city_recordV1(...args){
+    let [town, pop, treasury] = args;
+
+    return { "name": town, "population": pop, treasury };
+}
+
+function city_recordV2(name, population, treasury){
+    return { name, population, treasury };
+}
+
+function city_recordV3(name, population, treasury){
     const result = {
-        name: name,
-        population: population,
-        treasury: treasury,
+        name, population, treasury
     }
+
     return result;
 }
