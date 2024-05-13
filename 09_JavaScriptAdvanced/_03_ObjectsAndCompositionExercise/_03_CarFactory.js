17144f505930047e2ba464446417c6241576bd5b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 19:35:57 +0300

    03

--- 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_03_CarFactory.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_03_CarFactory.js
@@ -1,12 +1,52 @@
-function carPurchase(car){
-    let engines = [{ power: 90, volume: 1800 },{ power: 120, volume: 2400 },{ power: 200, volume: 3500 }];
-    let carriages = [{type: 'hatchback', color: car.color}, {type: 'coupe', color: car.color}];
-    let wheelsize = car.wheelsize % 2 == 1 ? car.wheelsize : car.wheelsize - 1;
+function car_factoryV1(obj){
+    let engines = [{power: 90, volume: 1800}, {power: 120, volume: 2400}, {power: 200, volume: 3500}];
+    let carriage = [{type: 'hatchback'}, {type: 'coupe'}];
+
+    return {
+        model: obj.model,
+        engine: engines.filter(e => e.power >= obj.power)[0],
+        carriage: Object.assign(carriage.filter(e => e.type === obj.carriage)[0], {color: obj.color}),
+        wheels: new Array(4).fill(obj.wheelsize % 2 === 0 ? obj.wheelsize - 1 : obj.wheelsize)
+    };
+}
+
+function car_factoryV2(obj){
+    let engines = [{ power: 90, volume: 1800 }, { power: 120, volume: 2400 }, { power: 200, volume: 3500 }];
+    let carriages = [{type: 'hatchback', color: obj.color}, {type: 'coupe', color: obj.color}];
+    let wheelsize = obj.wheelsize % 2 == 1 ? obj.wheelsize : obj.wheelsize - 1;
 
     return{
-        model: car.model,
-        engine: engines.filter(e => e.power >= car.power)[0],
-        carriage: carriages.filter(c => c.type == car.carriage)[0],
-        wheels: [wheelsize,wheelsize,wheelsize,wheelsize]
+        model: obj.model,
+        engine: engines.filter(e => e.power >= obj.power)[0],
+        carriage: carriages.filter(c => c.type === obj.carriage)[0],
+        wheels: [wheelsize, wheelsize, wheelsize, wheelsize]
+    }
+}
+
+function car_factoryV3({ model, power, color, carriage, wheelsize }) {
+    const storageEngines = [{ power: 90, volume: 1800 }, { power: 120, volume: 2400 }, { power: 200, volume: 3500 }];
+    const getWheels = s => {
+        const arr = [];
+        arr.length = 4;
+
+        return s % 2 === 0 ? arr.fill(s - 1) : arr.fill(s);
+    };
+
+    return {
+        model,
+        engine: storageEngines.reduce((a, v) => Math.abs(a.power - power) < Math.abs(v.power - power) ? a : v),
+        carriage: { type: carriage, color },
+        wheels: getWheels(wheelsize),
+    }
+}
+
+function car_factoryV4(obj){
+    const engines = [{ power: 90, volume: 1800 }, { power: 120, volume: 2400 }, { power: 200, volume: 3500 }];
+
+    return {
+        model: obj.model,
+        engine: engines.filter(e => e.power >= obj.power)[0],
+        carriage: {type: obj.carriage, color: obj.color},
+        wheels: new Array(4).fill(obj.wheelsize % 2 === 0 ? obj.wheelsize - 1 : obj.wheelsize)
     }
 }
