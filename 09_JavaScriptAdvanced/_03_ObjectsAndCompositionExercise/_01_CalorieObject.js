ee7f737d390952e3e6788bf4d52e14d78d823992
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-13 17:55:14 +0300

    01

--- 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_01_CalorieObject.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_01_CalorieObject.js
@@ -1,11 +1,32 @@
-function calorieObject(input){
-    const calories = {};
+function calorie_objectV1(arr){
+    let products = {};
 
-    for(let i = 0; i < input.length -1; i = i+2){
-        let first = input[i];
-        let second = input[i+1]
-        calories[input[i]] = Number(input[i +1]);
+    for (let i = 0; i < arr.length; i += 2) {
+        products[arr[i]] = Number(arr[i + 1]);
     }
 
-    console.log(calories);
+    return products;
+}
+
+function calorie_objectV2(input){
+    const calories = {};
+
+    for (let i = 0; i < input.length - 1; i += 2) {
+        let item = input[i];
+        let amount = input[i + 1];
+
+        calories[item] = Number(amount);
+    }
+
+    return calories;
+}
+
+function calorie_objectV3(arr){
+    return arr.reduce((a, v, i) => {
+        if(i % 2 === 0){
+            a[v] = Number(arr[i + 1]);
+        }
+
+        return a;
+    }, {});
 }
