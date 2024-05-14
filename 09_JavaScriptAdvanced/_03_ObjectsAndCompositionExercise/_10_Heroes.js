943ebd9ee149cba46d7787d5d8988f85dd1ad1b7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-14 16:38:40 +0300

    10

--- 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_10_Heroes.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_10_Heroes.js
@@ -1,19 +1,19 @@
-function solve() {
-    function hero(name, rss, attack, message) {
+function heroesV1(){
+    function Hero(name, rss, attack, message) {
         return {
             name,
             message,
             health: 100,
             [rss]: 100,
             [attack]: function (msg = "") {
-                console.log(this.message + msg)
-                this[rss] -= 1
+                console.log(this.message + msg);
+                this[rss] -= 1;
             },
         }
     }
 
     return {
-        mage: n => new hero(n, "mana", `cast`, `${n} cast `),
-        fighter: n => new hero(n, "stamina", `fight`, `${n} slashes at the foe!`),
+        mage: n => new Hero(n, "mana", `cast`, `${n} cast `),
+        fighter: n => new Hero(n, "stamina", `fight`, `${n} slashes at the foe!`),
     }
 }
