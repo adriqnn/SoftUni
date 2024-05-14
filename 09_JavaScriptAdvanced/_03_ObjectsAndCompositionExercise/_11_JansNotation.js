71d295f2b5d95517db09d198a418203720063e29
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-14 16:41:13 +0300

    11

--- 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_11_JansNotation.js
+++ 09_JavaScriptAdvanced/_03_ObjectsAndCompositionExercise/_11_JansNotation.js
@@ -1,8 +1,43 @@
-function solve(arr) {
-    let result = []
-    let workArr = arr.includes("error")
-        ? arr.slice(0, arr.indexOf("error"))
-        : arr.slice()
+function jans_notationV1(array){
+    let operands = [];
+    let length = array.length;
+    let result = ``;
+
+    let operations = {
+        "+": (a, b) => a + b,
+        "-": (a, b) => a - b,
+        "*": (a, b) => a * b,
+        "/": (a, b) => a / b
+    }
+
+    function isNumerical(value) {
+        return !isNaN(value) && typeof value === 'number';
+    }
+
+    for (let i = 0; i < length; i++) {
+        let current = array[i];
+
+        if(isNumerical(current)){
+            operands.push(current);
+        }else{
+            if(operands.length > 1){
+                let second = operands.pop();
+                let first = operands.pop();
+
+                operands.push(operations[current](first, second));
+            }else{
+                result += `Error: not enough operands!`;
+                break;
+            }
+        }
+    }
+
+    return result === `Error: not enough operands!` ? result : operands.length === 1 ? operands[0] : `Error: too many operands!`;
+}
+
+function jans_notationV2(arr){
+    let result = [];
+    let workArr = arr.includes("error") ? arr.slice(0, arr.indexOf("error")) : arr.slice();
     const operators = {
         "+": (x, y) => x + y,
         "-": (x, y) => y - x,
@@ -11,21 +46,23 @@ function solve(arr) {
     }
 
     const updateArr = (arr, o) => {
-        const temp = arr.slice()
-        temp.push(operators[o](temp.pop(), temp.pop()))
-        return temp
+        const temp = arr.slice();
+        temp.push(operators[o](temp.pop(), temp.pop()));
+
+        return temp;
     }
 
     for (let i = 0; i < workArr.length; i += 1) {
         if (typeof workArr[i] === "number") {
-            result.push(workArr[i])
+            result.push(workArr[i]);
         } else {
             if (result.length < 2) {
-                return `Error: not enough operands!`
+                return `Error: not enough operands!`;
             }
-            result = updateArr(result, workArr[i])
+
+            result = updateArr(result, workArr[i]);
         }
     }
 
-    return result.length === 1 ? result[0] : `Error: too many operands!`
+    return result.length === 1 ? result[0] : `Error: too many operands!`;
 }
