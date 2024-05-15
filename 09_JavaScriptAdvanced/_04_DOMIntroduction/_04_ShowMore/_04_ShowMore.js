5f365b3945ab11388083ebc0c8db9d7ed857baa9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-15 06:19:03 +0300

    04

---
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_04_ShowMore/_04_ShowMore.js
@@ -0,0 +1,9 @@
+function show_moreV1(){
+    document.getElementById('text').style.display = 'inline';
+    document.getElementById('more').style.display = 'none';
+}
+
+function show_moreV2() {
+    document.getElementById("more").style.display = "none";
+    document.getElementById("text").style.display = "inline";
+}
