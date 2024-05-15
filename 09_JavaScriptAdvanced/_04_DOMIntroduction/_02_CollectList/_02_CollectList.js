99c99de38942578bc3e1872870fa6cae42b2b7a9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-15 06:18:52 +0300

    02

---
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_02_CollectList/_02_CollectList.js
@@ -0,0 +1,14 @@
+function extract_textV1(){
+    const items = Array.from(document.querySelectorAll('li'));
+    const result = items.map(e => e.textContent).join('\n');
+    document.getElementById('result').value = result;
+}
+
+function extract_textV2(){
+    const html = {
+        list: () => document.getElementById("items"),
+        result: () => document.getElementById("result"),
+    }
+
+    html.result().innerHTML = Array.from(html.list().children).map(x => x.innerHTML).join("\n");
+}
