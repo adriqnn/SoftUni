f9f017107ac91a816b2c728e2129faea7ccbd7c5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-16 18:48:38 +0300

    01

--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_01_EditElement/_01_EditElement.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_01_EditElement/_01_EditElement.js
@@ -8,3 +8,8 @@ function edit_elementV2(element, match, replacer) {
     const r = new RegExp(match, "g")
     element.textContent = element.textContent.replace(r, replacer);
 }
+
+function edit_elementV3(htmlElement, match, replacerString){
+    const regex = new RegExp(match, "g");
+    htmlElement.textContent = htmlElement.textContent.replace(regex, replacerString);
+}
