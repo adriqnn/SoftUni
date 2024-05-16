3c48ade0d578e4784d35c8cf905fd5e39199a493
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-16 05:11:21 +0300

    03

---
+++ 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_03_Accordion/_03_Accordion.js
@@ -0,0 +1,47 @@
+function accordionV1(){
+    if(document.getElementById('extra').style.display == 'block'){
+        document.getElementById('extra').style.display = 'none';
+        document.getElementsByClassName('button')[0].textContent = 'More';
+    }else{
+        document.getElementById('extra').style.display = 'block';
+        document.getElementsByClassName('button')[0].textContent = 'Less';
+    }
+}
+
+function accordionV2(){
+    let textElement = document.getElementById('extra');
+    let buttonElement = document.getElementsByClassName('button')[0]; 
+
+    if(buttonElement.textContent == 'More'){
+        buttonElement.textContent = 'Less';
+        textElement.style.display = 'block';
+    }else{
+        buttonElement.textContent = 'More';
+        textElement.style.display = 'none';
+    }
+}
+
+function accordionV3(){
+    if(document.getElementById('extra').style.display == 'inline'){
+        document.getElementById('extra').style.display = '';
+        document.getElementsByClassName('button')[0].textContent = 'MORE';
+    }else{
+        document.getElementById('extra').style.display = 'inline';
+        document.getElementsByClassName('button')[0].textContent = 'LESS';
+    }
+}
+
+function accordionV4(){
+    const html = {
+        button: document.getElementsByClassName("button")[0],
+        content: document.getElementById("extra"),
+    }
+
+    if (html.button.innerHTML === "More") {
+        html.button.innerHTML = "Less";
+        html.content.style.display = "block";
+    } else {
+        html.button.innerHTML = "More";
+        html.content.style.display = "none";
+    }
+}
