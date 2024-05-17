4ccd8a1ea322a181ea67c83d5047cdc2a1db75fb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-17 05:23:43 +0300

    04

--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_04_ShowMore/_04_ShowMore.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_04_ShowMore/_04_ShowMore.js
@@ -7,3 +7,13 @@ function show_moreV2() {
     document.getElementById("more").style.display = "none";
     document.getElementById("text").style.display = "inline";
 }
+
+function show_moreV3(){
+    const html = {
+        moreStyle: document.getElementById("more"),
+        textStyle: document.getElementById("text")
+    }
+
+    html.moreStyle.style.display = "none";
+    html.textStyle.style.display = "inline";
+}
