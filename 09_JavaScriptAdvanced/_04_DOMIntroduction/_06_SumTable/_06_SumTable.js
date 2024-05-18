6cdb8d44c3285c43e666053e4e63601236dfcfb3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-18 21:02:52 +0300

    06

--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_06_SumTable/_06_SumTable.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_06_SumTable/_06_SumTable.js
@@ -10,8 +10,17 @@ function sum_tableV1(){
     document.getElementById(`sum`).textContent = sum;
 }
 
-function sum_tableV2() {
+function sum_tableV2(){
     let dataCells = Array.from(document.getElementsByTagName("td"));
 
     document.getElementById("sum").innerHTML = dataCells.slice(0, dataCells.length - 1).reduce((a, v) => a + (Number(v.innerHTML) || 0), 0);
+}
+
+function sum_tableV3(){
+    const html = {
+        rows: Array.from(document.getElementsByTagName("td")),
+        sum: document.getElementById("sum")
+    }
+
+    html.sum.textContent = html.rows.slice(0, html.rows.length - 1).filter((e, i) => i % 2 !== 0).reduce((a, v) => a + Number(v.textContent), 0);
 }
