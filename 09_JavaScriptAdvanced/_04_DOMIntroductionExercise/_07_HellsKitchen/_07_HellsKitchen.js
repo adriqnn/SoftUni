431e62828aacd5fbac4ca79e8424ec2f5ad30661
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-19 02:44:36 +0300

    07

--- 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_07_HellsKitchen/_07_HellsKitchen.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_07_HellsKitchen/_07_HellsKitchen.js
@@ -88,3 +88,38 @@ function hells_kitchenV2(){
 
     document.getElementById("btnSend").addEventListener("click", () => displayResult(getBest(deserialize(html.inputField.value))));
 }
+
+function hells_kitchenV3(){
+    const html = {
+        input: document.querySelector("#inputs textarea"),
+        bestRestaurant: document.querySelector("#bestRestaurant p"),
+        bestWorkers: document.querySelector("#workers p"),
+        button: document.getElementById("btnSend")
+    }
+
+    const getBest = data => Object.entries(data).sort((x, y) => getAverage(y[1]) - getAverage(x[1]))[0];
+
+    const getAverage = workersData => workersData.reduce((a, v) => a + v[1], 0) / workersData.length;
+
+    function deserialize(data) {
+        const getWorkers = data => data.split(", ").map(x => x.split(" ").map(y => (isNaN(y) ? y : Number(y))));
+
+        return JSON.parse(data).map(x => x.split(" - "))
+            .reduce((a, v) => {
+                const [name, workers] = v;
+                a[name] = a[name] ? [...a[name], ...getWorkers(workers)] : getWorkers(workers);
+
+                return a;
+            }, {});
+    }
+
+    function displayResult(data) {
+        let [name, workers] = data;
+        workers = workers.sort((x, y) => y[1] - x[1]);
+
+        html.bestRestaurant.innerHTML = `Name: ${name} Average Salary: ${getAverage(workers).toFixed(2)} Best Salary: ${workers[0][1].toFixed(2)}`;
+        html.bestWorkers.innerHTML = `${workers.map(x => `Name: ${x[0]} With Salary: ${x[1]}`).join(" ")}`;
+    }
+
+    html.button.addEventListener("click", () => displayResult(getBest(deserialize(html.input.value))));
+}
