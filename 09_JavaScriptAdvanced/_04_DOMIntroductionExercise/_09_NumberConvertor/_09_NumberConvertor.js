ca749bad67ded1c8dcb486d98832f58f36a9c713
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-19 06:16:41 +0300

    09

--- 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_09_NumberConvertor/_09_NumberConvertor.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_09_NumberConvertor/_09_NumberConvertor.js
@@ -1,4 +1,4 @@
-function number_convertorV1() {
+function number_convertorV1(){
     const html = {
         numberField: document.getElementById("input"),
         convertTo: document.getElementById("selectMenuTo"),
@@ -28,4 +28,39 @@ function number_convertorV1() {
     }
 
     html.button.addEventListener("click", () => print(convert(html.convertTo.value, html.numberField.value)));
+}
+
+function number_convertorV2(){
+    const html = {
+        numberField: document.getElementById("input"),
+        convertTo: document.getElementById("selectMenuTo"),
+        out: document.getElementById("result"),
+        button: document.querySelector("#container > button")
+    }
+
+    const createOptions = (arr) => arr.map(x => {
+        const option = document.createElement("option");
+        x = x.toLocaleLowerCase();
+        option.value = x;
+        option.text = `${x.charAt(0).toLocaleUpperCase()}${x.slice(1)}`;
+
+        return option;
+    });
+
+    createOptions(["BINARY", "HEXADECIMAL", "SAME"]).forEach(x => html.convertTo.add(x));
+
+    const showAnswer = (n) => { html.out.value = n };
+    const convert = (format, n) => {
+        n = Number(n) || 0;
+
+        const formats = {
+            binary: (n) => (n >>> 0).toString(2),
+            hexadecimal: (n) => n.toString(16).toLocaleUpperCase(),
+            same: (n) => n
+        }
+
+        return formats[format](n);
+    }
+
+    html.button.addEventListener("click", () => showAnswer(convert(html.convertTo.value, html.numberField.value)));
 }
