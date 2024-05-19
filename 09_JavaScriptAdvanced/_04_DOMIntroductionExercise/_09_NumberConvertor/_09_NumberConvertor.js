9576389821b8193bff4b202908a0e1fb610fbaa5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-19 06:23:53 +0300

    09

--- 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_09_NumberConvertor/_09_NumberConvertor.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroductionExercise/_09_NumberConvertor/_09_NumberConvertor.js
@@ -64,3 +64,40 @@ function number_convertorV2(){
 
     html.button.addEventListener("click", () => showAnswer(convert(html.convertTo.value, html.numberField.value)));
 }
+
+function number_convertorV3(){
+    const html = {
+        numberField: document.getElementById("input"),
+        convertTo: document.getElementById("selectMenuTo"),
+        out: document.getElementById("result"),
+        button: document.querySelector("#container > button")
+    }
+
+    const createOptions = (arr) => arr.map(x => {
+        const option = document.createElement("option");
+        x = x.toLocaleLowerCase();
+        option.value = x;
+        option.text = `${x.charAt(0).toLocaleUpperCase()}${x.slice(1)}`;
+
+        return option;
+    });
+
+    createOptions(["BINARY", "HEXADECIMAL", "SAME"]).forEach(x => html.convertTo.add(x));
+
+    const showAnswer = (n) => { html.out.value = n };
+    const convert = () => {
+        const format = html.convertTo.value;
+        const n = Number(html.numberField.value) || 0;
+
+        const formats = {
+            binary: (n) => n.toString(2),
+            hexadecimal: (n) => n.toString(16).toLocaleUpperCase(),
+            same: (n) => n
+        }
+
+        showAnswer(formats[format](n));
+
+    }
+
+    html.button.addEventListener("click", convert);
+}
