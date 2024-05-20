6390acd68fea048665c08e14883d4e0609b82192
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-20 06:15:39 +0300

    01

---
+++ 09_JavaScriptAdvanced/_05_DOMManipulationAndEvents/_01_ListOfItems/_01_ListOfItems.js
@@ -0,0 +1,50 @@
+function list_of_itemsV1(){
+    //select input field and read its value;
+    const content = document.getElementById('newItemText').value;
+    //create <li> element;
+    const liElement = document.createElement('li');
+    //assign input value to element text content;
+    liElement.textContent = content;
+    //select <ul> and append new element;
+    const ulElement = document.getElementById('items')
+    ulElement.appendChild(liElement);
+    //select input field and clear contents(value);
+    document.getElementById('newItemText').value = '';
+}
+
+function list_of_itemsV2(){
+    const input = document.getElementById('newItemText');
+    const liElement = document.createElement('li');
+    liElement.textContent = input.value;
+    document.getElementById('items').appendChild(liElement);
+    input.value = '';
+}
+
+function list_of_itemsV3(){
+    const html = {
+        input: document.getElementById("newItemText"),
+        list: document.getElementById("items")
+    }
+
+    const liElement = document.createElement("li");
+    liElement.textContent = html.input.value;
+    html.input.value = "";
+
+    html.list.appendChild(liElement);
+}
+
+function list_of_itemsV4() {
+    const data = {
+        valueToAdd: document.getElementById("newItemText").value,
+        list: document.getElementById("items"),
+    }
+
+    function eFactory(tag, content) {
+        const temp = document.createElement(tag);
+        temp.innerHTML = content;
+
+        return temp;
+    }
+
+    data.list.appendChild(eFactory("li", data.valueToAdd));
+}
