4b4929420d2c3d2f7db07ff5bfbbae7a82a90d2d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-20 03:24:12 +0300

    02

--- 09_JavaScriptAdvanced/_04_DOMIntroduction/_02_CollectList/_02_CollectList.js
+++ 09_JavaScriptAdvanced/_04_DOMIntroduction/_02_CollectList/_02_CollectList.js
@@ -1,10 +1,10 @@
-function extract_textV1(){
+function collect_listV1(){
     const items = Array.from(document.querySelectorAll('li'));
     const result = items.map(e => e.textContent).join('\n');
     document.getElementById('result').value = result;
 }
 
-function extract_textV2(){
+function collect_listV2(){
     const html = {
         list: () => document.getElementById("items"),
         result: () => document.getElementById("result"),
@@ -13,7 +13,7 @@ function extract_textV2(){
     html.result().innerHTML = Array.from(html.list().children).map(x => x.innerHTML).join("\n");
 }
 
-function extract_textV3(){
+function collect_listV3(){
     const html = {
         list: Array.from(document.getElementById("items").children),
         textArea: document.getElementById("result")
