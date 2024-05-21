b2eb61db43e808506c68f8baef2222d56722064e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-21 04:17:56 +0300

    07

---
+++ 09_JavaScriptAdvanced/_05_DOMManipulationsAndEvents/_07_ShoppingCart/_07_ShoppingCart.js
@@ -0,0 +1,79 @@
+function shopping_cartV1(){
+    const html = {
+        products: document.getElementsByClassName("product"),
+        output: document.querySelector("body > div > textarea"),
+        checkout: document.getElementsByClassName("checkout")[0],
+        buttons: document.getElementsByTagName("button")
+    }
+
+    const state = {
+        names: new Set(),
+        price: 0
+    }
+
+    function getSectionInfo(e){
+        return {
+            name: e.getElementsByClassName("product-title")[0].innerHTML,
+            button: e.getElementsByClassName("add-product")[0],
+            price: Number(e.getElementsByClassName("product-line-price")[0].innerHTML) || 0
+        }
+    }
+
+    function addToCart({name, price}){
+        state.names.add(name);
+        state.price += price;
+
+        return {name, price};
+    }
+
+    function displayToCart({ name, price }){
+        html.output.innerHTML += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+    }
+
+    Array.from(html.products).map(getSectionInfo).forEach(x => x.button.addEventListener("click", () => displayToCart(addToCart(x))));
+
+    html.checkout.addEventListener("click", () => {
+        html.output.innerHTML += `You bought ${[...state.names.keys()].join(", ")} for ${state.price.toFixed(2)}.`;
+        Array.from(html.buttons).map(x => (x.disabled = "true"));
+    });
+}
+
+function shopping_cartV2(){
+    const html = {
+        products: document.getElementsByClassName("product"),
+        output: document.querySelector("body > div > textarea"),
+        checkout: document.getElementsByClassName("checkout")[0],
+        buttons: document.getElementsByTagName("button")
+    }
+
+    const state = {
+        names: new Set(),
+        price: 0
+    }
+
+    function getSectionInfo(e) {
+        return {
+            name: e.getElementsByClassName("product-title")[0].textContent,
+            button: e.getElementsByClassName("add-product")[0],
+            price: Number(e.getElementsByClassName("product-line-price")[0].textContent) || 0,
+        }
+    }
+
+    function addToCart({name, price}) {
+        state.names.add(name);
+        state.price += price;
+
+        return {name, price};
+    }
+
+    function displayToCart({ name, price }) {
+        html.output.textContent += `Added ${name} for ${price.toFixed(2)} to the cart.\n`;
+    }
+
+    Array.from(html.products).map(getSectionInfo).forEach(x => x.button.addEventListener("click", () => displayToCart(addToCart(x))));
+
+    html.checkout.addEventListener("click", () => {
+        html.output.textContent += `You bought ${[...state.names.keys()].join(", ")} for ${state.price.toFixed(2)}.`;
+        Array.from(html.buttons).map(x => (x.disabled = "true"));
+    });
+}
