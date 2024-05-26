6b9b75c06debe7494fbf5850774d7040524982a1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-26 22:35:08 +0300

    01

--- 09_JavaScriptAdvanced/_06_AdvancedFunctions/_01_AreaAndVolumeCalculator.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctions/_01_AreaAndVolumeCalculator.js
@@ -1,13 +1,26 @@
-function areaVol(areaIn,volIn,input){
+function area_and_volume_calculatorV1(fArea, fVol, input){
     const data = JSON.parse(input);
-
     const result = [];
+
     for (const cube of data) {
-        const currentResult = {
-            area: areaIn.call(cube),
-            volume: volIn.call(cube)
+        const current = {
+            area: fArea.call(cube),
+            volume: fVol.call(cube)
         }
-        result.push(currentResult);
+
+        result.push(current);
     }
+
     return result;
+}
+
+function area_and_volume_calculatorV2(fArea, fVol, input){
+    return JSON.parse(input).reduce((a, v, i, obj) => {
+        a.push({
+            area: fArea.call(v),
+            volume: fVol.call(v)
+        });
+
+        return a;
+    }, []);
 }
