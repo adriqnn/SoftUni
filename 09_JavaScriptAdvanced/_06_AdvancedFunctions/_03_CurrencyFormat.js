748d99636ff6c18f6634b169783c836ed6320c5c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-26 23:54:19 +0300

    03

--- 09_JavaScriptAdvanced/_06_AdvancedFunctions/_03_CurrencyFormat.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctions/_03_CurrencyFormat.js
@@ -1,3 +1,7 @@
-function foo(a, b, c, d) {
-    return d.bind(undefined, a, b, c)
+function currency_formatV1(separator, symbol, symbolFirst, formatter){
+    return formatter.bind(undefined, separator, symbol, symbolFirst);
+}
+
+function currency_formatV2(a, b, c, d){
+    return d.bind(undefined, a, b, c);
 }
