41457586e0d9a750554d7c27b5d01a8b5609f59f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-27 01:09:20 +0300

    03

--- 09_JavaScriptAdvanced/_06_AdvancedFunctions/_03_CurrencyFormat.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctions/_03_CurrencyFormat.js
@@ -5,3 +5,7 @@ function currency_formatV1(separator, symbol, symbolFirst, formatter){
 function currency_formatV2(a, b, c, d){
     return d.bind(undefined, a, b, c);
 }
+
+function currency_formatV3(separator, symbol, symbolFirst, formatter){
+    return (value) => formatter(separator, symbol, symbolFirst, value);
+}
