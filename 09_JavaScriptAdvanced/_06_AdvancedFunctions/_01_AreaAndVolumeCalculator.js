be455135a03bf8fbe0de61fce4b332dde996b7a6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-26 23:21:48 +0300

    01

--- 09_JavaScriptAdvanced/_06_AdvancedFunctions/_01_AreaAndVolumeCalculator.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctions/_01_AreaAndVolumeCalculator.js
@@ -24,3 +24,16 @@ function area_and_volume_calculatorV2(fArea, fVol, input){
         return a;
     }, []);
 }
+
+function area_and_volume_calculatorV3(fArea, fVol, input){
+    let objects = JSON.parse(input);
+
+    function calc(obj){
+        let areaObj = Math.abs(fArea.call(obj));
+        let volumeObj = Math.abs(fVol.call(obj));
+
+        return {area: areaObj, volume: volumeObj};
+    }
+
+    return objects.map(calc);
+}
