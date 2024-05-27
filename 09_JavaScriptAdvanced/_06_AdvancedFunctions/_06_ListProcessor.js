668ae8416553799689609c510154d861a91b82aa
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-27 03:54:43 +0300

    06

--- 09_JavaScriptAdvanced/_06_AdvancedFunctions/_06_ListProcessor.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctions/_06_ListProcessor.js
@@ -1,15 +1,14 @@
-function foo(input) {
-    let arr = []
+function list_processorV1(input){
+    let arr = [];
 
     let obj = {
-        add: str => arr.push(str),
-        remove: str => (arr = arr.filter(x => x !== str)),
-        print: () => console.log(arr.join(",")),
+        add: (str) => arr.push(str),
+        remove: (str) => arr = arr.filter(x => x !== str),
+        print: () => console.log(arr.join(","))
     }
 
-    input.forEach(x => {
-        const [command, value] = x.split(" ")
-
-        obj[command](value)
-    })
+    input.forEach(e => {
+        const [command, value] = e.split(" ");
+        obj[command](value);
+    });
 }
