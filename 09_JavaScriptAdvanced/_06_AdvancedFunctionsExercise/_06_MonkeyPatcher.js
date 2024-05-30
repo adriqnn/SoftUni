a2a9b92cd2c0a11e4b40f9ebca9f041d0b2a2507
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-31 01:47:34 +0300

    06

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_06_MonkeyPatcher.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_06_MonkeyPatcher.js
@@ -1,33 +1,45 @@
-function result (command) {
+function monkey_patcherV1(command){
     const actions = {
-        upvote: x => (x.upvotes += 1), downvote: x => (x.downvotes += 1), score: x => getScore(x),
+        upvote: (x) => {x.upvotes += 1},
+        downvote: (x) => {x.downvotes += 1},
+        score: (x) => getScore(x)
     }
 
-    function isObfuscable (votes) {
-        return votes > 50
+    function shouldBeObfuscated(votes){
+        return votes > 50;
     }
 
-    function getObfNum (u, d) {
-        return Math.ceil(0.25 * Math.max(u, d))
+    function getObfNumber(u, d){
+        return Math.ceil(0.25 * Math.max(u, d));
     }
 
-    function calcRating (u, d) {
+    function calculateRating(u, d){
+        if(u + d < 10){
+            return 'new';
+        }
 
-        if (u + d < 10) return 'new'
-        if (u > (u + d) * 0.66) return 'hot'
-        if (u - d >= 0 && (u > 100 || d > 100)) return 'controversial'
-        if (u - d < 0) return 'unpopular'
+        if(u > (u + d) * 0.66){
+            return 'hot';
+        }
 
-        return 'new'
+        if(u - d >= 0 && (u > 100 || d > 100)){
+            return 'controversial';
+        }
+
+        if(u - d < 0){
+            return 'unpopular';
+        }
+
+        return 'new';
     }
 
-    function getScore ({upvotes, downvotes}) {
-        const obfNum = isObfuscable(upvotes + downvotes) ? getObfNum(upvotes, downvotes) : 0
-        const [rUps, rDowns] = [upvotes + obfNum, downvotes + obfNum]
-        const balance = rUps - rDowns
+    function getScore({upvotes, downvotes}){
+        const obfNumber = shouldBeObfuscated(upvotes + downvotes) ? getObfNumber(upvotes, downvotes) : 0;
+        const [rUpVotes, rDownVotes] = [upvotes + obfNumber, downvotes + obfNumber];
+        const balance = rUpVotes - rDownVotes;
 
-        return [rUps, rDowns, balance, calcRating(upvotes, downvotes)]
+        return [rUpVotes, rDownVotes, balance, calculateRating(upvotes, downvotes)];
     }
 
-    return actions[command](this)
+    return actions[command](this);
 }
