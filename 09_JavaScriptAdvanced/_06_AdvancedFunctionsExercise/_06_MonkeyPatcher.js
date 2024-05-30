b17765dc07e336fd257dfb3e79448e6c67a65ed0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-31 02:27:33 +0300

    06

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_06_MonkeyPatcher.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_06_MonkeyPatcher.js
@@ -43,3 +43,36 @@ function monkey_patcherV1(command){
 
     return actions[command](this);
 }
+
+function monkey_patcherV2(command){
+    const actions = {
+        upvote: x => (x.upvotes += 1), downvote: x => (x.downvotes += 1), score: x => getScore(x)
+    };
+
+    function shouldBeObfuscated(votes){
+        return votes > 50;
+    }
+
+    function getObfNumber(u, d){
+        return Math.ceil(0.25 * Math.max(u, d));
+    }
+
+    function calcRating(u, d){
+        if (u + d < 10) return 'new';
+        if (u > (u + d) * 0.66) return 'hot';
+        if (u - d >= 0 && (u > 100 || d > 100)) return 'controversial';
+        if (u - d < 0) return 'unpopular';
+
+        return 'new';
+    }
+
+    function getScore ({upvotes, downvotes}){
+        const obfNum = shouldBeObfuscated(upvotes + downvotes) ? getObfNumber(upvotes, downvotes) : 0;
+        const [rUps, rDowns] = [upvotes + obfNum, downvotes + obfNum];
+        const balance = rUps - rDowns;
+
+        return [rUps, rDowns, balance, calcRating(upvotes, downvotes)];
+    }
+
+    return actions[command](this);
+}
