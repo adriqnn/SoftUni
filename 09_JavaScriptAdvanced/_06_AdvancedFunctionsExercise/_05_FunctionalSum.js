11a8ae2376a27d20361c1dc75adb4f437b8b0ec8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-31 00:47:13 +0300

    05

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_05_FunctionalSum.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_05_FunctionalSum.js
@@ -11,3 +11,16 @@ function functional_sumV1(n){
 
     return inner;
 }
+
+function functional_sumV2(n){
+    let temp = 0;
+
+    function recursive(e){
+        temp += e;
+
+        return recursive;
+    }
+
+    recursive.toString = () => temp;
+    return recursive(n);
+}
