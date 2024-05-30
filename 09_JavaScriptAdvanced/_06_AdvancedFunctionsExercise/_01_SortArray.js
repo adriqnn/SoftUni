33862d6c95cd9884ffd955ddd51d1ba89045ce3e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-30 22:28:15 +0300

    01

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_01_SortArray.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_01_SortArray.js
@@ -1,16 +1,20 @@
-function sortArray(arr, type) {
-    if(type == 'asc'){
-        arr.sort((a,b) => a-b);
+function sort_arrayV1(arr, type){
+    if(type === 'asc'){
+        arr.sort((a, b) => a - b);
     }else{
-        arr.sort((a,b) => b-a);
+        arr.sort((a, b) => b - a);
     }
+
     return arr;
 }
-function sortv2(arr,type){
+
+function sort_arrayV2(arr,type){
     const methods = {
-        asc: (a,b) => a-b,
-        desc: (a,b) => b-a
+        asc: (a, b) => a - b,
+        desc: (a, b) => b - a
     }
+
     arr.sort(methods[type]);
+
     return arr;
 }
