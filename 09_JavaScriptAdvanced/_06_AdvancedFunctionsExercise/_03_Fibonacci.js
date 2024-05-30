e6990211408fa27a3aac58ce17d3f7d4df60ef8f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-30 22:54:16 +0300

    03

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_03_Fibonacci.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_03_Fibonacci.js
@@ -1,23 +1,13 @@
-function getFibonator(){
+function fibonacciV1(){
     let prev = 0;
     let curr = 1;
-    
+
     return function(){
         const next = prev + curr;
+
         prev = curr;
         curr = next;
+
         return prev;
     }
-}
-
-function getFibonatorv2(){
-    return (function() {
-        const next = this.prev + this.curr;
-        this.prev = this.curr;
-        this.curr = next;
-        return this.prv;
-    }).bing({
-        prev: 0,
-        curr: 1
-    });
 }
