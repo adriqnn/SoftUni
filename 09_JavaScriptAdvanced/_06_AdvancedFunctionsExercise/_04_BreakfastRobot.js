85fcf22ab2022705f73ef8001092f66653cfb2bc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-30 23:04:13 +0300

    04

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_04_BreakfastRobot.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_04_BreakfastRobot.js
@@ -1,36 +1,19 @@
-function solve(){
+function breakfast_robotV1(){
     const recipes = {
-        apple: {
-            carbohydrate: 1,
-            flavour: 2
-        },
-        lemonade: {
-            carbohydrate: 10,
-            flavour: 20
-        },
-        burger: {
-            carbohydrate: 5,
-            fat: 7,
-            flavour: 3
-        },
-        eggs: {
-            protein: 5,
-            fat: 1,
-            flavour: 1
-        },
-        turkey: {
-            protein: 10,
-            carbohydrate: 10,
-            fat: 10,
-            flavour: 10
-        }
+        apple: {carbohydrate: 1, flavour: 2},
+        lemonade: {carbohydrate: 10, flavour: 20},
+        burger: {carbohydrate: 5, fat: 7, flavour: 3},
+        eggs: {protein: 5, fat: 1, flavour: 1},
+        turkey: {protein: 10, carbohydrate: 10, fat: 10, flavour: 10}
     };
+
     const stock = {
         protein: 0,
         carbohydrate: 0,
         fat: 0,
         flavour: 0
     };
+
     const commands = {
         restock,
         prepare,
@@ -40,31 +23,33 @@ function solve(){
     return manager;
 
     function manager(line){
-        const [command,param,qty] = line.split(' ');
-        return commands[command](param,qty);
-    };
+        const [command, param, qty] = line.split(' ');
 
-    function restock(type,qty){
+        return commands[command](param, qty);
+    }
+
+    function restock(type, qty){
         stock[type] += Number(qty);
         return 'Success';
     }
 
-    function prepare(recipeAsString,qty){
+    function prepare(recipeAsString, qty){
         qty = Number(qty);
         const recipe = Object.entries(recipes[recipeAsString]);
         recipe.forEach(ingredient => ingredient[1] *= qty);
-        for (const [ingredient,required] of recipe) {
+
+        for (const [ingredient, required] of recipe) {
             if(stock[ingredient] < required){
-                return `Error: not enough ${ingredient} in stock`
+                return `Error: not enough ${ingredient} in stock`;
             }
         }
-        recipe.forEach(([ingredient,required]) => stock[ingredient] -= required);
+
+        recipe.forEach(([ingredient, required]) => stock[ingredient] -= required);
+
         return 'Success';
     }
-    
+
     function report(){
         return `protein=${stock.protein} carbohydrate=${stock.carbohydrate} fat=${stock.fat} flavour=${stock.flavour}`;
     }
-
-
 }
