c5db2673b0011defc5d59a44eda9987a8c1288dd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-30 22:56:38 +0300

    03

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_03_Fibonacci.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_03_Fibonacci.js
@@ -25,3 +25,14 @@ function fibonacciV2(){
         curr: 1
     });
 }
+
+function fibonacciV3() {
+    let nums = [0, 0];
+
+    return () => {
+        const result = nums[0] + nums[1];
+        nums = [nums[1], nums[1] + nums[0] || 1];
+        
+        return result || 1;
+    }
+}
