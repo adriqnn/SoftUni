16a0f652a167d2fbd76ecf926ee5cac243da0421
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-05-31 04:36:45 +0300

    07

--- 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_07_SimpleCalculator/app.js
+++ 09_JavaScriptAdvanced/_06_AdvancedFunctionsExercise/_07_SimpleCalculator/app.js
@@ -24,21 +24,26 @@ function simple_calculatorV1(){
     }
 }
 
-function calculator2(){
+function simple_calculatorV2(){
     function init(s1,s2,rs){
         this.num1 = document.querySelector(s1);
         this.num2 = document.querySelector(s2);
         this.result = document.querySelector(rs);
     }
+    
     function add(){
-        this.result.value = NUmber(this.num1.value) + Number(this.num2.value);
+        this.result.value = Number(this.num1.value) + Number(this.num2.value);
     }
     function subtract(){
         this.result.value = Number(this.num1.value) - Number(this.num2.value);
     }
+    
     return {
         init,
         add,
         subtract
     }
 }
+
+const calculate = simple_calculatorV1();
+calculate.init('#num1', '#num2', '#result');
