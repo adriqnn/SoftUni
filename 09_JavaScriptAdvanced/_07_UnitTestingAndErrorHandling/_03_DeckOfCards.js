0836a155d6c2a71b80ee36377736489a2dd4c99e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-01 23:57:05 +0300

    03

--- 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_03_DeckOfCards.js
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_03_DeckOfCards.js
@@ -1,4 +1,4 @@
-function deck(cards){
+function deck_of_cardsV1(cards){
     let result = [];
 
     for (let cardAsString of cards) {
@@ -12,17 +12,22 @@ function deck(cards){
             result = ['Invalid card: ' + cardAsString];
         }
     }
+
     console.log(result.join(' '));
-    function createCard(face,suit){
+
+    function createCard(face, suit){
         const faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
         const suits = {'S': '\u2660','H': '\u2665','D': '\u2666','C': '\u2663'
         };
-        if (faces.includes(face) == false){
+
+        if (faces.includes(face) === false){
             throw new Error('Invalid face: ' + face);
-        };
-        if(suits[suit] == undefined){
+        }
+
+        if(suits[suit] === undefined){
             throw new Error('Invalid suit: ' + suit);
-        };
+        }
+
         const result = {
             face,
             suit: suits[suit],
@@ -30,6 +35,7 @@ function deck(cards){
                 return this.face + this.suit;
             }
         };
+
         return result;
     }
 }
