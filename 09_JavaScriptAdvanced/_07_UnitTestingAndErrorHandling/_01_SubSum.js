d68b560af64ffb7cc5713f616ccdb91c888d9fc3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-01 06:17:15 +0300

    01

--- 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_01_SubSum.js
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_01_SubSum.js
@@ -19,3 +19,14 @@ function sub_sumV1(arr, start, end){
 
     return sum;
 }
+
+function sub_sumV2(arr, s, e){
+    s = arr[s] === undefined ? 0 : s;
+    e = arr[e] === undefined ? arr.length - 1 : e;
+    
+    try{
+        return(arr.slice(s, e + 1).reduce((a, v) => a + v, 0) * 10) / 10;
+    }catch(e){
+        return NaN;
+    }
+}
