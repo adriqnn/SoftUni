15fb1e5f5d54ee2f0497444af8d2a7df4ed0ee6c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-01 23:59:49 +0300

    03

--- 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_03_DeckOfCards.js
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_03_DeckOfCards.js
@@ -2,7 +2,7 @@ function deck_of_cardsV1(cards){
     let result = [];
 
     for (let cardAsString of cards) {
-        const face = cardAsString.slice(0,-1);
+        const face = cardAsString.slice(0, -1);
         const suit = cardAsString.slice(-1);
 
         try{
@@ -16,9 +16,8 @@ function deck_of_cardsV1(cards){
     console.log(result.join(' '));
 
     function createCard(face, suit){
-        const faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-        const suits = {'S': '\u2660','H': '\u2665','D': '\u2666','C': '\u2663'
-        };
+        const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits = {'S':'\u2660', 'H':'\u2665', 'D':'\u2666', 'C':'\u2663'};
 
         if (faces.includes(face) === false){
             throw new Error('Invalid face: ' + face);
@@ -44,7 +43,7 @@ function deck_of_cardsV2(cards){
     let result = [];
 
     for (let cardAsString of cards) {
-        const face = cardAsString.slice(0,-1);
+        const face = cardAsString.slice(0, -1);
         const suit = cardAsString.slice(-1);
 
         try{
@@ -58,9 +57,8 @@ function deck_of_cardsV2(cards){
     console.log(result.join(' '));
 
     function createCard(face, suit){
-        const faces = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'];
-        const suits = {'S': '\u2660','H': '\u2665','D': '\u2666','C': '\u2663'
-        };
+        const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+        const suits = {'S':'\u2660', 'H':'\u2665', 'D':'\u2666', 'C':'\u2663'};
 
         if (faces.includes(face) === false){
             throw new Error('Invalid face: ' + face);
