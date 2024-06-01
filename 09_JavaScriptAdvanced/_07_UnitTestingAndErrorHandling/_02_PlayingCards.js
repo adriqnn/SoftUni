edf105fbc4a904627670b7ca27c6ac2a644a2499
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-01 06:21:04 +0300

    02

--- 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_02_PlayingCards.js
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandling/_02_PlayingCards.js
@@ -1,20 +1,22 @@
-function createCard(face,suit){
-    const faces = ['2','3','4','5','6','7','8','9','10','J','D','K','A'];
-    if (faces.indexOf(face) == -1){
-        throw new Error('Invalid face: '+ face)
+function playing_cardsV1(face, suit){
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'D', 'K', 'A'];
+
+    if(faces.indexOf(face) === -1){
+        throw new Error('Invalid face: ' + face);
     }
+
     const suits = {
         'S': '\u2660',
         'H': '\u2665',
         'D': '\u2666',
         'C': '\u2663'
     };
+    
     const result = {
         face,
         suit: suits[suit],
-        toString(){
-            return this.face + this.suit;
-        }
+        toString(){return this.face + this.suit}
     };
+    
     return result;
 }
