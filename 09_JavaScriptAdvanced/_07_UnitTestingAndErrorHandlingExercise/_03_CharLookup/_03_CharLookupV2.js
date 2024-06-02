52ae7fb43d1bccd0455a59aa2fab05f704faf438
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-02 06:30:02 +0300

    03

--- 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandlingExercise/_03_CharLookup/_03_CharLookupV2.js
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandlingExercise/_03_CharLookup/_03_CharLookupV2.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 
-function lookupChar(string, index) {
+function char_lookupV1(string, index) {
     if (typeof(string) !== 'string' || !Number.isInteger(index)) {
         return undefined;
     }
@@ -12,28 +12,28 @@ function lookupChar(string, index) {
     return string.charAt(index);
 }
 
-describe('test', () => {
+describe('Char Lookup', () => {
     it('check whether first parameter type is valid', () => {
-        expect(lookupChar(0,0)).to.equal(undefined);
+        expect(char_lookupV1(0,0)).to.equal(undefined);
     });
 
     it('check whether second parameter type is valid', () => {
-        expect(lookupChar('hello','test')).to.equal(undefined);
+        expect(char_lookupV1('hello','test')).to.equal(undefined);
     });
 
     it('check whether second parameter type is valid integer', () => {
-        expect(lookupChar('hello', 0.1)).to.equal(undefined);
+        expect(char_lookupV1('hello', 0.1)).to.equal(undefined);
     });
 
     it('check value with index bigger than string length', () => {
-        expect(lookupChar('hi', 10)).to.equal('Incorrect index');
+        expect(char_lookupV1('hi', 10)).to.equal('Incorrect index');
     });
 
     it('check value with negative index', () => {
-        expect(lookupChar('hi',-10)).to.equal('Incorrect index')
+        expect(char_lookupV1('hi',-10)).to.equal('Incorrect index')
     });
 
     it('check value with valid params', () => {
-        expect(lookupChar('hello',1)).to.equal('e');
+        expect(char_lookupV1('hello',1)).to.equal('e');
     });
 });
