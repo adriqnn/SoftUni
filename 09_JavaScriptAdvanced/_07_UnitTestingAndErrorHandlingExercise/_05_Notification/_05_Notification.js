d02f9fc815a8387dcc0ca4103244958ab27d0d91
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-02 23:32:33 +0300

    05

---
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandlingExercise/_05_Notification/_05_Notification.js
@@ -0,0 +1,8 @@
+function notificationsV1(message){
+    let divEl = document.getElementById('notification');
+
+    divEl.textContent = message;
+    divEl.style.display = 'block';
+
+    divEl.addEventListener('click', () => { divEl.style.display = 'none';});
+}
