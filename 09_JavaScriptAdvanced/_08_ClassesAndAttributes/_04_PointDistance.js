7ae09804ff70b7f2692a72a07a2e7c36b0e12288
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-03 06:04:08 +0300

    04

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_04_PointDistance.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_04_PointDistance.js
@@ -8,3 +8,20 @@ class PointV1{
         return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
     }
 }
+
+class PointV2{
+    constructor(x, y) {
+        this._x = x;
+        this._y = y;
+    }
+
+    get x () {
+        return this._x;
+    }
+    
+    get y () {
+        return this._y;
+    }
+
+    static distance = (a, b) => Math.hypot(b.x - a.x, b.y - a.y);
+}
