f5b0022d285da32d6004f524fdb00734bfa0a5fb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-03 06:00:01 +0300

    03

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_03_Circle.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_03_Circle.js
@@ -1,12 +1,21 @@
-class Circle {
-    constructor (r) {
-        this._radius = r
+class CircleV1{
+    constructor (r){
+        this._radius = r;
     }
 
-    get radius () { return this._radius}
+    get radius (){
+        return this._radius;
+    }
 
-    get diameter () { return this._radius * 2 }
-    set diameter (d) { this._radius = d / 2}
+    get diameter (){
+        return this._radius * 2;
+    }
 
-    get area () { return Math.PI * this._radius * this._radius }
+    set diameter (d){
+        this._radius = d / 2;
+    }
+
+    get area (){
+        return Math.PI * this._radius * this._radius ;
+    }
 }
