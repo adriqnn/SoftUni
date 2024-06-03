fbc77dbf301fb8905c44fc8350f69535d36953b7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-03 05:51:34 +0300

    01

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_01_Person.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_01_Person.js
@@ -1,11 +1,12 @@
-class Person{
+class PersonV1{
     constructor(firstName, lastName, age, email){
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
         this.email = email;
     }
+
     toString(){
-        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
     }
 }
