79668d6a52d42a7165a186fef37680dada3abeb7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-03 05:58:21 +0300

    02

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_02_GetPersons.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributes/_02_GetPersons.js
@@ -1,4 +1,4 @@
-function get_peopleV1() {
+function get_peopleV1(){
     class Person {
         constructor(firstName, lastName, age, email) {
             this.firstName = firstName;
@@ -18,4 +18,22 @@ function get_peopleV1() {
     let person4 = new Person('Gabriel', 'Peterson', 24, 'g.p@gmail.com');
 
     return [person1, person2, person3, person4];
+}
+
+function get_peopleV2(){
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`;
+        }
+    }
+
+    const data = [['Anna', 'Simpson', 22, 'anna@yahoo.com'], ['SoftUni'], ['Stephan', 'Johnson', 25], ['Gabriel', 'Peterson', 24, 'g.p@gmail.com']];
+    return data.map(x => new Person(...x));
 }
