b0b6281c8b164a45ac34f5037605daa8290bf00e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-03 03:11:56 +0300

    06

---
+++ 09_JavaScriptAdvanced/_07_UnitTestingAndErrorHandlingExercise/_06_DynamicValidation/_06_DynamicValidation.js
@@ -0,0 +1,26 @@
+function dynamic_validationV1(){
+    let emailEl = document.getElementById('email');
+    let pattern = new RegExp('^[a-z]+@[a-z]+\\.[a-z]+');
+
+    emailEl.addEventListener('change', () => {
+        if(pattern.test(emailEl.value)){
+            emailEl.classList.remove('error');
+        }else{
+            emailEl.classList.add('error');
+        }
+    });
+}
+
+function dynamic_validationV2() {
+    const input = document.getElementById("email");
+
+    function isValidEmail(str) {
+        return /^[a-z]+@[a-z]+\.[a-z]+/g.test(str);
+    }
+
+    function applyStyle(e, email) {
+        e.className = isValidEmail(email) ? "" : "error";
+    }
+
+    input.addEventListener("change", e => applyStyle(e.target, e.target.value));
+}
