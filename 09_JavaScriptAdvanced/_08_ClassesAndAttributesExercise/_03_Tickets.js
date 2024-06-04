ce4990aa5a30a208fbe769c957f1859accefdcd0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-04 23:21:33 +0300

    03

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_03_Tickets.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_03_Tickets.js
@@ -1,6 +1,6 @@
-function solve(arr, criteria){
-    class Ticket {
-        constructor(destination,price,status){
+function ticketsV1(arr, criteria){
+    class Ticket{
+        constructor(destination, price, status){
             this.destination = destination;
             this.price = price;
             this.status = status;
@@ -10,19 +10,20 @@ function solve(arr, criteria){
     let tickets = [];
 
     for (const line of arr) {
-        let [destination,price,status] = line.split('|');
+        let [destination, price, status] = line.split('|');
         price = Number(price);
-        tickets.push(new Ticket(destination,price,status));
-    };
-    tickets.sort((a,b) => {
-        if(criteria == 'destination'){
+        tickets.push(new Ticket(destination, price, status));
+    }
+
+    tickets.sort((a, b) => {
+        if(criteria === 'destination'){
             return a.destination.localeCompare(b.destination);
-        }else if(criteria == 'status'){
+        }else if(criteria === 'status'){
             return a.status.localeCompare(b.status);
         }else{
             return a.price - b.price;
         }
     });
-    
+
     return tickets;
 }
