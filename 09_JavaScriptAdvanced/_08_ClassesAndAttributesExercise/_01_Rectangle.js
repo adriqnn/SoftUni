d589948d30cdd7eaefe7c7fd5efd56fb2eb4906c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-04 07:14:29 +0300

    01

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_01_Rectangle.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_01_Rectangle.js
@@ -13,3 +13,23 @@ class RectangleV1{
         return Number(this.width) * Number(this.height);
     }
 }
+
+class RectangleV2{
+    constructor(w, h, c){
+        this._width = w;
+        this._height = h;
+        this._color = c;
+    }
+
+    get width(){
+        return this._width;
+    }
+    get height(){
+        return this._height;
+    }
+    get color(){
+        return this._color.charAt(0).toLocaleUpperCase() + this._color.slice(1);
+    }
+
+    calcArea = () => this._width * this._height;
+}
