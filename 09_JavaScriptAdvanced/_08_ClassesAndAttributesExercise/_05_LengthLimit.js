33c992ba8467df9258e2ea66186e785c94fe74e0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-06 04:42:37 +0300

    05

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_05_LengthLimit.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_05_LengthLimit.js
@@ -24,3 +24,29 @@ class LengthLimitV1{
         }
     }
 }
+
+class LengthLimitV2{
+    constructor (s, l){
+        this.innerString = s;
+        this.innerLength = l;
+    }
+
+    increase (v){
+        this.innerLength += v;
+    }
+
+    decrease (v){
+        const result = this.innerLength - v;
+        this.innerLength = result < 0 ? 0 : result;
+    }
+
+    toString (){
+        if (this.innerLength === 0) return '...';
+
+        if (this.innerString.length > this.innerLength){
+            return `${this.innerString.slice(0, this.innerLength)}...`;
+        }
+
+        return this.innerString;
+    }
+}
