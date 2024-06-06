6c432e815c1e60b36a5481ea6f7e60d7bdd3f03f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-06 04:28:38 +0300

    04

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_04_SortedList.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_04_SortedList.js
@@ -27,3 +27,35 @@ class SortedListV1{
         }
     }
 }
+
+class SortedListV2{
+    constructor () {
+        this.list = [];
+        this.size = this.list.length;
+    }
+
+    updateSize = () => this.size = this.list.length;
+    orderList = () => this.list.sort((a, b) => a - b);
+
+    add (e) {
+        this.list.push(e);
+        this.updateSize();
+        this.orderList();
+    }
+
+    remove (i) {
+        if (this.list[i] !== undefined) {
+            this.list.splice(i, 1);
+            this.updateSize();
+            this.orderList();
+        }
+    }
+
+    get (i) {
+        if (this.list[i] !== undefined) {
+            this.updateSize();
+            this.orderList();
+            return this.list[i];
+        }
+    }
+}
