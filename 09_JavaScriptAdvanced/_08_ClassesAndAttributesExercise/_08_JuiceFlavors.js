15604b5941d28e493ee231180b8d5e1e47432775
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-07 05:16:45 +0300

    08

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_08_JuiceFlavors.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_08_JuiceFlavors.js
@@ -1,25 +1,31 @@
-function juiceFlavors(arr){
+function juice_flavorsV1(arr){
     let juices = {};
     let bottles = {};
 
     for(let juice of arr){
         let[flavor,qty] = juice.split(' => ');
+
         if(!juices.hasOwnProperty(flavor)){
             juices[flavor] = 0;
         }
+
         juices[flavor] += Number(qty);
         createBottle(flavor);
     }
 
+    let result = "";
     for(let flavor in bottles){
-        console.log(flavor + ' => ' + bottles[flavor]);
+        result += `${flavor} => ${bottles[flavor]}\n`;
     }
+    
+    return result;
 
     function createBottle(flavor){
         if(juices[flavor] >= 1000){
             if(!bottles.hasOwnProperty(flavor)){
                 bottles[flavor] = 0;
             }
+
             let numberOfBottles = Math.floor(juices[flavor] / 1000);
             bottles[flavor] += numberOfBottles;
             juices[flavor] -= (numberOfBottles * 1000);
