efd2071955781580c15e3a8d65b6249aabef457d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-07 06:19:46 +0300

    09

--- 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_09_AutoEngineeringCompany.js
+++ 09_JavaScriptAdvanced/_08_ClassesAndAttributesExercise/_09_AutoEngineeringCompany.js
@@ -1,22 +1,25 @@
-function comapny(arr){
+function auto_engineering_companyV1(arr){
     let carRegister = {};
 
     arr.forEach(element => {
-        let [brand,model,qty] = element.split(' | ');
+        let [brand, model, quality] = element.split(' | ');
 
         if(!carRegister.hasOwnProperty(brand)){
             carRegister[brand] = {};
         }
 
         if(!carRegister[brand].hasOwnProperty(model)){
-            carRegister[brand][model] = 0; 
+            carRegister[brand][model] = 0;
         }
 
-        carRegister[brand][model] += Number(qty);
+        carRegister[brand][model] += Number(quality);
     });
 
+    let result = "";
     for(let brand in carRegister){
-        console.log(brand);
-        Object.entries(carRegister[brand]).forEach(el => console.log('###' + el[0] + ' -> ' + el[1]));
+        result += `${brand}\n`;
+        Object.entries(carRegister[brand]).forEach(el => result += `###${el[0]} -> ${el[1]}\n`);
     }
+
+    return result;
 }
