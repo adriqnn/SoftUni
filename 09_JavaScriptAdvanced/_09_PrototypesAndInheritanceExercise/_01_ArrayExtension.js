f33e80633b4179f428843ec5e16c52c91599bae8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-09 02:45:29 +0300

    01

--- 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_01_ArrayExtension.js
+++ 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_01_ArrayExtension.js
@@ -1,37 +1,33 @@
-(function arrays(){
-    let arr = [1,2,3];
+(function array_extensionV1(){
+    let arr = [1, 2, 3];
 
     Array.prototype.last = function(){
-        return this[this.length-1];
+        return this[this.length - 1];
     };
 
     Array.prototype.skip = function(n){
         let result = [];
-        for (let i = n; i < this.length; i++) {
-            result.push(this[i]);  
+        for (let i = n; i < this.length; i++){
+            result.push(this[i]);
         }
+
         return result;
     };
 
     Array.prototype.take = function(n){
         let result = [];
-        for (let i = 0; i < n; i++) {
-            result.push(this[i]); 
+        for (let i = 0; i < n; i++){
+            result.push(this[i]);
         }
+
         return result;
     };
 
     Array.prototype.sum = function(){
-        // let sum = 0
-        // for (let i = 0; i < this.length; i++) {
-        //     sum += this[i];
-        // }
-        // return sum;
         return this.reduce((a,b) => a + b, 0);
     };
 
     Array.prototype.average = function(){
-        // return this.sum()/this.length;
         return this.reduce((a,b) => a + b) / arr.length;
     }
-})()
+})();
