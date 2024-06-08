9b5f6aba1593be82d1de6e4a4be693f349f525fa
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-08 07:21:58 +0300

    01

--- 09_JavaScriptAdvanced/_09_PrototypesAndInheritance/_01_Person.js
+++ 09_JavaScriptAdvanced/_09_PrototypesAndInheritance/_01_Person.js
@@ -1,20 +1,23 @@
-function createPerson(firstName, lastName){
+function personV1(firstName, lastName){
     const person = {
         firstName,
         lastName,
         fullName: firstName + ' ' + lastName
     };
+
     Object.defineProperty(person, 'fullName', {
         get(){
             return this.firstName + ' ' + this.lastName;
         },
         set(value){
             const [first, last] = value.split(' ');
-            if(first != undefined && last != undefined){
+
+            if(first !== undefined && last !== undefined){
                 this.firstName = first;
                 this.lastName = last;
-            };
+            }
         }
     });
+
     return person;
 }
