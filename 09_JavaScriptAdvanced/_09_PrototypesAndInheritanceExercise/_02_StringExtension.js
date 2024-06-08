f13c2cd4238cca8bd6b256a1941f463b56bc9f54
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-09 02:47:13 +0300

    02

--- 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_02_StringExtension.js
+++ 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_02_StringExtension.js
@@ -1,4 +1,4 @@
-(function string(){
+(function string_extensionV1(){
     String.prototype.ensureStart = function(str){
         if(this.startsWith(str)){
             return this.toString();
@@ -6,6 +6,7 @@
             return str + this.toString();
         }
     }
+    
     String.prototype.ensureEnd = function(str){
         if(this.endsWith(str)){
             return this.toString();
@@ -13,29 +14,34 @@
             return this.toString() + str;
         }
     }
+    
     String.prototype.isEmpty = function(){
         return this.length === 0;
     }
+    
     String.prototype.truncate = function(n){
         if(n <= 3){
             return '.'.repeat(n);
         }
+        
         if(this.toString().length <= n){
             return this.toString();
         }else{
             let lastIndex = this.substring(0, n-2).lastIndexOf(' ');
             if(lastIndex !== -1){
-                return this.substring(0, lastIndex) + '...'; 
+                return this.substring(0, lastIndex) + '...';
             }else{
                 return this.substring(0,n-3) + '...';
             }
         }
     };
+    
     String.format = function(string, ...params){
         let str = string;
         params.forEach((p,i) => {
             str = str.replace(`{${i}}`, p);
         });
+        
         return str;
     }
-})()
+})();
