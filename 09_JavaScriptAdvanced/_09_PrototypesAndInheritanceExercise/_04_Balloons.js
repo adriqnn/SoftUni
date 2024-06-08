4da9085e9acd97c79908400edd5e38985af28340
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-09 02:58:36 +0300

    04

--- 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_04_Balloons.js
+++ 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_04_Balloons.js
@@ -36,3 +36,44 @@ function balloonsV1(){
         BirthdayBalloon
     }
 }
+
+// for judge with the mistake in tests
+function balloonsV2(){
+    class Balloon{
+        constructor(color, hasWeight){
+            this.color = color;
+            this.hasWeight = hasWeight;
+            this.gasWeight = hasWeight;
+        }
+    }
+
+    class PartyBalloon extends Balloon{
+        constructor(color, hasWeight, ribbonColor, ribbonLength){
+            super(color, hasWeight)
+            this.ribbonColor = ribbonColor;
+            this.ribbonLength = ribbonLength;
+            this._ribbon = {color: ribbonColor, length: ribbonLength};
+        }
+
+        get ribbon(){
+            return this._ribbon;
+        }
+    }
+
+    class BirthdayBalloon extends PartyBalloon{
+        constructor(color, hasWeight, ribbonColor, ribbonLength, text){
+            super(color, hasWeight, ribbonColor, ribbonLength);
+            this._text = text;
+        }
+
+        get text(){
+            return this._text;
+        }
+    }
+
+    return {
+        Balloon,
+        PartyBalloon,
+        BirthdayBalloon
+    }
+}
