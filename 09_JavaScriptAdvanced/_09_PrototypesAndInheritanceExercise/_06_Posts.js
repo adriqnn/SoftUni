bfee7c6b2d2925ef4aae568e72b7825988ceb298
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-09 03:18:52 +0300

    06

--- 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_06_Posts.js
+++ 09_JavaScriptAdvanced/_09_PrototypesAndInheritanceExercise/_06_Posts.js
@@ -1,23 +1,27 @@
-function solve(){
+function postsV1(){
     class Post{
         constructor(title, content){
             this.title = title;
             this.content = content;
         }
+        
         toString(){
             return `Post: ${this.title}\nContent: ${this.content}`;
         }
     }
+    
     class SocialMediaPost extends Post{
-        constructor(title,content,likes,dislikes){
+        constructor(title, content, likes, dislikes){
             super(title,content);
             this.likes = likes;
             this.dislikes = dislikes;
             this.comments = [];
         }
+        
         addComment(comment){
             this.comments.push(comment);
         }
+        
         toString(){
             let superString = super.toString();
             let rating = this.likes - this.dislikes;
@@ -27,27 +31,34 @@ function solve(){
                 for (let i = 0; i < this.comments.length; i++) {
                     commentsText += `\n * ${this.comments[i]}`
                 }
+                
                 return `${superString}\nRating: ${rating}\nComments:${commentsText}`;
             }else{
                 return `${superString}\nRating: ${rating}`;
             }
         }
     }
-    class BlogPost extends Post {
-        constructor(title,content,views){
-            super(title,content);
+    
+    class BlogPost extends Post{
+        constructor(title, content, views){
+            super(title, content);
             this.views = views;
         }
+        
         view(){
             this.views++;
             return this;
         }
+        
         toString(){
             let superString = super.toString();
             return superString + `\nViews: ${this.views}`
         }
     }
+    
     return {
-        Post,SocialMediaPost,BlogPost
+        Post,
+        SocialMediaPost,
+        BlogPost
     }
 }
