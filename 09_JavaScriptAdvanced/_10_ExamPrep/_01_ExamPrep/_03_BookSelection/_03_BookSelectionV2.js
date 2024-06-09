979f7fc15fc9406ef11b271654eedd7ef451e5b2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-10 02:18:36 +0300

    01

--- 09_JavaScriptAdvanced/_10_ExamPrep/_01_ExamPrep/_03_BookSelection/_03_BookSelectionV2.js
+++ 09_JavaScriptAdvanced/_10_ExamPrep/_01_ExamPrep/_03_BookSelection/_03_BookSelectionV2.js
@@ -1,33 +1,33 @@
 import {expect} from "chai";
 
 const book_selectionV1 = {
-    isGenreSuitable(genre, age) {
-        if (age <= 12 && (genre === "Thriller" || genre === "Horror")) {
+    isGenreSuitable(genre, age){
+        if (age <= 12 && (genre === "Thriller" || genre === "Horror")){
             return `Books with ${genre} genre are not suitable for kids at ${age} age`;
         } else {
             return `Those books are suitable`;
         }
     },
-    isItAffordable(price, budget) {
-        if (typeof price !== "number" || typeof budget !== "number") {
+    isItAffordable(price, budget){
+        if (typeof price !== "number" || typeof budget !== "number"){
             throw new Error("Invalid input");
         }
 
         let result = budget - price;
-        if (result < 0) {
+        if (result < 0){
             return "You don't have enough money";
         } else {
             return `Book bought. You have ${result}$ left`;
         }
     },
-    suitableTitles(array, wantedGenre) {
+    suitableTitles(array, wantedGenre){
         let resultArr = [];
-        if (!Array.isArray(array) || typeof wantedGenre !== "string") {
+        if (!Array.isArray(array) || typeof wantedGenre !== "string"){
             throw new Error("Invalid input");
         }
 
         array.map((obj) => {
-            if (obj.genre === wantedGenre) {
+            if (obj.genre === wantedGenre){
                 resultArr.push(obj.title);
             }
         });
