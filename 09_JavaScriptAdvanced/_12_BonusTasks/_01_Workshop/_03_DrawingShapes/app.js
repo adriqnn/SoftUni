10c0ce6e1a09c39edf7a3e430e22ceb966cb9479
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-15 23:41:39 +0300

    03

---
+++ 09_JavaScriptAdvanced/_12_BonusTasks/_01_Workshop/_03_DrawingShapes/app.js
@@ -0,0 +1,36 @@
+const canvas = document.getElementById("canvas");
+/** @type { CanvasRenderingContext2D } */
+let ctx = canvas.getContext('2d');
+
+window.ctx = ctx;
+
+const width = canvas.width;
+const height = canvas.height;
+const hSize = 20;
+const vSize = 20;
+const gridSize = width / hSize;
+
+function drawGrid(){
+    ctx.beginPath();
+
+    for (let x = 1; x < hSize; x++) {
+        ctx.moveTo(x * gridSize, 0);
+        ctx.lineTo(x * gridSize, height);
+    }
+
+    for (let y = 1; y < vSize; y++) {
+        ctx.moveTo(0, y * gridSize);
+        ctx.lineTo(y * gridSize, width);
+    }
+
+    for (let y = 1; y < vSize; y++) {
+        ctx.moveTo(width, y * gridSize);
+        ctx.lineTo(width - y * gridSize, width);  
+    }
+
+
+    ctx.closePath();
+    ctx.stroke();
+}
+
+drawGrid();
