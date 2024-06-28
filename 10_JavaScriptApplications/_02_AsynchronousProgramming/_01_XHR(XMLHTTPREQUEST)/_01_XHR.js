c58da67e6753b3cdb38e44b80a352522fb668d21
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-28 18:26:59 +0300

    01

--- 10_JavaScriptApplications/_02_AsynchronousProgramming/_01_XHR(XMLHTTPREQUEST)/_01_XHR.js
+++ 10_JavaScriptApplications/_02_AsynchronousProgramming/_01_XHR(XMLHTTPREQUEST)/_01_XHR.js
@@ -1,14 +1,90 @@
-function XHRV1() {
+function XHRV1(){
     let url = 'https://api.github.com/users/testnakov/repos';
     const httpRequest = new XMLHttpRequest();
 
-    httpRequest.addEventListener('readystatechange', function () {
-       if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+    httpRequest.addEventListener('readystatechange', function(){
+       if(httpRequest.readyState === 4 && httpRequest.status === 200){
           document.getElementById("res").textContent = httpRequest.responseText;
        }
     });
 
     httpRequest.open("GET", url);
     httpRequest.send();
- }
+}
 
+function XHRV2(){
+    let button = document.getElementById("load");
+
+    button.addEventListener('click', function loadRepos(){
+        let url = 'https://api.github.com/users/testnakov/repos';
+        const httpRequest = new XMLHttpRequest();
+
+        httpRequest.addEventListener('readystatechange', function (){
+            if(httpRequest.readyState === 4 && httpRequest.status === 200){
+                document.getElementById("res").textContent = httpRequest.responseText;
+            }
+        });
+
+        httpRequest.open("GET", url);
+        httpRequest.send();
+    });
+}
+
+
+function XHRV3(){
+    let button = document.getElementById("load");
+    button.addEventListener('click', loadRepos);
+
+    function loadRepos(){
+        let url = 'https://api.github.com/users/testnakov/repos';
+        const xhr = new XMLHttpRequest();
+
+        xhr.open('GET', url, true);
+        xhr.onload = function(){
+            if(xhr.status >= 200 && xhr.status < 300){
+                document.getElementById("res").textContent = xhr.responseText;
+            }
+        }
+
+        xhr.send();
+    }
+}
+
+function XHRV4(){
+    let button = document.getElementById("load");
+
+    button.addEventListener('click', function loadRepos(){
+        let url = 'https://api.github.com/users/testnakov/repos';
+        const httpRequest = new XMLHttpRequest();
+
+        httpRequest.addEventListener('readystatechange', function (){
+            if(httpRequest.readyState === 4 && httpRequest.status === 200){
+                document.getElementById("res").textContent = httpRequest.responseText;
+            }
+        });
+
+        httpRequest.open("GET", url);
+        httpRequest.send();
+    });
+}
+
+function XHRV5(){
+    let button = document.getElementById("load");
+
+    button.addEventListener('click', function loadRepos(){
+        let url = 'https://api.github.com/users/testnakov/repos';
+        const httpRequest = new XMLHttpRequest();
+
+        httpRequest.addEventListener('readystatechange', function (){
+            if(httpRequest.readyState === 4 && httpRequest.status === 200){
+                document.getElementById("res").textContent = httpRequest.responseText;
+            }
+        });
+
+        httpRequest.open("GET", url);
+        httpRequest.send();
+    });
+}
+
+// Defer XHRV4();
+// Async XHRV5();
