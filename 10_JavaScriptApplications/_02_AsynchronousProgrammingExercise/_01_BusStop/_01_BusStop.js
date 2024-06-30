9aef7652808bd0457d57b814e8249b5c9c7cb611
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-30 22:50:17 +0300

    01

--- 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_01_BusStop/_01_BusStop.js
+++ 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_01_BusStop/_01_BusStop.js
@@ -149,3 +149,29 @@ async function bus_stopV3(){
 
     await updateBusStopInfo();
 }
+
+async function bus_stopV4(){
+    const input = document.getElementById('stopId');
+    const id = input.value;
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${id}`;
+
+    try {
+        const ul = document.getElementById('buses');
+        ul.innerHTML = '';
+        const response = await fetch(url);
+        const data = await response.json();
+
+        document.getElementById('stopName').textContent = data.name;
+
+        Object.entries(data.buses).map(([bus, time]) => {
+            const result = document.createElement('li');
+            result.textContent = `Bus ${bus} arrives in ${time} minutes`;
+            ul.appendChild(result);
+        });
+
+        input.value = '';
+
+    } catch (error) {
+        document.getElementById('stopName').textContent = 'Error';
+    }
+}
