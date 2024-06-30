a4c244a360f7d89ebfa9711de0f2eb26f6744586
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-06-30 22:47:34 +0300

    01

---
+++ 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_01_BusStop/_01_BusStop.js
@@ -0,0 +1,95 @@
+async function bus_stopV1() {
+    const url = `http://localhost:3030/jsonstore/bus/businfo/`;
+    const html = {
+        busStopId: document.getElementById("stopId"),
+        result: document.getElementById("result")
+    }
+
+    async function getBusInfoByID(){
+        try{
+            const res = await fetch(`${url}${html.busStopId.value}`);
+            if(res.status !== 200){
+                throw new Error(`Error`);
+            }
+
+            return await res.json();
+        }catch(err){
+            return 'Error';
+        }
+    }
+
+    function addInfo(obj){
+        html.result.innerHTML = "";
+
+        const div = document.createElement('div');
+        div.setAttribute('id', 'stopName');
+
+        const ul = document.createElement('ul');
+        ul.setAttribute('id', 'buses');
+
+        if(typeof obj === 'string'){
+            div.textContent = 'Error';
+        }else{
+            div.textContent = obj.name;
+            Object.entries(obj.buses).forEach(e => {
+               const li = document.createElement('li');
+
+               li.textContent = `Bus ${e[0]} arrives in ${e[1]} minutes`;
+               ul.appendChild(li);
+            });
+        }
+
+        html.result.append(div, ul);
+    }
+
+    addInfo(await getBusInfoByID());
+}
+
+async function bus_stopV2() {
+    const url = `http://localhost:3030/jsonstore/bus/businfo/`;
+    const html = {
+        busStopId: document.getElementById("stopId"),
+        result: document.getElementById("result")
+    }
+
+    async function getBusInfoByID(){
+        try{
+            const res = await fetch(`${url}${html.busStopId.value}`);
+            if(res.status !== 200){
+                throw new Error(`Error`);
+            }
+
+            return res.json();
+        }catch(err){
+            return 'Error';
+        }
+    }
+
+    function addInfo(obj){
+        html.result.innerHTML = "";
+
+        const div = document.createElement('div');
+        div.setAttribute('id', 'stopName');
+
+        const ul = document.createElement('ul');
+        ul.setAttribute('id', 'buses');
+
+        if(typeof obj === 'string'){
+            div.textContent = 'Error';
+        }else{
+            div.textContent = obj.name;
+            Object.entries(obj.buses).forEach(e => {
+                const li = document.createElement('li');
+
+                li.textContent = `Bus ${e[0]} arrives in ${e[1]} minutes`;
+                ul.appendChild(li);
+            });
+        }
+
+        html.result.appendChild(div);
+        html.result.appendChild(ul);
+    }
+
+    addInfo(await getBusInfoByID());
+}
+
