58322ed3a46d48a32b5c4d47c4dc08bcc02106c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-03 16:21:32 +0300

    04

--- 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_04_LockedProfile/_04_LockedProfile.js
+++ 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_04_LockedProfile/_04_LockedProfile.js
@@ -1,3 +1,52 @@
-function lockedProfile() {
-    console.log('TODO...')
+async function locked_profileV1(){
+    const url = `http://localhost:3030/jsonstore/advanced/profiles`;
+    const html = { main: document.getElementById('main') };
+
+    html.main.innerHTML = '';
+    await generateContent();
+
+    async function generateContent(){
+        let res = await fetch(url);
+        let data = await res.json();
+
+        html.main.innerHTML = Object.values(data).map(e => {
+            return `<div class="profile">
+                        <img src="iconProfile2.png" class="userIcon"/>
+                        <label>Lock</label>
+                        <input type="radio" name="${e._id}-radio" value="lock" checked>
+                        <label>Unlock</label>
+                        <input type="radio" name="${e._id}-radio" value="unlock"><br>
+                        <hr>
+                        <label>Username</label>
+                        <input type="text" name="${e._id}-name" value="${e.username}" disabled readonly/>
+                        <div class="user1Username" style="display:none">
+                            <hr>
+                            <label>Email:</label>
+                            <input type="email" name="${e._id}-email" value="${e.email}" disabled readonly/>
+                            <br>
+                            <label>Age:</label>
+                            <input type="text" name="${e._id}-age" value="${e.age}" disabled readonly/>
+                        </div>
+                        <button>Show more</button>
+                    </div>`;
+        }).join('');
+
+        document.querySelectorAll('button').forEach(e => e.addEventListener('click', showMoreOrHide));
+
+        function showMoreOrHide(e){
+            const button = e.target;
+            const content = button.previousElementSibling;
+            const radio = button.parentElement.children[2];
+
+            if(!radio.checked === true){
+                if(button.textContent === 'Show more'){
+                    button.textContent = 'Hide it';
+                    content.style.display = 'block';
+                }else{
+                    button.textContent = 'Show more';
+                    content.style.display = 'none';
+                }
+            }
+        }
+    }
 }
