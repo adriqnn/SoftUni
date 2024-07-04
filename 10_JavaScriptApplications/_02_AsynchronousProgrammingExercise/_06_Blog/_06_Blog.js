c4a2149c2e26958ef3bf7a54ccc3e08a50acd657
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-04 22:41:44 +0300

    06

--- 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_06_Blog/_06_Blog.js
+++ 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_06_Blog/_06_Blog.js
@@ -1,4 +1,4 @@
-function blogV1() {
+function blogV1(){
     const url = `http://localhost:3030/jsonstore/blog`;
     const ids = [];
     let clicked = false;
@@ -14,42 +14,50 @@ function blogV1() {
     html.btnView.addEventListener('click', loadComments);
 
     async function options(){
-        let res = await fetch(`${url}/posts`);
-        let loadedOptions = await res.json();
+        try{
+            let res = await fetch(`${url}/posts`);
+            let loadedOptions = await res.json();
 
-        if(!clicked){
-            html.postTitle.insertAdjacentHTML('afterend', Object.values(loadedOptions).map(o => {
-                ids.push(o.id);
+            if(!clicked){
+                html.postTitle.insertAdjacentHTML('afterend', Object.values(loadedOptions).map(o => {
+                    ids.push(o.id);
 
-                let summary = `<p id="${o.id}" style="display: none">${o.body}</p>`;
+                    let summary = `<p id="${o.id}" style="display: none">${o.body}</p>`;
 
-                const option = document.createElement('option');
-                option.value = o.id;
-                option.text = o.title;
+                    const option = document.createElement('option');
+                    option.value = o.id;
+                    option.text = o.title;
 
-                html.select.appendChild(option);
+                    html.select.appendChild(option);
 
-                return summary;
-            }).join(' '));
+                    return summary;
+                }).join(' '));
 
-            clicked = true;
+                clicked = true;
+            }
+        }catch(err){
+            console.log(err);
         }
     }
 
-    async function loadComments(e){
-        if(html.select.innerHTML !== ''){
-            let res = await fetch(`${url}/comments`);
-            let additionalInformation = await res.json();
+    async function loadComments(){
+        try{
+            if(html.select.innerHTML !== ''){
+                let res = await fetch(`${url}/comments`);
+                let additionalInformation = await res.json();
 
-            showElement(html.select.value);
+                showElement(html.select.value);
 
-            html.postComments.innerHTML = ``;
-            Object.entries(additionalInformation)
-                .filter(e => e[1].postId === html.select.value).forEach(e => {
+                html.postComments.innerHTML = ``;
+                Object.entries(additionalInformation)
+                    .filter(e => e[1].postId === html.select.value).forEach(e => {
                     let li = document.createElement('li');
                     li.textContent = e[1].text;
                     html.postComments.appendChild(li);
-            });
+                });
+            }
+        }catch(err){
+            console.log(err);
         }
     }
 
