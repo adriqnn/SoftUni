1a13183e3601d62d0da0229fdf77c9ebf28f22d9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-05 23:26:43 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/login.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/login.js
@@ -3,31 +3,31 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (ev => {
     ev.preventDefault();
     const formData = new FormData(ev.target);
+
     onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
 }));
 
-async function onSubmit(data) {
+async function onSubmit(data){
     const body = JSON.stringify({
         email: data.email,
-        password: data.password,
+        password: data.password
     });
 
-    try {
+    try{
         const response = await fetch('http://localhost:3030/users/login', {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body
         });
         const data = await response.json();
-        if (response.status == 200) {
+        
+        if(response.status === 200){
             sessionStorage.setItem('authToken', data.accessToken);
             window.location.pathname = 'index.html';
-        } else {
+        }else{
             throw new Error(data.message);
         }
-    } catch (err) {
+    }catch(err){
         console.error(err.message);
     }
 }
