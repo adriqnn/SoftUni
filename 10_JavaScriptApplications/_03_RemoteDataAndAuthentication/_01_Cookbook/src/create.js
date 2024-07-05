db9a8cb2f1e72d41c42a7a152aba95bdbb542d45
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-05 23:24:06 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/create.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/create.js
@@ -3,23 +3,24 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (ev => {
     ev.preventDefault();
     const formData = new FormData(ev.target);
+    
     onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
 }));
 
-async function onSubmit(data) {
+async function onSubmit(data){
     const body = JSON.stringify({
         name: data.name,
         img: data.img,
-        ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l != ''),
-        steps: data.steps.split('\n').map(l => l.trim()).filter(l => l != '')
+        ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l !== ''),
+        steps: data.steps.split('\n').map(l => l.trim()).filter(l => l !== '')
     });
 
     const token = sessionStorage.getItem('authToken');
-    if (token == null) {
+    if(token == null){
         return window.location.pathname = 'index.html';
     }
 
-    try {
+    try{
         const response = await fetch('http://localhost:3030/data/recipes', {
             method: 'post',
             headers: {
@@ -29,12 +30,12 @@ async function onSubmit(data) {
             body
         });
 
-        if (response.status == 200) {
+        if(response.status === 200){
             window.location.pathname = 'index.html';
-        } else {
+        }else{
             throw new Error(await response.json());
         }
-    } catch (err) {
+    }catch(err){
         console.error(err.message);
     }
 }
