23412ccb8b3dba1a09539e4007b0c79ee0d8ef73
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-05 23:27:50 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/regiester.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/regiester.js
@@ -3,35 +3,35 @@ const form = document.querySelector('form');
 form.addEventListener('submit', (ev => {
     ev.preventDefault();
     const formData = new FormData(ev.target);
+    
     onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
 }));
 
-async function onSubmit(data) {
-    if (data.password != data.rePass) {
+async function onSubmit(data){
+    if(data.password !== data.rePass){
         return console.error('Passwords don\'t match');
     }
 
     const body = JSON.stringify({
         email: data.email,
-        password: data.password,
+        password: data.password
     });
 
-    try {
+    try{
         const response = await fetch('http://localhost:3030/users/register', {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
+            headers: { 'Content-Type': 'application/json' },
             body
         });
         const data = await response.json();
-        if (response.status == 200) {
+        
+        if(response.status === 200){
             sessionStorage.setItem('authToken', data.accessToken);
             window.location.pathname = 'index.html';
-        } else {
+        }else{
             throw new Error(data.message);
         }
-    } catch (err) {
+    }catch(err){
         console.error(err.message);
     }
 }
