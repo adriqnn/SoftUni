cf4cc9bb128e6d738defa8fea30832baf52f2789
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-05 23:22:59 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/app.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthentication/_01_Cookbook/src/app.js
@@ -1,18 +1,18 @@
-async function getRecipes() {
+async function getRecipes(){
     const response = await fetch('http://localhost:3030/data/recipes');
     const recipes = await response.json();
 
     return recipes;
 }
 
-async function getRecipeById(id) {
+async function getRecipeById(id){
     const response = await fetch('http://localhost:3030/data/recipes/' + id);
     const recipe = await response.json();
 
     return recipe;
 }
 
-function createRecipePreview(recipe) {
+function createRecipePreview(recipe){
     const result = e('article', { className: 'preview', onClick: toggleCard },
         e('div', { className: 'title' }, e('h2', {}, recipe.name)),
         e('div', { className: 'small' }, e('img', { src: recipe.img })),
@@ -20,14 +20,14 @@ function createRecipePreview(recipe) {
 
     return result;
 
-    async function toggleCard() {
+    async function toggleCard(){
         const fullRecipe = await getRecipeById(recipe._id);
 
         result.replaceWith(createRecipeCard(fullRecipe));
     }
 }
 
-function createRecipeCard(recipe) {
+function createRecipeCard(recipe){
     const result = e('article', {},
         e('h2', {}, recipe.name),
         e('div', { className: 'band' },
@@ -46,26 +46,25 @@ function createRecipeCard(recipe) {
     return result;
 }
 
-async function logout() {
+async function logout(){
     const response = await fetch('http://localhost:3030/users/logout', {
         method: 'get',
-        headers: {
-            'X-Authorization': sessionStorage.getItem('authToken')
-        },
+        headers: { 'X-Authorization': sessionStorage.getItem('authToken') }
     });
-    if (response.status == 200) {
+    
+    if(response.status === 200){
         sessionStorage.removeItem('authToken');
         window.location.pathname = 'index.html';
-    } else {
+    }else{
         console.error(await response.json());
     }
 }
 
 window.addEventListener('load', async () => {
-    if (sessionStorage.getItem('authToken') != null) {
+    if(sessionStorage.getItem('authToken') != null){
         document.getElementById('user').style.display = 'inline-block';
         document.getElementById('logoutBtn').addEventListener('click', logout);
-    } else {
+    }else{
         document.getElementById('guest').style.display = 'inline-block';
     }
 
@@ -78,13 +77,13 @@ window.addEventListener('load', async () => {
     cards.forEach(c => main.appendChild(c));
 });
 
-function e(type, attributes, ...content) {
+function e(type, attributes, ...content){
     const result = document.createElement(type);
 
-    for (let [attr, value] of Object.entries(attributes || {})) {
-        if (attr.substring(0, 2) == 'on') {
+    for(let [attr, value] of Object.entries(attributes || {})){
+        if(attr.substring(0, 2) === 'on'){
             result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
-        } else {
+        }else{
             result[attr] = value;
         }
     }
@@ -92,10 +91,10 @@ function e(type, attributes, ...content) {
     content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
 
     content.forEach(e => {
-        if (typeof e == 'string' || typeof e == 'number') {
+        if(typeof e == 'string' || typeof e == 'number'){
             const node = document.createTextNode(e);
             result.appendChild(node);
-        } else {
+        }else{
             result.appendChild(e);
         }
     });
