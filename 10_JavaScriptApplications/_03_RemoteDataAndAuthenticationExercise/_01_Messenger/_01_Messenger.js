01ded96ac5a644eeaef3c37334216947fbf0c8ba
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-09 19:07:36 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_01_Messenger/_01_Messenger.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_01_Messenger/_01_Messenger.js
@@ -40,3 +40,31 @@ function messengerV1(){
 }
 
 messengerV1();
+
+function messengerV2(){
+    document.getElementById('submit').addEventListener('click', sendMessage);
+    document.getElementById('refresh').addEventListener('click', refresh);
+
+    async function sendMessage() {
+        const [name, message] = document.querySelectorAll('input');
+
+        await fetch('http://localhost:3030/jsonstore/messenger', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                author: name.value,
+                content: message.value
+            })
+        });
+
+        name.value = '';
+        message.value = '';
+    }
+
+    async function refresh() {
+        const response = await fetch('http://localhost:3030/jsonstore/messenger');
+        const data = await response.json();
+
+        document.getElementById('messages').value = Object.values(data).map(m => `${m.author}: ${m.content}`).join('\n');
+    }
+}
