e15f353b569c7496a7523116faa16e685d3a7123
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-09 20:09:30 +0300

    01

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_01_Messenger/_01_Messenger.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_01_Messenger/_01_Messenger.js
@@ -70,3 +70,43 @@ function messengerV2(){
         document.getElementById('messages').value = Object.values(data).map(m => `${m.author}: ${m.content}`).join('\n');
     }
 }
+
+function messengerV3(){
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const html = {
+        messages: document.getElementById('messages'),
+        author: document.querySelectorAll('input')[0],
+        content: document.querySelectorAll('input')[1],
+        submit: document.getElementById('submit'),
+        refresh: document.getElementById('refresh')
+    }
+
+    refreshInfo();
+
+    html.submit.addEventListener('click', submitInfo);
+    html.refresh.addEventListener('click', refreshInfo);
+
+    function refreshInfo(){
+       fetch(url).then(res => {
+           if(res.ok !== true){
+               throw new Error('Error');
+           }
+
+           return res.json();
+       }).then(data => {
+           html.messages.value = Object.entries(data).map(e => `${e[1].author}: ${e[1].content}`).join("\n");
+       }).catch(err => {
+           console.log(err);
+       });
+    }
+
+    function submitInfo(){
+        if(html.author.value !== '' && html.content.value !== ''){
+            fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ author: html.author.value, content: html.content.value })
+            }).then(res => res.json()).catch(err => console.log(err));
+        }
+    }
+}
