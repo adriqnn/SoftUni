a4334ba8674830f524e895189332bf50ef681351
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-11 23:36:50 +0300

    05

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/app.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/app.js
@@ -1,1 +1,32 @@
-console.log('TODO:// Implement Home functionality');
+import { showView, loggedUser } from "./helpers.js";
+
+function loadApplication(){
+    const url = `http://localhost:3030/data/catches`;
+    const html = {
+        main: document.getElementById('html-main'),
+        views: document.getElementById('views'),
+        register: document.getElementById('register-view'),
+        login: document.getElementById('login-view'),
+        home: document.getElementById('home-view'),
+        homeBtn: document.getElementById('home'),
+        loginBtn: document.getElementById('login'),
+        registerBtn: document.getElementById('register'),
+    }
+
+    const access = {
+        user: document.getElementById('user'),
+        guest: document.getElementById('guest'),
+        greeting: document.getElementById('greeting')
+    }
+
+    showView('home', html);
+    loggedUser(access);
+
+    html.homeBtn.addEventListener('click', () => { showView('home', html) });
+    html.loginBtn.addEventListener('click', () => { showView('login', html) });
+    html.registerBtn.addEventListener('click', () => { showView('register', html) });
+
+
+}
+
+loadApplication();
