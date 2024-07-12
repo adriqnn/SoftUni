94b438d3f75035558018dc458a97a8c228540442
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-12 16:43:51 +0300

    05

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/login.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/login.js
@@ -1,1 +1,26 @@
-console.log('TODO:// Implement Login functionality');
+import { getUserDetails, loggedUser, sendData, showView } from "./helpers.js";
+
+async function login(e){
+    e.preventDefault();
+    await sendData(e, 'login');
+}
+
+async function logout(e){
+    e.preventDefault();
+
+    const userDetails = getUserDetails();
+
+    await fetch('http://localhost:3030/users/logout', {
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Authorization': userDetails.token
+        }
+    });
+
+    sessionStorage.clear();
+
+    loggedUser();
+    showView('home');
+}
+
+export { login, logout };
