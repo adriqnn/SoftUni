6aee3d29fefce046e78fe0ffc46808fb1c9997c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-12 16:38:22 +0300

    05

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/app.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGame/src/app.js
@@ -1,32 +1,36 @@
 import { showView, loggedUser } from "./helpers.js";
+import { login, logout } from "./login.js";
+import { register } from "./register.js";
+import { loadCatches, addCatch } from "./home.js";
 
 function loadApplication(){
     const url = `http://localhost:3030/data/catches`;
     const html = {
-        main: document.getElementById('html-main'),
-        views: document.getElementById('views'),
-        register: document.getElementById('register-view'),
-        login: document.getElementById('login-view'),
-        home: document.getElementById('home-view'),
         homeBtn: document.getElementById('home'),
         loginBtn: document.getElementById('login'),
         registerBtn: document.getElementById('register'),
+        loginForm: document.getElementById('loginForm'),
+        registerForm: document.getElementById('registerForm'),
+        loginFormBtn: document.querySelector('#loginForm button'),
+        registerFormBtn: document.querySelector('#registerForm button'),
+        logoutBtn: document.getElementById('logout'),
+        loadBtn: document.querySelector('button.load'),
+        addBtn: document.querySelector('#addForm button')
     }
 
-    const access = {
-        user: document.getElementById('user'),
-        guest: document.getElementById('guest'),
-        greeting: document.getElementById('greeting')
-    }
-
-    showView('home', html);
-    loggedUser(access);
+    showView('home');
+    loggedUser();
 
     html.homeBtn.addEventListener('click', () => { showView('home', html) });
     html.loginBtn.addEventListener('click', () => { showView('login', html) });
     html.registerBtn.addEventListener('click', () => { showView('register', html) });
 
+    html.loginFormBtn.addEventListener('click', (e) => login(e));
+    html.registerFormBtn.addEventListener('click', register);
+    html.logoutBtn.addEventListener('click', logout);
 
+    html.loadBtn.addEventListener('click', loadCatches);
+    html.addBtn.addEventListener('click', addCatch);
 }
 
 loadApplication();
