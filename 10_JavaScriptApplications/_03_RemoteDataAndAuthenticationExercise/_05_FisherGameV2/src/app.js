b6f380b4f3a20cb0f6b35564d0b948a026d7a506
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-12 18:51:16 +0300

    05

--- 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGameV2/src/app.js
+++ 10_JavaScriptApplications/_03_RemoteDataAndAuthenticationExercise/_05_FisherGameV2/src/app.js
@@ -19,28 +19,25 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-function logout() {
+function logout(){
     fetch('http://localhost:3030/users/logout', {
         method: 'get',
-        headers: {
-            'Content-Type': 'application/json',
-            'X-Authorization': userData.token
-        }
+        headers: { 'Content-Type': 'application/json', 'X-Authorization': userData.token }
     });
 
     sessionStorage.removeItem('userData');
     window.location = './index.html';
 }
 
-function invokeHandler(event) {
-    if (event.target.textContent == 'Update') {
+function invokeHandler(event){
+    if(event.target.textContent === 'Update'){
         updateCatch(event);
-    } else if (event.target.textContent == 'Delete') {
+    }else if(event.target.textContent === 'Delete'){
         deleteCatch(event);
     }
 }
 
-async function updateCatch(event) {
+async function updateCatch(event){
     const [angler, weight, species, location, bait, captureTime] = event.target.parentNode.querySelectorAll('input');
     const id = event.target.getAttribute('data-id');
     const token = userData.token;
@@ -48,112 +45,100 @@ async function updateCatch(event) {
     try{
         const response = await fetch('http://localhost:3030/data/catches/' + id, {
             method: 'put',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Authorization': token
-            },
-            body: JSON.stringify({
-                angler: angler.value, weight: weight.value, species: species.value,
-                location: location.value, bait: bait.value, 'captureTime ': captureTime.value
-            })
+            headers: { 'Content-Type': 'application/json', 'X-Authorization': token },
+            body: JSON.stringify( { angler: angler.value, weight: weight.value, species: species.value, location: location.value, bait: bait.value, 'captureTime ': captureTime.value })
         });
 
-        if(response.ok != true){
+        if(response.ok !== true){
             const error = await response.json();
             throw new Error(error.message);
         }
-    }
-    catch(err) {
+    }catch(err){
         alert(err.message);
     }
 }
 
-async function deleteCatch(event) {
+async function deleteCatch(event){
     const id = event.target.getAttribute('data-id');
     const token = userData.token;
 
-    try {
+    try{
         const response = await fetch('http://localhost:3030/data/catches/' + id, {
             method: 'delete',
             headers: { 'X-Authorization': token }
         });
 
-        if(response.ok != true){
+        if(response.ok !== true){
             const error = await response.json();
             throw new Error(error.message);
         }
+        
         loadData();
-    }
-    catch(err) {
+    }catch(err){
         alert(err.message);
     }
 }
 
-async function addCatch(event) {
+async function addCatch(event){
     event.preventDefault();
+    
     if(!userData){
         window.location ='./login.html';
         return;
     }
 
     const formData = new FormData(event.target);
-
     const data = [...formData.entries()].reduce((a, [k, v]) => Object.assign(a, {[k]: v}), {});
 
-    try {
-        if(Object.values(data).some(x => x == '')){
-            throw new Error('All fields are required!')
+    try{
+        if(Object.values(data).some(x => x === '')){
+            throw new Error('All fields are required!');
         }
 
         const response = await fetch('http://localhost:3030/data/catches/', {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-Authorization': userData.token
-            },
+            headers: { 'Content-Type': 'application/json', 'X-Authorization': userData.token },
             body: JSON.stringify(data)
         });
 
-        if(response.ok != true){
+        if(response.ok !== true){
             const error = await response.json();
             throw new Error(error.message);
         }
 
         loadData();
         event.target.reset();
-    }
-    catch(err) {
+    }catch(err){
         alert(err.message);
     }
 }
 
-async function loadData() {
+async function loadData(){
     const response = await fetch('http://localhost:3030/data/catches/');
     const data = await response.json();
 
     document.getElementById('catches').replaceChildren(...data.map(createDiv));
 
-    function createDiv(data) {
-        const isOwner = (userData && data._ownerId == userData.id);
-        if(userData && data._ownerId == userData.id){
-
-        }
+    function createDiv(data){
+        const isOwner = (userData && data._ownerId === userData.id);
+        
         const element = document.createElement('div');
-        element.className = 'catch'
+        element.className = 'catch';
         element.innerHTML =`<label>Angler</label>
-<input type="text" class="angler" value="${data.angler}"${!isOwner ? 'disabled' : ''}>
-<label>Weight</label>
-<input type="text" class="weight" value="${data.weight}"${!isOwner ? 'disabled' : ''}>
-<label>Species</label>
-<input type="text" class="species" value="${data.species}"${!isOwner ? 'disabled' : ''}>
-<label>Location</label>
-<input type="text" class="location" value="${data.location}"${!isOwner ? 'disabled' : ''}>
-<label>Bait</label>
-<input type="text" class="bait" value="${data.bait}"${!isOwner ? 'disabled' : ''}>
-<label>Capture Time</label>
-<input type="number" class="captureTime" value="${data.captureTime}"${!isOwner ? 'disabled' : ''}>
-<button class="update" data-id="${data._id}"${!isOwner ? 'disabled' : ''}>Update</button>
-<button class="delete" data-id="${data._id}"${!isOwner ? 'disabled' : ''}>Delete</button>`;
+                            <input type="text" class="angler" value="${data.angler}"${!isOwner ? 'disabled' : ''}>
+                            <label>Weight</label>
+                            <input type="text" class="weight" value="${data.weight}"${!isOwner ? 'disabled' : ''}>
+                            <label>Species</label>
+                            <input type="text" class="species" value="${data.species}"${!isOwner ? 'disabled' : ''}>
+                            <label>Location</label>
+                            <input type="text" class="location" value="${data.location}"${!isOwner ? 'disabled' : ''}>
+                            <label>Bait</label>
+                            <input type="text" class="bait" value="${data.bait}"${!isOwner ? 'disabled' : ''}>
+                            <label>Capture Time</label>
+                            <input type="number" class="captureTime" value="${data.captureTime}"${!isOwner ? 'disabled' : ''}>
+                            <button class="update" data-id="${data._id}"${!isOwner ? 'disabled' : ''}>Update</button>
+                            <button class="delete" data-id="${data._id}"${!isOwner ? 'disabled' : ''}>Delete</button>`;
+        
         return element;
     }
 }
