941263c594c20a52e7679541855e2108993caed7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-15 01:06:22 +0300

    01

--- 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/util.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/util.js
@@ -1,26 +1,26 @@
 import { showHome } from './home.js';
 
-export function checkUserNav() {
+export function checkUserNav(){
     const userData = JSON.parse(sessionStorage.getItem('userData'));
-    if (userData != null) {
+
+    if(userData !== null){
         document.getElementById('greeting').textContent = `Welcome, ${userData.email}!`;
         document.getElementById('userNav').style.display = 'inline-block';
         document.getElementById('guestNav').style.display = 'none';
-    } else {
+    }else{
         document.getElementById('userNav').style.display = 'none';
         document.getElementById('guestNav').style.display = 'inline-block';
     }
 }
 
-export function onLogout() {
+export function onLogout(){
     const userData = JSON.parse(sessionStorage.getItem('userData'));
 
     fetch('http://localhost:3030/users/logout', {
         method: 'get',
-        headers: {
-            'X-Authorization': userData.accessToken
-        }
+        headers: { 'X-Authorization': userData.accessToken }
     });
+    
     sessionStorage.removeItem('userData');
     checkUserNav();
     showHome();
