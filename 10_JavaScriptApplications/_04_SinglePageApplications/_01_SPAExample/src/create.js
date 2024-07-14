ab29b17f6e9a976e92b2bb793d8d183937fa632b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-15 01:01:56 +0300

    01

--- 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/create.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/create.js
@@ -15,20 +15,20 @@ async function onSubmit(event){
 
     const title = formData.get('title').trim();
 
-    try {
+    try{
         const res = await fetch('http://localhost:3030/data/movies', {
             method: 'post',
             headers: { 'Content-Type': 'application/json', 'X-Authorization': JSON.parse(sessionStorage.getItem('userData')).accessToken },
             body: JSON.stringify({ title })
         });
 
-        if (res.ok === false) {
+        if(res.ok === false){
             const error = await res.json();
             throw Error(error.message);
         }
 
         await showCatalog();
-    } catch (err) {
+    }catch(err){
         alert(err.message);
     }
 }
