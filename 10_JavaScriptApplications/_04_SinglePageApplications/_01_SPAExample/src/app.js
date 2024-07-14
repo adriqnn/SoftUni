f76ee996161aa49e4278b61534fa445caae9605c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-15 01:00:48 +0300

    01

--- 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/app.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_01_SPAExample/src/app.js
@@ -24,7 +24,7 @@ const sections = {
 };
 
 function onNavigate(event){
-    if (event.target.tagName === 'A'){
+    if(event.target.tagName === 'A'){
         const view = sections[event.target.id];
 
         if(typeof view === 'function'){
