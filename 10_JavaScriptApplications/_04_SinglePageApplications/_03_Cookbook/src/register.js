9b5ceb1a0af3b03d3eb802a4e44b47a6238bcd7f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-16 23:12:49 +0300

    03

--- 10_JavaScriptApplications/_04_SinglePageApplications/_03_Cookbook/src/register.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_03_Cookbook/src/register.js
@@ -1,11 +1,10 @@
 import { showCatalog } from './catalog.js';
 
-
 let main;
 let section;
 let setActiveNav;
 
-export function setupRegister(targetMain, targetSection, onActiveNav) {
+export function setupRegister(targetMain, targetSection, onActiveNav){
     main = targetMain;
     section = targetSection;
     setActiveNav = onActiveNav;
@@ -17,37 +16,37 @@ export function setupRegister(targetMain, targetSection, onActiveNav) {
         onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
     }));
 
-    async function onSubmit(data) {
-        if (data.password != data.rePass) {
+    async function onSubmit(data){
+        if(data.password !== data.rePass){
             return alert('Passwords don\'t match');
         }
 
         const body = JSON.stringify({
             email: data.email,
-            password: data.password,
+            password: data.password
         });
 
         try {
             const response = await fetch('http://localhost:3030/users/register', {
                 method: 'post',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
+                headers: { 'Content-Type': 'application/json' },
                 body
             });
+            
             const data = await response.json();
-            if (response.status == 200) {
+            
+            if(response.status === 200){
                 sessionStorage.setItem('authToken', data.accessToken);
                 sessionStorage.setItem('userId', data._id);
                 document.getElementById('user').style.display = 'inline-block';
                 document.getElementById('guest').style.display = 'none';
 
                 showCatalog();
-            } else {
+            }else{
                 alert(data.message);
                 throw new Error(data.message);
             }
-        } catch (err) {
+        }catch(err){
             console.error(err.message);
         }
     }
