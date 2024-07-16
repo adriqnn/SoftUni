6b240d2c0a05c9e31ca997a4cc8563f1be57e3a3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-16 23:17:38 +0300

    03

--- 10_JavaScriptApplications/_04_SinglePageApplications/_03_Cookbook/src/create.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_03_Cookbook/src/create.js
@@ -1,11 +1,10 @@
 import { showDetails } from './details.js';
 
-
 let main;
 let section;
 let setActiveNav;
 
-export function setupCreate(targetMain, targetSection, onActiveNav) {
+export function setupCreate(targetMain, targetSection, onActiveNav){
     main = targetMain;
     section = targetSection;
     setActiveNav = onActiveNav;
@@ -17,43 +16,41 @@ export function setupCreate(targetMain, targetSection, onActiveNav) {
         onSubmit([...formData.entries()].reduce((p, [k, v]) => Object.assign(p, { [k]: v }), {}));
     }));
 
-    async function onSubmit(data) {
+    async function onSubmit(data){
         const body = JSON.stringify({
             name: data.name,
             img: data.img,
-            ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l != ''),
-            steps: data.steps.split('\n').map(l => l.trim()).filter(l => l != '')
+            ingredients: data.ingredients.split('\n').map(l => l.trim()).filter(l => l !== ''),
+            steps: data.steps.split('\n').map(l => l.trim()).filter(l => l !== '')
         });
 
         const token = sessionStorage.getItem('authToken');
-        if (token == null) {
+        
+        if(token == null){
             return alert('You\'re not logged in!');
         }
 
-        try {
+        try{
             const response = await fetch('http://localhost:3030/data/recipes', {
                 method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Authorization': token
-                },
+                headers: { 'Content-Type': 'application/json', 'X-Authorization': token },
                 body
             });
 
-            if (response.status == 200) {
+            if(response.status === 200){
                 showDetails((await response.json())._id);
-            } else {
+            }else{
                 const error = await response.json();
                 throw new Error(error.message);
             }
-        } catch (err) {
+        }catch(err){
             alert(err.message);
             console.error(err.message);
         }
     }
 }
 
-export function showCreate() {
+export function showCreate(){
     setActiveNav('createLink');
     main.innerHTML = '';
     main.appendChild(section);
