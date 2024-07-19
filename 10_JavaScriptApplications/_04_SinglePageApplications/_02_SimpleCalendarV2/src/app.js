cac966562455a26ddf4d16ef71ef82400bea3a45
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-19 10:44:17 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplications/_02_SimpleCalendarV2/src/app.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_02_SimpleCalendarV2/src/app.js
@@ -11,27 +11,26 @@ const months = [...document.getElementsByClassName('daysCalendar')];
 loadView(yearsView);
 
 document.addEventListener('click', e => {
-        if(clickedValidYear(e)){
-            displayYear(years, e.target.textContent.trim());
-        }else if(clickedValidMonth(e)){
-            const year = document.querySelector('caption').innerText;
-            const month = e.target.innerText.trim();
-            const monthIndex = validMonths.findIndex(x => x === month);
+    if(clickedValidYear(e)){
+        displayYear(years, e.target.textContent.trim());
+    }else if(clickedValidMonth(e)){
+        const year = document.querySelector('caption').innerText;
+        const month = e.target.innerText.trim();
+        const monthIndex = validMonths.findIndex(x => x === month);
 
-            if(monthIndex !== -1){
-                displayMonth(months, monthIndex, year);
-            }
-        }
-
-        if(e.target.tagName === 'CAPTION'){
-            const captionText = e.target.innerText;
-
-            if(isNaN(captionText)){
-                const year = captionText.match(/[1-2]{1}[0-9]{3}/g);
-                displayYear(years, year);
-            }else{
-                displayYears(yearsView);
-            }
+        if(monthIndex !== -1){
+            displayMonth(months, monthIndex, year);
         }
     }
-)
+
+    if(e.target.tagName === 'CAPTION'){
+        const captionText = e.target.innerText;
+
+        if(isNaN(captionText)){
+            const year = captionText.match(/[1-2]{1}[0-9]{3}/g);
+            displayYear(years, year);
+        }else{
+            displayYears(yearsView);
+        }
+    }
+});
