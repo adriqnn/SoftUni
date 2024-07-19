5b9d84d92420b027785cef343b13baf6350a58d4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-19 10:45:13 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplications/_02_SimpleCalendarV2/src/helper.js
+++ 10_JavaScriptApplications/_04_SinglePageApplications/_02_SimpleCalendarV2/src/helper.js
@@ -1,6 +1,6 @@
-const clickedValidYear = e => e.path.some(x => x.id === 'years' && x.tagName === 'SECTION') && (e.target.className === 'date' || e.target.className === 'day');
+const clickedValidYear = (e) => e.composedPath().some(x => x.id === 'years' && x.tagName === 'SECTION') && (e.target.className === 'date' || e.target.className === 'day');
 
-const clickedValidMonth = e => e.path.some(x => x.className === 'monthCalendar' && x.tagName === 'SECTION');
+const clickedValidMonth = (e) => e.composedPath().some(x => x.className === 'monthCalendar' && x.tagName === 'SECTION');
 
 const validMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
