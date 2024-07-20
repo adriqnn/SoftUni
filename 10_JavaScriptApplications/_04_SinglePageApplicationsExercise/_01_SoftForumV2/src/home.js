7e4d8eee71ca299e15bdbdba8adc74bb7f5fe55b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-20 04:00:13 +0300

    01

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_01_SoftForumV2/src/home.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_01_SoftForumV2/src/home.js
@@ -1,6 +1,5 @@
 import { showDetails } from './details.js';
 
-
 const section = document.getElementById('homeView');
 section.querySelector('div.topic-title').addEventListener('click', showDetails);
 const form = section.querySelector('form');
@@ -10,7 +9,7 @@ const container = section.querySelector('.topic-container');
 
 section.remove();
 
-export async function showHome(ev) {
+export async function showHome(ev){
     ev?.preventDefault();
     document.getElementById('main').replaceChildren('Loading...');
 
@@ -22,30 +21,28 @@ export async function showHome(ev) {
     document.getElementById('main').replaceChildren(section);
 }
 
-function createPostPreview(post) {
+function createPostPreview(post){
     const element = document.createElement('div');
+
     element.className = 'topic-name-wrapper';
-    element.innerHTML = `
-    <div class="topic-name">
-        <a href="/details" class="normal" id="${post._id}">
-            <h2>${post.title}</h2>
-        </a>
-        <div class="columns">
-            <div>
-                <p>Date: <time>${post.dateCreated}</time></p>
-                <div class="nick-name">
-                    <p>Username: <span>${post.username}</span></p>
-                </div>
-            </div>
-
-
-        </div>
-    </div>`;
+    element.innerHTML = `<div class="topic-name">
+                             <a href="/details" class="normal" id="${post._id}">
+                                 <h2>${post.title}</h2>
+                             </a>
+                             <div class="columns">
+                                 <div>
+                                     <p>Date: <time>${post.dateCreated}</time></p>
+                                     <div class="nick-name">
+                                         <p>Username: <span>${post.username}</span></p>
+                                     </div>
+                                 </div>
+                             </div>
+                         </div>`;
 
     return element;
 }
 
-async function onSubmit(ev) {
+async function onSubmit(ev){
     ev.preventDefault();
     const formData = new FormData(form);
 
@@ -54,37 +51,29 @@ async function onSubmit(ev) {
     const username = formData.get('username').trim();
     const content = formData.get('postText').trim();
 
-    try {
-        if (title == '' || username == '' || content == '') {
+    try{
+        if(title === '' || username === '' || content === ''){
             throw new Error('All fields are required!');
         }
 
         const res = await fetch('http://localhost:3030/jsonstore/collections/myboard/posts', {
             method: 'post',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                title,
-                username,
-                content,
-                dateCreated: new Date()
-            })
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ title, username, content, dateCreated: new Date() })
         });
 
-        if (res.ok != true) {
+        if(res.ok !== true){
             const error = await res.json();
             throw new Error(error.message);
         }
 
         form.reset();
         showHome();
-
     } catch (err) {
         alert(err.message);
     }
 }
 
-function clearForm() {
+function clearForm(){
     form.reset();
 }
