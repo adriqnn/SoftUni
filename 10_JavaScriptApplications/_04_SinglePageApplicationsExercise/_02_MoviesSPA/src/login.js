3e4df403aac4a8ef188e03ca200ea9cae22a1151
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-22 22:30:46 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/login.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/login.js
@@ -1,13 +1,30 @@
 import { displayPage } from "./display.js";
+import { sendData } from "./helper.js";
+import { logoutReq } from "./requests.js";
 
-function login(e){
-    e.preventDefault();
-
-    displayPage('login');
+const html = {
+    loginForm: document.querySelector('#login-form')
 }
 
-function logout(){
-    displayPage('home');
+html.loginForm.addEventListener('submit', loginSubmit);
+
+async function login(e){
+    e.preventDefault();
+    await displayPage('login');
+}
+
+async function loginSubmit(e){
+    e.preventDefault();
+    await sendData(e, 'login');
+}
+
+async function logout(e){
+    e.preventDefault();
+
+    logoutReq();
+    sessionStorage.clear();
+
+    await displayPage('login');
 }
 
 export { login, logout };
