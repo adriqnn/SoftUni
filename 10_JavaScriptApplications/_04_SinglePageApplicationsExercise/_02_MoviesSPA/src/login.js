f24144424d063a969df93eee40023864731fc9af
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-22 22:30:57 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/login.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/login.js
@@ -1,30 +1,86 @@
 import { displayPage } from "./display.js";
-import { sendData } from "./helper.js";
-import { logoutReq } from "./requests.js";
+import { loadMoviesReq, addMovieReq, loadMovieReq, movieLikesReq, userLikedReq, deleteMovieReq, editMovieReq, likeMovieReq } from "./requests.js";
+import { getUserDetails } from "./util.js";
 
 const html = {
-    loginForm: document.querySelector('#login-form')
+    formAddMovie: document.querySelector('#add-movie form')
 }
 
-html.loginForm.addEventListener('submit', loginSubmit);
+html.formAddMovie.addEventListener('submit', addMovieSubmit);
 
-async function login(e){
+async function addMovie(e){
     e.preventDefault();
-    await displayPage('login');
+    await displayPage('addMovie');
 }
 
-async function loginSubmit(e){
-    e.preventDefault();
-    await sendData(e, 'login');
-}
-
-async function logout(e){
+async function addMovieSubmit(e){
     e.preventDefault();
 
-    logoutReq();
-    sessionStorage.clear();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries([...formData.entries()]);
 
-    await displayPage('login');
+    const movie = {
+        title: data.title,
+        description: data.description,
+        img: data.img
+    }
+
+    await addMovieReq(movie, e)
 }
 
-export { login, logout };
+async function loadAllMovies(){
+    return await loadMoviesReq();
+}
+
+async function loadMovie(id){
+    const user = getUserDetails();
+    const movie = await loadMovieReq(id);
+    const movieLikes = await movieLikesReq(user.id, movie._id);
+    const userLiked = await userLikedReq(user.id, movie._id);
+
+    const divEl = document.createElement('div');
+    divEl.className = 'container';
+    divEl.id = movie._id;
+    divEl.setAttribute('data-owner', movie._ownerId);
+
+    divEl.innerHTML = `<h1>${movie.title}</h1>
+                       <div class="row bg-light text-dark">
+                            <div class="col-md-8">
+                                <img class="img-thumbnail" src="${movie.img}" alt="Movie"/>
+                            </div>
+                            <div id="user-elements" class="col-md-4 text-center">
+                                <h3 class="my-3">Movie Description</h3>
+                                <p>
+                                  ${movie.description}
+                                </p>
+                                <a class="btn btn-danger" href="#">Delete</a>
+'                               <a class="btn btn-warning" href="#">Edit</a>
+                                <a class="btn btn-primary" href="#">Like</a>
+                                <span class="enrolled-span">Liked</span>
+                            </div>
+                       </div>`;
+
+    const ux = {
+        deleteBtn: divEl.querySelector('a.btn-danger'),
+        editBtn: divEl.querySelector('a.btn-warning'),
+        likeBtn: divEl.querySelector('a.btn-primary'),
+        liked: divEl.querySelector('span.enrolled-span'),
+        divContainer: divEl.querySelector('#user-elements')
+    }
+
+    ux.deleteBtn.addEventListener('click', deleteMovieReq);
+    ux.editBtn.addEventListener('click', editMovieReq);
+    ux.likeBtn.addEventListener('click', likeMovieReq);
+
+    ux.divContainer.innerHTML = `<h3 class="my-3">Movie Description</h3>
+                                 <p>
+                                   ${movie.description}
+                                 </p>${user?.id === movie._ownerId ? '<a class="btn btn-danger" href="#">Delete</a>' +
+        '                                                             <a class="btn btn-warning" href="#">Edit</a>' : ""}
+                                                                      <a class="btn btn-primary" href="#">Like</a>
+                                <span class="enrolled-span">Liked ${movieLikes}</span>`;
+
+    return divEl;
+}
+
+export { addMovie, loadMovie, loadAllMovies };
