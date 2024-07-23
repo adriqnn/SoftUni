10b11dd01dd8a2cd6365400ef287b726b6510f7d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-24 01:58:26 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/components/navBar.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/components/navBar.js
@@ -1,10 +1,10 @@
 import { getNavBarLinks, eFactory } from '../src/helper.js';
 
 const createLink = (text) => `<li class="nav-item">
-                                         <a class="nav-link" data-id=${text.toLocaleLowerCase()} href="javascript:void(0)">${text}</a>
-                                     </li>`;
+                                  <a class="nav-link" data-id=${text.startsWith('W') ? 'welcome' : text.toLocaleLowerCase()} href="javascript:void(0)">${text}</a>
+                              </li>`;
 
 const navBar = () => eFactory('nav', 'navbar navbar-expand-lg navbar-dark bg-dark', `<a class='navbar-brand text-light' data-id="homeBtn" href='javascript:void(0)'>Movies</a>
-                                                                                              <ul class="navbar-nav ml-auto">${getNavBarLinks().map(x => createLink(x)).join('')}</ul>`);
+                                                                                     <ul class="navbar-nav ml-auto">${getNavBarLinks().map(x => createLink(x)).join('')}</ul>`);
 
 export { navBar };
