f68385f1d2cb7ee29ba7c3331eebb99db1627d2b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-23 21:26:29 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/movies.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPA/src/movies.js
@@ -1,17 +1,12 @@
-import { displayPage } from "./display.js";
-import { loadMoviesReq, addMovieReq, loadMovieReq, movieLikesReq, userLikedReq, deleteMovieReq, editMovieReq, likeMovieReq } from "./requests.js";
+import { loadMoviesReq, addMovieReq, loadMovieReq, movieLikesReq, userLikedReq, deleteMovieReq, editMovieReq, likeMovieReq, unlikeMovieReq } from "./requests.js";
 import { getUserDetails } from "./util.js";
+import { displayMovieEdit } from "./display.js";
 
 const html = {
     formAddMovie: document.querySelector('#add-movie form')
 }
 
 html.formAddMovie.addEventListener('submit', addMovieSubmit);
-
-async function addMovie(e){
-    e.preventDefault();
-    await displayPage('addMovie');
-}
 
 async function addMovieSubmit(e){
     e.preventDefault();
@@ -25,7 +20,43 @@ async function addMovieSubmit(e){
         img: data.img
     }
 
-    await addMovieReq(movie, e)
+    await addMovieReq(movie, e);
+}
+
+async function editMoviePage(movie, e){
+    e.preventDefault();
+    displayMovieEdit(movie);
+}
+
+async function editMovie(e){
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries([...formData.entries()]);
+
+    const movie = {
+        title: data.title,
+        description: data.description,
+        img: data.img
+    }
+
+    await editMovieReq(movie, e);
+}
+
+async function likeMovie(e){
+    e.preventDefault();
+    const movieId = e.target.parentElement.parentElement.parentElement.id;
+    await likeMovieReq(movieId);
+}
+
+async function unlikeMovie(e, id, movieId){
+    e.preventDefault();
+    await unlikeMovieReq(id, movieId);
+}
+
+async function deleteMovie(e){
+    e.preventDefault();
+    await deleteMovieReq(e.target.parentElement.parentElement.parentElement.id);
 }
 
 async function loadAllMovies(){
@@ -54,33 +85,49 @@ async function loadMovie(id){
                                   ${movie.description}
                                 </p>
                                 <a class="btn btn-danger" href="#">Delete</a>
-'                               <a class="btn btn-warning" href="#">Edit</a>
-                                <a class="btn btn-primary" href="#">Like</a>
-                                <span class="enrolled-span">Liked</span>
+                                <a class="btn btn-warning" href="#">Edit</a>
+                                <a class="btn btn-primary" id="like" href="#">Like</a>
+                                <a class="btn btn-primary" id="unlike" href="#">Unlike</a>
+                                <span class="enrolled-span">Liked ${movieLikes}</span>
                             </div>
                        </div>`;
 
     const ux = {
         deleteBtn: divEl.querySelector('a.btn-danger'),
         editBtn: divEl.querySelector('a.btn-warning'),
-        likeBtn: divEl.querySelector('a.btn-primary'),
+        likeBtn: divEl.querySelector('#like'),
+        unlikeBtn: divEl.querySelector('#unlike'),
         liked: divEl.querySelector('span.enrolled-span'),
         divContainer: divEl.querySelector('#user-elements')
     }
 
-    ux.deleteBtn.addEventListener('click', deleteMovieReq);
-    ux.editBtn.addEventListener('click', editMovieReq);
-    ux.likeBtn.addEventListener('click', likeMovieReq);
+    ux.deleteBtn.addEventListener('click', deleteMovie);
+    ux.editBtn.addEventListener('click', (e) =>  editMoviePage(movie, e));
+    ux.likeBtn.addEventListener('click', likeMovie);
+    ux.unlikeBtn.addEventListener('click', (e) =>  unlikeMovie(e, userLiked[0]._id, movie._id));
 
-    ux.divContainer.innerHTML = `<h3 class="my-3">Movie Description</h3>
-                                 <p>
-                                   ${movie.description}
-                                 </p>${user?.id === movie._ownerId ? '<a class="btn btn-danger" href="#">Delete</a>' +
-        '                                                             <a class="btn btn-warning" href="#">Edit</a>' : ""}
-                                                                      <a class="btn btn-primary" href="#">Like</a>
-                                <span class="enrolled-span">Liked ${movieLikes}</span>`;
+    if(user?.id === movie._ownerId){
+        ux.deleteBtn.style.display = 'inline';
+        ux.editBtn.style.display = 'inline';
+    }else{
+        ux.deleteBtn.style.display = 'none';
+        ux.editBtn.style.display = 'none';
+    }
+
+    if(user){
+        if(userLiked.length > 0){
+            ux.likeBtn.style.display = 'none';
+            ux.unlikeBtn.style.display = 'inline';
+        }else{
+            ux.likeBtn.style.display = 'inline';
+            ux.unlikeBtn.style.display = 'none';
+        }
+    }else{
+        ux.likeBtn.style.display = 'none';
+        ux.unlikeBtn.style.display = 'none';
+    }
 
     return divEl;
 }
 
-export { addMovie, loadMovie, loadAllMovies };
+export { editMoviePage, editMovie, likeMovie, unlikeMovie, deleteMovie, loadMovie, loadAllMovies };
