dfb6f707176e69cc9eebcf77d17936d7381b5e62
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-24 01:55:43 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/components/footer.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/components/footer.js
@@ -1,7 +1,7 @@
 import { eFactory } from '../src/helper.js';
 
-const footer = eFactory('footerComponent', 'page-footerComponent font-small', `<div className="footer-copyright text-center py-3">© 2020
-                                                                                   <a href="#" className="text-dark">JS Applications</a>
-                                                                               </div>`);
+const footer = eFactory('footer', 'page-footer font-small', `<div class="footer-copyright text-center py-3">
+                                                                                       &copy; <a href="#" class="text-dark">JS Applications</a>
+                                                                                   </div>`);
 
 export { footer };
