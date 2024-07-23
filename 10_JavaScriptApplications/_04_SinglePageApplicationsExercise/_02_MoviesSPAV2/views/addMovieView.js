804c71d7fd2360228ad301141f24adc3295d853c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-24 02:18:20 +0300

    02

--- 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/views/addMovieView.js
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV2/views/addMovieView.js
@@ -9,13 +9,13 @@ const innerHTML = `<form class="text-center border border-light p-5" data-id="ad
                        <div class="form-group">
                            <label for="description">Movie Description</label>
                            <textarea class="form-control" placeholder="Description" name="description"></textarea>
-                           </div>
-                           <div class="form-group">
-                               <label for="imageUrl">Image url</label>
-                               <input type="text" class="form-control" placeholder="Image Url" name="img" value="">
-                           </div>
-                           <button type="submit" class="btn btn-primary">Submit</button>
-                     </form>`;
+                       </div>
+                       <div class="form-group">
+                           <label for="imageUrl">Image url</label>
+                           <input type="text" class="form-control" placeholder="Image Url" name="img" value="">
+                       </div>
+                       <button type="submit" class="btn btn-primary">Submit</button>
+                   </form>`;
 
 const addMovieView = eFactory('section', '', innerHTML);
 addMovieView.id = 'add-movie';
