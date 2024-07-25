d6bab036907c5cd8e9f9594c913e60e403e48faf
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-25 23:40:16 +0300

    Update display.js

---
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV4/src/display.js
@@ -0,0 +1,12 @@
+// function displayMovieEdit(movie){
+
+//
+//
+//
+//     divEl.querySelector('form').addEventListener('submit', editMovie);
+//
+//     views.editMovie.replaceChildren(divEl);
+//     html.container.appendChild(views.editMovie);
+//
+//     addFooter();
+// }
