57441e3d1173e77d55406a72a7072566bf47d3e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-25 23:41:22 +0300

    02

---
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV4/src/movieUtil.js
@@ -0,0 +1,32 @@
+import { get, post, put, del } from "./api.js";
+import { getUserDetails } from "./generalUtil.js";
+
+async function onLikeMovie(ctx, movieId){
+    const userDetails = getUserDetails();
+    const like = { movieId, ownerID: userDetails.id };
+
+    await post('/data/likes', like);
+
+    ctx.goTo('movieDetailsPage', movieId);
+}
+
+async function onUnlikeMovie(ctx, data){
+    await del(`/data/likes/${data.likeId}`);
+
+    ctx.goTo('movieDetailsPage', data.movieId);
+}
+
+async function onDeleteMovie(ctx, movieId){
+    await del(`/data/movies/${movieId}`);
+    ctx.goTo('homePage');
+}
+
+async function movieLikes(movieId){
+    return await get(`/data/likes?where=movieId%3D%22${movieId}%22&distinct=_ownerId&count`);
+}
+
+async function userLiked(userId, movieId){
+    return await get(`/data/likes?where=movieId%3D%22${movieId}%22%20and%20_ownerId%3D%22${userId}%22`);
+}
+
+export { onLikeMovie, onUnlikeMovie, onDeleteMovie, movieLikes, userLiked };
