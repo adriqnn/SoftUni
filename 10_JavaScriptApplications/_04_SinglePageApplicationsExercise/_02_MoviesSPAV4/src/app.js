01386ff3271813b4a846a64861529dd49df22e24
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-25 23:40:02 +0300

    02

---
+++ 10_JavaScriptApplications/_04_SinglePageApplicationsExercise/_02_MoviesSPAV4/src/app.js
@@ -0,0 +1,50 @@
+import { showHome } from "../views/home.js";
+import { showRegister } from "../views/register.js";
+import { showLogin } from "../views/login.js";
+import { showAddMovie } from "../views/addMovie.js";
+import { showEditMovie } from "../views/editMovie.js";
+import { showDetailsMovie } from "../views/detailsMovie.js";
+
+import { checkUserNav, onLogout } from "./generalUtil.js";
+import { onDeleteMovie, onLikeMovie, onUnlikeMovie } from "./movieUtil.js";
+import { render } from "./render.js";
+
+document.querySelector('nav').addEventListener('click', navigate);
+
+const sections = {
+    'homePage': showHome,
+    'addMoviePage': showAddMovie,
+    'editMoviePage': showEditMovie,
+    'movieDetailsPage': showDetailsMovie,
+    'loginPage': showLogin,
+    'registerPage': showRegister,
+    'logoutBtn': onLogout,
+    'likeMovieBtn': onLikeMovie,
+    'unlikeMovieBtn': onUnlikeMovie,
+    'deleteMovieBtn': onDeleteMovie
+}
+
+checkUserNav();
+goTo('homePage');
+
+function navigate(ev){
+    if(ev.target.tagName === 'A'){
+        const viewName = ev.target.id;
+
+        if(goTo(viewName)){
+            ev.preventDefault();
+        }
+    }
+}
+
+function goTo(viewName, pageDetails){
+    const view = sections[viewName];
+
+    if(typeof view === 'function'){
+        view({ render, goTo, checkUserNav }, pageDetails);
+
+        return true;
+    }else{
+        return false;
+    }
+}
