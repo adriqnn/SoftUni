b60e184c04e038019088532e4102327897f8b2cc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-29 01:31:58 +0300

    02

--- 10_JavaScriptApplications/_05_ArchitectureAndTesting/_02_Accordion/_02_Accordion.test.js
+++ 10_JavaScriptApplications/_05_ArchitectureAndTesting/_02_Accordion/_02_Accordion.test.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright-chromium');
 const { assert } = require('chai');
 let browser, page;
 
-describe('App: End-to-end testing', function (){
+describe('App: End-to-End testing', function (){
     this.timeout(4000);
 
     before(async () => { browser = await chromium.launch({ headless: false, slowMo: 500 }); });
@@ -12,8 +12,8 @@ describe('App: End-to-end testing', function (){
 
     it('Should check if we load the right sections', async () => {
         await page.goto('http://localhost:3000');
-
-        let location = await page.locator('.accordion .head > span');
+        //let content = await page.content();
+        let location = await page.locator('.accordion .head > span'); // Locate the info you want to test
         const titles = await location.evaluateAll((section) => section.map(s => s.textContent));
 
         assert.include(titles, 'Scalable Vector Graphics');
@@ -25,7 +25,7 @@ describe('App: End-to-end testing', function (){
     it('Should check if we reveal the hidden info', async () => {
         await page.goto('http://localhost:3000');
         await page.click('text=More');
-        const visible = await page.isVisible('.extra');
+        const visible = await page.isVisible('.extra'); // Hidden info
 
         assert.equal(visible, true);
     });
