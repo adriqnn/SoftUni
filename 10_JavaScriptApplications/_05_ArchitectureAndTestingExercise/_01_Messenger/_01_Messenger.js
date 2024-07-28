b4f206e31d4a5e56173d2fb9fe756ccec190ac0e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-29 01:50:02 +0300

    01

---
+++ 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_01_Messenger/_01_Messenger.js
@@ -0,0 +1,186 @@
+function messengerV1(){
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const html = {
+        messages: document.getElementById('messages'),
+        author: document.querySelectorAll('input')[0],
+        content: document.querySelectorAll('input')[1],
+        submit: document.getElementById('submit'),
+        refresh: document.getElementById('refresh')
+    }
+
+    refreshInfo();
+
+    html.submit.addEventListener('click', submitInfo);
+    html.refresh.addEventListener('click', refreshInfo);
+
+    async function refreshInfo(){
+        try{
+            const res = await fetch(url);
+
+            if(res.ok !== true){
+                throw new Error('Error');
+            }
+
+            const data = await res.json();
+            html.messages.innerHTML = Object.entries(data).map(e => `${e[1].author}: ${e[1].content}`).join("\n");
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    async function submitInfo(){
+        if(html.author.value !== '' && html.content.value !== ''){
+            const res = await fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ author: html.author.value, content: html.content.value })
+            });
+        }
+    }
+}
+
+messengerV1();
+
+function messengerV2(){
+    document.getElementById('submit').addEventListener('click', sendMessage);
+    document.getElementById('refresh').addEventListener('click', refresh);
+
+    async function sendMessage() {
+        const [name, message] = document.querySelectorAll('input');
+
+        if(name.value !== '' && message.value !== ''){
+            await fetch('http://localhost:3030/jsonstore/messenger', {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    author: name.value,
+                    content: message.value
+                })
+            });
+
+            name.value = '';
+            message.value = '';
+        }
+    }
+
+    async function refresh() {
+        const response = await fetch('http://localhost:3030/jsonstore/messenger');
+        const data = await response.json();
+
+        document.getElementById('messages').value = Object.values(data).map(m => `${m.author}: ${m.content}`).join('\n');
+    }
+}
+
+function messengerV3(){
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const html = {
+        messages: document.getElementById('messages'),
+        author: document.querySelectorAll('input')[0],
+        content: document.querySelectorAll('input')[1],
+        submit: document.getElementById('submit'),
+        refresh: document.getElementById('refresh')
+    }
+
+    refreshInfo();
+
+    html.submit.addEventListener('click', submitInfo);
+    html.refresh.addEventListener('click', refreshInfo);
+
+    function refreshInfo(){
+        fetch(url).then(res => {
+            if(res.ok !== true){
+                throw new Error('Error');
+            }
+
+            return res.json();
+        }).then(data => {
+            html.messages.value = Object.entries(data).map(e => `${e[1].author}: ${e[1].content}`).join("\n");
+        }).catch(err => {
+            console.log(err);
+        });
+    }
+
+    function submitInfo(){
+        if(html.author.value !== '' && html.content.value !== ''){
+            fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ author: html.author.value, content: html.content.value })
+            }).then(res => res.json()).catch(err => console.log(err));
+        }
+    }
+}
+
+function messengerV4(){
+    const url = 'http://localhost:3030/jsonstore/messenger';
+    const html = {
+        messages: document.getElementById('messages'),
+        author: document.querySelectorAll('input')[0],
+        content: document.querySelectorAll('input')[1],
+        submit: document.getElementById('submit'),
+        refresh: document.getElementById('refresh')
+    }
+
+    refreshInfo();
+
+    html.submit.addEventListener('click', submitInfo);
+    html.refresh.addEventListener('click', refreshInfo);
+
+    async function refreshInfo(){
+        try{
+            const res = await fetch(url);
+
+            if(res.ok !== true){
+                throw new Error('Error');
+            }
+
+            const data = await res.json();
+            html.messages.value = Object.entries(data).map(e => `${e[1].author}: ${e[1].content}`).join("\n");
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    async function submitInfo(){
+        if(html.author.value !== '' && html.content.value !== ''){
+            await fetch(url, {
+                method: 'post',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ author: html.author.value, content: html.content.value })
+            });
+
+            html.author.value = '';
+            html.content.value = '';
+        }
+    }
+}
+
+function messengerV5(){
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    const clearInputs = (arr) => arr.forEach(x => x.value = '');
+
+    document.getElementById(`submit`).addEventListener('click', async () => {
+        const author = document.querySelector('#controls > input[type=text]:nth-child(2)');
+        const content = document.querySelector('#controls > input[type=text]:nth-child(5)');
+        const rawData = { author: author.value, content: content.value };
+
+        const response = await fetch(url, {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(rawData)
+        });
+
+        clearInputs([author, content]);
+
+        return response;
+    });
+
+    document.getElementById('refresh').addEventListener('click', async () => {
+        const data = await fetch(url);
+        const deserialized = await data.json();
+
+        console.log(deserialized);
+        document.getElementById(`messages`).value = Object.values(deserialized).map(x => `${x.author}: ${x.content}`).join('\n')
+    });
+}
