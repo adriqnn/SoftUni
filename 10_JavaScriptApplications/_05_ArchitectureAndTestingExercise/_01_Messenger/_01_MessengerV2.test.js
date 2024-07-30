f7ea9a3312cbc5505eccd6dbc8e0cb9e9d9c6f39
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-30 13:09:10 +0300

    01

--- 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_01_Messenger/_01_MessengerV2.test.js
+++ 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_01_Messenger/_01_MessengerV2.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 
 let browser, page; // Declare reusable variables
 
-describe('E2E tests', function () {
+describe('E2E testing', function () {
     this.timeout(6000);
 
     before(async () => { browser = await chromium.launch({headless: false, slowMo: 500}); });
@@ -11,7 +11,7 @@ describe('E2E tests', function () {
     beforeEach(async () => { page = await browser.newPage(); });
     afterEach(async () => { await page.close(); });
 
-    it('load messages', async () => {
+    it('Load messages.', async () => {
         await page.goto('http://localhost:63343/.../_01_Messenger');
 
         const response = await Promise.all([
@@ -28,7 +28,7 @@ describe('E2E tests', function () {
         expect(content).to.contains(`Ivan: Hello, George nice to see you! :)))`);
     });
 
-    it('send message', async () => {
+    it('Send message.', async () => {
         await page.goto('http://localhost:63343/.../_01_Messenger');
         await page.fill('#author', 'User');
         await page.fill('#content', 'message');
