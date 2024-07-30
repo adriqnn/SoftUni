6a37033a59e2a7fe322411f0801b850a7d26ee50
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-31 02:45:12 +0300

    03

--- 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_03_SoftTerest/html/register.js
+++ 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_03_SoftTerest/html/register.js
@@ -1,34 +1,34 @@
 import { eFactory } from "./factory.js";
 
 const registerHTML = eFactory('div', 'container home wrapper  my-md-5 pl-md-5', `<div class="row-form d-md-flex flex-mb-equal">
-                                                                                                                         <div class="col-md-4">
-                                                                                                                             <img class="responsive" src="./images/idea.png" alt="">
-                                                                                                                         </div>
-                                                                                                                         <form class="form-user col-md-7">
-                                                                                                                             <div class="text-center mb-4">
-                                                                                                                                 <h1 class="h3 mb-3 font-weight-normal">Register</h1>
-                                                                                                                             </div>
-                                                                                                                             <div class="form-label-group">
-                                                                                                                                 <label for="email">Email</label>
-                                                                                                                                 <input type="text" id="email" name="email" class="form-control" placeholder="Email" required="" autofocus="">
-                                                                                                                             </div>
-                                                                                                                             <div class="form-label-group">
-                                                                                                                                 <label for="password">Password</label>
-                                                                                                                                 <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
-                                                                                                                             </div>
-                                                                                                                             <div class="form-label-group">
-                                                                                                                                 <label for="inputRepeatPassword">Repeat Password</label>
-                                                                                                                                 <input type="password" id="inputRepeatPassword" name="repeatPassword" class="form-control" placeholder="Repeat Password" required="">
-                                                                                                                             </div>
-                                                                                                                             <button class="btn btn-lg btn-dark btn-block" type="submit">Sign Up</button>
-                                                                                                                             <div class="text-center mb-4">
-                                                                                                                                 <p class="alreadyUser">Don't have account? Then just
-                                                                                                                                     <a href="/login" id="sign-in">Sign-In</a>!
-                                                                                                                                 </p>
-                                                                                                                             </div>
-                                                                                                                             <p class="mt-5 mb-3 text-muted text-center">© SoftTerest.</p>
-                                                                                                                         </form>
-                                                                                                                         </div>
-                                                                                                                     </div>`);
+                                                                                     <div class="col-md-4">
+                                                                                         <img class="responsive" src="./images/idea.png" alt="">
+                                                                                     </div>
+                                                                                     <form class="form-user col-md-7">
+                                                                                         <div class="text-center mb-4">
+                                                                                             <h1 class="h3 mb-3 font-weight-normal">Register</h1>
+                                                                                         </div>
+                                                                                         <div class="form-label-group">
+                                                                                             <label for="email">Email</label>
+                                                                                             <input type="text" id="email" name="email" class="form-control" placeholder="Email" required="" autofocus="">
+                                                                                         </div>
+                                                                                         <div class="form-label-group">
+                                                                                             <label for="password">Password</label>
+                                                                                             <input type="password" id="password" name="password" class="form-control" placeholder="Password" required="">
+                                                                                         </div>
+                                                                                         <div class="form-label-group">
+                                                                                             <label for="inputRepeatPassword">Repeat Password</label>
+                                                                                             <input type="password" id="inputRepeatPassword" name="repeatPassword" class="form-control" placeholder="Repeat Password" required="">
+                                                                                         </div>
+                                                                                         <button class="btn btn-lg btn-dark btn-block" type="submit">Sign Up</button>
+                                                                                         <div class="text-center mb-4">
+                                                                                             <p class="alreadyUser">Don't have account? Then just
+                                                                                                 <a href="/login" id="sign-in">Sign-In</a>!
+                                                                                             </p>
+                                                                                         </div>
+                                                                                         <p class="mt-5 mb-3 text-muted text-center">© SoftTerest.</p>
+                                                                                     </form>
+                                                                                     </div>
+                                                                                 </div>`);
 
 export { registerHTML };
