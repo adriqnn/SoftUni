0a3a9422711c0d091c10110ee09043013d4e9af9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-07-31 02:39:44 +0300

    03

---
+++ 10_JavaScriptApplications/_05_ArchitectureAndTestingExercise/_03_SoftTerest/html/catalog.js
@@ -0,0 +1,23 @@
+import { eFactory } from "./factory.js";
+
+const catalogHTML = eFactory('div', 'dashboard-holder', ``);
+catalogHTML.id = `dashboard-holder`;
+
+const emptyCatalogHTML = eFactory('div', 'dashboard-holder', `<h1>No ideas yet! Be the first one :)</h1>`);
+emptyCatalogHTML.id = `dashboard-holder`;
+
+function catalogIdeaTemplate(idea){
+    const el = eFactory('div', '', `<div class="card overflow-hidden current-card details" style="width: 20rem; height: 360px;">
+                                                                             <div class="card-body">
+                                                                                 <p class="card-text">${idea.title}</p>
+                                                                             </div>
+                                                                             <img class="card-image" src="${idea.img}" alt="Card image cap">
+                                                                             <a class="btn" href="/details-${idea._id}">Details</a>
+                                                                         </div>`);
+
+    el.id = idea._id;
+
+    return el;
+}
+
+export { catalogHTML, emptyCatalogHTML, catalogIdeaTemplate };
