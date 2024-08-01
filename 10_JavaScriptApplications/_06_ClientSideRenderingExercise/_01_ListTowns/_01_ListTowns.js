55bd367c52d82cfe8b819cb35ddc60460486d058
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-02 01:46:54 +0300

    01

---
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_01_ListTowns/_01_ListTowns.js
@@ -0,0 +1,24 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+
+function list_of_townsV1(){
+    const htmlElements = {
+        input: document.getElementById('towns'),
+        btn: document.getElementById('btnLoadTowns'),
+        root: document.getElementById('root')
+    }
+
+    htmlElements.btn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        let input = htmlElements.input.value;
+        let townsList = input.split(', ');
+
+        let cardTemplate = html `<ul>
+                                     ${townsList.map(t => html `<li>${t}</li>`)}
+                                 </ul>`;
+
+        render(cardTemplate, htmlElements.root);
+    });
+}
+
+list_of_townsV1();
