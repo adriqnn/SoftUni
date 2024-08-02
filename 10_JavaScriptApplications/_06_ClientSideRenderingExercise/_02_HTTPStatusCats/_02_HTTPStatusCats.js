baa24ef6b73719ac9223e5b020aaff5fcc4c59f6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-03 01:58:04 +0300

    02

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_02_HTTPStatusCats/_02_HTTPStatusCats.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_02_HTTPStatusCats/_02_HTTPStatusCats.js
@@ -41,3 +41,41 @@ function http_status_catsV1(){
 }
 
 http_status_catsV1();
+
+function http_status_catsV2(){
+    class Cat extends LitElement{
+        constructor(cat){
+            super();
+            this.id = cat.id;
+            this.statusCode = cat.statusCode;
+            this.statusMessage = cat.statusMessage;
+            this.imageLocation = cat.imageLocation;
+            this.state = { display: 'none' };
+        }
+
+        changeVisibility = () => {
+            const oldValue = this.state.display;
+            this.state.display === 'none' ? this.state.display = 'block' : this.state.display = 'none';
+
+            this.requestUpdate(this.state.display, oldValue);
+        }
+
+        render(){
+            return html `<img src="./images/${this.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+                         <div class="info">
+                             <button class="showBtn" @click=${this.changeVisibility}>
+                                 Show status code
+                             </button>
+                             <div class="status" style="display: ${this.state.display}" id=${this.id}>
+                                 <h4>Status Code: ${this.statusCode}</h4>
+                                 <p>${this.statusMessage}</p>
+                             </div>
+                         </div>`;
+        }
+    }
+
+    customElements.define('cat-comp', Cat);
+    const catsHTML = cats => html `<ul>${cats.map(x => html `<li>${new Cat(x)}</li>`)}</ul>`;
+
+    render(catsHTML(cats), document.getElementById('allCats'));
+}
