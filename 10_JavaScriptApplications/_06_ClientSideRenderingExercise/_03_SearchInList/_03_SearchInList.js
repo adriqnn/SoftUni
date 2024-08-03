f41da0da460a7da9b7ac7b16b46a9ceb7b6f26b3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-03 21:45:24 +0300

    03

---
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_03_SearchInList/_03_SearchInList.js
@@ -0,0 +1,44 @@
+import { html, render } from "../node_modules/lit-html/lit-html.js";
+import { towns } from "./_03_Towns.js";
+
+function search_in_listV1(){
+   const htmlElements = {
+       towns: document.getElementById('towns'),
+       input: document.getElementById('searchText'),
+       btn: document.querySelector('button'),
+       result: document.getElementById('result')
+   }
+
+   const ulEl = document.createElement('ul');
+
+   const liEl = (e) => html `<li>${e}</li>`;
+   render(towns.map(e => liEl(e)), ulEl);
+   htmlElements.towns.appendChild(ulEl);
+
+   htmlElements.btn.addEventListener('click', onClick);
+
+   function onClick(){
+       const search = htmlElements.input.value.toLowerCase();
+       const liList = document.querySelectorAll('li');
+
+       htmlElements.result.innerHTML = '';
+
+       if(search === ''){
+           return;
+       }
+
+       liList.forEach(e => {
+           e.style.fontWeight = 'normal';
+           e.style.textDecoration = 'none';
+       })
+
+       let filtered = [...liList].filter(e => e.textContent.toLowerCase().includes(search)).map(e => {
+           e.style.fontWeight = 'bold';
+           e.style.textDecoration = 'underline';
+       });
+
+       htmlElements.result.textContent = `${filtered.length} matches found.`;
+   }
+}
+
+search_in_listV1();
