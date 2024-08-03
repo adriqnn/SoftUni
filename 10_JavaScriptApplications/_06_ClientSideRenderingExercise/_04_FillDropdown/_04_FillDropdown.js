714dd32b13f9a3affe06c569772b7357eada0f58
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-04 02:55:37 +0300

    04

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_04_FillDropdown/_04_FillDropdown.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_04_FillDropdown/_04_FillDropdown.js
@@ -43,3 +43,60 @@ function fill_dropdownV1(){
 }
 
 fill_dropdownV1();
+
+async function fill_dropdownV2(){
+    document.querySelector('body').innerHTML = '';
+    const option = ({ text, _id }) => html `<option value=${_id}>${text}</option>`;
+    const dropdownTemp = (options) => html `<h1>Dropdown Menu</h1>
+                                                <article>
+                                                    <div>
+                                                        <select id="menu">
+                                                            ${options.map(x => option(x))}
+                                                        </select>
+                                                    </div>
+                                                    <form>
+                                                        <label for="itemText">Text:</label>
+                                                        <input type="text" id="itemText"/>
+                                                        <input type="submit" value="Add">
+                                                    </form>
+                                                </article>`;
+
+    async function drawOptions (){
+        const data = await getData();
+        render(dropdownTemp(Object.values(data)), document.querySelector('body'));
+    }
+
+    await drawOptions();
+
+    document.addEventListener('submit', async e => {
+        e.preventDefault();
+        const input = document.getElementById(`itemText`);
+
+        await postEntry({ text: input.value});
+        await drawOptions();
+        input.value = '';
+    });
+
+    async function checkOkToJSON (response){
+        if(!response.ok){
+            throw new Error(response.status);
+        }
+
+        return await response.json();
+    }
+
+    async function getData (){
+        const response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown ');
+        return await checkOkToJSON(response);
+    }
+
+    async function postEntry (entry) {
+        const response = await fetch('http://localhost:3030/jsonstore/advanced/dropdown ', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(entry)
+        });
+
+        return await checkOkToJSON(response);
+    }
+}
