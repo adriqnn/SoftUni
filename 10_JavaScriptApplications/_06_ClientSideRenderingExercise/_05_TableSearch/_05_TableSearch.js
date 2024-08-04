3fb48416491ce70c229724163ae50976e37dc482
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-04 23:54:29 +0300

    05

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_05_TableSearch/_05_TableSearch.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_05_TableSearch/_05_TableSearch.js
@@ -1,8 +1,50 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from "../node_modules/lit-html/lit-html.js";
 
-   function onClick() {
-      //   TODO:
+function table_searchV1() {
+   const htmlElements = {
+      tbody: document.querySelector('tbody'),
+      input: document.querySelector('input'),
+      btn: document.querySelector('#searchBtn')
+   }
 
+   populateTable();
+   htmlElements.btn.addEventListener('click', onClick);
+
+   async function populateTable(){
+      try{
+         const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
+
+         if(!res.ok){
+            throw new Error('Error');
+         }
+
+         const data = await res.json();
+
+         const row = (e) => html `<tr id="${e._id}">
+                                      <td>${e.firstName} ${e.lastName}</td>
+                                      <td>${e.email}</td>
+                                      <td>${e.course}</td>
+                                   </tr>`;
+
+         render(Object.values(data).map(e => row(e)), htmlElements.tbody);
+      }catch(err){
+         console.log(err.message);
+      }
+   }
+
+   function onClick(){
+      let rows = document.querySelectorAll('tbody tr');
+
+      for(let row of rows){
+         row.classList.remove('select');
+
+         if(row.innerHTML.includes(htmlElements.input.value)){
+            row.className = 'select';
+         }
+      }
+
+      htmlElements.input.value = '';
    }
 }
+
+table_searchV1();
