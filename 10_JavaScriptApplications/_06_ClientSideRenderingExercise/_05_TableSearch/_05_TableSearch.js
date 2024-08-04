d3eaffd55b7f50e4026044b22b7e80be73820446
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-05 00:00:34 +0300

    05

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_05_TableSearch/_05_TableSearch.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_05_TableSearch/_05_TableSearch.js
@@ -48,3 +48,50 @@ function table_searchV1() {
 }
 
 table_searchV1();
+
+function table_searchV2() {
+   const htmlElements = {
+      tbody: document.querySelector('tbody'),
+      input: document.querySelector('input'),
+      btn: document.querySelector('#searchBtn')
+   }
+
+   populateTable();
+   htmlElements.btn.addEventListener('click', onClick);
+
+   async function populateTable(){
+      try{
+         const res = await fetch('http://localhost:3030/jsonstore/advanced/table');
+
+         if(!res.ok){
+            throw new Error('Error');
+         }
+
+         const data = await res.json();
+
+         const row = (e) => html `<tr id="${e._id}">
+                                      <td>${e.firstName} ${e.lastName}</td>
+                                      <td>${e.email}</td>
+                                      <td>${e.course}</td>
+                                   </tr>`;
+
+         render(Object.values(data).map(e => row(e)), htmlElements.tbody);
+      }catch(err){
+         console.log(err.message);
+      }
+   }
+
+   function onClick(){
+      let rows = document.querySelectorAll('tbody tr');
+
+      for(let row of rows){
+         row.classList.remove('select');
+
+         if(row.innerHTML.toLowerCase().includes(htmlElements.input.value.toLowerCase())){
+            row.className = 'select';
+         }
+      }
+
+      htmlElements.input.value = '';
+   }
+}
