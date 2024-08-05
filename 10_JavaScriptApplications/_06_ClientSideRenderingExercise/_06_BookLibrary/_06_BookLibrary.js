082fa4b4fe30436ed34870b9445cdbd4d4372559
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-05 21:46:37 +0300

    06

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_06_BookLibrary/_06_BookLibrary.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_06_BookLibrary/_06_BookLibrary.js
@@ -200,3 +200,89 @@ function book_libraryV2(){
         render(html ``, document.querySelector('#editDiv'));
     }
 }
+
+function book_libraryV3(){
+    const url = `http://localhost:3030/jsonstore/collections/books`;
+    const htmlElements = {
+        body: document.querySelector('body')
+    }
+
+    const button = html `<button id="loadBooks" @click="${loadAllBooks}">LOAD ALL BOOKS</button>`;
+    const emptyDiv = html `<div id="editDiv"></div>`;
+    const table = html `<table><thead><tr><th>Title</th><th>Author</th><th>Action</th></tr></thead><tbody></tbody></table>`;
+    const createForm = html `<form id="createForm"><h3>FORM</h3><label>TITLE</label><input type="text" name="title" placeholder="Title..."><label>AUTHOR</label><input type="text" name="author" placeholder="Author..."><button id="create" @click="${createBook}">Submit</button></form>`;
+
+    const elementsArray = [ button, table, createForm, emptyDiv ];
+    render(elementsArray, htmlElements.body);
+
+    async function loadAllBooks(){
+        const tbody = document.querySelector('tbody');
+
+        try{
+            const res = await fetch(url);
+            const data = await res.json();
+
+            const trEl = (book) => html `<tr id="${book[0]}"><td>${book[1].title}</td><td>${book[1].author}</td><td><button @click="${showEdit}">Edit</button><button @click="${deleteBook}">Delete</button></td></tr>`;
+            render(Object.entries(data).map(e => trEl(e)), tbody);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    async function createBook(e){
+        e.preventDefault();
+
+        await saveData(e, 'post');
+    }
+
+    async function deleteBook(e){
+        e.preventDefault();
+
+        await fetch(`${url}/${e.target.parentElement.parentElement.id}`, { method: 'delete'});
+        await loadAllBooks();
+    }
+
+    async function editBook(e){
+        e.preventDefault();
+
+        await saveData(e, 'put', e.target.parentElement.id);
+        removeEditForm();
+    }
+
+    function showEdit(e){
+        document.querySelector('#createForm').style.display = 'none';
+
+        const parentElement = e.target.parentElement.parentElement;
+
+        const title = parentElement.firstElementChild.textContent;
+        const author = parentElement.firstElementChild.nextElementSibling.textContent;
+        const id = parentElement.id;
+
+        const editForm = html `<form id="${id}"><h3>Edit FORM</h3><label>TITLE</label><input type="text" name="title" placeholder="Title..." value="${title}"><label>AUTHOR</label><input type="text" name="author" placeholder="Author..." value="${author}"><button id="edit" @click="${editBook}">Save</button><button id="cancel" @click="${cancelEdit}">Cancel</button></form>`;
+        render(editForm, document.querySelector('#editDiv'));
+    }
+
+    function cancelEdit(){
+        clear();
+    }
+
+    async function saveData(e, method, id){
+        const form = e.target.parentElement;
+        const formData = new FormData(e.target.parentElement);
+        const urlSaveData = method === 'post' ? url : `${url}/${id}`;
+
+        const entry = Object.fromEntries([...formData.entries()]);
+        const hasEmptyStrings = Object.entries(entry).some(([key, value]) => value === '');
+
+        if(!hasEmptyStrings){
+            await fetch(urlSaveData, { method, headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(entry)});
+            await loadAllBooks();
+            form.reset();
+        }
+    }
+
+    function removeEditForm(){
+        document.querySelector('#createForm').style.display = 'block';
+        render(html ``, document.querySelector('#editDiv'));
+    }
+}
