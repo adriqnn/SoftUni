50e6b54fe9b69870e6d779e8dfafad374a845c68
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-06 02:42:45 +0300

    01

---
+++ 10_JavaScriptApplications/_07_Routing/_01_RoutingExampleHash/_01_RoutingExample.js
@@ -0,0 +1,26 @@
+import { createHashRouter } from './_01_HashRouter.js';
+
+const views = {
+    '#': () => '<h2>Home Page</h2><p>Welcome to our site!</p>',
+    '#home': () => '<h2>Home Page</h2><p>Welcome to our site!</p>',
+    '#catalog': () => '<h2>Catalog</h2><ul><li>Product 1</li><li>Product 2</li><li>Product 3</li></ul>',
+    '#about': () => '<h2>About Us</h2><p>Contact: +1-555-6197</p>',
+};
+
+const main = document.querySelector('main');
+
+const getName = createHashRouter(main, views, start);
+
+// Start application
+start(getName());
+
+function start(name) {
+    const view = views[name];
+
+    if (typeof view == 'function') {
+        main.innerHTML = view();
+        return true;
+    }
+
+    return false;
+}
