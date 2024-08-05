8951bcb7ad4e3a094f725d9be87e7307bb07a950
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-05 23:17:23 +0300

    06

--- 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_06_BookLibrary/_06_BookLibrary.js
+++ 10_JavaScriptApplications/_06_ClientSideRenderingExercise/_06_BookLibrary/_06_BookLibrary.js
@@ -286,3 +286,97 @@ function book_libraryV3(){
         render(html ``, document.querySelector('#editDiv'));
     }
 }
+
+function book_libraryV4(){
+    const dataTd = (entryData) => html `<td>${entryData}</td>`;
+    const tBodyTr = ([id, data]) => html `<tr id=${id}>${Object.entries(data).filter(x => x[0] !== '_id').map(x => dataTd(x[1]))}<td><button class="editBookBtn">Edit</button><button class="deleteBookBtn">Delete</button></td></tr>`;
+    const tableTemp = (booksData = []) => html `<thead><tr><th>Title</th><th>Author</th><th>Action</th></tr></thead><tbody>${booksData.map(x => tBodyTr(x))}</tbody>`;
+    const formTemplate = (className = 'createForm', submitText = 'Submit', id = '') => html `<form id=${id} class=${className}><h3>${submitText === 'Submit' ? 'Create book' : 'Edit book'}</h3><label>TITLE</label><input type="text" name="title" placeholder="Title..."><label>AUTHOR</label><input type="text" name="author" placeholder="Author..."><input type="submit" .value=${submitText}></form>`;
+    const indexPage = (booksData = []) => html `<button id="loadBooks">LOAD ALL BOOKS</button><div id="forms"><table>${tableTemp(booksData)}</table><div id="formDiv">${formTemplate()}</div></div>`;
+
+    const request = async (url, options = {}) => {
+        const response = await fetch(url, options);
+        return response.json();
+    }
+
+    const createOptions = (method = 'GET', headers = {}, data = {}) => {
+        return {
+            method,
+            headers: { ...{ 'Content-Type': 'application/json', }, ...headers },
+            body: JSON.stringify(data)
+        }
+    }
+
+    const baseUrl = 'http://localhost:3030/jsonstore/collections/books';
+
+    const loadBooks = async () => await request(baseUrl);
+    const createBook = async data => request(baseUrl, createOptions('POST', undefined, data));
+    const getBook = async id => request(`${baseUrl}/${id}`);
+    const updateBook = async (id, data) => await request(`${baseUrl}/${id}`, createOptions('PUT', undefined, data));
+    const deleteBook = async (id) => request(`${baseUrl}/${id}`, createOptions('DELETE', undefined));
+
+    const clearInputs = (...inputs) => inputs.forEach(x => x.value = '');
+    const getId = e => e.target.parentNode.parentNode.id;
+    const checkValidInput = (formData) => formData.every(x => x !== '' && x !== ' ');
+
+    const body = document.querySelector('body');
+    render(indexPage(), body);
+
+    const clearInput = () => clearInputs(document.querySelector('input[name="title"]'), document.querySelector('input[name="author"]'));
+
+    document.addEventListener('click', e => {
+        if(e.target.tagName === 'BUTTON'){
+            const id = e.target.id ? e.target.id : e.target.className;
+
+            const actions = {
+                'loadBooks': async () => {
+                    const booksData = Object.entries(await loadBooks());
+                    render(indexPage(booksData), body);
+                },
+                'editBookBtn': async () => {
+                    const bookResponse = await getBook(getId(e));
+                    render(formTemplate('editForm', 'Save', getId(e)), document.getElementById('formDiv'));
+
+                    document.querySelector('input[name="title"]').value = bookResponse.title;
+                    document.querySelector('input[name="author"]').value = bookResponse.author;
+                },
+                'deleteBookBtn': async () => {
+                    await deleteBook(getId(e));
+                    const bookData = await loadBooks();
+
+                    render(tableTemp(Object.entries(bookData)), document.querySelector('table'));
+                }
+            }
+
+            actions[id]();
+        }
+    });
+
+    document.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const className = e.target.className;
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries([...formData.entries()]);
+
+        const actions = {
+            editForm: async () => {
+                await updateBook(e.target.id, data);
+                const booksData = Object.entries(await loadBooks());
+
+                render(indexPage(booksData), body);
+                render(formTemplate('createForm', 'Submit'), document.getElementById('formDiv'));
+                clearInput();
+            },
+            createForm: async () => {
+                await createBook(data);
+                clearInput();
+                render(indexPage(Object.entries(await loadBooks())), body);
+            }
+        }
+
+        if(checkValidInput(Object.values(data))){
+            actions[className]();
+        }
+    });
+}
