aea4218b45650781ec2e52bf7b6aaa8d6c95e1c3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-07 23:02:53 +0300

    02

--- 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_02_BusSchedule/_02_BusSchedule.js
+++ 10_JavaScriptApplications/_02_AsynchronousProgrammingExercise/_02_BusSchedule/_02_BusSchedule.js
@@ -135,3 +135,40 @@ function bus_scheduleV3(){
         arrive
     }
 }
+
+function bus_scheduleV4(){
+    let infoElement = document.querySelector('.info');
+    let departButton = document.getElementById('depart');
+    let arriveButton = document.getElementById('arrive');
+
+    let busStop = {
+        next: 'depot'
+    }
+
+    function depart(){
+        departButton.disabled = true;
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${busStop.next}`;
+
+        fetch(url).then(response => response.json()).then(data => {
+                busStop = JSON.parse(JSON.stringify(data)); // Object.assign(data) breaks the reference so we dont change the data
+                infoElement.textContent = `Next stop ${busStop.name}`;
+            }).catch(error => {
+                infoElement.textContent = "Error";
+                departButton.disabled = true;
+                arriveButton.disabled = true;
+            });
+        
+        arriveButton.disabled = false;
+    }
+
+    function arrive(){
+        infoElement.textContent = `Arriving at ${busStop.name}`;
+        departButton.disabled = false;
+        arriveButton.disabled = true;
+    }
+
+    return {
+        depart,
+        arrive
+    };
+}
