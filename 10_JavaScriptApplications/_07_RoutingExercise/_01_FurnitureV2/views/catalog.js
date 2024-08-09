68694f7a32258e7e15b3b6c7591167c372669d63
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-10 00:50:51 +0300

    Delete 10_JavaScriptApplications/_07_RoutingExercise/_01_FurnitureV2 directory

--- 10_JavaScriptApplications/_07_RoutingExercise/_01_FurnitureV2/views/catalog.js
+++
@@ -1,7 +0,0 @@
-import { get } from "../src/api.js";
-import { catalog } from "../html/catalog.js";
-
-export async function showCatalog(ctx){
-    const data = await get('/data/catalog');
-    ctx.render(catalog(data));
-}
--- 10_JavaScriptApplications/_07_RoutingExercise/_01_FurnitureV2/views/details.js
+++
@@ -1,9 +0,0 @@
-import { get } from "../src/api.js";
-import { details } from "../html/details.js";
-
-export async function showDetails(ctx){
-    const id = ctx.params.id;
-    const item = await get(`/data/catalog/${id}`);
-
-    ctx.render(details(item));
-}
