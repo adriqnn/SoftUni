1edf8c957d0dac06e73463166a922fca132f1571
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-11 23:46:21 +0300

    01

--- 10_JavaScriptApplications/_08_ModularApplications/_01_ModularApplicationExample/src/data/cars.js
+++ 10_JavaScriptApplications/_08_ModularApplications/_01_ModularApplicationExample/src/data/cars.js
@@ -1,33 +1,30 @@
 import * as api from './api.js';
 
-
 const pageSize = 4;
 const endpoints = {
     cars: `/data/cars?pageSize=${pageSize}&offset=`,
     size: '/data/cars?count',
-    carById: '/data/cars/',
+    carById: '/data/cars/'
 };
 
-
-export async function getAllCars(search, page) {
+export async function getAllCars(search, page){
     let dataUrl = endpoints.cars + (page - 1) * pageSize;
     let sizeUrl = endpoints.size;
-    if (search != '') {
+    
+    if(search !== ''){
         const param = '&where=' + encodeURIComponent(`make LIKE "${search}"`);
         dataUrl += param;
         sizeUrl += param;
     }
 
-    const [data, size] = await Promise.all([
-        api.get(dataUrl),
-        api.get(sizeUrl)
-    ]);
+    const [data, size] = await Promise.all([ api.get(dataUrl), api.get(sizeUrl) ]);
+    
     return {
         data,
         pages: Math.ceil(size / pageSize)
     };
 }
 
-export async function getById(id) {
+export async function getById(id){
     return api.get(endpoints.carById + id);
 }
