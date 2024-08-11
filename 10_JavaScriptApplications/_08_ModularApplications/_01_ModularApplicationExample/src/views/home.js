caad3e1c1710f528b055061d1ada3570e02deea6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-11 23:41:55 +0300

    01

--- 10_JavaScriptApplications/_08_ModularApplications/_01_ModularApplicationExample/src/views/home.js
+++ 10_JavaScriptApplications/_08_ModularApplications/_01_ModularApplicationExample/src/views/home.js
@@ -1,11 +1,8 @@
-import { html } from '../../node_modules/lit-html/lit-html.js';
+import { html } from '../../../node_modules/lit-html/lit-html.js';
 
+const homeTemplate = () => html `<h2>Home Page</h2>
+                                 <p>Welcome to our site!</p>`;
 
-const homeTemplate = () => html`
-<h2>Home Page</h2>
-<p>Welcome to our site!</p>`;
-
-
-export function showHome(ctx) {
+export function showHome(ctx){
     ctx.render(homeTemplate());
 }
