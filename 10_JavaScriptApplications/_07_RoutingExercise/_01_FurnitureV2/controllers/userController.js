0f54580fbebf51fa7cfb9f49f2f1ab9ba2ec96c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-12 17:49:34 +0300

    01

---
+++ 10_JavaScriptApplications/_07_RoutingExercise/_01_FurnitureV2/controllers/userController.js
@@ -0,0 +1,60 @@
+import page from '../../node_modules/page/page.mjs';
+
+import { user } from '../requests/requests.js';
+import { createFormObject } from '../src/helper.js';
+
+const isValidInputData = (formObj) => {
+    if(formObj.email === '' || formObj.password === ''){
+        return false;
+    }
+
+    if(formObj.rePass && (formObj.password !== formObj.rePass)){
+        return false;
+    }
+
+    return true;
+}
+
+const saveUserInStorage = data => {
+    sessionStorage.setItem('email', data.email);
+    sessionStorage.setItem('id', data._id);
+    sessionStorage.setItem('accessToken', data.accessToken);
+
+    return data;
+}
+
+const errors = (statusCode) => {
+    const codes = {
+        403: 'Invalid username and/or password',
+        409: 'Username already taken'
+    }
+
+    return codes[statusCode];
+}
+
+const genericLogin = async (type, e) => {
+    e.preventDefault();
+
+    const formObject = createFormObject(e.target);
+
+    if(isValidInputData(formObject)){
+        try{
+            const response = await user[type]({
+                email: formObject.email,
+                password: formObject.password
+            });
+
+            saveUserInStorage(response);
+        }catch(e){
+            alert(errors(e.message));
+            return;
+        }
+
+        page.redirect('/dashboard');
+    }
+}
+
+const register = e => genericLogin('register', e);
+const login = e => genericLogin('login', e);
+
+export { register, login };
