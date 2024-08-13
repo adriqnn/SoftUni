b5b90cccdf83746ebc62b0f813790b94e69220ea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-13 13:12:12 +0300

    04

---
+++ 10_JavaScriptApplications/_07_Routing/_04_Cookbook/src/views/register.js
@@ -0,0 +1,35 @@
+import { html } from '../dom.js';
+import { register } from '../api/data.js';
+
+const registerTemplate = () => html `<section id="register">
+                                         <article>
+                                             <h2>Register</h2>
+                                             <form id="registerForm">
+                                                 <label>E-mail: <input type="text" name="email"></label>
+                                                 <label>Password: <input type="password" name="password"></label>
+                                                 <label>Repeat: <input type="password" name="rePass"></label>
+                                                 <input type="submit" value="Register">
+                                             </form>
+                                         </article>
+                                     </section>`;
+
+export function setupRegister(){
+    return showRegister;
+
+    function showRegister(){
+        return registerTemplate();
+    }
+}
+
+export async function onRegisterSubmit(data, onSuccess){
+    if(data.password !== data.rePass){
+        return alert('Passwords don\'t match');
+    }
+
+    try{
+        await register(data.email, data.password);
+        onSuccess();
+    }catch(err){
+        alert(err.message);
+    }
+}
