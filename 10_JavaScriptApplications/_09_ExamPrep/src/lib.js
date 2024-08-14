bffd17fb49442779e4c15f3c624319a66dce42b7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-14 17:10:24 +0300

    Delete 10_JavaScriptApplications/_09_ExamPrep/src directory

--- 10_JavaScriptApplications/_09_ExamPrep/src/lib.js
+++
@@ -1,8 +0,0 @@
-import {html, render} from '../node_modules/lit-html/lit-html.js';
-import page from '../node_modules/page/page.mjs';
-//export the imported from node_modules functions for easier import in other parts of the app;
-export{
-    html,
-    render,
-    page
-}
