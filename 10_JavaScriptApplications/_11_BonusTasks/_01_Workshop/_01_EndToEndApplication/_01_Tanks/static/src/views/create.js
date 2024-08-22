9b0560faa137ad8810e6abe39a00ed82b8da7177
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-22 23:47:20 +0300

    01

--- 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/create.js
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/create.js
@@ -3,21 +3,21 @@ import { createGame } from '../api/games.js';
 import { bindForm } from '../util.js';
 
 const createTemplate = (onSubmit, error) => html `<section class="narrow">
-                                                           <h1>Create Game Lobby</h1>
-                                                           <form @submit=${onSubmit}>${error ? html `<p class="error-msg">${error}</p>` : nothing}
-                                                               <div class="form-align">
-                                                                   <label class="form-row"><span>Lobby name</span><input type="text" name="name"></label>
-                                                                   <label class="form-row"><span>Game mode</span>
-                                                                       <select name="mode">
-                                                                           <option value="deathmatch">Deathmatch</option>
-                                                                           <option value="team">Team Deathmatch</option>
-                                                                           <option value="last-man">Last Man Standing</option>
-                                                                       </select>
-                                                                   </label>
-                                                               </div>
-                                                               <button class="action">Host Game</button>
-                                                           </form>
-                                                       </section>`;
+                                                      <h1>Create Game Lobby</h1>
+                                                      <form @submit=${onSubmit}>${error ? html `<p class="error-msg">${error}</p>` : nothing}
+                                                          <div class="form-align">
+                                                              <label class="form-row"><span>Lobby name</span><input type="text" name="name"></label>
+                                                              <label class="form-row"><span>Game mode</span>
+                                                                  <select name="mode">
+                                                                      <option value="deathmatch">Deathmatch</option>
+                                                                      <option value="team">Team Deathmatch</option>
+                                                                      <option value="last-man">Last Man Standing</option>
+                                                                  </select>
+                                                              </label>
+                                                          </div>
+                                                          <button class="action">Host Game</button>
+                                                      </form>
+                                                  </section>`;
 
 export function createView(ctx){
     ctx.render(createTemplate(bindForm(onSubmit)));
