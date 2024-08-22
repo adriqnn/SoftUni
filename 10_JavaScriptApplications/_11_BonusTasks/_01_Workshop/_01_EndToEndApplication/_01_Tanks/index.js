6674dc9e99a8160908d9eb803f19e1889912b1dc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-22 23:32:10 +0300

    01

---
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const express = require('express');
+const socket = require('./src/server');
+
+const port = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static('static'));
+app.get('*', (req, res) => res.sendFile(__dirname + '/static/index.html'));
+const server = http.createServer(app);
+
+socket(server);
+
+server.listen(port, () => console.log(`Application listening on port ${port}`));
