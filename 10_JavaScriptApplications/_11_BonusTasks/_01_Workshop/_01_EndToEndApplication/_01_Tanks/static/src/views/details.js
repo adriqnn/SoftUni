075da4388f6dcd33aaf6d9279f7b1340657eca49
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-22 23:49:56 +0300

    01

---
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/details.js
@@ -0,0 +1,31 @@
+import { html } from '../lib/lit-html.js';
+import { until } from '../lib/directives/until.js';
+import { getGameById } from '../api/games.js';
+
+const detailsTemplate = (gamePromise) => html `<section>${until(gamePromise, html `<h1>Lobby</h1>
+                                                                                   <main>
+                                                                                       <p>Loading details ...</p>
+                                                                                   </main>`)}
+                                               </section>`;
+
+
+export function detailsView(ctx){
+    ctx.render(detailsTemplate(loadGame(ctx)));
+}
+
+async function loadGame(ctx){
+    const gameId = ctx.params.id;
+    const game = await getGameById(gameId);
+
+    return html `<h1>${game.name}</h1>
+                 <main>
+                     <p>Mode: ${game.mode}</p>
+                     <p><button @click=${joinGame} class="button">Join Game</button></p>
+                 </main>`;
+
+    function joinGame(event){
+        event.target.disabled = true;
+        event.target.textContent = 'Loading...';
+        ctx.page.redirect(`/play/${game.objectId}`);
+    }
+}
