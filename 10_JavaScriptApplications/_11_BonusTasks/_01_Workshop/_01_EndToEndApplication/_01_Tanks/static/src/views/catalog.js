0eb355913bd69a0ecdbe68943a0526bdaa888a52
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-22 23:44:18 +0300

    01

--- 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/catalog.js
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/catalog.js
@@ -4,12 +4,12 @@ import { until } from '../lib/directives/until.js';
 import { getAllGames } from '../api/games.js';
 
 const catalogTemplate = (gamesPromise) => html `<section>
-                                                         <h1>Game Lobbies</h1>
-                                                         <main>${until(gamesPromise, 'Loading games...')}</main>
-                                                     </section>`;
+                                                    <h1>Game Lobbies</h1>
+                                                    <main>${until(gamesPromise, 'Loading games...')}</main>
+                                                </section>`;
 
 const gamesList = (games) => games.length === 0 ? html `<p>No lobbies hosted yet. <a href="/create">Be the first!</a></p>`
-                                                     : html `<ul>${repeat(games, g => g.objectId, lobbyCard)}</ul>`;
+                                                : html `<ul>${repeat(games, g => g.objectId, lobbyCard)}</ul>`;
 
 const lobbyCard = game => html `<li><a class="lobby-link" href="/games/${game.objectId}">${game.name}</a></li>`;
 
