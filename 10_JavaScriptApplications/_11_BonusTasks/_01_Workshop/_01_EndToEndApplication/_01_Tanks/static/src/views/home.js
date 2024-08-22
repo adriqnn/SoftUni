7cc82bcb2fc96595494930e0fbfac52157efe608
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-22 23:51:41 +0300

    01

--- 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/home.js
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/home.js
@@ -1,16 +1,16 @@
 import { html } from '../lib/lit-html.js';
 
 const homeTemplate = () => html `<section>
-                                          <h1>Home page</h1>
-                                          <main>
-                                              <p>Welcome to our site!</p>
-                                              <h2>Recent changes:</h2>
-                                              <ul>
-                                                  <li>Fixed ghosting issue on reconnect</li>
-                                                  <li>Improved layout</li>
-                                              </ul>
-                                          </main>
-                                      </section>`;
+                                     <h1>Home page</h1>
+                                     <main>
+                                         <p>Welcome to our site!</p>
+                                         <h2>Recent changes:</h2>
+                                         <ul>
+                                             <li>Fixed ghosting issue on reconnect</li>
+                                             <li>Improved layout</li>
+                                         </ul>
+                                     </main>
+                                 </section>`;
 
 export function homeView(ctx){
     ctx.render(homeTemplate());
