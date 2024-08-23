b81902645b4ddad63164abd2f245dd1e15902721
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-08-23 23:29:53 +0300

    01

---
+++ 10_JavaScriptApplications/_11_BonusTasks/_01_Workshop/_01_EndToEndApplication/_01_Tanks/static/src/views/layout.js
@@ -0,0 +1,13 @@
+import { html } from '../lib/lit-html.js';
+
+export const layoutTemplate = (body, onLogout, user) => html `<header>
+                                                                       <a class="logo" href="/"><img class="logo-img" src="/assets/tank.png"><span>Tank Game</span></a>
+                                                                       <nav>
+                                                                           <a href="/games">View Lobbies</a>
+                                                                           ${user ? html `<a href="/create">Host Game</a>
+                                                                                          <a @click=${onLogout} href="javascript:void(0)">Logout</a>` 
+                                                                                  : html `<a href="/login">Login</a>
+                                                                                          <a href="/register">Register</a>`}
+                                                                       </nav>
+                                                                   </header>
+                                                                   ${body}`;
