948ee1823b598474c4cc3a91394b85010e7e5d83
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-04 22:22:58 +0200

    02

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_02_NumsFrom01To10/_02_NumsFrom01To10.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class _02_NumsFrom01To10
+{
+    public static void Main(string[] args)
+    {
+        //Print numbers from 1 to 10 without a loop
+        Console.WriteLine("1");
+        Console.WriteLine("2");
+        Console.WriteLine("3");
+        Console.WriteLine("4");
+        Console.WriteLine("5");
+        Console.WriteLine("6");
+        Console.WriteLine("7");
+        Console.WriteLine("8");
+        Console.WriteLine("9");
+        Console.WriteLine("10");
+    }
+}
