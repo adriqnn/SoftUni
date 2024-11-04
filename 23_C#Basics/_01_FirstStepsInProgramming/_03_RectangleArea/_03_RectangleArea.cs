f5bf1bf66f9f86ebbc8c5b9ea8768a1ae120bc6d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-04 22:23:04 +0200

    03

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_03_RectangleArea/_03_RectangleArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class _03_RectangleArea
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string inputA = Console.ReadLine();
+        string inputB = Console.ReadLine();
+
+        // Parse the inputs to integers
+        int sideA = int.Parse(inputA);
+        int sideB = int.Parse(inputB);
+
+        // Calculate the area
+        int areaOfRectangle = sideA * sideB;
+
+        // Print the area
+        Console.WriteLine(areaOfRectangle);
+    }
+}
