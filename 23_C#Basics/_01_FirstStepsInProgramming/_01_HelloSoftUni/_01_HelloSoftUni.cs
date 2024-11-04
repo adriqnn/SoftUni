79ba473750debf67177e4940ea3012c45a8ebb33
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-04 22:22:54 +0200

    01

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_01_HelloSoftUni/_01_HelloSoftUni.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Print a greeting message
+        Console.WriteLine("Hello SoftUni");
+    }
+}
