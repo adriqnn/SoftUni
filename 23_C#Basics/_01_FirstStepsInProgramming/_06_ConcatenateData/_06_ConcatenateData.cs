7aaa463115419204bee12a5fad84d93ad6fea70f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-05 23:34:04 +0200

    06

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_06_ConcatenateData/_06_ConcatenateData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class _06_ConcatenateData
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        string town = Console.ReadLine();
+
+        // Print formatted message
+        Console.WriteLine($"You are {firstName} {lastName}, a {age}-years old person from {town}.");
+    }
+}
