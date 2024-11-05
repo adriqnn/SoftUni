01bb4c6573429a690177639f74ebb79181832232
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-05 23:33:51 +0200

    05

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_05_GreetingByName/_05_GreetingByName.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class _05_GreetingByName
+{
+    public static void Main(string[] args)
+    {
+        // Read the input name
+        string name = Console.ReadLine();
+
+        // Greet the person by name
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
