ab7e1ddf433ac058a4222e9edc27fb1600246dd2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-05 23:33:45 +0200

    04

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_04_InchesToCentimeters/_04_InchesToCentimeters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class _04_InchesToCentimeters
+{
+    public static void Main(string[] args)
+    {
+        // Read input value in inches
+        double inches = double.Parse(Console.ReadLine());
+
+        // Convert inches to centimeters
+        double centimeters = inches * 2.54;
+
+        // Print the result in centimeters
+        Console.WriteLine(centimeters);
+    }
+}
