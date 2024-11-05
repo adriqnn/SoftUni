9b4f03171304968d3f8a6dd6ce7e029b6e3cfcf9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-05 23:34:16 +0200

    08

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_08_PetShop/_08_PetShop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class _08_PetShop
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        int dogsCount = int.Parse(Console.ReadLine());
+        int otherAnimalsCount = int.Parse(Console.ReadLine());
+
+        // Calculate costs for dogs and other animals
+        double costForAllDogs = dogsCount * 2.50;
+        double costForAllTheOtherAnimals = otherAnimalsCount * 4.00;
+
+        // Calculate total cost
+        double totalCost = costForAllDogs + costForAllTheOtherAnimals;
+
+        // Print the total cost
+        Console.WriteLine(totalCost + " lv.");
+    }
+}
