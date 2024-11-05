0aa3b1d499f3e111df7a3d80e755e701fecf33f2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-05 23:34:10 +0200

    07

---
+++ 23_C#Basics/_01_FirstStepsInProgramming/_07_ProjectsCreation/_07_ProjectsCreation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class _07_ProjectsCreation
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        String name = Console.ReadLine();
+        int projectsCount = int.Parse(Console.ReadLine());
+
+        // Calculate total hours
+        int allHours = projectsCount * 3;
+
+        // Print formatted message
+        Console.WriteLine($"The architect {name} will need {allHours} hours to complete {projectsCount} project/s.");
+    }
+}
