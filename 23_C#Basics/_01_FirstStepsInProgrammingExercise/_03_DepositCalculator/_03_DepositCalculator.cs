77b71bd1d1b887a6005d9c6e29cdd2359bbf94a9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-09 23:51:47 +0200

    03

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_03_DepositCalculator/_03_DepositCalculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class _03_DepositCalculator
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        double deposit = double.Parse(Console.ReadLine());
+        int interestPeriod = int.Parse(Console.ReadLine());
+        double interestRate = double.Parse(Console.ReadLine());
+
+        // Calculate yearly and monthly interest
+        double yearlyInterest = deposit * (interestRate * 1 / 100);
+        double monthlyInterest = yearlyInterest / 12;
+
+        // Calculate final deposit amount
+        double finalDeposit = deposit + (monthlyInterest * interestPeriod);
+
+        // Print the final deposit amount
+        Console.WriteLine(finalDeposit);
+    }
+}
