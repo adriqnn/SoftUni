74b7feade38b1449e84546f6d0064697136fcb37
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-09 23:51:52 +0200

    04

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_04_VacationBooksList/_04_VacationBooksList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class _04_VacationBooksList
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        int numberOfPages = int.Parse(Console.ReadLine());
+        int pagesReadPerHour = int.Parse(Console.ReadLine());
+        int daysOfReading = int.Parse(Console.ReadLine());
+
+        // Calculate hours needed for reading
+        int hoursNeededForReading = numberOfPages / pagesReadPerHour;
+
+        // Calculate hours of reading per day
+        int hoursPerDay = hoursNeededForReading / daysOfReading;
+
+        // Print the hours of reading per day
+        Console.WriteLine(hoursPerDay);
+    }
+}
