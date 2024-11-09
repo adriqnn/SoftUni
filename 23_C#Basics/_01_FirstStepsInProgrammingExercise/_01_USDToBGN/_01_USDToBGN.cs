3250090c837ae28a7247179a445c5ba5d1203521
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-09 23:51:36 +0200

    01

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_01_USDToBGN/_01_USDToBGN.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class _01_USDToBGN
+{
+    public static void Main(string[] args)
+    {
+        // Read input USD amount
+        double usd = double.Parse(Console.ReadLine());
+
+        // Convert USD to BGN
+        double bgn = usd * 1.79549;
+
+        // Print the converted amount
+        Console.WriteLine(bgn);
+    }
+}
