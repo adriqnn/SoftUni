2541d240fdccd3a5201b5b6aadbb952f7bfcfd16
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-09 23:51:41 +0200

    02

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_02_RadiansToDegrees/_02_RadiansToDegrees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class _02_RadiansToDegrees
+{
+    public static void Main(string[] args)
+    {
+        // Read input radians
+        double radians = double.Parse(Console.ReadLine());
+
+        // Convert radians to degrees
+        double degrees = radians * 180 / Math.PI;
+
+        // Print the converted degrees
+        Console.WriteLine(degrees);
+    }
+}
