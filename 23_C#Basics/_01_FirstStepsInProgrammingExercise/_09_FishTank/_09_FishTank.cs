e8136003ca7e68ecf53a01d59ec1d630c3cc4cc5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-17 23:55:35 +0200

    09

--- 23_C#Basics/_01_FirstStepsInProgrammingExercise/_09_FishTank/_09_FishTank.cs
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_09_FishTank/_09_FishTank.cs
@@ -1,1 +1,25 @@
-﻿
+using System;
+
+public class _09_FishTank
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        int length = int.Parse(Console.ReadLine());
+        int width = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+        double percentage = double.Parse(Console.ReadLine());
+
+        // Calculate the volume of the tank in liters
+        double volumeInLiters = length * width * height / 1000.0;
+
+        // Calculate the multiplier for water displacement
+        double multiplier = (100 - percentage) / 100;
+
+        // Calculate the actual liters of water in the tank
+        double litersOfWater = volumeInLiters * multiplier;
+
+        // Print the liters of water
+        Console.WriteLine(litersOfWater);
+    }
+}﻿
