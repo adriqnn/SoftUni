b57dcd0bf55a5b9cb42d989f25a07ec9e4379c9d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-17 23:52:03 +0200

    088888888

--- 23_C#Basics/_01_FirstStepsInProgrammingExercise/_08_BasketballEquipment/_08_BasketballEquipment.cs
+++ 23_C#Basics/_01_FirstStepsInProgrammingExercise/_08_BasketballEquipment/_08_BasketballEquipment.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_BasketballEquipment
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        int trainingPrice = int.Parse(Console.ReadLine());
+
+        // Calculate prices for each equipment
+        double shoes = trainingPrice * 0.6;
+        double outfit = shoes * 0.8;
+        double basketball = outfit * 0.25;
+        double accessories = basketball * 0.2;
+
+        // Calculate the total cost
+        double totalSum = trainingPrice + shoes + outfit + basketball + accessories;
+
+        // Print the total cost
+        Console.WriteLine("{0:F2}\n", totalSum);
+    }
+}
