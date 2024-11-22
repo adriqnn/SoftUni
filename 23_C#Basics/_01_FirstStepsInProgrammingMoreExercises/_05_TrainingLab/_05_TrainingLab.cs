9f3312261408025394262f6dde921fc0e47cbaff
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:39:24 +0200

    05

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_05_TrainingLab/_05_TrainingLab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class _05_TrainingLab
+{
+    public static void Main(string[] args)
+    {
+        // Read input dimensions
+        double lengthOfTheRoom = double.Parse(Console.ReadLine());
+        double widthOfTheRoom = double.Parse(Console.ReadLine());
+
+        // Calculate the number of desks per row and number of rows
+        long numberOfDesksPerRow = (long)Math.Round(lengthOfTheRoom * 100) / 120;
+        long numberOfRows = (long)Math.Round((widthOfTheRoom * 100) - 100) / 70;
+
+        // Calculate the total number of seats
+        long numberOfSeats = (numberOfDesksPerRow * numberOfRows) - 3;
+
+        // Print the total number of seats
+        Console.WriteLine("{0:F2}\n", numberOfSeats);
+    }
+}
