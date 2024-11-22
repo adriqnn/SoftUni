879bbc473ddb1eba8361674be8c55e7db28958fe
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:38:46 +0200

    02

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_02_TriangleArea/_02_TriangleArea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class _02_TriangleArea
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        double triangleSide = double.Parse(Console.ReadLine());
+        double triangleHeight = double.Parse(Console.ReadLine());
+
+        // Calculate the area of the triangle
+        double triangleArea = triangleSide * triangleHeight / 2;
+
+        // Print the calculated area with two decimal places
+        Console.WriteLine("{0:F2}\n", triangleArea);
+    }
+}
