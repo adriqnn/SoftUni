00e24f66b6c64b93ee737d93694dcc03bae4c00d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:39:18 +0200

    04

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_04_VegetableMarket/_04_VegetableMarket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class _04_VegetableMarket
+{
+    public static void Main(string[] args)
+    {
+        // Read input prices and kilograms
+        double priceForKilogramVegetables = double.Parse(Console.ReadLine());
+        double priceForKilogramFruits = double.Parse(Console.ReadLine());
+        int kilogramsVegetables = int.Parse(Console.ReadLine());
+        int kilogramsFruits = int.Parse(Console.ReadLine());
+
+        // Calculate total revenue
+        double revenueInEuro = ((priceForKilogramVegetables * kilogramsVegetables) + (priceForKilogramFruits * kilogramsFruits)) / 1.94;
+
+        // Print the total revenue
+        Console.WriteLine("{0:F2}\n", revenueInEuro);
+    }
+}
