38130925d97f06cdaade85d99d48b5954a9c489b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:38:39 +0200

    01

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_01_TrapezoidArea/_01_TrapezoidArea.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class _01_TrapezoidArea
+{
+    public static void Main(string[] args)
+    {
+        // Read input data
+        double basisOne = double.Parse(Console.ReadLine());
+        double basisTwo = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        // Calculate the area of the trapezoid
+        double trapezoidArea = (basisOne + basisTwo) * height / 2;
+
+        // Print the calculated area with two decimal places
+        Console.WriteLine("{0:F2}\n", trapezoidArea);
+    }
+}
