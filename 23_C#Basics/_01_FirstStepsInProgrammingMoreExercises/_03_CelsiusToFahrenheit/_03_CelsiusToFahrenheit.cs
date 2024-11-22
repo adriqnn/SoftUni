3298c09545dc2bbb70cf9f8fd33391ff411bf2de
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:39:12 +0200

    03

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_03_CelsiusToFahrenheit/_03_CelsiusToFahrenheit.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class _03_CelsiusToFahrenheit
+{
+    public static void Main(string[] args)
+    {
+        // Read input temperature in Celsius
+        double degreesInCelsius = double.Parse(Console.ReadLine());
+
+        // Convert Celsius to Fahrenheit
+        double turnCelsiusToFahrenheit = (degreesInCelsius * 9 / 5) + 32;
+
+        // Print the converted temperature in Fahrenheit
+        Console.WriteLine("{0:F2}\n", turnCelsiusToFahrenheit);
+    }
+}
