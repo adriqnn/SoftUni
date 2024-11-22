411b500c99613ba43dd1a58dcb459193f1b26e23
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-22 23:39:30 +0200

    06

---
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_06_FishLand/_06_FishLand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class _06_FishLand
+{
+    public static void Main(string[] args)
+    {
+        // Read input prices and kilos
+        double priceOfMackerelPerKilo = double.Parse(Console.ReadLine());
+        double priceOfSpratPerKilo = double.Parse(Console.ReadLine());
+        double bonitoKilos = double.Parse(Console.ReadLine());
+        double scadKilos = double.Parse(Console.ReadLine());
+        double musselsKilos = double.Parse(Console.ReadLine());
+
+        // Calculate prices for each type of fish
+        double priceOfBonito = bonitoKilos * (priceOfMackerelPerKilo * 1.6);
+        double priceOfScad = scadKilos * (priceOfSpratPerKilo * 1.8);
+        double priceOfMussels = musselsKilos * 7.5;
+
+        // Calculate the total sum
+        double totalSum = priceOfBonito + priceOfScad + priceOfMussels;
+
+        // Print the total sum with two decimal places
+        Console.WriteLine("{0:F2}\n", totalSum);
+    }
+}
