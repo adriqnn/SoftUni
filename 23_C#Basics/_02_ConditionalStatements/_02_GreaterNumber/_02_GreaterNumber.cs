07ae5e11ab50c8fa8ceef3e2f536221fdd1405a2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-24 22:57:53 +0200

    02

---
+++ 23_C#Basics/_02_ConditionalStatements/_02_GreaterNumber/_02_GreaterNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class _02_GreaterNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read the two integers
+        int numberOne = int.Parse(Console.ReadLine());
+        int numberTwo = int.Parse(Console.ReadLine());
+
+        // Print the greater of the two integers
+        Console.WriteLine(Math.Max(numberOne, numberTwo));
+    }
+}
