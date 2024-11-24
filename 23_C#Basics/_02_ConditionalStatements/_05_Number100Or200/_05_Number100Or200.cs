f4c61c30dc573431e511faecafebde2d30a572ba
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-24 22:58:20 +0200

    05

---
+++ 23_C#Basics/_02_ConditionalStatements/_05_Number100Or200/_05_Number100Or200.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class _05_Number100Or200
+{
+    public static void Main(string[] args)
+    {
+        // Read the number input
+        int number = int.Parse(Console.ReadLine());
+
+        // Categorize the number based on its value
+        if (number < 100)
+        {
+            Console.WriteLine("Less than 100");
+        }
+        else if (number <= 200)
+        {
+            Console.WriteLine("Between 100 and 200");
+        }
+        else
+        {
+            Console.WriteLine("Greater than 200");
+        }
+    }
+}
