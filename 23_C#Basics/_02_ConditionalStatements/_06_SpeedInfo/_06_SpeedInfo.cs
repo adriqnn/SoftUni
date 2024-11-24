194aa479ebddbb91929304983fe74ee86c1cbe5e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-24 22:58:25 +0200

    06

---
+++ 23_C#Basics/_02_ConditionalStatements/_06_SpeedInfo/_06_SpeedInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class _06_SpeedInfo
+{
+    public static void Main(string[] args)
+    {
+        // Read the speed input
+        double speed = double.Parse(Console.ReadLine());
+
+        // Categorize the speed based on its value
+        if (speed <= 10)
+        {
+            Console.WriteLine("slow");
+        }
+        else if (speed <= 50)
+        {
+            Console.WriteLine("average");
+        }
+        else if (speed <= 150)
+        {
+            Console.WriteLine("fast");
+        }
+        else if (speed <= 1000)
+        {
+            Console.WriteLine("ultra fast");
+        }
+        else
+        {
+            Console.WriteLine("extremely fast");
+        }
+    }
+}
