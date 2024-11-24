b73485240b6dca44f8b0fa9bd0f9d9ca2d1df85a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-24 22:57:47 +0200

    01

---
+++ 23_C#Basics/_02_ConditionalStatements/_01_ExcellentResult/_01_ExcellentResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class _01_ExcellentResult
+{
+    public static void Main(string[] args)
+    {
+        // Read the grade input
+        double grade = double.Parse(Console.ReadLine());
+
+        // Determine if the grade is excellent
+        bool isExcellent = grade >= 5.5;
+
+        // Print "Excellent!" if the grade is excellent
+        if (isExcellent)
+        {
+            Console.WriteLine("Excellent!");
+        }
+    }
+}
+
