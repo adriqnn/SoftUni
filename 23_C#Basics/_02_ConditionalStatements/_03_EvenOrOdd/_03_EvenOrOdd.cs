ef454ab943a2f4d65ee0d90d9511ed0dc7b819e1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-24 22:58:11 +0200

    03

---
+++ 23_C#Basics/_02_ConditionalStatements/_03_EvenOrOdd/_03_EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class _03_EvenOrOdd
+{
+    public static void Main(string[] args)
+    {
+        // Read the number input
+        int number = int.Parse(Console.ReadLine());
+
+        // Determine if the number is even or odd
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
+    }
+}
