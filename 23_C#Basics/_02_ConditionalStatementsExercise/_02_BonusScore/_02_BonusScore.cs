64d5acc474f828a9173e0629c4c89255b97076bb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-26 23:39:03 +0200

    02

--- 23_C#Basics/_02_ConditionalStatementsExercise/_02_BonusScore/_02_BonusScore.cs
+++ 23_C#Basics/_02_ConditionalStatementsExercise/_02_BonusScore/_02_BonusScore.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_BonusScore
+{
+    public static void Main(string[] args)
+    {
+        // Read the input number
+        int inputNumber = int.Parse(Console.ReadLine());
+        double bonus = 0;
+
+        // Calculate bonus based on the input number
+        if (inputNumber <= 100)
+        {
+            bonus = 5;
+        }
+        else if (inputNumber > 1000)
+        {
+            bonus = inputNumber * 0.1;
+        }
+        else
+        {
+            bonus = inputNumber * 0.2;
+        }
+
+        // Adjust bonus based on additional conditions
+        if (inputNumber % 2 == 0)
+        {
+            bonus = bonus + 1;
+        }
+        else if (inputNumber % 10 == 5)
+        {
+            bonus = bonus + 2;
+        }
+
+        // Print the bonus and total score
+        Console.WriteLine(bonus);
+        Console.WriteLine(inputNumber + bonus);
+    }
+}
