cc123a8a5051dd862d6a5e920ef195de2ea92f8d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-27 23:50:38 +0200

    08

--- 23_C#Basics/_02_ConditionalStatementsExercise/_08_LunchBreak/_08_LunchBreak.cs
+++ 23_C#Basics/_02_ConditionalStatementsExercise/_08_LunchBreak/_08_LunchBreak.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _08_LunchBreak
+{
+    public static void Main(string[] args)
+    {
+        // Read the input values
+        String nameOfTheSeries = Console.ReadLine();
+        int episodeLength = int.Parse(Console.ReadLine());
+        int breakLength = int.Parse(Console.ReadLine());
+
+        // Calculate time allocated for lunch and break
+        double lunchTime = breakLength * 1.0 / 8;
+        double breakTime = breakLength * 1.0 / 4;
+        double timeLeft = breakLength - lunchTime - breakTime;
+
+        // Compare time left with episode length and print the appropriate message
+        if (timeLeft >= episodeLength)
+        {
+            Console.WriteLine("You have enough time to watch {0} and left with {1:F0} minutes free time.", nameOfTheSeries, Math.Ceiling(timeLeft - episodeLength));
+        }
+        else
+        {
+            Console.WriteLine("You don't have enough time to watch {0}, you need {1:F0} more minutes.", nameOfTheSeries, Math.Ceiling(episodeLength - timeLeft));
+        }
+    }
+}
+
