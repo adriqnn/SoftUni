92c118c2c3fa0a1c17bb90e5f2071598ea46e742
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-27 23:46:45 +0200

    06

--- 23_C#Basics/_02_ConditionalStatementsExercise/_06_WorldSwimmingRecord/_06_WorldSwimmingRecord.cs
+++ 23_C#Basics/_02_ConditionalStatementsExercise/_06_WorldSwimmingRecord/_06_WorldSwimmingRecord.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_WorldSwimmingRecord
+{
+    public static void Main(string[] args)
+    {
+        // Read the input values
+        double record = double.Parse(Console.ReadLine());
+        double distance = double.Parse(Console.ReadLine());
+        double seconds = double.Parse(Console.ReadLine());
+
+        // Calculate the time it takes to swim the distance
+        double swimDistanceSeconds = distance * seconds;
+
+        // Calculate the resistance time due to each 15 meters
+        double resistanceSeconds = Math.Floor(distance / 15) * 12.5;
+
+        // Calculate the total time including resistance
+        double totalTime = swimDistanceSeconds + resistanceSeconds;
+
+        // Calculate the difference between the new record and the given record
+        double timeDifference = Math.Abs(totalTime - record);
+
+        // Compare the total time with the given record
+        if (totalTime < record)
+        {
+            Console.WriteLine("Yes, he succeeded! The new world record is {0:F2} seconds.", totalTime);
+        }
+        else
+        {
+            Console.WriteLine("No, he failed! He was {0:F2} seconds slower.", timeDifference);
+        }
+    }
+}
