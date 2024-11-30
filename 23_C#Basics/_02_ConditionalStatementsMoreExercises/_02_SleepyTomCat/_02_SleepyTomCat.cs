d58f5030d09fcd76a2a83fcce815736acec5df66
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-30 23:33:24 +0200

    02

--- 23_C#Basics/_02_ConditionalStatementsMoreExercises/_02_SleepyTomCat/_02_SleepyTomCat.cs
+++ 23_C#Basics/_02_ConditionalStatementsMoreExercises/_02_SleepyTomCat/_02_SleepyTomCat.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _02_SleepyTomCat
+{
+    public static void Main(string[] args)
+    {
+        // Read the input
+        double vacationDays = double.Parse(Console.ReadLine());
+        double workingDays = 365 - vacationDays;
+
+        // Calculate playtime during vacation and working days
+        double vacationDaysPlaytime = vacationDays * 127;
+        double workingDaysPlaytime = workingDays * 63;
+
+        // Calculate the difference between Tom's total playtime and the required norm
+        double playtimeNorm = 30000 - (vacationDaysPlaytime + workingDaysPlaytime);
+
+        if (playtimeNorm >= 0)
+        {
+            Console.WriteLine("Tom sleeps well");
+            Console.WriteLine($"{Math.Floor(playtimeNorm / 60.0)} hours and {playtimeNorm % 60} minutes less for play");
+        }
+        else
+        {
+            Console.WriteLine("Tom will run away");
+            Console.WriteLine($"{Math.Floor(Math.Abs(playtimeNorm / 60.0))} hours and {Math.Abs(playtimeNorm % 60)} minutes more for play");
+        }
+    }
+}
