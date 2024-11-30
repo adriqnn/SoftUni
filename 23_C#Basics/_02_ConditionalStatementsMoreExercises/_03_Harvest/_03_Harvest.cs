009095081b78586a924476a5669d571fd89477f1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-30 23:35:51 +0200

    03

--- 23_C#Basics/_02_ConditionalStatementsMoreExercises/_03_Harvest/_03_Harvest.cs
+++ 23_C#Basics/_02_ConditionalStatementsMoreExercises/_03_Harvest/_03_Harvest.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Harvest
+{
+    public static void Main(string[] args)
+    {
+        // Read the input values
+        double yardSquareMeters = double.Parse(Console.ReadLine());
+        double grapesPerSquareMeter = double.Parse(Console.ReadLine());
+        double requiredLitersOfWine = double.Parse(Console.ReadLine());
+        double numberOfWorkers = double.Parse(Console.ReadLine());
+
+        // Calculate grape and wine production
+        double grapeForWine = (yardSquareMeters * grapesPerSquareMeter) * 0.4;
+        double litersOfWine = grapeForWine / 2.5;
+
+        double wine = requiredLitersOfWine - litersOfWine;
+
+        if (wine <= 0)
+        {
+            // Enough wine for everyone
+            Console.WriteLine($"Good harvest this year! Total wine: {Math.Floor(litersOfWine):F0} liters.");
+            Console.WriteLine($"{Math.Ceiling(Math.Abs(wine)):F0} liters left -> {Math.Ceiling(Math.Abs(wine) / numberOfWorkers):F0} liters per person.");
+        }
+        else
+        {
+            // Not enough wine
+            Console.WriteLine($"It will be a tough winter! More {Math.Floor(Math.Abs(wine)):F0} liters wine needed.");
+        }
+    }
+}
