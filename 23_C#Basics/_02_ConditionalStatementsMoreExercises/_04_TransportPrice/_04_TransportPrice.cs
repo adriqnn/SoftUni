3d77bd2fb55ef294175570d2a2e81bb4fed21157
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-30 23:38:57 +0200

    04

--- 23_C#Basics/_02_ConditionalStatementsMoreExercises/_04_TransportPrice/_04_TransportPrice.cs
+++ 23_C#Basics/_02_ConditionalStatementsMoreExercises/_04_TransportPrice/_04_TransportPrice.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_TransportPrice
+{
+    public static void Main(string[] args)
+    {
+        // Read the input
+        int kilometers = int.Parse(Console.ReadLine());
+        String timePeriod = Console.ReadLine();
+        double tripPrice = 0;
+
+        // Calculate the trip price based on different cases
+        if (kilometers < 20)
+        {
+            if (timePeriod.Equals("day"))
+            {
+                tripPrice = 0.70 + (kilometers * 0.79);
+            }
+            else
+            {
+                tripPrice = 0.70 + (kilometers * 0.90);
+            }
+        }
+        else if (kilometers < 100)
+        {
+            tripPrice = kilometers * 0.09;
+        }
+        else
+        {
+            tripPrice = kilometers * 0.06;
+        }
+
+        // Output the calculated trip price
+        Console.WriteLine($"{tripPrice:F2}");
+    }
+}
+
+
