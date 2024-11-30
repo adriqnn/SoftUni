e4414b3f52434a09f96261414644f5c10ea7305b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-30 23:48:13 +0200

    07

--- 23_C#Basics/_02_ConditionalStatementsMoreExercises/_07_FuelTank/_07_FuelTank.cs
+++ 23_C#Basics/_02_ConditionalStatementsMoreExercises/_07_FuelTank/_07_FuelTank.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_FuelTank
+{
+    public static void Main(string[] args)
+    {
+        // Read input: fuel type and fuel amount
+        string fuelType = Console.ReadLine();
+        double fuelAmount = double.Parse(Console.ReadLine());
+
+        // Check if the fuel type is valid
+        bool validType = fuelType.Equals("Diesel") || fuelType.Equals("Gasoline") || fuelType.Equals("Gas");
+
+        // Initialize a message based on fuel amount
+        string fuelTank = fuelAmount < 25? $"Fill your tank with {fuelType.ToLower()}!" : $"You have enough {fuelType.ToLower()}.";
+
+        // Check if the fuel type is valid and output the appropriate message
+        if (validType)
+        {
+            Console.WriteLine(fuelTank);
+        }
+        else
+        {
+            Console.WriteLine("Invalid fuel!");
+        }
+    }
+}
