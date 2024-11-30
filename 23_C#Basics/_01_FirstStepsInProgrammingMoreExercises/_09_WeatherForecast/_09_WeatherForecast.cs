831632369d7aded9687307a9c3e1759b8833e752
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-11-30 23:55:15 +0200

    09

--- 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_09_WeatherForecast/_09_WeatherForecast.cs
+++ 23_C#Basics/_01_FirstStepsInProgrammingMoreExercises/_09_WeatherForecast/_09_WeatherForecast.cs
@@ -5,7 +5,7 @@ public class _09_WeatherForecast
     public static void Main(string[] args)
     {
         // Read the weather type input
-        String weatherType = Console.ReadLine();
+        string weatherType = Console.ReadLine();
 
         // Determine and print the appropriate message
         Console.WriteLine(weatherType.Equals("sunny") ? "It's warm outside!" : "It's cold outside!");
