06f8975d5eddbf488d896407368e0866abbe1636
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:46:17 +0200

    06

--- 23_C#Basics/_03_ComplexConditionalStatements/_06_NumberInRange/_06_NumberInRange.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_06_NumberInRange/_06_NumberInRange.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_NumberInRange
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int number = int.Parse(Console.ReadLine());
+
+        // Set a range for the check
+        const int MIN_RANGE = -100;
+        const int MAX_RANGE = 100;
+
+        // Check if the number is in the set range and print the result
+        if (number >= MIN_RANGE && number <= MAX_RANGE && number != 0)
+        {
+            Console.WriteLine("Yes");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
