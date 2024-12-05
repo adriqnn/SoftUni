b89a403cb1f0fa907a5b54724b98bcc91ea2d662
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:49:11 +0200

    07

--- 23_C#Basics/_03_ComplexConditionalStatements/_07_WorkingHours/_07_WorkingHours.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_07_WorkingHours/_07_WorkingHours.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_WorkingHours
+{
+    public static void Main(string[] args)
+    {
+        // Read input values for hours and day
+        int hours = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        // Set a check for the working hours of the store
+        bool isOpen = (hours >= 10 && hours <= 18);
+
+        // Determine if the store is opened or closed and print the result
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+            case "Saturday":
+                if (isOpen)
+                {
+                    Console.WriteLine("open");
+                }
+                else
+                {
+                    Console.WriteLine("closed");
+                }
+                break;
+            default:
+                Console.WriteLine("closed");
+                break;
+        }
+    }
+}
+
