c2c274bf2792022309d680ef498cb5b163a2d4d2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:33:39 +0200

    02

--- 23_C#Basics/_03_ComplexConditionalStatements/_02_WeekendOrWorkingDay/_02_WeekendOrWorkingDay.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_02_WeekendOrWorkingDay/_02_WeekendOrWorkingDay.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_WeekendOrWorkingDay
+{
+    public static void Main(string[] args)
+    {
+        // Read input value day from the console
+        String day = Console.ReadLine();
+
+        // Print whether the day if valid is working day or weekend day
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Wednesday":
+            case "Thursday":
+            case "Friday":
+                Console.WriteLine("Working day");
+                break;
+            case "Saturday":
+            case "Sunday":
+                Console.WriteLine("Weekend");
+                break;
+            default:
+                Console.WriteLine("Error");
+                break;
+        }
+
+    }
+}
