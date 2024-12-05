a2b575854993f84c03994c88533dded51c7e5580
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:41:57 +0200

    04

--- 23_C#Basics/_03_ComplexConditionalStatements/_04_PersonalTitles/_04_PersonalTitles.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_04_PersonalTitles/_04_PersonalTitles.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_PersonalTitles
+{
+    public static void Main(string[] args)
+    {
+        // Read input values for age and gender
+        double age = double.Parse(Console.ReadLine());
+        string gender = Console.ReadLine();
+
+        // Print the title based on the given age and gender
+        if (age < 16 && gender.Equals("f"))
+        {
+            Console.WriteLine("Miss");
+        }
+        else if (age >= 16 && gender.Equals("f"))
+        {
+            Console.WriteLine("Ms.");
+        }
+        else if (age < 16 && gender.Equals("m"))
+        {
+            Console.WriteLine("Master");
+        }
+        else if (age >= 16 && gender.Equals("m"))
+        {
+            Console.WriteLine("Mr.");
+        }
+    }
+}
