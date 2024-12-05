14ae466dfc31e7fe376beec9f34dfae415d20f59
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:54:01 +0200

    09

--- 23_C#Basics/_03_ComplexConditionalStatements/_09_FruitOrVegetable/_09_FruitOrVegetable.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_09_FruitOrVegetable/_09_FruitOrVegetable.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_FruitOrVegetable
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string entry = Console.ReadLine();
+
+        // Print based on the input if the item is fruit, vegetable or unknown
+        switch (entry)
+        {
+            case "banana":
+            case "apple":
+            case "kiwi":
+            case "cherry":
+            case "lemon":
+            case "grapes":
+                Console.WriteLine("fruit");
+                break;
+            case "tomato":
+            case "cucumber":
+            case "pepper":
+            case "carrot":
+                Console.WriteLine("vegetable");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
+
