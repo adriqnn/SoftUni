d69d3a27e1867fe7077561d0fc50f9ab26e4bc46
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:37:43 +0200

    03

--- 23_C#Basics/_03_ComplexConditionalStatements/_03_AnimalType/_03_AnimalType.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_03_AnimalType/_03_AnimalType.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_AnimalType
+{
+    public static void Main(string[] args)
+    {
+        // Read animal input value
+        string animal = Console.ReadLine().ToLower();
+
+        // Determine whether the animal is mammal, reptile or unknown
+        switch (animal)
+        {
+            case "dog":
+                Console.WriteLine("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
