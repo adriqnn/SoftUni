0ac0285ae24de2acf2a46981a4d5d8486aa26716
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:51:10 +0200

    08

--- 23_C#Basics/_03_ComplexConditionalStatements/_08_CinemaTicket/_08_CinemaTicket.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_08_CinemaTicket/_08_CinemaTicket.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_CinemaTicket
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string day = Console.ReadLine();
+
+        // Print the price of the ticket based on the day
+        switch (day)
+        {
+            case "Monday":
+            case "Tuesday":
+            case "Friday":
+                Console.WriteLine(12);
+                break;
+            case "Wednesday":
+            case "Thursday":
+                Console.WriteLine(14);
+                break;
+            default:
+                Console.WriteLine(16);
+                break;
+        }
+    }
+}
