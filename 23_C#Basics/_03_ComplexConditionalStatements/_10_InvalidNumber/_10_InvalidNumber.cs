c05ed9c167e754ebc8de3875fe2514a05f3978e3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-05 23:55:16 +0200

    Update _10_InvalidNumber.cs

--- 23_C#Basics/_03_ComplexConditionalStatements/_10_InvalidNumber/_10_InvalidNumber.cs
+++ 23_C#Basics/_03_ComplexConditionalStatements/_10_InvalidNumber/_10_InvalidNumber.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_InvalidNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int number = int.Parse(Console.ReadLine());
+
+        // Check if the number is valid or invalid
+        bool isValid = (number >= 100 && number <= 200 || number == 0);
+
+        // Print only if the number is invalid
+        if (!isValid)
+        {
+            Console.WriteLine("invalid");
+        }
+    }
+}
