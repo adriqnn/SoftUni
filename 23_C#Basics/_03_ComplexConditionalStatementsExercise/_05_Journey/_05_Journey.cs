67c52e3a7b249364f64f4cb06b0111d532703394
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-07 23:59:00 +0200

    05

--- 23_C#Basics/_03_ComplexConditionalStatementsExercise/_05_Journey/_05_Journey.cs
+++ 23_C#Basics/_03_ComplexConditionalStatementsExercise/_05_Journey/_05_Journey.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _05_Journey
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        double budget = double.Parse(Console.ReadLine());
+        string season = Console.ReadLine();
+
+        double price = 0;
+        string destination = "";
+        string accommodation = "";
+
+        // Determine the destination and accommodation based on budget and season
+        if (budget <= 100)
+        {
+            destination = "Bulgaria";
+            if (season.Equals("summer"))
+            {
+                accommodation = "Camp";
+                price = budget * 0.30;
+            }
+            else if (season.Equals("winter"))
+            {
+                accommodation = "Hotel";
+                price = budget * 0.70;
+            }
+        }
+        else if (budget <= 1000)
+        {
+            destination = "Balkans";
+            if (season.Equals("summer"))
+            {
+                accommodation = "Camp";
+                price = budget * 0.40;
+            }
+            else if (season.Equals("winter"))
+            {
+                accommodation = "Hotel";
+                price = budget * 0.80;
+            }
+        }
+        else
+        {
+            destination = "Europe";
+            accommodation = "Hotel";
+            price = budget * 0.90;
+        }
+
+        double difference = budget - price;
+
+        // Determine the appropriate output based on budget
+        if (budget >= price)
+        {
+            Console.WriteLine($"Somewhere in {destination}");
+            Console.WriteLine($"{accommodation} - {price:F2}");
+        }
+    }
+}
