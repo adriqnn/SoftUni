a3ce41337e61a35a422264ccbbad259f223ccfbb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-07 23:46:21 +0200

    01

--- 23_C#Basics/_03_ComplexConditionalStatementsExercise/_01_Cinema/_01_Cinema.cs
+++ 23_C#Basics/_03_ComplexConditionalStatementsExercise/_01_Cinema/_01_Cinema.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_Cinema
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string type = Console.ReadLine();
+        int row = int.Parse(Console.ReadLine());
+        int number = int.Parse(Console.ReadLine());
+
+        double ticketPrice = 0.00;
+
+        // Calculate the total earnings based on ticket type
+        switch (type)
+        {
+            case "Premiere":
+                ticketPrice = 12.00;
+                break;
+            case "Normal":
+                ticketPrice = 7.50;
+                break;
+            case "Discount":
+                ticketPrice = 5.00;
+                break;
+        }
+
+        // Calculate earnings
+        double totalEarnings = ticketPrice * row * number;
+
+        // Display the total earnings formatted with 2 decimal places
+        Console.WriteLine("Total earnings: {0:F2} leva", totalEarnings);
+    }
+}
