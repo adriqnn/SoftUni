ee5bb261d4a0c6a2d85097742148f8e6f114084b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-10 23:56:47 +0200

    10

--- 23_C#Basics/_03_ComplexConditionalStatementsMoreExercises/_10_MultiplyBy2/_10_MultiplyBy2.cs
+++ 23_C#Basics/_03_ComplexConditionalStatementsMoreExercises/_10_MultiplyBy2/_10_MultiplyBy2.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_MultiplyBy2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double number = double.Parse(Console.ReadLine());
+
+        // Multiply the number by 2 and print while it's non-negative
+        while (number >= 0)
+        {
+            double result = number * 2;
+            Console.WriteLine($"Result: {result:F2}");
+
+            number = double.Parse(Console.ReadLine());
+        }
+
+        // End the program if the number is below 0
+        if (number < 0)
+        {
+            Console.WriteLine("Negative number!");
+        }
+    }
+}
