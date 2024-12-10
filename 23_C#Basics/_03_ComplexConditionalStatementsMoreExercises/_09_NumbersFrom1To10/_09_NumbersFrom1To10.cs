bf8db11bee267c2a36fc1e2d84393d81a3728866
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-10 23:53:24 +0200

    09

--- 23_C#Basics/_03_ComplexConditionalStatementsMoreExercises/_09_NumbersFrom1To10/_09_NumbersFrom1To10.cs
+++ 23_C#Basics/_03_ComplexConditionalStatementsMoreExercises/_09_NumbersFrom1To10/_09_NumbersFrom1To10.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_NumbersFrom1To10
+{
+    public static void Main(string[] args)
+    {
+        // Print numbers from 1 to 10
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i + 1);
+        }
+    }
+}
