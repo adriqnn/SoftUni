25f1573af9e67395e37c0b44b31c27e38d948514
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:18:48 +0200

    03

--- 23_C#Basics/_04_LoopsPartOne/_03_Numbers1ToNWithStep3/_03_Numbers1ToNWithStep3.cs
+++ 23_C#Basics/_04_LoopsPartOne/_03_Numbers1ToNWithStep3/_03_Numbers1ToNWithStep3.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Numbers1ToNWithStep3
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Loop from 1 to n with a step of 3 and print each number
+        for (int i = 1; i <= n; i = i + 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
