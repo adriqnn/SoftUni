ac92c60a8019c4e98459c6aa2d9015f702ecb5d2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:30:48 +0200

    07

--- 23_C#Basics/_04_LoopsPartOne/_07_SumNumbers/_07_SumNumbers.cs
+++ 23_C#Basics/_04_LoopsPartOne/_07_SumNumbers/_07_SumNumbers.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _07_SumNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        // Sum all the numbers for the n loop
+        for (int i = 1; i <= n; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            sum += currentNum;
+        }
+
+        // Print the sum of numbers
+        Console.WriteLine(sum);
+    }
+}
