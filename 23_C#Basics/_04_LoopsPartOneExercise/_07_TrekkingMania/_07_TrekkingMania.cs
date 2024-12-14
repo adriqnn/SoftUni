01cbc3114246c1958ece8ab06236ec77aee7f708
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:47:56 +0200

    07

--- 23_C#Basics/_04_LoopsPartOneExercise/_07_TrekkingMania/_07_TrekkingMania.cs
+++ 23_C#Basics/_04_LoopsPartOneExercise/_07_TrekkingMania/_07_TrekkingMania.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_TrekkingMania
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        int countAll = 0;
+        double m5 = 0;
+        double m4 = 0;
+        double m3 = 0;
+        double m2 = 0;
+        double m1 = 0;
+
+        // Calculate the total number of trekkers and the number of trekkers in each group
+        for (int i = 1; i <= n; i++)
+        {
+            int trekkers = int.Parse(Console.ReadLine());
+            countAll += trekkers;
+
+            // Distribute trekkers into different groups based on group size
+            if (trekkers >= 41)
+            {
+                m5 += trekkers;
+            }
+            else if (trekkers >= 26)
+            {
+                m4 += trekkers;
+            }
+            else if (trekkers >= 13)
+            {
+                m3 += trekkers;
+            }
+            else if (trekkers >= 6)
+            {
+                m2 += trekkers;
+            }
+            else
+            {
+                m1 += trekkers;
+            }
+        }
+
+        // Calculate and print the percentage distribution of trekkers in each group
+        Console.WriteLine($"{(m1 / countAll * 100):F2}%");
+        Console.WriteLine($"{(m2 / countAll * 100):F2}%");
+        Console.WriteLine($"{(m3 / countAll * 100):F2}%");
+        Console.WriteLine($"{(m4 / countAll * 100):F2}%");
+        Console.WriteLine($"{(m5 / countAll * 100):F2}%");
+    }
+}
+
