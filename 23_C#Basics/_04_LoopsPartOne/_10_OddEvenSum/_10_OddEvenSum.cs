db35f2957ede023fb9987ac9ec837e4f1ab645d3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:34:50 +0200

    10

--- 23_C#Basics/_04_LoopsPartOne/_10_OddEvenSum/_10_OddEvenSum.cs
+++ 23_C#Basics/_04_LoopsPartOne/_10_OddEvenSum/_10_OddEvenSum.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_OddEvenSum
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        // Loop through the n sequence of numbers and sum even and odd numbers
+        for (int i = 1; i <= n; i++)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            if (i % 2 == 0)
+            {
+                // Add even-indexed numbers to sumEven
+                sumEven += num;
+            }
+            else
+            {
+                // Add odd-indexed numbers to sumOdd
+                sumOdd += num;
+            }
+        }
+
+        // Compare the sums and print the result
+        if (sumEven == sumOdd)
+        {
+            Console.WriteLine($"Yes\n Sum = {sumEven}");
+        }
+        else
+        {
+            Console.WriteLine($"No \n Diff = {Math.Abs(sumEven - sumOdd)}");
+        }
+    }
+}
+
