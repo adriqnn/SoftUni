e09b5c769fdff8b7b6533451a19dd713576ae3f7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:40:00 +0200

    02

--- 23_C#Basics/_04_LoopsPartOneExercise/_02_HalfSumElement/_02_HalfSumElement.cs
+++ 23_C#Basics/_04_LoopsPartOneExercise/_02_HalfSumElement/_02_HalfSumElement.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _02_HalfSumElement
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        int nSum = 0;
+        int maxNum = int.MinValue;
+
+        // Read integers and find the maximum
+        for (int i = 1; i <= n; i++)
+        {
+            // Read new input value
+            int num = int.Parse(Console.ReadLine());
+
+            if (num > maxNum)
+            {
+                maxNum = num;
+            }
+            nSum += num;
+        }
+
+        int element = nSum - maxNum;
+
+        // Compare the sums and print the result
+        if (element == maxNum)
+        {
+            Console.WriteLine($"Yes\nSum = {maxNum}");
+        }
+        else
+        {
+            Console.WriteLine($"No\nDiff = {Math.Abs(element - maxNum)}");
+        }
+    }
+}
