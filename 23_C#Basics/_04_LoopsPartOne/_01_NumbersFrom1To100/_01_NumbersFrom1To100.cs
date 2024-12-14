6e4ac658cc91d678d4a3def2d98b77266258a6d4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:15:05 +0200

    01

--- 23_C#Basics/_04_LoopsPartOne/_01_NumbersFrom1To100/_01_NumbersFrom1To100.cs
+++ 23_C#Basics/_04_LoopsPartOne/_01_NumbersFrom1To100/_01_NumbersFrom1To100.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_NumbersFrom1To100
+{
+    public static void Main(string[] args)
+    {
+        // Print the numbers from 1 to 100 
+        for (int i = 1; i <= 100; i = i + 1)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
