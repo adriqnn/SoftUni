2d7bbf7723bde35238488e4a7584312b505d5214
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:20:39 +0200

    04

--- 23_C#Basics/_04_LoopsPartOne/_04_EvenPowersOf2/_04_EvenPowersOf2.cs
+++ 23_C#Basics/_04_LoopsPartOne/_04_EvenPowersOf2/_04_EvenPowersOf2.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_EvenPowersOf2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        int power = 1;
+
+        // Loop through the powers of 2 from 2^0 to 2^n
+        for (int i = 0; i <= n; i++)
+        {
+            if (i == 0)
+            {
+                Console.WriteLine(1);
+            }
+            else if (i % 2 == 0)
+            {
+                Console.WriteLine(power);
+            }
+            power = power * 2;
+        }
+    }
+}
