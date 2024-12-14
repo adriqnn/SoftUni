f1b30d357070c86fc756716d219a4f697b05b9ad
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:16:40 +0200

    02

--- 23_C#Basics/_04_LoopsPartOne/_02_NumbersNTo1/_02_NumbersNTo1.cs
+++ 23_C#Basics/_04_LoopsPartOne/_02_NumbersNTo1/_02_NumbersNTo1.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_NumbersNTo1
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Loop from n down to 1 and print each number
+        for (int i = n; i >= 1; i--)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
