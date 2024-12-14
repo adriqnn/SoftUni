d8f5c1b27db32ea98de1ce7dea1458c60a8ae173
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-14 23:32:24 +0200

    08

--- 23_C#Basics/_04_LoopsPartOne/_08_NumberSequence/_08_NumberSequence.cs
+++ 23_C#Basics/_04_LoopsPartOne/_08_NumberSequence/_08_NumberSequence.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_NumberSequence
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Stage the default
+        int maxNum = int.MinValue;
+        int minNum = int.MaxValue;
+
+        // Find the max and min numbers in the n sequence
+        for (int i = 1; i <= n; i++)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+
+            if (currentNum > maxNum)
+            {
+                maxNum = currentNum;
+            }
+
+            if (currentNum < minNum)
+            {
+                minNum = currentNum;
+            }
+        }
+
+        // Print the max and the min numbers
+        Console.WriteLine($"Max number: {maxNum}");
+        Console.WriteLine($"Min number: {minNum}");
+    }
+}
