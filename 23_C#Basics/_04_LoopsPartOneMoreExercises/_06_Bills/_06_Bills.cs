3ba1fa63aae1b7bae03e00f1a186d88ad5d608f2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-15 23:54:34 +0200

    06

--- 23_C#Basics/_04_LoopsPartOneMoreExercises/_06_Bills/_06_Bills.cs
+++ 23_C#Basics/_04_LoopsPartOneMoreExercises/_06_Bills/_06_Bills.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_Bills
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int numberOfMonths = int.Parse(Console.ReadLine());
+
+        // Add variables
+        double electricityBill = 0;
+        double waterBill = numberOfMonths * 20;
+        double internetBill = numberOfMonths * 15;
+        double otherBills = 0;
+
+        // Process each month's bills and calculate expenses
+        for (int i = 0; i < numberOfMonths; i++)
+        {
+            double monthlyElectricityBill = double.Parse(Console.ReadLine());
+
+            electricityBill += monthlyElectricityBill;
+            otherBills += (monthlyElectricityBill + 20 + 15) * 1.2;
+        }
+
+        double allExpenses = electricityBill + waterBill + internetBill + otherBills;
+
+        // Print the expenses and average
+        Console.WriteLine("Electricity: {0:F2} lv", electricityBill);
+        Console.WriteLine("Water: {0:F2} lv", waterBill);
+        Console.WriteLine("Internet: {0:F2} lv", internetBill);
+        Console.WriteLine("Other: {0:F2} lv", otherBills);
+        Console.WriteLine("Average: {0:F2} lv", allExpenses / numberOfMonths);
+    }
+}
