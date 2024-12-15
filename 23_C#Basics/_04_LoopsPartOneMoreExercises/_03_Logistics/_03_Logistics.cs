94c4b9da61726d6a7c6a6b37b3dda598f698c5a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-15 23:50:37 +0200

    03

--- 23_C#Basics/_04_LoopsPartOneMoreExercises/_03_Logistics/_03_Logistics.cs
+++ 23_C#Basics/_04_LoopsPartOneMoreExercises/_03_Logistics/_03_Logistics.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Logistics
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int cargoCount = int.Parse(Console.ReadLine());
+
+        // Add variables
+        double totalCargoTransported = 0;
+        double totalPrice = 0;
+        double cargoByBus = 0;
+        double cargoByTruck = 0;
+        double cargoByTrain = 0;
+
+        // Process each cargo and calculate statistics
+        for (int i = 0; i < cargoCount; i++)
+        {
+            double cargoWeight = double.Parse(Console.ReadLine());
+
+            if (cargoWeight <= 3)
+            {
+                cargoByBus += cargoWeight;
+                totalPrice += cargoWeight * 200;
+            }
+            else if (cargoWeight <= 11)
+            {
+                cargoByTruck += cargoWeight;
+                totalPrice += cargoWeight * 175;
+            }
+            else
+            {
+                cargoByTrain += cargoWeight;
+                totalPrice += cargoWeight * 120;
+            }
+
+            totalCargoTransported += cargoWeight;
+        }
+
+        // Calculate and print the statistics
+        Console.WriteLine("{0:F2}", totalPrice / totalCargoTransported);
+        Console.WriteLine("{0:F2}%", cargoByBus / totalCargoTransported * 100);
+        Console.WriteLine("{0:F2}%", cargoByTruck / totalCargoTransported * 100);
+        Console.WriteLine("{0:F2}%", cargoByTrain / totalCargoTransported * 100);
+    }
+}
+
