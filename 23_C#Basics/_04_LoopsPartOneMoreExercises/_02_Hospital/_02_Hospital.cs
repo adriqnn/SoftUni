0987e4de590f5d962379050e2aa55f2bd213927b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-15 23:49:27 +0200

    02

--- 23_C#Basics/_04_LoopsPartOneMoreExercises/_02_Hospital/_02_Hospital.cs
+++ 23_C#Basics/_04_LoopsPartOneMoreExercises/_02_Hospital/_02_Hospital.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_Hospital
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int hospitalPeriod = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int availableDoctors = 7;
+        int treatedPatients = 0;
+        int untreatedPatients = 0;
+
+        // Loop through each day of the hospital period
+        for (int i = 1; i <= hospitalPeriod; i++)
+        {
+            // Check if it's a day to evaluate doctor availability
+            if (i % 3 == 0 && untreatedPatients > treatedPatients)
+            {
+                availableDoctors++;
+            }
+
+            // Read the number of patients for the current day
+            int patients = int.Parse(Console.ReadLine());
+
+            // Treat patients based on doctor availability
+            if (availableDoctors >= patients)
+            {
+                treatedPatients += patients;
+            }
+            else
+            {
+                treatedPatients += availableDoctors;
+                untreatedPatients += Math.Abs(patients - availableDoctors);
+            }
+        }
+
+        // Print the number of treated and untreated patients
+        Console.WriteLine("Treated patients: {0}.", treatedPatients);
+        Console.WriteLine("Untreated patients: {0}.", untreatedPatients);
+    }
+}
