462351c7762fb67c09e4bb9b699600eb754e8cb1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-15 23:59:55 +0200

    10

--- 23_C#Basics/_04_LoopsPartOneMoreExercises/_10_Clock2/_10_Clock2.cs
+++ 23_C#Basics/_04_LoopsPartOneMoreExercises/_10_Clock2/_10_Clock2.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_Clock2
+{
+    public static void Main(string[] args)
+    {
+        // Print time
+        for (int i = 0; i < 24; i++)
+        {
+            for (int j = 0; j < 60; j++)
+            {
+                for (int k = 0; k < 60; k++)
+                {
+                    Console.WriteLine($"{i} : {j} : {k}");
+                }
+            }
+        }
+    }
+}
