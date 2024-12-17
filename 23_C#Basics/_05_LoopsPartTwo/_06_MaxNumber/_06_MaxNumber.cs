b2e6c2c697e89e895995464f4cfe9d99b1016932
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:56:37 +0200

    06

--- 23_C#Basics/_05_LoopsPartTwo/_06_MaxNumber/_06_MaxNumber.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_06_MaxNumber/_06_MaxNumber.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_MaxNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string command = Console.ReadLine();
+
+        // Staging
+        int num = int.MinValue;
+
+        while (command != "Stop")
+        {
+            int number = int.Parse(command);
+
+            // Check if the current number is greater than the current maximum
+            if (number > num)
+            {
+                num = number;
+            }
+            command = Console.ReadLine();
+        }
+
+        // Display the maximum number
+        Console.WriteLine(num);
+    }
+}
