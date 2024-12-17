fb5f9eb30b4dc8bea7a3e8d194cf314025ab8255
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 23:41:07 +0200

    04

--- 23_C#Basics/_05_LoopsPartTwoExercise/_04_Walking/_04_Walking.cs
+++ 23_C#Basics/_05_LoopsPartTwoExercise/_04_Walking/_04_Walking.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_Walking
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string input = Console.ReadLine();
+
+        int stepsCounter = 0;
+
+        // Use a while loop to track the steps walked
+        while (input != "Going home")
+        {
+            int steps = int.Parse(input);
+            stepsCounter += steps;
+
+            if (stepsCounter >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{Math.Abs(10000 - stepsCounter)} steps over the goal!");
+                break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result at the end
+        if (input == "Going home")
+        {
+            int stepsAfter = int.Parse(Console.ReadLine());
+            stepsCounter += stepsAfter;
+
+            if (stepsCounter >= 10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+                Console.WriteLine($"{Math.Abs(10000 - stepsCounter)} steps over the goal!");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Abs(10000 - stepsCounter)} more steps to reach goal.");
+            }
+        }
+    }
+}
