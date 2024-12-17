a7aeea97805f759b7932b8a5d2dd5f7ce7b6a764
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:32:48 +0200

    01

--- 23_C#Basics/_05_LoopsPartTwo/_01_ReadText/_01_ReadText.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_01_ReadText/_01_ReadText.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_ReadText
+{
+    public static void Main(string[] args)
+    {
+        // Use a while loop to continuously read input until "Stop" is entered
+        while (true)
+        {
+            string input = Console.ReadLine();
+
+            if (string.Equals(input, "Stop", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+            // Print the input to the console
+            Console.WriteLine(input);
+        }
+    }
+}
