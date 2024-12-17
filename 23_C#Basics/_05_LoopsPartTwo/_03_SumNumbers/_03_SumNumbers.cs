887c6c57176cd206d6cf48a71e4c59e12154cf94
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:45:34 +0200

    03

--- 23_C#Basics/_05_LoopsPartTwo/_03_SumNumbers/_03_SumNumbers.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_03_SumNumbers/_03_SumNumbers.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_SumNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int initialNum = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        // Use a while loop to continuously read numbers and add them to the sum
+        while (sum < initialNum)
+        {
+            int currentNum = int.Parse(Console.ReadLine());
+            sum += currentNum;
+        }
+
+        // Display the final sum
+        Console.WriteLine(sum);
+    }
+}
