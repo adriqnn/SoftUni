3dd788c2cdc63e5f40615764bddbcdc4059e018e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:41:15 +0200

    02

--- 23_C#Basics/_05_LoopsPartTwo/_02_Password/_02_Password.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_02_Password/_02_Password.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_Password
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string username = Console.ReadLine();
+        string password = Console.ReadLine();
+
+        string input = Console.ReadLine();
+
+        // Use a while loop to repeatedly ask for the password until it matches
+        while (input != password)
+        {
+            input = Console.ReadLine();
+        }
+
+        // Welcome the user after successful login
+        Console.WriteLine("Welcome {0}!", username);
+    }
+}
