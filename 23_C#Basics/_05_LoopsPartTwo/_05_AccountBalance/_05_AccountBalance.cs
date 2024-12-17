5b5ac71109684a72b33024354de82b3f0f93535d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:54:05 +0200

    05

--- 23_C#Basics/_05_LoopsPartTwo/_05_AccountBalance/_05_AccountBalance.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_05_AccountBalance/_05_AccountBalance.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_AccountBalance
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string command = Console.ReadLine();
+
+        // Initialize the account balance
+        double accountBalance = 0;
+
+        while (!command.Equals("NoMoreMoney"))
+        {
+            double deposit = double.Parse(command);
+
+            // Exit the loop if the amount is invalid
+            if (deposit < 0)
+            {
+                Console.WriteLine("Invalid operation!");
+                break;
+            }
+
+            // Update the account balance and display the increase
+            accountBalance += deposit;
+            Console.WriteLine($"Increase: {deposit:F2}");
+
+            command = Console.ReadLine();
+        }
+
+        // Display the final account balance
+        Console.WriteLine($"Total: {accountBalance:F2}");
+    }
+}
