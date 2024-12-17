77232e7e82f4f30036ecbe68e43b6af66706b819
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 23:35:35 +0200

    01

--- 23_C#Basics/_05_LoopsPartTwoExercise/_01_OldBooks/_01_OldBooks.cs
+++ 23_C#Basics/_05_LoopsPartTwoExercise/_01_OldBooks/_01_OldBooks.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_OldBooks
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string book = Console.ReadLine();
+        string favoriteBook = Console.ReadLine();
+
+        // Staging
+        int count = 0;
+
+        // Use a while loop to search for the favorite book
+        while (!favoriteBook.Equals(book))
+        {
+            if (favoriteBook.Equals("No More Books"))
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {count} books.");
+                break;
+            }
+
+            count++;
+            favoriteBook = Console.ReadLine();
+        }
+
+        // Display the result based on whether the favorite book was found
+        if (favoriteBook.Equals(book))
+        {
+            Console.WriteLine($"You checked {count} books and found it.");
+        }
+    }
+}
