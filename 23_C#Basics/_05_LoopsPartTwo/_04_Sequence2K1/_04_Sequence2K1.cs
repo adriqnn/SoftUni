ce306fa9baa2e0570109302a352d9a5d82411b2e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 22:47:14 +0200

    04

--- 23_C#Basics/_05_LoopsPartTwo/_04_Sequence2K1/_04_Sequence2K1.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_04_Sequence2K1/_04_Sequence2K1.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_Sequence2K1
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Staging
+        int k = 1;
+
+        // Use a while loop to generate and print the sequence
+        while (k <= n)
+        {
+            Console.WriteLine(k);
+
+            k = 2 * k + 1;
+        }
+    }
+}
