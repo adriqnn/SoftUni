655fad2668b0d9dbde6301acd3ece5db706859f6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 23:47:58 +0200

    07

--- 23_C#Basics/_05_LoopsPartTwoExercise/_07_Moving/_07_Moving.cs
+++ 23_C#Basics/_05_LoopsPartTwoExercise/_07_Moving/_07_Moving.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _07_Moving
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int width = int.Parse(Console.ReadLine());
+        int length = int.Parse(Console.ReadLine());
+        int height = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int volume = width * length * height;
+        int boxesSum = 0;
+
+        string input = Console.ReadLine();
+
+        // Determine how many boxes are needed for the moving
+        while (input != "Done")
+        {
+            int boxes = int.Parse(input);
+            boxesSum += boxes;
+
+            if (boxesSum >= volume)
+            {
+                break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Display the remaining or excess space in the room
+        int diff = Math.Abs(boxesSum - volume);
+
+        // Print result 
+        if (boxesSum >= volume)
+        {
+            Console.WriteLine($"No more free space! You need {diff} Cubic meters more.");
+        }
+        else
+        {
+            Console.WriteLine($"{diff} Cubic meters left.");
+        }
+    }
+}
