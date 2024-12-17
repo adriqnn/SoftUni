5afb28de77972cc9a95840a962dfc03b2adb8125
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-17 23:06:07 +0200

    07

--- 23_C#Basics/_05_LoopsPartTwo/_07_MinNumber/_07_MinNumber.cs
+++ 23_C#Basics/_05_LoopsPartTwo/_07_MinNumber/_07_MinNumber.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_MinNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string command = Console.ReadLine();
+
+        // Staging
+        int num = int.MaxValue;
+
+        while (command != "Stop")
+        {
+            int number = int.Parse(command);
+
+            // Check if the current number is lower than the current minimum
+            if (number < num)
+            {
+                num = number;
+            }
+            command = Console.ReadLine();
+        }
+
+        // Display the minimum number
+        Console.WriteLine(num);
+    }
+}
