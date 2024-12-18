9c535fa8c1828a12a00fc85a75deb640dd5a983f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-18 23:19:21 +0200

    04

--- 23_C#Basics/_05_LoopsPartTwoMoreExercises/_04_NumbersDividedBy3/_04_NumbersDividedBy3.cs
+++ 23_C#Basics/_05_LoopsPartTwoMoreExercises/_04_NumbersDividedBy3/_04_NumbersDividedBy3.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_NumbersDividedBy3
+{
+    public static void Main(string[] args)
+    {
+        // Print numbers from 1 to 100 that are divisible by 3
+        for (int i = 1; i < 100; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
