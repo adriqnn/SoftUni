25c192dfd3096cba9c745fd0553f9116ce4cd94d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-18 23:21:37 +0200

    05

--- 23_C#Basics/_05_LoopsPartTwoMoreExercises/_05_AverageNumber/_05_AverageNumber.cs
+++ 23_C#Basics/_05_LoopsPartTwoMoreExercises/_05_AverageNumber/_05_AverageNumber.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_AverageNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            double number = double.Parse(Console.ReadLine());
+            sum += number;
+        }
+
+        // Print the average
+        Console.WriteLine($"{sum / n:F2}");
+    }
+}
