9ba9becc31fb134a8470c714d457a5b0e8bfae45
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:04:29 +0200

    03

--- 23_C#Basics/_06_NestedLoops/_03_Combinations/_03_Combinations.cs
+++ 23_C#Basics/_06_NestedLoops/_03_Combinations/_03_Combinations.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Combinations
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int count = 0;
+
+        for (int i = 0; i <= num; i++)
+        {
+            for (int j = 0; j <= num; j++)
+            {
+                for (int k = 0; k <= num; k++)
+                {
+                    if (i + j + k == num)
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+
+        // Print the result
+        Console.WriteLine(count);
+    }
+}
