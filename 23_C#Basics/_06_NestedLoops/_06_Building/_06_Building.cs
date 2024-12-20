3339c75e4fddfa3e4fd3fd2e4dac9b0662683aed
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:22:59 +0200

    06

--- 23_C#Basics/_06_NestedLoops/_06_Building/_06_Building.cs
+++ 23_C#Basics/_06_NestedLoops/_06_Building/_06_Building.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _06_Building
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int floors = int.Parse(Console.ReadLine());
+        int rooms = int.Parse(Console.ReadLine());
+
+        // Print last floor large apartments
+        for (int i = floors; i > floors - 1; i = floors - 1)
+        {
+            for (int j = 0; j < rooms; j++)
+            {
+                Console.Write($"L{i}{j} ");
+            }
+        }
+
+        // Cycle through the rest of the building
+        for (int i = floors - 1; i >= 1; i--)
+        {
+
+            // New line
+            Console.Write("\n");
+
+            // Print apartments
+            if (i % 2 != 0)
+            {
+                for (int j = 0; j < rooms; j++)
+                {
+                    Console.Write($"A{i}{j} ");
+                }
+            }
+
+            // Print offices
+            if (i % 2 == 0)
+            {
+                for (int j = 0; j < rooms; j++)
+                {
+                    Console.Write($"O{i}{j} ");
+                }
+            }
+        }
+    }
+}
