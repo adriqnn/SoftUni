927b6e2ecbc3d1a9f2f68ebe82fa0bea0e2d2ed9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:47:01 +0200

    04

--- 23_C#Basics/_06_NestedLoopsExercise/_04_TrainTheTrainers/_04_TrainTheTrainers.cs
+++ 23_C#Basics/_06_NestedLoopsExercise/_04_TrainTheTrainers/_04_TrainTheTrainers.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _04_TrainTheTrainers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n = int.Parse(Console.ReadLine());
+        string presentationName = Console.ReadLine();
+
+        // Add variables
+        double totalAverage = 0;
+        int presentationCount = 0;
+
+        // Evaluate all presentations
+        while (!presentationName.Equals("Finish"))
+        {
+            presentationCount++;
+            double presentationAverage = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double grade = double.Parse(Console.ReadLine());
+                presentationAverage += grade;
+            }
+
+            totalAverage += presentationAverage / n;
+
+            Console.WriteLine($"{presentationName} - {presentationAverage / n:F2}.");
+            presentationName = Console.ReadLine();
+        }
+
+        double finalAverage = totalAverage / presentationCount;
+
+        // Print the result
+        Console.WriteLine($"Student's final assessment is {finalAverage:F2}.");
+    }
+}
