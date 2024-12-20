2b84181e0879dc4827eab0c990444892e9aaf99b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:15:18 +0200

    05

--- 23_C#Basics/_06_NestedLoops/_05_Travelling/_05_Travelling.cs
+++ 23_C#Basics/_06_NestedLoops/_05_Travelling/_05_Travelling.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_Traveling
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        String console = Console.ReadLine();
+        String destination = "";
+
+        while (!console.Equals("End"))
+        {
+            // Store the current destination
+            destination = console;
+
+            double budget = double.Parse(Console.ReadLine());
+            double sum = 0;
+
+            while (sum < budget)
+            {
+                double expense = double.Parse(Console.ReadLine());
+                sum += expense;
+            }
+
+            // Print the destination you're going to
+            Console.WriteLine($"Going to {destination}!");
+
+            // Read the next destination or "End" to exit the loop
+            console = Console.ReadLine();
+        }
+    }
+}
