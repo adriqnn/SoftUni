0de73f279d594bb2159b1f1a9accfcd6363afd2a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:51:15 +0200

    05

--- 23_C#Basics/_06_NestedLoopsExercise/_05_SpecialNumbers/_05_SpecialNumbers.cs
+++ 23_C#Basics/_06_NestedLoopsExercise/_05_SpecialNumbers/_05_SpecialNumbers.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_SpecialNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Print all the special numbers
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    for (int l = 1; l <= 9; l++)
+                    {
+                        if (num % i == 0 && num % j == 0 && num % k == 0 && num % l == 0)
+                            Console.Write($"{i}{j}{k}{l} ", i, j, k, l);
+                    }
+                }
+            }
+        }
+    }
+}
