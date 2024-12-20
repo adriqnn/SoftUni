0b9c9115db1cb564339490db2906bea9ac949dcd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:41:51 +0200

    03

--- 23_C#Basics/_06_NestedLoopsExercise/_03_SumPrimeNonPrime/_03_SumPrimeNonPrime.cs
+++ 23_C#Basics/_06_NestedLoopsExercise/_03_SumPrimeNonPrime/_03_SumPrimeNonPrime.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_SumPrimeNonPrime
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variables
+        int primeSum = 0;
+        int nonPrimeSum = 0;
+
+        // Calculate prime and non-prime sums
+        while (!console.Equals("stop"))
+        {
+            bool isPrime = true;
+            int num = int.Parse(console);
+
+            if (num < 0)
+            {
+                Console.WriteLine("Number is negative.");
+            }
+            else
+            {
+                if (num <= 1)
+                {
+                    isPrime = false;
+                }
+                else
+                {
+                    for (int i = 2; i <= Math.Sqrt(num); i++)
+                    {
+                        if (num % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                }
+
+                if (isPrime)
+                {
+                    primeSum += num;
+                }
+                else
+                {
+                    nonPrimeSum += num;
+                }
+            }
+
+            console = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine($"Sum of all prime numbers is: {primeSum}");
+        Console.WriteLine($"Sum of all non prime numbers is: {nonPrimeSum}");
+    }
+}
