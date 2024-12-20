a142d423843790f87dfcccba89002906f05d72e6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:00:59 +0200

    02

--- 23_C#Basics/_06_NestedLoops/_02_MultiplicationTable/_02_MultiplicationTable.cs
+++ 23_C#Basics/_06_NestedLoops/_02_MultiplicationTable/_02_MultiplicationTable.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_MultiplicationTable
+{
+    public static void Main(string[] args)
+    {
+        // Print multiplication table from 1 to 10
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.WriteLine($"{i} * {j} = {i * j}");
+            }
+        }
+    }
+}
