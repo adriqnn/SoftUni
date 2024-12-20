dc8c19f933ceca64037f99bb3752ad40a1c8270e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:32:45 +0200

    01

--- 23_C#Basics/_06_NestedLoopsExercise/_01_NumberPyramid/_01_NumberPyramid.cs
+++ 23_C#Basics/_06_NestedLoopsExercise/_01_NumberPyramid/_01_NumberPyramid.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_NumberPyramid
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int current = 1;
+        bool isBigger = false;
+
+        // Print the number pyramid
+        for (int rows = 1; rows <= num; rows++)
+        {
+            for (int cols = 1; cols <= rows; cols++)
+            {
+                if (current > num)
+                {
+                    isBigger = true;
+                    break;
+                }
+
+                Console.Write(current + " ");
+                current++;
+            }
+
+            if (isBigger)
+            {
+                break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
