30c3fdff504a09692ad25051adecb6545b522ece
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-20 23:10:27 +0200

    04

--- 23_C#Basics/_06_NestedLoops/_04_SumOfTwoNumbers/_04_SumOfTwoNumbers.cs
+++ 23_C#Basics/_06_NestedLoops/_04_SumOfTwoNumbers/_04_SumOfTwoNumbers.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_SumOfTwoNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int start = int.Parse(Console.ReadLine());
+        int end = int.Parse(Console.ReadLine());
+        int magicNum = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int count = 0;
+        bool isDone = false;
+
+        // Cycle from start to end and search for the magic number
+        for (int i = start; i <= end; i++)
+        {
+            for (int j = start; j <= end; j++)
+            {
+                count++;
+
+                // Print result if magic number is found
+                if (i + j == magicNum)
+                {
+                    Console.WriteLine($"Combination N:{count} ({i} + {j} = {i + j})");
+                    isDone = true;
+                    break;
+                }
+            }
+
+            if (isDone)
+            {
+                break;
+            }
+        }
+
+        // Print result
+        if (!isDone)
+        {
+            Console.WriteLine($"{count} combinations - neither equals {magicNum}");
+        }
+    }
+}
