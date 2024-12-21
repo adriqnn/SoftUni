65185ffc4ec2488b3227c71f4dc7818dbaefd979
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-21 23:58:42 +0200

    04

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_04_CarNumber/_04_CarNumber.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_04_CarNumber/_04_CarNumber.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_CarNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int beginning = int.Parse(Console.ReadLine());
+        int ending = int.Parse(Console.ReadLine());
+
+        // Find special car numbers and print them
+        for (int i = beginning; i <= ending; i++)
+        {
+            for (int j = beginning; j <= ending; j++)
+            {
+                for (int k = beginning; k <= ending; k++)
+                {
+                    for (int l = beginning; l <= ending; l++)
+                    {
+                        if ((i > l && ((j + k) % 2 == 0)))
+                        {
+                            if ((i % 2 == 0) && (l % 2 != 0))
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                            else if ((i % 2 != 0) && (l % 2 == 0))
+                            {
+                                Console.Write($"{i}{j}{k}{l} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
