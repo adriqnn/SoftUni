c26ff1a7cca95a2cd13bacb7954c130908802d81
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-21 23:53:37 +0200

    03

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_03_LuckyNumbers/_03_LuckyNumbers.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_03_LuckyNumbers/_03_LuckyNumbers.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_LuckyNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Find the numbers based on the requirement and print them
+        for (int i = 1; i <= 9; i++)
+        {
+            for (int j = 1; j <= 9; j++)
+            {
+                for (int k = 1; k <= 9; k++)
+                {
+                    for (int l = 1; l <= 9; l++)
+                    {
+                        if ((i + j) == (k + l))
+                        {
+                            int num = int.Parse(string.Format($"{i}{j}{k}{l}"));
+                            int sum = i + j;
+
+                            if (n % sum == 0)
+                            {
+                                Console.Write($"{num} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
