dacd525e29566c144ba764102b9f2f7bc13fc292
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-21 23:47:04 +0200

    02

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_02_LettersCombinations/_02_LettersCombinations.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_02_LettersCombinations/_02_LettersCombinations.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_LettersCombinations
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        char first = Console.ReadLine()[0];
+        char second = Console.ReadLine()[0];
+        char skipLetter = Console.ReadLine()[0];
+
+        // Add variable
+        int count = 0;
+
+        // Find all the combinations and print them
+        for (char i = first; i <= second; i++)
+        {
+            if (i != skipLetter)
+            {
+                for (char j = first; j <= second; j++)
+                {
+                    if (j != skipLetter)
+                    {
+                        for (char k = first; k <= second; k++)
+                        {
+                            if (k != skipLetter)
+                            {
+                                count++;
+                                Console.Write($"{i}{j}{k} ", i, j, k);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // Print the count of the combinations
+        Console.WriteLine(count);
+    }
+}
