6fbb764b8a7937edcabc0ba08f7ee2609422618b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-23 23:51:13 +0200

    07

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_07_SafePasswordsGenerator/_07_SafePasswordsGenerator.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_07_SafePasswordsGenerator/_07_SafePasswordsGenerator.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_SafePasswordsGenerator
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int max = int.Parse(Console.ReadLine());
+
+        // Add variables
+        char A = '#';
+        char B = '@';
+        int count = 0;
+
+        // We use a boolean flag to break the outer loop
+        bool breakOuter = false;
+
+        for (int i = 1; i <= a && !breakOuter; i++)
+        {
+            for (int j = 1; j <= b; j++)
+            {
+                if (count == max)
+                {
+                    breakOuter = true;
+                    break; // Break inner loop
+                }
+
+                if (A == 56)
+                {
+                    A = '#';
+                }
+
+                if (B == 97)
+                {
+                    B = '@';
+                }
+
+                Console.Write($"{A}{B}{i}{j}{B++}{A++}|");
+                count++;
+            }
+        }
+    }
+}
