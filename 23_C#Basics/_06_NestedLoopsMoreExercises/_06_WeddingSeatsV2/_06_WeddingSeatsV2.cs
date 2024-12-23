852400d8ee470229c2bd795a3bb01561416c384b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-23 23:43:12 +0200

    06

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_06_WeddingSeatsV2/_06_WeddingSeatsV2.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_06_WeddingSeatsV2/_06_WeddingSeatsV2.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_WeddingSeatsV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        char lastSector = Console.ReadLine()[0];
+        int currentCountOfRows = int.Parse(Console.ReadLine());
+        int countOfOddSeats = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int countSeats = 0;
+
+        // Print all possible seats
+        for (char sector = 'A'; sector <= lastSector; sector++)
+        {
+            for (int row = 1; row <= currentCountOfRows; row++)
+            {
+                char startSeat = 'a';
+                char endSeat = (row % 2 != 0) ? (char)(startSeat + countOfOddSeats) : (char)(startSeat + countOfOddSeats + 2);
+
+                for (char seat = startSeat; seat < endSeat; seat++)
+                {
+                    countSeats++;
+                    Console.WriteLine($"{sector}{row}{seat}");
+                }
+            }
+
+            currentCountOfRows++;
+        }
+
+        // Print the total count of the seats
+        Console.WriteLine(countSeats);
+    }
+}
