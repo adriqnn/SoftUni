6565d84b50b398189c32706acd92eb30c4bcf83b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-23 23:22:48 +0200

    05

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_05_ChallengeTheWedding/_05_ChallengeTheWedding.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_05_ChallengeTheWedding/_05_ChallengeTheWedding.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _05_ChallengeTheWedding
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int clientsM = int.Parse(Console.ReadLine());
+        int clientsF = int.Parse(Console.ReadLine());
+        int maxTables = int.Parse(Console.ReadLine());
+
+        // Arrange guest based on gender and free tables
+        for (int i = 1; i <= clientsM; i++)
+        {
+            for (int j = 1; j <= clientsF; j++)
+            {
+                if (maxTables == 0)
+                {
+                    break;
+                }
+
+                maxTables--;
+                Console.Write($"({i} <-> {j}) ");
+            }
+
+            if (maxTables == 0)
+            {
+                break;
+            }
+        }
+    }
+}
