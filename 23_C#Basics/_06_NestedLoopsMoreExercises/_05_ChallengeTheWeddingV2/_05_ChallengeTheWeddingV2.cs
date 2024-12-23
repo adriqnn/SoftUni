b27df1bb1422c2fc67a411d168326fe675e98e1c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-23 23:26:32 +0200

    05

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_05_ChallengeTheWeddingV2/_05_ChallengeTheWeddingV2.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_05_ChallengeTheWeddingV2/_05_ChallengeTheWeddingV2.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _05_ChallengeTheWeddingV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int clientsM = int.Parse(Console.ReadLine());
+        int clientsF = int.Parse(Console.ReadLine());
+        int maxTables = int.Parse(Console.ReadLine());
+
+        // A boolean to indicate when we've run out of tables
+        bool noMoreTables = false;
+
+        for (int i = 1; i <= clientsM && !noMoreTables; i++)
+        {
+            for (int j = 1; j <= clientsF; j++)
+            {
+                if (maxTables == 0)
+                {
+                    noMoreTables = true;
+                    break; // Break out of the inner loop
+                }
+
+                maxTables--;
+                Console.Write($"({i} <-> {j}) ");
+            }
+        }
+    }
+}
