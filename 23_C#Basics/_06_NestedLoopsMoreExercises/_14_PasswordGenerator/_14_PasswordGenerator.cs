a71e8348fb6bfa79d7ce3cab3e0e82b369ed2712
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:33:12 +0200

    14

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_14_PasswordGenerator/_14_PasswordGenerator.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_14_PasswordGenerator/_14_PasswordGenerator.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _14_PasswordGenerator
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n = int.Parse(Console.ReadLine());
+        int l = int.Parse(Console.ReadLine());
+
+        // Determine the correct output based on the requirements and print
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                for (char k = 'a'; k < 'a' + l; k++)
+                {
+                    for (char m = 'a'; m < 'a' + l; m++)
+                    {
+                        for (int o = 1; o <= n; o++)
+                        {
+                            if (o > i && o > j)
+                            {
+                                Console.Write($"{i}{j}{k}{m}{o} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
