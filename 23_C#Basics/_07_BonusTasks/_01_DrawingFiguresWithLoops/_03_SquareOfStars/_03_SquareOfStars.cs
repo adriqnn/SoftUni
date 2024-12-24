70abadfb890ea199f9e74a5135c0fd3172d935aa
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:53:47 +0200

    03

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_03_SquareOfStars/_03_SquareOfStars.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_03_SquareOfStars/_03_SquareOfStars.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_SquareOfStars
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print a square nxn of stars with spaces
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= n; j++)
+            {
+                Console.Write("* ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
