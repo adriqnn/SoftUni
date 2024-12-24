91fb4a66d0e663de15d61bcf150c81a70f7bd8df
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:57:20 +0200

    05

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_05_SquareFrame/_05_SquareFrame.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_05_SquareFrame/_05_SquareFrame.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_SquareFrame
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int sideLength = int.Parse(Console.ReadLine());
+
+        // Print the square frame
+        for (int i = 1; i <= sideLength; i++)
+        {
+            for (int j = 1; j <= sideLength; j++)
+            {
+                if ((i == 1 && (j == 1 || j == sideLength)) ||
+                    (i == sideLength && (j == 1 || j == sideLength)))
+                {
+                    Console.Write("+ ");
+                }
+                else if (j == 1 || j == sideLength)
+                {
+                    Console.Write("| ");
+                }
+                else
+                {
+                    Console.Write("- ");
+                }
+            }
+
+            // Move to the next line
+            Console.WriteLine();
+        }
+    }
+}
