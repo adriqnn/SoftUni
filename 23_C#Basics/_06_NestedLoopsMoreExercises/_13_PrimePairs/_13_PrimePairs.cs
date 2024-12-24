f87aabca0e679b070960aaf024664ddea984d629
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:31:10 +0200

    13

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_13_PrimePairs/_13_PrimePairs.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_13_PrimePairs/_13_PrimePairs.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _13_PrimePairs
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int firstNumberStart = int.Parse(Console.ReadLine());
+        int secondNumberStart = int.Parse(Console.ReadLine());
+        int firstNumberEnd = int.Parse(Console.ReadLine());
+        int secondNumberEnd = int.Parse(Console.ReadLine());
+
+        // Determine the prime pairs numbers and print them
+        for (int i = firstNumberStart; i <= firstNumberStart + firstNumberEnd; i++)
+        {
+            for (int j = secondNumberStart; j <= secondNumberStart + secondNumberEnd; j++)
+            {
+                if (isPrime(i) && isPrime(j))
+                {
+                    Console.WriteLine($"{i}{j}");
+                }
+            }
+        }
+    }
+
+    // Function to check if a number is prime
+    public static bool isPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        // Check for divisibility from 2 to the square root of the number
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
