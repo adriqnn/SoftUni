f4564a5a0ccbcbdd8f97cdc2aea764ccf692443d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:50:21 +0200

    01

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_01_RectangleOf10x10Stars/_01_RectangleOf10x10Stars.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_01_RectangleOf10x10Stars/_01_RectangleOf10x10Stars.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_RectangleOf10x10Stars
+{
+    public static void Main(string[] args)
+    {
+        // Print a square 10x10 of stars
+        for (int i = 1; i <= 10; i++)
+        {
+            for (int j = 1; j <= 10; j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
