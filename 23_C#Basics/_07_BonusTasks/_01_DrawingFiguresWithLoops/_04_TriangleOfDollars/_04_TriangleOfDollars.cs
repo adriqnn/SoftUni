3c8082fcde3510ed03624ef5c8df84403aed5ef5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:55:39 +0200

    04

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_04_TriangleOfDollars/_04_TriangleOfDollars.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_04_TriangleOfDollars/_04_TriangleOfDollars.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_TriangleOfDollars
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print a triangle of $ signs
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write("$ ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
