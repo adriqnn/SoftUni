93e7a27a266f411f1f8cdb13fdcab34e2515de25
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:22:14 +0200

    11

--- 23_C#Basics/_06_NestedLoopsMoreExercises/_11_HappyCatParking/_11_HappyCatParking.cs
+++ 23_C#Basics/_06_NestedLoopsMoreExercises/_11_HappyCatParking/_11_HappyCatParking.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _11_HappyCatParking
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int numberOfDays = int.Parse(Console.ReadLine());
+        int numberOfHoursPerDay = int.Parse(Console.ReadLine());
+
+        // Add variable
+        double totalSum = 0;
+
+        // Calculate daily parking fee
+        for (int day = 1; day <= numberOfDays; day++)
+        {
+            double dailySum = 0;
+            for (int hour = 1; hour <= numberOfHoursPerDay; hour++)
+            {
+                if (day % 2 != 0)
+                {
+                    if (hour % 2 == 0)
+                    {
+                        dailySum += 1.25;
+                    }
+                    else
+                    {
+                        dailySum += 1;
+                    }
+                }
+                else
+                {
+                    if (hour % 2 != 0)
+                    {
+                        dailySum += 2.50;
+                    }
+                    else
+                    {
+                        dailySum += 1;
+                    }
+                }
+            }
+
+            totalSum += dailySum;
+            Console.WriteLine($"Day: {day} - {dailySum:F2} leva");
+        }
+
+        // Print the total amount
+        Console.WriteLine($"Total: {totalSum:F2} leva");
+    }
+}
