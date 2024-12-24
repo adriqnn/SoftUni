d63a5168f0a9f2827199679b6346f09a165bf945
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-24 23:59:41 +0200

    06

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_06_RhombusOfStars/_06_RhombusOfStars.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_06_RhombusOfStars/_06_RhombusOfStars.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_RhombusOfStars
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Upper triangle
+        for (int i = 1; i <= n; i++)
+        {
+
+            // Print upper triangle lines
+            printRowOfAFigure(n, i);
+
+            // Move to the next line
+            Console.WriteLine();
+        }
+
+        // Lower triangle
+        for (int i = n - 1; i >= 1; i--)
+        {
+
+            // Print lower triangle lines
+            printRowOfAFigure(n, i);
+
+            // Move to the next line
+            Console.WriteLine();
+        }
+    }
+
+
+    public static void printRowOfAFigure(int n, int i)
+    {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            Console.Write(" ");
+        }
+
+        // Print stars in each row
+        for (int k = 1; k <= 2 * i - k; k++)
+        {
+            if (k == 1)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write(" *");
+            }
+        }
+    }
+}
