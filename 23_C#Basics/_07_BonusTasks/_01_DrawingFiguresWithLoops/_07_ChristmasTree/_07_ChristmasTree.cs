0abe503e842296c0851ba56cf6ac36c8bacac609
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-25 23:38:53 +0200

    07

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_07_ChristmasTree/_07_ChristmasTree.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_07_ChristmasTree/_07_ChristmasTree.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_ChristmasTree
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print the tree
+        for (int i = 1; i <= n + 1; i++)
+        {
+
+            printRowOfAFigure(n + 1, i);
+
+            Console.WriteLine();
+        }
+    }
+
+    public static void printRowOfAFigure(int n, int i)
+    {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            Console.Write(" ");
+        }
+
+        // Print stars in each row
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            if (k == i)
+            {
+                Console.Write(" | ");
+            }
+            else
+            {
+                Console.Write("*");
+            }
+        }
+    }
+}
