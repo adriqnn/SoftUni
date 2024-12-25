d2db967438b7e7dd2c0a05760045ae06af20add4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-25 23:59:11 +0200

    10

--- 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_10_DiamondV2/_10_DiamondV2.cs
+++ 23_C#Basics/_07_BonusTasks/_01_DrawingFiguresWithLoops/_10_DiamondV2/_10_DiamondV2.cs
@@ -1,2 +1,60 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _10_DiamondV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+
+            // Print upper triangle lines
+            printRowOfAFigure(n, i);
+
+            // Move to the next line
+            Console.WriteLine();
+        }
+
+        // Lower triangle
+        for (int i = n - 1; i >= 1; i--)
+        {
+
+            // Print lower triangle lines
+            printRowOfAFigure(n, i);
+
+            // Move to the next line
+            Console.WriteLine();
+        }
+    }
+
+    public static void printRowOfAFigure(int n, int i)
+    {
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            Console.Write("-");
+        }
+
+        // Print stars in each row
+        for (int k = 1; k <= 2 * i - 1; k++)
+        {
+            if (k == 1 || k == 2 * i - 1)
+            {
+                Console.Write("*");
+            }
+            else
+            {
+                Console.Write("-");
+            }
+        }
+
+        // Print spaces
+        for (int j = 1; j <= n - i; j++)
+        {
+            Console.Write("-");
+        }
+    }
+}
