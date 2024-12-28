aa2b0d76fbfcc0cbc31c218c8bf03b2c7dd2b151
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-28 23:56:22 +0200

    10

--- 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercises/_10_WeatherForecast2.java
+++ 01_ProgrammingBasics/_01_FirstStepsInProgrammingMoreExercises/_10_WeatherForecast2.java
@@ -11,15 +11,15 @@ public class _10_WeatherForecast2 {
         String weatherType = "";
 
         // Categorize weather based on temperature ranges
-        if (degrees >= 5.00 && degrees <= 11.9){
+        if (degrees >= 5.00 && degrees <= 11.9) {
             weatherType = "Cold";
-        } else if (degrees >= 12.00 && degrees <= 14.9){
+        } else if (degrees >= 12.00 && degrees <= 14.9) {
             weatherType = "Cool";
-        } else if (degrees >= 15.00 && degrees <= 20.00){
+        } else if (degrees >= 15.00 && degrees <= 20.00) {
             weatherType = "Mild";
-        } else if (degrees >= 20.1 && degrees <= 25.9){
+        } else if (degrees >= 20.1 && degrees <= 25.9) {
             weatherType = "Warm";
-        } else if (degrees >= 26.00 && degrees <= 35.00){
+        } else if (degrees >= 26.00 && degrees <= 35.00) {
             weatherType = "Hot";
         }else {
             weatherType = "unknown";
