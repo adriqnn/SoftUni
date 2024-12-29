5ce1d3f7ea6e1c1cb2a266ecdc214f0a3952d000
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-29 23:46:36 +0200

    02

--- 01_ProgrammingBasics/_02_ConditionalStatementsMoreExercises/_02_SleepyTomCat.java
+++ 01_ProgrammingBasics/_02_ConditionalStatementsMoreExercises/_02_SleepyTomCat.java
@@ -17,6 +17,7 @@ public class _02_SleepyTomCat {
         // Calculate the difference between Tom's total playtime and the required norm
         double playtimeNorm = 30000 - (vacationDaysPlaytime + workingDaysPlaytime);
 
+        // Print the result
         if (playtimeNorm >= 0) {
             System.out.println("Tom sleeps well");
             System.out.printf("%.0f hours and %.0f minutes less for play", Math.floor(playtimeNorm / 60), playtimeNorm % 60);
