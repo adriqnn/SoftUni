83925a36b69857724a826200efe7edcc03b63885
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-29 22:56:49 +0200

    07

--- 01_ProgrammingBasics/_02_ConditionalStatements/_07_AreaOfFigures.java
+++ 01_ProgrammingBasics/_02_ConditionalStatements/_07_AreaOfFigures.java
@@ -13,23 +13,24 @@ public class _07_AreaOfFigures {
         if (shape.equals("square")) {
             double squareSide = Double.parseDouble(scan.nextLine());
             double squareArea = squareSide * squareSide;
+            
             System.out.printf("%.3f", squareArea);
-
         } else if (shape.equals("rectangle")) {
             double rectangleWidth = Double.parseDouble(scan.nextLine());
             double rectangleHeight = Double.parseDouble(scan.nextLine());
             double rectangleArea = rectangleWidth * rectangleHeight;
+            
             System.out.printf("%.3f", rectangleArea);
-
         } else if (shape.equals("circle")) {
             double circleRadius = Double.parseDouble(scan.nextLine());
             double circleArea = Math.PI * circleRadius * circleRadius;
+            
             System.out.printf("%.3f", circleArea);
-
         } else if (shape.equals("triangle")) {
             double triangleBase = Double.parseDouble(scan.nextLine());
             double triangleHeight = Double.parseDouble(scan.nextLine());
             double triangleArea = (triangleBase * triangleHeight) / 2;
+            
             System.out.printf("%.3f", triangleArea);
         }
         
