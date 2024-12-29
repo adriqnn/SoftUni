a74d7b7fbf9923aec5312007bbdf853bf61a8104
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-29 22:57:36 +0200

    07

--- 23_C#Basics/_02_ConditionalStatements/_07_AreaOfFigures/_07_AreaOfFigures.cs
+++ 23_C#Basics/_02_ConditionalStatements/_07_AreaOfFigures/_07_AreaOfFigures.cs
@@ -12,31 +12,31 @@ public class _07_AreaOfFigures
         {
             double squareSide = double.Parse(Console.ReadLine());
             double squareArea = squareSide * squareSide;
+            
             Console.WriteLine("{0:F3}", squareArea);
-
         }
         else if (shape.Equals("rectangle"))
         {
             double rectangleWidth = double.Parse(Console.ReadLine());
             double rectangleHeight = double.Parse(Console.ReadLine());
             double rectangleArea = rectangleWidth * rectangleHeight;
+            
             Console.WriteLine("{0:F3}", rectangleArea);
-
         }
         else if (shape.Equals("circle"))
         {
             double circleRadius = double.Parse(Console.ReadLine());
             double circleArea = Math.PI * circleRadius * circleRadius;
+            
             Console.WriteLine("{0:F3}", circleArea);
-
         }
         else if (shape.Equals("triangle"))
         {
             double triangleBase = double.Parse(Console.ReadLine());
             double triangleHeight = double.Parse(Console.ReadLine());
             double triangleArea = (triangleBase * triangleHeight) / 2;
+            
             Console.WriteLine("{0:F3}", triangleArea);
         }
-
     }
 }
