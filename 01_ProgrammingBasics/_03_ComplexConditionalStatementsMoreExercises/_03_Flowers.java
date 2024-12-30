58c9a92685f7f5452e5df627e579160a718c8bde
adriqnn <109970483+adriqnn@users.noreply.github.com>
2024-12-31 00:29:24 +0200

    03

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_03_Flowers.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_03_Flowers.java
@@ -1,4 +1,4 @@
-package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercise;
+package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercises;
 
 import java.util.Scanner;
 
@@ -19,26 +19,26 @@ public class _03_Flowers {
         boolean isHoliday = holiday.equals("y");
 
         // Calculate initial price based on season
-        if(season.equals("spring") || season.equals("summer")){
+        if (season.equals("spring") || season.equals("summer")) {
             flowerCost = (chrysanthemums * 2.00) + (roses * 4.10) + (tulips * 2.50);
-        }else{
+        } else {
             flowerCost = (chrysanthemums * 3.75) + (roses * 4.50) + (tulips * 4.15);
         }
 
         // Add price increase based on holiday
-        if(isHoliday) {
+        if (isHoliday) {
             flowerCost *= 1.15;
         }
 
         // Add discount based on season and flower type count
-        if(season.equals("spring") && tulips > 7){
+        if (season.equals("spring") && tulips > 7) {
             flowerCost *= 0.95;
-        }else if(season.equals("winter") && roses >= 10){
+        } else if (season.equals("winter") && roses >= 10){
             flowerCost *= 0.90;
         }
 
         // Add discount on overall flower count
-        if(allFlowersBought >= 20){
+        if (allFlowersBought >= 20) {
             flowerCost *= 0.8;
         }
 
