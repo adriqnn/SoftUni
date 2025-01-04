e21ada5dd9c42fa6b17c158eb52f2c73d7a348e9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-04 22:09:03 +0200

    10

--- 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_10_Profit.java
+++ 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_10_Profit.java
@@ -16,7 +16,7 @@ public class _10_Profit {
         for (int i = 0; i <= coinsOne; i++) {
             for (int j = 0; j <= coinsTwo; j++) {
                 for (int k = 0; k <= coinsFive; k++) {
-                    if(((i * 1) + (j * 2) + (k * 5)) == sum){
+                    if (((i * 1) + (j * 2) + (k * 5)) == sum) {
                         System.out.printf("%d * 1 lv. + %d * 2 lv. + %d * 5 lv. = %d lv.%n", i, j, k, sum);
                     }
                 }
