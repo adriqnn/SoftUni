c4aa0a08020111427d3f97f8fe9da6d42e2f6af3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-04 20:51:32 +0200

    06

--- 01_ProgrammingBasics/_06_NestedLoops/_06_Building.java
+++ 01_ProgrammingBasics/_06_NestedLoops/_06_Building.java
@@ -18,7 +18,7 @@ public class _06_Building {
         }
 
         // Cycle through the rest of the building
-        for (int i = floors - 1; i >= 1; i--){
+        for (int i = floors - 1; i >= 1; i--) {
 
             // New line
             System.out.printf("%n");
