67648a4ecf404ea504af25f2efddd485e3fe2a70
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-04 21:35:06 +0200

    04

--- 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_04_CarNumber.java
+++ 01_ProgrammingBasics/_06_NestedLoopsMoreExercises/_04_CarNumber.java
@@ -15,10 +15,10 @@ public class _04_CarNumber {
             for (int j = beginning; j <= ending; j++) {
                 for (int k = beginning; k <= ending; k++) {
                     for (int l = beginning; l <= ending; l++) {
-                        if((i > l && ((j + k) % 2 == 0))){
-                            if((i % 2 == 0) && (l % 2 != 0)){
+                        if ((i > l && ((j + k) % 2 == 0))) {
+                            if ((i % 2 == 0) && (l % 2 != 0)) {
                                 System.out.printf("%d%d%d%d ", i, j, k, l);
-                            }else if((i % 2 != 0) && (l % 2 == 0)){
+                            } else if ((i % 2 != 0) && (l % 2 == 0)) {
                                 System.out.printf("%d%d%d%d ", i, j, k, l);
                             }
                         }
