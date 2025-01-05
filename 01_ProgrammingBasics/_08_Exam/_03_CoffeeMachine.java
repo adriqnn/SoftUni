8c82d598efbce38f2d2f2a8b7bcf1adca9dc31c2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-06 00:07:16 +0200

    03

--- 01_ProgrammingBasics/_08_Exam/_03_CoffeeMachine.java
+++ 01_ProgrammingBasics/_08_Exam/_03_CoffeeMachine.java
@@ -1,4 +1,4 @@
-package _01_ProgrammingBasics._07_ExamPrep;
+package _01_ProgrammingBasics._07_Exam;
 
 import java.util.Scanner;
 
@@ -15,51 +15,58 @@ public class _03_CoffeeMachine {
         double price = 0;
 
         // Determine the price based on the order
-        switch(type){
+        switch (type) {
             case "Espresso":
-                if(sugar.equals("Without")){
+                if (sugar.equals("Without")) {
                     price = amount * 0.9 * 0.65;
-                    if (amount >= 5){
+                    
+                    if (amount >= 5) {
                         price *= 0.75;
                     }
-                }else if(sugar.equals("Normal")){
+                } else if (sugar.equals("Normal")) {
                     price = amount * 1.00;
-                    if (amount >= 5){
+                    
+                    if (amount >= 5) {
                         price *= 0.75;
                     }
-                }else if(sugar.equals("Extra")){
+                } else if (sugar.equals("Extra")) {
                     price = amount * 1.20;
-                    if (amount >= 5){
+                    
+                    if (amount >= 5) {
                         price *= 0.75;
                     }
                 }
+                
                 break;
             case "Cappuccino":
-                if(sugar.equals("Without")){
+                if (sugar.equals("Without")) {
                     price = amount * 1.00 * 0.65;
-                }else if(sugar.equals("Normal")){
+                } else if (sugar.equals("Normal")) {
                     price = amount * 1.20;
-                }else if(sugar.equals("Extra")){
+                } else if (sugar.equals("Extra")) {
                     price = amount * 1.60;
                 }
+
                 break;
             case "Tea":
-                if(sugar.equals("Without")){
+                if (sugar.equals("Without")) {
                     price = amount * 0.5 * 0.65;
-                }else if(sugar.equals("Normal")){
+                } else if (sugar.equals("Normal")) {
                     price = amount * 0.60;
-                }else if(sugar.equals("Extra")){
+                } else if (sugar.equals("Extra")) {
                     price =  amount * 0.70;
                 }
+                
                 break;
         }
 
         // Print result
-        if (price > 15){
+        if (price > 15) {
             System.out.printf("You bought %d cups of %s for %.2f lv.", amount, type, price * 0.8);
-        }else
+        } else {
             System.out.printf("You bought %d cups of %s for %.2f lv.", amount, type, price);
-
+        }
+        
         scan.close();
     }
 }
