270a3a2a841615dd3fd5f8186e9becde63d2668d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-05 20:06:07 +0200

    03

--- 01_ProgrammingBasics/_07_ExamPrep/_04_ExamPrep/_03_PartyShop.java
+++ 01_ProgrammingBasics/_07_ExamPrep/_04_ExamPrep/_03_PartyShop.java
@@ -1,4 +1,4 @@
-package _01_ProgrammingBasics._08_Exam;
+package _01_ProgrammingBasics._08_ExamPrep;
 
 import java.util.Scanner;
 
@@ -15,41 +15,44 @@ public class _03_PartyShop {
         double price = 0;
 
         // Determine the price based on the input
-        switch(type){
+        switch (type) {
             case "Cake":
-                if (days <= 15){
+                if (days <= 15) {
                     price = 24.00 * count;
-                }else{
+                } else {
                     price = 28.70 * count;
                 }
+                
                 break;
             case "Souffle":
-                if (days <= 15){
+                if (days <= 15) {
                     price = 6.66 * count;
-                }else{
+                } else {
                     price = 9.80 * count;
                 }
+                
                 break;
             case "Baklava":
-                if (days <= 15){
+                if (days <= 15) {
                     price = 12.60 * count;
-                }else{
+                } else {
                     price = 16.98 * count;
                 }
+                
                 break;
         }
 
         // Add discount if requirements are met
-        if(days <= 22){
-            if (price >= 100 && price <= 200){
+        if (days <= 22) {
+            if (price >= 100 && price <= 200) {
                 price *= 0.85;
-            }else if(price > 200){
+            } else if (price > 200) {
                 price *= 0.75;
             }
         }
 
         // Add discount if requirements are met
-        if (days <= 15){
+        if (days <= 15) {
             price *= 0.90;
         }
 
