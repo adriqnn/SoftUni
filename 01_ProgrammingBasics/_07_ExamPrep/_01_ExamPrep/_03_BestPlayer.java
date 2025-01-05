2b5101c3bc1f22a5ab1650d41a22e0dc5c87932e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-05 19:48:48 +0200

    03

--- 01_ProgrammingBasics/_07_ExamPrep/_01_ExamPrep/_03_BestPlayer.java
+++ 01_ProgrammingBasics/_07_ExamPrep/_01_ExamPrep/_03_BestPlayer.java
@@ -1,4 +1,4 @@
-package _01_ProgrammingBasics._08_Exam;
+package _01_ProgrammingBasics._08_ExamPrep;
 
 import java.util.Scanner;
 
@@ -14,26 +14,27 @@ public class _03_BestPlayer {
         int max = Integer.MIN_VALUE;
 
         // Find the best player
-        while(!console.equals("END")){
+        while (!console.equals("END")) {
             int score = Integer.parseInt(scan.nextLine());
-            if (score > max){
+            if (score > max) {
                 max = score;
                 bestPlayer = console;
-                if (score >= 10){
+                if (score >= 10) {
                     break;
                 }
             }
+            
             console = scan.nextLine();
         }
 
         // Print result
-        if(max >= 10){
+        if (max >= 10) {
             System.out.printf("%s is the best player!%n", bestPlayer);
             System.out.printf("He has scored %d goals and made a hat-trick !!!", max);
-        }else if(max >= 3){
+        } else if (max >= 3) {
             System.out.printf("%s is the best player!%n", bestPlayer);
             System.out.printf("He has scored %d goals and made a hat-trick !!!", max);
-        }else{
+        } else {
             System.out.printf("%s is the best player!%n", bestPlayer);
             System.out.printf("He has scored %d goals.", max);
         }
