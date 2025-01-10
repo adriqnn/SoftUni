ac37671d48ca0d0b537bf1d4948bb0f923f4939d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-10 23:08:21 +0200

    09

--- 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_09_NumbersFrom1To10.java
+++ 01_ProgrammingBasics/_03_ComplexConditionalStatementsMoreExercises/_09_NumbersFrom1To10.java
@@ -2,7 +2,6 @@ package _01_ProgrammingBasics._03_ComplexConditionalStatementsMoreExercises;
 
 public class _09_NumbersFrom1To10 {
     public static void main(String[] args) {
-
         // Print numbers from 1 to 10
         for (int i = 0; i < 10; i++) {
             System.out.println(i + 1);
