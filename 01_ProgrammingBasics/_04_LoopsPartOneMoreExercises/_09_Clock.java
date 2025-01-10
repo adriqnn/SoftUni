805d46ab7233d7a2ba212f4524c7916b6a763a4d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-10 23:34:42 +0200

    09

--- 01_ProgrammingBasics/_04_LoopsPartOneMoreExercises/_09_Clock.java
+++ 01_ProgrammingBasics/_04_LoopsPartOneMoreExercises/_09_Clock.java
@@ -2,7 +2,6 @@ package _01_ProgrammingBasics._04_LoopsPartOneMoreExercises;
 
 public class _09_Clock {
     public static void main(String[] args) {
-
         // Print time
         for (int i = 0; i < 24; i++) {
             for (int j = 0; j < 60; j++) {
