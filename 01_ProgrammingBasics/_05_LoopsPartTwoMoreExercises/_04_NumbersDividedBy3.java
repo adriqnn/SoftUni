05b0bb6a4477f0bc5589e93d957f69bcfdbe7139
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-11 00:08:51 +0200

    04

--- 01_ProgrammingBasics/_05_LoopsPartTwoMoreExercises/_04_NumbersDividedBy3.java
+++ 01_ProgrammingBasics/_05_LoopsPartTwoMoreExercises/_04_NumbersDividedBy3.java
@@ -2,7 +2,6 @@ package _01_ProgrammingBasics._05_LoopsPartTwoMoreExercises;
 
 public class _04_NumbersDividedBy3 {
     public static void main(String[] args) {
-
         // Print numbers from 1 to 100 that are divisible by 3
         for (int i = 1; i < 100; i++) {
             if (i % 3 == 0) {
