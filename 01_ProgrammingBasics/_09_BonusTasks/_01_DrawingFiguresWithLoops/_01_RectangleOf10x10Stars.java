c90462c8181036870fbff12c015db0b77f9ad641
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-01-12 00:25:40 +0200

    01

--- 01_ProgrammingBasics/_09_BonusTasks/_01_DrawingFiguresWithLoops/_01_RectangleOf10x10Stars.java
+++ 01_ProgrammingBasics/_09_BonusTasks/_01_DrawingFiguresWithLoops/_01_RectangleOf10x10Stars.java
@@ -2,7 +2,6 @@ package _01_ProgrammingBasics._09_BonusTasks._01_DrawingFiguresWithLoops;
 
 public class _01_RectangleOf10x10Stars {
     public static void main(String[] args) {
-
         // Print a square 10x10 of stars
         for (int i = 1; i <= 10; i++) {
             for (int j = 1; j <= 10; j++) {
