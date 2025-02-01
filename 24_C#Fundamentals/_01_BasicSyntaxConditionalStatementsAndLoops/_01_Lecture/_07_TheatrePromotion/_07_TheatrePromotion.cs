7436371f2de556208610816a69d8a4a88b935318
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:45:39 +0200

    07

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_07_TheatrePromotion/_07_TheatrePromotion.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_07_TheatrePromotion/_07_TheatrePromotion.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_TheatrePromotion
+{
+    public static void Main(string[] args)
+    {
+        // Read input values from the console
+        string day = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+
+        // Print price of a theater ticket
+        if (age < 0 || age > 122)
+        {
+            Console.WriteLine("Error!");
+        }
+        else if (age <= 18)
+        {
+            switch (day)
+            {
+                case "Weekday":
+                    Console.WriteLine("12$");
+                    break;
+                case "Weekend":
+                    Console.WriteLine("15$");
+                    break;
+                case "Holiday":
+                    Console.WriteLine("5$");
+                    break;
+            }
+        }
+        else if (age <= 64)
+        {
+            switch (day)
+            {
+                case "Weekday":
+                    Console.WriteLine("18$");
+                    break;
+                case "Weekend":
+                    Console.WriteLine("20$");
+                    break;
+                case "Holiday":
+                    Console.WriteLine("12$");
+                    break;
+            }
+        }
+        else
+        {
+            switch (day)
+            {
+                case "Weekday":
+                    Console.WriteLine("12$");
+                    break;
+                case "Weekend":
+                    Console.WriteLine("15$");
+                    break;
+                case "Holiday":
+                    Console.WriteLine("10$");
+                    break;
+            }
+        }
+    }
+}
