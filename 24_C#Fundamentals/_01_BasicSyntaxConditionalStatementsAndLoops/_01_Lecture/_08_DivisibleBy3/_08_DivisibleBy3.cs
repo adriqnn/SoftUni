e5a5ed3884d64d03945ce16817cb59c9e3afff27
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:47:32 +0200

    08

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_08_DivisibleBy3/_08_DivisibleBy3.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_08_DivisibleBy3/_08_DivisibleBy3.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_DivisibleBy3
+{
+    public static void Main(string[] args)
+    {
+        // Print numbers that are divisible by 3
+        for (int i = 3; i <= 99; i += 3)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
