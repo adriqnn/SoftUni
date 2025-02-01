1a22f897bdd4ad4a2b9cf5a63abd8eaebe1d79e6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:34:09 +0200

    05

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_05_MonthPrinter/_05_MonthPrinter.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_05_MonthPrinter/_05_MonthPrinter.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_MonthPrinter
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Add variable
+        string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+        // Print result
+        try
+        {
+            Console.WriteLine(months[num - 1]);
+        }catch (Exception e) {
+            Console.WriteLine("Error!");
+        }
+    }
+}
