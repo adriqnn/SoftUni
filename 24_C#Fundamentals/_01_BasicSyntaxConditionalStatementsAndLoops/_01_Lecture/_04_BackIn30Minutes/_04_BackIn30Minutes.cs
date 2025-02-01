1aff25d87368f3f5793f2d0af3e6df7a13c8badc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:30:41 +0200

    04

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_04_BackIn30Minutes/_04_BackIn30Minutes.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_04_BackIn30Minutes/_04_BackIn30Minutes.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_BackIn30Minutes
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int hours = int.Parse(Console.ReadLine());
+        int minutes = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int timePLus30 = hours * 60 + minutes + 30;
+        int newHour = timePLus30 / 60;
+        int newMinutes = timePLus30 % 60;
+
+        if (newHour > 23)
+        {
+            newHour = 0;
+        }
+
+        // Print result
+        Console.WriteLine($"{newHour}:{newMinutes:D2}");
+    }
+}
