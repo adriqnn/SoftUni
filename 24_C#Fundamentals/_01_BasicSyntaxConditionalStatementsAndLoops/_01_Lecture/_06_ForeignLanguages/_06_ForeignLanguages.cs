73a04303c2364b3c91350b058834ef9580acaab3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:41:59 +0200

    06

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_06_ForeignLanguages/_06_ForeignLanguages.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_06_ForeignLanguages/_06_ForeignLanguages.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_ForeignLanguages
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string country = Console.ReadLine();
+
+        // Print country based on the input
+        switch (country) 
+        {
+            case "England":
+            case "USA":
+                Console.WriteLine("English");
+                break;
+            case "Spain":
+            case "Argentina":
+            case "Mexico":
+                Console.WriteLine("Spanish");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
