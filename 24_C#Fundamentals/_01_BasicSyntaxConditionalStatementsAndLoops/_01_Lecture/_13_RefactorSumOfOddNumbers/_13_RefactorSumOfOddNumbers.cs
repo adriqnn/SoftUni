db01d5fc12ddb24392a24a329d37d0bf0855d4e9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:58:48 +0200

    13

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_13_RefactorSumOfOddNumbers/_13_RefactorSumOfOddNumbers.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_13_RefactorSumOfOddNumbers/_13_RefactorSumOfOddNumbers.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _13_RefactorSumOfOddNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value from the console
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        // Sum all the odd numbers and print each odd number
+        for (int i = 1; i < n * 2; i += 2)
+        {
+            sum += i;
+            Console.WriteLine(i);
+        }
+
+        // Print the sum of odd numbers
+        Console.WriteLine("Sum: " + sum);
+    }
+}
