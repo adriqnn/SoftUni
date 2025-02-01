c44ade04cea576beb23d3a59a0d0a05873fdc133
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 22:33:04 +0200

    01

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_01_StudentInformation/_01_StudentInformation.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_01_StudentInformation/_01_StudentInformation.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_StudentInformation
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string name = Console.ReadLine();
+        int age = int.Parse(Console.ReadLine());
+        double grade = double.Parse(Console.ReadLine());
+
+        // Print result 
+        System.Console.WriteLine(StudentPrint(name, age, grade));
+    }
+
+    // Add method for the printing
+    private static string StudentPrint(string name, int age, double grade)
+    {
+        return $"Name: {name}, Age: {age}, Grade: {grade:F2}";
+    }
+}
