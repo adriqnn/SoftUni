8e309c812bd1e1da8a0ebda8a88c7023f59205d6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:56:57 +0200

    12

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_12_EvenNumber/_12_EvenNumber.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_12_EvenNumber/_12_EvenNumber.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _12_EvenNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value from the console
+        string input = Console.ReadLine();
+
+        // Continue processing until "STOP" is entered
+        while (input != "STOP")
+        {
+            int n = Math.Abs(int.Parse(input));
+
+            if (n % 2 == 0)
+            {
+                Console.WriteLine($"The number is: {n}");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Please write an even number.");
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
