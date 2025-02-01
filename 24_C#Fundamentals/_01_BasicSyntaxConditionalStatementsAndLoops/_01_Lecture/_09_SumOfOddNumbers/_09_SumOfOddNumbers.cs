8b76e30b0a4de9795f8a4e4fca8459f07abb598e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:49:58 +0200

    09

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_09_SumOfOddNumbers/_09_SumOfOddNumbers.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_09_SumOfOddNumbers/_09_SumOfOddNumbers.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_SumOfOddNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int sum = 0;
+
+        // Sum odd numbers
+        for (int i = 1; i < n * 2; i += 2)
+        {
+            sum += i;
+            Console.WriteLine(i);
+        }
+
+        // Print Result
+        Console.WriteLine("Sum: " + sum);
+    }
+}
