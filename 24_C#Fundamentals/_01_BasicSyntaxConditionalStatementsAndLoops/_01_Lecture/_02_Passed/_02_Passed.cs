14c0d0c965cbd98e68c86de8de1128bb19c2824d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:01:10 +0200

    02

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_02_Passed/_02_Passed.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_02_Passed/_02_Passed.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_Passed
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double grade = double.Parse(Console.ReadLine());
+
+        // Print result
+        if (grade >= 3.00)
+        {
+            Console.WriteLine("Passed!");
+        }
+    }
+}
