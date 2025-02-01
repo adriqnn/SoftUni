84d314e3e1e19f38738d59cb56a739535e9a1bce
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-01 23:54:27 +0200

    11

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_11_MultiplicationTable2/_11_MultiplicationTable2.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_11_MultiplicationTable2/_11_MultiplicationTable2.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _11_MultiplicationTable2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        // Print the multiplication based on the input
+        if (n2 > 10)
+        {
+            Console.WriteLine($"{n1} X {n2} = {n1 * n2}");
+        }
+        else
+        {
+            for (int i = n2; i <= 10; i++)
+            {
+                Console.WriteLine($"{n1} X {i} = {n1 * i}");
+            }
+        }
+    }
+}
