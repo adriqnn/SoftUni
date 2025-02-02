7cc43f34a8fe493ec11ae176d16586d65c8065f0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 12:30:33 +0200

    01

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_01_Ages/_01_Ages.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_01_Ages/_01_Ages.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_Ages
+{
+    public static void Main(string[] args)
+    {
+        // Raed input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print result based on the input
+        if (n <= 2)
+        {
+            Console.WriteLine("baby");
+        }
+        else if (n <= 13)
+        {
+            Console.WriteLine("child");
+        }
+        else if (n <= 19)
+        {
+            Console.WriteLine("teenager");
+        }
+        else if (n <= 65)
+        {
+            Console.WriteLine("adult");
+        }
+        else 
+        {
+            Console.WriteLine("elder");
+        }
+    }
+}
