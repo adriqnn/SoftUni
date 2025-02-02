4631a114386aaab0e25852ca2a616df84098686f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 14:01:16 +0200

    08

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_08_TriangleOfNumbers/_08_TriangleOfNumbers.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_08_TriangleOfNumbers/_08_TriangleOfNumbers.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_TriangleOfNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print triangle of numbers
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
