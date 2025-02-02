679bdd8991fa943c45cdc0fbc796fe88e544098e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-03 00:25:18 +0200

    01

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_01_Ages.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_01_Ages.java
@@ -6,19 +6,19 @@ public class _01_Ages {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Raed input valuue
+        // Raed input value
         int n = Integer.parseInt(scan.nextLine());
 
         // Print result based on the input
-        if (n <= 2){
+        if (n <= 2) {
             System.out.println("baby");
-        }else if (n <= 13){
+        } else if (n <= 13) {
             System.out.println("child");
-        }else if (n <= 19){
+        } else if (n <= 19) {
             System.out.println("teenager");
-        }else if (n <= 65){
+        } else if (n <= 65) {
             System.out.println("adult");
-        }else{
+        } else {
             System.out.println("elder");
         }
 
