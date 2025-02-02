5e4407bf65f69df258fc7312f0074b26ed7a63f8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 17:50:17 +0200

    04

---
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseStringV2/_04_ReverseStringV2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class _04_ReverseStringV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value and reverse
+        string[] reversed = Console.ReadLine().ToCharArray().Reverse().ToArray();
+
+        // Print result
+        Console.WriteLine(reversed);
+    }
+}
