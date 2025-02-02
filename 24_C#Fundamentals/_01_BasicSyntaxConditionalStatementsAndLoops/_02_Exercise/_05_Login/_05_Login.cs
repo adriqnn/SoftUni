5573575525d2fd98c5245d5b1c1d7accdeb2b697
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 13:27:11 +0200

    05

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_05_Login/_05_Login.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_05_Login/_05_Login.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _05_Login
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string username = Console.ReadLine();
+        string console = Console.ReadLine();
+
+        // Add variables
+        StringBuilder reverseUsername = new StringBuilder();
+        int count = 0;
+
+        // Reverse the username
+        for (int i = username.Length - 1; i >= 0; i--)
+        {
+            reverseUsername.Append(username[i]);
+        }
+
+        // Check if the user can log in or block after few unsuccessful attempts
+        while (console != reverseUsername.ToString())
+        {
+            count++;
+
+            if (count == 4)
+            {
+                Console.WriteLine($"User {username} blocked!");
+                return;
+            }
+
+            Console.WriteLine("Incorrect password. Try again.");
+            console = Console.ReadLine();
+        }
+
+        Console.WriteLine($"User {username} logged in.");
+    }
+}
