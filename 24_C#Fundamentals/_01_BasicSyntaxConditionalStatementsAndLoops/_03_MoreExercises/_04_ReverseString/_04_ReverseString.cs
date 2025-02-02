71d7f5e75054f21309d133bede816e390f6f2d4c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 17:38:26 +0200

    04

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseString/_04_ReverseString.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_04_ReverseString/_04_ReverseString.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variable
+        StringBuilder rebuild = new StringBuilder();
+
+        // Reverse string
+        for (int i = console.Length - 1; i >= 0; i--)
+        {
+            char symbol = console[i];
+            rebuild.Append(symbol);
+        }
+
+        Console.WriteLine(rebuild.ToString());
+    }
+}
