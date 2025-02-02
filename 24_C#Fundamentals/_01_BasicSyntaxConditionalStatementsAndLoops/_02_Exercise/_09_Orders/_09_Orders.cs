2a0e90238507536a7c0d60fcf4d7662a60edcfde
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 14:17:37 +0200

    09

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_09_Orders/_09_Orders.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_09_Orders/_09_Orders.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_Orders
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int numberOfOrders = int.Parse(Console.ReadLine());
+
+        // Add variable
+        double totalPrice = 0;
+
+        // Calculate the price for all the orders
+        for (int i = 0; i < numberOfOrders; i++)
+        {
+            // Orders
+            double pricePerCapsule = double.Parse(Console.ReadLine());
+            int daysInMonth = int.Parse(Console.ReadLine());
+            int capsulesCount = int.Parse(Console.ReadLine());
+
+            double priceFormula = ((daysInMonth * capsulesCount) * pricePerCapsule);
+            Console.WriteLine($"The price for the coffee is: ${priceFormula:F2}");
+            totalPrice += priceFormula;
+        }
+
+        // Print result
+        Console.WriteLine($"Total: ${totalPrice:F2}");
+    }
+}
