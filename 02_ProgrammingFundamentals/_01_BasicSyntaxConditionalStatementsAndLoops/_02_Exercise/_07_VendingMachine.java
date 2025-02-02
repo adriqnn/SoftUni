ba7d6e9a566b687cb1bc4dd92b9591e9f4e1873b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-03 00:38:16 +0200

    07

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_07_VendingMachine.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_07_VendingMachine.java
@@ -13,11 +13,12 @@ public class _07_VendingMachine {
         double sum = 0;
 
         // Sum the coins
-        while(!console.equals("Start")){
+        while (!console.equals("Start")) {
             double coins = Double.parseDouble(console);
-            if(coins == 0.1 || coins == 0.2 || coins == 0.5 || coins == 1.0 || coins == 2.0){
+            
+            if (coins == 0.1 || coins == 0.2 || coins == 0.5 || coins == 1.0 || coins == 2.0) {
                 sum += coins;
-            }else{
+            } else {
                 System.out.printf("Cannot accept %.2f%n", coins);
             }
 
@@ -28,50 +29,56 @@ public class _07_VendingMachine {
         String console2 = scan.nextLine();
 
         // Use vending machine
-        while(!console2.equals("End")){
-            if (console2.equals("Nuts")){
+        while (!console2.equals("End")) {
+            if (console2.equals("Nuts")) {
                 sum -= 2.0;
+                
                 if (sum >= 0) {
                     System.out.printf("Purchased Nuts%n");
-                }else{
+                } else {
                     System.out.println("Sorry, not enough money");
                     sum += 2.0;
                 }
-            }else if (console2.equals("Water")){
+            } else if (console2.equals("Water")) {
                 sum -= 0.7;
+                
                 if (sum >= 0) {
                     System.out.printf("Purchased Water%n");
-                }else{
+                } else {
                     System.out.println("Sorry, not enough money");
                     sum += 0.7;
                 }
-            }else if (console2.equals("Crisps")){
+            } else if (console2.equals("Crisps")) {
                 sum -= 1.5;
+                
                 if (sum >= 0) {
                     System.out.printf("Purchased Crisps%n");
-                }else{
+                } else {
                     System.out.println("Sorry, not enough money");
                     sum += 1.5;
                 }
-            }else if (console2.equals("Soda")){
+            } else if (console2.equals("Soda")) {
                 sum -= 0.8;
+                
                 if (sum >= 0) {
                     System.out.printf("Purchased Soda%n");
-                }else{
+                } else {
                     System.out.println("Sorry, not enough money");
                     sum += 0.8;
                 }
-            }else if (console2.equals("Coke")){
+            } else if (console2.equals("Coke")) {
                 sum -= 1.0;
+                
                 if (sum >= 0) {
                     System.out.printf("Purchased Coke%n");
-                }else{
+                } else {
                     System.out.println("Sorry, not enough money");
                     sum += 1.0;
                 }
-            }else{
+            } else {
                 System.out.println("Invalid product");
             }
+            
             console2 = scan.nextLine();
         }
 
