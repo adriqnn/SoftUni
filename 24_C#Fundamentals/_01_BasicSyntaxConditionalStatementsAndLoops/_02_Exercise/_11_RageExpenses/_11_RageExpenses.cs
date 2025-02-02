28bce4a279746aaf0144736ee6ea7902392a6fc5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 14:32:38 +0200

    11

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_11_RageExpenses/_11_RageExpenses.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_11_RageExpenses/_11_RageExpenses.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _05_Login
+{
+    public static void Main(string[] args)
+    {
+        // Read input values from the console
+        int l = int.Parse(Console.ReadLine());
+        double headset = double.Parse(Console.ReadLine());
+        double mouse = double.Parse(Console.ReadLine());
+        double keyboard = double.Parse(Console.ReadLine());
+        double display = double.Parse(Console.ReadLine());
+
+        // Add variables
+        int headsetCount = 0;
+        int mouseCount = 0;
+        int keyboardCount = 0;
+
+        // Make a statistic for the items
+        for (int i = 1; i <= l; i++)
+        {
+            if (i % 2 == 0)
+            {
+                headsetCount++;
+            }
+            if (i % 3 == 0)
+            {
+                mouseCount++;
+            }
+            if (i % 2 == 0 && i % 3 == 0)
+            {
+                keyboardCount++;
+            }
+        }
+
+        // Calculate prices
+        double displayCount = Math.Floor(keyboardCount / 2.0);
+        double totalExpenses = (headset * headsetCount) + (mouse * mouseCount) + (keyboard * keyboardCount) + (display * displayCount);
+
+        // Print result
+        Console.WriteLine($"Rage expenses: {totalExpenses:F2} lv.");
+    }
+}
