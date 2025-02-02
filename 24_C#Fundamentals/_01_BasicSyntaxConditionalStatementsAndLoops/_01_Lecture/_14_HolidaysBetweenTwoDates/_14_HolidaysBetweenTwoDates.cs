1346a18f361ff47fecf2a52f21ea4c4526ce2af6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 12:08:51 +0200

    14

---
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_14_HolidaysBetweenTwoDates/_14_HolidaysBetweenTwoDates.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+public class _14_HolidaysBetweenTwoDates
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string dateOne = Console.ReadLine();
+        string dateTwo = Console.ReadLine();
+
+        DateTime startDate = DateTime.ParseExact(dateOne, "d.M.yyyy", CultureInfo.InvariantCulture);
+        DateTime endDate = DateTime.ParseExact(dateTwo, "d.M.yyyy", CultureInfo.InvariantCulture);
+
+        // Add variables
+        int holidaysCount = 0;
+
+        for (DateTime date = startDate; date <= endDate; date = date.AddDays(1))
+        {
+            // Check if the day is Saturday or Sunday.
+            if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+            {
+                holidaysCount++;
+            }
+        }
+
+        // Print result
+        Console.WriteLine(holidaysCount);
+    }
+}
