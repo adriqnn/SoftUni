5c3d8edea5120f940a3a6cef3051ef497bbf3fc5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 16:23:25 +0200

    02

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_02_EnglishNameOfTheLastDigit/_02_EnglishNameOfTheLastDigit.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_02_EnglishNameOfTheLastDigit/_02_EnglishNameOfTheLastDigit.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _02_EnglishNameOfTheLastDigit
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int n = num % 10;
+        string[] word = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+
+        Console.WriteLine(word[n]);
+    }
+}
