a2096ccc66eb025829f3091719e51773a6cef2a9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 17:31:37 +0200

    03

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_03_GamingStore/_03_GamingStore.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_03_GamingStore/_03_GamingStore.cs
@@ -1,2 +1,128 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _03_GamingStore
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        double balance = double.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        // Add variables
+        double budget = balance;
+        double spent = 0;
+
+        // Make statistics for game-store
+        while (input != "Game Time")
+        {
+            if (input == "OutFall 4")
+            {
+                budget -= 39.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 39.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought OutFall 4");
+                spent += 39.99;
+            }
+            else if (input == "CS: OG")
+            {
+                budget -= 15.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 15.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought CS: OG");
+                spent += 15.99;
+            }
+            else if (input == "Zplinter Zell")
+            {
+                budget -= 19.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 19.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought Zplinter Zell");
+                spent += 19.99;
+            }
+            else if (input == "Honored 2")
+            {
+                budget -= 59.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 59.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought Honored 2");
+                spent += 59.99;
+            }
+            else if (input == "RoverWatch")
+            {
+                budget -= 29.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 29.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought RoverWatch");
+                spent += 29.99;
+            }
+            else if (input == "RoverWatch Origins Edition")
+            {
+                budget -= 39.99;
+
+                if (budget < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                    budget += 39.99;
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Bought RoverWatch Origins Edition");
+                spent += 39.99;
+            }
+            else
+            {
+                Console.WriteLine("Not Found");
+            }
+
+            if (budget == 0)
+            {
+                Console.WriteLine("Out of money!");
+                break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        if (input == "Game Time")
+        {
+            Console.WriteLine($"Total spent: ${spent:F2}. Remaining: ${balance - spent:F2}");
+        }
+    }
+}
