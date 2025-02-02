b24246deb5fad17023195f04d0752eb9ed207aae
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 13:06:47 +0200

    04

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_04_PrintAndSum/_04_PrintAndSum.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_04_PrintAndSum/_04_PrintAndSum.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_PrintAndSum
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int nStart = int.Parse(Console.ReadLine());
+        int nEnd = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int sum = 0;
+
+        // Print numbers
+        for (int i = nStart; i <= nEnd; i++)
+        {
+            Console.Write(i + " ");
+            sum += i;
+        }
+
+        // Print result
+        Console.WriteLine();
+        Console.WriteLine($"Sum: {sum}");
+    }
+}
