2c46e6b990a6c179602d90bcdcac0f91bbc7266c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-03 00:13:00 +0200

    10

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_10_MultiplicationTable.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_10_MultiplicationTable.java
@@ -9,9 +9,9 @@ public class _10_MultiplicationTable {
         // Read input value 
         int n = Integer.parseInt(scan.nextLine());
 
-        // Print multiplication table for a given nmber
+        // Print multiplication table for a given number
         for (int i = 1; i <= 10; i++) {
-            System.out.printf("%d X %d = %d%n", n, i, n*i);
+            System.out.printf("%d X %d = %d%n", n, i, n * i);
         }
 
         scan.close();
