a342b41c2be6d4969b08f29fde653f6bf1bf4f47
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-03 00:27:12 +0200

    02

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_02_Division.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_02_Division.java
@@ -10,20 +10,20 @@ public class _02_Division {
         int n = Integer.parseInt(scan.nextLine());
 
         // Determine if the value is divisible by either 2, 3, 6, 7, 10 or if it's not divisible
-        if(n % 10 == 0){
+        if (n % 10 == 0) {
             System.out.print("The number is divisible by 10");
-        }else if(n % 7 == 0){
+        } else if (n % 7 == 0) {
             System.out.print("The number is divisible by 7");
-        }else if(n % 6 == 0){
+        } else if (n % 6 == 0) {
             System.out.print("The number is divisible by 6");
-        }else if(n % 3 == 0){
+        } else if (n % 3 == 0) {
             System.out.print("The number is divisible by 3");
-        }else if(n % 2 == 0){
+        } else if (n % 2 == 0) {
             System.out.print("The number is divisible by 2");
-        }else{
+        } else {
             System.out.println("Not divisible");
         }
-        
+
         scan.close();
     }
 }
