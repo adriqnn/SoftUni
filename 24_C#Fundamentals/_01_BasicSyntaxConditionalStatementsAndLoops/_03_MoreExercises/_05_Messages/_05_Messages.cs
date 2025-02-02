aac29e637771c985747e5728a878c7424b70844b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 18:03:32 +0200

    05

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_05_Messages/_05_Messages.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_05_Messages/_05_Messages.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _05_Messages
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        StringBuilder build = new StringBuilder();
+
+        // Decipher the message
+        for (int i = 1; i <= n; i++)
+        {
+            int console = int.Parse(Console.ReadLine());
+            int numDigits = console.ToString().Length;
+
+            int mainDigit = console % 10;
+            int offset = (mainDigit - 2) * 3;
+
+            if (mainDigit == 8 || mainDigit == 9)
+            {
+                offset += 1;
+            }
+
+            int index = offset + numDigits - 1;
+            char symbol = (char)(index + 97);
+
+            if (console == 0)
+            {
+                build.Append(" ");
+            }
+            else
+            {
+                build.Append(symbol);
+            }
+        }
+
+        Console.WriteLine(build.ToString());
+    }
+}
