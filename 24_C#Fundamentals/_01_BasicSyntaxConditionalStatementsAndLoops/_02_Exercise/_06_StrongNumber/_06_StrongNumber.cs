688b6ae6298a1d8b9c412f2e298aa986e16e8a35
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 13:38:18 +0200

    06

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_06_StrongNumber/_06_StrongNumber.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_02_Exercise/_06_StrongNumber/_06_StrongNumber.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.PortableExecutable;
+
+public class _06_StrongNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string console = Console.ReadLine();
+        int n = int.Parse(console);
+
+        // Add variable
+        int sum = 0;
+
+        // Sum values
+        for (int i = 0; i < console.Length; i++)
+        {
+            char symbol = console[i];
+            int num = (int) char.GetNumericValue(symbol);
+
+            int factorial = 1;
+            for (int j = 1; j <= num; j++)
+            {
+                factorial = factorial * j;
+
+            }
+
+            sum += factorial;
+        }
+
+        // Print result
+        if (sum == n)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
