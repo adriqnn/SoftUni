76e8270ec02fc3e2bc10af94ac0067e171d620d3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-03 00:14:45 +0200

    11

--- 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_11_MultiplicationTable2.java
+++ 02_ProgrammingFundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_01_Lecture/_11_MultiplicationTable2.java
@@ -6,14 +6,14 @@ public class _11_MultiplicationTable2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input value
+        // Read input values
         int n1 = Integer.parseInt(scan.nextLine());
         int n2 = Integer.parseInt(scan.nextLine());
 
         // Print the multiplication based on the input
-        if (n2 > 10){
+        if (n2 > 10) {
             System.out.printf("%d X %d = %d%n", n1, n2, n1 * n2);
-        }else {
+        } else {
             for (int i = n2; i <= 10; i++) {
                 System.out.printf("%d X %d = %d%n", n1, i, n1 * i);
             }
