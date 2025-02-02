efa47551b53426a1aefcbdfbd5d7850fbe0b6186
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-02 16:21:11 +0200

    01

--- 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_01_SortNumbers/_01_SortNumbers.cs
+++ 24_C#Fundamentals/_01_BasicSyntaxConditionalStatementsAndLoops/_03_MoreExercises/_01_SortNumbers/_01_SortNumbers.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_SortNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Add variable
+        List<int> numbers = new List<int>();
+
+        // Read input values
+        for (int i = 0; i < 3; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            numbers.Add(n);
+        }
+
+        // Print result
+        numbers.OrderByDescending(n => n).ToList().ForEach(Console.WriteLine);
+    }
+}
