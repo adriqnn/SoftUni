360bba7e6f9c2461272415c3c43a3f22a5d8430f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-04 23:48:31 +0200

    10

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_10_SpecialNumbers.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_10_SpecialNumbers.java
@@ -14,15 +14,15 @@ public class _10_SpecialNumbers {
             int num = i;
             int sum = 0;
 
-            while(num != 0){
+            while (num != 0) {
                 sum += num % 10;
                 num /= 10;
             }
 
             // Print result
-            if(sum  == 5 || sum  == 7 || sum == 11){
+            if (sum  == 5 || sum  == 7 || sum == 11) {
                 System.out.printf("%d -> True%n", i);
-            }else{
+            } else {
                 System.out.printf("%d -> False%n", i);
             }
         }
