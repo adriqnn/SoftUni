1068f5e1c7b23b955d58bcc815bf0aa0579ef33d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-04 23:52:06 +0200

    12

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_12_RefactorSpecialNumbers.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_01_Lecture/_12_RefactorSpecialNumbers.java
@@ -9,25 +9,24 @@ public class _12_RefactorSpecialNumbers {
         // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
-        
         // Find special number
         for (int i = 1; i <= n; i++) {
             int num = i;
             int sum = 0;
-            
-            while(num != 0){
+
+            while (num != 0) {
                 sum += num % 10;
                 num /= 10;
             }
-            
-            // Print result 
-            if(sum  == 5 || sum  == 7 || sum == 11){
+
+            // Print result
+            if (sum  == 5 || sum  == 7 || sum == 11) {
                 System.out.printf("%d -> True%n", i);
-            }else{
+            } else {
                 System.out.printf("%d -> False%n", i);
             }
         }
-        
+
         scan.close();
     }
 }
