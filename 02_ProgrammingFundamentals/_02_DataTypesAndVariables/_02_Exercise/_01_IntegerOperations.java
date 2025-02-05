26540bebc64689344cfc10869b27d4d64170b1d3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-05 23:48:19 +0200

    01

--- 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_01_IntegerOperations.java
+++ 02_ProgrammingFundamentals/_02_DataTypesAndVariables/_02_Exercise/_01_IntegerOperations.java
@@ -6,7 +6,7 @@ public class _01_IntegerOperations {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input values
+        // Read input value
         int n1 = Integer.parseInt(scan.nextLine()), n2 = Integer.parseInt(scan.nextLine()), n3 = Integer.parseInt(scan.nextLine()), n4 = Integer.parseInt(scan.nextLine());
 
         // Calculate result
