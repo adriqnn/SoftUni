251c646967e440bead5767b080c8861b5f4c9299
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-06 23:39:12 +0200

    03

--- 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_03_RecursiveFibonacci.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_03_RecursiveFibonacci.java
@@ -8,36 +8,36 @@ public class _03_RecursiveFibonacci {
 
         // Read input value
         int num = Integer.parseInt(scan.nextLine());
-        
+
         // Method for printing fibonacci
         printRecursiveFibonacci(num);
-        
+
         scan.close();
     }
 
     private static void printRecursiveFibonacci(int num) {
         // 0
-        if (num == 0){
+        if (num == 0) {
             System.out.println("0");
             return;
         }
-        
+
         // 1
-        if (num == 1){
+        if (num == 1) {
             System.out.println("1");
             return;
         }
-        
+
         long number1InSequence = 1;
         long number2InSequence = 1;
-        
+
         // fibonacci
         for (int i = 2; i < num; i++) {
             long temporary = number2InSequence;
             number2InSequence = number1InSequence + number2InSequence;
             number1InSequence = temporary;
         }
-        
+
         System.out.println(number2InSequence);
     }
 }
