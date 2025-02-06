035c1640bc40166b7064a27527068780761a8f04
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-06 23:36:31 +0200

    02

--- 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle.java
@@ -12,9 +12,9 @@ public class _02_PascalTriangle {
         // Print pascal triangle
         for (int i = 0; i < n; i++) {
             int number = 1;
+            
             for (int j = 0; j <= i; j++) {
                 System.out.print(number + " ");
-                
                 number = number * (i - j) / (j + 1);
             }
 
