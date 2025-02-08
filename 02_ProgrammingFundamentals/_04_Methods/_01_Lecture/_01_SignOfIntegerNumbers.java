147a32fab9c8d10a8695dca4b2858aab7e1e443e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-08 23:41:11 +0200

    01

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_01_SignOfIntegerNumbers.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_01_SignOfIntegerNumbers.java
@@ -11,16 +11,16 @@ public class _01_SignOfIntegerNumbers {
 
         // Create method to determine the char sign
         whatIsTheSign(num);
-        
+
         scan.close();
     }
 
     private static void whatIsTheSign(int num) {
-        if (num == 0){
+        if (num == 0) {
             System.out.println("The number 0 is zero.");
-        }else if (num > 0){
+        } else if (num > 0) {
             System.out.printf("The number %d is positive.", num);
-        }else{
+        } else {
             System.out.printf("The number %d is negative.", num);
         }
     }
