3606e163b03b1f84a14e0e50e08bc39382717c6c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-08 23:42:04 +0200

    02

--- 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_02_Grades.java
+++ 02_ProgrammingFundamentals/_04_Methods/_01_Lecture/_02_Grades.java
@@ -8,23 +8,23 @@ public class _02_Grades {
 
         // Read input value
         double grade = Double.parseDouble(scan.nextLine());
-        
+
         // Create method to qualify grades
         whatIsTheGrade(grade);
-        
+
         scan.close();
     }
 
     private static void whatIsTheGrade(double grade) {
-        if (grade >= 2.00 && grade < 3.00){
+        if (grade >= 2.00 && grade < 3.00) {
             System.out.println("Fail");
-        }else if (grade < 3.50){
+        } else if (grade < 3.50) {
             System.out.println("Poor");
-        }else if (grade < 4.50){
+        } else if (grade < 4.50) {
             System.out.println("Good");
-        }else if (grade < 5.50){
+        } else if (grade < 5.50) {
             System.out.println("Very good");
-        }else if (grade <6.00){
+        } else if (grade < 6.00) {
             System.out.println("Excellent");
         }
     }
