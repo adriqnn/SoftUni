6d043256fafaecb373332cf0a619aa3b603c6573
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-09 00:16:28 +0200

    05

--- 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_05_MultiplicationSign.java
+++ 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_05_MultiplicationSign.java
@@ -12,34 +12,33 @@ public class _05_MultiplicationSign {
         int num3 = Integer.parseInt(scan.nextLine());
 
         // Create method that determines math result
-        checkMath(num1,num2,num3);
-        
+        checkMath(num1, num2, num3);
+
         scan.close();
     }
 
     private static void checkMath(int num1, int num2, int num3) {
-        if(num1 == 0 || num2 == 0 || num3 == 0){
+        if (num1 == 0 || num2 == 0 || num3 == 0) {
             System.out.println("zero");
-            
             return;
         }
-        
+
         int count = 0;
-        if (num1 < 0){
+        if (num1 < 0) {
             count++;
         }
-        
-        if (num2 < 0){
+
+        if (num2 < 0) {
             count++;
         }
-        
-        if (num3 < 0){
+
+        if (num3 < 0) {
             count++;
         }
-        
-        if (count == 1 || count ==3){
+
+        if (count == 1 || count == 3) {
             System.out.println("negative");
-        }else{
+        } else {
             System.out.println("positive");
         }
     }
