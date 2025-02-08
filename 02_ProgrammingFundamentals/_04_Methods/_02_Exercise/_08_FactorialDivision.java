c7be556a54a38d110f3d45ab2b80e9221444e540
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-09 00:01:25 +0200

    08

--- 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_08_FactorialDivision.java
+++ 02_ProgrammingFundamentals/_04_Methods/_02_Exercise/_08_FactorialDivision.java
@@ -9,20 +9,20 @@ public class _08_FactorialDivision {
         // Read input values
         int n1 = Integer.parseInt(scan.nextLine());
         int n2 = Integer.parseInt(scan.nextLine());
-        
+
         // Print result from the factorial division method
         System.out.printf("%.2f", (1.00 * factorialDivision(n1)) / factorialDivision(n2));
-        
+
         scan.close();
     }
 
     private static long factorialDivision(int n) {
         long factorial = 1;
-        
+
         for (int i = 2; i <= n; i++) {
             factorial = factorial * i;
         }
-        
+
         return factorial;
     }
 }
