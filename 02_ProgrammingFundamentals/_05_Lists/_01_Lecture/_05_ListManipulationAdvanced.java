dd61dc4a5310465f555e91b49485916a9eb961b0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-09 23:40:13 +0200

    05

--- 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_05_ListManipulationAdvanced.java
+++ 02_ProgrammingFundamentals/_05_Lists/_01_Lecture/_05_ListManipulationAdvanced.java
@@ -14,85 +14,86 @@ public class _05_ListManipulationAdvanced {
         String console = scan.nextLine();
 
         // Manipulate the list based on the given commands
-        while(!console.equals("end")){
+        while (!console.equals("end")) {
             String[] commandLine = console.split("\\s+");
-            
-            if(commandLine[0].equals("Contains")){
-                if(numbersList.contains(Integer.parseInt(commandLine[1]))){
+
+            if (commandLine[0].equals("Contains")) {
+                if (numbersList.contains(Integer.parseInt(commandLine[1]))) {
                     System.out.println("Yes");
-                }else{
+                } else {
                     System.out.println("No such number");
                 }
-            }else if(commandLine[0].equals("Print")){
+            } else if (commandLine[0].equals("Print")) {
                 boolean isChanged = false;
-                
-                if(commandLine[1].equals("even")){
+
+                if (commandLine[1].equals("even")) {
                     for (int i = 0; i < numbersList.size(); i++) {
-                        if(numbersList.get(i) % 2 == 0){
+                        if (numbersList.get(i) % 2 == 0) {
                             isChanged = true;
                         }
                     }
-                    if(isChanged) {
+                    
+                    if (isChanged) {
                         numbersList.stream().forEach(e -> {
                             if (e % 2 == 0) {
                                 System.out.print(e + " ");
                             }
                         });
-                        
+
                         System.out.println();
                     }
-                }else if(commandLine[1].equals("odd")){
+                } else if (commandLine[1].equals("odd")) {
                     for (int i = 0; i < numbersList.size(); i++) {
-                        if(numbersList.get(i) % 2 != 0){
+                        if (numbersList.get(i) % 2 != 0) {
                             isChanged = true;
                         }
                     }
-                    
-                    if(isChanged) {
+
+                    if (isChanged) {
                         numbersList.stream().forEach(e -> {
                             if (e % 2 != 0) {
                                 System.out.print(e + " ");
                             }
                         });
-                        
+
                         System.out.println();
                     }
                 }
-            }else if(commandLine[0].equals("Get")){
+            } else if (commandLine[0].equals("Get")) {
                 int sum = 0;
-                
+
                 for (int i = 0; i < numbersList.size(); i++) {
                     sum += numbersList.get(i);
                 }
-                
+
                 System.out.println(sum);
-            }else if(commandLine[0].equals("Filter")){
-                numbersList.stream().forEach(e->{
-                    if(commandLine[1].equals("<")){
-                        if(e < Integer.parseInt(commandLine[2])){
+            } else if (commandLine[0].equals("Filter")) {
+                numbersList.stream().forEach(e -> {
+                    if (commandLine[1].equals("<")) {
+                        if (e < Integer.parseInt(commandLine[2])) {
                             System.out.print(e + " ");
                         }
-                    }else if(commandLine[1].equals(">")){
-                        if(e > Integer.parseInt(commandLine[2])){
+                    } else if (commandLine[1].equals(">")) {
+                        if (e > Integer.parseInt(commandLine[2])) {
                             System.out.print(e + " ");
                         }
-                    }else if(commandLine[1].equals(">=")){
-                        if(e >= Integer.parseInt(commandLine[2])){
+                    } else if (commandLine[1].equals(">=")) {
+                        if (e >= Integer.parseInt(commandLine[2])) {
                             System.out.print(e + " ");
                         }
-                    }else if(commandLine[1].equals("<=")){
-                        if(e <= Integer.parseInt(commandLine[2])){
+                    } else if (commandLine[1].equals("<=")) {
+                        if (e <= Integer.parseInt(commandLine[2])) {
                             System.out.print(e + " ");
                         }
                     }
                 });
-                
+
                 System.out.println();
             }
-            
+
             console = scan.nextLine();
         }
-        
+
         scan.close();
     }
 }
