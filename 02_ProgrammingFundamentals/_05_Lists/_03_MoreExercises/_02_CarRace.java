40d4ed5d35fc5fcdadb63911e027e60fbeff3b71
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-09 23:59:26 +0200

    02

--- 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_02_CarRace.java
+++ 02_ProgrammingFundamentals/_05_Lists/_03_MoreExercises/_02_CarRace.java
@@ -17,32 +17,32 @@ public class _02_CarRace {
         double timeSecondRacer = 0;
 
         // Determine the time of the first racer
-        for (int i = 0; i < numbersList.size()/2; i++) {
-            if(numbersList.get(i) == 0){
+        for (int i = 0; i < numbersList.size() / 2; i++) {
+            if (numbersList.get(i) == 0) {
                 timeFirstRacer *= 0.8;
                 continue;
             }
-            
+
             timeFirstRacer += numbersList.get(i);
         }
 
         // Determine the time of the second racer
-        for (int i = numbersList.size()-1; i > numbersList.size()/2; i--) {
-            if(numbersList.get(i) == 0){
+        for (int i = numbersList.size() - 1; i > numbersList.size() / 2; i--) {
+            if (numbersList.get(i) == 0) {
                 timeSecondRacer *= 0.8;
                 continue;
             }
-            
+
             timeSecondRacer += numbersList.get(i);
         }
-        
+
         // Print result
-        if(timeFirstRacer < timeSecondRacer){
+        if (timeFirstRacer < timeSecondRacer) {
             System.out.printf("The winner is left with total time: %.1f%n", timeFirstRacer);
-        }else if(timeFirstRacer > timeSecondRacer){
+        } else if (timeFirstRacer > timeSecondRacer) {
             System.out.printf("The winner is right with total time: %.1f%n", timeSecondRacer);
         }
-        
+
         scan.close();
     }
 }
