9081da65ed078bb86ae0dceb7f3897e308cccdd6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-09 23:49:27 +0200

    04

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_04_ListOperations.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_04_ListOperations.java
@@ -14,45 +14,45 @@ public class _04_ListOperations {
         String console = scan.nextLine();
 
         // Transform the list based on the given commands
-        while(!console.equals("End")){
+        while (!console.equals("End")) {
             String[] commandLine = console.split("\\s+");
-            
-            if(commandLine[0].equals("Add")){
+
+            if (commandLine[0].equals("Add")) {
                 numbersList.add(Integer.parseInt(commandLine[1]));
-            }else if(commandLine[0].equals("Insert")){
-                if(Integer.parseInt(commandLine[2]) >= 0 && Integer.parseInt(commandLine[2]) < numbersList.size()){
+            } else if (commandLine[0].equals("Insert")) {
+                if (Integer.parseInt(commandLine[2]) >= 0 && Integer.parseInt(commandLine[2]) < numbersList.size()) {
                     numbersList.add(Integer.parseInt(commandLine[2]), Integer.parseInt(commandLine[1]));
-                }else{
+                } else {
                     System.out.println("Invalid index");
                 }
-            }else if(commandLine[0].equals("Remove")){
-                if(Integer.parseInt(commandLine[1]) >= 0 && Integer.parseInt(commandLine[1]) < numbersList.size()) {
+            } else if (commandLine[0].equals("Remove")) {
+                if (Integer.parseInt(commandLine[1]) >= 0 && Integer.parseInt(commandLine[1]) < numbersList.size()) {
                     numbersList.remove(Integer.parseInt(commandLine[1]));
-                }else{
+                } else {
                     System.out.println("Invalid index");
                 }
-            }else if(commandLine[0].equals("Shift")){
+            } else if (commandLine[0].equals("Shift")) {
                 int n = Integer.parseInt(commandLine[2]);
-                
-                if(commandLine[1].equals("left")){
+
+                if (commandLine[1].equals("left")) {
                     for (int i = 0; i < n; i++) {
                         numbersList.add(numbersList.get(0));
                         numbersList.remove(0);
                     }
-                }else if(commandLine[1].equals("right")){
+                } else if (commandLine[1].equals("right")) {
                     for (int i = 0; i < n; i++) {
-                        numbersList.add(0,numbersList.get(numbersList.size()-1));
-                        numbersList.remove(numbersList.size()-1);
+                        numbersList.add(0,numbersList.get(numbersList.size() - 1));
+                        numbersList.remove(numbersList.size() - 1);
                     }
                 }
             }
-            
+
             console = scan.nextLine();
         }
-        
+
         //Print result
         numbersList.stream().forEach(e -> System.out.print(e + " "));
-        
+
         scan.close();
     }
 }
