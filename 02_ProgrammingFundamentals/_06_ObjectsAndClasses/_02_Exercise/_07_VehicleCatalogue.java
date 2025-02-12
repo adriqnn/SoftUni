4c4fea49d5f617739d867c59f05c5869d875e56a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-12 23:51:14 +0200

    07

--- 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_07_VehicleCatalogue.java
+++ 02_ProgrammingFundamentals/_06_ObjectsAndClasses/_02_Exercise/_07_VehicleCatalogue.java
@@ -19,50 +19,51 @@ public class _07_VehicleCatalogue {
         List<Vehicle> vehicles = new ArrayList<>();
 
         // Create a DB of all vehicles
-        while(!console.equals("End")){
+        while (!console.equals("End")) {
             String[] consoleLine = console.split("\\s+");
-            
+
             String type = consoleLine[0];
             String model = consoleLine[1];
             String color = consoleLine[2];
             int horsePower = Integer.parseInt(consoleLine[3]);
-            
-            if(type.equals("car")){
+
+            if (type.equals("car")) {
                 carHorsePower += horsePower;
                 cars++;
-            }else if(type.equals("truck")){
+            } else if (type.equals("truck")) {
                 truckHorsePower += horsePower;
                 trucks++;
             }
-            
+
             Vehicle catalogueEntry = new Vehicle(type, model, color, horsePower);
             vehicles.add(catalogueEntry);
-            
+
             console = scan.nextLine();
         }
 
         String models = scan.nextLine();
 
         // Print result
-        while(!models.equals("Close the Catalogue")){
+        while (!models.equals("Close the Catalogue")) {
             for (Vehicle element : vehicles) {
-                if(element.getModel().equals(models)){
+                if (element.getModel().equals(models)) {
                     System.out.print(element);
                 }
             }
+
             models = scan.nextLine();
         }
 
         // Print result
-        if(cars == 0){
+        if (cars == 0) {
             System.out.println("Cars have average horsepower of: 0.00.");
-        }else {
+        } else {
             System.out.printf("Cars have average horsepower of: %.2f.%n", carHorsePower / cars);
         }
 
-        if(trucks == 0){
+        if (trucks == 0) {
             System.out.println("Trucks have average horsepower of: 0.00.");
-        }else {
+        } else {
             System.out.printf("Trucks have average horsepower of: %.2f.%n", truckHorsePower / trucks);
         }
 
@@ -89,7 +90,7 @@ class Vehicle {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String format = this.type.substring(0,1).toUpperCase() + this.type.substring(1);
         //return String.format
         return "Type: " + format + "\n" + "Model: " + this.model + "\n" + "Color: " + this.color + "\n" + "Horsepower: " + this.horsePower + "\n";
