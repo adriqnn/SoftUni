1cd14ce268ac7f8e90ae2ba8ae33073a98fbc7c8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:51:46 +0200

    05

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_05_ConcatNames/_05_ConcatNames.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_05_ConcatNames/_05_ConcatNames.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_ConcatNames
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string first = Console.ReadLine(), second = Console.ReadLine(), third = Console.ReadLine();
+
+        // Print result
+        Console.WriteLine(first + third + second);
+    }
+}
