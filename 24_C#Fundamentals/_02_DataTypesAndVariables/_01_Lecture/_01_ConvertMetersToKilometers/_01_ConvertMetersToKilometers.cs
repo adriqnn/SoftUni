0122d7e6c0c08b8c87f9b8cedf0af983fbfecdd2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:25:29 +0200

    01

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_01_ConvertMetersToKilometers/_01_ConvertMetersToKilometers.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_01_ConvertMetersToKilometers/_01_ConvertMetersToKilometers.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_ConvertMetersToKilometers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double meters = double.Parse(Console.ReadLine());
+
+        // Add variable
+        double transformMetersToKilometers = meters / 1000;
+
+        // Print result
+        Console.WriteLine($"{transformMetersToKilometers:F2}");
+    }
+}
