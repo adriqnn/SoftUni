dcad0c955fafd20455eff995ac30c72261787f6d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:28:09 +0200

    02

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_02_PoundsToDollars/_02_PoundsToDollars.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_02_PoundsToDollars/_02_PoundsToDollars.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_PoundsToDollars
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double inputPounds = double.Parse(Console.ReadLine());
+
+        // Add variable
+        double poundsToDollars = inputPounds * 1.31;
+
+        // Print result
+        Console.WriteLine($"{poundsToDollars:F3}", poundsToDollars);
+    }
+}
