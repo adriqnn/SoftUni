3ad3833263f1bebdd6fdace175022234f74e8d10
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:53:10 +0200

    06

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString/_06_CharsToString.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString/_06_CharsToString.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _06_CharsToString
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        String one = Console.ReadLine(), two = Console.ReadLine(), three = Console.ReadLine();
+
+        // Print result
+        Console.WriteLine(one + two + three);
+    }
+}
