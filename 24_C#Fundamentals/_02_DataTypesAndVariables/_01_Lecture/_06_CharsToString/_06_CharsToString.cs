2bcd0bac4fd4a34c86c179cc65e6777299494f28
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:54:23 +0200

    06

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString/_06_CharsToString.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_06_CharsToString/_06_CharsToString.cs
@@ -5,7 +5,7 @@ public class _06_CharsToString
     public static void Main(string[] args)
     {
         // Read input values
-        String one = Console.ReadLine(), two = Console.ReadLine(), three = Console.ReadLine();
+        string one = Console.ReadLine(), two = Console.ReadLine(), three = Console.ReadLine();
 
         // Print result
         Console.WriteLine(one + two + three);
