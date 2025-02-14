02d43633825aa598f009a90cc4382089be38d5e4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:59:32 +0200

    09

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_09_CenturiesToMinutes/_09_CenturiesToMinutes.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_09_CenturiesToMinutes/_09_CenturiesToMinutes.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_CenturiesToMinutes
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int centuries = int.Parse(Console.ReadLine());
+
+        // Add variables
+        double years = centuries * 100;
+        double days = years * 365.2422;
+        double hours = days * 24;
+        double minutes = hours * 60;
+
+        // Print result
+        Console.WriteLine($"{centuries} centuries = {years:F0} years = {days:F0} days = {hours:F0} hours = {minutes:F0} minutes");
+    }
+}
