76a8e70d77f0c5259fa3991ae7c25175d166d745
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-14 23:56:24 +0200

    07

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_07_ReversedChars/_07_ReversedChars.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_07_ReversedChars/_07_ReversedChars.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _07_ReversedChars
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string one = Console.ReadLine(), two = Console.ReadLine(), three = Console.ReadLine();
+
+        // Print result
+        Console.WriteLine(three + " " + two + " " + one);
+    }
+}
