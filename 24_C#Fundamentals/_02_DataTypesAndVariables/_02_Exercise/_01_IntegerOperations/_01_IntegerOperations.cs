9ce71760bace3e8c57bc40bc95a9d43e08933679
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-15 23:47:51 +0200

    01

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_01_IntegerOperations/_01_IntegerOperations.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_01_IntegerOperations/_01_IntegerOperations.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n1 = int.Parse(Console.ReadLine()), n2 = int.Parse(Console.ReadLine()), n3 = int.Parse(Console.ReadLine()), n4 = int.Parse(Console.ReadLine());
+
+        // Calculate result
+        int result = ((n1 + n2) / n3) * n4;
+
+        // Print result
+        Console.WriteLine(result);
+    }
+}
