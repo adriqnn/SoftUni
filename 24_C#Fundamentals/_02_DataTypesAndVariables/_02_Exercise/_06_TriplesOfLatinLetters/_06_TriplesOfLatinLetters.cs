7c9f674dc8443ef432dc112e54b440c3657fbf94
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 00:00:57 +0200

    06

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_TriplesOfLatinLetters/_06_TriplesOfLatinLetters.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_06_TriplesOfLatinLetters/_06_TriplesOfLatinLetters.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_TriplesOfLatinLetters
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print latin letters
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    char first = (char)('a' + i);
+                    char second = (char)('a' + j);
+                    char third = (char)('a' + k);
+
+                    Console.WriteLine($"{first}{second}{third}");
+                }
+            }
+        }
+    }
+}
