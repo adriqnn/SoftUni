107a66b5439ca5e6ea25d41958eea675c8193f94
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-15 23:57:23 +0200

    04

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_04_SumOfChars/_04_SumOfChars.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_04_SumOfChars/_04_SumOfChars.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int sum = 0;
+
+        // Sum the chars
+        for (int i = 0; i < n; i++)
+        {
+            char symbol = Console.ReadLine()[0];
+            sum += symbol;
+        }
+
+        // Print result
+        Console.WriteLine("The sum equals: " + sum);
+    }
+}
