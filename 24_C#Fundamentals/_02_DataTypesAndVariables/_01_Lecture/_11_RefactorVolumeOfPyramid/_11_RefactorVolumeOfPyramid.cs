9847a2e944484c78983202d47f9411f478371292
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-15 23:28:48 +0200

    11

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_11_RefactorVolumeOfPyramid/_11_RefactorVolumeOfPyramid.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_01_Lecture/_11_RefactorVolumeOfPyramid/_11_RefactorVolumeOfPyramid.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _11_RefactorVolumeOfPyramid
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        double length = double.Parse(Console.ReadLine()), width = double.Parse(Console.ReadLine()), height = double.Parse(Console.ReadLine());
+
+        // Print result
+        Console.Write("Length: ");
+        Console.Write("Width: ");
+        Console.Write("Height: ");
+        Console.WriteLine($"Pyramid Volume: {(length * width * height) / 3:F2}");
+    }
+}
