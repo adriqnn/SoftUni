8266b9cbee0a5eae02d49a28111629ea5459ab99
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 00:03:33 +0200

    07

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_07_WaterOverflow/_07_WaterOverflow.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_07_WaterOverflow/_07_WaterOverflow.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_WaterOverflow
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int capacity = 0;
+
+        // Determine the capacity
+        for (int i = 0; i < n; i++)
+        {
+            int liters = int.Parse(Console.ReadLine());
+
+            if (capacity + liters > 255)
+            {
+                Console.WriteLine("Insufficient capacity!");
+                continue;
+            }
+
+            capacity += liters;
+        }
+
+        // Print result
+        Console.WriteLine(capacity);
+    }
+}
