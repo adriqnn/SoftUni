d2cc01f6a74552fe8551299bcb3c37905f4551e7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 00:07:18 +0200

    08

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_BeerKegs/_08_BeerKegs.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_08_BeerKegs/_08_BeerKegs.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _08_BeerKegs
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variables
+        double volumeOfTheBiggestKeg = 0;
+        string modelOfTheBiggestKeg = "";
+
+        // Determine the volume
+        for (int i = 0; i < n; i++)
+        {
+            string model = Console.ReadLine();
+            double radius = double.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double volume = Math.PI * radius * radius * height;
+
+            if (volume > volumeOfTheBiggestKeg)
+            {
+                volumeOfTheBiggestKeg = volume;
+                modelOfTheBiggestKeg = model;
+            }
+        }
+
+        // Print result
+        Console.WriteLine(modelOfTheBiggestKeg);
+    }
+}
