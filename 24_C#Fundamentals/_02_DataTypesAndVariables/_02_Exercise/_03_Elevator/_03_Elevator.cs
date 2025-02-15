b73f19841bf35b9e9ba50a194574e42c29bb6fac
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-15 23:56:07 +0200

    03

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_03_Elevator/_03_Elevator.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_03_Elevator/_03_Elevator.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Elevator
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int numberOfPeople = int.Parse(Console.ReadLine());
+        int elevatorCapacity = int.Parse(Console.ReadLine());
+
+        // Print result
+        Console.WriteLine($"{Math.Ceiling(numberOfPeople * 1.00 / elevatorCapacity):F0}");
+    }
+}
