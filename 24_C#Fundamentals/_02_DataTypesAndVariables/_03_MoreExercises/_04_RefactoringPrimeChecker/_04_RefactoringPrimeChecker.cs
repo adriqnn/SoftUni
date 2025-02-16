79759c1afff248ea8f25cc88238adb792bf1ed42
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:42:15 +0200

    04

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_04_RefactoringPrimeChecker/_04_RefactoringPrimeChecker.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_04_RefactoringPrimeChecker/_04_RefactoringPrimeChecker.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_RefactoringPrimeChecker
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Check if the number is prime or not
+        for (int i = 2; i <= n; i++)
+        {
+            bool isPrime = true;
+
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            // Print result
+            Console.WriteLine($"{i} -> {isPrime.ToString().ToLower()}");
+        }
+    }
+}
