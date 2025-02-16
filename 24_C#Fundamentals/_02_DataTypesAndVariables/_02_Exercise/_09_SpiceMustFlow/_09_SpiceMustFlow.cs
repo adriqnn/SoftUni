1da010b9c4d15f4eeb6c0f1951dc48ad7b0e8188
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 22:41:49 +0200

    09

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_09_SpiceMustFlow/_09_SpiceMustFlow.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_09_SpiceMustFlow/_09_SpiceMustFlow.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _09_SpiceMustFlow
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int startingYieldOfSource = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int yield = 0;
+        int days = 0;
+
+        // Calculate the yield of the source
+        while (startingYieldOfSource >= 100)
+        {
+            yield += startingYieldOfSource;
+            startingYieldOfSource -= 10;
+
+            days++;
+            yield -= 26;
+        }
+
+        // Yield check for additional days
+        if (yield >= 26)
+        {
+            yield -= 26;
+        }
+
+        // Print result
+        Console.WriteLine(days);
+        Console.WriteLine(yield);
+    }
+}
