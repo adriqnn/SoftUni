1d019e01fd9d717243d194df1d44c14c47d26661
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 22:45:26 +0200

    10

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_PokeMon/_10_PokeMon.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_02_Exercise/_10_PokeMon/_10_PokeMon.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_PokeMon
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int pokemonPowerN = int.Parse(Console.ReadLine());
+        int distanceM = int.Parse(Console.ReadLine());
+        int exhaustionFactorY = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int originalValue = pokemonPowerN / 2;
+        int successfulPokes = 0;
+
+        // Check if pokemon can make the move
+        while (pokemonPowerN >= distanceM)
+        {
+            successfulPokes++;
+            pokemonPowerN -= distanceM;
+
+            if (pokemonPowerN == originalValue && exhaustionFactorY != 0)
+            {
+                pokemonPowerN /= exhaustionFactorY;
+            }
+        }
+
+        // Print result
+        Console.WriteLine(pokemonPowerN);
+        Console.WriteLine(successfulPokes);
+    }
+}
