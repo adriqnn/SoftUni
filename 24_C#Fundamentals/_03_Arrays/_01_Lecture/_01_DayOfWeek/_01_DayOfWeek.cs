c57a8fc42231bf427f56a51f5c49a12d2e0719d2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-17 00:46:46 +0200

    01

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_01_DayOfWeek/_01_DayOfWeek.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_01_DayOfWeek/_01_DayOfWeek.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_DayOfWeek
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        // Print result
+        try
+        {
+            Console.WriteLine(days[n - 1]);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Invalid day!");
+        }
+    }
+}
