5338b4eb5022faa652a4c5d27083979cf4a80320
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:44:14 +0200

    05

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_05_DecryptingMessage/_05_DecryptingMessage.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_05_DecryptingMessage/_05_DecryptingMessage.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _05_DecryptingMessage
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int keyToDecrypt = int.Parse(Console.ReadLine());
+        int numberOfLines = int.Parse(Console.ReadLine());
+
+        // Add variable
+        StringBuilder message = new StringBuilder();
+
+        // Decrypt the message
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            char symbol = Console.ReadLine()[0];
+            symbol = (char)(symbol + keyToDecrypt);
+
+            message.Append(symbol);
+        }
+
+        // Print result
+        Console.WriteLine(message);
+    }
+}
