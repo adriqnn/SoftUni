d44d91b6faed2af52bca48769e88be921299a3c9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-17 01:07:24 +0200

    04

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_04_ReverseArrayOfStrings/_04_ReverseArrayOfStrings.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_04_ReverseArrayOfStrings/_04_ReverseArrayOfStrings.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_ReverseArrayOfStrings
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] arr = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Reverse the array
+        for (int i = 0; i < arr.Length / 2; i++)
+        {
+            string first = arr[i];
+            string last = arr[arr.Length - i - 1];
+            arr[i] = last;
+            arr[arr.Length - 1 - i] = first;
+        }
+
+        // Print result
+        foreach (string e in arr)
+        {
+            Console.Write(e + " ");
+        }
+    }
+}
