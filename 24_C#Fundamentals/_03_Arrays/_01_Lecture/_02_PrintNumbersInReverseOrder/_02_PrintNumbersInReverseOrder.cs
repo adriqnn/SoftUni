162b0ec484fe5fa034b23394a4c08208ef6adf28
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-17 00:50:18 +0200

    02

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_02_PrintNumbersInReverseOrder/_02_PrintNumbersInReverseOrder.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_02_PrintNumbersInReverseOrder/_02_PrintNumbersInReverseOrder.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int[] numbers = new int[n];
+
+        // Reverse the array
+        for (int i = n - 1; i >= 0; i--)
+        {
+            int number = int.Parse(Console.ReadLine());
+            numbers[i] = number;
+        }
+
+        // Print result
+        foreach (int num in numbers)
+        {
+            Console.Write(num + " ");
+        }
+    }
+}
