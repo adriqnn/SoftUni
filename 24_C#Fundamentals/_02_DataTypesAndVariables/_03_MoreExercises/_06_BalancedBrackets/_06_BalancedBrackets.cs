448b90ca401b132cb73322afc5f50ce74064dd44
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:47:16 +0200

    06

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_06_BalancedBrackets/_06_BalancedBrackets.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_06_BalancedBrackets/_06_BalancedBrackets.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_BalancedBrackets
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int numberOfLines = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int counterOpeningAndClosing = 0;
+        int counter = 0;
+        int stack = 0;
+
+        // Determine the count of the brackets
+        for (int i = 0; i < numberOfLines; i++)
+        {
+            string console = Console.ReadLine();
+
+            if (console.Length == 1 && console[0] == '(')
+            {
+                counterOpeningAndClosing++;
+
+                if (counter > 0)
+                {
+                    counter = 0;
+                }
+
+                counter++;
+            }
+            else if (console.Length == 1 && console[0] == ')')
+            {
+                counterOpeningAndClosing++;
+
+                if (counter > 0)
+                {
+                    counter = 0;
+                    stack++;
+                }
+            }
+        }
+
+        // Print result
+        if ((1.00 * counterOpeningAndClosing) / 2 != stack)
+        {
+            Console.WriteLine("UNBALANCED");
+        }
+        else
+        {
+            Console.WriteLine("BALANCED");
+        }
+    }
+}
