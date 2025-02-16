c8a53688521d6a532edcba4cbf8ca62279198f6b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-17 01:10:48 +0200

    05

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_05_EvenAndOddSubtraction/_05_EvenAndOddSubtraction.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_05_EvenAndOddSubtraction/_05_EvenAndOddSubtraction.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_EvenAndOddSubtraction
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbers = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+        // Add variables
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        // Calculate sums for even and odd
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                sumEven += numbers[i];
+            }
+            else
+            {
+                sumOdd += numbers[i];
+            }
+        }
+
+        // Print result
+        Console.WriteLine(sumEven - sumOdd);
+    }
+}
