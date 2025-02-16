ae62b5597e17848dae57c6195998ccfa2b21a44e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:38:54 +0200

    03

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_03_FloatingEquality/_03_FloatingEquality.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_03_FloatingEquality/_03_FloatingEquality.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_FloatingEquality
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        double n1 = double.Parse(Console.ReadLine());
+        double n2 = double.Parse(Console.ReadLine());
+
+        // Add comparator value
+        double epsilon = 0.000001;
+
+        // Print result
+        Console.WriteLine(Math.Abs(n1 - n2) <= epsilon ? "True" : "False");
+    }
+}
