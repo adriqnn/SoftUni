2914102dcb685727533d07ff8a1f4cdae744b059
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:36:25 +0200

    02

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_02_FromLeftToTheRight/_02_FromLeftToTheRight.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_02_FromLeftToTheRight/_02_FromLeftToTheRight.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_FromLeftToTheRight
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Examine left and right numbers
+        for (int i = 0; i < n; i++)
+        {
+            long[] numbersArray = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(long.Parse).ToArray();
+
+            // Add variables
+            long firstNumber = numbersArray[0];
+            long secondNumber = numbersArray[1];
+            long sum = 0;
+
+            // Compare numbers
+            if (firstNumber >= secondNumber)
+            {
+                firstNumber = Math.Abs(firstNumber);
+
+                while (firstNumber > 0)
+                {
+                    sum += firstNumber % 10;
+                    firstNumber = firstNumber / 10;
+                }
+            }
+            else
+            {
+                secondNumber = Math.Abs(secondNumber);
+
+                while (secondNumber > 0)
+                {
+                    sum += secondNumber % 10;
+                    secondNumber = secondNumber / 10;
+                }
+            }
+
+            // Print result
+            Console.WriteLine(sum);
+        }
+    }
+}
