68791e6ed6c1fcdeeff50379e1e7ac02cbb5980a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-17 00:58:45 +0200

    03

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_03_SumEvenNumbers/_03_SumEvenNumbers.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_03_SumEvenNumbers/_03_SumEvenNumbers.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbers = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+        // Add variable
+        int sum = 0;
+
+        // Calculate sum
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                sum += numbers[i];
+            }
+        }
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
