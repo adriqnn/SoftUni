c021f66d2ebaf5f37758ce52d360d3a963857095
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-16 23:26:33 +0200

    01

--- 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_01_DataTypeFinder/_01_DataTypeFinder.cs
+++ 24_C#Fundamentals/_02_DataTypesAndVariables/_03_MoreExercises/_01_DataTypeFinder/_01_DataTypeFinder.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _01_DataTypeFinder
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variables for regex
+        string regexInteger = "^-?[0-9]+$";
+        Regex patternInteger = new Regex(regexInteger);
+        string regexFloat = "^-?[0-9]+\\.[0-9]+$";
+        Regex patternFloat = new Regex(regexFloat);
+
+        // Determine what is the input
+        while (!input.Equals("END", StringComparison.OrdinalIgnoreCase))
+        {
+            // Check for boolean type
+            if (input.Equals("true", StringComparison.OrdinalIgnoreCase) || input.Equals("false", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"{input} is boolean type");
+            }
+            // Check for character type (single char that is not a digit)
+            else if (input.Length == 1 && (input[0] < '0' || input[0] > '9'))
+            {
+                Console.WriteLine($"{input} is character type");
+            }
+            // Check for floating point type using regex
+            else if (patternFloat.IsMatch(input))
+            {
+                Console.WriteLine($"{input} is floating point type");
+            }
+            // Check for integer type using regex
+            else if (patternInteger.IsMatch(input))
+            {
+                Console.WriteLine($"{input} is integer type");
+            }
+            // Otherwise, it's a string type
+            else
+            {
+                Console.WriteLine($"{input} is string type");
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
