11846ddd809660e79a1bc2178aa1e86d5552a193
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 23:55:33 +0200

    04

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_04_ArrayRotation/_04_ArrayRotation.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_04_ArrayRotation/_04_ArrayRotation.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_ArrayRotation
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            int numberInTheFront = numbers[0];
+            int[] changingArray = new int[numbers.Length];
+
+            for (int j = 0; j < numbers.Length - 1; j++)
+            {
+                changingArray[j] = numbers[j + 1];
+            }
+
+            changingArray[changingArray.Length - 1] = numberInTheFront;
+            numbers = changingArray;
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+}
