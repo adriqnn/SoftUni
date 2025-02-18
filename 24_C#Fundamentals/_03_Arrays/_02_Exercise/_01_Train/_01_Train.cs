369c3e6c7d842457b53af6fca3290ee42374eece
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 23:41:28 +0200

    01

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_01_Train/_01_Train.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_01_Train/_01_Train.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_Train
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int countOfTrainWagons = int.Parse(Console.ReadLine());
+
+        // Add variables
+        int[] train = new int[countOfTrainWagons];
+        int sum = 0;
+
+        // Add passengers to the train and sum all of them
+        for (int i = 0; i < countOfTrainWagons; i++)
+        {
+            train[i] = int.Parse(Console.ReadLine());
+            sum += train[i];
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", train));
+        Console.WriteLine(sum);
+    }
+}
