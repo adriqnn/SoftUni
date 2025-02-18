9b5d8ffe38bcb88795171eb315080adaec33dc6b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 23:30:46 +0200

    08

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_08_RoundingNumbers/_08_RoundingNumbers.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_08_RoundingNumbers/_08_RoundingNumbers.cs
@@ -5,7 +5,7 @@ public class _08_RoundingNumbers
     public static void Main(string[] args)
     {
         // Read input values  string result = string.Join(", " , Console.ReadLine().Split().Select(double.Parse).Select(x => $"{x} => {(x % 1 == 0.5 ? x + 0.5 : Math.Ceiling(x * 2) / 2)}"));
-        string result = string.Join(Environment.NewLine, Console.ReadLine().Split().Select(double.Parse).ToArray().Select(x => $"{x} => {Math.Round(x, MidpointRounding.AwayFromZero)}"));
+        string result = string.Join(Environment.NewLine, Console.ReadLine().Split().Select(double.Parse).ToArray().Select(x => $"{x} => {(int) Math.Round(x, MidpointRounding.AwayFromZero)}"));
 
         // Print result
         Console.WriteLine(result);
