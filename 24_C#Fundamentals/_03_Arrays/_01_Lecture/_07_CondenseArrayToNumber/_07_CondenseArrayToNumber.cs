b4b8db0a231e3e6139869294957eb6c8f478b68b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 22:48:23 +0200

    07

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_07_CondenseArrayToNumber/_07_CondenseArrayToNumber.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_07_CondenseArrayToNumber/_07_CondenseArrayToNumber.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_CondenseArrayToNumber
+{
+    public static void Main(string[] args)
+    {
+        // Raed input values
+        int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Condense the array
+        while (numbers.Length > 1)
+        {
+            int[] condensed = new int[numbers.Length - 1];
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                condensed[i] = numbers[i] + numbers[i + 1];
+
+            }
+
+            numbers = condensed;
+        }
+
+        // Print result
+        Console.WriteLine(numbers[0]);
+    }
+}
