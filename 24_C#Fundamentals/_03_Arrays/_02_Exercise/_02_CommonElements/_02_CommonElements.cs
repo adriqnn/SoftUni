c416e96422d38411bbe4adfe505c890cbcb7e5bb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 23:47:25 +0200

    02

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_02_CommonElements/_02_CommonElements.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_02_CommonElements/_02_CommonElements.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_CommonElements
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] arrOne = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] arrTwo = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Print common elements in two arrays
+        for (int i = 0; i < arrOne.Length; i++)
+        {
+            for (int j = 0; j < arrTwo.Length; j++)
+            {
+
+                if (arrOne[i].Equals(arrTwo[j]))
+                {
+                   Console.Write(arrTwo[j] + " ");
+                }
+            }
+        }
+    }
+}
