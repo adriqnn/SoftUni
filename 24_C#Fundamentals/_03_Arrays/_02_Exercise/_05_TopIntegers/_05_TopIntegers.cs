8c4ae47834b7893e0cc5528f662f2e1e74920cfc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 23:59:19 +0200

    05

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_05_TopIntegers/_05_TopIntegers.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_05_TopIntegers/_05_TopIntegers.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_HelloSoftUni
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbersArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Add variable
+        List<int> topIntegers = new List<int>();
+
+        // Determine the top integers
+        for (int i = 0; i < numbersArray.Length; i++)
+        {
+            bool isBigger = true;
+
+            for (int j = i + 1; j < numbersArray.Length; j++)
+            {
+                if (numbersArray[i] <= numbersArray[j])
+                {
+                    isBigger = false;
+                    break;
+                }
+            }
+
+            if (isBigger)
+            {
+                topIntegers.Add(numbersArray[i]);
+            }
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", topIntegers);
+    }
+}
