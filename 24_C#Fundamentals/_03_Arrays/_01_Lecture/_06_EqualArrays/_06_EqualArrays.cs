73237e631a16dcd46ddf183fa33771e1c0fab15d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-18 22:35:45 +0200

    06

--- 24_C#Fundamentals/_03_Arrays/_01_Lecture/_06_EqualArrays/_06_EqualArrays.cs
+++ 24_C#Fundamentals/_03_Arrays/_01_Lecture/_06_EqualArrays/_06_EqualArrays.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_EqualArrays
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numberOneArray = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int[] numberTwoArray = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+        // Add variables
+        int sum = 0;
+        bool isEqual = true;
+
+        // Compare each element of the arrays
+        for (int i = 0; i < numberOneArray.Length; i++)
+        {
+            if (numberOneArray[i] == numberTwoArray[i])
+            {
+                sum += numberOneArray[i];
+            }
+            else
+            {
+                isEqual = false;
+                sum = i;
+                break;
+            }
+        }
+
+        // Print result
+        if (isEqual)
+        {
+            Console.WriteLine("Arrays are identical. Sum: " + sum);
+        }
+        else
+        {
+            Console.WriteLine("Arrays are not identical. Found difference at " + sum + " index");
+        }
+    }
+}
