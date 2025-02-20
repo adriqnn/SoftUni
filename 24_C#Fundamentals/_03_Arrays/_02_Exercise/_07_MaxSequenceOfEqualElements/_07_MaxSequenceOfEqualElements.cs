e500feb9bbe41fdab83b38ac102249eca08027f1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-20 23:33:08 +0200

    07

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_07_MaxSequenceOfEqualElements/_07_MaxSequenceOfEqualElements.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_07_MaxSequenceOfEqualElements/_07_MaxSequenceOfEqualElements.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_MaxSequenceOfEqualElements
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbersArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Add variables
+        int length = 1;
+        int maxLength = 0;
+        int startIndex = 0;
+        int bestStartIndex = 0;
+
+        // Determine the longest sequence of equal numbers
+        for (int i = 1; i < numbersArray.Length; i++)
+        {
+            if (numbersArray[i] == numbersArray[i - 1])
+            {
+                length++;
+            }
+            else
+            {
+                length = 1;
+                startIndex = i;
+            }
+
+            if (length > maxLength)
+            {
+                maxLength = length;
+                bestStartIndex = startIndex;
+            }
+        }
+
+        // Print result
+        for (int i = bestStartIndex; i < bestStartIndex + maxLength; i++)
+        {
+            Console.Write(numbersArray[i] + " ");
+        }
+    }
+}
