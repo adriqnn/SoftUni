e1869b1d1e4697c46bbed909532af3ab602d6b63
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 01:01:09 +0200

    03

--- 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_03_RecursiveFibonacci/_03_RecursiveFibonacci.cs
+++ 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_03_RecursiveFibonacci/_03_RecursiveFibonacci.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _03_RecursiveFibonacci
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Method for printing fibonacci
+        printRecursiveFibonacci(num);
+    }
+
+    private static void printRecursiveFibonacci(int num)
+    {
+        // 0
+        if (num == 0)
+        {
+            Console.WriteLine("0");
+            return;
+        }
+
+        // 1
+        if (num == 1)
+        {
+            Console.WriteLine("1");
+            return;
+        }
+
+        long number1InSequence = 1;
+        long number2InSequence = 1;
+
+        // fibonacci
+        for (int i = 2; i < num; i++)
+        {
+            long temporary = number2InSequence;
+            number2InSequence = number1InSequence + number2InSequence;
+            number1InSequence = temporary;
+        }
+
+        Console.WriteLine(number2InSequence);
+    }
+}
