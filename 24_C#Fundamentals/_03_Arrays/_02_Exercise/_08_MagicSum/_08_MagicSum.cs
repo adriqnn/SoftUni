7faeaae29e127a0dff4f6de4ebed6e735e6c7d0d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-20 23:37:47 +0200

    08

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_08_MagicSum/_08_MagicSum.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_08_MagicSum/_08_MagicSum.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_MagicSum
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbersArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int n = int.Parse(Console.ReadLine());
+
+        // Print each pair that the sum of equals the number n
+        for (int i = 0; i < numbersArray.Length; i++)
+        {
+            for (int j = i + 1; j < numbersArray.Length; j++)
+            {
+                if (numbersArray[i] + numbersArray[j] == n)
+                {
+                    Console.WriteLine(numbersArray[i] + " " + numbersArray[j]);
+                }
+            }
+        }
+    }
+}
