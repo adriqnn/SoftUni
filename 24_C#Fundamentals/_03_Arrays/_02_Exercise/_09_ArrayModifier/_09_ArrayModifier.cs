53588e74a8487f38c001c87cf470c4a58a23b0d7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-20 23:48:51 +0200

    09

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_09_ArrayModifier/_09_ArrayModifier.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_09_ArrayModifier/_09_ArrayModifier.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+public class _09_ArrayModifier
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        string input = Console.ReadLine();
+
+        // Accept commands and modify the array
+        while (!input.Equals("end"))
+        {
+            string[] commands = input.Split("\\s+");
+            string command = commands[0];
+
+            if (command.Equals("swap"))
+            {
+                int firstNumber = int.Parse(commands[1]);
+                int secondNumber = int.Parse(commands[2]);
+                int swap = array[firstNumber];
+
+                array[firstNumber] = array[secondNumber];
+                array[secondNumber] = swap;
+            }
+            else if (command.Equals("multiply"))
+            {
+                int firstNumber = int.Parse(commands[1]);
+                int secondNumber = int.Parse(commands[2]);
+
+                array[firstNumber] = array[firstNumber] * array[secondNumber];
+            }
+            else
+            {
+                array = array.Select(e => e - 1).ToArray();
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(", ", array));
+    }
+}
