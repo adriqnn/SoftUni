818830581bc1090f7be8a7881762248fcb20b6f3
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 00:28:10 +0200

    01

--- 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_01_EncryptSortAndPrintArray/_01_EncryptSortAndPrintArray.cs
+++ 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_01_EncryptSortAndPrintArray/_01_EncryptSortAndPrintArray.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_EncryptSortAndPrintArray
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Add variable
+        int[] listOfNamesValues = new int[num];
+
+        // Transform the strings into numeric values
+        for (int i = 0; i < num; i++)
+        {
+            String console = Console.ReadLine();
+
+            int sum = 0;
+
+            for (int j = 0; j < console.Length; j++)
+            {
+                char symbol = console[j];
+
+                if (symbol == 'a' || symbol == 'e' || symbol == 'u' || symbol == 'i' || symbol == 'o')
+                {
+                    sum += symbol * console.Length;
+                }
+                else if (symbol == 'A' || symbol == 'E' || symbol == 'U' || symbol == 'I' || symbol == 'O')
+                {
+                    sum += symbol * console.Length;
+                }
+                else
+                {
+                    sum += symbol / console.Length;
+                }
+            }
+
+            listOfNamesValues[i] = sum;
+        }
+
+        // Print result
+        Array.Sort(listOfNamesValues);
+        foreach (int value in listOfNamesValues)
+        {
+            Console.WriteLine(value);
+        }
+    }
+}
