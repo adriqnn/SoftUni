fa933f354091e955f362865560bcddeca5106f38
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-20 23:31:32 +0200

    06

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_06_EqualSums/_06_EqualSums.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_06_EqualSums/_06_EqualSums.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_EqualSums
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] numbersArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Add variable
+        int arrayIndex = 0;
+        bool isEqual = false;
+
+        // Determine if the sum of the array on the left is equal to the sum on the right
+        for (int i = 0; i < numbersArray.Length; i++)
+        {
+            int sumLeft = 0;
+            int sumRight = 0;
+
+            for (int j = 0; j < i; j++)
+            {
+                sumLeft += numbersArray[j];
+            }
+
+            for (int j = i + 1; j < numbersArray.Length; j++)
+            {
+                sumRight += numbersArray[j];
+            }
+
+            if (sumLeft == sumRight)
+            {
+                isEqual = true;
+                arrayIndex = i;
+                break;
+            }
+        }
+
+        // Print result
+        if (isEqual)
+        {
+            Console.WriteLine(arrayIndex);
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
