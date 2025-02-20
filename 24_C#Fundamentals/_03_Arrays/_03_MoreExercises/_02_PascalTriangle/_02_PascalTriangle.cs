8a8ca78e23a0489459bc70b9668259d7ba0ec661
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 00:59:37 +0200

    02

--- 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle/_02_PascalTriangle.cs
+++ 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_02_PascalTriangle/_02_PascalTriangle.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_PascalTriangle
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print pascal triangle
+        for (int i = 0; i < n; i++)
+        {
+            int number = 1;
+
+            for (int j = 0; j <= i; j++)
+            {
+                Console.Write(number + " ");
+                number = number * (i - j) / (j + 1);
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
