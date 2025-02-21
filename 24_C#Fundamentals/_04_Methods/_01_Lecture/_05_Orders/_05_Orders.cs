31081d47a69988ab56011f032eec881d7a7f7815
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:48:07 +0200

    05

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_05_Orders/_05_Orders.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_05_Orders/_05_Orders.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_Orders
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string item = Console.ReadLine();
+        double quantity = double.Parse(Console.ReadLine());
+
+        // Create method for incoming orders
+        whatIsThePrice(item, quantity);
+    }
+
+    private static void whatIsThePrice(String item, double quantity)
+    {
+        if (item.Equals("coffee"))
+        {
+            Console.WriteLine($"{1.50 * quantity:F2}");
+        }
+        else if (item.Equals("water"))
+        {
+            Console.WriteLine($"{1.00 * quantity:F2}");
+        }
+        else if (item.Equals("coke"))
+        {
+            Console.WriteLine($"{1.40 * quantity:F2}");
+        }
+        else if (item.Equals("snacks"))
+        {
+            Console.WriteLine($"{2.00 * quantity:F2}");
+        }
+    }
+}
