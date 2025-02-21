11ff81a59c3c3ebe74e2916201602a01547ed12e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:53:17 +0200

    08

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_08_MathPower/_08_MathPower.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_08_MathPower/_08_MathPower.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_MathPower
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double num = double.Parse(Console.ReadLine());
+        double power = double.Parse(Console.ReadLine());
+
+        // Print result from the method
+        double result = WhatIsThePowerOF(num, power);
+        Console.WriteLine(result.ToString("0.####"));
+    }
+
+    private static double WhatIsThePowerOF(double num, double power)
+    {
+        return Math.Pow(num, power);
+    }
+}
