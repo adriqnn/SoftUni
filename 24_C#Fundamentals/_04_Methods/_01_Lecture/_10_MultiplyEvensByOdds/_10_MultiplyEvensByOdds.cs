ac3c08a021874afaea0dc9ab276a36d59442b3bc
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:59:28 +0200

    10

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_10_MultiplyEvensByOdds/_10_MultiplyEvensByOdds.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_10_MultiplyEvensByOdds/_10_MultiplyEvensByOdds.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_MultiplyEvensByOdds
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string num = Console.ReadLine();
+
+        // Create method for multiplication
+        whatIf(num);
+    }
+
+    private static void whatIf(String number)
+    {
+        int thisNumber = Math.Abs(int.Parse(number));
+        string num = thisNumber.ToString();
+
+        int sumEven = 0;
+        int sumOdd = 0;
+
+        for (int i = 0; i < num.Length; i++)
+        {
+            char symbol = num[i];
+            int n = symbol - '0';
+
+            if (n % 2 == 0)
+            {
+                sumEven += n;
+            }
+            else
+            {
+                sumOdd += n;
+            }
+        }
+
+        Console.WriteLine(sumEven * sumOdd);
+    }
+}
