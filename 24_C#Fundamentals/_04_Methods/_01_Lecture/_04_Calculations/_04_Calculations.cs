69c6ab0552836e3715ad09455e4db4d4f9645aee
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:40:26 +0200

    04

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_04_Calculations/_04_Calculations.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_04_Calculations/_04_Calculations.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_Calculations
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string operation = Console.ReadLine();
+        int num1 = int.Parse(Console.ReadLine());
+        int num2 = int.Parse(Console.ReadLine());
+
+        // Create method to do the calculations
+        whatIsTheResult(operation, num1, num2);
+
+    }
+
+    private static void whatIsTheResult(String operation, int num1, int num2)
+    {
+        if (operation.Equals("add"))
+        {
+            Console.WriteLine(num1 + num2);
+        }
+        else if (operation.Equals("multiply"))
+        {
+            Console.WriteLine(num1 * num2);
+        }
+        else if (operation.Equals("subtract"))
+        {
+            Console.WriteLine(num1 - num2);
+        }
+        else if (operation.Equals("divide"))
+        {
+            Console.WriteLine(num1 / num2);
+        }
+    }
+}
