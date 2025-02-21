1487f06bd932a4b99836c8c26deb8f32e973cf0a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:33:56 +0200

    02

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_02_Grades/_02_Grades.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_02_Grades/_02_Grades.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_Grades
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        double grade = double.Parse(Console.ReadLine());
+
+        // Create method to qualify grades
+        whatIsTheGrade(grade);
+    }
+
+    private static void whatIsTheGrade(double grade)
+    {
+        if (grade >= 2.00 && grade < 3.00)
+        {
+            Console.WriteLine("Fail");
+        }
+        else if (grade < 3.50)
+        {
+            Console.WriteLine("Poor");
+        }
+        else if (grade < 4.50)
+        {
+            Console.WriteLine("Good");
+        }
+        else if (grade < 5.50)
+        {
+            Console.WriteLine("Very good");
+        }
+        else if (grade < 6.00)
+        {
+            Console.WriteLine("Excellent");
+        }
+    }
+}
