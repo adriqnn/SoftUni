a10c767b081721e0bfb46477e1bf347712539643
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 00:01:28 +0200

    11

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_11_MathOperations/_11_MathOperations.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_11_MathOperations/_11_MathOperations.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _11_MathOperations
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n1 = int.Parse(Console.ReadLine());
+        String operation = Console.ReadLine();
+        int n2 = int.Parse(Console.ReadLine());
+
+        // Create method for math operations
+        mathOperations(n1, operation, n2);
+    }
+
+    private static void mathOperations(int n1, String operation, int n2)
+    {
+        double result = 0;
+
+        if (operation.Equals("/"))
+        {
+            result = n1 / n2;
+        }
+        else if (operation.Equals("*"))
+        {
+            result = n1 * n2;
+        }
+        else if (operation.Equals("+"))
+        {
+            result = n1 + n2;
+        }
+        else if (operation.Equals("-"))
+        {
+            result = n1 - n2;
+        }
+
+        Console.WriteLine(result.ToString("0.##"));
+    }
+}
