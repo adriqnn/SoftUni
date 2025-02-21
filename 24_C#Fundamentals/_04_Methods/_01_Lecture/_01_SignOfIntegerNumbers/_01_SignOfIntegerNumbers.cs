6b8bfa57e08660d62c6977afc9944ff67ab3483f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:31:44 +0200

    01

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_01_SignOfIntegerNumbers/_01_SignOfIntegerNumbers.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_01_SignOfIntegerNumbers/_01_SignOfIntegerNumbers.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_SignOfIntegerNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int num = int.Parse(Console.ReadLine());
+
+        // Create method to determine the char sign
+        whatIsTheSign(num);
+    }
+
+    private static void whatIsTheSign(int num)
+    {
+        if (num == 0)
+        {
+            Console.WriteLine("The number 0 is zero.");
+        }
+        else if (num > 0)
+        {
+            Console.WriteLine($"The number {num} is positive.");
+        }
+        else
+        {
+            Console.WriteLine($"The number {num} is negative.");
+        }
+    }
+}
