a3715c50dfcf7d0bd65bae5e7554cd8a911defbb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:38:07 +0200

    03

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_03_PrintingTriangle/_03_PrintingTriangle.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_03_PrintingTriangle/_03_PrintingTriangle.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_PrintingTriangle
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Create method to print a triangle
+        printTriangle(n);
+    }
+
+    private static void printTriangle(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        for (int i = n - 1; i > 0; i--)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
