0fde863aef50417c59a6912d7f6dbcddc20f9634
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:49:48 +0200

    07

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_07_RepeatString/_07_RepeatString.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_07_RepeatString/_07_RepeatString.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _07_RepeatString
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string console = Console.ReadLine();
+        int num = int.Parse(Console.ReadLine());
+
+        // Create method that repeats a string
+        repeatString(console, num);
+    }
+
+    private static void repeatString(String console, int num)
+    {
+        StringBuilder newString = new StringBuilder();
+
+        for (int i = 0; i < num; i++)
+        {
+            newString.Append(console);
+        }
+
+        Console.WriteLine(newString);
+    }
+}
