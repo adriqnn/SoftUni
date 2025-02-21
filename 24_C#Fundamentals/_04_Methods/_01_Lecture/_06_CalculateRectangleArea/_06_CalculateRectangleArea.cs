0a092b11936b648987506a07827fb729e9684d5c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-21 23:48:23 +0200

    06

--- 24_C#Fundamentals/_04_Methods/_01_Lecture/_06_CalculateRectangleArea/_06_CalculateRectangleArea.cs
+++ 24_C#Fundamentals/_04_Methods/_01_Lecture/_06_CalculateRectangleArea/_06_CalculateRectangleArea.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_CalculateRectangleArea
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        // Create method that calculates rectangle area
+        rectangleArea(width, height);
+    }
+
+    private static void rectangleArea(double width, double height)
+    {
+        Console.WriteLine($"{width * height:F0}");
+    }
+}
