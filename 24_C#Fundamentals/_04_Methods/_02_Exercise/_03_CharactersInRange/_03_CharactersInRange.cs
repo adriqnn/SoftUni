4576b3d3508862416dd4faa2b210c349b54787ff
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:22:39 +0200

    03

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_03_CharactersInRange/_03_CharactersInRange.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_03_CharactersInRange/_03_CharactersInRange.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_CharactersInRange
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        char charOne = Console.ReadLine()[0];
+        char charTwo = Console.ReadLine()[0];
+
+        // Create method that prints ASCII chars in a given range
+        printThis(charOne, charTwo);
+    }
+
+    private static void printThis(char charOne, char charTwo)
+    {
+        for (int i = Math.Min(charOne, charTwo) + 1; i < Math.Max(charOne, charTwo); i++)
+        {
+            Console.Write((char)i + " ");
+        }
+    }
+}
