c89c66ffd3ea403278b8c2aba7adfa67848f74d4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 00:39:24 +0200

    01

--- 24_C#Fundamentals/_04_Methods/_03_MoreExercises/_01_DataTypes/_01_DataTypes.cs
+++ 24_C#Fundamentals/_04_Methods/_03_MoreExercises/_01_DataTypes/_01_DataTypes.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_DataTypes
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string type = Console.ReadLine();
+
+        // Determine the data type of the input
+        if (type.Equals("int"))
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            printDataTypes(num);
+        }
+        else if (type.Equals("real"))
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            printDataTypes(num);
+        }
+        else if (type.Equals("string"))
+        {
+            string line = Console.ReadLine();
+
+            printDataTypes(line);
+        }
+    }
+
+    private static void printDataTypes(int num)
+    {
+        Console.WriteLine(num * 2);
+    }
+
+    private static void printDataTypes(double num)
+    {
+        Console.WriteLine($"{num * 1.5:F2}");
+    }
+
+    private static void printDataTypes(String line)
+    {
+        string newString = "$" + line + "$";
+        Console.WriteLine(newString);
+    }
+}
