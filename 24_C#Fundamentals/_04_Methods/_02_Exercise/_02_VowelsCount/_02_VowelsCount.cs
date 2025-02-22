f3657dcc1bf1a6b59c4b96a7252b95eb4c6cf869
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:20:51 +0200

    02

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_02_VowelsCount/_02_VowelsCount.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_02_VowelsCount/_02_VowelsCount.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_VowelsCount
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine().ToLower();
+
+        // Create method that prints vowels sum
+        printVowelsSum(console);
+    }
+
+    private static void printVowelsSum(String console)
+    {
+        int count = 0;
+
+        for (int i = 0; i < console.Length; i++)
+        {
+            switch (console[i])
+            {
+                case 'a':
+                case 'y':
+                case 'o':
+                case 'e':
+                case 'i':
+                case 'u':
+                    count++;
+                    break;
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
