860ff6091747973b38fc070f9295e3c740ca9541
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:39:43 +0200

    04

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_04_PasswordValidator/_04_PasswordValidator.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_04_PasswordValidator/_04_PasswordValidator.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_PasswordValidator
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string password = Console.ReadLine();
+
+        // Determine if a password is valid
+        if (isPasswordValidLength(password) && hasLettersAndDigitsOnly(password) && hasAtLeast2Digits(password))
+        {
+            Console.WriteLine("Password is valid");
+        }
+        else
+        {
+            if (!isPasswordValidLength(password))
+            {
+                Console.WriteLine("Password must be between 6 and 10 characters");
+            }
+            if (!hasLettersAndDigitsOnly(password))
+            {
+                Console.WriteLine("Password must consist only of letters and digits");
+            }
+            if (!hasAtLeast2Digits(password))
+            {
+                Console.WriteLine("Password must have at least 2 digits");
+            }
+        }
+    }
+
+    private static bool hasAtLeast2Digits(string password)
+    {
+        int count = 0;
+
+        for (int i = 0; i < password.Length; i++)
+        {
+            char symbol = password[i];
+
+            if (char.IsDigit(symbol))
+            {
+                count++;
+            }
+        }
+        return count >= 2;
+    }
+
+    private static bool hasLettersAndDigitsOnly(string password)
+    {
+        for (int i = 0; i < password.Length; i++)
+        {
+            char symbol = password[i];
+
+            if (!char.IsLetterOrDigit(symbol))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static bool isPasswordValidLength(string password)
+    {
+        if (password.Length >= 6 && password.Length <= 10)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
