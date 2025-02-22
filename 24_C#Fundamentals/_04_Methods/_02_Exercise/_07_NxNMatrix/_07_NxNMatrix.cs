518f4f0e8e249b1b769dd9d566b13092095b044b
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:53:21 +0200

    07

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_07_NxNMatrix/_07_NxNMatrix.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_07_NxNMatrix/_07_NxNMatrix.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _07_NxNMatrix
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Create method that prints a number matrix
+        printNxN(n);
+    }
+
+    private static void printNxN(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(n + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
