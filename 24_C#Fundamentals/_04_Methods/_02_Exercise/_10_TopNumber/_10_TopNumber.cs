177cecb8af5f64d1512c24035fa1308901e27983
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 00:00:02 +0200

    10

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_10_TopNumber/_10_TopNumber.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_10_TopNumber/_10_TopNumber.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _10_TopNumber
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Create method that determines the top numbers
+        topNUmber(n);
+    }
+
+    private static void topNUmber(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            int number = i;
+            int sum = 0;
+            bool hasOdd = false;
+
+            while (number != 0)
+            {
+                if (number % 2 != 0)
+                {
+                    hasOdd = true;
+                }
+
+                sum += number % 10;
+                number /= 10;
+            }
+
+            if (sum % 8 == 0 && hasOdd)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
