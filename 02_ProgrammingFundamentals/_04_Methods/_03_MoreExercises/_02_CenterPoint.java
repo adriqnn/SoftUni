13a176c0dd80d25eca2728a0bcd2737405346861
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 00:41:53 +0200

    02

--- 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_02_CenterPoint.java
+++ 02_ProgrammingFundamentals/_04_Methods/_03_MoreExercises/_02_CenterPoint.java
@@ -7,10 +7,10 @@ public class _02_CenterPoint {
         Scanner scan = new Scanner(System.in);
 
         // Read input values
-        double x1 = Integer.parseInt(scan.nextLine());
-        double y1 = Integer.parseInt(scan.nextLine());
-        double x2 = Integer.parseInt(scan.nextLine());
-        double y2 = Integer.parseInt(scan.nextLine());
+        double x1 = Double.parseInt(scan.nextLine());
+        double y1 = Double.parseInt(scan.nextLine());
+        double x2 = Double.parseInt(scan.nextLine());
+        double y2 = Double.parseInt(scan.nextLine());
 
         // Create method that prints the closest point
         rectangleDistance(x1, y1, x2, y2);
