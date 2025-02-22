403ccad5fb299cfa78963cc2d333b31d9584416c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:59:08 +0200

    09

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_09_PalindromeIntegers/_09_PalindromeIntegers.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_09_PalindromeIntegers/_09_PalindromeIntegers.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _09_PalindromeIntegers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string number = Console.ReadLine();
+
+        // Cycle through and print result
+        while (!number.Equals("END"))
+        {
+            // Create method that determines if the input is palindrome
+            Console.WriteLine(isPalindromeInteger(number));
+
+            number = Console.ReadLine();
+        }
+    }
+
+    private static bool isPalindromeInteger(string number)
+    {
+        StringBuilder second = new StringBuilder();
+
+        for (int i = number.Length - 1; i >= 0; i--)
+        {
+            second.Append(number[i]);
+        }
+
+        int n1 = int.Parse(number);
+        int n2 = int.Parse(second.ToString());
+
+        return n1 == n2;
+    }
+}
