8d685484320a55bad5fe6cb1eff5a8ef5d994e47
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 22:35:11 +0200

    07

---
+++ 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_07_FoldAndSum/_07_FoldAndSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class _07_FoldAndSum
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Add variables
+        int mid = arr.Length / 2 / 2;
+        List<int> listFirstRev = arr.Take(mid).Reverse().ToList();
+        List<int> listLastRev = arr.Skip(arr.Length - mid).Reverse().ToList();
+
+        // Operations
+        List<int> newArrFirstLast = listFirstRev.Concat(listLastRev).ToList();
+        List<int> newArrMiddle = arr.Skip(mid).Take(arr.Length - 2 * mid).ToList();
+        List<int> result = new List<int>();
+
+        for (int i = 0; i < newArrFirstLast.Count; i++)
+        {
+            result.Add(newArrFirstLast[i] + newArrMiddle[i]);
+        }
+
+
+        // Print result
+        Console.WriteLine(string.Join(" ", result.ToArray()));
+    }
+}
