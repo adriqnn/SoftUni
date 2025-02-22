aa2db3148462dd1525c54e7073583a9b4cb3d3c1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:18:38 +0200

    01

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_01_SmallestOfThreeNumbers/_01_SmallestOfThreeNumbers.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_01_SmallestOfThreeNumbers/_01_SmallestOfThreeNumbers.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_SmallestOfThreeNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+        int n3 = int.Parse(Console.ReadLine());
+
+        // Create method to determine the smallest number out of three numbers
+        printBiggestNumber(n1, n2, n3);
+    }
+
+    private static void printBiggestNumber(int n1, int n2, int n3)
+    {
+        Console.WriteLine(Math.Min(n1, (Math.Min(n2, n3))));
+    }
+}
