cc16c1f7a2e9054c773414a1d4b35e156b1eb2f4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:57:03 +0200

    08

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_08_FactorialDivision/_08_FactorialDivision.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_08_FactorialDivision/_08_FactorialDivision.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_FactorialDivision
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n1 = int.Parse(Console.ReadLine());
+        int n2 = int.Parse(Console.ReadLine());
+
+        // Print result from the factorial division method
+        Console.WriteLine($"{(1.00 * factorialDivision(n1)) / factorialDivision(n2):F2}");
+    }
+    private static long factorialDivision(int n)
+    {
+        long factorial = 1;
+
+        for (int i = 2; i <= n; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        return factorial;
+    }
+}
