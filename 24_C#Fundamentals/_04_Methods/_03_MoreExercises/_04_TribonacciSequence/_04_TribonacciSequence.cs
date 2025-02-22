3799dd295c6b405e6478b5bf9c863327fd0eea1a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 01:06:25 +0200

    04

--- 24_C#Fundamentals/_04_Methods/_03_MoreExercises/_04_TribonacciSequence/_04_TribonacciSequence.cs
+++ 24_C#Fundamentals/_04_Methods/_03_MoreExercises/_04_TribonacciSequence/_04_TribonacciSequence.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_TribonacciSequence
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Print tribonacci sequence
+        tribonacciSequence(n);
+    }
+
+    private static void tribonacciSequence(int n)
+    {
+        int n1 = 1;
+        int n2 = 1;
+        int n3 = 2;
+
+        if (n == 0)
+        {
+            Console.Write("0 ");
+            return;
+        }
+
+        if (n == 1)
+        {
+            Console.Write("1 ");
+            return;
+        }
+
+        if (n == 2)
+        {
+            Console.Write("1 1 ");
+            return;
+        }
+
+        Console.Write("1 ");
+        Console.Write("1 ");
+
+        for (int i = 3; i <= n; i++)
+        {
+            int temp1 = n1;
+            int temp2 = n2;
+
+            Console.Write(n3 + " ");
+
+            n1 = n2;
+            n2 = n3;
+
+            n3 += temp1 + temp2;
+        }
+    }
+}
