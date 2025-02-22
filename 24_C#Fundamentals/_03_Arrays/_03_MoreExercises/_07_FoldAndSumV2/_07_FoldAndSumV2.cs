7e1b4c35bdc56ca1bb96782bb4561600ee3f5f16
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 22:35:17 +0200

    07

---
+++ 24_C#Fundamentals/_03_Arrays/_03_MoreExercises/_07_FoldAndSumV2/_07_FoldAndSumV2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class _07_FoldAndSumV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+        // Add variables
+        int n = arr.Length / 4;
+        int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++)
+        {
+            result[i] = arr[n - 1 - i] + arr[n + i];
+            result[i + n] = arr[arr.Length - 1 - i] + arr[2 * n + i];
+        }
+
+        // Print the resulting sums
+        Console.WriteLine(string.Join(" ", result));
+    }
+}
