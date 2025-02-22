9049d7cd4c32b44ba23ae1eb5170d9cc1e7be6fd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-22 23:51:56 +0200

    06

--- 24_C#Fundamentals/_04_Methods/_02_Exercise/_06_MiddleCharacters/_06_MiddleCharacters.cs
+++ 24_C#Fundamentals/_04_Methods/_02_Exercise/_06_MiddleCharacters/_06_MiddleCharacters.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_MiddleCharacters
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Create method that prints the middle of a string
+        middleString(console);
+    }
+
+    private static void middleString(string console)
+    {
+        if (console.Length % 2 == 0)
+        {
+            char middleEvenOne = console[(console.Length / 2) - 1];
+            char middleEvenTwo = console[console.Length / 2];
+
+            Console.WriteLine($"{middleEvenOne}{middleEvenTwo}");
+        }
+        else
+        {
+            char middleOdd = console[console.Length / 2];
+
+            Console.WriteLine(middleOdd);
+        }
+    }
+}
