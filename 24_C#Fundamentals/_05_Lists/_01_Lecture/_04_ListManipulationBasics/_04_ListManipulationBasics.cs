dfe3bc7fb78f00454edb9c43136b37c10d264ac2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 22:36:23 +0200

    Update _04_ListManipulationBasics.cs

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_04_ListManipulationBasics/_04_ListManipulationBasics.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_04_ListManipulationBasics/_04_ListManipulationBasics.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Xml.Linq;
+
+public class _04_ListManipulationBasics
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> numberList = Console.ReadLine().Split().Select(int.Parse).ToList();
+        string console = Console.ReadLine();
+
+        // Manipulate lists based on the given commands
+        while (!console.Equals("end"))
+        {
+            string commandline = console.Split()[0];
+
+            if (commandline.Equals("Add"))
+            {
+                numberList.Add(int.Parse(console.Split()[1]));
+            }
+            else if (commandline.Equals("Remove"))
+            {
+                numberList.Remove(int.Parse(console.Split()[1]));
+            }
+            else if (commandline.Equals("RemoveAt"))
+            {
+                numberList.Remove(int.Parse(console.Split()[1]));
+            }
+            else if (commandline.Equals("Insert"))
+            {
+                numberList.Insert(int.Parse(console.Split()[2]), int.Parse(console.Split()[1]));
+            }
+
+            console = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", numberList));
+    }
+}
