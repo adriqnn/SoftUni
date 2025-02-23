83d6918570f102827768bd5669cd4ace79d3af7f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 23:36:56 +0200

    06

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_06_ListOfProducts/_06_ListOfProducts.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_06_ListOfProducts/_06_ListOfProducts.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_ListOfProducts
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        List<string> productsList = new List<string>();
+
+        // Create list
+        for (int i = 0; i < n; i++)
+        {
+            productsList.Add(Console.ReadLine());
+        }
+
+        // Arrange list
+        productsList.Sort();
+
+        // Print result
+        for (int i = 0; i < productsList.Count; i++)
+        {
+            Console.WriteLine($"{i + 1}.{productsList[i]}");
+        }
+    }
+}
