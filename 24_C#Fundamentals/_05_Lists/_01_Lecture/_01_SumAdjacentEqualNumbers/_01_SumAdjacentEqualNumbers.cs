b54293a5f76c3b5cd9a045a6328bd8bfa223f691
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 21:58:27 +0200

    01

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_01_SumAdjacentEqualNumbers/_01_SumAdjacentEqualNumbers.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_01_SumAdjacentEqualNumbers/_01_SumAdjacentEqualNumbers.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+
+public class _01_SumAdjacentEqualNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<double> listNumbers = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+
+        // Sum adjacent numbers in list
+        for (int i = 0; i < listNumbers.Count - 1; i++)
+        {
+            if (listNumbers[i] == listNumbers[i + 1])
+            {
+                double sum = listNumbers[i] + listNumbers[i + 1];
+                listNumbers[i] = sum;
+                listNumbers.RemoveAt(i + 1);
+                i = -1;
+            }
+        }
+
+        // Print result
+        string output = string.Join(" ", listNumbers.Select(n => n.ToString("0.#", CultureInfo.InvariantCulture)));
+        Console.WriteLine(output);
+    }
+}
