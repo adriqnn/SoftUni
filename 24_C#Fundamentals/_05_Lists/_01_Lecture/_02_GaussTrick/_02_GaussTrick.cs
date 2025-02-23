7591e2b17b3d423d74a7c252ec3dea03ee35fadb
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 22:08:30 +0200

    02

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_02_GaussTrick/_02_GaussTrick.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_02_GaussTrick/_02_GaussTrick.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Xml.Linq;
+using System.Globalization;
+
+public class _02_GaussTrick
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> listNumbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        // Add variable
+        int middle = listNumbers.Count / 2;
+
+        // Sum numbers in the given pattern
+        for (int i = 0; i < middle; i++)
+        {
+            listNumbers[i] = listNumbers[i] + listNumbers[listNumbers.Count - 1];
+            listNumbers.RemoveAt(listNumbers.Count - 1);
+        }
+
+        //System.out.println(listNumbers);
+        Console.WriteLine(string.Join(" ", listNumbers));
+    }
+}
