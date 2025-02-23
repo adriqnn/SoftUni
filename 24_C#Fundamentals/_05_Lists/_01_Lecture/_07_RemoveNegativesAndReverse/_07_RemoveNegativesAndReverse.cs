d9d5ede5c5997b52c4bc0ca146fed83ca48eb010
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 23:42:47 +0200

    07

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_07_RemoveNegativesAndReverse/_07_RemoveNegativesAndReverse.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_07_RemoveNegativesAndReverse/_07_RemoveNegativesAndReverse.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Globalization;
+using System.Xml.Linq;
+
+public class _07_RemoveNegativesAndReverse
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> numbersList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        // Remove negative numbers
+        for (int i = 0; i < numbersList.Count; i++)
+        {
+            if (numbersList[i] < 0)
+            {
+                numbersList.Remove(i);
+                i--;
+            }
+        }
+
+        // Reverse list
+        numbersList.Reverse();
+
+        // Print result
+        if (numbersList.Count == 0)
+        {
+           Console.WriteLine("empty");
+        }
+        else
+        {
+            Console.WriteLine(string.Join(" ", numbersList));
+        }
+    }
+}
