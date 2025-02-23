9c59cb99d7e569e9d602a00d418294a919eba6d5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 23:33:43 +0200

    05

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_05_ListManipulationAdvanced/_05_ListManipulationAdvanced.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_05_ListManipulationAdvanced/_05_ListManipulationAdvanced.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class _05_ListManipulationAdvanced
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> numbersList = Console.ReadLine().Split().Select(int.Parse).ToList();
+        string console = Console.ReadLine();
+
+        // Add variable
+        bool isChanged = false;
+
+        // Manipulate the list based on the given commands
+        while (!console.Equals("end"))
+        {
+            string[] commandLine = console.Split();
+            string command = commandLine[0].ToLower();
+
+            if (command.Equals("add"))
+            {
+                numbersList.Add(int.Parse(commandLine[1]));
+                isChanged = true;
+            }
+            else if (command.Equals("remove"))
+            {
+                numbersList.Remove(int.Parse(commandLine[1]));
+                isChanged = true;
+            }
+            else if (command.Equals("removeat"))
+            {
+                numbersList.RemoveAt(int.Parse(commandLine[1]));
+                isChanged = true;
+            }
+            else if (command.Equals("insert"))
+            {
+                numbersList.Insert(int.Parse(commandLine[2]), int.Parse(commandLine[1]));
+                isChanged = true;
+            }
+            else if (command == "contains")
+            {
+                Console.WriteLine(numbersList.Contains(int.Parse(commandLine[1])) ? "Yes" : "No such number");
+            }
+            else if (command == "printeven")
+            {
+                Console.WriteLine(string.Join(' ', numbersList.Where(n => n % 2 == 0)));
+            }
+            else if (command == "printodd")
+            {
+                Console.WriteLine(string.Join(' ', numbersList.Where(n => n % 2 != 0)));
+            }
+            else if (command == "getsum")
+            {
+                Console.WriteLine(numbersList.Sum());
+            }
+            else if (command == "filter")
+            {
+                if (commandLine[1] == "<")
+                {
+                    Console.WriteLine(string.Join(' ', numbersList.Where(n => n < int.Parse(commandLine[2]))));
+                }
+                else if (commandLine[1] == ">")
+                {
+                    Console.WriteLine(string.Join(' ', numbersList.Where(n => n > int.Parse(commandLine[2]))));
+                }
+                else if (commandLine[1] == ">=")
+                {
+                    Console.WriteLine(string.Join(' ', numbersList.Where(n => n >= int.Parse(commandLine[2]))));
+                }
+                else if (commandLine[1] == "<=")
+                {
+                    Console.WriteLine(string.Join(' ', numbersList.Where(n => n <= int.Parse(commandLine[2]))));
+                }
+            }
+
+            console = Console.ReadLine();
+        }
+
+        if (isChanged)
+        {
+            Console.WriteLine(string.Join(' ', numbersList));
+        }
+    }
+}
