8ad22a34b0d6f7d3d884a46794571660df5ec089
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-23 22:20:13 +0200

    03

--- 24_C#Fundamentals/_05_Lists/_01_Lecture/_03_MergingLists/_03_MergingLists.cs
+++ 24_C#Fundamentals/_05_Lists/_01_Lecture/_03_MergingLists/_03_MergingLists.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+public class _03_MergingLists
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> numbersList1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> numbersList2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        // Add variables
+        List<int> newNumbersList1and2 = new List<int>();
+        int sizeOfSmaller = Math.Min(numbersList1.Count, numbersList2.Count);
+
+        // Merge the lists based on the smaller one
+        for (int i = 0; i < sizeOfSmaller; i++)
+        {
+            newNumbersList1and2.Add(numbersList1[i]);
+            newNumbersList1and2.Add(numbersList2[i]);
+        }
+
+        // Continue merging the lists
+        if (numbersList1.Count < numbersList2.Count)
+        {
+            for (int i = sizeOfSmaller; i < numbersList2.Count; i++)
+            {
+                newNumbersList1and2.Add(numbersList2[i]);
+            }
+        }
+        else
+        {
+            for (int i = sizeOfSmaller; i < numbersList1.Count; i++)
+            {
+                newNumbersList1and2.Add(numbersList1[i]);
+            }
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", newNumbersList1and2));
+    }
+}
