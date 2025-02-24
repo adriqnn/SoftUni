d444a95b087ad06662982855e54e880842db4974
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-24 23:16:12 +0200

    01

--- 24_C#Fundamentals/_05_Lists/_02_Exercise/_01_Train/_01_Train.cs
+++ 24_C#Fundamentals/_05_Lists/_02_Exercise/_01_Train/_01_Train.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class _01_Train
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> trainWagonsPassengers = Console.ReadLine().Split().Select(int.Parse).ToList();
+        int capacityOfWagon = int.Parse(Console.ReadLine());
+        string passengersListActions = Console.ReadLine();
+
+        // Arrange the train based on commands
+        while (!passengersListActions.Equals("end"))
+        {
+            if (passengersListActions.Split()[0].Equals("Add"))
+            {
+                int addWagonAndPassengers = int.Parse(passengersListActions.Split()[1]);
+
+                trainWagonsPassengers.Add(addWagonAndPassengers);
+            }
+            else
+            {
+                int addPassengers = int.Parse(passengersListActions.Split()[0]);
+
+                for (int i = 0; i < trainWagonsPassengers.Count; i++)
+                {
+                    if (trainWagonsPassengers[i] + addPassengers <= capacityOfWagon)
+                    {
+                        trainWagonsPassengers[i] = trainWagonsPassengers[i] + addPassengers;
+                        break;
+                    }
+                }
+            }
+
+            passengersListActions = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(" ", trainWagonsPassengers));
+    }
+}
