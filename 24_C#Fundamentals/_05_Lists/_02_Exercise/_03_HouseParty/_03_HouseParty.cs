8b3380d29b7c32aa5042f0b4e5c46d41681e4a37
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-24 23:37:44 +0200

    03

--- 24_C#Fundamentals/_05_Lists/_02_Exercise/_03_HouseParty/_03_HouseParty.cs
+++ 24_C#Fundamentals/_05_Lists/_02_Exercise/_03_HouseParty/_03_HouseParty.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class _03_HouseParty
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        List<string> partyList = new List<string>();
+
+        // Determine who is going to the party
+        for (int i = 0; i < n; i++)
+        {
+            string[] commandline = Console.ReadLine().Split();
+
+            if (commandline[2].Equals("going!"))
+            {
+                if (!partyList.Contains(commandline[0]))
+                {
+                    partyList.Add(commandline[0]);
+                }
+                else
+                {
+                    Console.WriteLine($"{commandline[0]} is already in the list!");
+                }
+            }
+            else
+            {
+                if (partyList.Contains(commandline[0]))
+                {
+                    partyList.Remove(commandline[0]);
+                }
+                else
+                {
+                    Console.WriteLine($"{commandline[0]} is not in the list!");
+                }
+            }
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(Environment.NewLine, partyList));
+    }
+}
