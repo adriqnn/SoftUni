2edba0e33532357f21e6826cbff2c88aab7985dd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-25 23:39:47 +0200

    07

--- 24_C#Fundamentals/_05_Lists/_02_Exercise/_07_AppendArrays/_07_AppendArrays.cs
+++ 24_C#Fundamentals/_05_Lists/_02_Exercise/_07_AppendArrays/_07_AppendArrays.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class _07_AppendArrays
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<string> numbersList = Console.ReadLine().Split(new char[] { '|' }, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        // Reverse the input
+        numbersList.Reverse();
+
+        string result = string.Join(" ", numbersList);
+        result = Regex.Replace(result, @"\s+", " ");
+
+        // Print result
+        Console.WriteLine(result);
+    }
+}
