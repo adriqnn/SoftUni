bdc9d8c9448bb24c312a2b3a58318d667a640124
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-25 23:19:47 +0200

    06

--- 24_C#Fundamentals/_05_Lists/_02_Exercise/_06_CardsGame/_06_CardsGame.cs
+++ 24_C#Fundamentals/_05_Lists/_02_Exercise/_06_CardsGame/_06_CardsGame.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Xml.Linq;
+using System.Linq;
+
+public class _06_CardsGame
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> cardsList1 = Console.ReadLine().Split().Select(int.Parse).ToList();
+        List<int> cardsList2 = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+        // Play card game and determine who wins
+        while (cardsList1.Count > 0 && cardsList2.Count > 0)
+        {
+            if (cardsList1[0].Equals(cardsList2[0]))
+            {
+                cardsList1.RemoveAt(0);
+                cardsList2.RemoveAt(0);
+            }
+            else if (cardsList1[0] > cardsList2[0])
+            {
+                cardsList1.Add(cardsList1[0]);
+                cardsList1.Add(cardsList2[0]);
+
+                cardsList1.RemoveAt(0);
+                cardsList2.RemoveAt(0);
+            }
+            else if (cardsList1[0] < cardsList2[0])
+            {
+                cardsList2.Add(cardsList2[0]);
+                cardsList2.Add(cardsList1[0]);
+
+                cardsList1.RemoveAt(0);
+                cardsList2.RemoveAt(0);
+            }
+        }
+
+        int sum = 0;
+
+        // Print result
+        if (cardsList1.Count == 0)
+        {
+            foreach (int integer in cardsList2)
+            {
+                sum += integer;
+            }
+
+            Console.WriteLine($"Second player wins! Sum: {sum}");
+        }
+        else
+        {
+            foreach (int integer in cardsList1)
+            {
+                sum += integer;
+            }
+
+            Console.WriteLine($"First player wins! Sum: {sum}");
+        }
+    }
+}
