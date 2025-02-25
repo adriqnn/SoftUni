728e9c60af8ee11832d4bb31ac3ce40db4a8f53e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-26 01:10:24 +0200

    02

--- 24_C#Fundamentals/_05_Lists/_03_MoreExercises/_02_CarRace/_02_CarRace.cs
+++ 24_C#Fundamentals/_05_Lists/_03_MoreExercises/_02_CarRace/_02_CarRace.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _02_CarRace
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> numbersList = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+        // Add variables
+        double timeFirstRacer = 0;
+        double timeSecondRacer = 0;
+
+        // Determine the time of the first racer
+        for (int i = 0; i < numbersList.Count / 2; i++)
+        {
+            if (numbersList[i] == 0)
+            {
+                timeFirstRacer *= 0.8;
+                continue;
+            }
+
+            timeFirstRacer += numbersList[i];
+        }
+
+        // Determine the time of the second racer
+        for (int i = numbersList.Count - 1; i > numbersList.Count / 2; i--)
+        {
+            if (numbersList[i] == 0)
+            {
+                timeSecondRacer *= 0.8;
+                continue;
+            }
+
+            timeSecondRacer += numbersList[i];
+        }
+
+        // Print result
+        if (timeFirstRacer < timeSecondRacer)
+        {
+            Console.WriteLine($"The winner is left with total time: {timeFirstRacer}");
+        }
+        else if (timeFirstRacer > timeSecondRacer)
+        {
+            Console.WriteLine($"The winner is right with total time: {timeSecondRacer}");
+        }
+    }
+}
