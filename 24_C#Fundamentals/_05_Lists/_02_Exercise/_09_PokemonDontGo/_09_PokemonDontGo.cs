1579003fed0c1c85a4f9d4473ff00309e3d87f14
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-26 00:17:04 +0200

    09

--- 24_C#Fundamentals/_05_Lists/_02_Exercise/_09_PokemonDontGo/_09_PokemonDontGo.cs
+++ 24_C#Fundamentals/_05_Lists/_02_Exercise/_09_PokemonDontGo/_09_PokemonDontGo.cs
@@ -1,2 +1,78 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Xml.Linq;
+
+public class _09_PokemonDontGo
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        List<int> pokemonDistance = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+        // Add variable
+        int sum = 0;
+
+        // Read and execute commands based on the input
+        while (pokemonDistance.Count > 0)
+        {
+            int element = int.Parse(Console.ReadLine());
+
+            if (element >= 0 && element < pokemonDistance.Count)
+            {
+                int removedElement = pokemonDistance[element];
+                pokemonDistance.RemoveAt(element);
+                sum += removedElement;
+
+                for (int i = 0; i < pokemonDistance.Count; i++)
+                {
+                    if (pokemonDistance[i] <= removedElement)
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] + removedElement;
+                    }
+                    else
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] - removedElement;
+                    }
+                }
+            }
+            else if (element < 0)
+            {
+                int removedElement = pokemonDistance[0];
+                pokemonDistance[0] = pokemonDistance[pokemonDistance.Count - 1];
+                sum += removedElement;
+
+                for (int i = 0; i < pokemonDistance.Count; i++)
+                {
+                    if (pokemonDistance[i] <= removedElement)
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] + removedElement;
+                    }
+                    else
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] - removedElement;
+                    }
+                }
+            }
+            else if (element > pokemonDistance.Count - 1)
+            {
+                int removedElement = pokemonDistance[pokemonDistance.Count - 1];
+                pokemonDistance[pokemonDistance.Count - 1] = pokemonDistance[0];
+                sum += removedElement;
+
+                for (int i = 0; i < pokemonDistance.Count; i++)
+                {
+                    if (pokemonDistance[i] <= removedElement)
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] + removedElement;
+                    }
+                    else
+                    {
+                        pokemonDistance[i] = pokemonDistance[i] - removedElement;
+                    }
+                }
+            }
+        }
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
