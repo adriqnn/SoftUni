6aaedfde8ffc97833442ef38a26edfb0ab1e1e53
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-02-27 23:46:52 +0200

    03

--- 24_C#Fundamentals/_05_Lists/_03_MoreExercises/_03_TakeSkipRope/_03_TakeSkipRope.cs
+++ 24_C#Fundamentals/_05_Lists/_03_MoreExercises/_03_TakeSkipRope/_03_TakeSkipRope.cs
@@ -1,2 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _03_TakeSkipRope
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        StringBuilder console = new StringBuilder(Console.ReadLine());
+
+        // Add variables
+        List<int> decryptIntegers = new List<int>();
+        List<string> decryptStrings = new List<string>();
+        List<int> takeList = new List<int>();
+        List<int> skipList = new List<int>();
+
+        // Determine if char is digit or not
+        for (int i = 0; i < console.Length; i++)
+        {
+            char symbol = console[i];
+
+            if (Char.IsDigit(symbol))
+            {
+                decryptIntegers.Add(symbol - '0');
+            }
+            else
+            {
+                decryptStrings.Add(symbol.ToString());
+            }
+        }
+
+        // Determine the needed integers
+        for (int i = 0; i < decryptIntegers.Count; i++)
+        {
+            if (i % 2 == 0)
+            {
+                takeList.Add(decryptIntegers[i]);
+            }
+            else
+            {
+                skipList.Add(decryptIntegers[i]);
+            }
+        }
+
+        string fromDecryptStrings = string.Concat(decryptStrings);
+
+        StringBuilder result = new StringBuilder();
+
+        // Decrypt String
+        for (int i = 0; i < takeList.Count; i++)
+        {
+            int take = takeList[i];
+            int skip = skipList[i];
+
+            if (take > fromDecryptStrings.Length)
+            {
+                take = fromDecryptStrings.Length;
+            }
+
+            result.Append(fromDecryptStrings.Substring(0, take));
+
+            fromDecryptStrings = fromDecryptStrings.Substring(take);
+
+            if (skip > fromDecryptStrings.Length)
+            {
+                skip = fromDecryptStrings.Length;
+            }
+
+            fromDecryptStrings = fromDecryptStrings.Substring(skip);
+        }
+
+        // Print result
+        Console.WriteLine(result);
+    }
+}
