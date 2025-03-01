30d1a7a8cd14ccc70119156b486fc07b0b1d99ea
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-01 23:54:19 +0200

    02

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_02_SumBigNumbers/_02_SumBigNumbers.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_02_SumBigNumbers/_02_SumBigNumbers.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Numerics;
+
+public class _02_SumBigNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        BigInteger one = BigInteger.Parse(Console.ReadLine());
+        BigInteger two = BigInteger.Parse(Console.ReadLine());
+
+        // Sum the input values
+        BigInteger sum = one + two;
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
