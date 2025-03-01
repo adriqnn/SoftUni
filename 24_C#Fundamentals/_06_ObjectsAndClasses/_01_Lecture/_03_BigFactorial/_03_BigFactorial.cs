6d0a7ac1e6d2c8e4a28f4b710fd76d3f638e4e65
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-01 23:56:42 +0200

    03

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_03_BigFactorial/_03_BigFactorial.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_03_BigFactorial/_03_BigFactorial.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Numerics;
+
+public class _03_BigFactorial
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int factorial = int.Parse(Console.ReadLine());
+
+        // Add variable
+        BigInteger result = BigInteger.One;
+
+        // Determine the factorial of the input
+        for (int i = 1; i <= factorial; i++)
+        {
+            result *= i;
+        }
+
+        // Print result
+        Console.WriteLine(result);
+    }
+}
