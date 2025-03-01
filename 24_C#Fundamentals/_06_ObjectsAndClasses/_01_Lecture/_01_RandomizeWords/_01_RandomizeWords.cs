e5d336087c72c21f71bd19b5085692f025ba52ca
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-01 23:47:37 +0200

    01

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_01_RandomizeWords/_01_RandomizeWords.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_01_RandomizeWords/_01_RandomizeWords.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_RandomizeWords
+{
+    public static void Main(string[] args)
+    {
+        // Read input values 
+        List<string> words = Console.ReadLine().Split(" ").ToList();
+
+        // Add variable
+        Random rnd = new Random();
+
+        // Randomize the input and print
+        while (words.Count > 0)
+        {
+            int index = rnd.Next(words.Count);
+            string word = words[index];
+            words.RemoveAt(index);
+            Console.WriteLine(word);
+        }
+    }
+}
