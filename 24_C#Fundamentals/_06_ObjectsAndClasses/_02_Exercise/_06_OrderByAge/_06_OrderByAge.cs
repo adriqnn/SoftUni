5cfcc2d92f2d55ce0cd6024a166240ed94bac135
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-03 01:40:19 +0200

    06

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge/_06_OrderByAge.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_06_OrderByAge/_06_OrderByAge.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_OrderByAge
+{
+    public static void Main(string[] args)
+    {
+        // Read input vale
+        string console = Console.ReadLine();
+
+        // Add variable
+        List<Album> names = new List<Album>();
+
+
+        // Create collection of people
+        while (!console.Equals("End"))
+        {
+            string[] consoleLine = console.Split(" ");
+
+            string name = consoleLine[0];
+            string personalNumber = consoleLine[1];
+            int age = int.Parse(consoleLine[2]);
+
+            Album person = new Album(name, personalNumber, age);
+            names.Add(person);
+
+            console = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(Environment.NewLine, names.OrderBy(n => n.Age).Select(e => e.ToString())));
+    }
+}
+
+// Create practice Album class
+class Album
+{
+    public string Name { get; set; }
+    public string PersonalNumber { get; set; }
+    public int Age { get; set; }
+
+    public Album(string name, string personalNUmber, int age)
+    {
+        this.Name = name;
+        this.PersonalNumber = personalNUmber;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{this.Name} with ID: {this.PersonalNumber} is {this.Age} years old.";
+    }
+}
