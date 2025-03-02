77ba274c56d8ffd6807706f071b1db40441c69b1
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-03 00:53:58 +0200

    04

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_04_Articles2/_04_Articles2.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_04_Articles2/_04_Articles2.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_Articles2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // add variable
+        List<ArticlesDatabase> articles = new List<ArticlesDatabase>();
+
+        // Collect data for all the articles
+        for (int i = 0; i < n; i++)
+        {
+            string[] articleLines = Console.ReadLine().Split(", ", StringSplitOptions.None);
+            string title = articleLines[0];
+            string content = articleLines[1];
+            string author = articleLines[2];
+
+            ArticlesDatabase article = new ArticlesDatabase(title, content, author);
+            articles.Add(article);
+        }
+
+        string command = Console.ReadLine();
+
+        // Sort and print result based on the command
+        if (command == "title")
+        {
+            articles.Sort((a, b) => a.Title.CompareTo(b.Title));
+        }
+        else if (command == "content")
+        {
+            articles.Sort((a, b) => a.Content.CompareTo(b.Content));
+        }
+        else if (command == "author")
+        {
+            articles.Sort((a, b) => a.Author.CompareTo(b.Author));
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(System.Environment.NewLine, articles.Select(e => $"{e.Title} - {e.Content}: {e.Author}")));
+    }
+}
+
+// Create practice ArticleDatabase class
+class ArticlesDatabase
+{
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public string Author { get; set; }
+
+    public ArticlesDatabase(string title, string content, string author)
+    {
+        this.Title = title;
+        this.Content = content;
+        this.Author = author;
+    }
+}
