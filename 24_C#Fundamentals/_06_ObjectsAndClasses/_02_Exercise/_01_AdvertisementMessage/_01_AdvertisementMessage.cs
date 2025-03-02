51fe009a44baaae1204c41d08e558aefe4456cf0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-02 22:53:12 +0200

    01

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_01_AdvertisementMessage/_01_AdvertisementMessage.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_01_AdvertisementMessage/_01_AdvertisementMessage.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _01_AdvertisementMessage
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variables
+        List<string> one = new List<string> { "Excellent product.", "Such a great product.", "I always use that product.", "Best product of its category.", "Exceptional product.", "I can't live without this product." };
+        List<string> two = new List<string> { "Now I feel good.", "I have succeeded with this product.", "Makes miracles. I am happy of the results!", "I cannot believe but now i feel awesome.", "Try it yourself, I am very satisfied.", "I feel great!" };
+        List<string> three = new List<string> { "Diana", "Petya", "Stella", "Elena", "Katya", "Iva", "Annie", "Eva" };
+        List<string> four = new List<string> { "Burgas", "Sofia", "Plovdiv", "Varna", "Ruse" };
+
+        // Generate and print randomized advertisement message
+        for (int i = 0; i < n; i++)
+        {
+            Random rnd = new Random();
+
+            int randomOne = rnd.Next(one.Count);
+            string first = one[randomOne];
+
+            int randomTwo = rnd.Next(two.Count);
+            string second = two[randomTwo];
+
+            int randomThree = rnd.Next(three.Count);
+            string third = three[randomThree];
+
+            int randomFour = rnd.Next(four.Count);
+            string fourth = four[randomFour];
+
+            Console.WriteLine($"{first} {second} {third} - {fourth}");
+        }
+    }
+}
