0f6801e6025a0c58f501d75aeefe6f2ddc675a85
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-02 22:27:45 +0200

    06

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_06_Students2/_06_Students2.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_06_Students2/_06_Students2.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_Students2
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variable
+        List<StudentInfo> studentList = new List<StudentInfo>();
+
+        // Save a list of students and their information
+        while (!console.Equals("end"))
+        {
+            string[] lineFromConsole = console.Split(" ");
+
+            string firstName = lineFromConsole[0];
+            string secondName = lineFromConsole[1];
+            int age = int.Parse(lineFromConsole[2]);
+            string t = lineFromConsole[3]!;
+
+            if (!isAlreadyExistingInsideTheList(firstName, secondName, age, t, studentList))
+            {
+                StudentInfo student = new StudentInfo(firstName, secondName, age, t);
+
+                studentList.Add(student);
+            }
+
+            console = Console.ReadLine();
+        }
+
+        string town = Console.ReadLine();
+
+        // Print result
+        foreach (StudentInfo element in studentList)
+        {
+            if (element.Town.Equals(town))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+    // Define method to check if the value already exists in the list
+    private static bool isAlreadyExistingInsideTheList(string firstName, string secondName, int age, string town, List<StudentInfo> studentList)
+    {
+        foreach (StudentInfo element in studentList)
+        {
+            bool firstNameBoolean = element.FirstName.Equals(firstName);
+            bool secondNameBoolean = element.SecondName.Equals(secondName);
+
+            if (firstNameBoolean && secondNameBoolean)
+            {
+                element.Age = age;
+                element.Town = town;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
+
+// Create practice StudentInfo class
+class StudentInfo
+{
+    public string FirstName { get; }
+    public string SecondName { get; }
+    public int Age { get; set; }
+    public string Town { get; set; }
+
+    public StudentInfo(string firstName, string secondName, int age, string town)
+    {
+        FirstName = firstName;
+        SecondName = secondName;
+        Age = age;
+        Town = town;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {SecondName} is {Age} years old.";
+    }
+}
