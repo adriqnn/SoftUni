3a8d45bcfe35c6c80fe5f2b028dc9d1fc3f67fc6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-03 00:22:54 +0200

    03

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_03_OpinionPoll/_03_OpinionPoll.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_03_OpinionPoll/_03_OpinionPoll.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _03_OpinionPoll
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        List<Person> listOfPeople = new List<Person>();
+
+        // Populate the list with data for people
+        for (int i = 0; i < n; i++)
+        {
+            string[] consoleLine = Console.ReadLine().Split(" ");
+
+            string person = consoleLine[0];
+            int age = int.Parse(consoleLine[1]);
+
+            Person newPerson = new Person(person, age);
+            listOfPeople.Add(newPerson);
+        }
+
+        // Sort the list
+        listOfPeople.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+
+        // Print result
+        foreach (Person person in listOfPeople)
+        {
+            if (person.Age > 30)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
+
+// Create practice Person class
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
