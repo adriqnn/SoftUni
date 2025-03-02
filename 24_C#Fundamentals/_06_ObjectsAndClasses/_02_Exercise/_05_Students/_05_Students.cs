e7f4dcc946064b34c54219d84122de89853f5c25
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-03 01:10:28 +0200

    05

--- 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_05_Students/_05_Students.cs
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_02_Exercise/_05_Students/_05_Students.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class _05_Students
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        List<Student> students = new List<Student>();
+
+        // Create collection of students
+        for (int i = 0; i < n; i++)
+        {
+            string[] tokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string firstName = tokens[0];
+            string lastName = tokens[1];
+            double grade = double.Parse(tokens[2]);
+
+            Student student = new Student(firstName, lastName, grade);
+            students.Add(student);
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(Environment.NewLine, students.OrderByDescending(e => e.Grade).Select(e => e.ToString())));
+    }
+}
+
+// Create practice Student class
+class Student
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public double Grade { get; set; }
+
+    public Student(string firstName, string lastName, double grade)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.Grade = grade;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {LastName}: {Grade:F2}";
+    }
+}
