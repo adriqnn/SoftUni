5653053f26a3e2a68f399e8f673939324f2bba9f
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-03-23 23:41:16 +0200

    03

--- 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_03_SoftUniBarIncome.java
+++ 02_ProgrammingFundamentals/_11_RegularExpressions/_02_Exercise/_03_SoftUniBarIncome.java
@@ -15,28 +15,28 @@ public class _03_SoftUniBarIncome {
         String regex = "\\%(?<customer>[A-Z][a-z]+)\\%[^|%.$]*<(?<product>\\w+)>[^|%.$]*\\|(?<count>[0-9]+)\\|[^|%.$\\d]*(?<price>[0-9]+\\.?[0-9][0-9]?)\\$";
         Pattern pattern = Pattern.compile(regex);
         double total = 0;
-        
+
         // Calculate the income of a bar
-        while (!input.equals("end of shift")){
+        while (!input.equals("end of shift")) {
             Matcher matcher = pattern.matcher(input);
-            
+
             if (matcher.find()) {
                 String customer = matcher.group("customer");
                 String product = matcher.group("product");
                 int count = Integer.parseInt(matcher.group("count"));
                 double price = Double.parseDouble(matcher.group("price"));
-                
+
                 total += count * price;
-                
+
                 System.out.printf("%s: %s - %.2f%n", customer, product, count * price);
             }
-            
+
             input = scan.nextLine();
         }
-        
+
         // Print result
         System.out.printf("Total income: %.2f", total);
-        
+
         scan.close();
     }
 }
