118877d1ce235727abf38eb95ce1fc9074a37d31
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-13 02:18:12 +0300

    12

--- 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_12_CondenseArrayToNumber.java
+++ 02_ProgrammingFundamentals/_03_Arrays/_02_Exercise/_12_CondenseArrayToNumber.java
@@ -1,1 +1,24 @@
+package _02_ProgrammingFundamentals._03_Arrays._02_Exercise;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _12_CondenseArrayToNumber {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scan.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        while (arr.length > 1) {
+            int[] arrNew = new int[arr.length - 1];
+
+            for (int i = 0; i < arr.length - 1; i++) {
+                arrNew[i] = arr[i] + arr[i + 1];
+            }
+
+            arr = arrNew;
+        }
+
+        System.out.println(arr[0]);
+    }
+}
