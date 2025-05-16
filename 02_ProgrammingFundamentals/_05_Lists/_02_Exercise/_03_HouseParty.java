4efd087eb56848039a1f79b0bf9d2c4b410905a8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-16 23:23:07 +0300

    03

--- 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_03_HouseParty.java
+++ 02_ProgrammingFundamentals/_05_Lists/_02_Exercise/_03_HouseParty.java
@@ -8,7 +8,7 @@ public class _03_HouseParty {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input values
+        // Read input value
         int n = Integer.parseInt(scan.nextLine());
 
         // Add variable
