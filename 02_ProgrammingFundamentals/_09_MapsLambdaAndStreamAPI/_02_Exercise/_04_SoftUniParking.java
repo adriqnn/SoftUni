250bb2c86cbf18aeaf00d074b5aefafa3dd5bcc6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-19 00:28:41 +0300

    04

--- 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_04_SoftUniParking.java
+++ 02_ProgrammingFundamentals/_09_MapsLambdaAndStreamAPI/_02_Exercise/_04_SoftUniParking.java
@@ -8,7 +8,7 @@ public class _04_SoftUniParking {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        // Read input values
+        // Read input value
         int numberOfCommands = Integer.parseInt(scan.nextLine());
 
         // Add variable
@@ -21,18 +21,18 @@ public class _04_SoftUniParking {
             String command = commands[0];
             String username = commands[1];
 
-            if(command.equals("register")){
+            if (command.equals("register")) {
                 String licensePlate = commands[2];
                 
-                if(registry.containsKey(username)){
+                if (registry.containsKey(username)) {
                     System.out.printf("ERROR: already registered with plate number %s%n", licensePlate);
                     continue;
                 }
                 
                 registry.put(username, licensePlate);
                 System.out.printf("%s registered %s successfully%n", username, licensePlate);
-            }else{
-                if(!registry.containsKey(username)) {
+            } else {
+                if (!registry.containsKey(username)) {
                     System.out.printf("ERROR: user %s not found%n", username);
                     continue;
                 }
