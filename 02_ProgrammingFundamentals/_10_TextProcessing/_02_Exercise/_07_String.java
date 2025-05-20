3813a025d155414d01412bf49d3e924db3940606
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-20 22:52:09 +0300

    07

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_07_String.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_07_String.java
@@ -18,27 +18,27 @@ public class _07_String {
             int index = 0;
             int remove = 0;
 
-            if (console.charAt(i) == '>'){
-                if(has){
-                    index = Character.getNumericValue(console.charAt(i+1)) + leftover;
-                    remove = Character.getNumericValue(console.charAt(i+1)) + leftover;
+            if (console.charAt(i) == '>') {
+                if (has) {
+                    index = Character.getNumericValue(console.charAt(i + 1)) + leftover;
+                    remove = Character.getNumericValue(console.charAt(i + 1)) + leftover;
 
                     has = false;
-                }else{
-                    index = Character.getNumericValue(console.charAt(i+1));
-                    remove = Character.getNumericValue(console.charAt(i+1));
+                } else {
+                    index = Character.getNumericValue(console.charAt(i + 1));
+                    remove = Character.getNumericValue(console.charAt(i + 1));
                 }
 
-                if(i + index >= console.length()-1 && !console.substring(i + 1, console.length()-1).contains(">")){
+                if (i + index >= console.length() - 1 && !console.substring(i + 1, console.length() - 1).contains(">")) {
                     console.delete(i + 1, i + 1 + index);
                     continue;
-                }else if(i + index >= console.length()-1){
-                    for (int j = i+1; j < i+index + 1; j++) {
-                        if(console.charAt(j) != '>'){
+                } else if (i + index >= console.length() - 1) {
+                    for (int j = i + 1; j < i + index + 1; j++) {
+                        if (console.charAt(j) != '>') {
                             console.deleteCharAt(j);
                             remove--;
                             j--;
-                        }else{
+                        } else {
                             leftover = remove;
                             has = true;
                             break;
@@ -48,21 +48,20 @@ public class _07_String {
                     continue;
                 }
 
-                if(!console.substring(i + 1, i + index + 1).contains(">")){
+                if (!console.substring(i + 1, i + index + 1).contains(">")) {
                     console.delete(i + 1, i + 1 + index);
-                }else{
-                    for (int j = i+1; j < i+index + 1; j++) {
-                        if(console.charAt(j) != '>'){
+                } else {
+                    for (int j = i + 1; j < i + index + 1; j++) {
+                        if (console.charAt(j) != '>') {
                             console.deleteCharAt(j);
                             remove--;
                             j--;
-                        }else{
+                        } else {
                             leftover = remove;
                             has = true;
                             break;
                         }
                     }
-
                 }
             }
         }
