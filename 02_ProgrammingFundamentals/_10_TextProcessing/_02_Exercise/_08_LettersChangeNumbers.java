75d62f02f2178efaf4f14494661c003a232da8bd
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-20 22:54:29 +0300

    08

--- 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_08_LettersChangeNumbers.java
+++ 02_ProgrammingFundamentals/_10_TextProcessing/_02_Exercise/_08_LettersChangeNumbers.java
@@ -15,19 +15,20 @@ public class _08_LettersChangeNumbers {
         // Solve numbers game
         for (int i = 0; i < console.length; i++) {
             char first = console[i].charAt(0);
-            char last = console[i].charAt(console[i].length()-1);
+            char last = console[i].charAt(console[i].length() - 1);
 
-            double num = Double.parseDouble(console[i].substring(1, console[i].length()-1));
+            double num = Double.parseDouble(console[i].substring(1, console[i].length() - 1));
             double sumThisOne = 0;
 
-            if(Character.isLowerCase(first)){
+            if (Character.isLowerCase(first)) {
                 sumThisOne = num * (first - 96);
-            }else{
+            } else {
                 sumThisOne = num / (first - 64);
             }
-            if(Character.isLowerCase(last)){
+            
+            if (Character.isLowerCase(last)) {
                 sumThisOne += last - 96;
-            }else{
+            } else {
                 sumThisOne -= last - 64;
             }
 
