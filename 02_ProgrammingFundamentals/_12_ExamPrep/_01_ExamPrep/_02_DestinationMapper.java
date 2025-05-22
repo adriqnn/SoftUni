8725bfb3f6b44c75605dce08af7e57f4455b26e8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-22 23:23:07 +0300

    02

--- 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_02_DestinationMapper.java
+++ 02_ProgrammingFundamentals/_12_ExamPrep/_01_ExamPrep/_02_DestinationMapper.java
@@ -21,9 +21,9 @@ public class _02_DestinationMapper {
         List<String> locations = new ArrayList<>();
         int sum = 0;
         
-        //int travelPoints = locations.stream().mapToInt(location -> location.length()).sum();
-        //int travelPoints = locations.stream().mapToInt(String::length).sum();
-        //String resultText = String.join(", ", locations);
+        // int travelPoints = locations.stream().mapToInt(location -> location.length()).sum();
+        // int travelPoints = locations.stream().mapToInt(String::length).sum();
+        // String resultText = String.join(", ", locations);
 
         // Determine destination
         while (regexMatcher.find()) {
@@ -36,7 +36,7 @@ public class _02_DestinationMapper {
 
         // Print result
         System.out.print("Destinations: ");
-        //locations.stream().forEach(e -> System.out.print(e));
+        // locations.stream().forEach(e -> System.out.print(e));
         System.out.println(locations.toString().replace("[", "").replace("]",  ""));
         System.out.println("Travel Points: " + sum);
 
