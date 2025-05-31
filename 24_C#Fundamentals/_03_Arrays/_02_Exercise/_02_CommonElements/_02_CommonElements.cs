3298de389ed13b24b31e6ceed2f368c8220d33ee
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-05-31 23:17:29 +0300

    02

--- 24_C#Fundamentals/_03_Arrays/_02_Exercise/_02_CommonElements/_02_CommonElements.cs
+++ 24_C#Fundamentals/_03_Arrays/_02_Exercise/_02_CommonElements/_02_CommonElements.cs
@@ -5,15 +5,14 @@ public class _02_CommonElements
     public static void Main(string[] args)
     {
         // Read input values
-        string[] arrOne = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-        string[] arrTwo = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] arrOne = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+        string[] arrTwo = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries);
 
         // Print common elements in two arrays
         for (int i = 0; i < arrOne.Length; i++)
         {
             for (int j = 0; j < arrTwo.Length; j++)
             {
-
                 if (arrOne[i].Equals(arrTwo[j]))
                 {
                    Console.Write(arrTwo[j] + " ");
