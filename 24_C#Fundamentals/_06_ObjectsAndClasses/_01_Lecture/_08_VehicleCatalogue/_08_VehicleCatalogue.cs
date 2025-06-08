e6def6dd331741197d349a563d3207557ae9a34a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-09 00:55:54 +0300

    08

---
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_01_Lecture/_08_VehicleCatalogue/_08_VehicleCatalogue.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class _08_VehicleCatalogue
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variable
+        List<Student> studentList = new List<Student>();
+
+        // Save a list of students and their information
+        while (!console.Equals("end"))
+        {
+            string[] lineFromConsole = console.Split(" ");
+
+            string firstName = lineFromConsole[0];
+            string secondName = lineFromConsole[1];
+            int age = int.Parse(lineFromConsole[2]);
+            string town = lineFromConsole[3];
+
+            Student student = new Student(firstName, secondName, age, town);
+            studentList.Add(student);
+
+            console = Console.ReadLine();
+        }
+
+        string consoleTown = Console.ReadLine();
+
+        // Print result
+        foreach (Student element in studentList)
+        {
+            if (element.Town.Equals(consoleTown))
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
+
+// Create practice Student class
+class Student
+{
+    public string FirstName { get; }
+    public string SecondName { get; }
+    public int Age { get; }
+    public string Town { get; }
+
+    public Student(string firstName, string secondName, int age, string town)
+    {
+        FirstName = firstName;
+        SecondName = secondName;
+        Age = age;
+        Town = town;
+    }
+
+    public override string ToString()
+    {
+        return $"{FirstName} {SecondName} is {Age} years old.";
+    }
+}
