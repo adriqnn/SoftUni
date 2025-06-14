f2f6ab3e939ac433932e8f1a46f4dcea9a166601
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-15 00:36:45 +0300

    05

---
+++ 24_C#Fundamentals/_06_ObjectsAndClasses/_03_MoreExercises/_05_OldestFamilyMember/_05_OldestFamilyMember.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class _05_OldestFamilyMember
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int n = int.Parse(Console.ReadLine());
+
+        // Add variable
+        List<Person> list = new List<Person>();
+
+        // Person db
+        for (int i = 0; i < n; i++)
+        {
+            string[] consoleLine = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = consoleLine[0];
+            int age = int.Parse(consoleLine[1]);
+
+            Person person = new Person(name, age);
+            list.Add(person);
+        }
+
+        list = list.OrderByDescending(e => e.Age).ToList();
+        Console.WriteLine(list[0]);
+    }
+}
+
+// Create practice Person class
+class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name} {Age}";
+    }
+}
