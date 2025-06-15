828ee0951689296ed207f8bb028378da85be8c97
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-16 00:32:48 +0300

    01

--- 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_01_CountRealNumbers/_01_CountRealNumbers.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_01_CountRealNumbers/_01_CountRealNumbers.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class _01_CountRealNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] input = Console.ReadLine().Split(" ");
+        double[] nums = input.Select(double.Parse).ToArray();
+
+        // Add variable
+        SortedDictionary<double, int> numbers = new SortedDictionary<double, int>();
+
+        // Count numbers
+        foreach (double number in nums)
+        {
+            if (!numbers.ContainsKey(number))
+            {
+                numbers[number] = 1;
+            }
+            else
+            {
+                numbers[number]++;
+            }
+        }
+
+        // Print result
+        foreach (KeyValuePair<double, int> entry in numbers)
+        {
+            Console.WriteLine($"{entry.Key:0} -> {entry.Value}");
+        }
+    }
+}
