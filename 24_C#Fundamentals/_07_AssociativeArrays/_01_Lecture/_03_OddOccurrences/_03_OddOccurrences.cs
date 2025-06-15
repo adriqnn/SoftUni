f1633e473b16a2bb9a2550fea2c98a47a09a8336
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-16 00:52:09 +0300

    03

--- 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_03_OddOccurrences/_03_OddOccurrences.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_03_OddOccurrences/_03_OddOccurrences.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_OddOccurrences
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] words = Console.ReadLine().Split(" ");
+
+        // Add variable
+        Dictionary<string, int> counts = new Dictionary<string, int>();
+
+        // Extract the elements that occur odd times
+        foreach (string word in words)
+        {
+            string wordInLowerCase = word.ToLower();
+
+            if (counts.ContainsKey(wordInLowerCase))
+            {
+                counts[wordInLowerCase]++;
+            }
+            else
+            {
+                counts[wordInLowerCase] = 1;
+            }
+        }
+
+        List<string> odds = new List<string>();
+
+        foreach (KeyValuePair<string, int> entry in counts)
+        {
+            if (entry.Value % 2 == 1)
+            {
+                odds.Add(entry.Key);
+            }
+        }
+
+        // Print result
+        for (int i = 0; i < odds.Count; i++)
+        {
+            Console.Write(odds[i].ToString() + " ");
+        }
+    }
+}
