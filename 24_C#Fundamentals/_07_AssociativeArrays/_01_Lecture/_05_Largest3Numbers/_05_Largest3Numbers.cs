967fb6d456b3dc758329213fa288007d7d6bc16a
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-16 01:16:59 +0300

    05

--- 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_05_Largest3Numbers/_05_Largest3Numbers.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_05_Largest3Numbers/_05_Largest3Numbers.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_Largest3Numbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] input = Console.ReadLine().Split(" ");
+
+        // Parse, sort descending, take top 3, and print
+        int[] numbers = input.Select(s => int.Parse(s)).ToArray();
+        int[] topThree = numbers.OrderByDescending(n => n).Take(3).ToArray();
+
+        // Sort, limit and print result
+        foreach (int number in topThree)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
