ed2cfd1fa9d8e727dbe16a90553ec936758efdf7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-16 01:15:14 +0300

    04

--- 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_04_WordFilter/_04_WordFilter.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_01_Lecture/_04_WordFilter/_04_WordFilter.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_WordFilter
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] input = Console.ReadLine().Split(" ");
+        string[] evenWords = input.Where(word => word.Length % 2 == 0).ToArray();
+
+        // Print result
+        foreach (string word in evenWords)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
