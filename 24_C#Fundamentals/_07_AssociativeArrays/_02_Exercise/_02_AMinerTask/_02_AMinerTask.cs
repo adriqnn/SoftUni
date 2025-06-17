4710ae1445dbf93a4ec8dd427cfe1d04402317d9
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-17 23:29:42 +0300

    02

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_02_AMinerTask/_02_AMinerTask.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_02_AMinerTask/_02_AMinerTask.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_AMinerTask
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string resource = Console.ReadLine();
+
+        // Add variable
+        Dictionary<string, int> resourcesQuantity = new Dictionary<string, int>();
+
+        // Complete miners task
+        while (!resource.Equals("stop"))
+        {
+            int quantity = int.Parse(Console.ReadLine());
+
+            if (!resourcesQuantity.ContainsKey(resource))
+            {
+                resourcesQuantity[resource] = quantity;
+            }
+            else
+            {
+                resourcesQuantity[resource] += quantity;
+            }
+
+            resource = Console.ReadLine();
+        }
+
+        // Print result
+        foreach (var curr in resourcesQuantity)
+        {
+            Console.WriteLine($"{curr.Key} -> {curr.Value}");
+        }
+    }
+}
