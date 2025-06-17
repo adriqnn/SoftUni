fc239972b84f579c7822681eb625f396ee8f2b99
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-17 23:22:55 +0300

    01

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_01_CountCharsInAString/_01_CountCharsInAString.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_01_CountCharsInAString/_01_CountCharsInAString.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_CountCharsInAString
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string text = Console.ReadLine();
+
+        // Add variable
+        Dictionary<char, int> lettersCount = new Dictionary<char, int>();
+
+        // Count chars in a string
+        foreach (char currentSymbol in text)
+        {
+            if (currentSymbol == ' ')
+            {
+                continue;
+            }
+
+            if (!lettersCount.ContainsKey(currentSymbol))
+            {
+                lettersCount[currentSymbol] = 1;
+            }
+            else
+            {
+                lettersCount[currentSymbol]++;
+            }
+        }
+
+        // Print result
+        // Can use var instead!
+        foreach (KeyValuePair<char, int> current in lettersCount)
+        {
+            Console.WriteLine($"{current.Key} -> {current.Value}");
+        }
+    }
+}
