54c4fb0501f2721dd53dddcfe8af872490d62eb5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-19 23:43:14 +0300

    03

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_03_Orders/_03_Orders.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_03_Orders/_03_Orders.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_Orders
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variable
+        Dictionary<string, List<double>> orders = new Dictionary<string, List<double>>();
+
+        // Combine all the orders and sort them
+        while (input != "buy")
+        {
+            string[] order = input.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries);
+
+            string item = order[0];
+            double price = double.Parse(order[1]);
+            double quantity = double.Parse(order[2]);
+
+            if (!orders.ContainsKey(item))
+            {
+                orders[item] = new List<double> { price, quantity };
+            }
+            else
+            {
+                if (orders[item][0] != price)
+                {
+                    orders[item][0] = price;
+                }
+
+                orders[item][1] += quantity;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        foreach (KeyValuePair<string, List<double>> item in orders)
+        {
+            Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):F2}");
+        }
+    }
+}
