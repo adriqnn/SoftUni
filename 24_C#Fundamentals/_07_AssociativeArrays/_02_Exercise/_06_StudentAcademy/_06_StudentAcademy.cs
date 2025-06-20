70351f57119395d31e58cd802084769e0051ebb5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-20 23:49:22 +0300

    06

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_06_StudentAcademy/_06_StudentAcademy.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_06_StudentAcademy/_06_StudentAcademy.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _06_StudentAcademy
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int inputNumber = int.Parse(Console.ReadLine());
+
+        // Add variable
+        Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+
+        // Create Student stats
+        for (int i = 0; i < inputNumber; i++)
+        {
+            string name = Console.ReadLine();
+            double grade = double.Parse(Console.ReadLine());
+
+            if (!students.ContainsKey(name))
+            {
+                students[name] = new List<double>();
+            }
+
+            students[name].Add(grade);
+        }
+
+        // Print result
+        students.Where(s => s.Value.Average() >= 4.5).ToList().ForEach(s => Console.WriteLine($"{s.Key} -> {s.Value.Average():F2}"));
+    }
+}
