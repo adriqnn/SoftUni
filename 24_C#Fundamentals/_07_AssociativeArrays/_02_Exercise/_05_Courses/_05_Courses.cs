e354002fd67fdcdc7e0d1a7ab5b9832e57bba35d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-20 23:41:16 +0300

    05

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_05_Courses/_05_Courses.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_05_Courses/_05_Courses.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_Courses
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variable
+        Dictionary<string, List<string>> courses = new Dictionary<string, List<string>>();
+
+        // Register courses
+        while (!input.Equals("end"))
+        {
+            string[] courseInformation = input.Split(" : ");
+
+            string course = courseInformation[0];
+            string name = courseInformation[1];
+
+            if (!courses.ContainsKey(course))
+            {
+                List<string> names = new List<string>();
+                names.Add(name);
+
+                courses[course] = names;
+            }
+            else
+            {
+                courses[course].Add(name);
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        foreach (var course in courses) 
+        {
+            Console.WriteLine($"{course.Key}: {course.Value.Count}");
+            foreach (var e in course.Value)
+            {
+                Console.WriteLine($"-- {e}");
+            }
+        }
+    }
+}
