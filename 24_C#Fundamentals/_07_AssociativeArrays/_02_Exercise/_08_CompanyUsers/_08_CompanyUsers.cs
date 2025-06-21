e36a31e6f8afc66b1a502550b0502e5633cf4140
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-21 23:55:06 +0300

    08

--- 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_08_CompanyUsers/_08_CompanyUsers.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_02_Exercise/_08_CompanyUsers/_08_CompanyUsers.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+
+public class _08_CompanyUsers
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variable
+        Dictionary<string, HashSet<string>> employees = new Dictionary<string, HashSet<string>>();
+
+        // Make employee database
+        while (!input.Equals("End"))
+        {
+            string[] line = input.Split(" -> ");
+
+            string company = line[0];
+            string id = line[1];
+
+            if (!employees.ContainsKey(company))
+            {
+                employees[company] = new HashSet<string>();
+            }
+
+            employees[company].Add(id);
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        foreach (var kvp in employees)
+        {
+            Console.WriteLine(kvp.Key);
+            foreach (var id in kvp.Value)
+            {
+                Console.WriteLine($"-- {id}");
+            }
+        }
+    }
+}
