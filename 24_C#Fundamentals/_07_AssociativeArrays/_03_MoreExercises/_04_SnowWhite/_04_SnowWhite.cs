377c87e3487fb3021089db54de991d504e884c30
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-22 23:13:35 +0300

    04

--- 24_C#Fundamentals/_07_AssociativeArrays/_03_MoreExercises/_04_SnowWhite/_04_SnowWhite.cs
+++ 24_C#Fundamentals/_07_AssociativeArrays/_03_MoreExercises/_04_SnowWhite/_04_SnowWhite.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _04_SnowWhite
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variables
+        Dictionary<string, int> dwarfs = new Dictionary<string, int>();
+        Dictionary<string, int> colorGroups = new Dictionary<string, int>();
+
+        // Create database for dwarfs
+        while (!input.Equals("Once upon a time"))
+        {
+            string[] information = input.Split(" <:> ");
+
+            string name = information[0];
+            string color = information[1];
+            int physics = int.Parse(information[2]);
+
+            string key = name + ">>>>>" + color;
+
+            if (!colorGroups.ContainsKey(color))
+            {
+                colorGroups[color] = 0;
+            }
+
+            if (!dwarfs.ContainsKey(key))
+            {
+                dwarfs[key] = physics;
+                colorGroups[color]++;
+            }
+            else
+            {
+                if (dwarfs[key] < physics)
+                {
+                    dwarfs[key] = physics;
+                }
+            }
+
+            input = Console.ReadLine();
+        }
+
+        // Print result
+        var sorted = dwarfs.OrderByDescending(x => x.Value).ThenByDescending(x =>
+        {
+            string color = x.Key.Split(">>>>>")[1];
+            return colorGroups[color];
+        });
+        foreach (var key in sorted) 
+        {
+            string[] parts = key.Key.Split(">>>>>");
+            string name = parts[0];
+            string color = parts[1];
+            int physics = key.Value;
+
+            Console.WriteLine($"({color}) {name} <-> {physics}");
+        }
+    }
+}
