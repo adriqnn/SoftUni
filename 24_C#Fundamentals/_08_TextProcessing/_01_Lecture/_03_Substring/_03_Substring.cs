8e9f6e24bf4d801963e454b3872a0460578abece
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 01:19:57 +0300

    03

--- 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_03_Substring/_03_Substring.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_03_Substring/_03_Substring.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Linq;
+
+public class _03_Substring
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string stringToRemove = Console.ReadLine();
+        string consoleString = Console.ReadLine();
+
+        // Remove the string from console
+        while (consoleString.Contains(stringToRemove))
+        {
+            int index = consoleString.IndexOf(stringToRemove);
+            if (index != -1)
+            {
+                consoleString = consoleString.Remove(index, stringToRemove.Length);
+            }
+        }
+
+        // Print result
+        Console.WriteLine(consoleString);
+    }
+}
