7cc87dec2fbd2c275e7d53b2cc02b0bee7bd13ee
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-23 22:58:18 +0300

    01

--- 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_01_ReverseStrings/_01_ReverseStrings.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_01_ReverseStrings/_01_ReverseStrings.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _01_ReverseStrings
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Reverse strings
+        while (!input.Equals("end"))
+        {
+            string original = input;
+            string reversed = new string(input.Reverse().ToArray());
+
+            // Print result
+            Console.WriteLine($"{original} = {reversed}");
+
+            input = Console.ReadLine();
+        }
+    }
+}
