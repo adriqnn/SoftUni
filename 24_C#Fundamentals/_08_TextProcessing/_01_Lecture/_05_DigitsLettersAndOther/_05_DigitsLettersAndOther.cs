a8ace42f3dd2bd8a4c622e92fe354e08ba6e1bd4
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 01:29:26 +0300

    05

--- 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_05_DigitsLettersAndOther/_05_DigitsLettersAndOther.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_05_DigitsLettersAndOther/_05_DigitsLettersAndOther.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _05_DigitsLettersAndOther
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variables
+        StringBuilder digits = new StringBuilder();
+        StringBuilder letters = new StringBuilder();
+        StringBuilder other = new StringBuilder();
+
+        // Separate digits, letters and other
+        for (int i = 0; i < console.Count(); i++)
+        {
+            char symbol = console[i];
+
+            if (char.IsDigit(symbol))
+            {
+                digits.Append(symbol);
+            }
+            else if (char.IsLetter(symbol))
+            {
+                letters.Append(symbol);
+            }
+            else
+            {
+                other.Append(symbol);
+            }
+        }
+
+        // Print result
+        Console.WriteLine(digits);
+        Console.WriteLine(letters);
+        Console.WriteLine(other);
+
+    }
+}
