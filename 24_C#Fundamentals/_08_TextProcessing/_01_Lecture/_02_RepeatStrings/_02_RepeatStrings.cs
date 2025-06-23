2ce65dfc5cdb1bbd4a9b8edea9c2f2eaf97cf782
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 00:21:54 +0300

    02

--- 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_02_RepeatStrings/_02_RepeatStrings.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_02_RepeatStrings/_02_RepeatStrings.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+public class _02_RepeatStrings
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] console = Console.ReadLine().Split(" ");
+
+        // Repeat string
+        foreach (string word in console)
+        {
+            string repeatedWord = repeatWord(word, word.Count());
+
+            // Print result
+            Console.Write(repeatedWord);
+        }
+    }
+
+    // Method that concatenates a string multiple times
+    private static string repeatWord(string word, int times)
+    {
+        var result = new StringBuilder();
+        for (int i = 0; i < times; i++)
+        {
+            result.Append(word);
+        }
+
+        return result.ToString();
+    }
+}
