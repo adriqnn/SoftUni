c8ab45e27e082e6dfbac00cf844758a69f443592
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-23 22:44:09 +0300

    01

--- 23_C#Basics/_01_FirstStepsInProgramming/_01_HelloSoftUni/_01_HelloSoftUni.cs
+++ 23_C#Basics/_01_FirstStepsInProgramming/_01_HelloSoftUni/_01_HelloSoftUni.cs
@@ -4,7 +4,18 @@ public class _01_HelloSoftUni
 {
     public static void Main(string[] args)
     {
-        // Print a greeting message
-        Console.WriteLine("Hello SoftUni");
+        // Read input value
+        StringBuilder input = new StringBuilder(scan.nextLine());
+        
+        // Reverse strings
+        while (!input.toString().equals("end")) {
+            String original = input.toString();
+            input.reverse();
+            
+            // Print result
+            System.out.printf("%s = %s%n", original, input);
+            
+            input = new StringBuilder(scan.nextLine());
+        }
     }
 }
