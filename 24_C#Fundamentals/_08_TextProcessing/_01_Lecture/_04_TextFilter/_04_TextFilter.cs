3ca99985a3d2737e5cf706d1eeed9fa4890498a2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 01:26:38 +0300

    04

--- 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_04_TextFilter/_04_TextFilter.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_01_Lecture/_04_TextFilter/_04_TextFilter.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _04_TextFilter
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] words = Console.ReadLine().Split(", ");
+        string text = Console.ReadLine();
+
+        // Filter the words
+        foreach (string word in words)
+        {
+            string replacement = new string('*', word.Length);
+            text = text.Replace(word, replacement);
+        }
+
+        // Print result
+        Console.WriteLine(text);
+    }
+}
