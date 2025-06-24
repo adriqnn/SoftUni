03208362719e4aaf38c8a2ec34e7d16d6890a2a6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:48:35 +0300

    02

--- 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_02_ASCIISumator/_02_ASCIISumator.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_02_ASCIISumator/_02_ASCIISumator.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _02_ASCIISumator
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        char one = Console.ReadLine()[0];
+        char two = Console.ReadLine()[0];
+        string consoleLine = Console.ReadLine();
+
+        // Add variable
+        StringBuilder charList = new StringBuilder();
+
+        // Generate character string
+        for (int i = one + 1; i < two; i++)
+        {
+            charList.Append((char) i);
+        }
+
+        int sum = 0;
+
+        // Extract repeating characters
+        for (int i = 0; i < charList.Length; i++)
+        {
+            for (int j = 0; j < consoleLine.Length; j++)
+            {
+                if (charList[i] == consoleLine[j])
+                {
+                    sum += charList[i];
+                }
+            }
+        }
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
