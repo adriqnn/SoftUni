7718072a28b8c9e6c9f86ca1bb1179623bb0a8f0
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:40:53 +0300

    08

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_08_LettersChangeNumbers/_08_LettersChangeNumbers.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_08_LettersChangeNumbers/_08_LettersChangeNumbers.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _08_LettersChangeNumbers
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] console = Console.ReadLine().Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Add variable
+        double sum = 0;
+
+        // Solve numbers game
+        for (int i = 0; i < console.Length; i++)
+        {
+            char first = console[i][0];
+            char last = console[i][console[i].Length - 1];
+
+            double num = double.Parse(console[i].Substring(1, console[i].Length - 2));
+            double sumThisOne = 0;
+
+            if (char.IsLower(first))
+            {
+                sumThisOne = num * (first - 96);
+            }
+            else
+            {
+                sumThisOne = num / (first - 64);
+            }
+
+            if (char.IsLower(last))
+            {
+                sumThisOne += last - 96;
+            }
+            else
+            {
+                sumThisOne -= last - 64;
+            }
+
+            sum += sumThisOne;
+        }
+
+        // Print result
+        Console.WriteLine($"{sum:F2}", sum);
+    }
+}
