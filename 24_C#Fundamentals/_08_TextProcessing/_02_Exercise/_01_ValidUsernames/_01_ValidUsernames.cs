c4803192e52781661c1dbde3426267d58a8df07c
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 22:43:52 +0300

    01

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_01_ValidUsernames/_01_ValidUsernames.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_01_ValidUsernames/_01_ValidUsernames.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _01_ValidUsernames
+{
+    public static void Main(string[] args)
+    {
+        string[] console = Console.ReadLine().Split(", ");
+
+        // Validate usernames
+        for (int i = 0; i < console.Length; i++)
+        {
+            // Validate the length
+            if (console[i].Length >= 3 && console[i].Length <= 16)
+            {
+                bool isValid = true;
+
+                // Validate characters
+                for (int j = 0; j < console[i].Length; j++)
+                {
+                    char symbol = console[i][j];
+
+                    isValid = char.IsLetterOrDigit(symbol) || symbol == '-' || symbol == '_';
+                    if (!isValid)
+                    {
+                        break;
+                    }
+                }
+
+                // Print result
+                if (isValid)
+                {
+                    Console.WriteLine(console[i]);
+                }
+            }
+        }
+    }
+}
