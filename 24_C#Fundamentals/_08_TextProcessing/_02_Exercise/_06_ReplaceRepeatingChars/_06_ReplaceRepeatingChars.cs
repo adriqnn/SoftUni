fe5eb351649674136088cb46cda804a192463bd5
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:16:29 +0300

    06

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_06_ReplaceRepeatingChars/_06_ReplaceRepeatingChars.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_06_ReplaceRepeatingChars/_06_ReplaceRepeatingChars.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text;
+
+public class _06_ReplaceRepeatingChars
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        StringBuilder console = new StringBuilder(Console.ReadLine());
+
+        // Replace repeating character
+        for (int i = 1; i < console.Length; i++)
+        {
+            if (console[i] == console[i - 1])
+            {
+                console.Remove(i, 1);
+                i--;
+            }
+        }
+
+        // Print result
+        Console.WriteLine(console);
+    }
+}
