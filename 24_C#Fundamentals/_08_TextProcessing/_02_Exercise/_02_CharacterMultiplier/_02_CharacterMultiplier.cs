d141860e01afa50cf4e69721079515f0b860252d
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 22:54:37 +0300

    02

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_02_CharacterMultiplier/_02_CharacterMultiplier.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_02_CharacterMultiplier/_02_CharacterMultiplier.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_CharacterMultiplier
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] console = Console.ReadLine().Split(" ");
+
+        // Add variables
+        string firstWord = console[0];
+        string secondWord = console[1];
+        int sum = 0;
+
+        // Multiply characters
+        if (firstWord.Length > secondWord.Length)
+        {
+            for (int i = 0; i < secondWord.Length; i++)
+            {
+                sum += firstWord[i] * secondWord[i];
+            }
+
+            for (int i = secondWord.Length; i < firstWord.Length; i++)
+            {
+                sum += firstWord[i];
+            }
+        }
+        else if (firstWord.Length < secondWord.Length)
+        {
+            for (int i = 0; i < firstWord.Length; i++)
+            {
+                sum += firstWord[i] * secondWord[i];
+            }
+
+            for (int i = firstWord.Length; i < secondWord.Length; i++)
+            {
+                sum += secondWord[i];
+            }
+        }
+        else
+        {
+            for (int i = 0; i < secondWord.Length; i++)
+            {
+                sum += firstWord[i] * secondWord[i];
+            }
+        }
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
