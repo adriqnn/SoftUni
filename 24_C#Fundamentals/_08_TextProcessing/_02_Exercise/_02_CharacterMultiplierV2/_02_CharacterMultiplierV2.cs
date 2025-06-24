09efb7f7adf440f29a89f9452837f25a2f9a6e61
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:00:25 +0300

    02

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_02_CharacterMultiplierV2/_02_CharacterMultiplierV2.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_02_CharacterMultiplierV2/_02_CharacterMultiplierV2.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _02_CharacterMultiplierV2
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] console = Console.ReadLine().Split(" ");
+
+        // Add variables
+        string firstWord = console[0];
+        string secondWord = console[1];
+        int sum = 0;
+
+        // Multiply characters
+        int minLength = Math.Min(firstWord.Length, secondWord.Length);
+        int maxLength = Math.Max(firstWord.Length, secondWord.Length);
+
+        for (int i = 0; i < minLength; i++)
+        {
+            sum += firstWord[i] * secondWord[i];
+        }
+
+        string longerWord = (firstWord.Length > secondWord.Length) ? firstWord : secondWord;
+
+        for (int i = minLength; i < maxLength; i++)
+        {
+            sum += longerWord[i];
+        }
+
+        // Print result
+        Console.WriteLine(sum);
+    }
+}
