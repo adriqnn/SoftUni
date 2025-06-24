f2507ec937cc9a1998530c593f05a52b08ac2a23
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:46:06 +0300

    01

--- 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_01_ExtractPersonInformation/_01_ExtractPersonInformation.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_01_ExtractPersonInformation/_01_ExtractPersonInformation.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _01_ExtractPersonInformation
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        int numberOfStrings = int.Parse(Console.ReadLine());
+
+        // Add variables
+        string regexName = "@(?<name>[A-Za-z]+)\\|";
+        string regexAge = "#(?<age>[0-9]+)\\*";
+        Regex patterName = new Regex(regexName);
+        Regex patternAge = new Regex(regexAge);
+
+        // Extract user information
+        for (int i = 0; i < numberOfStrings; i++)
+        {
+            string newLine = Console.ReadLine();
+
+            Match matcherName = patterName.Match(newLine);
+            Match matcherAge = patternAge.Match(newLine);
+
+            string name = "";
+            string age = "";
+
+            if (matcherName.Success)
+            {
+                name = matcherName.Groups["name"].Value;
+            }
+
+            if (matcherAge.Success)
+            {
+                age = matcherAge.Groups["age"].Value;
+            }
+
+            if (name.Length > 0 && age.Length > 0)
+            {
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+    }
+}
