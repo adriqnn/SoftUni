229fc6b2ce4fc9264245d6a228c3a0d6e239d9e8
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-25 00:04:14 +0300

    04

--- 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_04_MorseCodeTranslator/_04_MorseCodeTranslator.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_04_MorseCodeTranslator/_04_MorseCodeTranslator.cs
@@ -1,2 +1,109 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class _04_MorseCodeTranslator
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string code = Console.ReadLine();
+
+        // Add variable
+        List<string> morseCode = code.Split(" | ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+        // Decipher morse code message
+        for (int i = 0; i < morseCode.Count; i++)
+        {
+            string[] actualDecode = morseCode[i].Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int j = 0; j < actualDecode.Length; j++)
+            {
+                string thisPart = actualDecode[j].Trim();
+
+                switch (thisPart)
+                {
+                    case ".-":
+                        Console.Write("A");
+                        break;
+                    case "-...":
+                        Console.Write("B");
+                        break;
+                    case "-.-.":
+                        Console.Write("C");
+                        break;
+                    case "-..":
+                        Console.Write("D");
+                        break;
+                    case ".":
+                        Console.Write("E");
+                        break;
+                    case "..-.":
+                        Console.Write("F");
+                        break;
+                    case "--.":
+                        Console.Write("G");
+                        break;
+                    case "....":
+                        Console.Write("H");
+                        break;
+                    case "..":
+                        Console.Write("I");
+                        break;
+                    case ".---":
+                        Console.Write("J");
+                        break;
+                    case "-.-":
+                        Console.Write("K");
+                        break;
+                    case ".-..":
+                        Console.Write("L");
+                        break;
+                    case "--":
+                        Console.Write("M");
+                        break;
+                    case "-.":
+                        Console.Write("N");
+                        break;
+                    case "---":
+                        Console.Write("O");
+                        break;
+                    case ".--.":
+                        Console.Write("P");
+                        break;
+                    case "--.-":
+                        Console.Write("Q");
+                        break;
+                    case ".-.":
+                        Console.Write("R");
+                        break;
+                    case "...":
+                        Console.Write("S");
+                        break;
+                    case "-":
+                        Console.Write("T");
+                        break;
+                    case "..-":
+                        Console.Write("U");
+                        break;
+                    case "...-":
+                        Console.Write("V");
+                        break;
+                    case ".--":
+                        Console.Write("W");
+                        break;
+                    case "-..-":
+                        Console.Write("X");
+                        break;
+                    case "-.--":
+                        Console.Write("Y");
+                        break;
+                    case "--..":
+                        Console.Write("Z");
+                        break;
+                }
+            }
+
+            // Print result
+            Console.Write(" ");
+        }
+    }
+}
