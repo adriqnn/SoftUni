2b95f7e55e84bcaf1d81395b2f26ffa87709713e
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:07:55 +0300

    04

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_04_CaesarCipher/_04_CaesarCipher.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_04_CaesarCipher/_04_CaesarCipher.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+public class _04_CaesarCipher
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variable
+        StringBuilder encrypt = new StringBuilder();
+
+        // Encrypt the message
+        for (int i = 0; i < console.Length; i++)
+        {
+            char symbol = (char)(console[i] + 3);
+
+            encrypt.Append(symbol);
+        }
+
+        // Print result
+        Console.WriteLine(encrypt);
+    }
+}
