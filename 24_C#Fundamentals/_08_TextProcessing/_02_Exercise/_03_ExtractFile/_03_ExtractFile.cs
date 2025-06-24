5348413e8524cb1df24c5f6cef4f11b74d2810a6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-24 23:03:48 +0300

    03

--- 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_03_ExtractFile/_03_ExtractFile.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_02_Exercise/_03_ExtractFile/_03_ExtractFile.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _03_ExtractFile
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string[] console = Console.ReadLine().Split("\\");
+
+        // Add variable
+        string[] file = console[console.Length - 1].Split(".");
+
+        // Print result
+        Console.WriteLine($"File name: {file[0]}\nFile extension: {file[1]}", file[0], file[1]);
+    }
+}
