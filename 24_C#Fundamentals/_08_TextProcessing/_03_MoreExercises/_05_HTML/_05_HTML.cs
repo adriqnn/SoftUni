c351083de7e008f8fb57439c0c18a3cf15136320
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-25 00:07:10 +0300

    05

--- 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_05_HTML/_05_HTML.cs
+++ 24_C#Fundamentals/_08_TextProcessing/_03_MoreExercises/_05_HTML/_05_HTML.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+public class _05_HTML
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        string title = Console.ReadLine();
+        string content = Console.ReadLine();
+        string comments = Console.ReadLine();
+
+        // Add variable
+        List<string> commentsList = new List<string>();
+
+        // Create the HTML list of comments
+        while (!comments.Equals("end of comments"))
+        {
+            commentsList.Add(comments);
+            comments = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine($"<h1>\n    {title}\n</h1>");
+        Console.WriteLine($"<article>\n    {content}\n</article>");
+        for (int i = 0; i < commentsList.Count; i++)
+        {
+            Console.WriteLine($"<div>\n    {commentsList[i]}\n</div>");
+        }
+    }
+}
