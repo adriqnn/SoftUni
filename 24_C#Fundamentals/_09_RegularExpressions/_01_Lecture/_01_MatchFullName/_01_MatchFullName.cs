104a34ad1acdd5a454c3e56704e763bb4e102878
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-28 23:07:06 +0300

    01

--- 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_01_MatchFullName/_01_MatchFullName.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_01_MatchFullName/_01_MatchFullName.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _01_MatchFullName
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variables
+        string regexName = "(?<name>\\b[A-Z][a-z]+ [A-Z][a-z]+\\b)";
+        Regex patternName = new Regex(regexName);
+        Match matcherName = patternName.Match(console);
+
+        // Match the name and print result
+        while (matcherName.Success)
+        {
+            string find = matcherName.Groups["name"].Value;
+            Console.Write(find + " ");
+
+            matcherName = matcherName.NextMatch();
+        }
+    }
+}
