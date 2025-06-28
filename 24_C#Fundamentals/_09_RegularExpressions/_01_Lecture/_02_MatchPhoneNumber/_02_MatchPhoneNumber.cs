b87c16a45fbeb0654df1116d452f39499630e6e6
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-28 23:10:11 +0300

    02

--- 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_02_MatchPhoneNumber/_02_MatchPhoneNumber.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_02_MatchPhoneNumber/_02_MatchPhoneNumber.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _03_MatchDates
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variables
+        string regexPhone = "\\+359(?<separator>[ -])2\\1\\d{3}\\1\\d{4}\\b";
+        Regex patternPhone = new Regex(regexPhone);
+        Match matcherPhone = patternPhone.Match(console);
+        List<string> matchedPhones = new List<string>();
+
+        // Match phone numbers and save them
+        while (matcherPhone.Success)
+        {
+            matchedPhones.Add(matcherPhone.Value);
+            matcherPhone = matcherPhone.NextMatch();
+        }
+
+        // Print result
+        Console.WriteLine(string.Join(", ", matchedPhones));
+    }
+}
