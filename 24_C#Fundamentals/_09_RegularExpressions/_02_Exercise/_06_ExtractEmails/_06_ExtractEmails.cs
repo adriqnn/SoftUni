792e3493ca54b3010247b80195cb2219cc156bf2
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-30 00:01:52 +0300

    06

--- 24_C#Fundamentals/_09_RegularExpressions/_02_Exercise/_06_ExtractEmails/_06_ExtractEmails.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_02_Exercise/_06_ExtractEmails/_06_ExtractEmails.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class _06_ExtractEmails
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variables
+        string regexEmail = "((?<=\\s)[a-zA-Z0-9]+([-.]\\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\\.[a-z]{2,}))";
+        Regex patternEmail = new Regex(regexEmail);
+        Match matcherEmail = patternEmail.Match(input);
+
+        // Extract emails and print result
+        while (matcherEmail.Success)
+        {
+            Console.WriteLine(matcherEmail.Value);
+            matcherEmail = matcherEmail.NextMatch();
+        }
+    }
+}
