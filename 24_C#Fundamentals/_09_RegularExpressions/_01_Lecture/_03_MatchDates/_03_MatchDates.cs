6b18514f84241a69f28f771d9bb9d304fa28c2d7
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-29 22:13:53 +0300

    03

--- 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_03_MatchDates/_03_MatchDates.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_01_Lecture/_03_MatchDates/_03_MatchDates.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _03_MatchDates
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string dates = Console.ReadLine();
+
+        // Add variables
+        string regex = "\\b(?<day>\\d{2})(?<separator>[\\/\\-.])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
+        Regex pattern = new Regex(regex);
+        Match matcher = pattern.Match(dates);
+
+        // Match dates and print result
+        while (matcher.Success)
+        {
+            string day = matcher.Groups["day"].Value;
+            string month = matcher.Groups["month"].Value;
+            string year = matcher.Groups["year"].Value;
+
+            Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            matcher = matcher.NextMatch();
+        }
+    }
+}
