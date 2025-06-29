ff2504949a26c362ee74382c61b8dd4aef44ee96
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-29 22:24:33 +0300

    01

--- 24_C#Fundamentals/_09_RegularExpressions/_02_Exercise/_01_Furniture/_01_Furniture.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_02_Exercise/_01_Furniture/_01_Furniture.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Text.RegularExpressions;
+
+public class _01_Furniture
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string console = Console.ReadLine();
+
+        // Add variables
+        string regex = ">>(?<furniture>[A-Za-z]+)<<(?<price>[0-9]+.?[0-9]*)!(?<quantity>[0-9]+)";
+        Regex pattern = new Regex(regex);
+        List<string> furnitureList = new List<string>();
+        double totalMoney = 0;
+
+        // Purchase furniture
+        while (!console.Equals("Purchase"))
+        {
+            Match matcher = pattern.Match(console);
+
+            if (matcher.Success)
+            {
+                string furniture = matcher.Groups["furniture"].Value;
+
+                double price = double.Parse(matcher.Groups["price"].Value);
+                int quantity = int.Parse(matcher.Groups["quantity"].Value);
+
+                furnitureList.Add(furniture);
+
+                totalMoney += price * quantity;
+            }
+
+            console = Console.ReadLine();
+        }
+
+        // Print result
+        Console.WriteLine("Bought furniture:");
+        furnitureList.ForEach(i => Console.WriteLine(i));
+        Console.WriteLine($"Total money spend: {totalMoney:F2}");
+    }
+}
