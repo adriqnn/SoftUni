9ae40b3bf491eb11657d3502b594600987b46245
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-30 00:42:17 +0300

    02

--- 24_C#Fundamentals/_09_RegularExpressions/_03_MoreExercises/_02_RageQuit/_02_RageQuit.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_03_MoreExercises/_02_RageQuit/_02_RageQuit.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Text;
+
+public class _02_RageQuit
+{
+    public static void Main(string[] args)
+    {
+        // Read input value
+        string input = Console.ReadLine();
+
+        // Add variable
+        StringBuilder result = new StringBuilder();
+
+        string regexInput = "(?<match>(?<string>[^0-9]+)(?<number>[0-9]+))";
+        Regex patternInput = new Regex(regexInput);
+        MatchCollection matches = patternInput.Matches(input);
+
+        // Decipher the message
+        foreach (Match match in matches)
+        {
+            string stringType = match.Groups["string"].Value.ToUpper();
+            int times = int.Parse(match.Groups["number"].Value);
+
+            for (int i = 0; i < times; i++)
+            {
+                result.Append(stringType);
+            }
+        }
+
+        // Calculate distinct uppercase symbols in the final result
+        long uniqueCount = result.ToString().Distinct().Count();
+
+        // Print result
+        Console.WriteLine($"Unique symbols used: {uniqueCount}");
+        Console.WriteLine(result);
+    }
+}
