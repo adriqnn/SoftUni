9e387a5c23bce49779305886e41c5ca59e8a6907
adriqnn <109970483+adriqnn@users.noreply.github.com>
2025-06-30 00:54:25 +0300

    04

--- 24_C#Fundamentals/_09_RegularExpressions/_03_MoreExercises/_04_SantaSSecretHelper/_04_SantaSSecretHelper.cs
+++ 24_C#Fundamentals/_09_RegularExpressions/_03_MoreExercises/_04_SantaSSecretHelper/_04_SantaSSecretHelper.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Text;
+using System.Linq;
+
+public class _04_SantaSSecretHelper
+{
+    public static void Main(string[] args)
+    {
+        // Read input values
+        int subtractNumber = int.Parse(Console.ReadLine());
+        string input = Console.ReadLine();
+
+        // Read the message and decipher
+        while (!input.Equals("end"))
+        {
+            StringBuilder message = new StringBuilder();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                message.Append((char) (input[i] - subtractNumber));
+            }
+
+            string decodeRegex = @"@(?<name>[A-Za-z]+)[^@\-!:>]*!(?<grade>[GN])!";
+            Regex decodePattern = new Regex(decodeRegex);
+            MatchCollection decodeMatcher = decodePattern.Matches(message.ToString());
+
+            foreach (Match match in decodeMatcher)
+            {
+                if (match.Groups["grade"].Value.Equals("G"))
+                {
+                    Console.WriteLine(match.Groups["name"].Value);
+                }
+            }
+
+            input = Console.ReadLine();
+        }
+    }
+}
